b85adb7d6d4bd8e689bfba77f1cebcca51225463
David Hale <dghale@amazon.com>
2016-08-30 13:40:38 -0700

    initial example for Go SDK

---
+++ go/example_code/ec2/create_instance.go
@@ -0,0 +1,33 @@
+    svc := ec2.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
+    // Specify the details of the instance that you want to create.
+    runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
+        // An Amazon Linux AMI ID for t2.micro instances in the us-west-2 region
+        ImageId:      aws.String("ami-e7527ed7"),
+        InstanceType: aws.String("t2.micro"),
+        MinCount:     aws.Int64(1),
+        MaxCount:     aws.Int64(1),
+    })
+
+    if err != nil {
+        log.Println("Could not create instance", err)
+        return
+    }
+
+    log.Println("Created instance", *runResult.Instances[0].InstanceId)
+
+    // Add tags to the created instance
+    _ , errtag := svc.CreateTags(&ec2.CreateTagsInput{
+        Resources: []*string{runResult.Instances[0].InstanceId},
+        Tags: []*ec2.Tag{
+            {
+                Key:   aws.String("Name"),
+                Value: aws.String("MyFirstInstance"),
+            },
+        },
+    })
+    if errtag != nil {
+        log.Println("Could not create tags for instance", runResult.Instances[0].InstanceId, errtag)
+        return
+    }
+
+    log.Println("Successfully tagged instance")
