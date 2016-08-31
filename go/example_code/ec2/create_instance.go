c232f316d12b4a460c0be5ea10aaa444442e4bfd
David Hale <dghale@amazon.com>
2016-08-31 14:45:51 -0700

    add license info to code examples

--- go/example_code/ec2/create_instance.go
+++ go/example_code/ec2/create_instance.go
@@ -1,3 +1,17 @@
+/*
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
     svc := ec2.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
     // Specify the details of the instance that you want to create.
     runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
--- go/example_code/s3/list_all_buckets.go
+++ go/example_code/s3/list_all_buckets.go
@@ -1,3 +1,17 @@
+/*
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
     svc := s3.New(session.New(&aws.Config{Region: aws.String("us-west-2")}))
     result, err := svc.ListBuckets(&s3.ListBucketsInput{})
     if err != nil {
