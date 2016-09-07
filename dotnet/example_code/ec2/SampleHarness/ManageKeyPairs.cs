ae3b0c4a3e5ea5e65d145e0468bf8fed6260b4bb
David Hale <dghale@amazon.com>
2016-09-07 15:20:15 -0700

    Add initial set of .NET examples.

---
+++ dotnet/example_code/ec2/SampleHarness/ManageKeyPairs.cs
@@ -0,0 +1,75 @@
+﻿/*******************************************************************************
+* Copyright 2009-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+* 
+* http://aws.amazon.com/apache2.0/
+* 
+* or in the "license" file accompanying this file. This file is
+* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the specific
+* language governing permissions and limitations under the License.
+*******************************************************************************/
+
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.Samples
+{
+    public partial class Program
+    {
+
+        public static void CreateKeyPair( AmazonEC2Client ec2Client,  string keyPairName,  string privateKeyFile )
+        {
+            var request = new CreateKeyPairRequest();
+            request.KeyName = keyPairName;
+
+            try
+            {
+                var response = ec2Client.CreateKeyPair(request);
+                Console.WriteLine();
+                Console.WriteLine("New key: " + keyPairName);
+
+                // Save the private key in a .pem file
+                using (FileStream s = new FileStream(privateKeyFile, FileMode.Create))
+                using (StreamWriter writer = new StreamWriter(s))
+                {
+                    writer.WriteLine(response.KeyPair.KeyMaterial);
+                }
+            }
+            catch (AmazonEC2Exception ex)
+            {
+                // Check the ErrorCode to see if the key already exists.
+                if ("InvalidKeyPair.Duplicate" == ex.ErrorCode)
+                {
+                    Console.WriteLine("The key pair \"{0}\" already exists.", keyPairName);
+                }
+                else
+                {
+                    // The exception was thrown for another reason, so re-throw the exception.
+                    throw;
+                }
+            }
+        }
+
+        public static void EnumerateKeyPairs(AmazonEC2Client ec2Client)
+        {
+
+            var request = new DescribeKeyPairsRequest();
+            var response = ec2Client.DescribeKeyPairs(request);
+
+            foreach (KeyPairInfo item in response.KeyPairs)
+            {
+                Console.WriteLine("Existing key pair: " + item.KeyName);
+            }
+        }
+    }
+}
---
+++ dotnet/example_code/ec2/SampleHarness/ManageSecurityGroups.cs
@@ -0,0 +1,47 @@
+﻿/*******************************************************************************
+* Copyright 2009-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+* 
+* http://aws.amazon.com/apache2.0/
+* 
+* or in the "license" file accompanying this file. This file is
+* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the specific
+* language governing permissions and limitations under the License.
+*******************************************************************************/
+
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.Samples
+{
+    public partial class Program
+    {
+        static void EnumerateSecurityGroups(AmazonEC2Client ec2Client)
+        {
+            var request = new DescribeSecurityGroupsRequest();
+            var response = ec2Client.DescribeSecurityGroups(request);
+
+            List<SecurityGroup> mySGs = response.SecurityGroups;
+
+            foreach (SecurityGroup item in mySGs)
+            {
+                Console.WriteLine("Security group: ");
+                Console.WriteLine("\tGroupId: "      + item.GroupId);
+                Console.WriteLine("\tGroupName: "    + item.GroupName);
+                Console.WriteLine("\tVpcId: "        + item.VpcId);
+                Console.WriteLine("\tDescription: " + item.Description);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
