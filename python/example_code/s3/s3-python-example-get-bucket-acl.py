165361d58574f5fca030a21a88f1f5c2a3fd1b10
JordonPhillips <phjordon@amazon.com>
2016-12-14 15:36:29 -0800

    Add acl snippet

---
+++ python/example_code/s3/s3-python-example-get-bucket-acl.py
@@ -0,0 +1,19 @@
+# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import boto3
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Call to S3 to retrieve the policy for the given bucket
+result = s3.get_bucket_acl(Bucket='my-bucket')
