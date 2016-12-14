ef27e9c613ecfa5fbcb551bef125506d0b792fb7
Jason Del Ponte <delpontej@gmail.com>
2016-12-14 15:39:42 -0800

    Add S3 example for create and using buckets

---
+++ go/example_code/s3/s3_create_bucket.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Creates a S3 Bucket in the region configured in the shared config
+// or AWS_REGION environment variable.
+//
+// Usage:
+//    go run s3_create_bucket BUCKET_NAME
+func main() {
+	if len(os.Args) != 2 {
+		exitErrorf("bucket name required\nUsage: %s bucket_name", os.Args[0])
+	}
+	bucket := os.Args[1]
+
+	// Inititalize a session that the SDK will use to load configuration,
+	// credentials, and region from the shared config file. (~/.aws/config).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	// Create the S3 Bucket
+	_, err := svc.CreateBucket(&s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+	})
+	if err != nil {
+		exitErrorf("Unable to create bucket %q, %v", bucket, err)
+	}
+
+	// Wait until bucket is created before finishing
+	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
+	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+		Bucket: aws.String(bucket),
+	})
+	if err != nil {
+		exitErrorf("Error occurred while waiting for bucket to be created, %v", bucket)
+	}
+
+	fmt.Printf("Bucket %q succesffuly created\n", bucket)
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
