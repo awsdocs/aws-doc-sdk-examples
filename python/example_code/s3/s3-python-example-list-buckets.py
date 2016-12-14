2f1be1547e7ba9bc00e83bacf726ef7d4fe2d042
JordonPhillips <phjordon@amazon.com>
2016-12-14 13:22:04 -0800

    Add python bucket snippets

---
+++ python/example_code/s3/s3-python-example-list-buckets.py
@@ -0,0 +1,25 @@
+# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import boto3
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
