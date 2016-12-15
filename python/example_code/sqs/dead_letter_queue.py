496754c54005cf7e1b49ada8e612207f5e2846ff
kyleknap <kyleknap@amazon.com>
2016-12-15 15:12:43 -0800

    Add dead letter SQS queue example

---
+++ python/example_code/sqs/dead_letter_queue.py
@@ -0,0 +1,37 @@
+# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import json
+
+import boto3
+
+
+# Create SQS client
+sqs = boto3.client('sqs')
+
+queue_url = 'SOURCE_QUEUE_URL'
+dead_letter_queue_arn = 'DEAD_LETTER_QUEUE_ARN'
+
+redrive_policy = {
+    'deadLetterTargetArn': dead_letter_queue_arn,
+    'maxReceiveCount': '10'
+}
+
+
+# Configure queue to send messages to dead letter queue
+sqs.set_queue_attributes(
+    QueueUrl=queue_url,
+    Attributes={
+        'RedrivePolicy': json.dumps(redrive_policy)
+    }
+)
