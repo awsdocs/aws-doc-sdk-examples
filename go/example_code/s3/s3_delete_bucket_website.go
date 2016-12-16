e5e42119fcacd64d9ea694351f2eed46a41ddb0c
David Hale <dghale@amazon.com>
2016-12-16 15:59:21 -0800

    add copyright headers

--- go/example_code/s3/s3_delete_bucket_website.go
+++ go/example_code/s3/s3_delete_bucket_website.go
@@ -1,3 +1,17 @@
+/*
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
 package main
 
 import (
@@ -18,7 +32,7 @@ import (
 // that configuration
 //
 // Usage:
-//    go run s3_delete_bucekt_website.go BUCKET_NAME
+//    go run s3_delete_bucket_website.go BUCKET_NAME
 func main() {
 	if len(os.Args) != 2 {
 		exitErrorf("bucket name required\nUsage: %s bucket_name",
