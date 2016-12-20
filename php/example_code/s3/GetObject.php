c7a3b88236a37c29872b89ec03a84f00516d7a6c
shashank agarwal <agarwal.202@osu.edu>
2016-12-20 12:28:56 -0800

    Update GetObject.php

--- php/example_code/s3/GetObject.php
+++ php/example_code/s3/GetObject.php
@@ -19,7 +19,7 @@ use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
 /**
- * List your Amazon S3 buckets.
+ * Get/Download an Object from Amazon S3.
  *
  * This code expects that you have AWS credentials set up per:
  * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
