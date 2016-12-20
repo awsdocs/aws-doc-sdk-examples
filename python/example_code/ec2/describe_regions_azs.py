e255689338b0619c95b84f72da491408d5a8288e
stealthycoin <jcarlyl@amazon.com>
2016-12-20 10:35:43 -0800

    Python EC2 samples draft

---
+++ python/example_code/ec2/describe_regions_azs.py
@@ -0,0 +1,24 @@
+# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+
+ec2 = boto3.client('ec2')
+
+# Retrieves all regions/endpoints that work with EC2
+response = ec2.describe_regions()
+print('Regions:', response['Regions'])
+
+# Retrieves availability zones only for region of the ec2 object
+response = ec2.describe_availability_zones()
+print('Availability Zones:', response['AvailabilityZones'])
