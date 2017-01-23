8c6ff3b243ba12557299c23aeddc2cb4fbe1149a
David Deyo <daviddeyo@users.noreply.github.com>
2017-01-23 14:50:11 -0800

    Add files via upload
    Code examples for CloudWatch Logs subscription fileters

---
+++ javascript/example_code/cloudwatch/cwl_putsubscriptionfilter.js
@@ -0,0 +1,36 @@
+/*
+   Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Load credentials and set region from JSON file
+AWS.config.loadFromPath('./config.json');
+
+// Create CloudWatch service object
+var cwl = new AWS.CloudWatchLogs({apiVersion: '2014-03-28'});
+
+var params = {
+  destinationArn: 'LAMBDA_FUNCTION_ARN',
+  filterName: 'FILTER_NAME',
+  filterPattern: 'ERROR',
+  logGroupName: 'LOG_GROUP',
+};
+
+cwl.putSubscriptionFilter(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
