4bd9d84b54284f01e18935f6aa61eefab6110bea
imshashank <agshasha@amazon.com>
2017-03-02 13:01:17 -0800

    Add example code for IAM

---
+++ php/example_code/iam/DeleteLoginProfile.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+require 'vendor/autoload.php';
+
+use Aws\Iam\IamClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Deletes the password for the specified user, which terminates the user's ability
+ * to access AWS services through the AWS Management Console.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
+ */
+
+$client = new IamClient([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2010-05-08'
+]);
+
+try {
+    $result = $client->deleteLoginProfile(array(
+        // UserName is required
+        'UserName' => 'string',
+    ));
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
