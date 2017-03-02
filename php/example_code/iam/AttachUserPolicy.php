7b6dca38fc12671088c8c4b2a9bcd419103c5e05
imshashank <agshasha@amazon.com>
2017-03-02 13:55:02 -0800

    Add IAM example

---
+++ php/example_code/iam/AttachUserPolicy.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+require 'vendor/autoload.php';
+
+use Aws\Iam\IamClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Attaches the specified policy to the specified user
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
+ */
+
+$client = new IamClient([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2010-05-08'
+]);
+
+$userName = 'agshasha';
+
+$policyName = 'AmazonDynamoDBFullAccess';
+
+$policyArn = 'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess';
+
+try {
+    $attachedGroupPolicies = $client->getIterator('ListAttachedUserPolicies', ([
+        'UserName' => $userName,
+    ]));
+    if (count($attachedGroupPolicies) > 0) {
+        foreach ($attachedGroupPolicies as $attachedGroupPolicy) {
+            if ($attachedGroupPolicy['PolicyName'] == $policyName) {
+                echo $policyName . " is already attached to this role. \n";
+                exit();
+            }
+        }
+    }
+    $result = $client->attachUserPolicy(array(
+        // UserName is required
+        'UserName' => $userName,
+        // PolicyArn is required
+        'PolicyArn' => $policyArn,
+    ));
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
