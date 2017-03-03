acd863519bfb65400a9d78c13f9218bd573e202e
imshashank <agshasha@amazon.com>
2017-03-02 16:54:28 -0800

    Add example code for AWS s3

--- php/example_code/s3/DeleteBucketCors.php
+++ php/example_code/s3/DeleteBucketCors.php
@@ -12,43 +12,31 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-
 require 'vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
 
 /**
- * Delete an Object inside Amazon S3.
+ * Delete bucket cors
  *
  * This code expects that you have AWS credentials set up per:
  * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
  */
 
-$USAGE = "\n" .
-    "To run this example, supply the name of an S3 bucket and object\n" .
-    "name (key) to delete.\n" .
-    "\n" .
-    "Ex: php DeleteObject.php <bucketname> <objectname>\n";
+$bucketName = 'BUCKET_NAME';
 
-if (count($argv) <= 2) {
-    echo $USAGE;
-    exit();
-}
-
-$bucket = $argv[1];
-$key = $argv[2];
+$client = new S3Client([
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
 
 try {
-    //Create a S3Client
-    $s3Client = new S3Client([
-        'region' => 'us-west-2',
-        'version' => '2006-03-01'
+    $result = $client->deleteBucketCors([
+        'Bucket' => $bucketName, // REQUIRED
     ]);
-    $result = $s3Client->deleteObject([
-        'Bucket' => $bucket,
-        'Key' => $key,
-    ]);
-} catch (S3Exception $e) {
-    echo $e->getMessage() . "\n";
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
 }
