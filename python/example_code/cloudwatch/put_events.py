2030a3991d07fafec9935fd4a684504ae8376bcc
kyleknap <kyleknap@amazon.com>
2017-03-07 10:19:59 -0800

    Add examples for working with CloudWatch Events

---
+++ python/example_code/cloudwatch/put_events.py
@@ -0,0 +1,34 @@
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import json
+
+import boto3
+
+
+# Create CloudWatchEvents client
+cloudwatch_events = boto3.client('events')
+
+# Put an event
+response = cloudwatch_events.put_events(
+    Entries=[
+        {
+            'Detail': json.dumps({'key1': 'value1', 'key2': 'value2'}),
+            'DetailType': 'appRequestSubmitted',
+            'Resources': [
+                'RESOURCE_ARN',
+            ],
+            'Source': 'com.company.myapp'
+        }
+    ]
+)
+print(response['Entries'])
