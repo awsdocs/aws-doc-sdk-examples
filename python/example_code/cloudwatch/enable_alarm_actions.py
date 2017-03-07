e236d4547ff54f4d60fd0eff93b7c003ba151cfd
kyleknap <kyleknap@amazon.com>
2017-03-06 16:13:14 -0800

    Add examples for alarm actions

---
+++ python/example_code/cloudwatch/enable_alarm_actions.py
@@ -0,0 +1,41 @@
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+import boto3
+
+
+# Create CloudWatch client
+cloudwatch = boto3.client('cloudwatch')
+
+# Create alarm with actions enabled
+cloudwatch.put_metric_alarm(
+    AlarmName='Web_Server_CPU_Utilization',
+    ComparisonOperator='GreaterThanThreshold',
+    EvaluationPeriods=1,
+    MetricName='CPUUtilization',
+    Namespace='AWS/EC2',
+    Period=60,
+    Statistic='Average',
+    Threshold=70.0,
+    ActionsEnabled=True,
+    AlarmActions=[
+      'arn:aws:swf:us-west-2:{CUSTOMER_ACCOUNT}:action/actions/AWS_EC2.InstanceId.Reboot/1.0'
+    ],
+    AlarmDescription='Alarm when server CPU exceeds 70%',
+    Dimensions=[
+        {
+          'Name': 'InstanceId',
+          'Value': 'INSTANCE_ID'
+        },
+    ],
+    Unit='Seconds'
+)
