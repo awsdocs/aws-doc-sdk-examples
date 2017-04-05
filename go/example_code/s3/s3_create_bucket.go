d3c296db0785f994da0411bf572a34d9d9e8ac11
Doug Schwartz <dougsch@amazon.com>
2017-04-05 12:13:28 -0700

    Updated S3 code example for creating a bucket

--- go/example_code/s3/s3_create_bucket.go
+++ go/example_code/s3/s3_create_bucket.go
@@ -32,6 +32,7 @@ func main() {
 	if len(os.Args) != 2 {
 		exitErrorf("bucket name required\nUsage: %s bucket_name", os.Args[0])
 	}
+
 	bucket := os.Args[1]
 
 	// Initialize a session that the SDK will use to load configuration,
@@ -47,15 +48,18 @@ func main() {
 	_, err := svc.CreateBucket(&s3.CreateBucketInput{
 		Bucket: aws.String(bucket),
 	})
+
 	if err != nil {
 		exitErrorf("Unable to create bucket %q, %v", bucket, err)
 	}
 
 	// Wait until bucket is created before finishing
 	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
+
 	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
 		Bucket: aws.String(bucket),
 	})
+
 	if err != nil {
 		exitErrorf("Error occurred while waiting for bucket to be created, %v", bucket)
 	}
