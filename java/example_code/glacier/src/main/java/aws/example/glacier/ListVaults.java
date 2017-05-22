94b5a621afb11a497054cde55f1bed0703e9dbcd
Hennessey <eronh@amazon.com>
2017-05-22 12:42:16 -0700

    ListVaults example for Amazon Glacier

---
+++ java/example_code/glacier/src/main/java/aws/example/glacier/ListVaults.java
@@ -0,0 +1,59 @@
+/*
+   Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+package aws.example.glacier;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.glacier.AmazonGlacier;
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.model.ListVaultsRequest;
+import com.amazonaws.services.glacier.model.ListVaultsResult;
+import com.amazonaws.services.glacier.model.DescribeVaultOutput;
+import java.util.List;
+
+/**
+ * List your Amazon Glacier vaults.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
+ */
+public class ListVaults
+{
+    public static void main(String[] args)
+    {
+        final AmazonGlacier glacier = new AmazonGlacierClient();
+
+        ListVaultsRequest request = new ListVaultsRequest();
+
+        boolean list_complete = false;
+        int total_vaults = 0;
+        System.out.println("Your Amazon Glacier vaults:");
+        while (!list_complete) {
+            ListVaultsResult result = glacier.listVaults(request);
+            List<DescribeVaultOutput> vault_list = result.getVaultList();
+            if (vault_list.size() > 0) {
+                total_vaults += vault_list.size();
+                for (DescribeVaultOutput v: vault_list) {
+                    System.out.println("* " + v.getVaultName());
+                }
+                // check to see if there are more vaults to retrieve.
+                request.setMarker(result.getMarker());
+            } else {
+                if (total_vaults == 0) {
+                    System.out.println("? No vaults were found.");
+                }
+                list_complete = true;
+            }
+        }
+    }
+}
+
