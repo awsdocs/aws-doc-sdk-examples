5d0b24a7988b21e29e2d11f040aa69a429aa4ed6
Huntsperger <huntsper@amazon.com>
2017-05-22 07:15:12 -0700

    new example showing how to use download_file method

---
+++ python/example_code/s3/s3-python-example-download-file.py
@@ -0,0 +1,27 @@
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import boto3
+import botocore
+
+BUCKET_NAME = 'my-bucket' # replace with your bucket name
+KEY = 'my_image_in_s3.jpg' # replace with your object key
+
+s3 = boto3.resource('s3')
+
+try:
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 'my_local_image.jpg')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
