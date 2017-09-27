c9f9ebe8c2570b59d7ab184dc8485bc1c22b30d1
Doug Schwartz <dougsch@amazon.com>
2017-09-27 08:21:22 -0700

    Added SES examples in Ruby

---
+++ ruby/example_code/ses/ses_list_emails.rb
@@ -0,0 +1,33 @@
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+require 'aws-sdk'
+
+# Create client in us-west-2 region
+client = Aws::SES::Client.new(region: 'us-west-2')
+
+# Get up to 1000 identities
+ids = client.list_identities({
+  identity_type: "EmailAddress"
+})
+
+ids.identities.each do |email|
+  attrs = client.get_identity_verification_attributes({
+    identities: [email]
+  })
+
+  status = attrs.verification_attributes[email].verification_status
+
+  # Display email addresses that have been verified
+  if status == "Success"
+    puts email
+end
---
+++ ruby/example_code/ses/ses_send_email.rb
@@ -0,0 +1,83 @@
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+require 'aws-sdk'
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+sender = "sender@example.com"
+
+# Replace recipient@example.com with a "To" address. If your account
+# is still in the sandbox, this address must be verified.
+recipient = "recipient@example.com"
+
+# Specify a configuration set. To use a configuration
+# set, uncomment the next line and line 74.
+#   configsetname = "ConfigSet"
+
+# The subject line for the email.
+subject = 'Amazon SES test (AWS SDK for Ruby)'
+
+# The HTML body of the email.
+htmlbody =
+  '<h1>Amazon SES test (AWS SDK for Ruby)</h1>'\
+  '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'\
+  'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-ruby/">'\
+  'AWS SDK for Ruby</a>.'
+
+# The email body for recipients with non-HTML email clients.
+textbody = 'This email was sent with Amazon SES using the AWS SDK for Ruby.'
+
+# Specify the text encoding scheme.
+encoding = 'UTF-8'
+
+# Create a new SES client in the us-west-2 region.
+# Replace us-west-2 with the AWS Region you're using for Amazon SES.
+ses = Aws::SES::Client.new(region: 'us-west-2')
+
+# Try to send the email.
+begin
+  # Provide the contents of the email.
+  resp = ses.send_email({
+    destination: {
+    to_addresses: [
+      recipient,
+      ],
+    },
+    message: {
+      body: {
+        html: {
+          charset: encoding,
+          data: htmlbody,
+        },
+        text: {
+          charset: encoding,
+          data: textbody,
+        },
+      },
+    subject: {
+      charset: encoding,
+      data: subject,
+    },
+  },
+  source: sender,
+  # Uncomment the following line to use a configuration set.
+  # configuration_set_name: configsetname,
+  })
+
+  puts 'Email sent to ' + email_address
+end
+
+# If something goes wrong, display an error message.
+rescue Aws::SES::Errors::ServiceError => error
+  puts "Email not sent. Error message: #{error}"
+end
