90d232695f5854baa1a05df60b66eba34bcf538b
Eric Battalio <ebattalio@hotmail.com>
2017-10-03 11:52:47 -0700

    Lambda examples (and Unicode S3 examples) (#97)
    Added Lambda and S3 Unicode examples, fixed other minor issues.

---
+++ cpp/example_code/s3/get_object_unicode.cpp
@@ -0,0 +1,59 @@
+﻿/*
+Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+This file is licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License. A copy of
+the License is located at
+
+http://aws.amazon.com/apache2.0/
+
+This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+*/
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/GetObjectRequest.h>
+#include <fstream>
+
+/**
+* Put a Unicode file to an Amazon S3 bucket.
+*/
+int main(int argc, char** argv)
+{
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+    {
+        const Aws::String bucket_name = "unicode-test";
+        const Aws::WString key_name = L"unicode-Ⓢ③Ⓢ③Ⓒⓛⓘ.txt";
+        const Aws::String region = "us-west-2";
+
+        std::cout << "Downloading " << key_name.c_str() << " from S3 bucket: " <<
+            bucket_name << std::endl;
+
+        Aws::Client::ClientConfiguration clientConfig;
+        clientConfig.region = region;
+        Aws::S3::S3Client s3_client(clientConfig);
+
+        Aws::S3::Model::GetObjectRequest object_request;
+        object_request.SetBucket(bucket_name);
+        object_request.SetKey(Aws::Utils::StringUtils::FromWString(key_name.c_str()));
+
+        auto get_object_outcome = s3_client.GetObject(object_request);
+
+        if (get_object_outcome.IsSuccess())
+        {
+            Aws::OFStream local_file;
+            local_file.open(key_name.c_str(), std::ios::out | std::ios::binary);
+            local_file << get_object_outcome.GetResult().GetBody().rdbuf();
+            std::cout << "Done!" << std::endl;
+        }
+        else
+        {
+            std::cout << "GetObject error: " <<
+                get_object_outcome.GetError().GetExceptionName() << " " <<
+                get_object_outcome.GetError().GetMessage() << std::endl;
+        }
+    }
+    Aws::ShutdownAPI(options);
+}
