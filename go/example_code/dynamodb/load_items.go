17e5abc789aabd924424e0c335878305a0f1e7da
Doug Schwartz <dougsch@amazon.com>
2017-10-09 08:32:57 -0700

    Updated loading DynamoDB items from JSON file example in Go

--- go/example_code/dynamodb/load_items.go
+++ go/example_code/dynamodb/load_items.go
@@ -15,7 +15,9 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "os"
 
     "github.com/aws/aws-sdk-go/aws"
@@ -36,6 +38,20 @@ type Item struct {
     Info ItemInfo`json:"info"`
 }
 
+// Get table items from JSON file
+func getItems() []Item {
+    raw, err := ioutil.ReadFile("./movie_data.json")
+
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    var items []Item
+    json.Unmarshal(raw, &items)
+    return items
+}
+
 func main() {
     // Initialize a session in us-west-2 that the SDK will use to load configuration,
     // and credentials from the shared config file ~/.aws/config.
@@ -43,45 +59,43 @@ func main() {
         Region: aws.String("us-west-2")},
     )
 
+    if err != nil {
+        fmt.Println("Error creating session:")
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
     // Create DynamoDB client
     svc := dynamodb.New(sess)
 
-    info := ItemInfo{
-        Plot: "Nothing happens at all.",
-        Rating: 0.0,
+    // Get table items from movie_data.json
+    items := getItems()
+
+    // Add each item to Movies table:
+    for _, item := range items {
+        av, err := dynamodbattribute.MarshalMap(item)
+
+        if err != nil {
+            fmt.Println("Got error marshalling map:")
+            fmt.Println(err.Error())
+            os.Exit(1)
+        }
+
+        // Create item in table Movies
+        input := &dynamodb.PutItemInput{
+            Item: av,
+            TableName: aws.String("Movies"),
+        }
+
+        _, err = svc.PutItem(input)
+
+        if err != nil {
+            fmt.Println("Got error calling PutItem:")
+            fmt.Println(err.Error())
+            os.Exit(1)
+        }
+
+        fmt.Println("Successfully added '",item.Title,"' (",item.Year,") to Movies table")
     }
-
-    item := Item{
-        Year: 2015,
-        Title: "The Big New Movie",
-        Info: info,
-    }
-
-    // Open movie_data.json
-
-    // dd each movie:
-
-    av, err := dynamodbattribute.MarshalMap(item)
-
-    if err != nil {
-        fmt.Println("Got error marshalling map:")
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
-
-    // Create item in table Movies
-    input := &dynamodb.PutItemInput{
-        Item: av,
-        TableName: aws.String("Movies"),
-    }
-
-    _, err = svc.PutItem(input)
-
-    if err != nil {
-        fmt.Println("Got error calling PutItem:")
-        fmt.Println(err.Error())
-        os.Exit(1)
-    }
-
-    fmt.Println("Successfully added 'The Big New Movie' (2015) to Movies table")
 }
+
