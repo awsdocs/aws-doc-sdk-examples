421865b4d2eeddd0279ffafd363b911db8a4a5f5
David Deyo <ddeyo@amazon.com>
2017-11-08 14:57:02 -0800

    EC2 samples updated to use shared creds

--- javascript/example_code/ec2/ec2_describeaddresses.js
+++ javascript/example_code/ec2/ec2_describeaddresses.js
@@ -14,8 +14,8 @@
 
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Load credentials and set region from JSON file
-AWS.config.loadFromPath('./config.json');
+// Set the region 
+AWS.config.update({region: 'REGION'});
 
 // Create EC2 service object
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
