c4ba9fe9275c4745e4581d984bf9858e93d058b9
David Deyo <ddeyo@amazon.com>
2017-11-08 15:03:41 -0800

    S3 samples updated to use shared creds

--- javascript/example_code/s3/s3_getcors.js
+++ javascript/example_code/s3/s3_getcors.js
@@ -14,8 +14,8 @@
 
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-// Load credentials and set region from JSON file
-AWS.config.loadFromPath('./config.json');
+// Set the region 
+AWS.config.update({region: 'REGION'});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
