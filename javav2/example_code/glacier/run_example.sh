8adbaaa21f69f685ee819694dd81a09e2fc1e13f
Soo Sung <soosung@amazon.com>
2017-12-04 13:24:52 -0800

    adding glacier example for v2

---
+++ javav2/example_code/glacier/run_example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ -z $* ]] ; then
+    echo 'Supply the name of one of the example classes as an argument.'
+    exit 1
+fi
+export CLASSPATH=target/sdk-glacier-examples-1.0.jar:$JAVA_SDK_HOME
+echo "## Running $1..."
+java com.example.glacier.$@
