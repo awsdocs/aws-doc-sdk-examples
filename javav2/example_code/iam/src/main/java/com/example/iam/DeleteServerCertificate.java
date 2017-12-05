3f0e3de071747879610aa01f316d9b625ca73f25
Soo Sung <soosung@amazon.com>
2017-12-04 21:53:33 -0800

    adding java v2 iam examples converted from v1

---
+++ javav2/example_code/iam/src/main/java/com/example/iam/DeleteServerCertificate.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.example.iam;
+import software.amazon.awssdk.services.iam.model.DeleteServerCertificateRequest;
+import software.amazon.awssdk.services.iam.model.DeleteServerCertificateResponse;
+
+import software.amazon.awssdk.core.regions.Region;
+import software.amazon.awssdk.services.iam.IAMClient;
+
+/**
+ * Deletes an IAM server certificate
+ */
+public class DeleteServerCertificate {
+    public static void main(String[] args) {
+
+        final String USAGE =
+            "To run this example, supply a certificate name\n" +
+            "Ex: DeleteServerCertificate <certificate-name>\n";
+
+        if (args.length != 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String cert_name = args[0];
+
+        Region region = Region.AWS_GLOBAL;
+        IAMClient iam = IAMClient.builder().region(region).build();
+        
+        DeleteServerCertificateRequest request =
+            DeleteServerCertificateRequest.builder()
+                .serverCertificateName(cert_name).build();
+
+        DeleteServerCertificateResponse response =
+            iam.deleteServerCertificate(request);
+
+        System.out.println("Successfully deleted server certificate " +
+                cert_name);
+    }
+}
+
---
+++ javav2/example_code/iam/src/main/java/com/example/iam/ListServerCertificates.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.example.iam;
+import software.amazon.awssdk.services.iam.model.ListServerCertificatesRequest;
+import software.amazon.awssdk.services.iam.model.ListServerCertificatesResponse;
+import software.amazon.awssdk.services.iam.model.ServerCertificateMetadata;
+
+import software.amazon.awssdk.core.regions.Region;
+import software.amazon.awssdk.services.iam.IAMClient;
+
+/**
+ * Lists all server certificates associated with an AWS account
+ */
+public class ListServerCertificates {
+    public static void main(String[] args) {
+
+        Region region = Region.AWS_GLOBAL;
+        IAMClient iam = IAMClient.builder().region(region).build();
+
+        boolean done = false;
+        String new_marker = null;
+
+        while(!done) {
+        	ListServerCertificatesResponse response;
+        	
+        	if (new_marker == null) {
+                ListServerCertificatesRequest request = 
+                		ListServerCertificatesRequest.builder().build();
+                response = iam.listServerCertificates(request);        		
+        	}
+        	else {
+                ListServerCertificatesRequest request = 
+                		ListServerCertificatesRequest.builder()
+                		.marker(new_marker).build();
+                response = iam.listServerCertificates(request);
+        	}
+
+            for(ServerCertificateMetadata metadata :
+                    response.serverCertificateMetadataList()) {
+                System.out.printf("Retrieved server certificate %s",
+                        metadata.serverCertificateName());
+            }
+
+            if(!response.isTruncated()) {
+                done = true;
+            }
+            else {
+            	new_marker = response.marker();
+            }
+        }
+    }
+}
+
