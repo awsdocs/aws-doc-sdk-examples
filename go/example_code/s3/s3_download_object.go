71522a93f87f992733412a6b5a4f9556b68b9d7f
Doug Schwartz <dougsch@amazon.com>
2017-12-12 07:50:56 -0800

    Replaced tabs with 4 spaces in S3 download example in Go

--- go/example_code/s3/s3_download_object.go
+++ go/example_code/s3/s3_download_object.go
@@ -15,13 +15,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 // Downloads an item from an S3 Bucket in the region configured in the shared config
@@ -30,42 +30,42 @@ import (
 // Usage:
 //    go run s3_download_object.go BUCKET ITEM
 func main() {
-	if len(os.Args) != 3 {
-		exitErrorf("Bucket and item names required\nUsage: %s bucket_name item_name",
-			os.Args[0])
-	}
+    if len(os.Args) != 3 {
+        exitErrorf("Bucket and item names required\nUsage: %s bucket_name item_name",
+            os.Args[0])
+    }
 
-	bucket := os.Args[1]
-	item := os.Args[2]
+    bucket := os.Args[1]
+    item := os.Args[2]
 
-	// Initialize a session in us-west-2 that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials.
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
+    // Initialize a session in us-west-2 that the SDK will use to load
+    // credentials from the shared credentials file ~/.aws/credentials.
+    sess, _ := session.NewSession(&aws.Config{
+        Region: aws.String("us-west-2")},
+    )
 
-	file, err := os.Create(item)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
+    file, err := os.Create(item)
+    if err != nil {
+        exitErrorf("Unable to open file %q, %v", err)
+    }
 
-	defer file.Close()
+    defer file.Close()
 
-	downloader := s3manager.NewDownloader(sess)
+    downloader := s3manager.NewDownloader(sess)
 
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key:    aws.String(item),
-		})
-	if err != nil {
-		exitErrorf("Unable to download item %q, %v", item, err)
-	}
+    numBytes, err := downloader.Download(file,
+        &s3.GetObjectInput{
+            Bucket: aws.String(bucket),
+            Key:    aws.String(item),
+        })
+    if err != nil {
+        exitErrorf("Unable to download item %q, %v", item, err)
+    }
 
-	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+    fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 }
 
 func exitErrorf(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(1)
+    fmt.Fprintf(os.Stderr, msg+"\n", args...)
+    os.Exit(1)
 }
