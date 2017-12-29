cbb4aa6c159b437b9cb2c4e02a4db521665abd09
David Deyo <ddeyo@amazon.com>
2017-12-29 14:02:59 -0800

    New S3.listObjects sample

---
+++ javascript/example_code/s3/s3_listobjects.js
@@ -0,0 +1,35 @@
+/*
+   Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Create params for S3.createBucket
+var readParams = {
+  Bucket : 'BUCKET_NAME',
+};
+
+// call S3 to create the bucket
+s3.listObjects(readParams, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
