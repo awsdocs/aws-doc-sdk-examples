9946287590783a1c9c7311112ccaeb38c7cdcc5f
Hüseyin TUNÇ <huseyin.tunc@bulutfon.com>
2018-01-03 12:03:11 +0300

    credentials added to new client

--- ruby/example_code/ses/ses_send_email.rb
+++ ruby/example_code/ses/ses_send_email.rb
@@ -42,7 +42,7 @@ encoding = 'UTF-8'
 
 # Create a new SES client in the us-west-2 region.
 # Replace us-west-2 with the AWS Region you're using for Amazon SES.
-ses = Aws::SES::Client.new(region: 'us-west-2')
+ses = Aws::SES::Client.new(secret_access_key: 'YOUR_SECRET_ACCESS_KEY', access_key_id: 'YOUR_ACCESS_KEY_ID', region: 'us-west-2')
 
 # Try to send the email.
 begin
@@ -73,7 +73,7 @@ begin
   # Uncomment the following line to use a configuration set.
   # configuration_set_name: configsetname,
   })
-  
+
   puts 'Email sent to ' + recipient
 
 # If something goes wrong, display an error message.
