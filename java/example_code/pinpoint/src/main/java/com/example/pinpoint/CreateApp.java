3a0ae4538b20bae5405b1d80d14721ce36e383d5
Soo Sung <soosung@amazon.com>
2018-01-15 14:46:37 -0800

    updating copyright for java examples and fixing pinpoint example

--- java/example_code/pinpoint/src/main/java/com/example/pinpoint/CreateApp.java
+++ java/example_code/pinpoint/src/main/java/com/example/pinpoint/CreateApp.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package com.example.pinpoint;
 
 import com.amazonaws.services.pinpoint.AmazonPinpoint;
@@ -28,7 +42,7 @@ public class CreateApp {
 		AmazonPinpoint pinpoint = AmazonPinpointClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 		
 		CreateApplicationRequest appRequest = new CreateApplicationRequest()
-				.withName("MyTestApp");
+				.withName(appName);
 		
 		CreateAppRequest request = new CreateAppRequest();
 		request.withCreateApplicationRequest(appRequest);
--- javav2/example_code/swf/src/main/java/com/example/helloswf/HelloTypes.java
+++ javav2/example_code/swf/src/main/java/com/example/helloswf/HelloTypes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights
  * Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
