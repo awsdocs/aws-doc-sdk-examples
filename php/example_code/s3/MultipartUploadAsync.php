6421768b68b1519e36e3930c7cf0cc633e9fd54c
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-03-16 18:18:43 -0700

    Creating Multipart example

---
+++ php/example_code/s3/MultipartUploadAsync.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/aws-sdk-php/v3/guide/service/s3-multipart-upload.html
+ * 
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+use Aws\S3\MultipartUploader;
+use Aws\Exception\MultipartUploadException;
+
+// Create a S3Client
+$s3Client = new S3Client([
+    'region' => 'us-west-2',
+    'version' => '2006-03-01'
+]);
+
+
+$source = '/path/to/large/file.zip';
+$uploader = new MultipartUploader($s3Client, $source, [
+    'bucket' => 'your-bucket',
+    'key'    => 'my-file.zip',
+]);
+
+$promise = $uploader->promise();
+	
