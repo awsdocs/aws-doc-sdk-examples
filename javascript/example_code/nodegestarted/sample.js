b6c1f4e6445be5dc7f044c6b8923a820240b140a
David Deyo <daviddeyo@users.noreply.github.com>
2018-06-02 20:10:50 -0700

    Update sample.js
    Code rewritten for promise callback

--- javascript/example_code/nodegestarted/sample.js
+++ javascript/example_code/nodegestarted/sample.js
@@ -6,21 +6,28 @@
 
 // Load the SDK and UUID
 var AWS = require('aws-sdk');
-var uuidv4 = require('uuid/v4');
+var uuid = require('uuid');
 
-// Create an S3 client
-var s3 = new AWS.S3();
-
-// Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuidv4();
+// Create unique bucket name
+var bucketName = 'node-sdk-sample-' + uuid.v4();
+// Create name for uploaded object key
 var keyName = 'hello_world.txt';
 
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-  });
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+
+// Handle promise fulfilled/rejected states
+bucketPromise.then(
+  function(data) {
+    // Create params for putObject call
+    var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    // Create object upload promise
+    var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+    uploadPromise.then(
+      function(data) {
+        console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+      });
+}).catch(
+  function(err) {
+    console.error(err, err.stack);
 });
