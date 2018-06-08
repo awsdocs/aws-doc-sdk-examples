fa84bdef8015bd6104da611c340468b7a1be9812
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-06-07 22:58:40 -0700

    Code Samples -Using Media Convert with AWS SDK for PHP

---
+++ php/example_code/mediaconvert/CreateClient.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\MediaConvert\MediaConvertClient;
+
+
+/**
+ * Creating an Amazon Elemental MediaConvert Client.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
+ */
+
+//Create a MediaConvert Client
+$client = new Aws\MediaConvert\MediaConvertClient([
+    'profile' => 'default',
+    'version' => '2017-08-29',
+    'region'  => 'us-east-1'
+]);
+
+var_dump($client);
---
+++ php/example_code/mediaconvert/GetJob.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE => This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/emc-examples-jobs.html
+ *
+ */
+require 'vendor/autoload.php';
+
+use Aws\MediaConvert\MediaConvertClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Retrieve resutls from a specific completed transcoding job for AWS Elemental MediaConvert.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html
+ */
+
+//Create an AWSMediaConvert client object with your account specific endpoint. 
+$mediaConvertClient = new MediaConvertClient([
+    'version' => '2017-08-29',
+    'region'  => 'us-east-1',
+    'profile' => 'default',
+    'endpoint' => 'ACCOUNT_ENDPOINT'
+]); 
+
+try {
+    $result = $mediaConvertClient -> getJob([
+       'Id' => 'JOB_ID', 
+   ]);    
+}catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
