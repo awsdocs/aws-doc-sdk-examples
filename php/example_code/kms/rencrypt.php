d41f4551067aca3f444ff09128fae3ab276f7f2b
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-07-06 22:36:04 -0700

    Creating php code examples of programing encryption for KMS developer guide

---
+++ php/example_code/kms/rencrypt.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *  ABOUT THIS PHP SAMPLE: This sample is part of the KMS Developer Guide topic at
+ *  https://docs.aws.amazon.com/kms/latest/developerguide/programming-keys.html
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\Kms\KmsClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Creating an Amazon KMS client.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+//Create a KMSClient
+$KmsClient = new Aws\Kms\KmsClient([
+    'profile' => 'default',
+    'version' => '2014-11-01',
+    'region'  => 'us-east-1'
+]);
+
+$keyId = 'arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab';
+$encryptedMessage = 'Place your cipher text blob here';
+
+try {
+    $result = $KmsClient->rencrypt([
+        'CiphertextBlob' => $encryptedMessage,
+		'DestinationKeyId' => $keyID,
+	]);
+    var_dump($result);
+}catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
