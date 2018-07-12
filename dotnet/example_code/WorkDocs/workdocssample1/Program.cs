0b27c4204dddb5deb5208feeba0d1ae8e3988ca9
Eric Battalio <ebattalio@hotmail.com>
2018-07-12 09:07:59 -0700

    Added WorkDocs samples

---
+++ dotnet/example_code/WorkDocs/workdocssample1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.WorkDocs;
+using Amazon.WorkDocs.Model;
+
+namespace WorkdocsSample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListUsers.Sample();
+            ListUserDocs.Sample();
+            UploadUserDoc.Sample();
+            DownloadUserDoc.Sample();
+        }
+    }
+}
