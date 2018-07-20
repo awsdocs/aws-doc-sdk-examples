90354fd8a7b15ee5153e6d9c0ad279d665569ed1
Jami Schwarzwalder <jamisch@amazon.com>
2018-07-20 12:15:37 -0700

    setting us-east-2 as default region

--- php/example_code/kms/ListKeys.php
+++ php/example_code/kms/ListKeys.php
@@ -33,7 +33,7 @@ use Aws\Exception\AwsException;
 $KmsClient = new Aws\Kms\KmsClient([
     'profile' => 'default',
     'version' => '2014-11-01',
-    'region'  => 'us-east-1'
+    'region'  => 'us-east-2'
 ]);
 
 $limit = 10;
--- php/example_code/s3/CreateClient.php
+++ php/example_code/s3/CreateClient.php
@@ -33,7 +33,7 @@ use Aws\S3\S3Client;
 $s3 = new Aws\S3\S3Client([
     'profile' => 'default',
     'version' => 'latest',
-    'region'  => 'us-east-1'
+    'region'  => 'us-east-2'
 ]);
 
 // The same options that can be provided to a specific client constructor can also be supplied to the Aws\Sdk class.
