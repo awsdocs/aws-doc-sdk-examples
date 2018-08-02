041e6de4041a19e0c13eaf10d0d42b833cfc483b
Soo Sung <soosung@amazon.com>
2018-08-02 13:28:20 -0700

    moving license info to bottom of kinesis example for dev guide text alignment

--- javav2/example_code/kinesis/src/main/java/com/example/kinesis/KinesisStreamEx.java
+++ javav2/example_code/kinesis/src/main/java/com/example/kinesis/KinesisStreamEx.java
@@ -1,17 +1,3 @@
-/*
- * Copyright 2011-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *    http://aws.amazon.com/apache2.0
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
- * OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.kinesis;
 
 import java.net.URI;
@@ -239,3 +225,19 @@ public class KinesisStreamEx {
     }
 
 }
+
+/*
+ * Copyright 2011-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *    http://aws.amazon.com/apache2.0
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
--- javav2/example_code/kinesis/src/main/java/com/example/kinesis/KinesisStreamRxJavaEx.java
+++ javav2/example_code/kinesis/src/main/java/com/example/kinesis/KinesisStreamRxJavaEx.java
@@ -1,17 +1,3 @@
-/*
- * Copyright 2011-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *    http://aws.amazon.com/apache2.0
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
- * OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.kinesis;
 
 import java.net.URI;
@@ -92,3 +78,18 @@ public class KinesisStreamRxJavaEx {
     
     
 }
+
+/*
+ * Copyright 2011-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *    http://aws.amazon.com/apache2.0
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and
+ * limitations under the License.
+ */
