7664d817378b4c441721473e8978ccdc551b7afc
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-08-08 01:09:41 -0700

    Updating acceptable parameters for IdentityType

--- python/example_code/ses/ses_listidentities.py
+++ python/example_code/ses/ses_listidentities.py
@@ -17,7 +17,7 @@ import boto3
 ses = boto3.client('ses')
 
 response = ses.list_identities(
-  IdentityType = 'EMAIN_ADDRESS' | 'DOMAIN',
+  IdentityType = 'EmailAddress' | 'Domain' | 'ManagedDomain',
   MaxItems = 10
 )
 
