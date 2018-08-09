792ae726ee1b2f2a4adab73c96a84f796d6fa847
Jami Schwarzwalder <jamisch@amazon.com>
2018-08-09 01:55:26 -0700

    adding code sample to send Email with SES

---
+++ php/example_code/ses/Send_Email.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *
+ *
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\SES\SESClient;
+use Aws\Exception\AwsException;
+
+//Create a SESClient
+$SesClient = new Aws\SES\SESClient([
+    'profile' => 'default',
+    'version' => '2010-12-01',
+    'region'  => 'us-east-2'
+]);
+
+$html_body = '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+                  '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+                  'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+                  'AWS SDK for PHP</a>.</p>';
+$subject = 'Amazon SES test (AWS SDK for PHP)';
+$plaintext_body = 'This email was send with Amazon SES using the AWS SDK for PHP.' ;
+$sender_email = 'email_address';
+$verified_recipeint_emails = ['email_address'];
+$char_set = 'UTF-8';
+
+try {
+    $result = $SesClient->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $verified_recipeint_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+        
+        'Body' => [
+            'Html' => [
+                'Charset' => $char_set,
+                'Data' => $html_body,
+            ],
+            'Text' => [
+                'Charset' => $char_set,
+                'Data' => $plaintext_body,
+            ],
+        ],
+        'Subject' => [
+            'Charset' => $char_set,
+            'Data' => $subject,
+        ],
+    ],
+            ]);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
