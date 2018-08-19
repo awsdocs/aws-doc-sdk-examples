3d42b64c981fbb2271dfcac4a84d2796be34e549
Tapasweni Pathak <tapaswenipathak@gmail.com>
2018-08-19 06:14:26 +0530

    Add EBS CPP example codes

---
+++ cpp/example_code/ebs/delete_volume.cpp
@@ -0,0 +1,52 @@
+/*
+   Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+    http://aws.amazon.com/apache2.0/
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+#include <aws/core/Aws.h>
+#include <aws/ec2/EC2Client.h>
+#include <aws/ec2/model/DeleteVolumeRequest.h>
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "Usage: delete_volume <volume_id>"
+              << std::endl;
+    return 1;
+  }
+
+  Aws::SDKOptions options;
+  Aws::InitAPI(options);
+  {
+    Aws::String volume_id(argv[1]);
+
+    Aws::EC2::EC2Client ec2;
+
+    Aws::EC2::Model::DeleteVolumeRequest dv_req;
+
+    dv_req.SetVolumeId(volume_id);
+
+    auto dv_out = ec2.DeleteVolume(dv_req);
+
+    if (dv_out.IsSuccess())
+    {
+      std::cout << "Successfully deleted volume. " << std::endl;
+    }
+    else
+    {
+      std::cout << "Error deleting volume." << dv_out.GetError().GetMessage()
+        << std::endl;
+    }
+  }
+
+  Aws::ShutdownAPI(options);
+  return 0;
+}
