8bc853f124f81d65420793d18e3d5eb1d7ecbd2d
Keith Walker <keiwlk@amazon.com>
2018-08-22 18:15:14 -0700

    Format code lines to 80 characters in cf update_distribution_certificate

--- python/example_code/cloudfront/update_distribution_certificate.py
+++ python/example_code/cloudfront/update_distribution_certificate.py
@@ -36,24 +36,30 @@ for distributionlist in paginator.paginate():
       print("Certificate ARN: " + distribution['ViewerCertificate']['Certificate'])
     print("")
 
-print('Enter the Distribution Id of the CloudFront Distribution who\'s ACM Certificate you would like to replace. ')
-distribution_id = read('Note that certificate source must be ACM - DistributionId: ')
+print('Enter the Distribution Id of the CloudFront Distribution who\'s ACM \
+Certificate you would like to replace. ')
+
+distribution_id = read('Note that certificate source must be ACM - \
+DistributionId: ')
 
 distribution_config_response=cf.get_distribution_config(Id=distribution_id)
 distribution_config=distribution_config_response['DistributionConfig']
 distribution_etag=distribution_config_response['ETag']
 
 if (distribution_config['ViewerCertificate']['CertificateSource'] != "acm"):
-	print("\nThe DistributionId you have entered is not currently using an ACM Certificate, exiting...\n")
+	print("\nThe DistributionId you have entered is not currently using \
+an ACM Certificate, exiting...\n")
 	exit()
 
 old_cert_arn=distribution_config['ViewerCertificate']['ACMCertificateArn']
 
-new_cert_arn=read("Please enter the ARN of the new ACM Certificate you would like to attach to Distribution " + distribution_id + ": ")
+new_cert_arn=read("Please enter the ARN of the new ACM Certificate you would \
+like to attach to Distribution " + distribution_id + ": ")
 
 print("Replacing: " + old_cert_arn + "\nwith: " + new_cert_arn + "\n")
 
 distribution_config['ViewerCertificate']['ACMCertificateArn']=new_cert_arn
 distribution_config['ViewerCertificate']['Certificate']=new_cert_arn
 
-cf.update_distribution(DistributionConfig=distribution_config,Id=distribution_id,IfMatch=distribution_etag)
+cf.update_distribution(DistributionConfig=distribution_config, \
+Id=distribution_id,IfMatch=distribution_etag)
