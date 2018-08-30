54ef02f627c166ed5bd08d80d14647f2e16829da
Jami Schwarzwalder <jamisch@amazon.com>
2018-08-30 13:25:26 -0700

    adding a check for empty distribution list

--- python/example_code/cloudfront/list_distributions.py
+++ python/example_code/cloudfront/list_distributions.py
@@ -20,11 +20,13 @@ cf = boto3.client('cloudfront')
 # List distributions
 print("\nCloudFront Distributions:\n")
 distributions=cf.list_distributions()
-
-for distribution in distributions['DistributionList']['Items']:
-  print("Domain: " + distribution['DomainName'])
-  print("Distribution Id: " + distribution['Id'])
-  print("Certificate Source: " + distribution['ViewerCertificate']['CertificateSource'])
-  if (distribution['ViewerCertificate']['CertificateSource'] == "acm"):
-    print("Certificate: " + distribution['ViewerCertificate']['Certificate'])
-  print("")
+if distributions['DistributionList']['Quantity'] > 0:
+  for distribution in distributions['DistributionList']['Items']:
+    print("Domain: " + distribution['DomainName'])
+    print("Distribution Id: " + distribution['Id'])
+    print("Certificate Source: " + distribution['ViewerCertificate']['CertificateSource'])
+    if (distribution['ViewerCertificate']['CertificateSource'] == "acm"):
+      print("Certificate: " + distribution['ViewerCertificate']['Certificate'])
+    print("")
+else:    
+  print("Error - No CloudFront Distributions Detected.")
--- python/example_code/cloudfront/list_distributions_paginator.py
+++ python/example_code/cloudfront/list_distributions_paginator.py
@@ -21,11 +21,14 @@ cf = boto3.client('cloudfront')
 print("\nCloudFront Distributions:\n")
 paginator = cf.get_paginator('list_distributions')
 for distributionlist in paginator.paginate():
-  for distribution in distributionlist['DistributionList']['Items']:
-    #print(distribution)
-    print("Domain: " + distribution['DomainName'])
-    print("Distribution Id: " + distribution['Id'])
-    print("Certificate Source: " + distribution['ViewerCertificate']['CertificateSource'])
-    if (distribution['ViewerCertificate']['CertificateSource'] == "acm"):
-      print("Certificate ARN: " + distribution['ViewerCertificate']['Certificate'])
-    print("")
+  if distributionlist['DistributionList']['Quantity'] > 0:
+    for distribution in distributionlist['DistributionList']['Items']:
+      #print(distribution)
+      print("Domain: " + distribution['DomainName'])
+      print("Distribution Id: " + distribution['Id'])
+      print("Certificate Source: " + distribution['ViewerCertificate']['CertificateSource'])
+      if (distribution['ViewerCertificate']['CertificateSource'] == "acm"):
+        print("Certificate ARN: " + distribution['ViewerCertificate']['Certificate'])
+      print("")
+  else:    
+    print("Error - No CloudFront Distributions Detected.")
