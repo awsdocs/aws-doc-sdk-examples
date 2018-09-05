3b3bbcbf7a78b1417c3c638a412ce8bb80452b02
Keith Walker <keiwlk@amazon.com>
2018-09-05 12:04:12 -0700

    Added requested error handling changes

--- python/example_code/acm/get_certificate.py
+++ python/example_code/acm/get_certificate.py
@@ -17,12 +17,25 @@ import boto3
 # Create ACM client
 acm = boto3.client('acm')
 
-# Describe the specified certificate.
-response = acm.get_certificate(
-    CertificateArn='arn:aws:acm:region:123456789012:certificate/12345678-1234-1234-1234-123456789012'
-)
+try:
+    # Describe the specified certificate.
+    response = acm.get_certificate(
+        CertificateArn='arn:aws:acm:region:123456789012:certificate/12345678-1234-1234-1234-123456789012'
+    )
 
-print('\n\rCertificate:\n\r')
-print(response['Certificate'])
-print('CertificateChain:\n\r')
-print(response['CertificateChain'])
+    print('\n\rCertificate:\n\r')
+    print(response['Certificate'])
+    print('CertificateChain:\n\r')
+    print(response['CertificateChain'])
+
+except acm.exceptions.ResourceNotFoundException as e:
+    print(e.response['Error']['Code'] + ': ' + e.response['Error']['Message'])
+    exit(1)
+
+except acm.exceptions.RequestInProgressException as e:
+    print(e.response['Error']['Code'] + ': ' + e.response['Error']['Message'])
+    exit(1)
+
+except:
+    print('There was an error.')
+    exit(1)
