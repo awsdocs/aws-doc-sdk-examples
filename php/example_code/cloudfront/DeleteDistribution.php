cec41c5c9b03613fb3d0410364e0e21bea799acd
Jami Schwarzwalder <jamisch@amazon.com>
2018-09-14 17:57:46 -0700

    Creating first draft of some CloudFormation PHP Scripts

---
+++ php/example_code/cloudfront/DeleteDistribution.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE => This sample is part of the SDK for PHP Developer Guide topic at
+ * 
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\CloudFront\CloudFrontClient;
+use Aws\Exception\AwsException;
+
+
+/**
+ * Removes an Amazon CloudFront Distribution.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+//Create a CloudFront Client
+$client = new Aws\CloudFront\CloudFrontClient([
+    'profile' => 'default',
+    'version' => '2018-06-18',
+    'region'  => 'us-east-2'
+]);
+
+$id = 'E1A2B3C4D5E';
+
+try {
+    $result = $client -> deleteDistribution([
+       'Id' => $id, //REQUIRED       
+   ]); 
+   var_dump($result);
+}catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
