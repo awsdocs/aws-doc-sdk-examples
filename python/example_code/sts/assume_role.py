92903897f83bf0725c1f24c7bd9fbf5dfd1bb810
Keith Walker <keiwlk@amazon.com>
2018-09-17 17:56:54 -0700

    Refactored Python STS assume_role() example to assume a single Role instead of two

--- python/example_code/sts/assume_role.py
+++ python/example_code/sts/assume_role.py
@@ -18,38 +18,21 @@ sts_default_provider_chain = boto3.client('sts')
 
 print('Default Provider Identity: : ' + sts_default_provider_chain.get_caller_identity()['Arn'])
 
-role1_to_assume_arn='arn:aws:iam::123456789012:role/roleName'
-role1_session_name='test_session'
+role_to_assume_arn='arn:aws:iam::123456789012:role/roleName'
+role_session_name='test_session'
 
-role2_to_assume_arn='arn:aws:iam::210987654321:role/roleName'
-role2_session_name='test_session'
-
-response1=sts_default_provider_chain.assume_role(
-    RoleArn=role1_to_assume_arn,
-    RoleSessionName=role1_session_name
+response=sts_default_provider_chain.assume_role(
+    RoleArn=role_to_assume_arn,
+    RoleSessionName=role_session_name
 )
 
-response2=sts_default_provider_chain.assume_role(
-    RoleArn=role2_to_assume_arn,
-    RoleSessionName=role2_session_name
+creds=response['Credentials']
+
+sts_assumed_role = boto3.client('sts',
+    aws_access_key_id=creds['AccessKeyId'],
+    aws_secret_access_key=creds['SecretAccessKey'],
+    aws_session_token=creds['SessionToken'],
 )
 
-creds1=response1['Credentials']
+print('AssumedRole Identity: ' + sts_assumed_role.get_caller_identity()['Arn'])
 
-creds2=response2['Credentials']
-
-sts_account1 = boto3.client('sts',
-    aws_access_key_id=creds1['AccessKeyId'],
-    aws_secret_access_key=creds1['SecretAccessKey'],
-    aws_session_token=creds1['SessionToken'],
-)
-
-sts_account2 = boto3.client('sts',
-    aws_access_key_id=creds2['AccessKeyId'],
-    aws_secret_access_key=creds2['SecretAccessKey'],
-    aws_session_token=creds2['SessionToken'],
-)
-
-print('AssumedRole1: ' + sts_account1.get_caller_identity()['Arn'])
-
-print('AssumedRole2: ' + sts_account2.get_caller_identity()['Arn'])
