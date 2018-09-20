138a3936376b4d37e81b9967e033f61619e7b86d
Jami Schwarzwalder <jamisch@amazon.com>
2018-09-20 00:39:42 -0700

    renaming files

---
+++ php/example_code/sns/CreateTopic.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *  ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic
+ *
+ *
+ */
+require 'vendor/autoload.php';
+
+use Aws\Sns\SnsClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Lists the Simple Notification Service topics in your AWS account at the requested region.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+$SnSclient = new SnsClient([
+    'profile' => 'default',
+    'region' => 'us-east-1',
+    'version' => '2010-03-31'
+]);
+
+try {
+    $result = $SnSclient->listTopics([
+    ]);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
