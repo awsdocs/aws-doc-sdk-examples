f50f7484cb13839c1de74492916a6fc96b91b7c7
Jami Schwarzwalder <jamisch@amazon.com>
2018-09-21 20:08:05 -0700

    updating descriptions of samples

--- php/example_code/cloudfront/CreateDistributionS3.php
+++ php/example_code/cloudfront/CreateDistributionS3.php
@@ -24,7 +24,7 @@ use Aws\Exception\AwsException;
 
 
 /**
- * Creating an Amazon CloudFront Distribution.
+ * Creating an Amazon CloudFront Distribution for an S3 Bucket.
  *
  * This code expects that you have AWS credentials set up per:
  * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
