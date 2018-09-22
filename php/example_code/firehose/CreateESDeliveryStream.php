712ec2118957ba6a33b5dee487f9747d48cc52bd
Jami Schwarzwalder <jamisch@amazon.com>
2018-09-21 20:16:33 -0700

    Adding PHP code sample for Elastic Search Delivery Stream

---
+++ php/example_code/firehose/CreateESDeliveryStream.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *  ABOUT THIS PHP SAMPLE: This sample is part of the
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\Firehose\FirehoseClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Creating an Amazon Kinesis Firehose client.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+//Create a KinesisClient
+$FirehoseClient = new Aws\Firehose\FirehoseClient([
+    'profile' => 'default',
+    'version' => '2015-08-04',
+    'region' => 'us-east-2'
+]);
+
+$name = "my_ES_stream_name";
+$stream_type = "DirectPut";
+$esDomainARN = 'arn:aws:es:us-east-2:0123456789:domain/Name';
+$esRole = 'arn:aws:iam::0123456789:policy/Role';
+$esIndex = 'root';
+$esType = 'PHP_SDK';
+$s3bucket = 'arn:aws:s3:::bucket_name';
+$s3Role = 'arn:aws:iam::0123456789:policy/Role';
+
+try {
+    $result = $FirehoseClient->createDeliveryStream([
+        'DeliveryStreamName' => $name,
+        'DeliveryStreamType' => $stream_type,
+        'ElasticsearchDestinationConfiguration' => [
+            'CloudWatchLoggingOptions' => [
+                'Enabled' => true,
+                'LogGroupName' => '<string>',
+                'LogStreamName' => '<string>',
+            ],
+            'DomainARN' => $esDomainARN,
+            'IndexName' => $esIndex,
+            'IndexRotationPeriod' => 'OneDay',
+            'ProcessingConfiguration' => [
+                'Enabled' => false,
+                'Processors' => [],
+            ],
+            'RetryOptions' => [
+                'DurationInSeconds' => 300,
+            ],
+            'S3BackupMode' => 'FailedDocumentsOnly',
+            'RoleARN' => $esRole,
+            'S3Configuration' => [
+
+                'BucketARN' => $s3bucket,
+                'CloudWatchLoggingOptions' => [
+                    'Enabled' => false,
+                ],
+                'RoleARN' => $s3Role,
+
+
+                'CompressionFormat' => 'UNCOMPRESSED',
+                'EncryptionConfiguration' => [
+                    'NoEncryptionConfig' => 'NoEncryption',
+                ],
+            ],
+
+            'TypeName' => $esType,
+        ],
+    ]);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
