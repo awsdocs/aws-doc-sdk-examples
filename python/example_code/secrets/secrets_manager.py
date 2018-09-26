d22c20e03013a86613e90ebcd1bfe1b7e4d5c31d
Jami Schwarzwalder <jamisch@amazon.com>
2018-09-25 18:24:30 -0700

    Adding Get Secret Value Code Sample for multiple languages

--- python/example_code/secrets/secrets_manager.py
+++ python/example_code/secrets/secrets_manager.py
@@ -18,38 +18,57 @@
 # https://aws.amazon.com/developers/getting-started/python/
 
 import boto3
+import base64
 from botocore.exceptions import ClientError
 
 
 def get_secret():
-    secret_name = "MySecretName"
-    endpoint_url = "https://secretsmanager.us-west-2.amazonaws.com"
-    region_name = "us-west-2"
 
+    secret_name = "<<{{MySecretName}}>>"
+    region_name = "<<{{MyRegionName}}>>"
+
+    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
-        region_name=region_name,
-        endpoint_url=endpoint_url
+        region_name=region_name
     )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
 
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("The requested secret " + secret_name + " was not found")
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            print("The request was invalid due to:", e)
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
         elif e.response['Error']['Code'] == 'InvalidParameterException':
-            print("The request had invalid params:", e)
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
     else:
-        # Decrypted secret using the associated KMS CMK
-        # Depending on whether the secret was a string or binary, one of these fields will be populated
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
         if 'SecretString' in get_secret_value_response:
-            text_secret_data = get_secret_value_response['SecretString']
+            secret = get_secret_value_response['SecretString']
         else:
-            binary_secret_data = get_secret_value_response['SecretBinary']
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
             
-        # Your code goes here. 
+    # Your code goes here. 
