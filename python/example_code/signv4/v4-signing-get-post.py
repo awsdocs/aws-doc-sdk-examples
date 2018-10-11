9ddbbf2145dbe340a23c02521616fb09f6ea5944
Jami Schwarzwalder <jamisch@amazon.com>
2018-10-11 15:09:24 -0700

    updating details for Signature Version 4 python samples

--- python/example_code/signv4/v4-signing-get-post.py
+++ python/example_code/signv4/v4-signing-get-post.py
@@ -143,11 +143,11 @@ print('Response code: %d\n' % r.status_code)
 print(r.text)
  
 
-#snippet-sourcedescription:[<<FILENAME>> demonstrates how to ...]
+#snippet-sourcedescription:[v4-signing-get-post  shows how to make a request using the Amazon EC2 query API. The request makes a GET request and passes authentication information to AWS using the Authorization header.]
 #snippet-keyword:[Python]
 #snippet-keyword:[Code Sample]
-#snippet-service:[<<ADD SERVICE>>]
+#snippet-service:[AWS Signature Version 4 Signing Process]
 #snippet-sourcetype:[full-example]
-#snippet-sourcedate:[]
+#snippet-sourcedate:[9/20/2018]
 #snippet-sourceauthor:[AWS]
 
