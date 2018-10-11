b212c014ccc047adcc8f4e4f04f7db281c21c518
Jami Schwarzwalder <jamisch@amazon.com>
2018-10-11 15:14:33 -0700

    adding Service Names

--- python/example_code/config/put_config_rule.py
+++ python/example_code/config/put_config_rule.py
@@ -43,7 +43,7 @@ except ClientError as e:
 #snippet-sourcedescription:[<<FILENAME>> demonstrates how to ...]
 #snippet-keyword:[Python]
 #snippet-keyword:[Code Sample]
-#snippet-service:[<<ADD SERVICE>>]
+#snippet-service:[AWS Config]
 #snippet-sourcetype:[full-example]
 #snippet-sourcedate:[]
 #snippet-sourceauthor:[AWS]
