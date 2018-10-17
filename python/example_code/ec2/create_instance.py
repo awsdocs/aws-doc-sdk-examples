686f20604c44a7cddf663b6b2a2b5ac5ab00d927
Yajana <31155354+Yajan@users.noreply.github.com>
2018-10-17 18:34:54 +0530

    Update create_instance.py

--- python/example_code/ec2/create_instance.py
+++ python/example_code/ec2/create_instance.py
@@ -1,4 +1,16 @@
-# program to create instance from ami with all configaration
+# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
 import boto3
 try:
     ec2 = boto3.resource('ec2', region_name='REGION')
