fa700c02a341609d2e78b890771375018d752ffc
Soo Sung <soosung@amazon.com>
2018-10-17 12:44:49 -0700

    adding service ARN for athena

--- java/example_code/athena/src/main/java/aws/example/athena/AthenaClientFactory.java
+++ java/example_code/athena/src/main/java/aws/example/athena/AthenaClientFactory.java
@@ -1,8 +1,8 @@
- 
+
 //snippet-sourcedescription:[<<FILENAME>> demonstrates how to ...]
 //snippet-keyword:[Java]
 //snippet-keyword:[Code Sample]
-//snippet-service:[<<ADD SERVICE>>]
+//snippet-service:[athena]
 //snippet-sourcetype:[<<snippet or full-example>>]
 //snippet-sourcedate:[]
 //snippet-sourceauthor:[AWS]
