9217e91024f15f85e3d3fcd07ae872455675598f
Doug-AWS <dougsch@amazon.com>
2018-10-19 07:46:42 -0700

    Added snippet tags to SQS, Glacier, and Extending sections for Go

--- go/example_code/glacier/create_vault.go
+++ go/example_code/glacier/create_vault.go
@@ -1,3 +1,12 @@
+//snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+//snippet-sourceauthor:[Doug-AWS]
+//snippet-sourcedescription:[Creates a Glacier vault.]
+//snippet-keyword:[Amazon Glacier]
+//snippet-keyword:[CreateVault function]
+//snippet-keyword:[Go]
+//snippet-service:[glacier]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[2018-03-16]
 /*
    Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
