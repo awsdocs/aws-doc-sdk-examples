7f7b8c974347bc423c9c1229be549f861445ca7f
Gabriel Rabelo <31012788+omitogabriel@users.noreply.github.com>
2018-10-19 10:27:57 -0300

    Alter

---
+++ SendMessage.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ *  ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/sqs-examples-send-receive-messages.html
+ *
+ */
+require 'vendor/autoload.php';
+
+use Aws\Sqs\SqsClient;
+use Aws\Exception\AwsException;
+
+/**
+ * Receive SQS Queue with Long Polling
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+
+$client = new SqsClient([
+    'profile' => 'default',
+    'region' => 'us-west-2',
+    'version' => '2012-11-05'
+]);
+
+$params = [
+    'DelaySeconds' => 10,
+    'MessageAttributes' => [
+        "Title" => [
+            'DataType' => "String",
+            'StringValue' => "The Hitchhiker's Guide to the Galaxy"
+        ],
+        "Author" => [
+            'DataType' => "String",
+            'StringValue' => "Autor name."
+        ],
+        "WeeksOn" => [
+            'DataType' => "Number",
+            'StringValue' => "6"
+        ]
+    ],
+    'MessageBody' => "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+    'QueueUrl' => 'QUEUE_URL'
+];
+
+try {
+    $result = $client->sendMessage($params);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
