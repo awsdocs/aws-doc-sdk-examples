1181f3976282c2f5f84fe24b81036c0ff1870224
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2018-10-22 15:59:44 -0700

    Update DeleteBucketCors.php

--- php/example_code/s3/DeleteBucketCors.php
+++ php/example_code/s3/DeleteBucketCors.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -53,50 +52,3 @@ try {
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2018-09-20]
 //snippet-sourceauthor:[jschwarzwalder]
-
-=======
-<?php
-/**
- * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-
-require 'vendor/autoload.php';
-
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-
-/**
- * Delete bucket cors
- *
- * This code expects that you have AWS credentials set up per:
- * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
- */
-
-$bucketName = 'BUCKET_NAME';
-
-$client = new S3Client([
-    'profile' => 'default',
-    'region' => 'us-west-2',
-    'version' => '2006-03-01'
-]);
-
-try {
-    $result = $client->deleteBucketCors([
-        'Bucket' => $bucketName, // REQUIRED
-    ]);
-    var_dump($result);
-} catch (AwsException $e) {
-    // output error message if fails
-    error_log($e->getMessage());
-}
->>>>>>> master
