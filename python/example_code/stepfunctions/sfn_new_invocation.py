18116b0c393bd5c5b39a76b305e56f11e30e4543
Prajilesh <nprajilesh@gmail.com>
2018-10-26 14:52:28 +0530

    metadata added

--- python/example_code/stepfunctions/sfn_new_invocation.py
+++ python/example_code/stepfunctions/sfn_new_invocation.py
@@ -13,11 +13,12 @@
 
 import boto3
 
-#The Amazon Resource Name (ARN) of the state machine to execute.
-STATE_MACHINE_ARN = 'sfArn'
+# The Amazon Resource Name (ARN) of the state machine to execute.
+# Example - arn:aws:states:us-west-2:112233445566:stateMachine:HelloWorld-StateMachine
+STATE_MACHINE_ARN = 'statemachineArn'
 
 #The name of the execution
-EXECUTION_NAME = 'name'
+EXECUTION_NAME = 'HelloWorld-StateMachine-Exec3'
 
 #The string that contains the JSON input data for the execution
 INPUT = "{}"
@@ -31,4 +32,16 @@ response = sfn.start_execution(
 )
 
 #display the arn that identifies the execution
-print(response.get('stateMachineArn'))
+print(response.get('executionArn'))
+
+
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[sfn_new_invocation.py demonstrates how to invoke a statemachine using its ARN]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[AWS Step Functions]
+#snippet-service:[states]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2018-10-25]
+#snippet-sourceauthor:[nprajilesh]
