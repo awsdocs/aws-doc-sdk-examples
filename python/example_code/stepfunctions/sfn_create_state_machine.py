f349d546b8d3cef9a24e9c80447faa3d5346bda7
Prajilesh <nprajilesh@gmail.com>
2018-10-26 14:34:28 +0530

    helloworld stepfunction defintion added

--- python/example_code/stepfunctions/sfn_create_state_machine.py
+++ python/example_code/stepfunctions/sfn_create_state_machine.py
@@ -13,22 +13,24 @@
 
 import boto3
 
-#The name of the state machine
-SF_NAME = 'name'
+# The name of the state machine
+SF_NAME = 'HelloWorld-StateMachine'
 
 # The Amazon States Language definition of the state machine
-SF_DEFINITION = 'definition'
+HELLO_WORLD_SF_DEF = '{"StartAt": "HelloWorld", "States": ' \
+                     '{"HelloWorld": {"Type": "Pass", "Result": "Hello World!", "End": true}}}'
 
-#Arn of the IAM role to use for this state machine
+# Arn of the IAM role to use for this state machine
+# Replace the value with a valid RoleArn
 ROLE_ARN = 'roleArn'
 
 sfn = boto3.client('stepfunctions')
 
 response = sfn.create_state_machine(
-    name= SF_NAME,
-    definition=SF_DEFINITION,
+    name=SF_NAME,
+    definition=HELLO_WORLD_SF_DEF,
     roleArn=ROLE_ARN
 )
 
-#print the statemachine Arn
+# print the statemachine Arn
 print(response.get('stateMachineArn'))
