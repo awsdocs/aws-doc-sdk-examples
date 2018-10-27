0b276decca3c592558b3872340bb1fd0abd08263
Jason Hedges <jasonhedges@live.com>
2018-10-27 17:55:55 -0500

    Added metadata to both files and implemented suggested changes.

--- python/example_code/sns/sns-python-example-create-topic.py
+++ python/example_code/sns/sns-python-example-create-topic.py
@@ -14,5 +14,17 @@
 import boto3
 
 sns = boto3.client('sns')
-sns.create_topic(Name='my-topic')
+response = sns.create_topic(Name='my-topic')
+
+print(response)
  
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[sns-python-example-create-topic.py demonstrates how to create a new AWS SNS topic with the specified name.]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[Amazon Simple Notification Service]
+#snippet-service:[sns]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2018-10-26]
+#snippet-sourceauthor:[jasonhedges]
