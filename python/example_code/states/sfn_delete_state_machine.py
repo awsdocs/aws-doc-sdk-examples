21c083ba976eac7da24a1a72f47ca311974b02a7
Jami Schwarzwalder <jamisch@amazon.com>
2018-10-26 22:59:12 -0700

    adding more samples for step functions

---
+++ python/example_code/states/sfn_delete_state_machine.py
@@ -0,0 +1,41 @@
+# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+
+# The Amazon Resource Name (ARN) of the state machine to execute.
+# Example - arn:aws:states:us-west-2:112233445566:stateMachine:HelloWorld-StateMachine
+STATE_MACHINE_ARN = 'statemachineArn'
+
+
+sfn = boto3.client('stepfunctions')
+
+response = sfn.delete_state_machine(
+    stateMachineArn=STATE_MACHINE_ARN
+)
+
+# print the statemachine Arn
+print(response)
+
+
+
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[sfn_delete_state_machine.py demonstrates how to delete an existing statemachine]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[AWS Step Functions]
+#snippet-service:[states]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2018-10-26]
+#snippet-sourceauthor:[jschwarzwalder (AWS)]
