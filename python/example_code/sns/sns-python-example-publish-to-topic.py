36c732326a138d9084f2fc627abf2a43c339ace9
Jason Hedges <jasonhedges@live.com>
2018-10-28 14:38:23 -0500

    Python sample code to publish message to SNS topic

---
+++ python/example_code/sns/sns-python-example-publish-to-topic.py
@@ -0,0 +1,37 @@
+# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+
+# Create an SNS client
+sns = boto3.client('sns')
+
+# Publish a simple message to the specified SNS topic
+response = sns.publish(
+    TopicArn='My-TopicArn',    
+    Message='Hello World!',    
+)
+
+# Print out the response
+print(response)
+ 
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[sns-python-example-publich-to-topic.py demonstrates how to publish a message to an AWS SNS topic.]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[Amazon Simple Notification Service]
+#snippet-service:[sns]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2018-10-28]
+#snippet-sourceauthor:[jasonhedges]
