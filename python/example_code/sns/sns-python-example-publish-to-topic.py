833f6fdbcae84f32313bbb48e8a1bb65b6dd8bd1
Jason Hedges <jasonhedges@live.com>
2018-11-02 08:13:05 -0500

    Modified the sample TopicArn styling to better match a real Arn

--- python/example_code/sns/sns-python-example-publish-to-topic.py
+++ python/example_code/sns/sns-python-example-publish-to-topic.py
@@ -18,7 +18,7 @@ sns = boto3.client('sns')
 
 # Publish a simple message to the specified SNS topic
 response = sns.publish(
-    TopicArn='my-topic-arn',    
+    TopicArn='arn:aws:sns:region:0123456789:my-topic-arn',    
     Message='Hello World!',    
 )
 
