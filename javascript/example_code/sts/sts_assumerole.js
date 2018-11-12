c1d12c418c72cb5937d5cf44db2a0f12b455308b
Keith Walker <keiwlk@amazon.com>
2018-11-12 12:21:29 -0800

    Added JS STS AssumeRole example

---
+++ javascript/example_code/sts/sts_assumerole.js
@@ -0,0 +1,51 @@
+ 
+//snippet-sourcedescription:[sts_assumerole.js demonstrates how to use STS to assume an IAM Role.]
+//snippet-keyword:[JavaScript]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[AWS Security Token Service (STS)]
+//snippet-service:[sts]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[2018-11-12]
+//snippet-sourceauthor:[walkerk1980]
+
+
+// Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Apache-2.0 License on an "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND.   
+
+// ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sts-examples-policies.html
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+var roleToAssume = {RoleArn: 'arn:aws:iam::123456789012:role/RoleName', RoleSessionName: 'session1', DurationSeconds: 900,};
+var roleCreds;
+
+// Create the STS service object    
+var sts = new AWS.STS({apiVersion: '2011-06-15'});
+
+//Assume Role
+sts.assumeRole(roleToAssume, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else{
+        roleCreds = { accessKeyId: data.Credentials.AccessKeyId,secretAccessKey: data.Credentials.SecretAccessKey,sessionToken: data.Credentials.SessionToken};
+        stsGetCallerIdentity(roleCreds);
+    }
+});
+
+//Get Arn of current identity
+function stsGetCallerIdentity(creds) {
+    var stsParams = {credentials: creds };
+    // Create STS service object
+    var sts = new AWS.STS(stsParams);
+        
+    sts.getCallerIdentity({}, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        else {
+            console.log(data.Arn);
+        }
+    });    
+}
