a24a89f94f7499b9557a4976e0ea9a0226f93d2a
Prajilesh <nprajilesh@gmail.com>
2018-11-13 11:28:10 +0530

    python example for describe subnets

---
+++ python/example_code/ec2/describe_subnets.py
@@ -0,0 +1,31 @@
+# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_subnets()
+print(response)
+
+
+# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+# snippet-sourcedescription:[describe_subnets.py demonstrates how to describe describe one or more of your Subnets.]
+# snippet-keyword:[Python]
+# snippet-keyword:[AWS SDK for Python (Boto3)]
+# snippet-keyword:[Code Sample]
+# snippet-keyword:[Amazon EC2]
+# snippet-service:[ec2]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[2018-11-13]
+# snippet-sourceauthor:[nprajilesh]
+
