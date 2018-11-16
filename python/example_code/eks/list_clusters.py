a2f0d95754d5e86c490fa152db26098f8e7bdf73
Steven Calwas <scalwas@amazon.com>
2018-11-15 16:57:47 -0800

    Enhance method capabilities

--- python/example_code/eks/list_clusters.py
+++ python/example_code/eks/list_clusters.py
@@ -10,20 +10,53 @@
 # OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-import boto3, sys
 
-# Create EKS client
+import boto3
 
-eks = boto3.client('eks');
 
-# List clusters
-clusters = eks.list_clusters()['clusters']
+def list_clusters(max_clusters=10, iter_marker=''):
+    """List the Amazon EKS clusters in the AWS account's default region.
 
-if not clusters:
-  print "No EKS cluster configured in this region"
-  sys.exit(0)
+    :param max_clusters: Maximum number of clusters to retrieve.
+    :param iter_marker: Marker used to identify start of next batch of clusters to retrieve
+    :return: List of cluster names
+    :return: String marking the start of next batch of clusters to retrieve. Pass this string as the iter_marker
+        argument in the next invocation of list_clusters().
+    """
 
-print 'list of clusters:'
-for cluster in eks.list_clusters()['clusters']: 
-  print cluster
+    eks = boto3.client('eks')
 
+    clusters = eks.list_clusters(maxResults=max_clusters, nextToken=iter_marker)
+    marker = clusters.get('nextToken')       # None if no more clusters to retrieve
+    return clusters['clusters'], marker
+
+
+def main():
+    clusters, marker = list_clusters()
+    if not clusters:
+        print('No clusters exist.')
+    else:
+        while True:
+            # Print cluster names
+            for cluster in clusters:
+                print(cluster)
+
+            # If no more clusters exist, exit loop, otherwise retrieve the next batch
+            if marker is None:
+                break
+            clusters, marker = list_clusters(iter_marker=marker)
+
+
+if __name__ == '__main__':
+    main()
+
+# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+# snippet-sourcedescription:[list_clusters.py demonstrates how to list the Amazon EKS clusters in the AWS account's default region.]
+# snippet-keyword:[Python]
+# snippet-keyword:[AWS SDK for Python (Boto3)]
+# snippet-keyword:[Code Sample]
+# snippet-keyword:[Amazon EKS]
+# snippet-service:[eks]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[2018-11-15]
+# snippet-sourceauthor:[scalwas (AWS)]
