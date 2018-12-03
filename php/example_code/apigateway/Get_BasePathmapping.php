e7e99541e33844e57d0b9e1c37c247d11c747397
Jami Schwarzwalder <jamisch@amazon.com>
2018-12-03 10:59:49 -0800

    examples for BasePathMaping for Aws\ApiGateway\ApiGatewayClient

---
+++ php/example_code/apigateway/Get_BasePathmapping.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE => This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/ses-template.html
+ *
+ */
+
+require 'vendor/autoload.php';
+
+use Aws\ApiGateway\ApiGatewayClient;
+use Aws\Exception\AwsException;
+
+//Create a ApiGatewayClient
+$APIGatClient = new Aws\ApiGateway\ApiGatewayClient([
+    'profile' => 'default',
+    'version' => '2015-07-09',
+    'region' => 'us-east-2'
+]);
+
+$basePath = '(none)';
+$domainName = 'example.com';
+
+try {
+    $result = $APIGatClient->getBasePathMapping([
+        'basePath' => $basePath
+        'domainName' => $domainName,
+        'patchOperations' => [
+        [
+            'op' => 'move',
+            'path' => '/admin',
+            'value' => 'a1b2c3-admin',
+        ],
+    ]);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo "\n";
+}
+ 
+
+//snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+//snippet-sourcedescription:[Get_BasePathmapping.php demonstrates how to view Base Path Mapping in API Gateway.]
+//snippet-keyword:[PHP]
+//snippet-keyword:[AWS SDK for PHP v3]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[Amazon API Gateway]
+//snippet-service:[apigateway]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[]
+//snippet-sourceauthor:[jschwarzwalder (AWS)]
+
