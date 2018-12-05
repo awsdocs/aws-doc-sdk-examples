1e3dcdeabe28d27932bf83fe7ee049583d2f4fe3
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2018-12-05 02:09:51 -0500

    Added copyright info, setup & usage info

--- typescript/example_code/s3_rxjs/controller.ts
+++ typescript/example_code/s3_rxjs/controller.ts
@@ -1,3 +1,25 @@
+// Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// This file is licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
+// To install stable release of RxJS: npm install rxjs
+// To install TypeStrong/ts-node: npm install -D ts-node
+// To install AWS SDK: npm install aws-sdkservice
+
+// This CRUD API can be used in an Angular 2+ service
+// To create Angular service: ng generate service [Name of service]
+// Please refer to https://angular.io/tutorial/toh-pt4 for more information on Angular services
+// Can be implemented inside ReactJS or React Native components to easily manage asynchronous data streams 
+// Please see https://hackernoon.com/what-happens-when-you-use-rxjs-in-react-11ae5163fc0a for more information
+
 import * as AWS from 'aws-sdk/global';
 import * as S3 from 'aws-sdk/clients/s3';
 import { Observable } from 'rxjs';
