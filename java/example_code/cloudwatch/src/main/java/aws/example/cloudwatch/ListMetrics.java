0edf8c7a52a77e405b71931bd9cee9c06f84368c
Jami Schwarzwalder <jamisch@amazon.com>
2018-12-28 10:35:52 -0800

    updating copyright to 2019

--- java/example_code/cloudwatch/src/main/java/aws/example/cloudwatch/ListMetrics.java
+++ java/example_code/cloudwatch/src/main/java/aws/example/cloudwatch/ListMetrics.java
@@ -7,7 +7,7 @@
 //snippet-sourcedate:[2018-01-15]
 //snippet-sourceauthor:[soo-aws]
 /*
- * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
--- java/example_code/s3/src/main/java/aws/example/s3/DeleteWebsiteConfiguration.java
+++ java/example_code/s3/src/main/java/aws/example/s3/DeleteWebsiteConfiguration.java
@@ -7,7 +7,7 @@
 //snippet-sourcedate:[]
 //snippet-sourceauthor:[soo-aws]
 /*
-   Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
    This file is licensed under the Apache License, Version 2.0 (the "License").
    You may not use this file except in compliance with the License. A copy of
