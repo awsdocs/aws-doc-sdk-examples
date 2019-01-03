fb5132172634790a446898c3b625a9cafb3a20c5
Jami Schwarzwalder <jschwarzwalder@users.noreply.github.com>
2019-01-03 11:30:27 -0800

    Update create_role.py

--- python/example_code/iam/create_role.py
+++ python/example_code/iam/create_role.py
@@ -11,6 +11,8 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License. 
 # snippet-start:[iam.python.create_user.complete]
+
+# Requires boto3 1.9.72
 import boto3, json
 
 # Create IAM client
