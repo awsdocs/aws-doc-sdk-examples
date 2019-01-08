d93ca6ffdd238f3e72025df74677cd0a42c59b4f
Kurt De Ding <kurtding@amazon.com>
2019-01-08 01:20:11 +0000

    added new examples for deeplens

---
+++ python/example_code/deeplens/deeplens_frameworks_mxnet.py
@@ -0,0 +1,43 @@
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+# snippet-start:[deeplens.python.deeplens_frameworks_mxnet.export_model_gluon_api]
+
+import mxnet as mx
+from mxnet.gluon.model_zoo import vision
+squeezenet = vision.squeezenet_v1(pretrained=True, ctx=mx.cpu())
+
+# To export, you need to hybridize your gluon model,
+squeezenet.hybridize()
+
+# SqueezeNet’s input pattern is 224 pixel X 224 pixel images. Prepare a fake image,
+fake_image = mx.nd.random.uniform(shape=(1,3,224,224), ctx=mx.cpu())
+
+# Run the model once.
+result = squeezenet(fake_image)
+
+# Now you can export the model. You can use a path if you want ‘models/squeezenet’.
+squeezenet.export(‘squeezenet')
+
+#snippet-end:[deeplens.python.deeplens_frameworks_mxnet.export_model_gluon_api]
+
+
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[deeplens_kinesis_integration.py demonstrates how to create an inference Lambda function on an AWS DeepLens model.]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[AWS DeepLens]
+#snippet-service:[deeplens]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2019-01-07]
+#snippet-sourceauthor:[AWS]
---
+++ python/example_code/deeplens/deeplens_kvi_api.py
@@ -0,0 +1,68 @@
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+# snippet-start:[deeplens.python.deeplens_kvi_api.intro]
+
+import time
+import os
+import DeepLens_Kinesis_Video as dkv
+from botocore.session import Session
+import greengrasssdk
+
+def greengrass_hello_world_run():
+    # Create the green grass client so that we can send messages to IoT console
+    client = greengrasssdk.client('iot-data')
+    iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+
+    # Stream configuration, name and retention
+    # Note that the name will appear as deeplens-myStream
+    stream_name = 'myStream'
+    retention = 2 #hours
+
+    # Amount of time to stream
+    wait_time = 60 * 60 * 5 #seconds
+
+    # Use the boto session API to grab credentials
+    session = Session()
+    creds = session.get_credentials()
+
+    # Create producer and stream.
+    producer = dkv.createProducer(creds.access_key, creds.secret_key, creds.token, "us-east-1")
+    client.publish(topic=iot_topic, payload="Producer created")
+    kvs_stream = producer.createStream(stream_name, retention)
+    client.publish(topic=iot_topic, payload="Stream {} created".format(stream_name))
+
+    # Start putting data into the KVS stream
+    kvs_stream.start()
+    client.publish(topic=iot_topic, payload="Stream started")
+    time.sleep(wait_time)
+    # Stop putting data into the KVS stream
+    kvs_stream.stop()
+    client.publish(topic=iot_topic, payload="Stream stopped")
+
+# Execute the function above
+greengrass_hello_world_run()
+
+
+#snippet-end:[deeplens.python.deeplens_kvi_api.intro]
+
+
+#snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+#snippet-sourcedescription:[deeplens_kinesis_integration.py demonstrates how to create an inference Lambda function on an AWS DeepLens model.]
+#snippet-keyword:[Python]
+#snippet-keyword:[AWS SDK for Python (Boto3)]
+#snippet-keyword:[Code Sample]
+#snippet-keyword:[AWS DeepLens]
+#snippet-service:[deeplens]
+#snippet-sourcetype:[full-example]
+#snippet-sourcedate:[2019-01-07]
+#snippet-sourceauthor:[AWS]
