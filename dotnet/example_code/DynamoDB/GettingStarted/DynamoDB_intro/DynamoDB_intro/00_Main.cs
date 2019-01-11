6eed95892812dbdcc7b172c8ce7d01ae6680b365
Ahmed Moustafa <mouahm@amazon.com>
2019-01-11 20:15:44 +0000

    regenerated zip file to include updated code

--- dotnet/example_code/DynamoDB/GettingStarted/DynamoDB_intro/DynamoDB_intro/00_Main.cs
+++ dotnet/example_code/DynamoDB/GettingStarted/DynamoDB_intro/DynamoDB_intro/00_Main.cs
@@ -22,20 +22,6 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
 */
-/*******************************************************************************
-* Copyright 2009-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"). You may
-* not use this file except in compliance with the License. A copy of the
-* License is located at
-*
-* http://aws.amazon.com/apache2.0/
-*
-* or in the "license" file accompanying this file. This file is
-* distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied. See the License for the specific
-* language governing permissions and limitations under the License.
-*******************************************************************************/
 using System;
 using System.Collections.Generic;
 using System.Threading;
