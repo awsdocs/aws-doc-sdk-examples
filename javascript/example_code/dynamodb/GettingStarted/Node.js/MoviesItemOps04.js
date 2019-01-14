2e2b3898b274b82b9e80513a45c2858b2f4007ba
Ahmed Moustafa <mouahm@amazon.com>
2019-01-14 19:03:56 +0000

    Ruby, Python and Javascript samples

---
+++ javascript/example_code/dynamodb/GettingStarted/Node.js/MoviesItemOps04.js
@@ -0,0 +1,62 @@
+// snippet-sourcedescription:[MoviesItemOps04.js demonstrates how to ]
+// snippet-service:[dynamodb]
+// snippet-keyword:[JavaScript]
+// snippet-keyword:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[ ]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[ ]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[dynamodb.JavaScript.CodeExample.MoviesItemOps04] 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient()
+
+var table = "Movies";
+
+var year = 2015;
+var title = "The Big New Movie";
+
+// Increment an atomic counter
+
+var params = {
+    TableName:table,
+    Key:{
+        "year": year,
+        "title": title
+    },
+    UpdateExpression: "set info.rating = info.rating + :val",
+    ExpressionAttributeValues:{
+        ":val": 1
+    },
+    ReturnValues:"UPDATED_NEW"
+};
+
+console.log("Updating the item...");
+docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    }
+});
+// snippet-end:[dynamodb.JavaScript.CodeExample.MoviesItemOps04]
---
+++ ruby/example_code/dynamodb/GettingStarted/MoviesLoadData.rb
@@ -0,0 +1,56 @@
+# snippet-sourcedescription:[MoviesLoadData.rb demonstrates how to ]
+# snippet-service:[dynamodb]
+# snippet-keyword:[Ruby]
+# snippet-keyword:[Amazon DynamoDB]
+# snippet-keyword:[Code Sample]
+# snippet-keyword:[ ]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[ ]
+# snippet-sourceauthor:[AWS]
+# snippet-start:[dynamodb.Ruby.CodeExample.MoviesLoadData] 
+
+#
+#  Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  This file is licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License. A copy of
+#  the License is located at
+# 
+#  http://aws.amazon.com/apache2.0/
+# 
+#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#  specific language governing permissions and limitations under the License.
+#
+require "aws-sdk"
+require "json"
+
+Aws.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+})
+
+dynamodb = Aws::DynamoDB::Client.new
+
+table_name = 'Movies'
+
+file = File.read('moviedata.json')
+movies = JSON.parse(file)
+movies.each{|movie|
+
+    params = {
+        table_name: table_name,
+        item: movie
+    }
+
+    begin
+        result = dynamodb.put_item(params)
+        puts "Added movie: #{movie["year"]} #{movie["title"]}"
+
+    rescue  Aws::DynamoDB::Errors::ServiceError => error
+        puts "Unable to add movie:"
+        puts "#{error.message}"
+    end
+}
+
+# snippet-end:[dynamodb.Ruby.CodeExample.MoviesLoadData]
