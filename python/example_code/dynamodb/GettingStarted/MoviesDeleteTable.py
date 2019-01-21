264db699c3cd8f28198ef12611d11b07561b5ed5
Ahmed Moustafa <mouahm@amazon.com>
2019-01-21 18:25:30 +0000

    moving python examples

---
+++ python/example_code/dynamodb/GettingStarted/MoviesDeleteTable.py
@@ -0,0 +1,33 @@
+# snippet-sourcedescription:[MoviesDeleteTable.py demonstrates how to ]
+# snippet-service:[dynamodb]
+# snippet-keyword:[Python]
+# snippet-keyword:[Amazon DynamoDB]
+# snippet-keyword:[Code Sample]
+# snippet-keyword:[ ]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[ ]
+# snippet-sourceauthor:[AWS]
+# snippet-start:[dynamodb.Python.CodeExample.MoviesDeleteTable] 
+
+#
+#  Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  This file is licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License. A copy of
+#  the License is located at
+# 
+#  http://aws.amazon.com/apache2.0/
+# 
+#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#  specific language governing permissions and limitations under the License.
+#
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+table = dynamodb.Table('Movies')
+
+table.delete()
+# snippet-end:[dynamodb.Python.CodeExample.MoviesDeleteTable]
---
+++ python/example_code/dynamodb/GettingStarted/MoviesItemOps06.py
@@ -0,0 +1,69 @@
+# snippet-sourcedescription:[MoviesItemOps06.py demonstrates how to ]
+# snippet-service:[dynamodb]
+# snippet-keyword:[Python]
+# snippet-keyword:[Amazon DynamoDB]
+# snippet-keyword:[Code Sample]
+# snippet-keyword:[ ]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[ ]
+# snippet-sourceauthor:[AWS]
+# snippet-start:[dynamodb.Python.CodeExample.MoviesItemOps06] 
+
+#
+#  Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  This file is licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License. A copy of
+#  the License is located at
+# 
+#  http://aws.amazon.com/apache2.0/
+# 
+#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#  specific language governing permissions and limitations under the License.
+#
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+from botocore.exceptions import ClientError
+import json
+import decimal
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+
+table = dynamodb.Table('Movies')
+
+title = "The Big New Movie"
+year = 2015
+
+print("Attempting a conditional delete...")
+
+try:
+    response = table.delete_item(
+        Key={
+            'year': year,
+            'title': title
+        },
+        ConditionExpression="info.rating <= :val",
+        ExpressionAttributeValues= {
+            ":val": decimal.Decimal(5)
+        }
+    )
+except ClientError as e:
+    if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+        print(e.response['Error']['Message'])
+    else:
+        raise
+else:
+    print("DeleteItem succeeded:")
+    print(json.dumps(response, indent=4, cls=DecimalEncoder))
+# snippet-end:[dynamodb.Python.CodeExample.MoviesItemOps06]
