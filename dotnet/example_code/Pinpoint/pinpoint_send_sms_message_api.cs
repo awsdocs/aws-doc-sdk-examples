74c40a2097b8eadfe5fde68dbc5d9fcc372cb49d
Brent Meyer <brmey@amazon.com>
2019-01-22 13:06:08 -0800

    Added more Pinpoint message-sending examples.

---
+++ dotnet/example_code/Pinpoint/pinpoint_send_sms_message_api.cs
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
+// snippet-sourcedescription:[pinpoint_send_sms_message_api demonstrates how to send a transactional SMS message by using the SendMessages operation in the Amazon Pinpoint API.]
+// snippet-service:[Amazon Pinpoint]
+// snippet-keyword:[dotnet]
+// snippet-keyword:[.NET]
+// snippet-keyword:[Amazon Pinpoint]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[SendMessages]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[2019-01-20]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[pinpoint.dotnet.pinpoint_send_sms_message_api.complete]
+
+using System;
+using System.Collections.Generic;
+using Amazon;
+using Amazon.Pinpoint;
+using Amazon.Pinpoint.Model;
+
+namespace SendMessage
+{
+    class MainClass
+    {
+        // The AWS Region that you want to use to send the message. For a list of
+        // AWS Regions where the Amazon Pinpoint API is available, see
+        // https://alpha-docs-aws.amazon.com/pinpoint/latest/apireference/
+        private static readonly string region = "us-east-1";
+
+        // The phone number or short code to send the message from. The phone number
+        // or short code that you specify has to be associated with your Amazon Pinpoint
+        // account. For best results, specify long codes in E.164 format.
+        private static readonly string originationNumber = "+12065550199";
+
+        // The recipient's phone number.  For best results, you should specify the
+        // phone number in E.164 format.
+        private static readonly string destinationNumber = "+14255550142";
+
+        // The content of the SMS message.
+        private static readonly string message = "This message was sent through Amazon Pinpoint"
+                + "using the AWS SDK for .NET. Reply STOP to opt out.";
+
+        // The Pinpoint project/application ID to use when you send this message.
+        // Make sure that the SMS channel is enabled for the project or application
+        // that you choose.
+        private static readonly string appId = "ce796be37f32f178af652b26eexample";
+
+        // The type of SMS message that you want to send. If you plan to send
+        // time-sensitive content, specify TRANSACTIONAL. If you plan to send
+        // marketing-related content, specify PROMOTIONAL.
+        private static readonly string messageType = "TRANSACTIONAL";
+
+        // The registered keyword associated with the originating short code.
+        private static readonly string registeredKeyword = "myKeyword";
+
+        // The sender ID to use when sending the message. Support for sender ID
+        // varies by country or region. For more information, see
+        // https://alpha-docs-aws.amazon.com/pinpoint/latest/userguide/channels-sms-countries.html
+        private static readonly string senderId = "mySenderId";
+
+        public static void Main(string[] args)
+        {
+            using (AmazonPinpointClient client = new AmazonPinpointClient(RegionEndpoint.GetBySystemName(region)))
+            {
+                SendMessagesRequest sendRequest = new SendMessagesRequest
+                {
+                    ApplicationId = appId,
+                    MessageRequest = new MessageRequest
+                    {
+                        Addresses = new Dictionary<string, AddressConfiguration>
+                        {
+                            {
+                                destinationNumber,
+                                new AddressConfiguration
+                                {
+                                    ChannelType = "SMS"
+                                }
+                            }
+                        },
+                        MessageConfiguration = new DirectMessageConfiguration
+                        {
+                            SMSMessage = new SMSMessage
+                            {
+                                Body = message,
+                                MessageType = messageType,
+                                OriginationNumber = originationNumber,
+                                SenderId = senderId,
+                                Keyword = registeredKeyword
+                            }
+                        }
+                    }
+                };
+                try
+                {
+                    Console.WriteLine("Sending message...");
+                    SendMessagesResponse response = client.SendMessages(sendRequest);
+                    Console.WriteLine("Message sent!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The message wasn't sent. Error message: " + ex.Message);
+                }
+            }
+        }
+    }
+}
+
+// snippet-end:[pinpoint.dotnet.pinpoint_send_sms_message_api.complete]
