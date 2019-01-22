313af4ede8d24b7a46cea0575da763d5de1b0218
Ahmed Moustafa <mouahm@amazon.com>
2019-01-22 21:23:06 +0000

    PHP dynamodb samples

---
+++ php/example_code/dynamodb/GettingStarted/MoviesItemOps04.php
@@ -0,0 +1,82 @@
+<?php
+// snippet-sourcedescription:[MoviesItemOps04.php demonstrates how to ]
+// snippet-service:[dynamodb]
+// snippet-keyword:[PHP]
+// snippet-keyword:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[ ]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[ ]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[dynamodb.php.codeexample.MoviesItemOps04] 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
+require 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$sdk = new Aws\Sdk([
+    'endpoint'   => 'http://localhost:8000',
+    'region'   => 'us-west-2',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+$tableName = 'Movies';
+
+$year = 2015;
+$title = 'The Big New Movie';
+
+$key = $marshaler->marshalJson('
+    {
+        "year": ' . $year . ', 
+        "title": "' . $title . '"
+    }
+');
+
+$eav = $marshaler->marshalJson('
+    {
+        ":val": 1
+    }
+');
+
+$params = [
+    'TableName' => $tableName,
+    'Key' => $key,
+    'UpdateExpression' => 'set info.rating = info.rating + :val',
+    'ExpressionAttributeValues'=> $eav,
+    'ReturnValues' => 'UPDATED_NEW'
+];
+
+try {
+    $result = $dynamodb->updateItem($params);
+    echo "Updated item. ReturnValues are:\n";
+    print_r($result['Attributes']);
+
+} catch (DynamoDbException $e) {
+    echo "Unable to update item:\n";
+    echo $e->getMessage() . "\n";
+}
+
+
+
+// snippet-end:[dynamodb.php.codeexample.MoviesItemOps04] 
+?>
