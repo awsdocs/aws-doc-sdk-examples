0a230c2e6640b4b03296bdc324272a8c13e17aa1
Ahmed Moustafa <mouahm@amazon.com>
2019-01-23 17:41:21 +0000

    Dynamodb Dax Samples- last samples to move

---
+++ javascript/example_code/dynamodb/TryDax/01-create-table.js
@@ -0,0 +1,62 @@
+// snippet-sourcedescription:[01-create-table.js demonstrates how to ]
+// snippet-service:[dynamodb]
+// snippet-keyword:[JavaScript]
+// snippet-keyword:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[ ]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[ ]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[dynamodb.javascript.trydax.01-create-table] 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+const AmazonDaxClient = require('amazon-dax-client');
+var AWS = require("aws-sdk");
+
+var region = "us-west-2";
+
+AWS.config.update({
+  region: region
+});
+
+var dynamodb = new AWS.DynamoDB() //low-level client
+
+var tableName = "TryDaxTable";
+
+var params = {
+    TableName : tableName,
+    KeySchema: [       
+        { AttributeName: "pk", KeyType: "HASH"},  //Partition key
+        { AttributeName: "sk", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "pk", AttributeType: "N" },
+        { AttributeName: "sk", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
+
+// snippet-end:[dynamodb.javascript.trydax.01-create-table] 
---
+++ javascript/example_code/dynamodb/TryDax/03-getitem-test.js
@@ -0,0 +1,79 @@
+// snippet-sourcedescription:[03-getitem-test.js demonstrates how to ]
+// snippet-service:[dynamodb]
+// snippet-keyword:[JavaScript]
+// snippet-keyword:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[ ]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[ ]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[dynamodb.javascript.trydax.03-getitem-test] 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+const AmazonDaxClient = require('amazon-dax-client');
+var AWS = require("aws-sdk");
+
+var region = "us-west-2";
+
+AWS.config.update({
+  region: region
+});
+
+var ddbClient = new AWS.DynamoDB.DocumentClient()
+var daxClient = null;
+
+if (process.argv.length > 2) {
+    var dax = new AmazonDaxClient({endpoints: [process.argv[2]], region: region})
+    daxClient = new AWS.DynamoDB.DocumentClient({service: dax });
+}
+
+var client = daxClient != null ? daxClient : ddbClient;
+var tableName = "TryDaxTable";
+
+var pk = 1;
+var sk = 10;
+var iterations = 5;
+
+for (var i = 0; i < iterations; i++) {
+
+    var startTime = new Date().getTime();
+
+    for (var ipk = 1; ipk <= pk; ipk++) {
+        for (var isk = 1; isk <= sk; isk++) {
+
+            var params = {
+                TableName: tableName,
+                Key:{
+                    "pk": ipk,
+                    "sk": isk
+                }
+            };
+
+            client.get(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    // GetItem succeeded
+                }
+            });
+        }
+    }
+    
+    var endTime = new Date().getTime();
+    console.log("\tTotal time: ", (endTime - startTime) , "ms - Avg time: ", (endTime - startTime) / iterations, "ms");
+
+}
+
+// snippet-end:[dynamodb.javascript.trydax.03-getitem-test] 
