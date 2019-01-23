1583c12a799d2c0d9986deeb9658a85e5cda3642
Ahmed Moustafa <mouahm@amazon.com>
2019-01-23 17:41:21 +0000

    Dynamodb Dax Samples- last samples to move

---
+++ dotnet/example_code/DynamoDB/TryDax/06-DeleteTable.cs
@@ -0,0 +1,55 @@
+// snippet-sourcedescription:[06-DeleteTable.cs demonstrates how to ]
+// snippet-service:[dynamodb]
+// snippet-keyword:[dotNET]
+// snippet-keyword:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[ ]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[ ]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[dynamodb.dotNET.trydax.06-DeleteTable] 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+using Amazon.DynamoDBv2.Model;
+using System;
+using Amazon.DynamoDBv2;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+            var tableName = "TryDaxTable";
+
+            var request = new DeleteTableRequest()
+            {
+                TableName = tableName
+            };
+
+            var response = client.DeleteTableAsync(request).Result;
+
+
+            Console.WriteLine("Hit <enter> to continue...");
+            Console.ReadLine();
+        }
+    }
+
+}
+
+// snippet-end:[dynamodb.dotNET.trydax.06-DeleteTable] 
