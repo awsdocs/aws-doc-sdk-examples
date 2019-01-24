29ac3da46cfd34bcdbd291982eaa941887bfd3cb
Jami Schwarzwalder <jamisch@amazon.com>
2019-01-24 12:56:21 -0800

    Revert "Revert "Pinpoint code examples""
    This reverts commit 759839224ea71aee6f255b0fe110aef096689b80.

---
+++ java/example_code/pinpoint/pinpoint_send_sms_message_api.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
+// snippet-sourcedescription:[pinpoint_send_sms_message_api demonstrates how send a transactional SMS message by using the SendMessages operation in the Amazon Pinpoint API.]
+// snippet-service:[Amazon Pinpoint]
+// snippet-keyword:[Java]
+// snippet-keyword:[Amazon Pinpoint]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[SendMessages]
+// snippet-sourcetype:[snippet]
+// snippet-sourcedate:[2019-01-20]
+// snippet-sourceauthor:[AWS]
+// snippet-start:[pinpoint.java.pinpoint_send_sms_message_api.complete]
+
+package com.amazonaws.samples;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.pinpoint.AmazonPinpoint;
+import com.amazonaws.services.pinpoint.AmazonPinpointClientBuilder;
+import com.amazonaws.services.pinpoint.model.AddressConfiguration;
+import com.amazonaws.services.pinpoint.model.ChannelType;
+import com.amazonaws.services.pinpoint.model.DirectMessageConfiguration;
+import com.amazonaws.services.pinpoint.model.MessageRequest;
+import com.amazonaws.services.pinpoint.model.SMSMessage;
+import com.amazonaws.services.pinpoint.model.SendMessagesRequest;
+
+public class SendMessage {
+
+    // The AWS Region that you want to use to send the message. For a list of
+    // AWS Regions where the Amazon Pinpoint API is available, see
+    // https://docs.aws.amazon.com/pinpoint/latest/apireference/
+    public static String region = "us-east-1";
+     
+    // The phone number or short code to send the message from. The phone number 
+    // or short code that you specify has to be associated with your Amazon Pinpoint 
+    // account. For best results, specify long codes in E.164 format.
+    public static String originationNumber = "+12065550199";
+     
+    // The recipient's phone number.  For best results, you should specify the
+    // phone number in E.164 format.
+    public static String destinationNumber = "+14255550142";
+     
+    // The content of the SMS message.     
+    public static String message = "This message was sent through Amazon Pinpoint "
+            + "using the AWS SDK for Java. Reply STOP to "
+            + "opt out.";
+     
+    // The Pinpoint project/application ID to use when you send this message.
+    // Make sure that the SMS channel is enabled for the project or application
+    // that you choose.
+    public static String appId = "ce796be37f32f178af652b26eexample";
+
+    // The type of SMS message that you want to send. If you plan to send 
+    // time-sensitive content, specify TRANSACTIONAL. If you plan to send 
+    // marketing-related content, specify PROMOTIONAL.
+    public static String messageType = "TRANSACTIONAL";
+    
+    // The registered keyword associated with the originating short code.
+    public static String registeredKeyword = "myKeyword";
+     
+    // The sender ID to use when sending the message. Support for sender ID 
+    // varies by country or region. For more information, see
+    // https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-countries.html
+    public static String senderId = "MySenderID";
+     
+    public static void main(String[] args) throws IOException {
+          
+        try {               
+            Map<String,AddressConfiguration> addressMap = 
+                    new HashMap<String,AddressConfiguration>();
+               
+            addressMap.put(destinationNumber, new AddressConfiguration()
+                    .withChannelType(ChannelType.SMS));
+               
+            AmazonPinpoint client = AmazonPinpointClientBuilder.standard()
+                    .withRegion(region).build();
+               
+            SendMessagesRequest request = new SendMessagesRequest()
+                    .withApplicationId(appId)
+                    .withMessageRequest(new MessageRequest()
+                    .withAddresses(addressMap)                                   
+                    .withMessageConfiguration(new DirectMessageConfiguration()
+                            .withSMSMessage(new SMSMessage()
+                                    .withBody(message)
+                                    .withMessageType(messageType)
+                                    .withOriginationNumber(originationNumber)
+                                    .withSenderId(senderId)
+                                    .withKeyword(registeredKeyword)
+                            )
+                    )
+            );
+            System.out.println("Sending message...");               
+            client.sendMessages(request);
+            System.out.println("Message sent!");
+    } catch (Exception ex) {
+        System.out.println("The message wasn't sent. Error message: " 
+                + ex.getMessage());
+        }
+    }
+}
+
+// snippet-end:[pinpoint.java.pinpoint_send_sms_message_api.complete]
---
+++ python/example_code/pinpoint/pinpoint_send_email_smtp.py
@@ -0,0 +1,128 @@
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http:#aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# snippet-sourcedescription:[pinpoint_send_email_smtp demonstrates how to send a transactional email by using the Amazon Pinpoint SMTP interface.]
+# snippet-service:[mobiletargeting]
+# snippet-keyword:[Python]
+# snippet-keyword:[Amazon Pinpoint]
+# snippet-keyword:[Code Sample]
+# snippet-sourcetype:[full-example]
+# snippet-sourcedate:[2019-01-20]
+# snippet-sourceauthor:[AWS]
+# snippet-start:[pinpoint.python.pinpoint_send_email_smtp.complete]
+
+import smtplib
+import email.utils
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# If you're using Amazon Pinpoint in a region other than US West (Oregon),
+# replace email-smtp.us-west-2.amazonaws.com with the Amazon Pinpoint SMTP
+# endpoint in the appropriate AWS Region.
+HOST = "email-smtp.us-west-2.amazonaws.com"
+
+# The port to use when connecting to the SMTP server.
+PORT = 587
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified.
+SENDER = 'Mary Major <sender@example.com>'
+
+# Replace recipient@example.com with a "To" address. If your account
+# is still in the sandbox, this address has to be verified.
+RECIPIENT  = 'recipient@example.com'
+
+# CC and BCC addresses. If your account is in the sandbox, these
+# addresses have to be verified.
+CCRECIPIENT = "cc_recipient@example.com"
+BCCRECIPIENT = "bcc_recipient@example.com"
+
+# Replace smtp_username with your Amazon Pinpoint SMTP user name.
+USERNAME_SMTP = "AKIAIOSFODNN7EXAMPLE"
+
+# Replace smtp_password with your Amazon Pinpoint SMTP password.
+PASSWORD_SMTP = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# (Optional) the name of a configuration set to use for this message.
+# If you comment out this line, you also need to remove or comment out
+# the "X-Pinpoint-CONFIGURATION-SET:" header below.
+CONFIGURATION_SET = "ConfigSet"
+
+# The subject line of the email.
+SUBJECT = 'Amazon Pinpoint Test (Python smtplib)'
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("Amazon Pinpoint Test\r\n"
+             "This email was sent through the Amazon Pinpoint SMTP "
+             "Interface using the Python smtplib package."
+            )
+
+# Create a MIME part for the text body.
+textPart = MIMEText(BODY_TEXT, 'plain')
+
+# The HTML body of the email.
+BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Amazon Pinpoint SMTP Email Test</h1>
+  <p>This email was sent with Amazon Pinpoint using the
+    <a href='https://www.python.org/'>Python</a>
+    <a href='https://docs.python.org/3/library/smtplib.html'>
+    smtplib</a> library.</p>
+</body>
+</html>
+            """
+
+# Create a MIME part for the HTML body.
+htmlPart = MIMEText(BODY_HTML, 'html')
+
+# The message tags that you want to apply to the email.
+TAG0 = "key0=value0"
+TAG1 = "key1=value1"
+
+# Create message container. The correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+
+# Add sender and recipient addresses to the message
+msg['From'] = SENDER
+msg['To'] = RECIPIENT
+msg['Cc'] = CCRECIPIENT
+msg['Bcc'] = BCCRECIPIENT
+
+# Add the subject line, text body, and HTML body to the message.
+msg['Subject'] = SUBJECT
+msg.attach(textPart)
+msg.attach(htmlPart)
+
+# Add  headers for configuration set and message tags to the message.
+msg.add_header('X-SES-CONFIGURATION-SET',CONFIGURATION_SET)
+msg.add_header('X-SES-MESSAGE-TAGS',TAG0)
+msg.add_header('X-SES-MESSAGE-TAGS',TAG1)
+
+# Open a new connection to the SMTP server and begin the SMTP conversation.
+try:
+    with smtplib.SMTP(HOST, PORT) as server:
+        server.ehlo()
+        server.starttls()
+        #stmplib docs recommend calling ehlo() before and after starttls()
+        server.ehlo()
+        server.login(USERNAME_SMTP, PASSWORD_SMTP)
+        #Uncomment the next line to send SMTP server responses to stdout
+        #server.set_debuglevel(1)
+        server.sendmail(SENDER, RECIPIENT, msg.as_string())
+        server.quit()
+except Exception as e:
+    print ("Error: ", e)
+else:
+    print ("Email sent!")
+
+# snippet-end:[pinpoint.python.pinpoint_send_email_smtp.complete]
