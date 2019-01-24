e4fe7b3094c2440eeceb5fd7f7c9a0134e3253a7
Paul Bartos <pbart@amazon.com>
2019-01-24 01:13:48 +0000

    Updates for moving code examples to the Code Example Catalog.

--- javascript/example_code/sns/sns_publishtotopic.js
+++ javascript/example_code/sns/sns_publishtotopic.js
@@ -1,34 +1,45 @@
- 
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
 //snippet-sourcedescription:[sns_publishtotopic.js demonstrates how to send a message to an Amazon SNS topic.]
+//snippet-service:[sns]
 //snippet-keyword:[JavaScript]
 //snippet-keyword:[Code Sample]
 //snippet-keyword:[Amazon Simple Notification Service]
-//snippet-service:[sns]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2018-06-02]
-//snippet-sourceauthor:[daviddeyo]
-
-
-// Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// Licensed under the Apache-2.0 License on an "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND.   
+//snippet-sourceauthor:[AWS-JSDG]
 
 // ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
 // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sns-examples-publishing-messages.html 
+
+// snippet-start:[sns.JavaScript.topics.publishMessages]
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set region
-AWS.config.update({region: 'us-west-2'});
+AWS.config.update({region: 'REGION'});
 
 // Create publish parameters
 var params = {
-  Message: 'This is David sending another text message from programming code.', /* required */
-  TopicArn: 'TOPIC_ARN',
+  Message: 'MESSAGE_TEXT', /* required */
+  TopicArn: 'TOPIC_ARN'
 };
 
 // Create promise and SNS service object
 var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
 
-// handle promise's fulfilled/rejected states
+// Handle promise's fulfilled/rejected states
 publishTextPromise.then(
   function(data) {
     console.log("Message ${params.Message} send sent to the topic ${params.TopicArn}");
@@ -37,3 +48,4 @@ publishTextPromise.then(
     function(err) {
     console.error(err, err.stack);
   });
+// snippet-end:[sns.JavaScript.topics.publishMessages]
