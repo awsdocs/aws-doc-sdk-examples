11b9e1f0f16790a482405e20fcbf3ccd52f21394
Chris Rees <reesch@amazon.com>
2019-01-30 15:22:31 -0800

    added license info

--- java/example_code/rekognition/rekognition-video-java-detect-labels-lambda.java
+++ java/example_code/rekognition/rekognition-video-java-detect-labels-lambda.java
@@ -11,6 +11,21 @@
 // snippet-sourceauthor:[reesch(AWS)]
 // snippet-start:[rekognition.java.rekognition-video-java-detect-labels-lambda.complete]
 
+
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
 //Lambda function for detecting labels. For more information, see https://docs.aws.amazon.com/rekognition/latest/dg/stored-video-lambda.html. 
 package com.amazonaws.lambda.demo;
 
