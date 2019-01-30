8f86b331587dffde2ae0113101a38f17d81172a8
Doug <dougsch@amazon.com>
2019-01-30 05:14:51 -0800

    Update index.ts
    Fixed split source description snippet tag.

--- typescript/example_code/codepipeline/index.ts
+++ typescript/example_code/codepipeline/index.ts
@@ -10,8 +10,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-// snippet-sourcedescription:[index.ts provides a 
-// hello script in TypeScript]
+// snippet-sourcedescription:[index.ts provides a hello script in TypeScript]
 // snippet-service:[codepipeline]
 // snippet-keyword:[TypeScript]
 // snippet-keyword:[AWS CodePipeline]
