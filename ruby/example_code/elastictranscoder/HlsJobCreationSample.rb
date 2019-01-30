1688e6db44ad1c46daa8cadbca4af5fa932d20d0
Doug <dougsch@amazon.com>
2019-01-30 04:47:44 -0800

    Update HlsJobCreationSample.rb
    Use Elastic Transcoder AWS module instead of all-encompassing AWS module; uses OpenSSL instead of Digest; fixed some non-idiomatic Ruby.

--- ruby/example_code/elastictranscoder/HlsJobCreationSample.rb
+++ ruby/example_code/elastictranscoder/HlsJobCreationSample.rb
@@ -19,8 +19,8 @@
 # OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 # snippet-start:[elastictranscoder.ruby.create_hls_job.import]
-require 'aws-sdk'
-require 'Digest'
+require 'aws-sdk-elastictranscoder'
+require 'openssl'
 
 # This is the ID of the Elastic Transcoder pipeline that was created when
 # setting up your AWS environment:
@@ -34,27 +34,27 @@ input_key = 'Enter your input key here.'
 region = 'us-east-1'
 
 # HLS Presets that will be used to create an adaptive bitrate playlist.
-hls_64k_audio_preset_id = '1351620000001-200071';
-hls_0400k_preset_id     = '1351620000001-200050';
-hls_0600k_preset_id     = '1351620000001-200040';
-hls_1000k_preset_id     = '1351620000001-200030';
-hls_1500k_preset_id     = '1351620000001-200020';
-hls_2000k_preset_id     = '1351620000001-200010';
+hls_64k_audio_preset_id = '1351620000001-200071'
+hls_0400k_preset_id     = '1351620000001-200050'
+hls_0600k_preset_id     = '1351620000001-200040'
+hls_1000k_preset_id     = '1351620000001-200030'
+hls_1500k_preset_id     = '1351620000001-200020'
+hls_2000k_preset_id     = '1351620000001-200010'
 
 # HLS Segment duration that will be targeted.
 segment_duration = '2'
 
-#All outputs will have this prefix prepended to their output key.
+# All outputs will have this prefix prepended to their output key.
 output_key_prefix = 'elastic-transcoder-samples/output/hls/'
 
 # Create the client for Elastic Transcoder.
-transcoder_client = AWS::ElasticTranscoder::Client.new(region: region)
+transcoder_client = Aws::ElasticTranscoder::Client.new(region: region)
 
 # Setup the job input using the provided input key.
 input = { key: input_key }
 
-#Setup the job outputs using the HLS presets.
-output_key = Digest::SHA256.hexdigest(input_key.encode('UTF-8'))
+# Setup the job outputs using the HLS presets.
+output_key = OpenSSL::Digest::SHA256.new(input_key.encode('UTF-8'))
 
 hls_audio = {
   key: 'hlsAudio/' + output_key,
@@ -92,7 +92,7 @@ hls_2000k = {
   segment_duration: segment_duration
 }
 
-outputs = [ hls_audio, hls_400k, hls_600k, hls_1000k, hls_1500k, hls_2000k ]
+outputs = [hls_audio, hls_400k, hls_600k, hls_1000k, hls_1500k, hls_2000k]
 playlist = {
   name: 'hls_' + output_key,
   format: 'HLSv3',
@@ -104,7 +104,8 @@ job = transcoder_client.create_job(
   input: input,
   output_key_prefix: output_key_prefix + output_key + '/',
   outputs: outputs,
-  playlists: [ playlist ])[:job]
+  playlists: [playlist]
+)[:job]
 
 puts 'HLS job has been created: ' + JSON.pretty_generate(job)
 # snippet-end:[elastictranscoder.ruby.create_hls_job.import]
