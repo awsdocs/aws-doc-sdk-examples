5462c280bb8ca540b405382cb8809afc50453154
Pat Fletcher <fletpatr@amazon.com>
2019-01-30 22:24:03 +0000

    Add PHP S3 code samples

---
+++ php/example_code/s3/s3-listing-object-keys.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/s3-examples-creating-buckets.html
+ *
+ */
+ 
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourcedescription:[s3-listing-object-keys.php demonstrates how to list the keys of objects in an Amazon S3 bucket.]
+// snippet-keyword:[PHP]
+// snippet-keyword:[AWS SDK for PHP v3]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[Amazon S3]
+// snippet-keyword:[getPaginator]
+// snippet-keyword:[listObjects]
+// snippet-service:[s3]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[2019-01-30]
+// snippet-sourceauthor:[fletpatr (AWS)]
+// snippet-start:[s3.php.example.listingobjectkeys]
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucket = '*** Your Bucket Name ***';
+
+// Instantiate the client.
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+
+// Use the high-level iterators (returns ALL of your objects).
+try {
+    $results = $s3->getPaginator('ListObjects', [
+        'Bucket' => $bucket
+    ]);
+
+    foreach ($results as $result) {
+        foreach ($result['Contents'] as $object) {
+            echo $object['Key'] . PHP_EOL;
+        }
+    }
+} catch (S3Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
+// Use the plain API (returns ONLY up to 1000 of your objects).
+try {
+    $objects = $s3->listObjects([
+        'Bucket' => $bucket
+    ]);
+    foreach ($objects['Contents']  as $object) {
+        echo $object['Key'] . PHP_EOL;
+    }
+} catch (S3Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+// snippet-end:[s3.php.example.listingobjectkeys]
