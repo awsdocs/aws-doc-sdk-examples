3a6e2e7430aafbde19670bb74dd26629b2aedb92
Doug <dougsch@amazon.com>
2019-01-30 07:24:51 -0800

    Update sts_assume_role.go
    Added snippet tags.

--- go/example_code/sts/sts_assume_role.go
+++ go/example_code/sts/sts_assume_role.go
@@ -1,17 +1,25 @@
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourceauthor:[AWS]
+// snippet-sourcedescription:[sts_assume_role.go creates a set of temporary security credentials.]
+// snippet-keyword:[Amazon Security Token Service]
+// snippet-keyword:[Amazon STS]
+// snippet-keyword:[AssumeRole function]
+// snippet-keyword:[Go]
+// snippet-service:[s3]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[2019-01-30]
 /*
    Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
    This file is licensed under the Apache License, Version 2.0 (the "License").
    You may not use this file except in compliance with the License. A copy of
    the License is located at
-
     http://aws.amazon.com/apache2.0/
-
    This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the License.
 */
-
+// snippet-start:[sts.go.assume_role]
 package main
 
 import (
@@ -53,3 +61,4 @@ func main() {
 
 	fmt.Println(result.AssumedRoleUser)
 }
+// snippet-end:[sts.go.assume_role]
