1a0d827940732d76108af26056cf284a8ec91b3c
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-01-30 22:00:22 -0800

    adding examples for Command Pool, Middleware, and MockHandler

---
+++ php/example_code/s3/GetCommand.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/s3-examples-creating-buckets.html
+ *
+ */
+// snippet-start:[s3.php.command_pool_get_command.complete]
+// snippet-start:[s3.php.command_pool_get_command.import]
+use Aws\S3\S3Client;
+use Aws\CommandPool;
+// snippet-end:[s3.php.command_pool_get_command..import]
+/**
+ * Use Command Pool to upload a file to an Amazon S3 bucket.
+ *
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ */
+// Create the client
+// snippet-start:[s3.php.command_pool_get_command.main] 
+$client = new S3Client([
+    'region'  => 'us-standard',
+    'version' => '2006-03-01'
+]);
+
+$bucket = 'example';
+$commands = [
+    $client->getCommand('HeadObject', ['Bucket' => $bucket, 'Key' => 'a']),
+    $client->getCommand('HeadObject', ['Bucket' => $bucket, 'Key' => 'b']),
+    $client->getCommand('HeadObject', ['Bucket' => $bucket, 'Key' => 'c'])
+];
+
+$pool = new CommandPool($client, $commands);
+
+// Initiate the pool transfers
+$promise = $pool->promise();
+
+// Force the pool to complete synchronously
+$promise->wait();
+// snippet-end:[s3.php.command_pool_get_command.main]
+// snippet-end:[s3.php.command_pool_get_command.complete]
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourcedescription:[GetCommand.php demonstrates how to send a few comands using a Command Pool.]
+// snippet-keyword:[PHP]
+// snippet-keyword:[AWS SDK for PHP v3]
+// snippet-keyword:[Code Sample]
+// snippet-keyword:[Amazon S3]
+// snippet-keyword:[CommandPool]
+// snippet-service:[s3]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[]
+// snippet-sourceauthor:[jschwarzwalder (AWS)]
