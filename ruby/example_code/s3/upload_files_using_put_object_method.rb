64d8ab4971ac8a640b0a686815c759679344e03d
Doug <dougsch@amazon.com>
2019-02-11 08:13:06 -0800

    Update upload_files_using_put_object_method.rb

--- ruby/example_code/s3/upload_files_using_put_object_method.rb
+++ ruby/example_code/s3/upload_files_using_put_object_method.rb
@@ -11,8 +11,6 @@
  #* CONDITIONS OF ANY KIND, either express or implied. See the License for the
  #* specific language governing permissions and limitations under the License.
 #**
-
-
 # snippet-sourcedescription:[upload_files_using_put_object_method.rb shows how to upload an object using the #put method of Amazon S3 object. This is useful if the object is a string or an I/O object that is not a file on disk.] 
 # snippet-service:[s3]
 # snippet-keyword:[Ruby]
@@ -20,20 +18,16 @@
 # snippet-keyword:[Code Sample]
 # snippet-keyword:[PUT Object]
 # snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2019-01-28]
+# snippet-sourcedate:[2019-02-11]
 # snippet-sourceauthor:[AWS]
-
 # snippet-start:[s3.ruby.upload_files_using_put_object_method.rb]
-  require 'aws-sdk-s3'
+require 'aws-sdk-s3'
 
-  s3 = Aws::S3::Resource.new(region:'us-west-2')
-  obj = s3.bucket('bucket-name').object('key')
-
-# string data
-  obj.put(body: 'Hello World!')
+s3 = Aws::S3::Resource.new(region:'us-west-2')
+obj = s3.bucket('bucket-name').object('key')
 
 # I/O object
-  File.open('/path/to/source.file', 'rb') do |file|
+File.open('/path/to/source.file', 'rb') do |file|
   obj.put(body: file)
-  end
+end
 # snippet-end:[s3.ruby.upload_files_using_put_object_method.rb]
