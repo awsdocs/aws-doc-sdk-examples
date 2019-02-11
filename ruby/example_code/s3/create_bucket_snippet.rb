86a4165e79d1e42562b9b98bf23cac012df77d6c
Doug <dougsch@amazon.com>
2019-02-11 07:26:21 -0800

    Update create_bucket_snippet.rb

--- ruby/example_code/s3/create_bucket_snippet.rb
+++ ruby/example_code/s3/create_bucket_snippet.rb
@@ -11,7 +11,6 @@
  #* CONDITIONS OF ANY KIND, either express or implied. See the License for the
  #* specific language governing permissions and lim2019itations under the License.
 #**
-
 # snippet-sourcedescription:[create_bucket_snippet creates a bucket programmatically by using the AWS SDK for Ruby.] 
 # snippet-service:[s3]
 # snippet-keyword:[Ruby]
@@ -21,9 +20,7 @@
 # snippet-sourcetype:[snippet]
 # snippet-sourcedate:[2019-01-28]
 # snippet-sourceauthor:[AWS]
-
 # snippet-start:[s3.ruby.create_bucket_snippet.rb]
-
 require 'aws-sdk-s3'
 	        
 s3 = Aws::S3::Client.new(region: 'us-west-2')
