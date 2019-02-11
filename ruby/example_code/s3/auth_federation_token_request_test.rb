a4731547fe59a54dba193698a85eb159dd466ef1
Doug <dougsch@amazon.com>
2019-02-11 06:30:37 -0800

    Update auth_federation_token_request_test.rb

--- ruby/example_code/s3/auth_federation_token_request_test.rb
+++ ruby/example_code/s3/auth_federation_token_request_test.rb
@@ -11,8 +11,6 @@
  #* CONDITIONS OF ANY KIND, either express or implied. See the License for the
  #* specific language governing permissions and limitations under the License.
 #**
-
-
 # snippet-sourcedescription:[auth_federation_token_request_test.rb allows a federated user with a limited set of permissions to lists keys in the specified bucket.] 
 # snippet-service:[s3]
 # snippet-keyword:[Ruby]
@@ -20,17 +18,15 @@
 # snippet-keyword:[Code Sample]
 # snippet-keyword:[GET Bucket, GET Object, GET Keys]
 # snippet-sourcetype:[full-example]
-# snippet-sourcedate:[YYYY-MM-DD]
+# snippet-sourcedate:[2019-02-11]
 # snippet-sourceauthor:[AWS]
-
 # snippet-start:[s3.ruby.auth_federation_token_request_test.rb]
-
 require 'aws-sdk-s3'
 require 'aws-sdk-iam'
 
-USAGE = << DOC
+USAGE = <<DOC
 
-Usage: federated_create_bucket_policy.rb -b BUCKET -u USER [-r REGION] [-d] [-h]
+Usage: ruby auth_federation_token_request_test.rb -b BUCKET -u USER [-r REGION] [-d] [-h]
 
   Creates a federated policy for USER to list items in BUCKET for one hour.
 
@@ -46,35 +42,40 @@ Usage: federated_create_bucket_policy.rb -b BUCKET -u USER [-r REGION] [-d] [-h]
 
 DOC
 
-$debug = false
-
-def print_debug(s)
-  if $debug
+def print_debug(debug, s)
+  if debug
     puts s
   end
 end
 
-def get_user(region, user_name, create)
-  user = nil
+# Get the user if they exist, otherwise create them
+def get_user(region, user_name, debug)
   iam = Aws::IAM::Client.new(region: 'us-west-2')
   
-begin
-  user = iam.create_user(user_name: user_name)
-  iam.wait_until(:user_exists, user_name: user_name)
-  print_debug("Created new user #{user_name}")
-rescue Aws::IAM::Errors::EntityAlreadyExists
-  print_debug("Found user #{user_name} in region #{region}")
-end
+  # See if user exists
+  user = iam.user(user_name)
+ 
+  # If user does not exist, create them
+  if user == nil
+    user = iam.create_user(user_name: user_name)
+    iam.wait_until(:user_exists, user_name: user_name)
+    print_debug(debug, "Created new user #{user_name}")
+  else
+    print_debug(debug, "Found user #{user_name} in region #{region}")
+  end
+ 
+ user
 end
 
 # main
 region = 'us-west-2'
 user_name = ''
 bucket_name = ''
+debug = false
 
 i = 0
 
-while i &lt; ARGV.length
+while i < ARGV.length
   case ARGV[i]
 
     when '-b'
@@ -92,7 +93,7 @@ while i &lt; ARGV.length
 
     when '-d'
       puts 'Debugging enabled'
-      $debug = true
+      debug = true
 
     when '-h'
       puts USAGE
@@ -102,7 +103,7 @@ while i &lt; ARGV.length
       puts 'Unrecognized option: ' + ARGV[i]
       puts USAGE
       exit 1
-
+   
   end
 
   i += 1
@@ -120,8 +121,8 @@ if user_name == ''
   exit 1
 end
 
-#Identify the IAM user we allow to list Amazon S3 bucket items for an hour.
-user = get_user(region, user_name, true)
+# Identify the IAM user we allow to list Amazon S3 bucket items for an hour.
+user = get_user(region, user_name, debug)
 
 # Create a new STS client and get temporary credentials.
 sts = Aws::STS::Client.new(region: region)
@@ -138,7 +139,12 @@ s3 = Aws::S3::Resource.new(region: region, credentials: creds)
 puts "Contents of '%s':" % bucket_name
 puts '  Name => GUID'
 
- s3.bucket(bucket_name).objects.limit(50).each do |obj|
-      puts "  #{obj.key} => #{obj.etag}"
+begin
+  s3.bucket(bucket_name).objects.limit(50).each do |obj|
+    puts "  #{obj.key} => #{obj.etag}"
+  end
+rescue Exception => ex
+  puts 'Caught exception accessing bucket ' +  bucket_name + ':'
+  puts ex.message
 end
 # snippet-end:[s3.ruby.auth_federation_token_request_test.rb]
