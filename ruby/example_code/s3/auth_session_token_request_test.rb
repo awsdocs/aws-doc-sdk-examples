311e1f6b5e6760a421350af045d3ee85deabcc8d
Doug <dougsch@amazon.com>
2019-02-11 07:06:27 -0800

    Update auth_session_token_request_test.rb

--- ruby/example_code/s3/auth_session_token_request_test.rb
+++ ruby/example_code/s3/auth_session_token_request_test.rb
@@ -11,8 +11,6 @@
  #* CONDITIONS OF ANY KIND, either express or implied. See the License for the
  #* specific language governing permissions and limitations under the License.
 #**
-
-
 # snippet-sourcedescription:[auth_session_token_request_test.rb creates a temporary user to list the items in a specified bucket for one hour. To use this example, you must have AWS credentials that have the necessary permissions to create new AWS Security Token Service (AWS STS) clients, and list Amaazon S3 buckets using temporary security credentials] 
 # snippet-service:[s3]
 # snippet-keyword:[Ruby]
@@ -20,11 +18,9 @@
 # snippet-keyword:[Code Sample]
 # snippet-keyword:[PUT Bucket]
 # snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2019-01-28]
+# snippet-sourcedate:[2019-02-11]
 # snippet-sourceauthor:[AWS]
-
 # snippet-start:[s3.ruby.auth_session_token_request_test.rb]
-
 # This snippet example does the following:
 # The following Ruby example creates a temporary user to list the items in a specified bucket
 # for one hour. To use this example, you must have AWS credentials that have the necessary
@@ -35,7 +31,6 @@
 require 'aws-sdk-core'
 require 'aws-sdk-s3'
 require 'aws-sdk-iam'
-
 
 USAGE = <<DOC
 
@@ -55,31 +50,36 @@ Usage: assumerole_create_bucket_policy.rb -b BUCKET -u USER [-r REGION] [-d] [-h
 
 DOC
 
-$debug = false
-
-def print_debug(s)
-  if $debug
+def print_debug(debug, s)
+  if debug
     puts s
   end
 end
 
-def get_user(region, user_name, create)
-  user = nil
-  iam = Aws::IAM::Client.new(region: 'us-west-2')
-  
-begin
-  user = iam.create_user(user_name: user_name)
-  iam.wait_until(:user_exists, user_name: user_name)
-  print_debug("Created new user #{user_name}")
-rescue Aws::IAM::Errors::EntityAlreadyExists
-  print_debug("Found user #{user_name} in region #{region}")
-end
+# Get the user if they exist, otherwise create them
+def get_user(region, user_name, debug)
+  iam = Aws::IAM::Resource.new(region: region)
+
+  # See if user exists
+  user = iam.user(user_name)
+
+  # If user does not exist, create them
+  if user == nil
+    user = iam.create_user(user_name: user_name)
+    iam.wait_until(:user_exists, user_name: user_name)
+    print_debug(debug, "Created new user #{user_name}")
+  else
+    print_debug(debug, "Found user #{user_name} in region #{region}")
+  end
+
+  user
 end
 
 # main
 region = 'us-west-2'
 user_name = ''
 bucket_name = ''
+debug = false
 
 i = 0
 
@@ -101,7 +101,7 @@ while i &lt; ARGV.length
 
     when '-d'
       puts 'Debugging enabled'
-      $debug = true
+      debug = true
 
     when '-h'
       puts USAGE
@@ -130,22 +130,27 @@ if user_name == ''
 end
 
 #Identify the IAM user that is allowed to list Amazon S3 bucket items for an hour.
-user = get_user(region, user_name, true)
+user = get_user(region, user_name, debug)
 
 # Create a new Amazon STS client and get temporary credentials. This uses a role that was already created.
-creds = Aws::AssumeRoleCredentials.new(
-  client: Aws::STS::Client.new(region: region),
-  role_arn: "arn:aws:iam::111122223333:role/assumedrolelist",
-  role_session_name: "assumerole-s3-list"
-)
+begin
+  creds = Aws::AssumeRoleCredentials.new(
+    client: Aws::STS::Client.new(region: region),
+    role_arn: "arn:aws:iam::111122223333:role/assumedrolelist",
+    role_session_name: "assumerole-s3-list"
+  )
 
-# Create an Amazon S3 resource with temporary credentials.
-s3 = Aws::S3::Resource.new(region: region, credentials: creds)
+  # Create an Amazon S3 resource with temporary credentials.
+  s3 = Aws::S3::Resource.new(region: region, credentials: creds)
 
-puts "Contents of '%s':" % bucket_name
-puts '  Name => GUID'
+  puts "Contents of '%s':" % bucket_name
+  puts '  Name => GUID'
 
- s3.bucket(bucket_name).objects.limit(50).each do |obj|
-      puts "  #{obj.key} => #{obj.etag}"
+  s3.bucket(bucket_name).objects.limit(50).each do |obj|
+    puts "  #{obj.key} => #{obj.etag}"
+  end
+ rescue Exception => ex
+  puts 'Caught exception accessing bucket ' +  bucket_name + ':'
+  puts ex.message
 end
 # snippet-end:[s3.ruby.auth_session_token_request_test.rb]
