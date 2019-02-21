cf807d22e5d77d53080dca8046374ac4bd467a93
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-02-20 21:09:33 -0800

    adding snippets for cloudwatch java2 article: examples-cloudwatch-publish-custom-metrics.rst

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutMetricData.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutMetricData.java
@@ -19,6 +19,8 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+// snippet-start:[cloudwatch.java.put_metric_data.complete]
+// snippet-start:[cloudwatch.java.put_metric_data.import]
 package com.example.cloudwatch;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
 import software.amazon.awssdk.services.cloudwatch.model.Dimension;
@@ -27,6 +29,8 @@ import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
 import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataResponse;
 import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
 
+// snippet-end:[cloudwatch.java.put_metric_data.import]
+// snippet-start:[cloudwatch.java.put_metric_data.main]
 /**
  * Puts a sample metric data point
  */
@@ -66,3 +70,5 @@ public class PutMetricData {
         System.out.printf("Successfully put data point %f", data_point);
     }
 }
+// snippet-end:[cloudwatch.java.put_metric_data.main]
+// snippet-end:[cloudwatch.java.put_metric_data.complete]
