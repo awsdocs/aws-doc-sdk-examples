0752b3c458baa08fcfa34dcd3c8eb63fc4f7edc7
Doug-AWS <dougsch@amazon.com>
2019-04-01 07:38:33 -0700

    Added initial Python code for CDK

---
+++ python/example_code/cdk/app.py
@@ -0,0 +1,19 @@
+from aws_cdk import (
+    aws_s3 as s3,
+    cdk,
+)
+
+
+class S3Stack(cdk.Stack):
+    def __init__(self, app: cdk.App, id: str) -> None:
+        super().__init__(app, id)
+
+        bucket = aws_s3.Bucket(
+            self,
+            "MyBucket",
+            versioned=True)
+
+
+app = cdk.App()
+S3Stack(app, "MyStack")
+app.run()
