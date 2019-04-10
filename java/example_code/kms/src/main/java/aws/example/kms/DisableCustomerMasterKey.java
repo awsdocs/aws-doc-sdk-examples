122c1cc0d2b64f8333319f7cc22e74177c704daa
Joe Wright <wrjoe@amazon.com>
2019-04-10 16:14:44 -0700

    Update KMS code samples for the Java AWS SDK v1
    Updates KMS code samples to take parameters as program arguments, print
    output, and paginate over list operations.

--- java/example_code/kms/src/main/java/aws/example/kms/DisableCustomerMasterKey.java
+++ java/example_code/kms/src/main/java/aws/example/kms/DisableCustomerMasterKey.java
@@ -15,11 +15,21 @@ import com.amazonaws.services.kms.model.DisableKeyRequest;
 
 public class DisableCustomerMasterKey {
     public static void main(String[] args) {
+        final String USAGE =
+            "To run this example, supply a key id or ARN\n" +
+            "Usage: DisableCustomerMasterKey <key-id>\n" +
+            "Example: DisableCustomerMasterKey 1234abcd-12ab-34cd-56ef-1234567890ab\n";
+
+        if (args.length != 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String keyId = args[0];
+
         AWSKMS kmsClient = AWSKMSClientBuilder.standard().build();
+
         // Disable a CMK
-        //
-        // Replace the following fictitious CMK ARN with a valid CMK ID or ARN
-        String keyId = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab";
 
         DisableKeyRequest req = new DisableKeyRequest().withKeyId(keyId);
         kmsClient.disableKey(req);
--- java/example_code/kms/src/main/java/aws/example/kms/ViewGrants.java
+++ java/example_code/kms/src/main/java/aws/example/kms/ViewGrants.java
@@ -11,23 +11,41 @@ package aws.example.kms;
 
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.GrantListEntry;
 import com.amazonaws.services.kms.model.ListGrantsRequest;
 import com.amazonaws.services.kms.model.ListGrantsResult;
 
 public class ViewGrants {
     public static void main(String[] args) {
+        final String USAGE =
+            "To run this example, supply a key id or ARN\n" +
+            "Usage: ViewGrants <key-id>\n" +
+            "Example: ViewGrants 1234abcd-12ab-34cd-56ef-1234567890ab\n";
+
+        if (args.length != 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String keyId = args[0];
+
         AWSKMS kmsClient = AWSKMSClientBuilder.standard().build();
 
         // Listing grants on a CMK
-        //
-        // Replace the following fictitious CMK ARN with a valid CMK ID or ARN
-        String keyId = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab";
-        Integer limit = 10;
+        String nextMarker = null;
+        do {
+            ListGrantsRequest request = new ListGrantsRequest()
+                    .withKeyId(keyId)
+                    .withMarker(nextMarker);
 
-        ListGrantsRequest request = new ListGrantsRequest()
-                .withKeyId(keyId)
-                .withLimit(limit);
+            ListGrantsResult result = kmsClient.listGrants(request);
+            for (GrantListEntry grant : result.getGrants()) {
+                System.out.printf("Found grant \"%s\" with grantee principal " +
+                "%s, operations %s, and constraints %s.%n", grant.getGrantId(),
+                grant.getGranteePrincipal(), grant.getOperations(), grant.getConstraints());
+            }
+            nextMarker = result.getNextMarker();
+        } while (nextMarker != null);
 
-        ListGrantsResult result = kmsClient.listGrants(request);
     }
 }
