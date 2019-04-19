7b7b569138011f3b7629ac9741e49935e6a7abb1
rajib76@yahoo.com <rajib76@yahoo.com>
2019-04-18 22:54:25 -0700

    added copyright message

--- java/example_code/cloudmap/src/main/java/aws/example/cloudmap/DiscoverInstanceRequest.java
+++ java/example_code/cloudmap/src/main/java/aws/example/cloudmap/DiscoverInstanceRequest.java
@@ -1,6 +1,28 @@
 /*
  * Author : Rajib Deb
  */
+// Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// This file is licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License. A copy of
+// the License is located at
+//
+// http://aws.amazon.com/apache2.0/
+//
+// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+// snippet-sourcedescription:[DiscoverInstanceRequest.java demonstrates how to ....]
+// snippet-service:[cloudmap]
+// snippet-keyword:[java]
+// snippet-keyword:[AWS Cloud Map]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[2019-04-18]
+// snippet-sourceauthor:[rajib76]
+// snippet-start:[cloudmap.java.discover_instance_request.complete]
+
 package aws.example.cloudmap;
 
 import com.amazonaws.AmazonClientException;
@@ -36,3 +58,4 @@ public class DiscoverInstanceRequest {
 	}
 
 }
+//snippet-end:[cloudmap.java.discover_instance_request.complete]
