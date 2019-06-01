51af48c77c6f7a56f8e7713ac838eab9cf445c8f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-06-01 15:09:52 -0700

    switching snippetids to java2

--- javav2/example_code/athena/src/main/java/aws/example/athena/ExampleConstants.java
+++ javav2/example_code/athena/src/main/java/aws/example/athena/ExampleConstants.java
@@ -5,7 +5,21 @@
 //snippet-service:[athena]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2019-04-15]
-//snippet-sourceauthor:[jschwarzwalder]
+//snippet-sourceauthor:[jschwarzwalder AWS]
+/*
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 //snippet-start:[athena.java2.ExampleConstants.complete]
 package aws.example.athena;
 
