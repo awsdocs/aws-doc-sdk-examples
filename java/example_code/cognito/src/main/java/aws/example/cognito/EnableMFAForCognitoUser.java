b59a9a2f4229012fe42311ffda815e688012ed28
Wendler Zacariotto <wenzaca@gmail.com>
2019-06-20 08:40:39 +0100

    Adding metadata to the sample class

--- java/example_code/cognito/src/main/java/aws/example/cognito/EnableMFAForCognitoUser.java
+++ java/example_code/cognito/src/main/java/aws/example/cognito/EnableMFAForCognitoUser.java
@@ -1,3 +1,26 @@
+//snippet-sourcedescription:[Filename.java demonstrates how to enable MFA in Cognito Userpool which MFA is optional to users]
+//snippet-keyword:[Java]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[Amazon Cognito]
+//snippet-service:[cognito]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[2019-06-20]
+//snippet-sourceauthor:[wenzaca]
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). 
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+* http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+ */
+//snippet-start:[cognito.java.mfa.complete]
 package aws.example.cognito;
 
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
