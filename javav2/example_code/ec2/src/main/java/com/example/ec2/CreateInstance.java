67e6a0fc1cec179d4f834037e6b5b3e578d8a8c8
anthonybgale <anthonybgale@hotmail.com>
2019-06-21 20:37:58 +0000

    Correct assignment of instance_id
    EC2 Instance ID is available in RunInstancesResponse.instances().get(0).instanceId();

--- javav2/example_code/ec2/src/main/java/com/example/ec2/CreateInstance.java
+++ javav2/example_code/ec2/src/main/java/com/example/ec2/CreateInstance.java
@@ -63,7 +63,7 @@ public class CreateInstance
 
         RunInstancesResponse response = ec2.runInstances(run_request);
 
-        String instance_id = response.reservationId();
+        String instance_id = response.instances().get(0).instanceId();
 
         Tag tag = Tag.builder()
             .key("Name")
