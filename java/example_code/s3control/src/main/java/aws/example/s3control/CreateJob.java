426aa4f150b8dd6f823733b20ac04ea165c7a80a
panjws <47164335+panjws@users.noreply.github.com>
2019-08-21 17:05:04 -0700

    Updating variable "reportBucketName". Using just the what's there right now returns a HTTP 400 Error

--- java/example_code/s3control/src/main/java/aws/example/s3control/CreateJob.java
+++ java/example_code/s3control/src/main/java/aws/example/s3control/CreateJob.java
@@ -46,7 +46,7 @@ public class CreateJob {
         // snippet-start:[s3.java.create_batch_job.main]
         String accountId = "Account ID";
         String iamRoleArn = "IAM Role ARN";
-        String reportBucketName = "bucket-where-completion-report-goes";
+        String reportBucketName = "arn:aws:s3:::bucket-where-completion-report-goes";
         String uuid = UUID.randomUUID().toString();
 
         ArrayList tagSet = new ArrayList<S3Tag>();
