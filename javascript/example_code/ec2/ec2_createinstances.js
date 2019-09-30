f4a1156121838d50e6a2a55f5629b0f9af519ee8
Doug-AWS <dougsch@amazon.com>
2019-09-30 13:35:01 -0700

    Updated EC2 examples to use AWS.config.update

--- javascript/example_code/ec2/ec2_createinstances.js
+++ javascript/example_code/ec2/ec2_createinstances.js
@@ -29,7 +29,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Load credentials and set region from JSON file
-AWS.config.loadFromPath('./config.json');
+AWS.config.update({region: 'REGION'});
 
 // Create EC2 service object
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
