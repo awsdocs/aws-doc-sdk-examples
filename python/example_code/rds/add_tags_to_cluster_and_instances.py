83df4e77bf90c3b3f9099e4f47e9dc0da1202d61
Darko Martinovikj <martdark@amazon.com>
2019-10-04 18:47:47 -0700

    added script argumnets

--- python/example_code/rds/add_tags_to_cluster_and_instances.py
+++ python/example_code/rds/add_tags_to_cluster_and_instances.py
@@ -22,16 +22,18 @@
 # snippet-start:[rds.python.add_tags_to_cluster_and_instances.complete]
 
 import boto3
+import sys
+import json
 
-# Defined constants
-# Change to the appropriate region of your cluster
-regionName = 'us-east-1'
-# Specify the cluster identifier
-clusterIdentifier = 'cluster-name'
-# Specify your account number
-accountNumber = '123456789012'
-#Specify the list of tags that will be applied to cluster and the instances in it
-tags = [{'Key':'tag1', 'Value':'value1'}, {'Key':'tag2', 'Value':'value2'}]
+# Sample usage
+# python3 add_tags_to_cluster_and_instances.py regionName=us-east-1 clusterIdentifier=cluster-name accountNumber=123456789012 tags='[{"Key":"Tag1","Value":"Value1"},{"Key":"Tag2","Value":"Value2"}]'
+# 
+# Four arguments need to be supplied
+# regionName=Region
+# clusterIdentifier=Name_Of_Cluster
+# accountNumber=Customer_account_number
+# tags=Json_with_tags format [{"Key":"key","Value":"value"},...]
+
 
 def fetch_cluster_instances():
 	"""Fetches all of the instance Ids that are in a given cluster"""
@@ -59,7 +61,6 @@ def apply_tags_to_instances_and_cluster(instanceIdentifiers):
 	manually check and revert the applied tags to the instances and the cluster.
 
 	"""
-
 	rds = boto3.client('rds', region_name = regionName)
 	try:
 		clusterARN = generate_ARN_for_resource(clusterIdentifier, True)
@@ -78,16 +79,50 @@ def generate_ARN_for_resource(resourceId, isCluster):
 	resourceType = ":cluster:" if isCluster else ":db:"
 	return "arn:aws:rds:" + regionName + ":" + accountNumber + resourceType + resourceId
  
-def main():
+def applyArguments(argv):
+	""" Reads the supplied arguments and applies them """
+	settings = dict()
+	settings["regionName"] = ""
+	settings["clusterIdentifier"] = ""
+	settings["accountNumber"] = ""
+	settings["tags"] = ""
+	if (len(settings) != len(argv) - 1):
+		raise Exception("Incorrect number of argumnets supplied. Expected ", settings.keys())
+	for pos,arg in enumerate(argv):
+		if pos==0:
+			continue
+		a=arg.split('=')
+		if len(a) != 2:
+			raise Exception("Incorrect usage. Format argument=value ...")
+		if not a[0] in settings:
+			raise Exception("Argument  " + a[0] + " does not exist. Expected ", settings.keys())	
+		settings[a[0]] = a[1]	
+	for key, value in settings.items():
+		if value == "":
+			raise Exception("Missing value for argument " + key)
+	global regionName 
+	regionName = settings["regionName"]
+	global clusterIdentifier 
+	clusterIdentifier = settings["clusterIdentifier"]
+	global accountNumber
+	accountNumber = settings["accountNumber"]
+	global tags
+	try:
+		tags = json.loads(settings["tags"])
+	except Exception as e:
+		raise Exception("Cannot parse json supplied for tags ", e)
+
+def main(argv):
 	""" This script fetches all of the instances of the given cluster and applies the specified tags
 	to the cluster and its instances """
-
+	print("Checking arguments")
+	applyArguments(argv);
 	print("Applying tags to cluster and all of the instances in the cluster")
 	instanceIdentifiers = fetch_cluster_instances()
 	apply_tags_to_instances_and_cluster(instanceIdentifiers)
 	print("SUCCESS")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
 # snippet-end:[rds.python.add_tags_to_cluster_and_instances.complete]
