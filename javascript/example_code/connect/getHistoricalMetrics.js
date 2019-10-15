7d0695f1d4331634076e398c6a304d112958c5d2
Tim Pugh <nwtpugh@gmail.com>
2019-10-15 15:41:00 -0700

    Add all historical metrics

--- javascript/example_code/connect/getHistoricalMetrics.js
+++ javascript/example_code/connect/getHistoricalMetrics.js
@@ -2,23 +2,162 @@ const aws = require('aws-sdk');
 const connect = new aws.Connect();
 
 let params = {
-        InstanceId: "<provide your connect instance id>",
-        EndTime: 1570845600,  //change the end time
-        StartTime:1570780800, //change the start time
+        InstanceId: "1e8f045a-d7f1-4405-96c1-66701c25fd32",
+        EndTime: 1571184000,
+        StartTime:1571153400,
         Filters:{
-            Queues:["<provide your connect instances queue id>"],
+            Queues:["943031b0-62ce-49a7-8688-9e84accd94fc"],
             Channels:["VOICE"]
         },
-        HistoricalMetrics:[{
-            Name: "CONTACTS_ABANDONED", //add more metrics in this section. View https://docs.aws.amazon.com/connect/latest/APIReference/API_GetMetricData.html
-            Unit: "COUNT",
-            Statistic:"SUM"
-        },
-        {
-        Name: "CONTACTS_HOLD_ABANDONS",
-        Unit: "COUNT",
-        Statistic: "SUM"
-        }]
+        HistoricalMetrics : [
+      {
+         "Name" : "AFTER_CONTACT_WORK_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "CONTACTS_QUEUED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_HANDLED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "HANDLE_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "CONTACTS_TRANSFERRED_OUT",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_MISSED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "OCCUPANCY",
+         "Unit" : "PERCENT",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "QUEUED_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "MAX"
+      },
+      {
+         "Name" : "HOLD_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "SERVICE_LEVEL",
+         "Threshold" : {
+            "Comparison" : "LT",
+            "ThresholdValue" : 60.0
+         },
+         "Unit" : "PERCENT",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "SERVICE_LEVEL",
+         "Threshold" : {
+            "Comparison" : "LT",
+            "ThresholdValue" : 120.0
+         },
+         "Unit" : "PERCENT",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "SERVICE_LEVEL",
+         "Threshold" : {
+            "Comparison" : "LT",
+            "ThresholdValue" : 30.0
+         },
+         "Unit" : "PERCENT",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "CONTACTS_ABANDONED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_CONSULTED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_AGENT_HUNG_UP_FIRST",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_HANDLED_INCOMING",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_HANDLED_OUTBOUND",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_HOLD_ABANDONS",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_TRANSFERRED_IN",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_TRANSFERRED_IN_FROM_QUEUE",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CONTACTS_TRANSFERRED_OUT_FROM_QUEUE",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "CALLBACK_CONTACTS_HANDLED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "API_CONTACTS_HANDLED",
+         "Unit" : "COUNT",
+         "Statistic" : "SUM"
+      },
+      {
+         "Name" : "ABANDON_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "QUEUE_ANSWER_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "INTERACTION_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      },
+      {
+         "Name" : "INTERACTION_AND_HOLD_TIME",
+         "Unit" : "SECONDS",
+         "Statistic" : "AVG"
+      }
+   ]
     };
 
 
@@ -35,6 +174,4 @@ exports.handler = async (event) => {
     
     var metrics = result["MetricResults"]
         console.log(metrics[0].Collections);
-    
-    
 };
