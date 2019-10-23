db43932b9769afd2411f87c9764b4f48a63dd2e8
Copolycube <kaourintin+github@gmail.com>
2019-10-23 12:21:47 -0400

    Update count-ec2-types-current-region.py

--- python/example_code/ec2/count-ec2-types-current-region.py
+++ python/example_code/ec2/count-ec2-types-current-region.py
@@ -7,7 +7,7 @@
 # This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
 # OF ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License. 
-# snippet-start:[ec2.python.describe_instances.complete]
+# snippet-start:[ec2.python.count-ec2-types-current-region.complete]
 
 import boto3
 ec2 = boto3.client('ec2')
