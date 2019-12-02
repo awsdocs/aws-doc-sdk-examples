591ebcc3a3d9432dddc5a8f45326d1397b1d8e7c
scmacdon <57190223+scmacdon@users.noreply.github.com>
2019-12-02 11:48:10 -0500

    Update DeleteFunction.java

--- java/example_code/lambda/src/main/java/com/example/lambda/DeleteFunction.java
+++ java/example_code/lambda/src/main/java/com/example/lambda/DeleteFunction.java
@@ -14,7 +14,7 @@
  */
 
 // snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-sourcedescription:[LambdaInvoke.java demonstrates how to invoke an AWS Lambda function by using the AWSLambda object]
+// snippet-sourcedescription:[DeleteFunction.java demonstrates how to delete an AWS Lambda function by using the LambdaClient object]
 // snippet-service:[Lambda]
 // snippet-keyword:[Java]
 // snippet-keyword:[Amazon Lambda]
@@ -23,26 +23,37 @@
 // snippet-sourcedate:[2019-11-19]
 // snippet-sourceauthor:[AWS-scmacdon]
 
+// snippet-start:[lambda.java1.DeleteFunction.complete]
 package com.example.lambda;
 
+// snippet-start:[lambda.java1.delete.import]
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.model.ServiceException;
 import com.amazonaws.services.lambda.model.DeleteFunctionRequest;
+// snippet-end:[lambda.java1.delete.import]
+
 
 public class DeleteFunction {
 
     public static void main(String[] args) {
 
+        if (args.length < 1) {
+            System.out.println("Please specify a function name");
+            System.exit(1);
+        }
+
+        // snippet-start:[lambda.java1.delete.main]
+        String functionName = args[0];
         try {
         AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
                 .withCredentials(new ProfileCredentialsProvider())
                 .withRegion(Regions.US_WEST_2).build();
 
         DeleteFunctionRequest delFunc = new DeleteFunctionRequest();
-        delFunc.withFunctionName("somefunction");
+        delFunc.withFunctionName(functionName);
 
         //Delete the functiom
         awsLambda.deleteFunction(delFunc);
@@ -52,6 +63,7 @@ public class DeleteFunction {
     ServiceException e) {
         System.out.println(e);
     }
+        // snippet-end:[lambda.java1.delete.main]
    }
 }
-
+// snippet-end:[lambda.java1.DeleteFunction.complete]
