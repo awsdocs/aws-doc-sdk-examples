54b80822512ad826d6d519f563bc35e3f553ead0
Doug-AWS <dougsch@amazon.com>
2020-02-04 14:50:43 -0800

    Got example working locally. On to using Lambda!

--- go/use_cases/audio_to_text/audioToText.go
+++ go/use_cases/audio_to_text/audioToText.go
@@ -14,20 +14,55 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/transcribeservice"
 )
 
+type TranscriptResults struct {
+	JobName   string `json:"jobName"`
+	AccountID string `json:"accountId"`
+	Results   struct {
+		Transcripts []struct {
+			Transcript string `json:"transcript"`
+		} `json:"transcripts"`
+		Items []struct {
+			StartTime    string `json:"start_time,omitempty"`
+			EndTime      string `json:"end_time,omitempty"`
+			Alternatives []struct {
+				Confidence string `json:"confidence"`
+				Content    string `json:"content"`
+			} `json:"alternatives"`
+			Type string `json:"type"`
+		} `json:"items"`
+	} `json:"results"`
+	Status string `json:"status"`
+}
+
+func DebugPrint(debug bool, s string) {
+	if debug {
+		fmt.Println("INFO: " + s)
+	}
+}
+
 // dropFile adds MP3/4 file to a bucket
-func dropFile(sess *session.Session, bucketName string, filename string) error {
+// and returns the URI (for StartTranscriptionJob)
+func dropFile(debug bool, sess *session.Session, filename string, inputBucket string) (string, error) {
+	DebugPrint(debug, "Opening "+filename)
 	file, err := os.Open(filename)
 	if err != nil {
-		return err
+		fmt.Println("Could not open " + filename)
+		return "", err
 	}
 
 	defer file.Close()
@@ -36,48 +71,131 @@ func dropFile(sess *session.Session, bucketName string, filename string) error {
 
 	// Upload the file's body to S3 bucket as an object with the key being the
 	// same as the filename.
+	DebugPrint(debug, "Uploading "+filename+" to bucket "+inputBucket)
 	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucketName),
-
-		// Can also use the `filepath` standard library package to modify the
-		// filename as need for an S3 object key. Such as turning absolute path
-		// to a relative path.
-		Key: aws.String(filename),
-
-		// The file to be uploaded. io.ReadSeeker is preferred as the Uploader
-		// will be able to optimize memory when uploading large content. io.Reader
-		// is supported, but will require buffering of the reader's bytes for
-		// each part.
-		Body: file,
+		Bucket: aws.String(inputBucket),
+		Key:    aws.String(filename),
+		Body:   file,
 	})
 	if err != nil {
+		fmt.Println("Failed to upload " + filename + " to bucket " + inputBucket)
+		return "", err
+	}
+
+	// Create URI based on bucket name and filename
+	uri := "s3://" + inputBucket + "/" + filename
+
+	return uri, nil
+}
+
+func startTranscription(sess *session.Session, mediaURI string, outputBucket string, jobName string) error {
+	svc := transcribeservice.New(sess)
+
+	input := &transcribeservice.StartTranscriptionJobInput{
+		LanguageCode: aws.String("en-US"),
+		Media: &transcribeservice.Media{
+			MediaFileUri: aws.String(mediaURI),
+		},
+		MediaFormat:          aws.String("wav"),
+		OutputBucketName:     aws.String(outputBucket),
+		TranscriptionJobName: aws.String(jobName),
+	}
+
+	_, err := svc.StartTranscriptionJob(input)
+	if err != nil {
+		fmt.Println("Got an error starting transcription service:")
+		fmt.Println(err)
 		return err
 	}
 
 	return nil
 }
 
+func isTranscriptionDone(sess *session.Session, jobName string) (bool, error) {
+	svc := transcribeservice.New(sess)
+
+	input := &transcribeservice.ListTranscriptionJobsInput{
+		JobNameContains: aws.String(jobName),
+	}
+
+	result, err := svc.ListTranscriptionJobs(input)
+	if err != nil {
+		return false, err
+	}
+
+	if result.TranscriptionJobSummaries[0].TranscriptionJobStatus == nil {
+		return false, nil
+	}
+
+	// QUEUED | IN_PROGRESS | FAILED | COMPLETED
+	if *result.TranscriptionJobSummaries[0].TranscriptionJobStatus == "COMPLETED" {
+		return true, nil
+	}
+
+	if *result.TranscriptionJobSummaries[0].TranscriptionJobStatus == "FAILED" {
+		return false, errors.New("Job failed")
+	}
+
+	return false, nil
+}
+
 // getFile gets a file from a bucket
-func getFile(sess *session.Session, bucketName string, filename string) (*os.File, error) {
-	file, err := os.Create(filename)
-	if err != nil {
-		err = errors.New("Could not open file " + filename)
-		return nil, err
+func getResultURI(sess *session.Session, jobName string) (string, error) {
+	svc := transcribeservice.New(sess)
+
+	input := &transcribeservice.GetTranscriptionJobInput{
+		TranscriptionJobName: aws.String(jobName),
 	}
 
+	results, err := svc.GetTranscriptionJob(input)
+	if err != nil {
+		return "", err
+	}
+
+	return *results.TranscriptionJob.Transcript.TranscriptFileUri, nil
+}
+
+func getTextFromURI(sess *session.Session, outputBucket string, uri string) (string, error) {
+	svc := s3.New(sess)
+	// The URI should look something like:
+	// "s3://" + outputBucket + "/" + filename
+	parts := strings.Split(uri, "/")
+	filename := parts[len(parts)-1]
+
+	// Now get the guts of the file and return it
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(outputBucket),
+		Key:    aws.String(filename),
+	}
+
+	result, err := svc.GetObject(input)
+	if err != nil {
+		return "", err
+	}
+
+	// Body is of type io.ReadCloser
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(result.Body)
+	text := buf.String()
+
+	job := TranscriptResults{}
+
+	// Marshall JSON string in text into job struct
+	json.Unmarshal([]byte(text), &job)
+
+	return job.Results.Transcripts[0].Transcript, nil
+}
+
+func getTextFromFile(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
 	defer file.Close()
-	downloader := s3manager.NewDownloader(sess)
 
-	_, err = downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(filename),
-		})
-	if err != nil {
-		return nil, err
-	}
+	b, err := ioutil.ReadAll(file)
 
-	return file, nil
+	return string(b), nil
 }
 
 func main() {}
--- go/use_cases/audio_to_text/audioToText_test.go
+++ go/use_cases/audio_to_text/audioToText_test.go
@@ -14,10 +14,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 	"testing"
 	"time"
 
@@ -27,50 +29,81 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-func multiplyDuration(factor int64, d time.Duration) time.Duration {
-	return time.Duration(factor) * d // method 1 -- multiply in 'Duration'
-	// return time.Duration(factor * int64(d)) // method 2 -- multiply in 'int64'
+type Config struct {
+	Region       string `json:"Region"`
+	InputBucket  string `json:"InputBucket"`
+	OutputBucket string `json:"OutputBucket"`
+	AudioFile    string `json:"AudioFile"`
+	ResultFile   string `json:"ResultFile"`
+	SleepSeconds int    `json:"SleepSeconds"`
+	JobName      string `json:"JobName"`
+	Debug        bool   `json:"Debug"`
 }
 
+func multiplyDuration(factor int64, d time.Duration) time.Duration {
+	return time.Duration(factor) * d
+}
+
+func getNameOrDefault(defaultName string, envName string) string {
+	name := os.Getenv(envName)
+
+	if name == "" {
+		return defaultName
+	}
+
+	return name
+}
+
+/*
+func DebugPrint(debug bool, s string) {
+	if debug {
+		fmt.Println("INFO: " + s)
+	}
+}
+*/
+
 func TestAudioToText(t *testing.T) {
-	// Make sure we have all of the names
-	// Get log filename from environment variable LOG_FILE
-	logFile := os.Getenv("LOG_FILE")
+	// When the test started:
+	thisTime := time.Now()
+	nowString := thisTime.Format("20060102150405")
+	fmt.Println("Starting unit test at " + nowString)
+	fmt.Println("")
 
-	if logFile == "" {
-		// Create a log file based on the time and date
-		t := time.Now()
-		logFile = "audiototext-" + t.Format("20060102150405") + "-log"
+	// Get configuration from config.json
+	configFileName := "config.json"
 
-		err := os.Setenv("LOG_FILE", logFile)
-		if err != nil {
-			os.Exit(1)
-		}
-	}
-
-	f, err := os.OpenFile(logFile,
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// Get entire file as a JSON string
+	content, err := ioutil.ReadFile(configFileName)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
-	defer f.Close()
 
-	logger := log.New(f, "AUDIO_TO_TEXT", log.LstdFlags)
+	// Convert []byte to string
+	text := string(content)
+	config := Config{}
+
+	// Marshall JSON string in text into job struct
+	json.Unmarshal([]byte(text), &config)
+
+	// Add timestamp to job name
+	config.JobName = config.JobName + "-" + nowString
+
+	DebugPrint(config.Debug, "Got configuration values:")
+	DebugPrint(config.Debug, "Region:       "+config.Region)
+	DebugPrint(config.Debug, "InputBucket:  "+config.InputBucket)
+	DebugPrint(config.Debug, "OutputBucket: "+config.OutputBucket)
+	DebugPrint(config.Debug, "AudioFile:    "+config.AudioFile)
+	DebugPrint(config.Debug, "ResultFile:   "+config.ResultFile)
+	seconds := strconv.Itoa(config.SleepSeconds)
+	DebugPrint(config.Debug, "SleepSeconds: "+seconds)
+	DebugPrint(config.Debug, "JobName:      "+config.JobName)
+
+	// Create random string
+	id := guuid.New()
+	defaultName := id.String()
 
 	// Get region from environment variable AWS_REGION
-	region := os.Getenv("AWS_REGION")
-
-	if region == "" {
-		// If no region is set, use us-west-2
-		region = "us-west-2"
-		err = os.Setenv("AWS_REGION", region)
-		if err != nil {
-			logger.Println("Got an error setting the AWS_REGION env variable to us-west-2")
-			os.Exit(1)
-		}
-
-		logger.Println("Setting AWS_REGION env variable to us-west-2")
-	}
+	region := getNameOrDefault(config.Region, "AWS_REGION")
 
 	/*
 		Bucket names can be up to 63 characters long,
@@ -78,64 +111,35 @@ func TestAudioToText(t *testing.T) {
 	*/
 
 	// Get input bucket name from INPUT_BUCKET env variable
-	inputBucket := os.Getenv("INPUT_BUCKET")
+	inputBucket := config.InputBucket
 
 	if inputBucket == "" {
-		// Create unique bucket name
-		// GUIDs are 32 chars long
-		id := guuid.New()
-		bucketName := "input-" + id.String()
-		logger.Println("Setting the INPUT_BUCKET env variable to " + bucketName)
-		err = os.Setenv("INPUT_BUCKET", bucketName)
-		if err != nil {
-			logger.Println("Got an error setting the INPUT_BUCKET env variable to " + bucketName)
-			os.Exit(1)
-		}
+		inputBucket = "input-" + defaultName
 	}
+
+	inputBucket = getNameOrDefault(inputBucket, "INPUT_BUCKET")
 
 	// Get output bucket name from OUTPUT_BUCKET env variable
-	outputBucket := os.Getenv("OUTPUT_BUCKET")
+	outputBucket := config.OutputBucket
 
 	if outputBucket == "" {
-		// Create unique bucket name
-		// GUIDs are 32 chars long
-		id := guuid.New()
-		bucketName := "output-" + id.String()
-		logger.Println("Setting the OUTPUT_BUCKET env variable to " + bucketName)
-		err = os.Setenv("OUTPUT_BUCKET", bucketName)
-		if err != nil {
-			logger.Println("Got an error setting the OUTPUT_BUCKET env variable to " + bucketName)
-			os.Exit(1)
-		}
+		outputBucket = "output-" + defaultName
 	}
-
-	// Get Lambda function name from LAMBDA_FUNCTION env variable
-	lambdaFunction := os.Getenv("LAMBDA_FUNCTION")
-
-	if lambdaFunction == "" {
-		logger.Println("You must specify a Lambda function name in the LAMBDA_FUNCTION env variable")
-		os.Exit(1)
-	}
+	outputBucket = getNameOrDefault(outputBucket, "OUTPUT_BUCKET")
 
 	// Get audio file name from AUDIO_FILE env variable
-	audioFile := os.Getenv("AUDIO_FILE")
+	audioFile := getNameOrDefault(config.AudioFile, "AUDIO_FILE")
 
-	if audioFile == "" {
-		logger.Println("You must specify an audio file name in the AUDIO_FILE env variable")
-		os.Exit(1)
-	}
+	// Get results file name from RESULTS_FILE env variable
+	resultsFile := getNameOrDefault(config.ResultFile, "RESULTS_FILE")
 
 	// Get how long to wait, in seconds, for file to show up in output bucket from SLEEP_SECONDS env variable
-	sleepSecondsStr := os.Getenv("SLEEP_SECONDS")
-
-	if sleepSecondsStr == "" {
-		logger.Println("You must specify how long, in seconds, to wait for output file to appear in the SLEEP_SECONDS env variable")
-		os.Exit(1)
-	}
+	sleepSecondsStr := getNameOrDefault(seconds, "SLEEP_SECONDS")
 
 	sleepSeconds, err := strconv.ParseInt(sleepSecondsStr, 10, 64)
 	if err != nil {
-		logger.Println(sleepSecondsStr + " is not an integer")
+		fmt.Println("Got an error parsing " + sleepSecondsStr + " as an integer")
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
@@ -145,30 +149,121 @@ func TestAudioToText(t *testing.T) {
 		Region: aws.String(region),
 	}))
 
-	// Upload audio file to bucket
-	err = dropFile(sess, inputBucket, audioFile)
+	// Create input and output buckets
+	err = createBucket(sess, inputBucket)
 	if err != nil {
-		logger.Println("Failed to drop " + audioFile + " into bucket " + inputBucket)
+		fmt.Println("Could not create bucket " + inputBucket)
 		os.Exit(1)
+	} else {
+		fmt.Println("Created bucket " + inputBucket)
 	}
 
-	// Output file should be the same name as input file, but with a txt extension
-	parts := strings.Split(inputBucket, ".")
-	filename := parts[0] + ".txt"
+	err = createBucket(sess, outputBucket)
+	if err != nil {
+		fmt.Println("Could not create bucket " + outputBucket)
+		os.Exit(1)
+	} else {
+		fmt.Println("Created bucket " + outputBucket)
+	}
 
+	fmt.Println("")
+
+	// Upload audio file to bucket
+	fileURI, err := dropFile(config.Debug, sess, audioFile, inputBucket)
+	if err != nil {
+		fmt.Println("Failed to drop " + audioFile + " into bucket " + inputBucket)
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		DebugPrint(config.Debug, "Dropped "+audioFile+" into bucket "+inputBucket)
+	}
+
+	err = startTranscription(sess, fileURI, outputBucket, config.JobName)
+	if err != nil {
+		fmt.Println("Failed to start transcribing " + audioFile)
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Started transcription")
+	}
+
+	// loop until the job is done
 	// Wait for sleepSeconds seconds (is there a better way???)
 	ts := multiplyDuration(sleepSeconds, time.Second)
-	time.Sleep(ts)
 
-	// Get txt file from bucket
-	file, err := getFile(sess, outputBucket, filename)
+	for true {
+		done, err := isTranscriptionDone(sess, config.JobName)
+		if err != nil {
+			fmt.Println("Transcribing " + audioFile + " failed")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if done {
+			fmt.Println("")
+			fmt.Println("Transcription complete")
+			break
+		} else {
+			fmt.Print(".")
+		}
+
+		time.Sleep(ts)
+	}
+
+	fmt.Println("")
+
+	// Now get results of transcription
+	resultsURI, err := getResultURI(sess, config.JobName)
 	if err != nil {
-		logger.Println("Failed to get " + filename + " from bucket " + outputBucket)
+		fmt.Println("Failed to get text from " + outputBucket)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Get content from file
+	text, err = getTextFromURI(sess, outputBucket, resultsURI)
+	if err != nil {
+		fmt.Println("Could not get results from bucket " + outputBucket)
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	// Compare it with FourScoreResult.txt
+	expected, err := getTextFromFile(resultsFile)
+	if err != nil {
+		fmt.Println("Could not get text from " + resultsFile)
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	// Compare the results with the expected results
+	if text == expected {
+		fmt.Println("Got the expected results: ")
+		fmt.Println("    " + text)
+	} else {
+		fmt.Println("Did NOT get the expected results. Got:")
+		fmt.Println("'" + text + "'")
+		fmt.Println("instead of:")
+		fmt.Println("'" + expected + "'")
+
+	}
+
+	fmt.Println("")
+
+	// Clean up
+	err = deleteBucket(sess, inputBucket)
+	if err != nil {
+		fmt.Println("Could not delete bucket: " + inputBucket)
+		os.Exit(1)
+	} else {
+		fmt.Println("Deleted bucket " + inputBucket)
+	}
+
+	err = deleteBucket(sess, outputBucket)
+	if err != nil {
+		fmt.Println("Could not delete bucket: " + outputBucket)
+		os.Exit(1)
+	} else {
+		fmt.Println("Deleted bucket " + outputBucket)
+	}
+
+	fmt.Println("")
 }
