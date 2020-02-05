cacde827755dcbd0a4baadd9dcb25b7ab610567e
Doug-AWS <dougsch@amazon.com>
2020-02-05 05:54:40 -0800

    Added readme to Go S3 example; converted tabs in Go code to 4 spaces

--- go/example_code/s3/delete-buckets-by-prefix/s3_delete_buckets_test.go
+++ go/example_code/s3/delete-buckets-by-prefix/s3_delete_buckets_test.go
@@ -14,134 +14,133 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"testing"
-	"time"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+    "testing"
+    "time"
 
-	guuid "github.com/google/uuid"
+    guuid "github.com/google/uuid"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func createBucket(sess *session.Session, bucketName string) error {
-	// Create S3 service client
-	svc := s3.New(sess)
+    // Create S3 service client
+    svc := s3.New(sess)
 
-	// Create the S3 Bucket
-	_, err := svc.CreateBucket(&s3.CreateBucketInput{
-		Bucket: aws.String(bucketName),
-	})
-	if err != nil {
-		return err
-	}
+    // Create the S3 Bucket
+    _, err := svc.CreateBucket(&s3.CreateBucketInput{
+        Bucket: aws.String(bucketName),
+    })
+    if err != nil {
+        return err
+    }
 
-	// Wait until bucket is created before finishing
-	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
-		Bucket: aws.String(bucketName),
-	})
-	if err != nil {
-		return err
-	}
+    // Wait until bucket is created before finishing
+    err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+        Bucket: aws.String(bucketName),
+    })
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func populateBucket(sess *session.Session, bucket string) error {
-	s3Svc := s3.New(sess)
+    s3Svc := s3.New(sess)
 
-	// Create an uploader with S3 client and default options
-	uploader := s3manager.NewUploaderWithClient(s3Svc)
+    // Create an uploader with S3 client and default options
+    uploader := s3manager.NewUploaderWithClient(s3Svc)
 
-	// Add some dummy data to the bucket
-	// Upload input parameters
-	file := "abc"
-	body := strings.NewReader(file)
+    // Add some dummy data to the bucket
+    text := "abc"
+    body := strings.NewReader(text)
 
-	upParams := &s3manager.UploadInput{
-		Bucket: &bucket,
-		Key:    aws.String("dummy"),
-		Body:   body,
-	}
+    upParams := &s3manager.UploadInput{
+        Bucket: &bucket,
+        Key:    aws.String("dummy"),
+        Body:   body,
+    }
 
-	// Perform an upload.
-	_, err := uploader.Upload(upParams)
+    _, err := uploader.Upload(upParams)
 
-	return err
+    return err
 }
 
+// listBuckets lists the S3 buckets with the given prefix
 func listBuckets(sess *session.Session, prefix string) error {
-	// Create S3 service client
-	svc := s3.New(sess)
+    // Create S3 service client
+    svc := s3.New(sess)
 
-	result, err := svc.ListBuckets(nil)
-	if err != nil {
-		fmt.Println("Could not list buckets")
-		return err
-	}
+    result, err := svc.ListBuckets(nil)
+    if err != nil {
+        fmt.Println("Could not list buckets")
+        return err
+    }
 
-	for _, b := range result.Buckets {
-		if strings.HasPrefix(*b.Name, prefix) {
-			fmt.Println(*b.Name)
-		}
-	}
+    for _, b := range result.Buckets {
+        if strings.HasPrefix(*b.Name, prefix) {
+            fmt.Println(*b.Name)
+        }
+    }
 
-	return nil
+    return nil
 }
 
 func TestAudioToText(t *testing.T) {
-	// When the test started:
-	thisTime := time.Now()
-	nowString := thisTime.Format("20060102150405")
-	fmt.Println("Starting unit test at " + nowString)
-	fmt.Println("")
+    // When the test started:
+    thisTime := time.Now()
+    nowString := thisTime.Format("20060102150405")
+    fmt.Println("Starting unit test at " + nowString)
+    fmt.Println("")
 
-	// Initialize a session that the SDK uses to load
-	// credentials from the shared credentials file. (~/.aws/credentials).
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+    // Initialize a session that the SDK uses to load
+    // credentials from the shared credentials file. (~/.aws/credentials).
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
 
-	// Create a unique GUID
-	id := guuid.New()
-	defaultName := id.String()
+    // Create a unique GUID
+    id := guuid.New()
+    defaultName := id.String()
 
-	// Create 10 dummy buckets with prefix "dummy-"
-	prefix := "dummy-"
+    // Create 3 dummy buckets with prefix "dummy-"
+    prefix := "dummy-"
 
-	for i := 0; i < 10; i++ {
-		name := prefix + defaultName + strconv.Itoa(i)
-		err := createBucket(sess, name)
-		if err != nil {
-			fmt.Println("Could not create bucket " + name)
-			os.Exit(1)
-		} else {
-			fmt.Println("Created bucket " + name)
+    for i := 0; i < 3; i++ {
+        name := prefix + defaultName + strconv.Itoa(i)
+        err := createBucket(sess, name)
+        if err != nil {
+            fmt.Println("Could not create bucket " + name)
+            os.Exit(1)
+        } else {
+            fmt.Println("Created bucket " + name)
 
-			err = populateBucket(sess, name)
-		}
-	}
+            err = populateBucket(sess, name)
+        }
+    }
 
-	// List the buckets
-	fmt.Println("")
-	fmt.Println("Here are your buckets that start with " + prefix)
-	listBuckets(sess, prefix)
+    // List the buckets
+    fmt.Println("")
+    fmt.Println("Here are your buckets that start with " + prefix)
+    listBuckets(sess, prefix)
 
-	// Now delete them
-	err := deleteBucketsByPrefix(sess, prefix)
-	if err != nil {
-		fmt.Println("Could not delete buckets with prefix " + prefix)
-	} else {
-		fmt.Println("Deleted buckets with prefix " + prefix)
-	}
+    // Now delete them
+    err := deleteBucketsByPrefix(sess, prefix)
+    if err != nil {
+        fmt.Println("Could not delete buckets with prefix " + prefix)
+    } else {
+        fmt.Println("Deleted buckets with prefix " + prefix)
+    }
 
-	// List the buckets
-	fmt.Println("")
-	fmt.Println("Now here are your buckets that start with " + prefix)
-	listBuckets(sess, prefix)
+    // List the buckets
+    fmt.Println("")
+    fmt.Println("Now here are your buckets that start with " + prefix)
+    listBuckets(sess, prefix)
 }
