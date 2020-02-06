24c1809c5a409ae3041e40d503bd2b14331cc3fd
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-05 19:49:35 -0500

    Update UpdateTable.java
    Rolled in comments made by the editor. 
    This file has had a linter test by https://maven.apache.org/plugins/maven-checkstyle-plugin
    The Java logic has been test by using JUNIT 5

--- javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/UpdateTable.java
+++ javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/UpdateTable.java
@@ -1,84 +1,80 @@
-//snippet-sourcedescription:[UpdateTable.java demonstrates how to ...]
+/*
+   Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+    http://aws.amazon.com/apache2.0/
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+//snippet-sourcedescription:[UpdateTable.java demonstrates how to update an AWS DynamoDB table]
 //snippet-keyword:[SDK for Java 2.0]
 //snippet-keyword:[Code Sample]
 //snippet-service:[dynamodb]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[]
 //snippet-sourceauthor:[soo-aws]
-/*
-   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
-
-    http://aws.amazon.com/apache2.0/
-
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
-*/
 package com.example.dynamodb;
 // snippet-start:[dynamodb.java2.update_table.complete]
 // snippet-start:[dynamodb.java2.update_table.import]
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
 import software.amazon.awssdk.services.dynamodb.model.UpdateTableRequest;
-
 import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
- 
 // snippet-end:[dynamodb.java2.update_table.import]
+
 /**
- * Update a DynamoDB table (change provisioned throughput).
+ * Updates an AWS DynamoDB table (change provisioned throughput).
  *
- * Takes the name of the table to update, the read capacity and the write
- * capacity to use.
- *
- * This code expects that you have AWS credentials set up per:
+ * This code expects that you have AWS credentials set up, as described here:
  * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
  */
-public class UpdateTable
-{
-    public static void main(String[] args)
-    {
+public class UpdateTable {
+
+    public static void main(String[] args) {
         final String USAGE = "\n" +
-            "Usage:\n" +
-            "    UpdateTable <table> <read> <write>\n\n" +
-            "Where:\n" +
-            "    table - the table to put the item in.\n" +
-            "    read  - the new read capacity of the table.\n" +
-            "    write - the new write capacity of the table.\n\n" +
-            "Example:\n" +
-            "    UpdateTable HelloTable 16 10\n";
+                "Usage:\n" +
+                "    UpdateTable <table> <read> <write>\n\n" +
+                "Where:\n" +
+                "    table - the table to put the item in (i.e., Music3)\n" +
+                "    read  - the new read capacity of the table (i.e., 16)\n" +
+                "    write - the new write capacity of the table (i.e., 10)\n\n" +
+                "Example:\n" +
+                "    UpdateTable Music3 16 10\n";
 
         if (args.length < 3) {
             System.out.println(USAGE);
             System.exit(1);
         }
 
-        String table_name = args[0];
-Long read_capacity = Long.parseLong(args[1]);
-        Long write_capacity = Long.parseLong(args[2]);
+        // snippet-start:[dynamodb.java2.update_table.main]
+        String tableName = args[0];
+        Long readCapacity = Long.parseLong(args[1]);
+        Long writeCapacity = Long.parseLong(args[2]);
+
+        // Create the DynamoDbClient object
+        Region region = Region.US_WEST_2;
+        DynamoDbClient ddb = DynamoDbClient.builder().region(region).build();
 
         System.out.format(
                 "Updating %s with new provisioned throughput values\n",
-                table_name);
-        System.out.format("Read capacity : %d\n", read_capacity);
-        System.out.format("Write capacity : %d\n", write_capacity);
+                tableName);
+        System.out.format("Read capacity : %d\n", readCapacity);
+        System.out.format("Write capacity : %d\n", writeCapacity);
 
-        // snippet-start:[dynamodb.java2.update_table.main]
-        ProvisionedThroughput table_throughput = ProvisionedThroughput.builder()
-        		.readCapacityUnits(read_capacity)
-        		.writeCapacityUnits(write_capacity)
-        		.build();
-
-        DynamoDbClient ddb = DynamoDbClient.create();
+        ProvisionedThroughput tableThroughput = ProvisionedThroughput.builder()
+                .readCapacityUnits(readCapacity)
+                .writeCapacityUnits(writeCapacity)
+                .build();
 
         UpdateTableRequest request = UpdateTableRequest.builder()
-        		.provisionedThroughput(table_throughput)
-        		.tableName(table_name)
-        		.build();
+                .provisionedThroughput(tableThroughput)
+                .tableName(tableName)
+                .build();
 
         try {
             ddb.updateTable(request);
@@ -91,5 +87,4 @@ Long read_capacity = Long.parseLong(args[1]);
         System.out.println("Done!");
     }
 }
- 
 // snippet-end:[dynamodb.java2.update_table.complete]
