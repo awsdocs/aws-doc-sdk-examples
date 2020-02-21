129725178c26db65887979a157ff091edb9636c9
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-21 15:22:46 -0500

    Update PutMetricAlarm.java
    This file passed a Linter and has a unit test

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutMetricAlarm.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutMetricAlarm.java
@@ -3,11 +3,11 @@
 //snippet-keyword:[Code Sample]
 //snippet-service:[cloudwatch]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[]
-//snippet-sourceauthor:[soo-aws]
-// snippet-start:[cloudwatch.java2.put_metric_alarm.complete]
+//snippet-sourcedate:[13/02/2020]
+//snippet-sourceauthor:[scmacdon]
+
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -21,14 +21,16 @@
  * permissions and limitations under the License.
  */
 package com.example.cloudwatch;
+// snippet-start:[cloudwatch.java2.put_metric_alarm.complete]
 // snippet-start:[cloudwatch.java2.put_metric_alarm.import]
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
-import software.amazon.awssdk.services.cloudwatch.model.ComparisonOperator;
 import software.amazon.awssdk.services.cloudwatch.model.Dimension;
 import software.amazon.awssdk.services.cloudwatch.model.PutMetricAlarmRequest;
-import software.amazon.awssdk.services.cloudwatch.model.PutMetricAlarmResponse;
-import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
+import software.amazon.awssdk.services.cloudwatch.model.ComparisonOperator;
 import software.amazon.awssdk.services.cloudwatch.model.Statistic;
+import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
+import software.amazon.awssdk.services.cloudwatch.model.PutMetricAlarmResponse;
+import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
 // snippet-end:[cloudwatch.java2.put_metric_alarm.import]
 /**
  * Creates a new CloudWatch alarm based on CPU utilization for an instance
@@ -37,8 +39,8 @@ public class PutMetricAlarm {
     public static void main(String[] args) {
 
         final String USAGE =
-            "To run this example, supply an alarm name and instance id\n" +
-            "Ex: DeleteAlarm <alarm-name> <instance-id>\n";
+                "To run this example, supply an alarm name and instance id\n" +
+                        "Ex: DeleteAlarm <alarm-name> <instance-id>\n";
 
         if (args.length != 2) {
             System.out.println(USAGE);
@@ -49,36 +51,41 @@ public class PutMetricAlarm {
         String instanceId = args[1];
 
         // snippet-start:[cloudwatch.java2.put_metric_alarm.main]
-        CloudWatchClient cw =
-        		CloudWatchClient.builder().build();
+        CloudWatchClient cw = CloudWatchClient.builder().build();
 
-        Dimension dimension = Dimension.builder()
-            .name("InstanceId")
-            .value(instanceId).build();
+        try {
 
-        PutMetricAlarmRequest request = PutMetricAlarmRequest.builder()
-            .alarmName(alarmName)
-            .comparisonOperator(
-                ComparisonOperator.GREATER_THAN_THRESHOLD)
-            .evaluationPeriods(1)
-            .metricName("CPUUtilization")
-            .namespace("AWS/EC2")
-            .period(60)
-            .statistic(Statistic.AVERAGE)
-            .threshold(70.0)
-            .actionsEnabled(false)
-            .alarmDescription(
-                "Alarm when server CPU utilization exceeds 70%")
-            .unit(StandardUnit.SECONDS)
-            .dimensions(dimension)
-            .build();
+            Dimension dimension = Dimension.builder()
+                .name("InstanceId")
+                .value(instanceId).build();
 
-        PutMetricAlarmResponse response = cw.putMetricAlarm(request);
+            PutMetricAlarmRequest request = PutMetricAlarmRequest.builder()
+                .alarmName(alarmName)
+                .comparisonOperator(
+                        ComparisonOperator.GREATER_THAN_THRESHOLD)
+                .evaluationPeriods(1)
+                .metricName("CPUUtilization")
+                .namespace("AWS/EC2")
+                .period(60)
+                .statistic(Statistic.AVERAGE)
+                .threshold(70.0)
+                .actionsEnabled(false)
+                .alarmDescription(
+                        "Alarm when server CPU utilization exceeds 70%")
+                .unit(StandardUnit.SECONDS)
+                .dimensions(dimension)
+                .build();
+
+            PutMetricAlarmResponse response = cw.putMetricAlarm(request);
+
+        } catch (CloudWatchException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
+       System.out.printf(
+                "Successfully created alarm with name %s", alarmName);
         // snippet-end:[cloudwatch.java2.put_metric_alarm.main]
-
-        System.out.printf(
-            "Successfully created alarm with name %s", alarmName);
-
     }
 }
 // snippet-end:[cloudwatch.java2.put_metric_alarm.complete]
