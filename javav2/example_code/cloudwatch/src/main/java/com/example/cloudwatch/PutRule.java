51cfd979fb636267329bfc0d0d3ef65e766f77f0
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-21 15:25:19 -0500

    Update PutRule.java
    This file passed a Linter and has a unit test

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutRule.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutRule.java
@@ -3,11 +3,11 @@
 //snippet-keyword:[Code Sample]
 //snippet-service:[cloudwatch]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[]
-//snippet-sourceauthor:[soo-aws]
-// snippet-start:[cloudwatch.java2.put_rule.complete]
+//snippet-sourcedate:[13/02/2020]
+//snippet-sourceauthor:[scmacdon]
+
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -21,7 +21,9 @@
  * permissions and limitations under the License.
  */
 package com.example.cloudwatch;
+// snippet-start:[cloudwatch.java2.put_rule.complete]
 // snippet-start:[cloudwatch.java2.put_rule.import]
+import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
 import software.amazon.awssdk.services.cloudwatchevents.CloudWatchEventsClient;
 import software.amazon.awssdk.services.cloudwatchevents.model.PutRuleRequest;
 import software.amazon.awssdk.services.cloudwatchevents.model.PutRuleResponse;
@@ -36,34 +38,44 @@ public class PutRule {
     public static void main(String[] args) {
 
         final String USAGE =
-            "To run this example, supply a rule name and role arn\n" +
-            "Ex: PutRule <rule-name> <role-arn>\n";
+                "To run this example, supply a rule name and role arn\n" +
+                        "Ex: PutRule <rule-name> <role-arn>\n";
 
         if (args.length != 2) {
             System.out.println(USAGE);
             System.exit(1);
         }
 
-        String rule_name = args[0];
-        String role_arn = args[1];
+        String ruleName = args[0];
+        String roleArn = args[1];
 
-        // snippet-start:[cloudwatch.java2.put_rule.main]
-        CloudWatchEventsClient cwe =
-        		CloudWatchEventsClient.builder().build();
+        PutRuleResponse response = null;
 
-        PutRuleRequest request = PutRuleRequest.builder()
-            .name(rule_name)
-            .roleArn(role_arn)
-            .scheduleExpression("rate(5 minutes)")
-            .state(RuleState.ENABLED)
-            .build();
+        try {
 
-        PutRuleResponse response = cwe.putRule(request);
+            // snippet-start:[cloudwatch.java2.put_rule.main]
+            CloudWatchEventsClient cwe =
+                CloudWatchEventsClient.builder().build();
+
+            PutRuleRequest request = PutRuleRequest.builder()
+                .name(ruleName)
+                .roleArn("arn:aws:iam::335446330391:role/testRole1")
+                .scheduleExpression("rate(5 minutes)")
+                .state(RuleState.ENABLED)
+                .build();
+
+            response = cwe.putRule(request);
         // snippet-end:[cloudwatch.java2.put_rule.main]
 
+        } catch (
+            CloudWatchException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+    }
+
         System.out.printf(
-            "Successfully created CloudWatch events rule %s with arn %s",
-            rule_name, response.ruleArn());
+                "Successfully created CloudWatch events rule %s with arn %s",
+                roleArn, response.ruleArn());
     }
 }
 // snippet-end:[cloudwatch.java2.put_rule.complete]
