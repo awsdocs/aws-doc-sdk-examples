d5985e92c841bf7e3580967817799ef1873581d6
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-21 14:59:02 -0500

    Update PutEvents.java
    This file passed a Linter and has a unit test

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutEvents.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/PutEvents.java
@@ -4,10 +4,10 @@
 //snippet-service:[cloudwatch]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[]
-//snippet-sourceauthor:[soo-aws]
+//snippet-sourceauthor:[scmacdon-aws]
 // snippet-start:[cloudwatch.java2.put_events.complete]
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
