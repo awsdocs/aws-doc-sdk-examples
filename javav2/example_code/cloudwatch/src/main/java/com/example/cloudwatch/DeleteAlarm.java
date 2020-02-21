baed7d42cfecebfdf380fc663d00a0fd29525a2f
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-21 14:40:52 -0500

    Update DeleteAlarm.java
    This file passed a Linter and has a unit test

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/DeleteAlarm.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/DeleteAlarm.java
@@ -3,11 +3,10 @@
 //snippet-keyword:[Code Sample]
 //snippet-service:[cloudwatch]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[]
-//snippet-sourceauthor:[soo-aws]
-// snippet-start:[cloudwatch.java2.delete_metrics.complete]
+//snippet-sourcedate:[12/12/2020]
+//snippet-sourceauthor:[scmacdon]
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -20,9 +19,11 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+// snippet-start:[cloudwatch.java2.delete_metrics.complete]
 package com.example.cloudwatch;
 // snippet-start:[cloudwatch.java2.delete_metrics.import]
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
+import software.amazon.awssdk.services.cloudwatch.model.CloudWatchException;
 import software.amazon.awssdk.services.cloudwatch.model.DeleteAlarmsRequest;
 import software.amazon.awssdk.services.cloudwatch.model.DeleteAlarmsResponse;
 // snippet-end:[cloudwatch.java2.delete_metrics.import]
@@ -34,26 +35,29 @@ public class DeleteAlarm {
     public static void main(String[] args) {
 
         final String USAGE =
-            "To run this example, supply an alarm name\n" +
-            "Ex: DeleteAlarm <alarm-name>\n";
+                "To run this example, supply an alarm name\n" +
+                        "Ex: DeleteAlarm <alarm-name>\n";
 
         if (args.length != 1) {
             System.out.println(USAGE);
             System.exit(1);
         }
 
-        String alarm_name = args[0];
-
         // snippet-start:[cloudwatch.java2.delete_metrics.main]
-        CloudWatchClient cw = CloudWatchClient.builder().build();
+        String alarmName = args[0];
+        try {
+            CloudWatchClient cw = CloudWatchClient.builder().build();
 
+            DeleteAlarmsRequest request = DeleteAlarmsRequest.builder()
+                .alarmNames(alarmName).build();
 
-        DeleteAlarmsRequest request = DeleteAlarmsRequest.builder()
-            .alarmNames(alarm_name).build();
+            DeleteAlarmsResponse response = cw.deleteAlarms(request);
+            System.out.printf("Successfully deleted alarm %s", alarmName);
 
-        DeleteAlarmsResponse response = cw.deleteAlarms(request);
-
-        System.out.printf("Successfully deleted alarm %s", alarm_name);
+        } catch (CloudWatchException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
         // snippet-end:[cloudwatch.java2.delete_metrics.main]
     }
 }
