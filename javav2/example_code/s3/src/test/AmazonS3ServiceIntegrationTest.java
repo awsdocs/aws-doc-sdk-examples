370b19f922335fc43e941eb233bf26579623cae9
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-27 11:11:24 -0500

    Create AmazonS3ServiceIntegrationTest.java

---
+++ javav2/example_code/s3/src/test/AmazonS3ServiceIntegrationTest.java
@@ -0,0 +1,383 @@
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AmazonS3ServiceIntegrationTest {
+
+    private static S3Client s3;
+    private static Region region;
+
+    // Define the data members required for the tests
+    private static String bucketName = "";
+    private static String objectKey = "";
+    private static String objectPath = "";
+    private static String toBucket = "";
+    private static String policyText = "";
+    private static String id = "";
+    private static String access = "";
+
+    @BeforeAll
+    public static void setUp() throws IOException {
+
+        // Run tests on Real AWS Resources
+        region = Region.US_WEST_2;
+        s3 = S3Client.builder().region(region).build();
+        try (InputStream input = AmazonS3ServiceIntegrationTest.class.getClassLoader().getResourceAsStream("config.properties")) {
+
+            Properties prop = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            // Populate the data members required for all tests
+            bucketName = prop.getProperty("bucketName");
+            objectKey = prop.getProperty("objectKey");
+            objectPath= prop.getProperty("objectPath");
+            toBucket = prop.getProperty("toBucket");
+            policyText = prop.getProperty("policyText");
+            id  = prop.getProperty("id");
+            access  = prop.getProperty("access");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    @Order(1)
+    public void whenInitializingAWSS3Service_thenNotNull() {
+        assertNotNull(s3);
+        System.out.println("Running Amazon S3 Test 1");
+    }
+
+    @Test
+    @Order(2)
+   public void createBucket() {
+
+       // Create bucket
+       CreateBucketRequest createBucketRequest = CreateBucketRequest
+               .builder()
+               .bucket(bucketName)
+               .createBucketConfiguration(CreateBucketConfiguration.builder()
+                       .locationConstraint(region.id())
+                       .build())
+               .build();
+
+       try {
+           s3.createBucket(createBucketRequest);
+       } catch (S3Exception e) {
+           System.err.println(e.awsErrorDetails().errorMessage());
+           System.exit(1);
+       }
+        System.out.println("Test 2 passed");
+   }
+
+    @Test
+    @Order(3)
+   public void putObject() {
+
+        try {
+        //Put a file into the bucket
+       s3.putObject(PutObjectRequest.builder().bucket(bucketName).key(objectKey)
+                       .build(),
+               RequestBody.fromBytes(getObjectFile(objectPath)));
+
+        } catch (S3Exception | FileNotFoundException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+        System.out.println("Test 3 passed");
+   }
+
+    @Test
+    @Order(4)
+   public void copyBucketObject() {
+
+        String encodedUrl="";
+        try {
+            encodedUrl = URLEncoder.encode(bucketName + "/" + objectKey, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("URL could not be encoded: " + e.getMessage());
+        }
+
+        CopyObjectRequest copyReq = CopyObjectRequest.builder()
+                .copySource(encodedUrl)
+                .bucket(toBucket)
+                .key(objectKey)
+                .build();
+
+        try {
+            CopyObjectResponse copyRes = s3.copyObject(copyReq);
+            System.out.println(copyRes.copyObjectResult().toString());
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        System.out.println("Test 4 passed");
+    }
+
+
+    @Test
+    @Order(5)
+    public void setBucketPolicy() {
+
+        System.out.println("Setting policy:");
+        System.out.println("----");
+        System.out.println(policyText);
+        System.out.println("----");
+        System.out.format("On S3 bucket: \"%s\"\n", bucketName);
+
+        policyText = getBucketPolicyFromFile(policyText);
+
+        try {
+            PutBucketPolicyRequest policyReq = PutBucketPolicyRequest.builder()
+                    .bucket(bucketName)
+                    .policy(policyText)
+                    .build();
+
+            s3.putBucketPolicy(policyReq);
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        System.out.println("Test 5 passed");
+
+    }
+
+    @Test
+    @Order(6)
+    public void getBucketPolicy() {
+
+        GetBucketPolicyRequest policyReq = GetBucketPolicyRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+        try {
+            GetBucketPolicyResponse policyRes = s3.getBucketPolicy(policyReq);
+            policyText = policyRes.policy();
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
+        if (policyText == null) {
+            System.out.println("The specified bucket has no bucket policy.");
+        } else {
+            System.out.println("Returned policy:");
+            System.out.println("----");
+            System.out.println(policyText);
+            System.out.println("----\n");
+        }
+        System.out.println("Test 6 passed");
+    }
+
+    @Test
+    @Order(7)
+    public void deleteBucketPolicy() {
+
+        try {
+            DeleteBucketPolicyRequest delPolReq = DeleteBucketPolicyRequest.builder()
+                .bucket(bucketName)
+                .build();
+
+            s3.deleteBucketPolicy(delPolReq);
+
+    } catch (S3Exception e) {
+        System.err.println(e.awsErrorDetails().errorMessage());
+        System.exit(1);
+    }
+        System.out.println("Test 7 passed");
+    }
+
+    @Test
+    @Order(8)
+    public void setBucketACL()
+    {
+
+        System.out.format("Running Amazon S3 Test 8");
+        System.out.println("for object: " + objectKey);
+        System.out.println(" in bucket: " + bucketName);
+
+        try {
+
+            // set access for the grantee  in acl
+            Grantee grantee = Grantee.builder().emailAddress(id).build();
+            Permission permission = Permission.valueOf(access);
+
+            Grant ownerGrant = Grant.builder()
+                    .grantee(builder -> {
+                        builder.id(id)
+                                .type(Type.CANONICAL_USER);
+                    })
+                    .permission(access)
+                    .build();
+
+            List<Grant> grantList2 = new ArrayList<>();
+            grantList2.add(ownerGrant);
+
+            //put the new acl
+            AccessControlPolicy acl = AccessControlPolicy.builder()
+                    .owner(builder -> builder.id(id))
+                    .grants(grantList2)
+                    .build();
+            //put the new acl
+            PutBucketAclRequest putAclReq = PutBucketAclRequest.builder()
+                    .bucket(bucketName)
+                    .accessControlPolicy(acl)
+                    .build();
+
+            s3.putBucketAcl(putAclReq);
+
+        } catch (S3Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("Test 8 passed");
+    }
+
+    @Test
+    @Order(9)
+    public void getACL(){
+
+        System.out.println("                in bucket: " + bucketName);
+
+        GetObjectAclRequest aclReq = GetObjectAclRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
+
+        try {
+            GetObjectAclResponse aclRes = s3.getObjectAcl(aclReq);
+            List<Grant> grants = aclRes.grants();
+            for (Grant grant : grants) {
+                System.out.format("  %s: %s\n", grant.grantee().id(),
+                        grant.permission());
+            }
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
+        System.out.println("Test 9 passed");
+    }
+
+    @Test
+    @Order(10)
+    public void deleteObjects() {
+
+        ArrayList<ObjectIdentifier> toDelete = new ArrayList<ObjectIdentifier>();
+       toDelete.add(ObjectIdentifier.builder().key(objectKey).build());
+
+        try {
+            DeleteObjectsRequest dor = DeleteObjectsRequest.builder()
+                    .bucket(bucketName)
+                    .delete(Delete.builder().objects(toDelete).build())
+                    .build();
+            s3.deleteObjects(dor);
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        System.out.println("Test 11 passed");
+    }
+
+    @Test
+    @Order(11)
+    public void deleteBucket() {
+
+       try {
+        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucketName).build();
+        s3.deleteBucket(deleteBucketRequest);
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        System.out.println("Test 12 passed");
+    }
+
+   public byte[] getObjectFile(String path) throws FileNotFoundException {
+
+       byte[] bFile = readBytesFromFile(path);
+       return bFile;
+   }
+
+    private static byte[] readBytesFromFile(String filePath) {
+
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+
+        try {
+            File file = new File(filePath);
+            bytesArray = new byte[(int) file.length()];
+
+            //read file into bytes[]
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bytesArray;
+    }
+
+    private static String getBucketPolicyFromFile(String policFile) {
+
+        StringBuilder fileText = new StringBuilder();
+        try {
+            List<String> lines = Files.readAllLines(
+                    Paths.get(policFile), Charset.forName("UTF-8"));
+            for (String line : lines) {
+                fileText.append(line);
+            }
+        } catch (IOException e) {
+            System.out.format("Problem reading file: \"%s\"", policFile);
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            final JsonParser parser = new ObjectMapper().getFactory().createParser(fileText.toString());
+            while (parser.nextToken() != null) {
+            }
+
+        } catch (JsonParseException jpe) {
+            jpe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return fileText.toString();
+    }
+}
