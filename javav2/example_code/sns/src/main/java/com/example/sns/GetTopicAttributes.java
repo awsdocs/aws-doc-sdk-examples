b07a5e7e0ad89a247b4c48e2126ed3ccc9c37ac2
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-03-04 10:37:59 -0500

    Update GetTopicAttributes.java
    File has been put through a linter and unit tested

--- javav2/example_code/sns/src/main/java/com/example/sns/GetTopicAttributes.java
+++ javav2/example_code/sns/src/main/java/com/example/sns/GetTopicAttributes.java
@@ -6,9 +6,9 @@
 //snippet-service:[sns]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2019-07-20]
-//snippet-sourceauthor:[jschwarzwalder AWS]
+//snippet-sourceauthor:[scmacdon AWS]
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -25,11 +25,11 @@
 package com.example.sns;
 
 //snippet-start:[sns.java2.GetTopicAttributes.import]
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.GetTopicAttributesRequest;
 import software.amazon.awssdk.services.sns.model.GetTopicAttributesResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
 //snippet-end:[sns.java2.GetTopicAttributes.import]
 
 public class GetTopicAttributes {
@@ -41,25 +41,32 @@ public class GetTopicAttributes {
                 "  topicArn - the arn of the topic to look up.\n\n";
 
         if (args.length < 1) {
-            System.out.println(USAGE);
-            System.exit(1);
+           System.out.println(USAGE);
+           System.exit(1);
         }
+
         //snippet-start:[sns.java2.GetTopicAttributes.main]
         String topicArn = args[0];
-
         System.out.println("Getting attributes for a topic with name: " + topicArn);
 
-        SnsClient snsClient = SnsClient.builder().region(Region.US_EAST_1).build();
+        // Create a SnsClient object
+        SnsClient snsClient = SnsClient.builder()
+                .region(Region.US_WEST_2)
+                .build();
 
-        GetTopicAttributesRequest request = GetTopicAttributesRequest.builder()
+        try {
+            GetTopicAttributesRequest request = GetTopicAttributesRequest.builder()
                 .topicArn(topicArn)
                 .build();
 
-        GetTopicAttributesResponse result = snsClient.getTopicAttributes(request);
+            GetTopicAttributesResponse result = snsClient.getTopicAttributes(request);
+            System.out.println("\n\nStatus was " + result.sdkHttpResponse().statusCode() + "\n\nAttributes: \n\n" + result.attributes());
 
-        System.out.println("\n\nStatus was " + result.sdkHttpResponse().statusCode() + "\n\nAttributes: \n\n" + result.attributes());
+        } catch (SnsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
         //snippet-end:[sns.java2.GetTopicAttributes.main]
     }
 }
 //snippet-end:[sns.java2.GetTopicAttributes.complete]
-
