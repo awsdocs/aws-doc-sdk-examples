a8bd15a8adbcc7443055f77265bd222d8b2a3857
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-03-04 10:51:12 -0500

    Update SubscribeHTTPS.java
    File has been put through a linter and unit tested

--- javav2/example_code/sns/src/main/java/com/example/sns/SubscribeHTTPS.java
+++ javav2/example_code/sns/src/main/java/com/example/sns/SubscribeHTTPS.java
@@ -6,9 +6,9 @@
 //snippet-service:[sns]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2019-07-20]
-//snippet-sourceauthor:[jschwarzwalder AWS]
+//snippet-sourceauthor:[scmacdon AWS]
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -24,10 +24,11 @@
 //snippet-start:[sns.java2.SubscribeHTTPS.complete]
 
 package com.example.sns;
-//snippet-start:[sns.java2.SubscribeHTTPS.import]
 
+//snippet-start:[sns.java2.SubscribeHTTPS.import]
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.SnsException;
 import software.amazon.awssdk.services.sns.model.SubscribeRequest;
 import software.amazon.awssdk.services.sns.model.SubscribeResponse;
 //snippet-end:[sns.java2.SubscribeHTTPS.import]
@@ -42,27 +43,34 @@ public class SubscribeHTTPS {
                 "  topicArn - the arn of the topic to subscribe.\n\n" +
                 "  url - https endpoint to subscribe.\n\n";
 
-        if (args.length < 2) {
+       if (args.length < 2) {
             System.out.println(USAGE);
             System.exit(1);
         }
+
 
         //snippet-start:[sns.java2.SubscribeHTTPS.main]
         String topicArn = args[0];
         String url = args[1];
 
-        SnsClient snsClient = SnsClient.builder().region(Region.US_EAST_1).build();
+        SnsClient snsClient = SnsClient.builder().region(Region.US_WEST_2).build();
 
-        SubscribeRequest request = SubscribeRequest.builder()
-                .protocol("https")
+        try {
+            SubscribeRequest request = SubscribeRequest.builder()
+                .protocol("http")
                 .endpoint(url)
                 .returnSubscriptionArn(true)
                 .topicArn(topicArn)
                 .build();
 
-        SubscribeResponse result = snsClient.subscribe(request);
+            SubscribeResponse result = snsClient.subscribe(request);
+            System.out.println("Subscription ARN: " + result.subscriptionArn() + "\n\n Status was " + result.sdkHttpResponse().statusCode());
 
-        System.out.println("Subscription ARN: " + result.subscriptionArn() + "\n\n Status was " + result.sdkHttpResponse().statusCode());
+        } catch (SnsException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+         }
+
         //snippet-end:[sns.java2.SubscribeHTTPS.main]
     }
 }
