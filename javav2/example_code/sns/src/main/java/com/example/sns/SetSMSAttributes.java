a96c7d3efe3d236dc210daa4b3de5b56ad331a82
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-03-04 11:41:53 -0500

    Update SetSMSAttributes.java

--- javav2/example_code/sns/src/main/java/com/example/sns/SetSMSAttributes.java
+++ javav2/example_code/sns/src/main/java/com/example/sns/SetSMSAttributes.java
@@ -8,7 +8,7 @@
 //snippet-sourcedate:[2019-07-20]
 //snippet-sourceauthor:[scmacdon AWS]
 /*
- * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
