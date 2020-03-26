ee13b75438af7c82e635731dd6d775def523a330
Doug-AWS <dougsch@amazon.com>
2020-03-26 14:05:52 -0700

    More SQS tweaking in Go

--- go/sqs/ReceiveMessage/ReceiveMessage.go
+++ go/sqs/ReceiveMessage/ReceiveMessage.go
@@ -16,13 +16,14 @@ package main
 
 // snippet-start:[sqs.go.receive_messages.imports]
 import (
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
+
 // snippet-end:[sqs.go.receive_messages.imports]
 
 // GetQueueURL gets the URL of an Amazon SQS queue
@@ -33,20 +34,20 @@ import (
 //     If success, the URL of the queue and nil
 //     Otherwise, an empty string and an error from the call to
 func GetQueueURL(sess *session.Session, queue *string) (*sqs.GetQueueUrlOutput, error) {
-    // Create an SQS service client
-    svc := sqs.New(sess)
+	// Create an SQS service client
+	svc := sqs.New(sess)
 
-    result, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
-        QueueName: queue,
-    })
-    if err != nil {
-        return nil, err
-    }
+	result, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: queue,
+	})
+	if err != nil {
+		return nil, err
+	}
 
-    return result, nil
+	return result, nil
 }
 
-// GetMessage gets the latest message from an Amazon SQS queue
+// GetMessages gets the messages from an Amazon SQS queue
 // Inputs:
 //     sess is the current session, which provides configuration for the SDK's service clients
 //     queueURL is the URL of the queue
@@ -54,78 +55,80 @@ func GetQueueURL(sess *session.Session, queue *string) (*sqs.GetQueueUrlOutput, 
 // Output:
 //     If success, the latest message and nil
 //     Otherwise, nil and an error from the call to ReceiveMessage
-func GetMessage(sess *session.Session, queueURL *string, timeout *int64) (*sqs.Message, error) {
-    var msg *sqs.Message
+func GetMessages(sess *session.Session, queueURL *string, timeout *int64) (*sqs.ReceiveMessageOutput, error) {
+	// Create an SQS service client
+	// snippet-start:[sqs.go.receive_messages.call]
+	svc := sqs.New(sess)
 
-    // Create an SQS service client
-    // snippet-start:[sqs.go.receive_messages.call]
-    svc := sqs.New(sess)
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            queueURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   timeout,
+	})
+	// snippet-end:[sqs.go.receive_messages.call]
+	if err != nil {
+		return nil, err
+	}
 
-    result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-        AttributeNames: []*string{
-            aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
-        },
-        MessageAttributeNames: []*string{
-            aws.String(sqs.QueueAttributeNameAll),
-        },
-        QueueUrl:            queueURL,
-        MaxNumberOfMessages: aws.Int64(1),
-        VisibilityTimeout:   timeout,
-    })
-    // snippet-end:[sqs.go.receive_messages.call]
-    if err != nil {
-        return msg, err
-    }
-
-    return result.Messages[0], nil
+	return result, nil
 }
 
 func main() {
-    // snippet-start:[sqs.go.receive_messages.args]
-    queue := flag.String("q", "", "The name of the queue")
-    timeout := flag.Int64("t", 5, "How long, in seconds, that the message is hidden from others")
-    flag.Parse()
+	// snippet-start:[sqs.go.receive_messages.args]
+	queue := flag.String("q", "", "The name of the queue")
+	timeout := flag.Int64("t", 5, "How long, in seconds, that the message is hidden from others")
+	flag.Parse()
 
-    if *queue == "" {
-        fmt.Println("You must supply the name of a queue (-q QUEUE)")
-        return
-    }
+	if *queue == "" {
+		fmt.Println("You must supply the name of a queue (-q QUEUE)")
+		return
+	}
 
-    if *timeout < 0 {
-        *timeout = 0
-    }
+	if *timeout < 0 {
+		*timeout = 0
+	}
 
-    if *timeout > 12*60*60 {
-        *timeout = 12 * 60 * 60
-    }
-    // snippet-end:[sqs.go.receive_messages.args]
+	if *timeout > 12*60*60 {
+		*timeout = 12 * 60 * 60
+	}
+	// snippet-end:[sqs.go.receive_messages.args]
 
-    // Create a session that gets credential values from ~/.aws/credentials
-    // and the default region from ~/.aws/config
-    // snippet-start:[sqs.go.receive_messages.sess]
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
-    // snippet-end:[sqs.go.receive_messages.sess]
+	// Create a session that gets credential values from ~/.aws/credentials
+	// and the default region from ~/.aws/config
+	// snippet-start:[sqs.go.receive_messages.sess]
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	// snippet-end:[sqs.go.receive_messages.sess]
 
-    // Get URL of queue
-    result, err := GetQueueURL(sess, queue)
-    if err != nil {
-        fmt.Println("Got an error getting the queue URL:")
-        fmt.Println(err)
-        return
-    }
+	// Get URL of queue
+	urlResult, err := GetQueueURL(sess, queue)
+	if err != nil {
+		fmt.Println("Got an error getting the queue URL:")
+		fmt.Println(err)
+		return
+	}
 
-    queueURL := result.QueueUrl
+	queueURL := urlResult.QueueUrl
 
-    msg, err := GetMessage(sess, queueURL, timeout)
-    if err != nil {
-        fmt.Println("Got an error receiving messages:")
-        fmt.Println(err)
-        return
-    }
+	result, err := GetMessages(sess, queueURL, timeout)
+	if err != nil {
+		fmt.Println("Got an error receiving messages:")
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Println("Message ID:     " + *msg.MessageId)
-    fmt.Println("Message Handle: " + *msg.ReceiptHandle)
+	fmt.Println("Message ID:     " + *result.Messages[0].MessageId)
+
+	// snippet-start:[sqs.go.receive_messages.print_handle]
+	fmt.Println("Message Handle: " + *result.Messages[0].ReceiptHandle)
+	// snippet-end:[sqs.go.receive_messages.print_handle]
 }
+
 // snippet-end:[sqs.go.receive_messages]
--- go/sqs/ReceiveMessage/ReceiveMessage_test.go
+++ go/sqs/ReceiveMessage/ReceiveMessage_test.go
@@ -15,25 +15,25 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "strconv"
-    "testing"
-    "time"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+	"testing"
+	"time"
 
-    "github.com/google/uuid"
+	"github.com/google/uuid"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 // Config defines a set of configuration values
 type Config struct {
-    Message      string `json:"Message"`
-    Queue        string `json:"Queue"`
-    SleepSeconds int64  `json:"SleepSeconds"`
-    Timeout      int64  `json:"Timeout"`
+	Message      string `json:"Message"`
+	Queue        string `json:"Queue"`
+	SleepSeconds int64  `json:"SleepSeconds"`
+	Timeout      int64  `json:"Timeout"`
 }
 
 // configFile defines the name of the file containing configuration values
@@ -43,158 +43,158 @@ var configFileName = "config.json"
 var globalConfig Config
 
 func populateConfiguration(t *testing.T) error {
-    // Get configuration from config.json
+	// Get configuration from config.json
 
-    // Get entire file as a JSON string
-    content, err := ioutil.ReadFile(configFileName)
-    if err != nil {
-        return err
-    }
+	// Get entire file as a JSON string
+	content, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		return err
+	}
 
-    // Convert []byte to string
-    text := string(content)
+	// Convert []byte to string
+	text := string(content)
 
-    // Marshall JSON string in text into global struct
-    err = json.Unmarshal([]byte(text), &globalConfig)
-    if err != nil {
-        return err
-    }
+	// Marshall JSON string in text into global struct
+	err = json.Unmarshal([]byte(text), &globalConfig)
+	if err != nil {
+		return err
+	}
 
-    if globalConfig.Timeout < 0 {
-        globalConfig.Timeout = 0
-    }
+	if globalConfig.Timeout < 0 {
+		globalConfig.Timeout = 0
+	}
 
-    if globalConfig.Timeout > 12*60*60 {
-        globalConfig.Timeout = 12 * 60 * 60
-    }
+	if globalConfig.Timeout > 12*60*60 {
+		globalConfig.Timeout = 12 * 60 * 60
+	}
 
-    t.Log("Message:           " + globalConfig.Message)
-    t.Log("Queue:             " + globalConfig.Queue)
-    t.Log("SleepSeconds:      " + strconv.Itoa(int(globalConfig.SleepSeconds)))
-    t.Log("Timeout (seconds): " + strconv.Itoa(int(globalConfig.Timeout)))
+	t.Log("Message:           " + globalConfig.Message)
+	t.Log("Queue:             " + globalConfig.Queue)
+	t.Log("SleepSeconds:      " + strconv.Itoa(int(globalConfig.SleepSeconds)))
+	t.Log("Timeout (seconds): " + strconv.Itoa(int(globalConfig.Timeout)))
 
-    return nil
+	return nil
 }
 
 func createQueue(sess *session.Session, queue *string) (string, error) {
-    // Create an SQS service client
-    svc := sqs.New(sess)
+	// Create an SQS service client
+	svc := sqs.New(sess)
 
-    result, err := svc.CreateQueue(&sqs.CreateQueueInput{
-        QueueName: queue,
-        Attributes: map[string]*string{
-            "DelaySeconds":           aws.String("60"),
-            "MessageRetentionPeriod": aws.String("86400"),
-        },
-    })
-    if err != nil {
-        return "", err
-    }
+	result, err := svc.CreateQueue(&sqs.CreateQueueInput{
+		QueueName: queue,
+		Attributes: map[string]*string{
+			"DelaySeconds":           aws.String("60"),
+			"MessageRetentionPeriod": aws.String("86400"),
+		},
+	})
+	if err != nil {
+		return "", err
+	}
 
-    return *result.QueueUrl, nil
+	return *result.QueueUrl, nil
 }
 
 func sendMessage(sess *session.Session, queueURL *string, message *string) (string, error) {
-    // Create an SQS service client
-    svc := sqs.New(sess)
+	// Create an SQS service client
+	svc := sqs.New(sess)
 
-    result, err := svc.SendMessage(&sqs.SendMessageInput{
-        DelaySeconds: aws.Int64(10),
-        MessageBody:  message,
-        QueueUrl:     queueURL,
-    })
-    if err != nil {
-        return "", err
-    }
+	result, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageBody:  message,
+		QueueUrl:     queueURL,
+	})
+	if err != nil {
+		return "", err
+	}
 
-    return *result.MessageId, nil
+	return *result.MessageId, nil
 }
 
 func multiplyDuration(factor int64, d time.Duration) time.Duration {
-    return time.Duration(factor) * d
+	return time.Duration(factor) * d
 }
 
 func deleteQueue(sess *session.Session, queueURL *string) error {
-    // Create an SQS service client
-    svc := sqs.New(sess)
+	// Create an SQS service client
+	svc := sqs.New(sess)
 
-    _, err := svc.DeleteQueue(&sqs.DeleteQueueInput{
-        QueueUrl: queueURL,
-    })
-    if err != nil {
-        return err
-    }
+	_, err := svc.DeleteQueue(&sqs.DeleteQueueInput{
+		QueueUrl: queueURL,
+	})
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func TestReceiveMessages(t *testing.T) {
-    thisTime := time.Now()
-    nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
-    t.Log("Starting unit test at " + nowString)
+	thisTime := time.Now()
+	nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
+	t.Log("Starting unit test at " + nowString)
 
-    err := populateConfiguration(t)
-    if err != nil {
-        t.Fatal(err)
-    }
+	err := populateConfiguration(t)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    // Create a session using credentials from ~/.aws/credentials
-    // and the Region from ~/.aws/config
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+	// Create a session using credentials from ~/.aws/credentials
+	// and the Region from ~/.aws/config
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-    if globalConfig.Message == "" {
-        globalConfig.Message = "Sent " + thisTime.Format("2006-01-02 15:04:05 Monday")
-    }
+	if globalConfig.Message == "" {
+		globalConfig.Message = "Sent " + thisTime.Format("2006-01-02 15:04:05 Monday")
+	}
 
-    queueCreated := false
-    queueURL := ""
+	queueCreated := false
+	queueURL := ""
 
-    if globalConfig.Queue == "" {
-        // Create a unique, random queue name
-        id := uuid.New()
-        globalConfig.Queue = "myqueue-" + id.String()
+	if globalConfig.Queue == "" {
+		// Create a unique, random queue name
+		id := uuid.New()
+		globalConfig.Queue = "myqueue-" + id.String()
 
-        queueURL, err = createQueue(sess, &globalConfig.Queue)
-        if err != nil {
-            t.Fatal(err)
-        }
+		queueURL, err = createQueue(sess, &globalConfig.Queue)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-        queueCreated = true
-        t.Log("Created queue " + globalConfig.Queue)
-    }
+		queueCreated = true
+		t.Log("Created queue " + globalConfig.Queue)
+	}
 
-    msgID, err := sendMessage(sess, &queueURL, &globalConfig.Message)
-    if err != nil {
-        t.Fatal(err)
-    }
+	msgID, err := sendMessage(sess, &queueURL, &globalConfig.Message)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    t.Log("Sent message with ID " + msgID + " to queue " + globalConfig.Queue)
+	t.Log("Sent message with ID " + msgID + " to queue " + globalConfig.Queue)
 
-    ts := multiplyDuration(globalConfig.SleepSeconds, time.Second)
-    time.Sleep(ts)
+	ts := multiplyDuration(globalConfig.SleepSeconds, time.Second)
+	time.Sleep(ts)
 
-    t.Log("Slept " + strconv.Itoa(int(globalConfig.SleepSeconds)) + " second(s)")
+	t.Log("Slept " + strconv.Itoa(int(globalConfig.SleepSeconds)) + " second(s)")
 
-    msg, err := GetMessage(sess, &queueURL, &globalConfig.Timeout)
-    if err != nil {
-        t.Fatal(err)
-    }
+	result, err := GetMessages(sess, &queueURL, &globalConfig.Timeout)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if msgID == *msg.MessageId {
-        t.Log("Found sent message")
-    } else {
-        t.Log("Did NOT find message")
-    }
+	if msgID == *result.Messages[0].MessageId {
+		t.Log("Found sent message")
+	} else {
+		t.Log("Did NOT find message")
+	}
 
-    if queueCreated {
-        err = deleteQueue(sess, &queueURL)
-        if err != nil {
-            t.Log("You'll have to delete queue " + globalConfig.Queue + " yourself")
-            t.Fatal(err)
-        }
+	if queueCreated {
+		err = deleteQueue(sess, &queueURL)
+		if err != nil {
+			t.Log("You'll have to delete queue " + globalConfig.Queue + " yourself")
+			t.Fatal(err)
+		}
 
-        t.Log("Deleted queue " + globalConfig.Queue)
-    }
+		t.Log("Deleted queue " + globalConfig.Queue)
+	}
 }
