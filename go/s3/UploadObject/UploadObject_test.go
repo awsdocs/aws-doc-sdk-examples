d84d40f7bf65369c54dbd4dcd603f3b56f03b5e1
Doug-AWS <dougsch@amazon.com>
2020-04-01 15:02:27 -0700

    WIP

---
+++ go/s3/UploadObject/UploadObject_test.go
@@ -0,0 +1,158 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+package main
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "testing"
+    "time"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
+
+    "github.com/google/uuid"
+)
+
+type Config struct {
+    Bucket   string `json:"Bucket"`
+    Filename string `json:"Filename"`
+}
+
+var configFileName = "config.json"
+
+var globalConfig Config
+
+func populateConfiguration(t *testing.T) error {
+    content, err := ioutil.ReadFile(configFileName)
+    if err != nil {
+        return err
+    }
+
+    text := string(content)
+
+    err = json.Unmarshal([]byte(text), &globalConfig)
+    if err != nil {
+        return err
+    }
+
+    t.Log("Bucket:    " + globalConfig.Bucket)
+    t.Log("Filename: " + globalConfig.Filename)
+
+    return nil
+}
+
+func createBucket(sess *session.Session, bucket *string) error {
+    svc := s3.New(sess)
+
+    _, err := svc.CreateBucket(&s3.CreateBucketInput{
+        Bucket: bucket,
+    })
+    if err != nil {
+        return err
+    }
+
+    err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
+        Bucket: bucket,
+    })
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func deleteBucket(sess *session.Session, bucket *string) error {
+    svc := s3.New(sess)
+
+    iter := s3manager.NewDeleteListIterator(svc, &s3.ListObjectsInput{
+        Bucket: bucket,
+    })
+
+    err := s3manager.NewBatchDeleteWithClient(svc).Delete(aws.BackgroundContext(), iter)
+    if err != nil {
+        return err
+    }
+
+    _, err = svc.DeleteBucket(&s3.DeleteBucketInput{
+        Bucket: bucket,
+    })
+    if err != nil {
+        return err
+    }
+
+    err = svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
+        Bucket: bucket,
+    })
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func TestUploadObject(t *testing.T) {
+    thisTime := time.Now()
+    nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
+    t.Log("Starting unit test at " + nowString)
+
+    err := populateConfiguration(t)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+    createdBucket := false
+
+    if globalConfig.Bucket == "" {
+        // Create a unique, random name (import "github.com/google/uuid"):
+        id := uuid.New()
+        globalConfig.Bucket = "test-bucket-" + id.String()
+
+        // Create bucket
+        err := createBucket(sess, &globalConfig.Bucket)
+        if err != nil {
+            t.Fatal(err)
+        }
+
+        t.Log("Created bucket " + globalConfig.Bucket)
+        createdBucket = true
+    }
+
+    if globalConfig.Filename == "" {
+        globalConfig.Filename = "test.txt"
+    }
+
+    err = PutFile(sess, &globalConfig.Bucket, &globalConfig.Filename)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    t.Log("Uploaded " + globalConfig.Filename + " to bucket " + globalConfig.Bucket)
+
+    if createdBucket {
+        // Delete bucket and contents
+        err := deleteBucket(sess, &globalConfig.Bucket)
+        if err != nil {
+            t.Log("You must delete bucket " + globalConfig.Bucket + " yourself")
+        }
+
+        t.Log("Deleted bucket " + globalConfig.Bucket)
+    }
+}
---
+++ go/s3/UploadStream/UploadStream.go
@@ -0,0 +1,101 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+// snippet-start:[s3.go.upload_stream]
+package main
+
+// snippet-start:[s3.go.upload_stream.imports]
+import (
+    "compress/gzip"
+    "flag"
+    "fmt"
+    "io"
+    "os"
+
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+// snippet-end:[s3.go.upload_stream.imports]
+
+// UploadStream uploads a stream for a file to a bucket
+// Inputs:
+//     sess is the current session, which provides configuration for the SDK's service clients
+//     bucket is the name of the bucket
+//     key is the name of the object in the bucket
+//     filename is the name of the file to stream to the bucket
+// Output:
+//     If success, nil
+//     Otherwise, an error from the call to Open or
+func UploadStream(sess *session.Session, bucket *string, key *string, filename *string) error {
+    // snippet-start:[s3.go.upload_stream.call]
+    file, err := os.Open(*filename)
+    if err != nil {
+        return err
+    }
+
+    reader, writer := io.Pipe()
+
+    go func() {
+        gw := gzip.NewWriter(writer)
+        _, err := io.Copy(gw, file)
+        if err != nil {
+            return
+        }
+        file.Close()
+        gw.Close()
+        writer.Close()
+    }()
+
+    uploader := s3manager.NewUploader(sess)
+
+    _, err = uploader.Upload(&s3manager.UploadInput{
+        Body:   reader,
+        Bucket: bucket,
+        Key:    key,
+    })
+    // snippet-end:[s3.go.upload_stream.call]
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    // snippet-start:[s3.go.upload_stream.args]
+    bucket := flag.String("b", "", "The bucket to which the stream is uploaded")
+    filename := flag.String("f", "", "The file to upload to the bucket")
+    key := flag.String("k", "", "The name of the object in the bucket")
+    flag.Parse()
+
+    if *bucket == "" || *filename == "" || *key == "" {
+        fmt.Println("You must supply a bucket name (-b BUCKET), filename (-f FILENAME), and key value (-k KEY)")
+        return
+    }
+
+    // snippet-end:[s3.go.upload_stream.args]
+
+    // snippet-start:[s3.go.upload_stream.session]
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+    // snippet-end:[s3.go.upload_stream.session]
+
+    err := UploadStream(sess, bucket, key, filename)
+    if err != nil {
+        fmt.Println("Failed to upload " + *filename + " to bucket " + *bucket)
+    }
+
+    fmt.Println("Successfully uploaded " + *filename + " to " + *bucket)
+}
+// snippet-end:[s3.go.upload_stream]
