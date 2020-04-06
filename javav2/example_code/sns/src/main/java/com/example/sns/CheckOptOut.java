8308f0a4deaa0f141ab09d3280b2a1f9b7577a5a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-06 15:02:13 -0400

    Update CheckOptOut.java
    Refactored the code

--- javav2/example_code/sns/src/main/java/com/example/sns/CheckOptOut.java
+++ javav2/example_code/sns/src/main/java/com/example/sns/CheckOptOut.java
@@ -5,7 +5,7 @@
 //snippet-keyword:[Amazon Simple Notification Service]
 //snippet-service:[sns]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[2019-07-20]
+//snippet-sourcedate:[4/6/2020]
 //snippet-sourceauthor:[scmacdon AWS]
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -21,11 +21,9 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-//snippet-start:[sns.java2.CheckOptOut.complete]
 package com.example.sns;
 
 //snippet-start:[sns.java2.CheckOptOut.import]
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.CheckIfPhoneNumberIsOptedOutRequest;
@@ -47,10 +45,18 @@ public class CheckOptOut {
         }
         //snippet-start:[sns.java2.CheckOptOut.main]
         String phoneNumber = args[0];
-        try {
 
-            SnsClient snsClient = SnsClient.builder().region(Region.US_EAST_1).build();
+        SnsClient snsClient = SnsClient.builder()
+                    .region(Region.US_EAST_1)
+                    .build();
 
+        checkPhone(snsClient, phoneNumber);
+        }
+
+        //snippet-start:[sns.java2.CheckOptOut.main]
+        public static void checkPhone(SnsClient snsClient, String phoneNumber) {
+
+            try {
             CheckIfPhoneNumberIsOptedOutRequest request = CheckIfPhoneNumberIsOptedOutRequest.builder()
                 .phoneNumber(phoneNumber)
                 .build();
@@ -64,8 +70,6 @@ public class CheckOptOut {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-
         //snippet-end:[sns.java2.CheckOptOut.main]
     }
 }
-//snippet-end:[sns.java2.CheckOptOut.complete]
