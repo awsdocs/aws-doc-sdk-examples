c6d7c2963bbc308389c8a5507dcaaf23b38edeb5
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-07 14:04:52 -0400

    Create StockTrade.java

---
+++ javav2/example_code/kinesis/src/main/java/com/example/kinesis/StockTrade.java
@@ -0,0 +1,112 @@
+//snippet-sourcedescription:[StockTrade.java is a helper class]
+//snippet-keyword:[Java]
+//snippet-sourcesyntax:[java]
+//snippet-keyword:[SDK for Java 2.0]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[Amazon Kinesis]
+//snippet-service:[kinesis]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[3-26-2020]
+//snippet-sourceauthor:scmacdon - AWS]
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.example.kinesis;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Captures the key elements of a stock trade, such as the ticker symbol, price,
+ * number of shares, the type of the trade (buy or sell), and an id uniquely identifying
+ * the trade.
+ */
+public class StockTrade {
+
+    private final static ObjectMapper JSON = new ObjectMapper();
+    static {
+        JSON.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    /**
+     * Represents the type of the stock trade eg buy or sell.
+     */
+    public enum TradeType {
+        BUY,
+        SELL
+    }
+
+    private String tickerSymbol;
+    private TradeType tradeType;
+    private double price;
+    private long quantity;
+    private long id;
+
+    public StockTrade() {
+    }
+
+    public StockTrade(String tickerSymbol, TradeType tradeType, double price, long quantity, long id) {
+        this.tickerSymbol = tickerSymbol;
+        this.tradeType = tradeType;
+        this.price = price;
+        this.quantity = quantity;
+        this.id = id;
+    }
+
+    public String getTickerSymbol() {
+        return tickerSymbol;
+    }
+
+    public TradeType getTradeType() {
+        return tradeType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public byte[] toJsonAsBytes() {
+        try {
+            return JSON.writeValueAsBytes(this);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public static StockTrade fromJsonAsBytes(byte[] bytes) {
+        try {
+            return JSON.readValue(bytes, StockTrade.class);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID %d: %s %d shares of %s for $%.02f",
+                id, tradeType, quantity, tickerSymbol, price);
+    }
+
+}
