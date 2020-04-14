2cddeaa9ceb0b5be5de19eb576c0f5f1a64b8bee
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-14 11:19:44 -0400

    Update GetBucketPolicy.java

--- javav2/example_code/s3/src/main/java/com/example/s3/GetBucketPolicy.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/GetBucketPolicy.java
@@ -1,12 +1,12 @@
-//snippet-sourcedescription:[GetBucketPolicy.java demonstrates how to get the bucket policy for an existing S3 bucket]
+//snippet-sourcedescription:[GetBucketPolicy.java demonstrates how to get the bucket policy for an existing Amazon S3 bucket]
 //snippet-keyword:[SDK for Java 2.0]
 //snippet-keyword:[Code Sample]
-//snippet-service:[s3]
+//snippet-service:[Amazon S3]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[2020-02-06]
+//snippet-sourcedate:[2/6/2020]
 //snippet-sourceauthor:[scmacdon]
 /*
-Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 This file is licensed under the Apache License, Version 2.0 (the "License").
 You may not use this file except in compliance with the License. A copy of
 the License is located at
@@ -16,7 +16,7 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 package com.example.s3;
-// snippet-start:[s3.java2.get_bucket_policy.complete]
+
 // snippet-start:[s3.java2.get_bucket_policy.import]
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.regions.Region;
@@ -31,7 +31,7 @@ import software.amazon.awssdk.services.s3.model.GetBucketPolicyResponse;
  * This code expects that you have AWS credentials set up, as described here:
  * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
  */
-// snippet-start:[s3.java2.get_bucket_policy.main]
+
 public class GetBucketPolicy {
 
     public static void main(String[] args) {
@@ -44,18 +44,28 @@ public class GetBucketPolicy {
                 "Example:\n" +
                 "    GetBucketPolicy bucket1\n\n";
 
-        if (args.length < 1) {
-            System.out.println(USAGE);
+          if (args.length < 1) {
+             System.out.println(USAGE);
             System.exit(1);
-        }
+         }
 
         String bucketName = args[0];
-        String policyText = null;
-
         System.out.format("Getting policy for bucket: \"%s\"\n\n", bucketName);
 
         Region region = Region.US_WEST_2;
-        S3Client s3 = S3Client.builder().region(region).build();
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        String polText = GetPolicy(s3, bucketName );
+        System.out.println("Policy Text: "+polText);
+    }
+
+    // snippet-start:[s3.java2.get_bucket_policy.main]
+    public static String GetPolicy(S3Client s3, String bucketName) {
+
+        String policyText = "";
+        System.out.format("Getting policy for bucket: \"%s\"\n\n", bucketName);
 
         GetBucketPolicyRequest policyReq = GetBucketPolicyRequest.builder()
                 .bucket(bucketName)
@@ -64,22 +74,12 @@ public class GetBucketPolicy {
         try {
             GetBucketPolicyResponse policyRes = s3.getBucketPolicy(policyReq);
             policyText = policyRes.policy();
+            return policyText;
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-
-        if (policyText == null) {
-            System.out.println("The specified bucket has no bucket policy.");
-        } else {
-            System.out.println("Returned policy:");
-            System.out.println("----");
-            System.out.println(policyText);
-            System.out.println("----\n");
-        }
-
-        System.out.println("Done!");
+        return "";
     }
-}
 // snippet-end:[s3.java2.get_bucket_policy.main]
-// snippet-end:[s3.java2.get_bucket_policy.complete]
+}
