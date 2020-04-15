9446c497d4e00c030abc62480bc48fb1bd017b09
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-15 11:06:28 -0400

    Update S3AsyncOps.java

--- javav2/example_code/s3/src/main/java/com/example/s3/S3AsyncOps.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/S3AsyncOps.java
@@ -1,11 +1,10 @@
-//snippet-sourcedescription:[S3AsyncOps.java demonstrates how to use the S3 asynchronous client to put an object into an Amazon S3 bucket]
+//snippet-sourcedescription:[S3AsyncOps.java demonstrates how to use the S3 asynchronous client.]
 //snippet-keyword:[SDK for Java 2.0]
 //snippet-keyword:[Code Sample]
-//snippet-service:[Amazon S3]
+//snippet-service:[s3]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2/6/2020]
 //snippet-sourceauthor:[scmacdon-aws]
-
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -21,54 +20,31 @@
  * limitations under the License.
  */
 package com.example.s3;
-// snippet-start:[s3.java2.async_ops.complete]
-
 // snippet-start:[s3.java2.async_ops.import]
+
 import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.utils.FunctionalUtils;
+
 import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 // snippet-end:[s3.java2.async_ops.import]
-
+// snippet-start:[s3.java2.async_ops.main]
 public class S3AsyncOps {
 
-       public static void main(String[] args) {
+    private static final String BUCKET = "sample-bucket";
+    private static final String KEY = "testfile.in";
 
-           final String USAGE = "\n" +
-                   "Usage:\n" +
-                   "    S3AsyncOps <bucketname> <objectname> <path>\n\n" +
-                   "Where:\n" +
-                   "    bucketname - the name of the bucket (i.e., bucket1)\n\n" +
-                   "    objectname - the name pf the object object (i.e., book.pdf)\n" +
-                   "    path - the local path to the file (i.e., C:\\AWS\\book.pdf)\n" +
-                   "Example:\n" +
-                   "    bucket1 book.pdf  C:\\AWS\\book.pdf\n";
-
-           if (args.length < 3) {
-               System.out.println(USAGE);
-               System.exit(1);
-           }
-
-           String bucketName = args[0];
-           String objectKey = args[1];
-           String path = args[2];
-
-           // Create a S3AsyncClient object
-           S3AsyncClient client = S3AsyncClient.create();
-           S3AsyncOperations( client, bucketName,  objectKey , path );
-       }
-
-    // snippet-start:[s3.java2.async_ops.main]
-       public static void  S3AsyncOperations( S3AsyncClient client,  String bucketName,  String objectKey ,String path  ) {
-
+    public static void main(String[] args) {
+        S3AsyncClient client = S3AsyncClient.create();
         CompletableFuture<PutObjectResponse> future = client.putObject(
                 PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(objectKey)
+                        .bucket(BUCKET)
+                        .key(KEY)
                         .build(),
-                AsyncRequestBody.fromFile(Paths.get(path))
+                AsyncRequestBody.fromFile(Paths.get("myfile.in"))
         );
         future.whenComplete((resp, err) -> {
             try {
@@ -79,7 +55,7 @@ public class S3AsyncOps {
                     err.printStackTrace();
                 }
             } finally {
-                // Lets the app shut down. Only close the client when you are completely done with it.
+                // Lets the application shut down. Only close the client when you are completely done with it.
                 client.close();
             }
         });
@@ -87,4 +63,5 @@ public class S3AsyncOps {
         future.join();
     }
 }
+
 // snippet-end:[s3.java2.async_ops.main]
