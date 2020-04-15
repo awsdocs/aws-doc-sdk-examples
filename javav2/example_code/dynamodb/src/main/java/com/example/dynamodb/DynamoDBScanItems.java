45504a6e242b0f9742ea5b2f29fcf2f64b9cd71a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-15 13:27:04 -0400

    Update DynamoDBScanItems.java

--- javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/DynamoDBScanItems.java
+++ javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/DynamoDBScanItems.java
@@ -1,6 +1,6 @@
 // snippet-sourcedescription:[DynamoDBScanItems demonstrates how to return one or more items and item attributes by accessing every item in a table.]
-// snippet-service:[dynamodb]
-// snippet-keyword:[Code Sample]]
+// snippet-service:[Amazon DynamoDB]
+// snippet-keyword:[Code Sample]
 // snippet-sourcesyntax:[java]
 // snippet-keyword:[Amazon DynamoDB]
 // snippet-sourcetype:[full-example]
@@ -8,7 +8,7 @@
 // snippet-sourceauthor:[AWS-scmacdon]
 
 /**
- * Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * This file is licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License. A copy of
@@ -20,8 +20,6 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-
-// snippet-start:[dynamodb.java2.dynamoDB_scan.complete]
 
 package com.example.dynamodb;
 
@@ -49,28 +47,30 @@ public class DynamoDBScanItems {
 
     public static void main(String[] args) {
 
-       final String USAGE = "\n" +
-                    "Usage:\n" +
-                    "    DynamoDBScanItems <table>\n\n" +
-                    "Where:\n" +
-                    "    table - the table to get information from (i.e., Music3)\n\n" +
-                    "Example:\n" +
-                    "    DescribeTable Music3\n";
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    DynamoDBScanItems <table>\n\n" +
+                "Where:\n" +
+                "    table - the table to get information from (i.e., Music3)\n\n" +
+                "Example:\n" +
+                "    DescribeTable Music3\n";
 
-            if (args.length < 1) {
-                System.out.println(USAGE);
-                System.exit(1);
-            }
-
-        // snippet-start:[dynamodb.java2.dynamoDB_scan.main]
+        if (args.length < 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
         String tableName = args[0];
 
         // Create the DynamoDbClient object
-        Region region = Region.US_WEST_2;
+        Region region = Region.US_EAST_1;
         DynamoDbClient ddb = DynamoDbClient.builder().region(region).build();
+        scanItems(ddb,tableName);
+    }
+
+    // snippet-start:[dynamodb.java2.dynamoDB_scan.main]
+    public static void scanItems( DynamoDbClient ddb,String tableName ) {
 
         try {
-
             ScanRequest scanRequest = ScanRequest.builder()
                     .tableName(tableName)
                     .build();
@@ -88,8 +88,6 @@ public class DynamoDBScanItems {
         } catch (DynamoDbException e) {
             e.printStackTrace();
         }
-
         // snippet-end:[dynamodb.java2.dynamoDB_scan.main]
     }
 }
-// snippet-end:[dynamodb.java2.dynamoDB_scan.complete]
