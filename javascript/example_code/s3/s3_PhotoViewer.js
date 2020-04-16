b4852b362e2799fe4ced0bc99f48bdf4353c899b
Brian Murray <brmur@amazon.com>
2020-04-16 11:46:37 +0100

    updates to photo examples
    In PhotoExample, I changed the script ref source for the SDK to something generic (previous version was out of date). Will add instructions for where to find latest version in the documentation.
    In PhotoViewer, I fixed a bug preventing photos from displaying

--- javascript/example_code/s3/s3_PhotoViewer.js
+++ javascript/example_code/s3/s3_PhotoViewer.js
@@ -104,7 +104,7 @@ function listAlbums() {
 // snippet-start:[s3.JavaScript.s3_PhotoViewer.viewAlbum]
 // Show the photos that exist in an album.
 function viewAlbum(albumName) {
-  var albumPhotosKey = encodeURIComponent(albumName) + '/_';
+  var albumPhotosKey = encodeURIComponent(albumName) + '/';
   s3.listObjects({Prefix: albumPhotosKey}, function(err, data) {
     if (err) {
       return alert('There was an error viewing your album: ' + err.message);
@@ -156,6 +156,7 @@ function viewAlbum(albumName) {
       '</div>',
     ]
     document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
+    document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
   });
 }
 // snippet-end:[s3.JavaScript.s3_PhotoViewer.viewAlbum]
