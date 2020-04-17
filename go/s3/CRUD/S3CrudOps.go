5ccd85390e1f057d49da6178864b5ba533fc15b0
Doug-AWS <dougsch@amazon.com>
2020-04-17 09:12:10 -0700

    Updated S3 CRUD example in Go based on editor's feedback

--- go/s3/CRUD/S3CrudOps.go
+++ go/s3/CRUD/S3CrudOps.go
@@ -16,12 +16,12 @@ import (
 )
 // snippet-end: [s3.go.crud_ops.imports]
 
-// Functions to perform CRUD (create, read, update, delete) operations in S3
+// Functions to perform CRUD (create, read, update, delete) operations in Amazon S3
 
-// MakeBucket creates a bucket
+// MakeBucket creates an S3 bucket
 func MakeBucket(svc s3iface.S3API, bucket *string) error {
     // snippet-start: [s3.go.crud_ops.call]
-    // Create the S3 Bucket
+    // Create the S3 bucket
     _, err := svc.CreateBucket(&s3.CreateBucketInput{
         Bucket: bucket,
     })
@@ -34,7 +34,6 @@ func MakeBucket(svc s3iface.S3API, bucket *string) error {
 }
 
 func waitForCreate(svc s3iface.S3API, bucket *string) error {
-    // Wait until bucket is created before finishing
     // snippet-start: [s3.go.crud_ops.wait_create]
     err := svc.WaitUntilBucketExists(&s3.HeadBucketInput{
         Bucket: bucket,
@@ -47,9 +46,7 @@ func waitForCreate(svc s3iface.S3API, bucket *string) error {
     return nil
 }
 
-// GetBucket returns nil if we own this bucket
 func GetBucket(svc s3iface.S3API, bucket *string) error {
-    // Do we have this Bucket?
     // snippet-start: [s3.go.crud_ops.head_bucket]
     _, err := svc.HeadBucket(&s3.HeadBucketInput{
         Bucket: bucket,
@@ -62,7 +59,7 @@ func GetBucket(svc s3iface.S3API, bucket *string) error {
     return nil
 }
 
-// HasACL returns nil if the bucket has a read-only ACL
+// HasACL returns nil if the S3 bucket has a read-only ACL
 func HasACL(svc s3iface.S3API, bucket *string) error {
     // snippet-start: [s3.go.crud_ops.get_acl]
     acl, err := svc.GetBucketAcl(&s3.GetBucketAclInput{
@@ -73,7 +70,7 @@ func HasACL(svc s3iface.S3API, bucket *string) error {
         return err
     }
 
-    // Determine whether the group allusers has read permission
+    // Determine whether the group AllUsers has read permission
     // snippet-start: [s3.go.crud_ops.check_grants]
     for _, g := range acl.Grants {
         if *g.Grantee.Type == "Group" && *g.Grantee.URI == "http://acs.amazonaws.com/groups/global/AllUsers" && *g.Permission == "READ" {
@@ -85,9 +82,8 @@ func HasACL(svc s3iface.S3API, bucket *string) error {
     return errors.New("All users do not have read access")
 }
 
-// UpdateBucket changes the bucket to give all users read permission
+// UpdateBucket changes the S3 bucket to give all users read permission
 func UpdateBucket(svc s3iface.S3API, bucket *string) error {
-    // Give all users read permission
     // snippet-start: [s3.go.crud_ops.put_acl]
     _, err := svc.PutBucketAcl(&s3.PutBucketAclInput{
         ACL:    aws.String("public-read"),
@@ -107,9 +103,8 @@ func UpdateBucket(svc s3iface.S3API, bucket *string) error {
     return nil
 }
 
-// RemoveBucket deletes a bucket
+// RemoveBucket deletes an S3 bucket
 func RemoveBucket(svc s3iface.S3API, bucket *string) error {
-    // Delete the S3 Bucket
     // snippet-start: [s3.go.crud_ops.delete_bucket]
     _, err := svc.DeleteBucket(&s3.DeleteBucketInput{
         Bucket: bucket,
@@ -122,13 +117,12 @@ func RemoveBucket(svc s3iface.S3API, bucket *string) error {
     return nil
 }
 
-func waitForBucketGone(svc s3iface.S3API, bucket *string) error {
-    // Wait until bucket is gone before finishing
-    // snippet-start: [s3.go.crud_ops.wait_deleted]
+func waitForDelete(svc s3iface.S3API, bucket *string) error {
+    // snippet-start: [s3.go.crud_ops.wait_delete]
     err := svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
         Bucket: bucket,
     })
-    // snippet-end: [s3.go.crud_ops.wait_deleted]
+    // snippet-end: [s3.go.crud_ops.wait_delete]
     if err != nil {
         return err
     }
@@ -138,11 +132,11 @@ func waitForBucketGone(svc s3iface.S3API, bucket *string) error {
 
 func main() {
     // snippet-start: [s3.go.crud_ops.args]
-    bucket := flag.String("b", "", "The name of the bucket")
+    bucket := flag.String("b", "", "The name of the S3 bucket")
     flag.Parse()
 
     if *bucket == "" {
-        fmt.Println("You must supply a bucket name (-b BUCKET)")
+        fmt.Println("You must supply an S3 bucket name (-b BUCKET)")
         return
     }
     // snippet-end: [s3.go.crud_ops.args]
@@ -157,46 +151,46 @@ func main() {
 
     err := MakeBucket(svc, bucket)
     if err != nil {
-        fmt.Println("Got error creating bucket:")
+        fmt.Println("Got error creating S3 bucket:")
         fmt.Println(err)
         return
     }
 
     err = waitForCreate(svc, bucket)
     if err != nil {
-        fmt.Println("Got error creating bucket:")
+        fmt.Println("Got error creating S3 bucket:")
         fmt.Println(err)
         return
     }
 
     err = GetBucket(svc, bucket)
     if err != nil {
-        fmt.Println("Got error reading bucket:")
+        fmt.Println("Got error reading S3 bucket:")
         fmt.Println(err)
         return
     }
 
     err = UpdateBucket(svc, bucket)
     if err != nil {
-        fmt.Println("Got error updating bucket:")
+        fmt.Println("Got error updating S3 bucket:")
         fmt.Println(err)
         return
     }
 
     err = RemoveBucket(svc, bucket)
     if err != nil {
-        fmt.Println("Got error deleting bucket:")
+        fmt.Println("Got error deleting S3 bucket:")
         fmt.Println(err)
         return
     }
 
-    err = waitForBucketGone(svc, bucket)
+    err = waitForDelete(svc, bucket)
     if err != nil {
-        fmt.Println("Got error deleting bucket:")
+        fmt.Println("Got error deleting S3 bucket:")
         fmt.Println(err)
         return
     }
 
-    fmt.Println("Successfully ran CRUD operations on bucket " + *bucket)
+    fmt.Println("Successfully ran CRUD operations on S3 bucket " + *bucket)
 }
 // snippet-end: [s3.go.crud_ops]
