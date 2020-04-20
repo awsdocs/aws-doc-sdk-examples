24426a6627db1520bd30a72759fa9acc4213335c
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-20 10:35:07 -0400

    Update ListUsers.java

--- javav2/example_code/cognito/src/main/java/com/example/cognito/ListUsers.java
+++ javav2/example_code/cognito/src/main/java/com/example/cognito/ListUsers.java
@@ -5,10 +5,10 @@
 //snippet-keyword:[Amazon Cognito]
 //snippet-service:[cognito]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[2019-06-22]
-//snippet-sourceauthor:[jschwarzwalder AWS]
+//snippet-sourcedate:[4/16/2020]
+//snippet-sourceauthor:[scmacdon - AWS]
 /*
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -21,13 +21,12 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-//snippet-start:[cognito.java2.ListUsers.complete]
 
 package com.example.cognito;
 //snippet-start:[cognito.java2.ListUsers.import]
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersRequest;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersResponse;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.UserType;
@@ -44,23 +43,36 @@ public class ListUsers {
                 "Example:\n" +
                 "    ListUsers us-east-2_P0oL1D\n";
 
-        if (args.length < 1) {
-            System.out.println(USAGE);
-            System.exit(1);
-        }
+         if (args.length < 1) {
+              System.out.println(USAGE);
+              System.exit(1);
+           }
 
-        String user_pool_id = args[0];
-        //snippet-start:[cognito.java2.ListUsers.main]
-        CognitoIdentityProviderClient cognitoclient = CognitoIdentityProviderClient.builder().region(Region.US_EAST_1).build();
+        String userPoolId = args[0];
 
-        ListUsersResponse response = cognitoclient.listUsers(ListUsersRequest.builder()
-                .userPoolId(user_pool_id)
+        CognitoIdentityProviderClient cognitoclient = CognitoIdentityProviderClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        listAllUsers(cognitoclient, userPoolId );
+    }
+
+    //snippet-start:[cognito.java2.ListUsers.main]
+    public static void listAllUsers(CognitoIdentityProviderClient cognitoclient, String userPoolId ) {
+
+        try {
+
+            ListUsersResponse response = cognitoclient.listUsers(ListUsersRequest.builder()
+                .userPoolId(userPoolId)
                 .build());
 
-        for(UserType user : response.users()) {
-            System.out.println("User " + user.username() + " Status " + user.userStatus() + " Created " + user.userCreateDate() );
-        }
+            for(UserType user : response.users()) {
+                System.out.println("User " + user.username() + " Status " + user.userStatus() + " Created " + user.userCreateDate() );
+            }
+
+           } catch (CognitoIdentityProviderException e){
+            e.getStackTrace();
+          }
         //snippet-end:[cognito.java2.ListUsers.main]
     }
 }
-//snippet-end:[cognito.java2.ListUsers.complete]
