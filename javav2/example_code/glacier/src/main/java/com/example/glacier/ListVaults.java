64837ab986f7351835fb13b9b34bb479e20805e7
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-04-23 09:42:17 -0400

    Update ListVaults.java

--- javav2/example_code/glacier/src/main/java/com/example/glacier/ListVaults.java
+++ javav2/example_code/glacier/src/main/java/com/example/glacier/ListVaults.java
@@ -3,80 +3,93 @@
 //snippet-keyword:[Code Sample]
 //snippet-service:[glacier]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[]
-//snippet-sourceauthor:[soo-aws]
+//snippet-sourcedate:[4/17/2020]
+//snippet-sourceauthor:[scmacdon-aws]
 /*
    Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
    This file is licensed under the Apache License, Version 2.0 (the "License").
    You may not use this file except in compliance with the License. A copy of
    the License is located at
-
     http://aws.amazon.com/apache2.0/
-
    This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied. See the License for the
    specific language governing permissions and limitations under the License.
 */
 package com.example.glacier;
-// snippet-start:[glacier.java2.list_vaults.complete]
+
 // snippet-start:[glacier.java2.list_vaults.import]
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.glacier.model.ListVaultsRequest;
 import software.amazon.awssdk.services.glacier.model.ListVaultsResponse;
 import software.amazon.awssdk.services.glacier.GlacierClient;
 import software.amazon.awssdk.services.glacier.model.DescribeVaultOutput;
+import software.amazon.awssdk.services.glacier.model.GlacierException;
 import java.util.List;
- 
 // snippet-end:[glacier.java2.list_vaults.import]
+
 /**
  * List your Amazon Glacier vaults.
  *
  * This code expects that you have AWS credentials set up per:
  * http://docs.aws.amazon.com/java-sdk/latest/developer-guide/setup-credentials.html
  */
-// snippet-start:[glacier.java2.list_vaults.main]
-public class ListVaults
-{
-    public static void main(String[] args)
-    {
-        GlacierClient glacier = GlacierClient.builder().build();
 
-        boolean list_complete = false;
-        String new_marker = null;
-        int total_vaults = 0;
+public class ListVaults {
+
+    public static void main(String[] args) {
+
+        // Create a GlacierClient object
+        GlacierClient glacier = GlacierClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        listAllVault(glacier);
+    }
+
+    // snippet-start:[glacier.java2.list_vaults.main]
+    public static void listAllVault(GlacierClient glacier) {
+
+        boolean listComplete = false;
+        String newMarker = null;
+        int totalVaults = 0;
         System.out.println("Your Amazon Glacier vaults:");
-        while (!list_complete) {
-            ListVaultsResponse response = null;
 
-        	if (new_marker != null) {
-        		ListVaultsRequest request = ListVaultsRequest.builder()
-        				.marker(new_marker)
-        				.build();
+        try {
+
+            while (!listComplete) {
+                ListVaultsResponse response = null;
+
+            if (newMarker != null) {
+                ListVaultsRequest request = ListVaultsRequest.builder()
+                        .marker(newMarker)
+                        .build();
                 response = glacier.listVaults(request);
-        	}
-        	else {
-        		ListVaultsRequest request = ListVaultsRequest.builder()
-        				.build();
+            }
+            else {
+                ListVaultsRequest request = ListVaultsRequest.builder()
+                        .build();
                 response = glacier.listVaults(request);
-        	}
+            }
 
             List<DescribeVaultOutput> vault_list = response.vaultList();
             for (DescribeVaultOutput v: vault_list) {
-                total_vaults += 1;
+                totalVaults += 1;
                 System.out.println("* " + v.vaultName());
             }
             // check for further results.
-            new_marker = response.marker();
-            if (new_marker == null) {
-            	list_complete = true;
+            newMarker = response.marker();
+            if (newMarker == null) {
+                listComplete = true;
+                }
             }
-        }
 
-        if (total_vaults == 0) {
-            System.out.println("  no vaults found.");
+            if (totalVaults == 0) {
+                System.out.println("  no vaults found.");
+            }
+        } catch(GlacierException e) {
+            e.getStackTrace();
+
         }
     }
 }
- 
 // snippet-end:[glacier.java2.list_vaults.main]
-// snippet-end:[glacier.java2.list_vaults.complete]
