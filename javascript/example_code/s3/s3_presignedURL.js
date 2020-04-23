b5de1c969123a17074417317aeee49f1b4e319a3
Brian Murray <brmur@amazon.com>
2020-04-23 10:44:39 +0100

    editorial changes

--- javascript/example_code/s3/s3_presignedURL.js
+++ javascript/example_code/s3/s3_presignedURL.js
@@ -32,11 +32,11 @@ const AWS = require('aws-sdk');
 // Create S3 service object
 const s3 = new AWS.S3();
 
-// Set the region 
+// Set the REGION 
 AWS.config.update({region: 'REGION'});
 
 // **DO THIS**:
-//   Replace BUCKET_NAME with the name of the bucket,  FILE_NAME with the name of the file you want to upload (including relative page), and EXPIRATION with the duration in validaty in seconds (e.g 60 *5)
+//   Replace BUCKET_NAME with the name of the bucket,  FILE_NAME with the name of the file to upload (including relative page), and EXPIRATION with the duration of validity in seconds (e.g., 60 *5)
 const myBucket = 'BUCKET_NAME'
 const myKey = 'FILE_NAME'
 const signedUrlExpireSeconds = EXPIRATION
