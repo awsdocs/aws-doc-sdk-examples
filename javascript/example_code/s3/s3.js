c615226f3c764a96898d59a9d4ed8fffd92c8289
Brian Murray <brmur@amazon.com>
2020-05-01 14:46:54 +0100

    updated date

--- javascript/example_code/s3/s3.js
+++ javascript/example_code/s3/s3.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2010-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * This file is licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License. A copy of
@@ -30,70 +30,70 @@ if (process.argv.length < 4) {
       'Example: node s3.js my-test-bucket us-east-2');
     process.exit(1);
   }
-  
+
   var AWS = require('aws-sdk'); // To set the AWS credentials and region.
   var async = require('async'); // To call AWS operations asynchronously.
-  
+
   AWS.config.update({
     region: region
   });
-  
+
   var s3 = new AWS.S3({apiVersion: '2006-03-01'});
   var bucket_name = process.argv[2];
   var region = process.argv[3];
-  
+
   var create_bucket_params = {
     Bucket: bucket_name,
     CreateBucketConfiguration: {
       LocationConstraint: region
     }
   };
-  
+
   var delete_bucket_params = {Bucket: bucket_name};
-  
+
   // List all of your available buckets in this AWS Region.
   function listMyBuckets(callback) {
     s3.listBuckets(function(err, data) {
       if (err) {
-  
+
       } else {
         console.log("My buckets now are:\n");
-  
+
         for (var i = 0; i < data.Buckets.length; i++) {
           console.log(data.Buckets[i].Name);
         }
       }
-  
+
       callback(err);
     });
   }
-  
+
   // Create a bucket in this AWS Region.
   function createMyBucket(callback) {
     console.log('\nCreating a bucket named ' + bucket_name + '...\n');
-  
+
     s3.createBucket(create_bucket_params, function(err, data) {
       if (err) {
         console.log(err.code + ": " + err.message);
       }
-  
+
       callback(err);
     });
   }
-  
+
   // Delete the bucket you just created.
   function deleteMyBucket(callback) {
     console.log('\nDeleting the bucket named ' + bucket_name + '...\n');
-  
+
     s3.deleteBucket(delete_bucket_params, function(err, data) {
       if (err) {
         console.log(err.code + ": " + err.message);
       }
-  
+
       callback(err);
     });
   }
-  
+
   // Call the AWS operations in the following order.
   async.series([
     listMyBuckets,
