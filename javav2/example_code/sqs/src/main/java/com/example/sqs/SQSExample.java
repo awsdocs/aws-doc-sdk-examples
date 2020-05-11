2b37bba3a7ead8a9e8d5d4004e5e0dfe06ed509d
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-05-11 13:58:45 -0400

    Update SQSExample.java

--- javav2/example_code/sqs/src/main/java/com/example/sqs/SQSExample.java
+++ javav2/example_code/sqs/src/main/java/com/example/sqs/SQSExample.java
@@ -1,11 +1,11 @@
 //snippet-sourcedescription:[SQSExample.java demonstrates how to create, list and delete queues.]
 //snippet-keyword:[SDK for Java 2.0]
 //snippet-keyword:[Code Sample]
-//snippet-service:[sqs]
+//snippet-service:[Amazon Simple Queue Service]
 //snippet-sourcetype:[full-example]
 //snippet-sourcedate:[2/24/2020]
 //snippet-sourceauthor:[scmacdon-aws]
-// snippet-start:[sqs.java2.sqs_example.complete]
+
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -23,13 +23,12 @@
 
 package com.example.sqs;
 
-import software.amazon.awssdk.regions.Region;
 // snippet-start:[sqs.java2.sqs_example.import]
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.ChangeMessageVisibilityRequest;
 import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
-import software.amazon.awssdk.services.sqs.model.DeleteQueueRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
 import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
@@ -53,9 +52,24 @@ public class SQSExample {
                 .region(Region.US_WEST_2)
                 .build();
 
+        //Create a Queue
+        String queueUrl= createQueue(sqsClient, queueName );
+        listQueues(sqsClient);
+        listQueuesFilter(sqsClient, queueUrl);
+        List<Message> messages = receiveMessages(sqsClient, queueUrl);
+        changeMessages(sqsClient, queueUrl, messages);
+        deleteMessages(sqsClient, queueUrl,  messages) ;
+    }
+
+    public static String createQueue(SqsClient sqsClient,String queueName ) {
+
         System.out.println("\nCreate Queue");
         // snippet-start:[sqs.java2.sqs_example.create_queue]
-        CreateQueueRequest createQueueRequest = CreateQueueRequest.builder().queueName(queueName).build();
+
+        CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
+                .queueName(queueName)
+                .build();
+
         sqsClient.createQueue(createQueueRequest);
         // snippet-end:[sqs.java2.sqs_example.create_queue]
 
@@ -64,9 +78,11 @@ public class SQSExample {
         GetQueueUrlResponse getQueueUrlResponse =
                 sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
         String queueUrl = getQueueUrlResponse.queueUrl();
-        System.out.println(queueUrl);
+        return queueUrl;
         // snippet-end:[sqs.java2.sqs_example.get_queue]
+    }
 
+    public static void listQueues(SqsClient sqsClient) {
 
         System.out.println("\nList Queues");
         // snippet-start:[sqs.java2.sqs_example.list_queues]
@@ -77,7 +93,9 @@ public class SQSExample {
             System.out.println(url);
         }
         // snippet-end:[sqs.java2.sqs_example.list_queues]
+    }
 
+    public static void listQueuesFilter(SqsClient sqsClient, String queueUrl ) {
         // List queues with filters
         String namePrefix = "queue";
         ListQueuesRequest filterListRequest = ListQueuesRequest.builder()
@@ -97,6 +115,9 @@ public class SQSExample {
                 .delaySeconds(10)
                 .build());
         // snippet-end:[sqs.java2.sqs_example.send_message]
+    }
+
+    public static void sendBatchMessages(SqsClient sqsClient, String queueUrl) {
 
         System.out.println("\nSend multiple messages");
         // snippet-start:[sqs.java2.sqs_example.send__multiple_messages]
@@ -107,6 +128,9 @@ public class SQSExample {
                 .build();
         sqsClient.sendMessageBatch(sendMessageBatchRequest);
         // snippet-end:[sqs.java2.sqs_example.send__multiple_messages]
+    }
+
+    public static  List<Message> receiveMessages(SqsClient sqsClient, String queueUrl) {
 
 
         System.out.println("\nReceive messages");
@@ -115,9 +139,13 @@ public class SQSExample {
                 .queueUrl(queueUrl)
                 .maxNumberOfMessages(5)
                 .build();
-        List<Message> messages= sqsClient.receiveMessage(receiveMessageRequest).messages();
-        // snippet-end:[sqs.java2.sqs_example.retrieve_messages]
+        List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
 
+        return messages;
+        // snippet-end:[sqs.java2.sqs_example.retrieve_messages]
+    }
+
+    public static void changeMessages(SqsClient sqsClient, String queueUrl, List<Message> messages) {
 
         System.out.println("\nChange Message Visibility");
         for (Message message : messages) {
@@ -129,6 +157,9 @@ public class SQSExample {
             sqsClient.changeMessageVisibility(req);
         }
 
+    }
+
+    public static void deleteMessages(SqsClient sqsClient, String queueUrl,  List<Message> messages) {
         System.out.println("\nDelete Messages");
         // snippet-start:[sqs.java2.sqs_example.delete_message]
         for (Message message : messages) {
@@ -139,13 +170,6 @@ public class SQSExample {
             sqsClient.deleteMessage(deleteMessageRequest);
         }
         // snippet-end:[sqs.java2.sqs_example.delete_message]
-
-        System.out.println("\nDelete Queue");
-        // snippet-start:[sqs.java2.sqs_example.delete_queue]
-        DeleteQueueRequest deleteQueueRequest = DeleteQueueRequest.builder().queueUrl(queueUrl).build();
-        sqsClient.deleteQueue(deleteQueueRequest);
-        // snippet-end:[sqs.java2.sqs_example.delete_queue]
-    }
+   }
 }
 // snippet-end:[sqs.java2.sqs_example.main]
-// snippet-end:[sqs.java2.sqs_example.complete]
