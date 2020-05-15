2680cbf793f74b49dadaed796d749d607e44404f
Doug-AWS <dougsch@amazon.com>
2020-05-15 13:24:34 -0700

    Updated CodeBuild examples in Go

---
+++ go/codebuild/BuildProject/BuildProject.go
@@ -0,0 +1,64 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+// snippet-start:[codebuild.go.build_project]
+package main
+
+// snippet-start:[codebuild.go.build_project.imports]
+import (
+    "flag"
+    "fmt"
+
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/codebuild"
+    "github.com/aws/aws-sdk-go/service/codebuild/codebuildiface"
+)
+// snippet-end:[codebuild.go.build_project.imports]
+
+// BldProject builds a CodeBuild project
+// Inputs:
+//     svc is a CodeBuild service client
+//     proj is the name of the project
+// Output:
+//     If success, nil
+//     Otherwise, an error from the call to
+func BldProject(svc codebuildiface.CodeBuildAPI, project *string) error {
+    // snippet-start:[codebuild.go.build_project.call]
+    _, err := svc.StartBuild(&codebuild.StartBuildInput{
+        ProjectName: project,
+    })
+    // snippet-end:[codebuild.go.build_project.call]
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+func main() {
+    // snippet-start:[codebuild.go.build_project.args]
+    project := flag.String("p", "", "The name of the project")
+    flag.Parse()
+
+    if *project == "" {
+        fmt.Println("You must supply the name of the project to build")
+        return
+    }
+    // snippet-end:[codebuild.go.build_project.args]
+
+    // snippet-start:[codebuild.go.build_project.session]
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+    svc := codebuild.New(sess)
+    // snippet-end:[codebuild.go.build_project.args.session]
+
+    err := BldProject(svc, project)
+    if err != nil {
+        fmt.Println("Got an error building the project")
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("Started build for project ", *project)
+}
+// snippet-end:[codebuild.go.build_project]
---
+++ go/codebuild/ListBuilds/ListBuilds.go
@@ -0,0 +1,69 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+// snippet-start:[codebuild.go.list_builds]
+package main
+
+// snippet-start:[codebuild.go.list_builds.imports]
+import (
+    "fmt"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/codebuild"
+)
+// snippet-end:[codebuild.go.list_builds.imports]
+
+// GetBuilds retrieves a list of CodeBuild builds
+// Inputs:
+//     sess is the current session, which provides configuration for the SDK's service clients
+// Output:
+//     If success, the list of the builds and nil
+//     Otherwise, nil and an error from the call to ListBuilds or BatchGetBuilds
+func GetBuilds(sess *session.Session) (*codebuild.BatchGetBuildsOutput, error) {
+    // snippet-start:[codebuild.go.list_builds.call]
+    svc := codebuild.New(sess)
+    // Get the list of builds
+    names, err := svc.ListBuilds(&codebuild.ListBuildsInput{
+        SortOrder: aws.String("ASCENDING"),
+    })
+    // snippet-end:[codebuild.go.list_builds.call]
+    if err != nil {
+        return nil, err
+    }
+
+    // snippet-start:[codebuild.go.list_builds.batch]
+    builds, err := svc.BatchGetBuilds(&codebuild.BatchGetBuildsInput{
+        Ids: names.Ids,
+    })
+    // snippet-end:[codebuild.go.list_builds.batch]
+    if err != nil {
+        return nil, err
+    }
+
+    return builds, nil
+}
+
+func main() {
+    // snippet-start:[codebuild.go.list_builds.session]
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+    // snippet-end:[codebuild.go.list_builds.session]
+
+    builds, err := GetBuilds(sess)
+    if err != nil {
+        fmt.Println("Got error getting builds:")
+        fmt.Println(err)
+        return
+    }
+
+    // snippet-start:[codebuild.go.list_builds.display]
+    for _, build := range builds.Builds {
+        fmt.Printf("Project: %s\n", aws.StringValue(build.ProjectName))
+        fmt.Printf("Phase:   %s\n", aws.StringValue(build.CurrentPhase))
+        fmt.Printf("Status:  %s\n", aws.StringValue(build.BuildStatus))
+        fmt.Println("")
+    }
+    // snippet-end:[codebuild.go.list_builds.display]
+}
+// snippet-end:[codebuild.go.list_builds]
