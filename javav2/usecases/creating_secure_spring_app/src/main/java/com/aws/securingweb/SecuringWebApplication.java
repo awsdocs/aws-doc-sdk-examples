354ad63f299d0e298d8a36e585d8dbc46e7c9028
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-05-21 14:27:44 -0400

    Create SecuringWebApplication.java

---
+++ javav2/usecases/creating_secure_spring_app/src/main/java/com/aws/securingweb/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.aws.securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
