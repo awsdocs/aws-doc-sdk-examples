7eaad44ae1d2516a4bdb3893017f71903a82b248
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-06-02 15:51:37 -0400

    Create PersistCase.java

---
+++ javav2/usecases/creating_serverless workflows/src/main/java/example/PersistCase.java
@@ -0,0 +1,124 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+    http://aws.amazon.com/apache2.0/
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+
+ */
+
+package example;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/*
+    Prior to running this code example, create a table named Case with a PK named id
+ */
+public class PersistCase {
+
+
+    // Puts an item into a DynamoDB table
+    public void putRecord(String caseId, String employeeName, String email) {
+
+        // Create a DynamoDbClient object
+        Region region = Region.US_WEST_2;
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .region(region)
+                .build();
+
+        // Create a DynamoDbEnhancedClient and use the DynamoDbClient object
+        DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(ddb)
+                .build();
+
+
+        try {
+            // Create a DynamoDbTable object
+            DynamoDbTable<Case> custTable = enhancedClient.table("Case", TableSchema.fromBean(Case.class));
+
+            // Create an Instat
+            LocalDate localDate = LocalDate.parse("2020-04-07");
+            LocalDateTime localDateTime = localDate.atStartOfDay();
+            Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+
+            // Populate the Table
+            Case custRecord = new Case();
+            custRecord.setName(employeeName);
+            custRecord.setId(caseId);
+            custRecord.setEmail(email);
+            custRecord.setRegistrationDate(instant) ;
+
+            // Put the customer data into a DynamoDB table
+            custTable.putItem(custRecord);
+
+        } catch (DynamoDbException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("done");
+    }
+
+
+    //Create the Case table to track open cases created in the workflow
+    @DynamoDbBean
+    public static class Case {
+
+        private String id;
+        private String name;
+        private String email;
+        private Instant regDate;
+
+        @DynamoDbPartitionKey
+        public String getId() {
+            return this.id;
+        };
+
+        public void setId(String id) {
+
+            this.id = id;
+        }
+
+        @DynamoDbSortKey
+        public String getName() {
+            return this.name;
+
+        }
+
+        public void setName(String name) {
+
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return this.email;
+        }
+
+        public void setEmail(String email) {
+
+            this.email = email;
+        }
+
+        public Instant getRegistrationDate() {
+            return regDate;
+        }
+        public void setRegistrationDate(Instant registrationDate) {
+
+            this.regDate = registrationDate;
+        }
+    }
+  }
