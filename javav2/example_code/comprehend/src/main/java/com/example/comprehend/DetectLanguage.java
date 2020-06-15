424ce4d9b607ed1847cb59d204a82c4ec6190459
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-06-15 09:45:58 -0400

    Create DetectLanguage.java

---
+++ javav2/example_code/comprehend/src/main/java/com/example/comprehend/DetectLanguage.java
@@ -0,0 +1,81 @@
+// snippet-sourcedescription:[DetectLanguage demonstrates how to detect the language of the text.]
+// snippet-service:[Amazon Comprehend]
+// snippet-keyword:[Java]
+// snippet-sourcesyntax:[java]
+// snippet-keyword:[Amazon Comprehend]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[6/3/2020]
+// snippet-sourceauthor:[scmacdon AWS]
+
+
+
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.example.comprehend;
+
+//snippet-start:[comprehend.java2.detect_language.import]
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.comprehend.ComprehendClient;
+import software.amazon.awssdk.services.comprehend.model.ComprehendException;
+import software.amazon.awssdk.services.comprehend.model.DetectDominantLanguageRequest;
+import software.amazon.awssdk.services.comprehend.model.DetectDominantLanguageResponse;
+import software.amazon.awssdk.services.comprehend.model.DominantLanguage;
+import java.util.Iterator;
+import java.util.List;
+//snippet-end:[comprehend.java2.detect_language.import]
+
+
+public class DetectLanguage {
+
+    public static void main(String[] args) {
+
+        //Specify French text - "It is raining today in Seattle"
+        String text = "Il pleut aujourd'hui à Seattle";
+        Region region = Region.US_EAST_1;
+
+        ComprehendClient comClient = ComprehendClient.builder()
+                .region(region)
+                .build();
+
+        System.out.println("Calling DetectDominantLanguage");
+        detectTheDominantLanguage(comClient, text);
+    }
+
+    //snippet-start:[comprehend.java2.detect_language.main]
+    public static void detectTheDominantLanguage(ComprehendClient comClient, String text){
+
+       try {
+
+            DetectDominantLanguageRequest request = DetectDominantLanguageRequest.builder()
+                    .text(text)
+                    .build();
+
+            DetectDominantLanguageResponse resp = comClient.detectDominantLanguage(request);
+            List<DominantLanguage> allLanList = resp.languages();
+            Iterator<DominantLanguage> lanIterator = allLanList.iterator();
+
+            while (lanIterator.hasNext()) {
+                DominantLanguage lang = lanIterator.next();
+                System.out.println("Language is " + lang.languageCode());
+            }
+
+           } catch (ComprehendException e) {
+               System.err.println(e.awsErrorDetails().errorMessage());
+               System.exit(1);
+           }
+        //snippet-end:[comprehend.java2.detect_language.main]
+        }
+    }
