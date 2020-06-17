a1f21fe6f718fb41426d5de35b50807f9ded427f
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-06-17 09:38:01 -0400

    Update DetectPHI.java

--- javav2/example_code/comprehend/src/main/java/com/example/comprehend/medical/DetectPHI.java
+++ javav2/example_code/comprehend/src/main/java/com/example/comprehend/medical/DetectPHI.java
@@ -1,14 +1,41 @@
+// snippet-sourcedescription:[DetectPHI demonstrates how to retrieve protected health information (PHI).]
+// snippet-service:[Amazon Comprehend Medical]
+// snippet-keyword:[Java]
+// snippet-sourcesyntax:[java]
+// snippet-keyword:[Amazon Comprehend Medical]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[6/15/2020]
+// snippet-sourceauthor:[scmacdon AWS]
+
+
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+
 package com.example.comprehend.medical;
 
+//snippet-start:[comprehendmed.java2.detect_phi.import]
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.comprehendmedical.ComprehendMedicalClient;
 import software.amazon.awssdk.services.comprehendmedical.model.ComprehendMedicalException;
 import software.amazon.awssdk.services.comprehendmedical.model.DetectPhiRequest;
 import software.amazon.awssdk.services.comprehendmedical.model.DetectPhiResponse;
 import software.amazon.awssdk.services.comprehendmedical.model.Entity;
-
 import java.util.Iterator;
 import java.util.List;
+//snippet-end:[comprehendmed.java2.detect_phi.import]
 
 public class DetectPHI {
 
@@ -35,6 +62,7 @@ public class DetectPHI {
         detectPHIValues(medClient, text) ;
 
     }
+    //snippet-start:[comprehendmed.java2.detect_phi.main]
     public static void detectPHIValues(ComprehendMedicalClient medClient, String text ) {
 
         try {
@@ -55,5 +83,6 @@ public class DetectPHI {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
+        //snippet-end:[comprehendmed.java2.detect_phi.main]
     }
 }
