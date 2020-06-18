3d9e5e29c549e8af0adffb66f1a548f7f44fafd9
Brian Murray <brmur@amazon.com>
2020-06-18 11:34:07 +0100

    EC2 clean up (incorporating edits)

--- javascript/example_code_v3/ec2/ec2_startstopinstances.js
+++ javascript/example_code_v3/ec2/ec2_startstopinstances.js
@@ -1,29 +1,21 @@
-/*
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 
-This file is licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License. A copy of
-the License is located at http://aws.amazon.com/apache2.0/
-
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-
-ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
-https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-managing-instances.html
+ABOUT THIS NODE.JS EXAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-managing-instances.html.
 
 Purpose:
 ec2_startstopinstances.js demonstrates how to start and stop an Amazon EC2 instance that is backed by Amazon Elastic Block Store.
 
 Inputs:
-- REGION (in command line input below)
-- COMMAND (i.e. START or STOP) (in command line input below)
+- REGION (into command line  below)
+- COMMAND (into command line  below): START or STOP
 
 Running the code:
 node ec2_startstopinstances.js REGION COMMAND INSTANCE_ID
 */
 
-// snippet-start:[ec2.JavaScript.Instances.start_stopInstances]
+// snippet-start:[ec2.JavaScript.v3.Instances.start_stopInstances]
 async function run() {
     var params = {InstanceIds: [process.argv[3]]};
     const {EC2, StartInstancesCommand, StopInstancesCommand} = require("@aws-sdk/client-ec2");
@@ -49,5 +41,5 @@ async function run() {
 };
 run();
 
-// snippet-end:[ec2.JavaScript.Instances.start_stopInstances]
+// snippet-end:[ec2.JavaScript.v3.Instances.start_stopInstances]
 exports.run = run;
