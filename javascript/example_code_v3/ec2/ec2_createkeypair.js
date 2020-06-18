b0242f68c4faa5cf9e261144167b2a6fa8a8b420
Brian Murray <brmur@amazon.com>
2020-06-18 11:34:07 +0100

    EC2 clean up (incorporating edits)

--- javascript/example_code_v3/ec2/ec2_createkeypair.js
+++ javascript/example_code_v3/ec2/ec2_createkeypair.js
@@ -1,28 +1,20 @@
-/*
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 
-This file is licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License. A copy of
-the License is located at http://aws.amazon.com/apache2.0/
-
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-
-ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
-https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-key-pairs.html
+ABOUT THIS NODE.JS EXAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-key-pairs.html.
 
 Purpose:
 ec2_createkeypair.js demonstrates how to create an RSA key pair for an Amazon EC2 instance.
 
 Inputs:
 - REGION (into command line below)
-- MY_KEY_PAIR (in command line below)
+- MY_KEY_PAIR (into command line below)
 
 Running the code:
 node ec2_createkeypair.js REGION MY_KEY_PAIR
  */
-// snippet-start:[ec2.JavaScript.keypairs.createKeyPair]
+// snippet-start:[ec2.JavaScript.v3.keypairs.createKeyPair]
 async function run(){
    try {
       const {
@@ -41,5 +33,5 @@ async function run(){
    }
 };
 run();
-// snippet-end:[ec2.JavaScript.keypairs.createKeyPair]
+// snippet-end:[ec2.JavaScript.v3.keypairs.createKeyPair]
 exports.run = run;
--- javascript/example_code_v3/ec2/ec2_describesecuritygroups.js
+++ javascript/example_code_v3/ec2/ec2_describesecuritygroups.js
@@ -1,29 +1,21 @@
-/*
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
 
-This file is licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License. A copy of
-the License is located at http://aws.amazon.com/apache2.0/
-
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-
-ABOUT THIS NODE.JS SAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
-https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-security-groups.html
+ABOUT THIS NODE.JS EXAMPLE: This sample is part of the SDK for JavaScript Developer Guide topic at
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-security-groups.html.
 
 Purpose:
 ec2_describesecuritygroups.js demonstrates how to retrieve information about one or more security groups.
 
 Inputs:
-- REGION (in command line input below)
-- SECURITY_GROUP_ID (in command line input below)
+- REGION (into command line  below)
+- SECURITY_GROUP_ID (into command line  below)
 
 Running the code:
 node ec2_describesecuritygroups.js REGION SECURITY_GROUP_ID
 
  */
-// snippet-start:[ec2.JavaScript.SecurityGroups.describeSecurityGroups]
+// snippet-start:[ec2.JavaScript.v3.SecurityGroups.describeSecurityGroups]
 async function run(){
    try {
       const {EC2, DescribeSecurityGroupsCommand} = require("@aws-sdk/client-ec2");
@@ -40,5 +32,5 @@ async function run(){
 run();
 
 
-// snippet-end:[ec2.JavaScript.SecurityGroups.describeSecurityGroups]
+// snippet-end:[ec2.JavaScript.v3.SecurityGroups.describeSecurityGroups]
 exports.run = run;
