79c778aced67d2904af1fa7fa321ae736614f43d
Brian Murray <brmur@amazon.com>
2020-06-29 17:25:01 +0100

    updated note about date of V3 release

--- javascript/example_code_v3/dynamodb/QueryExample/ddb_batchwriteritem_tv.js
+++ javascript/example_code_v3/dynamodb/QueryExample/ddb_batchwriteritem_tv.js
@@ -1,7 +1,7 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 
-// ABOUT THIS NODE.JS EXAMPLE:This sample is part of the SDK for JavaScript Developer Guide (scheduled for release September 2020) topic at
+// ABOUT THIS NODE.JS EXAMPLE:This sample is part of the SDK for JavaScript Developer Guide (scheduled for release later in 2020) topic at
 // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-query-scan.html.
 
 Purpose:
