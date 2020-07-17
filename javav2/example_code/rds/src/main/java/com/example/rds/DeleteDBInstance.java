7eb4c320db7b61ac41935b11cb313df49fce8084
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-07-17 15:07:15 -0400

    Create DeleteDBInstance.java

---
+++ javav2/example_code/rds/src/main/java/com/example/rds/DeleteDBInstance.java
@@ -0,0 +1,74 @@
+//snippet-sourcedescription:[DeleteDBInstance.java demonstrates how to delete a RDS snapshot.]
+//snippet-keyword:[SDK for Java 2.0]
+//snippet-keyword:[Code Sample]
+//snippet-service:[Amazon Relational Database Service]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[7/6/2020]
+//snippet-sourceauthor:[scmacdon - aws]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+    http://aws.amazon.com/apache2.0/
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+
+package com.example.rds;
+
+// snippet-start:[rds.java2.delete_instance.import]
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.DeleteDbInstanceRequest;
+import software.amazon.awssdk.services.rds.model.DeleteDbInstanceResponse;
+import software.amazon.awssdk.services.rds.model.RdsException;
+// snippet-end:[rds.java2.delete_instance.import]
+
+
+public class DeleteDBInstance {
+
+    public static void main(String[] args) {
+
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    DeleteDBInstance <dbInstanceIdentifier> \n\n" +
+                "Where:\n" +
+                "    dbInstanceIdentifier - the database instance identifier \n";
+
+        if (args.length < 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String dbInstanceIdentifier = args[0];
+
+        Region region = Region.US_WEST_2;
+        RdsClient rdsClient = RdsClient.builder()
+                .region(region)
+                .build();
+
+        deleteDatabaseInstance(rdsClient, dbInstanceIdentifier);
+    }
+
+    // snippet-start:[rds.java2.delete_instance.main]
+    public static void deleteDatabaseInstance( RdsClient rdsClient, String dbInstanceIdentifier) {
+        try {
+            DeleteDbInstanceRequest deleteDbInstanceRequest = DeleteDbInstanceRequest.builder()
+                    .dbInstanceIdentifier(dbInstanceIdentifier)
+                    .deleteAutomatedBackups(true)
+                    .skipFinalSnapshot(true)
+                    .build();
+
+            DeleteDbInstanceResponse response = rdsClient.deleteDBInstance(deleteDbInstanceRequest);
+            System.out.print("The status of the database is " + response.dbInstance().dbInstanceStatus());
+
+        } catch (RdsException e) {
+            System.out.println(e.getLocalizedMessage());
+            System.exit(1);
+        }
+        // snippet-end:[rds.java2.delete_instance.main]
+    }
+}
