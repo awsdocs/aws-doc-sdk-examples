43b52cdd79c9072cc006444b07a2e46d2ff9d491
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-07-28 14:53:30 -0400

    Create AnalyzeDocument.java

---
+++ javav2/example_code/textract/src/main/java/com/example/textract/AnalyzeDocument.java
@@ -0,0 +1,107 @@
+// snippet-sourcedescription:[AnalyzeDocument.java demonstrates how to analyze a document.]
+// snippet-service:[Amazon Textract]
+// snippet-keyword:[Java]
+// snippet-keyword:[Amazon Textract]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[7-8-2020]
+// snippet-sourceauthor:[scmacdon - AWS]
+
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.example.textract;
+
+// snippet-start:[textract.java2._analyze_doc.import]
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.textract.TextractClient;
+import software.amazon.awssdk.services.textract.model.AnalyzeDocumentRequest;
+import software.amazon.awssdk.services.textract.model.Document;
+import software.amazon.awssdk.services.textract.model.FeatureType;
+import software.amazon.awssdk.services.textract.model.AnalyzeDocumentResponse;
+import software.amazon.awssdk.services.textract.model.Block;
+import software.amazon.awssdk.services.textract.model.TextractException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+// snippet-end:[textract.java2._analyze_doc.import]
+
+public class AnalyzeDocument {
+
+    public static void main(String[] args) {
+
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    AnalyzeDocument <sourceDoc> \n\n" +
+                "Where:\n" +
+                "    sourceDoc - the path where the document is located  (must be an image ie - C:/AWS/book.png) \n";
+
+        if (args.length < 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String sourceDoc = args[0];
+
+        Region region = Region.US_EAST_2;
+        TextractClient textractClient = TextractClient.builder()
+                .region(region)
+                .build();
+
+        analyzeDoc(textractClient, sourceDoc);
+    }
+
+    // snippet-start:[textract.java2._analyze_doc.main]
+    public static void analyzeDoc(TextractClient textractClient, String sourceDoc) {
+
+        try {
+            InputStream sourceStream = new FileInputStream(new File(sourceDoc));
+            SdkBytes sourceBytes = SdkBytes.fromInputStream(sourceStream);
+
+            // Get the input Document as bytes
+            Document myDoc = Document.builder()
+                    .bytes(sourceBytes)
+                    .build();
+
+            List<FeatureType> featureTypes = new ArrayList<FeatureType>();
+            featureTypes.add(FeatureType.FORMS);
+            featureTypes.add(FeatureType.TABLES);
+
+            AnalyzeDocumentRequest analyzeDocumentRequest = AnalyzeDocumentRequest.builder()
+                    .featureTypes(featureTypes)
+                    .document(myDoc)
+                    .build();
+
+            AnalyzeDocumentResponse analyzeDocument = textractClient.analyzeDocument(analyzeDocumentRequest);
+            List<Block> docInfo = analyzeDocument.blocks();
+            Iterator<Block> blockIterator = docInfo.iterator();
+
+            while(blockIterator.hasNext()) {
+                Block block = blockIterator.next();
+                System.out.println("The block type is " +block.blockType().toString());
+            }
+
+        } catch (TextractException | FileNotFoundException e) {
+
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        // snippet-end:[textract.java2._analyze_doc.main]
+    }
+}
