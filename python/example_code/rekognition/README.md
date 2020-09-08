f8f8c051a82256fef584702d8e5d47fffa02fda9
Laren-AWS <larencra@amazon.com>
2020-09-08 13:04:29 -0700

    Amazon Rekognition image and video detection example This example shows how to detect people, object, and text in images and videos.

---
+++ python/example_code/rekognition/README.md
@@ -0,0 +1,119 @@
+# Amazon Rekognition image and video detection example
+
+## Purpose
+
+Shows how to use the AWS SDK for Python (Boto3) with Amazon Rekognition to
+recognize people, objects, and text in images and videos.
+
+* Detect faces, celebrities, objects, and text in an image.
+* Create a collection of indexed faces and search for faces in your collection 
+that match a reference image.
+* Detect faces, celebrities, and objects in a video.
+* Create a notification channel so your code can determine when a video
+detection job has completed.
+
+## Prerequisites
+
+- You must have an AWS account, and have your default credentials and AWS Region
+  configured as described in the [AWS Tools and SDKs Shared Configuration and
+  Credentials Reference Guide](https://docs.aws.amazon.com/credref/latest/refdocs/creds-config-files.html).
+- Python 3.6 or later
+- Boto3 1.14.47 or later
+- Requests 2.23.0 or later
+- Pillow 7.2.0 or later 
+- PyTest 5.3.5 or later (to run unit tests)
+
+## Cautions
+
+- As an AWS best practice, grant this code least privilege, or only the 
+  permissions required to perform a task. For more information, see 
+  [Grant Least Privilege](https://docs.aws.amazon.com/IAM/latest/UserGuide/best-practices.html#grant-least-privilege) 
+  in the *AWS Identity and Access Management 
+  User Guide*.
+- This code has not been tested in all AWS Regions. Some AWS services are 
+  available only in specific Regions. For more information, see the 
+  [AWS Region Table](https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services/)
+  on the AWS website.
+- Running this code might result in charges to your AWS account.
+
+## Running the code
+
+There are three demonstrations in this set of examples:
+
+* Detecting items in a single image.
+* Building a collection of indexed faces and searching for matches.
+* Detecting items in a video.
+
+**Image detection**
+
+Run this example at a command prompt with the following command.
+
+```
+python rekognition_image_detection.py
+``` 
+
+**Face collection**
+
+Run this example at a command prompt with the following command.
+
+```
+python rekognition_collections.py
+``` 
+
+**Video detection**
+
+Run this example at a command prompt with the following command.
+
+```
+python rekognition_video_detection.py
+``` 
+
+### Example structure
+
+The example contains the following files.
+
+**rekognition_collections.py**
+
+Shows how to use Amazon Rekognition collection APIs. The `usage_demo` script creates 
+a collection, indexes faces from a series of images that each contain pictures of 
+the same group of people, and searches for matches against reference images.  
+
+**rekognition_image_detection.py**
+
+Shows how to use Amazon Rekognition image detection APIs. The `usage_demo` script 
+detects faces, objects, text, and more by passing images to Amazon Rekognition. 
+
+**rekognition_objects.py**
+
+A set of classes that encapsulate data returned from Amazon Rekognition APIs,
+such as faces, labels, and people. These classes are used to transform data from 
+the service format to an object format.
+
+**rekognition_video_detection.py**
+
+Shows how to use Amazon Rekognition video detection APIs. The `usage_demo` script 
+starts detection jobs that detect things like faces, objects, and people in a video. 
+Because video detection is performed asynchronously, the demo also shows how to create 
+a notification channel that uses Amazon Simple Notification Service and
+Amazon Simple Queue Service to let the code poll for a job completion message.
+
+## Running the tests
+
+The unit tests in this module use the botocore Stubber. This captures requests before 
+they are sent to AWS, and returns a mocked response. To run all of the tests, 
+run the following in your [GitHub root]/python/example_code/rekognition 
+folder.
+
+```    
+python -m pytest
+```
+
+## Additional information
+
+- [Boto3 Amazon Rekognition service reference](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rekognition.html)
+- [Amazon Rekognition documentation](https://docs.aws.amazon.com/rekognition)
+
+---
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
---
+++ python/test_tools/rekognition_stubber.py
@@ -0,0 +1,287 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Stub functions that are used by the Amazon Rekognition unit tests.
+"""
+
+from test_tools.example_stubber import ExampleStubber
+
+
+class RekognitionStubber(ExampleStubber):
+    """
+    A class that implements a variety of stub functions that are used by the
+    Amazon Rekognition unit tests.
+
+    The stubbed functions all expect certain parameters to be passed to them as
+    part of the tests, and will raise errors when the actual parameters differ from
+    the expected.
+    """
+    def __init__(self, client, use_stubs=True):
+        """
+        Initializes the object with a specific client and configures it for
+        stubbing or AWS passthrough.
+
+        :param client: A Boto3 Rekognition client.
+        :param use_stubs: When True, use stubs to intercept requests. Otherwise,
+                          pass requests through to AWS.
+        """
+        super().__init__(client, use_stubs)
+
+    @staticmethod
+    def _face_to_dict(face):
+        face_dict = {}
+        if face.bounding_box is not None:
+            face_dict['BoundingBox'] = face.bounding_box
+        if face.confidence is not None:
+            face_dict['Confidence'] = face.confidence
+        if face.landmarks is not None:
+            face_dict['Landmarks'] = face.landmarks
+        if face.pose is not None:
+            face_dict['Pose'] = face.pose
+        if face.quality is not None:
+            face_dict['Quality'] = face.quality
+        if face.age_range is not None:
+            face_dict.update({
+                'AgeRange': {'Low': face.age_range[0], 'High': face.age_range[1]},
+                'Smile': {'Value': face.smile},
+                'Eyeglasses': {'Value': face.eyeglasses},
+                'Sunglasses': {'Value': face.sunglasses},
+                'Gender': {'Value': face.gender},
+                'Beard': {'Value': face.beard},
+                'Mustache': {'Value': face.mustache},
+                'EyesOpen': {'Value': face.eyes_open},
+                'MouthOpen': {'Value': face.mouth_open},
+                'Emotions': [{'Type': emotion, 'Confidence': 80}
+                             for emotion in face.emotions]})
+        return face_dict
+
+    @staticmethod
+    def _celebrity_to_dict(celebrity):
+        return {
+            'Urls': celebrity.info_urls,
+            'Name': celebrity.name,
+            'Id': celebrity.id,
+            'Face': RekognitionStubber._face_to_dict(celebrity.face)}
+
+    @staticmethod
+    def _person_to_dict(person):
+        return {
+            'Index': person.index,
+            'Face': RekognitionStubber._face_to_dict(person.face)}
+
+    @staticmethod
+    def _label_to_dict(label):
+        return {
+            'Name': label.name,
+            'Confidence': label.confidence,
+            'Instances': label.instances,
+            'Parents': label.parents
+        }
+
+    @staticmethod
+    def _moderation_label_to_dict(label):
+        return {
+            'Name': label.name,
+            'Confidence': label.confidence,
+            'ParentName': label.parent_name
+        }
+
+    @staticmethod
+    def _text_to_dict(text):
+        return {
+            'DetectedText': text.text,
+            'Type': text.kind,
+            'Id': text.id,
+            'ParentId': text.parent_id,
+            'Confidence': text.confidence,
+            'Geometry': text.geometry
+        }
+
+    def stub_detect_faces(self, image, faces, error_code=None):
+        expected_params = {'Image': image, 'Attributes': ['ALL']}
+        response = {'FaceDetails': [self._face_to_dict(face) for face in faces]}
+        self._stub_bifurcator(
+            'detect_faces', expected_params, response, error_code=error_code)
+
+    def stub_compare_faces(
+            self, source_image, target_image, similarity, matches, unmatches,
+            error_code=None):
+        expected_params = {
+            'SourceImage': source_image,
+            'TargetImage': target_image,
+            'SimilarityThreshold': similarity}
+        response = {
+            'FaceMatches': [{
+                'Similarity': similarity,
+                'Face': self._face_to_dict(match)
+            } for match in matches],
+            'UnmatchedFaces': [self._face_to_dict(unmatch) for unmatch in unmatches]}
+        self._stub_bifurcator(
+            'compare_faces', expected_params, response, error_code=error_code)
+
+    def stub_detect_labels(self, image, max_labels, labels, error_code=None):
+        expected_params = {'Image': image, 'MaxLabels': max_labels}
+        response = {'Labels': [self._label_to_dict(label) for label in labels]}
+        self._stub_bifurcator(
+            'detect_labels', expected_params, response, error_code=error_code)
+
+    def stub_detect_moderation_labels(self, image, labels, error_code=None):
+        expected_params = {'Image': image}
+        response = {
+            'ModerationLabels': [
+                self._moderation_label_to_dict(label) for label in labels]}
+        self._stub_bifurcator(
+            'detect_moderation_labels', expected_params, response,
+            error_code=error_code)
+
+    def stub_detect_text(self, image, texts, error_code=None):
+        expected_params = {'Image': image}
+        response = {'TextDetections': [self._text_to_dict(text) for text in texts]}
+        self._stub_bifurcator(
+            'detect_text', expected_params, response, error_code=error_code)
+
+    def stub_recognize_celebrities(self, image, celebrities, normals, error_code=None):
+        expected_params = {'Image': image}
+        response = {
+            'CelebrityFaces': [
+                self._celebrity_to_dict(celeb) for celeb in celebrities],
+            'UnrecognizedFaces': [self._face_to_dict(face) for face in normals]}
+        self._stub_bifurcator(
+            'recognize_celebrities', expected_params, response, error_code=error_code)
+
+    def stub_describe_collection(self, collection_id, collection, error_code=None):
+        expected_params = {'CollectionId': collection_id}
+        response = {
+            'CollectionARN': collection.collection_arn,
+            'FaceCount': collection.face_count,
+            'CreationTimestamp': collection.created
+        }
+        self._stub_bifurcator(
+            'describe_collection', expected_params, response, error_code=error_code)
+
+    def stub_delete_collection(self, collection_id, error_code=None):
+        expected_params = {'CollectionId': collection_id}
+        self._stub_bifurcator(
+            'delete_collection', expected_params, error_code=error_code)
+
+    def stub_index_faces(
+            self, collection_id, image, max_faces, indexed_faces, unindexed_faces,
+            error_code=None):
+        expected_params = {
+            'CollectionId': collection_id, 'Image': image.image,
+            'ExternalImageId': image.image_name, 'MaxFaces': max_faces,
+            'DetectionAttributes': ['ALL']}
+        response = {
+            'FaceRecords': [{
+                'Face': {'FaceId': face.face_id, 'ImageId': face.image_id},
+                'FaceDetail': self._face_to_dict(face)
+            } for face in indexed_faces],
+            'UnindexedFaces': [{
+                'FaceDetail': self._face_to_dict(face)
+            }for face in unindexed_faces]}
+        self._stub_bifurcator(
+            'index_faces', expected_params, response, error_code=error_code)
+
+    def stub_list_faces(self, collection_id, max_results, faces, error_code=None):
+        expected_params = {'CollectionId': collection_id, 'MaxResults': max_results}
+        response = {'Faces': [self._face_to_dict(face) for face in faces]}
+        self._stub_bifurcator(
+            'list_faces', expected_params, response, error_code=error_code)
+
+    def stub_search_faces_by_image(
+            self, collection_id, image, threshold, max_faces, image_face,
+            collection_faces, error_code=None):
+        expected_params = {
+            'CollectionId': collection_id, 'Image': image.image,
+            'FaceMatchThreshold': threshold, 'MaxFaces': max_faces}
+        response = {
+            'SearchedFaceBoundingBox': image_face.bounding_box,
+            'SearchedFaceConfidence': image_face.confidence,
+            'FaceMatches': [
+                {'Face': self._face_to_dict(face)} for face in collection_faces]}
+        self._stub_bifurcator(
+            'search_faces_by_image', expected_params, response, error_code=error_code)
+
+    def stub_search_faces(
+            self, collection_id, face_id, threshold, max_faces, faces, error_code=None):
+        expected_params = {
+            'CollectionId': collection_id, 'FaceId': face_id,
+            'FaceMatchThreshold': threshold, 'MaxFaces': max_faces}
+        response = {
+            'FaceMatches': [{'Face': self._face_to_dict(face)} for face in faces]}
+        self._stub_bifurcator(
+            'search_faces', expected_params, response, error_code=error_code)
+
+    def stub_delete_faces(self, collection_id, face_ids, error_code=None):
+        expected_params = {'CollectionId': collection_id, 'FaceIds': face_ids}
+        response = {'DeletedFaces': face_ids}
+        self._stub_bifurcator(
+            'delete_faces', expected_params, response, error_code=error_code)
+
+    def stub_create_collection(self, collection_id, collection, error_code=None):
+        expected_params = {'CollectionId': collection_id}
+        response = {
+            'CollectionArn': collection.collection_arn
+        }
+        self._stub_bifurcator(
+            'create_collection', expected_params, response, error_code=error_code)
+
+    def stub_list_collections(self, max_results, collection_ids, error_code=None):
+        expected_params = {'MaxResults': max_results}
+        response = {'CollectionIds': collection_ids}
+        self._stub_bifurcator(
+            'list_collections', expected_params, response, error_code=error_code)
+
+    def stub_start_detection(
+            self, func_name, video, notification_channel, job_id, error_code=None):
+        expected_params = {'Video': video, 'NotificationChannel': notification_channel}
+        response = {'JobId': job_id}
+        self._stub_bifurcator(
+            func_name, expected_params, response, error_code=error_code)
+
+    def stub_get_label_detection(
+            self, job_id, job_status, labels, error_code=None):
+        expected_params = {'JobId': job_id}
+        response = {'JobStatus': job_status, 'Labels': [{
+            'Timestamp': label.timestamp,
+            'Label': self._label_to_dict(label)} for label in labels]}
+        self._stub_bifurcator(
+            'get_label_detection', expected_params, response, error_code=error_code)
+
+    def stub_get_face_detection(
+            self, job_id, job_status, faces, error_code=None):
+        expected_params = {'JobId': job_id}
+        response = {'JobStatus': job_status, 'Faces': [{
+            'Timestamp': face.timestamp,
+            'Face': self._face_to_dict(face)} for face in faces]}
+        self._stub_bifurcator(
+            'get_face_detection', expected_params, response, error_code=error_code)
+
+    def stub_get_person_tracking(
+            self, job_id, job_status, persons, error_code=None):
+        expected_params = {'JobId': job_id}
+        response = {'JobStatus': job_status, 'Persons': [{
+            'Timestamp': person.timestamp,
+            'Person': self._person_to_dict(person)} for person in persons]}
+        self._stub_bifurcator(
+            'get_person_tracking', expected_params, response, error_code=error_code)
+
+    def stub_get_celebrity_recognition(
+            self, job_id, job_status, celebrities, error_code=None):
+        expected_params = {'JobId': job_id}
+        response = {'JobStatus': job_status, 'Celebrities': [{
+            'Timestamp': celebrity.timestamp,
+            'Celebrity': self._celebrity_to_dict(celebrity)} for celebrity in celebrities]}
+        self._stub_bifurcator(
+            'get_celebrity_recognition', expected_params, response, error_code=error_code)
+
+    def stub_get_content_moderation(
+            self, job_id, job_status, labels, error_code=None):
+        expected_params = {'JobId': job_id}
+        response = {'JobStatus': job_status, 'ModerationLabels': [{
+            'Timestamp': label.timestamp,
+            'ModerationLabel': self._moderation_label_to_dict(label)}
+            for label in labels]}
+        self._stub_bifurcator(
+            'get_content_moderation', expected_params, response, error_code=error_code)
