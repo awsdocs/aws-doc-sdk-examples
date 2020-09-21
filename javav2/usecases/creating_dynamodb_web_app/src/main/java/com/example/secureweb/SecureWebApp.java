9e55bc8d9f09d01578ac0e004a99d58e32fcf283
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-09-21 12:00:29 -0400

    Add files via upload

---
+++ javav2/usecases/creating_dynamodb_web_app/src/main/java/com/example/secureweb/SecureWebApp.java
@@ -0,0 +1,12 @@
+package com.example.secureweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebApp {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecureWebApp.class, args);
+    }
+}
