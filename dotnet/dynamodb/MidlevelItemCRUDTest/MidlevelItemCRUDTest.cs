efc0dfe72096671aaab0c38cf6ee634d18f61241
Doug Schwartz <dougsch@amazon.com>
2020-10-09 04:40:09 -0700

    WIP

---
+++ dotnet/dynamodb/MidlevelItemCRUDTest/MidlevelItemCRUDTest.cs
@@ -0,0 +1,99 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DynamoDBCRUD
+{
+    public class MidlevelItemCRUDTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public MidlevelItemCRUDTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private static string ip = "localhost";
+        private static int port = 8000;
+        private readonly string _endpointURL = "http://" + ip + ":" + port.ToString();
+
+        private IDynamoDBContext CreateMockDynamoDBContext(AmazonDynamoDBClient client)
+        {
+            var mockDynamoDBContext = new DynamoDBContext(client);
+
+            return mockDynamoDBContext;
+        }
+
+        private bool IsPortInUse(int port)
+        {
+            bool isAvailable = true;
+
+            // Evaluate current system tcp connections. This is the same information provided
+            // by the netstat command line application, just in .Net strongly-typed object
+            // form.  We will look through the list, and if our port we would like to use
+            // in our TcpClient is occupied, we will set isAvailable to false.
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
+
+            foreach (IPEndPoint endpoint in tcpConnInfoArray)
+            {
+                if (endpoint.Port == port)
+                {
+                    isAvailable = false;
+                    break;
+                }
+            }
+
+            return isAvailable;
+        }
+
+        [Fact]
+        public async void CheckMidlevelItemCRUD()
+        {
+            var portUsed = IsPortInUse(port);
+            if (portUsed)
+            {
+                throw new Exception("You must run local DynamoDB on port 8000");
+            }
+
+            var clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.ServiceURL = _endpointURL;
+            var client = new AmazonDynamoDBClient(clientConfig);
+
+            // Create and load ProductCatalog table.
+            output.WriteLine("Creating ProductCatalog table");
+            await CreateTablesLoadData.CreateTableProductCatalog(client);
+
+            output.WriteLine("Loading ProductCatalog table");
+            var productCatalog = MidlevelItemCRUD.LoadTable(client, MidlevelItemCRUD._tableName);
+
+            // Create, retrieve, and update Book item.
+            output.WriteLine("Creating Book item");
+            MidlevelItemCRUD.CreateBookItem(client, productCatalog);
+            output.WriteLine("Retrieving Book item");
+            MidlevelItemCRUD.RetrieveBook(client, productCatalog);
+            output.WriteLine("Updating Book item");
+            MidlevelItemCRUD.UpdateMultipleAttributes(client, productCatalog);
+            output.WriteLine("Updating Book item price");
+            MidlevelItemCRUD.UpdateBookPriceConditionally(client, productCatalog);
+
+            // Delete Book item.
+            output.WriteLine("Deleting item");
+            MidlevelItemCRUD.DeleteBook(client, productCatalog);
+
+            // Delete ProductCatalog table.
+            output.WriteLine("Deleting ProductCatalog table");
+            await CreateTablesLoadData.DeleteTable(client, "ProductCatalog");
+
+            output.WriteLine("Done");
+        }
+    }
+}
