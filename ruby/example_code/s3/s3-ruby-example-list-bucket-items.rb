b0de1566c3963ec6255823098530c818fc22bf90
Paul Cornell <pccornel@amazon.com>
2020-10-14 14:15:40 -0700

    Cleaned up remaining existing Amazon S3 code examples. Also added missing tests.

--- ruby/example_code/s3/s3-ruby-example-list-bucket-items.rb
+++ ruby/example_code/s3/s3-ruby-example-list-bucket-items.rb
@@ -1,35 +1,61 @@
-# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-# snippet-sourceauthor:[Doug-AWS]
-# snippet-sourcedescription:[???.]
-# snippet-keyword:[Amazon Simple Storage Service]
-# snippet-keyword:[Resource.bucket method]
-# snippet-keyword:[Bucket.objects method]
-# snippet-keyword:[Ruby]
-# snippet-sourcesyntax:[ruby]
-# snippet-service:[s3]
-# snippet-keyword:[Code Sample]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2018-03-16]
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX - License - Identifier: Apache - 2.0
+
+require 'aws-sdk-s3'
+
+# Lists the objects in an Amazon Simple Storage Service (Amazon S3) bucket.
 #
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
+# Prerequisites:
 #
-# http://aws.amazon.com/apache2.0/
+# - An S3 bucket, preferrably containing at least one object.
 #
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+# @param s3_client [Aws::S3::Client] An initialized S3 client.
+# @param bucket_name [String] The name of the bucket.
+# @param max_objects [Integer] The maximum number of objects to list. The
+#   number must be between 1 and 1,000. If not specified, only up to the
+#   first 50 objects will be listed.
+# @example
+#   list_bucket_objects(
+#     Aws::S3::Client.new(region: 'us-east-1'),
+#     'doc-example-bucket',
+#     100
+#   )
+def list_bucket_objects(s3_client, bucket_name, max_objects = 50)
+  if max_objects < 1 || max_objects > 1000
+    puts 'Maximum number of objects to request must be between 1 and 1,000.'
+    return
+  end
 
-require 'aws-sdk-s3'  # v2: require 'aws-sdk'
+  objects = s3_client.list_objects_v2(
+    bucket: bucket_name,
+    max_keys: max_objects
+  ).contents
 
-s3 = Aws::S3::Resource.new(region: 'us-west-2')
-
-bucket = s3.bucket('my-bucket')
-
-# Show only the first 50 items
-bucket.objects.limit(50).each do |item|
-  puts "Name:  #{item.key}"
-  puts "URL:   #{item.presigned_url(:get)}"
+  if objects.count.zero?
+    puts "No objects in bucket '#{bucket_name}'."
+    return
+  else
+    if objects.count == max_objects
+      puts "First #{objects.count} objects in bucket '#{bucket_name}':"
+    else
+      puts "Objects in bucket '#{bucket_name}':"
+    end
+    objects.each do |object|
+      puts object.key
+    end
+  end
+rescue StandardError => e
+  puts "Error accessing bucket '#{bucket_name}' " \
+    "or listing its objects: #{e.message}"
 end
+
+# Full example call:
+def run_me
+  bucket_name = 'doc-example-bucket'
+  region = 'us-east-1'
+  s3_client = Aws::S3::Client.new(region: region)
+
+  list_bucket_objects(s3_client, bucket_name)
+end
+
+run_me if $PROGRAM_NAME == __FILE__
--- ruby/example_code/s3/s3-ruby-example-upload-item-with-metadata.rb
+++ ruby/example_code/s3/s3-ruby-example-upload-item-with-metadata.rb
@@ -1,42 +1,70 @@
-# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-# snippet-sourceauthor:[Doug-AWS]
-# snippet-sourcedescription:[Uploads a file, with metadata, to an S3 bucket.]
-# snippet-keyword:[Amazon Simple Storage Service]
-# snippet-keyword:[Resource.bucket method]
-# snippet-keyword:[Bucket.upload_file method]
-# snippet-keyword:[Ruby]
-# snippet-sourcesyntax:[ruby]
-# snippet-service:[s3]
-# snippet-keyword:[Code Sample]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2018-03-16]
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX - License - Identifier: Apache - 2.0
+
+require 'aws-sdk-s3'
+ 
+# Uploads an object to a bucket in Amazon Simple Storage Service (Amazon S3).
+#   Also associates specified metadata with the uploaded object.
 #
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
+# Prerequisites:
 #
-# http://aws.amazon.com/apache2.0/
+# - An S3 bucket.
+# - An object to upload to the bucket.
 #
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+# @param s3_client [Aws::S3::Client] An initialized S3 client.
+# @param bucket_name [String] The name of the bucket.
+# @param object_key [String] The name of the object.
+# @param metadata [Hash] Metadata to associate with the uploaded object.
+# @return [Boolean] true if the object was uploaded; otherwise, false.
+# @example
+#   exit 1 unless object_uploaded?(
+#     Aws::S3::Client.new(region: 'us-east-1'),
+#     'doc-example-bucket',
+#     'my-file.txt'
+#   )
+def object_uploaded_with_metadata?(
+  s3_client,
+  bucket_name,
+  object_key,
+  metadata
+)
+  response = s3_client.put_object(
+    bucket: bucket_name,
+    key: object_key,
+    metadata: metadata
+  )
+  if response.etag
+    return true
+  else
+    return false
+  end
+rescue StandardError => e
+  puts "Error uploading object: #{e.message}"
+  return false
+end
 
-require 'aws-sdk-s3'  # v2: require 'aws-sdk'
-      
-s3 = Aws::S3::Resource.new(region: 'us-west-2')
+# Full example call:
+def run_me
+  bucket_name = 'doc-example-bucket'
+  object_key = 'my-file.txt'
+  metadata = {
+    author: 'Mary Doe',
+    version: '1.0.0.0'
+  }
+  region = 'us-east-1'
+  s3_client = Aws::S3::Client.new(region: region)
 
-file = 'C:\file.txt'
-bucket = 'my-bucket'
-      
-# Get just the file name
-name = File.basename(file)
+  if object_uploaded_with_metadata?(
+    s3_client,
+    bucket_name,
+    object_key,
+    metadata
+  )
+    puts "Object '#{object_key}' uploaded to bucket '#{bucket_name}' " \
+      'with metadata.'
+  else
+    puts "Object '#{object_key}' not uploaded to bucket '#{bucket_name}'."
+  end
+end
 
-# Create the object to upload
-obj = s3.bucket(bucket).object(name)
-
-# Metadata to add
-metadata = {"answer" => "42"}
-
-# Upload it      
-obj.upload_file(file, metadata: metadata)
+run_me if $PROGRAM_NAME == __FILE__
