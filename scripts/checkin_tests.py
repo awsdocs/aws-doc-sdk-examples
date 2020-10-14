3fe96b1414c59704b41ce2af5271f75b96bcd1c4
PaulC-AWS <pccornel@users.noreply.github.com>
2020-10-14 14:20:39 -0700

    Update checkin_tests.py

--- scripts/checkin_tests.py
+++ scripts/checkin_tests.py
@@ -60,6 +60,7 @@ DENY_LIST = {'alpha-docs-aws.amazon.com', 'integ-docs-aws.amazon.com'}
 
 # whitelist of 20- or 40-character strings to allow
 ALLOW_LIST = {
+    'AKIAEXAMPLEACCESSKEY',
     'AKIAIOSFODNN7EXAMPLE',
     'AppStreamUsageReportsCFNGlueAthenaAccess',
     'aws/acm/model/DescribeCertificateRequest',
