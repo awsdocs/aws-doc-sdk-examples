a92acfc993f4efde3d1ff8cd9fbc7d65fb5954ad
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-10-20 10:33:58 -0400

    Update StreamTranscriptionBehaviorImpl.java

--- javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/retryclient/StreamTranscriptionBehaviorImpl.java
+++ javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/retryclient/StreamTranscriptionBehaviorImpl.java
@@ -16,7 +16,7 @@
  *
  * COPYRIGHT:
  *
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
