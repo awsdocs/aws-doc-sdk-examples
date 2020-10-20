10d4d07c4cf2da2d13bc3e00cece3910c79f5d4a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-10-20 15:06:19 -0400

    Update StreamTranscriptionBehavior.java

--- javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/StreamTranscriptionBehavior.java
+++ javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/StreamTranscriptionBehavior.java
@@ -9,7 +9,7 @@
 // snippet-sourcetype:[snippet]
 // snippet-sourcedate:[2019-01-10]
 // snippet-sourceauthor:[AWS]
-// snippet-start:[transcribe.java-streaming-client-behavior]
+
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -24,6 +24,7 @@
  * specific language governing permissions and limitations under the License.
  *
  */
+// snippet-start:[transcribe.java-streaming-client-behavior]
 package com.amazonaws.transcribestreaming;
 
 import software.amazon.awssdk.services.transcribestreaming.model.StartStreamTranscriptionResponse;
@@ -36,22 +37,16 @@ import software.amazon.awssdk.services.transcribestreaming.model.TranscriptResul
 public interface StreamTranscriptionBehavior {
     /**
      * Defines how to respond when encountering an error on the stream transcription.
-     *
-     * @param e The exception
      */
     void onError(Throwable e);
 
     /**
      * Defines how to respond to the Transcript result stream.
-     *
-     * @param e The TranscriptResultStream event
      */
     void onStream(TranscriptResultStream e);
 
     /**
      * Defines what to do on initiating a stream connection with the service.
-     *
-     * @param r StartStreamTranscriptionResponse
      */
     void onResponse(StartStreamTranscriptionResponse r);
 
