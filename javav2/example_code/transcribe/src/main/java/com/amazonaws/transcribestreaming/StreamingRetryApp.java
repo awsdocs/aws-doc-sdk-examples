a523f2b0ef584b47506530697edefe058700d79d
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-10-20 11:20:29 -0400

    Update StreamingRetryApp.java

--- javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/StreamingRetryApp.java
+++ javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribestreaming/StreamingRetryApp.java
@@ -9,22 +9,19 @@
 // snippet-sourcetype:[snippet]
 // snippet-sourcedate:[2019-01-10]
 // snippet-sourceauthor:[AWS]
-// snippet-start:[transcribe.java-streaming-retry-app]
 /**
- * COPYRIGHT:
- * <p>
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
  */
 package com.amazonaws.transcribestreaming;
 
