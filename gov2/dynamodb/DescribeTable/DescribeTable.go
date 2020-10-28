cd25530659aa8493d6d1bc31b971e002bcb48399
Doug Schwartz <dougsch@amazon.com>
2020-10-28 11:42:06 -0700

    Added snippet tags to DynamoDB DescribeTable code example in Go v2

--- gov2/dynamodb/DescribeTable/DescribeTable.go
+++ gov2/dynamodb/DescribeTable/DescribeTable.go
@@ -1,60 +1,75 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+// snippet-start:[dynamodb.gov2.DescribeTable]
 package main
 
+// snippet-start:[dynamodb.gov2.DescribeTable.imports]
 import (
-	"context"
-	"flag"
-	"fmt"
+    "context"
+    "flag"
+    "fmt"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
+// snippet-end:[dynamodb.gov2.DescribeTable.imports]
 
-// DynamoDBDescribeTableAPI defines the interface for DescribeTable function
+// DynamoDBDescribeTableAPI defines the interface for DescribeTable function.
+// snippet-start:[dynamodb.gov2.DescribeTable.interface]
 type DynamoDBDescribeTableAPI interface {
-	DescribeTable(ctx context.Context,
-		params *dynamodb.DescribeTableInput,
-		optFns ...func(*dynamodb.Options)) (*dynamodb.DescribeTableOutput, error)
+    DescribeTable(ctx context.Context,
+        params *dynamodb.DescribeTableInput,
+        optFns ...func(*dynamodb.Options)) (*dynamodb.DescribeTableOutput, error)
 }
+// snippet-end:[dynamodb.gov2.DescribeTable.interface]
 
-// GetTableInfo retrieves information about the tabl
+// GetTableInfo retrieves information about the table.
+// snippet-start:[dynamodb.gov2.DescribeTable.GetTableInfo]
 func GetTableInfo(c context.Context, api DynamoDBDescribeTableAPI, input *dynamodb.DescribeTableInput) (*dynamodb.DescribeTableOutput, error) {
-	resp, err := api.DescribeTable(c, input)
+    resp, err := api.DescribeTable(c, input)
 
-	return resp, err
+    return resp, err
 }
+// snippet-end:[dynamodb.gov2.DescribeTable.GetTableInfo]
 
 func main() {
-	table := flag.String("t", "", "The name of the table")
-	region := flag.String("r", "us-west-2", "The region")
-	flag.Parse()
+    // snippet-start:[dynamodb.gov2.DescribeTable.args]
+    table := flag.String("t", "", "The name of the table")
+    flag.Parse()
 
-	if *table == "" {
-		fmt.Println("You must specify a table name (-t TABLE)")
-		return
-	}
+    if *table == "" {
+        fmt.Println("You must specify a table name (-t TABLE)")
+        return
+    }
+    // snippet-end:[dynamodb.gov2.DescribeTable.args]
 
-	// Using the SDK's default configuration, loading additional config
-	// and credentials values from the environment variables, shared
-	// credentials, and shared configuration files
-	cfg, err := config.LoadDefaultConfig(config.WithRegion(*region))
-	if err != nil {
-		panic("unable to load SDK config, " + err.Error())
-	}
+    // snippet-start:[dynamodb.gov2.DescribeTable.config_and_client]
+    // Use the SDK's default configuration.
+    cfg, err := config.LoadDefaultConfig()
+    if err != nil {
+        panic("unable to load SDK config, " + err.Error())
+    }
 
-	// Using the Config value, create the DynamoDB client
-	// Create a new DynamoDB Service Client
-	client := dynamodb.NewFromConfig(cfg)
+    // Using the Config value, create the DynamoDB client
+    // Create a new DynamoDB Service Client
+    client := dynamodb.NewFromConfig(cfg)
+    // snippet-end:[dynamodb.gov2.DescribeTable.config_and_client]
 
-	// Build the request with its input parameters
-	resp, err := client.DescribeTable(context.Background(), &dynamodb.DescribeTableInput{
-		TableName: table,
-	})
-	if err != nil {
-		panic("failed to describe table, " + err.Error())
-	}
+    // snippet-start:[dynamodb.gov2.DescribeTable.call]
+    // Build the request with its input parameters
+    resp, err := client.DescribeTable(context.Background(), &dynamodb.DescribeTableInput{
+        TableName: table,
+    })
+    if err != nil {
+        panic("failed to describe table, " + err.Error())
+    }
+    // snippet-end:[dynamodb.gov2.DescribeTable.call]
 
-	fmt.Println("Info about " + *table + ":")
-	fmt.Println("  #items:     ", *resp.Table.ItemCount)
-	fmt.Println("  Size (bytes)", *resp.Table.TableSizeBytes)
-	fmt.Println("  Status:     ", string(resp.Table.TableStatus))
+    // snippet-start:[dynamodb.gov2.DescribeTable.print]
+    fmt.Println("Info about " + *table + ":")
+    fmt.Println("  #items:     ", *resp.Table.ItemCount)
+    fmt.Println("  Size (bytes)", *resp.Table.TableSizeBytes)
+    fmt.Println("  Status:     ", string(resp.Table.TableStatus))
+    // snippet-end:[dynamodb.gov2.DescribeTable.print]
 }
+// snippet-end:[dynamodb.gov2.DescribeTable]
