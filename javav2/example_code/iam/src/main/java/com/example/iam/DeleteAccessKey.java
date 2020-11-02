d5d28abdbd4bebf35716fa3ab8cb3e2bcb67931b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-02 09:52:05 -0500

    Add files via upload

--- javav2/example_code/iam/src/main/java/com/example/iam/DeleteAccessKey.java
+++ javav2/example_code/iam/src/main/java/com/example/iam/DeleteAccessKey.java
@@ -1,24 +1,15 @@
-//snippet-sourcedescription:[DeleteAccessKey.java demonstrates how to delete an access key from an IAM user.]
-//snippet-keyword:[SDK for Java 2.0]
+//snippet-sourcedescription:[DeleteAccessKey.java demonstrates how to delete an access key from an AWS Identity and Access Management (IAM) user.]
+//snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[AWS IAM]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[03/02/2020]
+//snippet-sourcedate:[11/02/2020]
 //snippet-sourceauthor:[scmacdon-aws]
+
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 package com.example.iam;
 
 // snippet-start:[iam.java2.delete_access_key.import]
@@ -28,20 +19,22 @@ import software.amazon.awssdk.services.iam.model.DeleteAccessKeyRequest;
 import software.amazon.awssdk.services.iam.model.IamException;
 // snippet-end:[iam.java2.delete_access_key.import]
 
-/**
- * Deletes an access key from an IAM user
- */
 public class DeleteAccessKey {
     public static void main(String[] args) {
 
-        final String USAGE =
-                "To run this example, supply a username and access key id\n" +
-                        "Ex: DeleteAccessKey <username> <access-key-id>\n";
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    DeleteAccessKey <username> <accessKey> \n\n" +
+                "Where:\n" +
+                "    username - the name of the user. \n\n" +
+                "    accessKey - the access key ID for the secret access key you want to delete. \n\n" ;
 
         if (args.length != 2) {
             System.out.println(USAGE);
             System.exit(1);
         }
+
+        // Read the command line arguments
         String username = args[0];
         String accessKey = args[1];
 
@@ -50,7 +43,8 @@ public class DeleteAccessKey {
                 .region(region)
                 .build();
 
-        deleteKey(iam , username, accessKey);
+        deleteKey(iam, username, accessKey);
+        iam.close();
     }
 
     // snippet-start:[iam.java2.delete_access_key.main]
@@ -58,8 +52,9 @@ public class DeleteAccessKey {
 
         try {
             DeleteAccessKeyRequest request = DeleteAccessKeyRequest.builder()
-                .accessKeyId(accessKey)
-                .userName(username).build();
+                    .accessKeyId(accessKey)
+                    .userName(username)
+                    .build();
 
             iam.deleteAccessKey(request);
             System.out.println("Successfully deleted access key " + accessKey +
--- javav2/example_code/iam/src/main/java/com/example/iam/ListAccountAliases.java
+++ javav2/example_code/iam/src/main/java/com/example/iam/ListAccountAliases.java
@@ -1,24 +1,15 @@
 //snippet-sourcedescription:[ListAccountAliases.java demonstrates how to list all aliases associated with an AWS account.]
-//snippet-keyword:[SDK for Java 2.0]
+//snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[AWS IAM]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[03/02/2020]
+//snippet-sourcedate:[11/02/2020]
 //snippet-sourceauthor:[scmacdon-aws]
+
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 package com.example.iam;
 
 // snippet-start:[iam.java2.list_account_aliases.import]
@@ -28,15 +19,17 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.iam.IamClient;
 // snippet-end:[iam.java2.list_account_aliases.import]
 
-/**
- * Lists all aliases associated with an AWS account
- */
 public class ListAccountAliases {
     public static void main(String[] args) {
 
         Region region = Region.AWS_GLOBAL;
-        IamClient iam = IamClient.builder().region(region).build();
+        IamClient iam = IamClient.builder()
+                .region(region)
+                .build();
+
         listAliases(iam);
+        System.out.println("Done");
+        iam.close();
     }
 
     // snippet-start:[iam.java2.list_account_aliases.main]
@@ -53,7 +46,7 @@ public class ListAccountAliases {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-        System.out.println("Done");
+
     }
     // snippet-end:[iam.java2.list_account_aliases.main]
 }
