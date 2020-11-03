40773e26a05eaba06d1b856960574cb005b49f51
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-03 18:09:08 -0500

    Add files via upload

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/GetLogEvents.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/GetLogEvents.java
@@ -1,25 +1,15 @@
-//snippet-sourcedescription:[GetLogEvents.java demonstrates how to get log events from Amazon CloudWatch in a specified AWS Region. ]
-//snippet-keyword:[SDK for Java 2.0]
+//snippet-sourcedescription:[GetLogEvents.java demonstrates how to get log events from Amazon CloudWatch.]
+//snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[Amazon CloudWatch]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[03/02/2020]
-//snippet-sourceauthor:[scmacdon]
+//snippet-sourcedate:[11/02/2020]
+//snippet-sourceauthor:[scmacdon - aws]
 
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 package com.example.cloudwatch;
 
 // snippet-start:[cloudwatch.java2.get_logs.import]
@@ -29,43 +19,40 @@ import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsRequest;
 // snippet-end:[cloudwatch.java2.get_logs.import]
 
-/**
- * Gets log events from CloudWatch
- */
 public class GetLogEvents {
 
     public static void main(String[] args) {
 
-        final String usage =
-                "To run this example, supply a logGroupName and streamName as command line arguments\n" +
-                        "Ex: GetLogEvents <logGroupName> <streamName>\n";
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "  GetLogEvents <logStreamName> <logGroupName>\n\n" +
+                "Where:\n" +
+                "  logStreamName - the name of the log stream (for example, mystream).\n" +
+                "  logGroupName - the name of the log group (for example, myloggroup).\n" ;
 
         if (args.length != 2) {
-            System.out.print(usage);
+            System.out.print(USAGE);
             System.exit(1);
         }
 
         String logStreamName = args[0];
         String logGroupName = args[1];
 
-        // Create a CloudWatchLogClient
         Region region = Region.US_WEST_2;
         CloudWatchLogsClient cloudWatchLogsClient = CloudWatchLogsClient.builder()
                 .region(region)
                 .build();
 
         getCWLogEvebts(cloudWatchLogsClient, logGroupName, logStreamName) ;
+        cloudWatchLogsClient.close();
     }
 
     // snippet-start:[cloudwatch.java2.get_logs.main]
     public static void getCWLogEvebts(CloudWatchLogsClient cloudWatchLogsClient, String logGroupName, String logStreamName) {
 
-
         try {
 
-            // Designate the logGroupName and logStream you want to get logs from
-            // Assume only one stream name exists, however, this isn't always the case
-            GetLogEventsRequest getLogEventsRequest = GetLogEventsRequest.builder()
+             GetLogEventsRequest getLogEventsRequest = GetLogEventsRequest.builder()
                 .logGroupName(logGroupName)
                 .logStreamName(logStreamName)
                 .startFromHead(true)
@@ -73,15 +60,15 @@ public class GetLogEvents {
 
             int logLimit = cloudWatchLogsClient.getLogEvents(getLogEventsRequest).events().size();
             for (int c = 0; c < logLimit; c++) {
-                // Prints the messages to the console
-                System.out.println(cloudWatchLogsClient.getLogEvents(getLogEventsRequest).events().get(c).message());
+                 System.out.println(cloudWatchLogsClient.getLogEvents(getLogEventsRequest).events().get(c).message());
             }
+
+            System.out.println("Successfully got CloudWatch log events!");
         } catch (CloudWatchException e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
 
-        System.out.println("Successfully got CloudWatch log events!");
         // snippet-end:[cloudwatch.java2.get_logs.main]
     }
 }
