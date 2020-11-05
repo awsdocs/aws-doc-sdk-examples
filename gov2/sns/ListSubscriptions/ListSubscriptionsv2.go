03d4711755b83161652ec36df513b05c20b32387
Doug Schwartz <dougsch@amazon.com>
2020-11-05 11:42:45 -0800

    Fixed license for SNS examples in Go V2

--- gov2/sns/ListSubscriptions/ListSubscriptionsv2.go
+++ gov2/sns/ListSubscriptions/ListSubscriptionsv2.go
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+// SPDX - License - Identifier: Apache - 2.0
 // snippet-start:[sns.go-v2.ListSubscriptions]
 package main
 
