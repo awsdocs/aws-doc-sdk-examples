8108853dfcf4219e43792a92d874c1eefba8d625
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-13 14:03:08 -0500

    Add files via upload

--- javav2/example_code/forecast/src/main/java/com/example/forecast/ListDataSets.java
+++ javav2/example_code/forecast/src/main/java/com/example/forecast/ListDataSets.java
@@ -1,20 +1,14 @@
-//snippet-sourcedescription:[ListDataSets.java demonstrates how to list Amazon Forecast datasets.]
-//snippet-keyword:[SDK for Java 2.0]
+//snippet-sourcedescription:[ListDataSets.java demonstrates how to list Amazon Forecast data sets.]
+//snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[Amazon Forecast]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[5/4/2020]
+//snippet-sourcedate:[11/04/2020]
 //snippet-sourceauthor:[scmacdon-aws]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
-    http://aws.amazon.com/apache2.0/
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
+   SPDX-License-Identifier: Apache-2.0
 */
 
 package com.example.forecast;
@@ -32,13 +26,14 @@ import java.util.List;
 public class ListDataSets {
 
     public static void main(String[] args) {
-        // Create a Forecast client
+
         Region region = Region.US_WEST_2;
         ForecastClient forecast = ForecastClient.builder()
                 .region(region)
                 .build();
 
         listForecastDataSets(forecast);
+        forecast.close();
     }
 
     // snippet-start:[forecast.java2.list_datasets.main]
@@ -56,7 +51,7 @@ public class ListDataSets {
            while(groupsIterator.hasNext()) {
 
             DatasetSummary myGroup = groupsIterator.next();
-            System.out.println("The dataset name is " +myGroup.datasetName()) ;
+            System.out.println("The Data Set name is " +myGroup.datasetName()) ;
         }
        } catch (ForecastException e) {
            System.err.println(e.awsErrorDetails().errorMessage());
