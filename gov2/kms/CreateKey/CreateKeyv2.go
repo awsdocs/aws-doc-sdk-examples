5c8b0602ce20c7999383cd54a4c110a0f8ed055b
Doug Schwartz <dougsch@amazon.com>
2020-11-17 06:11:32 -0800

    Added a missing snippet end tag; expanded tabs to spaces

--- gov2/kms/CreateKey/CreateKeyv2.go
+++ gov2/kms/CreateKey/CreateKeyv2.go
@@ -4,21 +4,21 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
+    "context"
+    "flag"
+    "fmt"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/kms"
-	"github.com/aws/aws-sdk-go-v2/service/kms/types"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/kms"
+    "github.com/aws/aws-sdk-go-v2/service/kms/types"
 )
 
 // KMSCreateKeyAPI defines the interface for the CreateKey function.
 // We use this interface to test the function using a mocked service.
 type KMSCreateKeyAPI interface {
-	CreateKey(ctx context.Context,
-		params *kms.CreateKeyInput,
-		optFns ...func(*kms.Options)) (*kms.CreateKeyOutput, error)
+    CreateKey(ctx context.Context,
+        params *kms.CreateKeyInput,
+        optFns ...func(*kms.Options)) (*kms.CreateKeyOutput, error)
 }
 
 // MakeKey creates an AWS Key Management Service (AWS KMS) customer master key (CMK).
@@ -30,43 +30,45 @@ type KMSCreateKeyAPI interface {
 //     If success, a CreateKeyOutput object containing the result of the service call and nil.
 //     Otherwise, nil and an error from the call to CreateKey.
 func MakeKey(c context.Context, api KMSCreateKeyAPI, input *kms.CreateKeyInput) (*kms.CreateKeyOutput, error) {
-	result, err := api.CreateKey(c, input)
+    result, err := api.CreateKey(c, input)
 
-	return result, err
+    return result, err
 }
 
 func main() {
-	key := flag.String("k", "", "The KMS key name")
-	value := flag.String("v", "", "The value of the KMS key")
-	flag.Parse()
+    key := flag.String("k", "", "The KMS key name")
+    value := flag.String("v", "", "The value of the KMS key")
+    flag.Parse()
 
-	if *key == "" || *value == "" {
-		fmt.Println("You must supply a KMS key name and value (-k KEY-NAME -v KEY-VALUE)")
-		return
-	}
+    if *key == "" || *value == "" {
+        fmt.Println("You must supply a KMS key name and value (-k KEY-NAME -v KEY-VALUE)")
+        return
+    }
 
-	cfg, err := config.LoadDefaultConfig()
-	if err != nil {
-		panic("configuration error, " + err.Error())
-	}
+    cfg, err := config.LoadDefaultConfig()
+    if err != nil {
+        panic("configuration error, " + err.Error())
+    }
 
-	client := kms.NewFromConfig(cfg)
+    client := kms.NewFromConfig(cfg)
 
-	input := &kms.CreateKeyInput{
-		Tags: []*types.Tag{
-			{
-				TagKey:   key,
-				TagValue: value,
-			},
-		},
-	}
+    input := &kms.CreateKeyInput{
+        Tags: []*types.Tag{
+            {
+                TagKey:   key,
+                TagValue: value,
+            },
+        },
+    }
 
-	result, err := MakeKey(context.Background(), client, input)
-	if err != nil {
-		fmt.Println("Got error creating key:")
-		fmt.Println(err)
-		return
-	}
+    result, err := MakeKey(context.Background(), client, input)
+    if err != nil {
+        fmt.Println("Got error creating key:")
+        fmt.Println(err)
+        return
+    }
 
-	fmt.Println(*result.KeyMetadata.KeyId)
+    fmt.Println(*result.KeyMetadata.KeyId)
 }
+
+// snippet-end:[kms-go-v2.CreateKey]
--- gov2/kms/EncryptData/EncryptDatav2_test.go
+++ gov2/kms/EncryptData/EncryptDatav2_test.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"testing"
-	"time"
+    "context"
+    "encoding/json"
+    "errors"
+    "io/ioutil"
+    "testing"
+    "time"
 
-	"github.com/aws/aws-sdk-go-v2/service/kms"
+    "github.com/aws/aws-sdk-go-v2/service/kms"
 )
 
 type KMSEncryptImpl struct{}
 
 func (dt KMSEncryptImpl) Encrypt(ctx context.Context,
-	params *kms.EncryptInput,
-	optFns ...func(*kms.Options)) (*kms.EncryptOutput, error) {
+    params *kms.EncryptInput,
+    optFns ...func(*kms.Options)) (*kms.EncryptOutput, error) {
 
-	blob := []byte(globalConfig.Text)
+    blob := []byte(globalConfig.Text)
 
-	output := &kms.EncryptOutput{
-		CiphertextBlob: blob,
-	}
+    output := &kms.EncryptOutput{
+        CiphertextBlob: blob,
+    }
 
-	return output, nil
+    return output, nil
 }
 
 type Config struct {
-	KeyID string `json:"KeyID"`
-	Text  string `json:"Text"`
+    KeyID string `json:"KeyID"`
+    Text  string `json:"Text"`
 }
 
 var configFileName = "config.json"
@@ -36,50 +36,50 @@ var configFileName = "config.json"
 var globalConfig Config
 
 func populateConfiguration(t *testing.T) error {
-	content, err := ioutil.ReadFile(configFileName)
-	if err != nil {
-		return err
-	}
+    content, err := ioutil.ReadFile(configFileName)
+    if err != nil {
+        return err
+    }
 
-	text := string(content)
+    text := string(content)
 
-	err = json.Unmarshal([]byte(text), &globalConfig)
-	if err != nil {
-		return err
-	}
+    err = json.Unmarshal([]byte(text), &globalConfig)
+    if err != nil {
+        return err
+    }
 
-	if globalConfig.KeyID == "" || globalConfig.Text == "" {
-		msg := "You must supply a value for KeyID and Text in " + configFileName
-		return errors.New(msg)
-	}
+    if globalConfig.KeyID == "" || globalConfig.Text == "" {
+        msg := "You must supply a value for KeyID and Text in " + configFileName
+        return errors.New(msg)
+    }
 
-	return nil
+    return nil
 }
 
 func TestEncrypt(t *testing.T) {
-	thisTime := time.Now()
-	nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
-	t.Log("Starting unit test at " + nowString)
+    thisTime := time.Now()
+    nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
+    t.Log("Starting unit test at " + nowString)
 
-	err := populateConfiguration(t)
-	if err != nil {
-		t.Fatal(err)
-	}
+    err := populateConfiguration(t)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	api := &KMSEncryptImpl{}
+    api := &KMSEncryptImpl{}
 
-	input := &kms.EncryptInput{
-		KeyId:     &globalConfig.KeyID,
-		Plaintext: []byte(globalConfig.Text),
-	}
+    input := &kms.EncryptInput{
+        KeyId:     &globalConfig.KeyID,
+        Plaintext: []byte(globalConfig.Text),
+    }
 
-	resp, err := EncryptText(context.Background(), api, input)
-	if err != nil {
-		t.Log("Got an error ...:")
-		t.Log(err)
-		return
-	}
+    resp, err := EncryptText(context.Background(), api, input)
+    if err != nil {
+        t.Log("Got an error ...:")
+        t.Log(err)
+        return
+    }
 
-	t.Log("Blob (base-64 byte array):")
-	t.Log(resp.CiphertextBlob)
+    t.Log("Blob (base-64 byte array):")
+    t.Log(resp.CiphertextBlob)
 }
