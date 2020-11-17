80f546bf4d62561b009dd8b00382c4e25e9ea01e
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-17 13:05:33 -0500

    Update CreateVault.java

--- javav2/example_code/glacier/src/main/java/com/example/glacier/CreateVault.java
+++ javav2/example_code/glacier/src/main/java/com/example/glacier/CreateVault.java
@@ -1,4 +1,4 @@
-//snippet-sourcedescription:[CreateVault.java demonstrates how to create an Amazon Glacier vault.]
+//snippet-sourcedescription:[CreateVault.java demonstrates how to create an Amazon Simple Storage Service Glacier (Amazon S3 Glacier) vault.]
 //snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[Amazon Glacier]
