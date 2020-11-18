00a40ac6db10e086b4bc4be9668c85304e178bb4
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-18 14:08:22 -0500

    Update KinesisServiceTest.java

--- javav2/example_code/kinesis/src/test/java/KinesisServiceTest.java
+++ javav2/example_code/kinesis/src/test/java/KinesisServiceTest.java
@@ -1,3 +1,7 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 import com.example.kinesis.CreateDataStream;
 import com.example.kinesis.DescribeLimits;
 import org.junit.jupiter.api.*;
