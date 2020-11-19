df77e0eae63f82d1fb78c2842e6cbf13df67620c
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-19 08:44:07 -0500

    Update LambdaTest.java

--- javav2/example_code/lambda/src/test/java/LambdaTest.java
+++ javav2/example_code/lambda/src/test/java/LambdaTest.java
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 import com.example.lambda.*;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import org.junit.jupiter.api.*;
