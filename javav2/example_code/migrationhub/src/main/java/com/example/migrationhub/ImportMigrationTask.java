f48700c769fdd558a59a1b66860d74c02e423a2a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-19 08:57:40 -0500

    Add files via upload

--- javav2/example_code/migrationhub/src/main/java/com/example/migrationhub/ImportMigrationTask.java
+++ javav2/example_code/migrationhub/src/main/java/com/example/migrationhub/ImportMigrationTask.java
@@ -1,25 +1,16 @@
 // snippet-sourcedescription:[ImportMigrationTask.java demonstrates how to register a new migration task.]
-// snippet-service:[Amazon Migration Hub]
-// snippet-keyword:[Java]
-// snippet-keyword:[Amazon Migration Hub]
+//snippet-keyword:[AWS SDK for Java v2]
+// snippet-service:[AWS Migration Hub]
 // snippet-keyword:[Code Sample]
 // snippet-sourcetype:[full-example]
-// snippet-sourcedate:[6-23-2020]
+// snippet-sourcedate:[11-05-2020]
 // snippet-sourceauthor:[scmacdon - AWS]
 
-/**
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 package com.example.migrationhub;
 
 // snippet-start:[migration.java2.import_migration.import]
@@ -35,11 +26,13 @@ public class ImportMigrationTask {
     public static void main(String[] args) {
 
         final String USAGE = "\n" +
-                "To run this example, supply the name of a migration task and progress stream\n" +
-                "\n" +
-                "Ex: ImportMigrationTask SampleApp ProgressStream\n";
+                "Usage:\n" +
+                "    ImportMigrationTask <migrationTask> <progressStream> \n\n" +
+                "Where:\n" +
+                "    migrationTask - the name of a migration task. \n"+
+                "    progressStream - the name of a progress stream. \n";
 
-        if (args.length < 2) {
+        if (args.length != 2) {
             System.out.println(USAGE);
             System.exit(1);
         }
@@ -53,6 +46,7 @@ public class ImportMigrationTask {
                 .build();
 
         importMigrTask(migrationClient, migrationTask, progressStream);
+        migrationClient.close();
     }
 
     // snippet-start:[migration.java2.import_migration.main]
