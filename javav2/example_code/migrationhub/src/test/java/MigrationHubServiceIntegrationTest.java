471919675966ca583bcbb3e453aeff39696ef8ec
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-19 08:48:47 -0500

    Update MigrationHubServiceIntegrationTest.java

--- javav2/example_code/migrationhub/src/test/java/MigrationHubServiceIntegrationTest.java
+++ javav2/example_code/migrationhub/src/test/java/MigrationHubServiceIntegrationTest.java
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 import com.example.migrationhub.*;
 import software.amazon.awssdk.services.migrationhub.MigrationHubClient;
 import org.junit.jupiter.api.*;
