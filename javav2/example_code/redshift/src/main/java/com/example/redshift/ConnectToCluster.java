825a075420df15e2549ef67192482254d0eac436
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-23 09:05:21 -0500

    Add files via upload

--- javav2/example_code/redshift/src/main/java/com/example/redshift/ConnectToCluster.java
+++ javav2/example_code/redshift/src/main/java/com/example/redshift/ConnectToCluster.java
@@ -1,20 +1,14 @@
 //snippet-sourcedescription:[ConnectToCluster.java demonstrates how to connect to an Amazon Redshift cluster using the JDBC API.]
-//snippet-keyword:[SDK for Java 2.0]
+//snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Code Sample]
 //snippet-service:[Amazon Redshift ]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[7/6/2020]
+//snippet-sourcedate:[11/05/2020]
 //snippet-sourceauthor:[scmacdon - aws]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
-    http://aws.amazon.com/apache2.0/
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
+   SPDX-License-Identifier: Apache-2.0
 */
 package com.example.redshift;
 
@@ -39,11 +33,11 @@ public class ConnectToCluster {
                 "Usage:\n" +
                 "    ConnectToCluster <dbURL><masterUsername><masterUserPassword> \n\n" +
                 "Where:\n" +
-                "    dbURL - The URL to the Amazon Redshift cluster \n" +
-                "    masterUsername - The master user name \n" +
-                "    masterUserPassword - The password that corresponds to the master user name \n" ;
+                "    dbURL - the URL to the Redshift cluster \n" +
+                "    masterUsername - the master user name \n" +
+                "    masterUserPassword - the password that corresponds to the master user name \n" ;
 
-        if (args.length < 3) {
+        if (args.length != 3) {
             System.out.println(USAGE);
             System.exit(1);
         }
@@ -60,35 +54,35 @@ public class ConnectToCluster {
         Connection conn = null;
         Statement stmt = null;
         try{
-            // Dynamically load driver at runtime
-            // Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
-            // Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
+            //Dynamically load driver at runtime.
+            //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
+            //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
             Class.forName("com.amazon.redshift.jdbc.Driver");
 
-            // Open a connection and define properties
+            //Open a connection and define properties.
             System.out.println("Connecting to database...");
             Properties props = new Properties();
 
-            // Uncomment the following line if using a key store
-            // props.setProperty("ssl", "true");
+            //Uncomment the following line if using a keystore.
+            //props.setProperty("ssl", "true");
             props.setProperty("user", masterUsername);
             props.setProperty("password", masterUserPassword);
             conn = DriverManager.getConnection(dbURL, props);
 
-            // Try a simple query
+            //Try a simple query.
             System.out.println("Listing system tables...");
             stmt = conn.createStatement();
             String sql;
             sql = "select * from information_schema.tables;";
             ResultSet rs = stmt.executeQuery(sql);
 
-            // Get the data from the result set
+            //Get the data from the result set.
             while(rs.next()){
-                // Retrieve two columns
+                //Retrieve two columns.
                 String catalog = rs.getString("table_catalog");
                 String name = rs.getString("table_name");
 
-                // Display values
+                //Display values.
                 System.out.print("Catalog: " + catalog);
                 System.out.println(", Name: " + name);
             }
@@ -97,17 +91,17 @@ public class ConnectToCluster {
             stmt.close();
             conn.close();
         }catch(SQLException | ClassNotFoundException ex){
-            // For convenience, handle all errors here
+            //For convenience, handle all errors here.
             ex.printStackTrace();
             System.exit(1);
         }finally{
-            // Finally block to close resources
+            //Finally block to close resources.
             try{
                 if(stmt!=null)
                     stmt.close();
             }catch(SQLException ex){
                 System.exit(1);
-            }// Nothing we can do
+            }// nothing we can do
             try{
                 if(conn!=null)
                     conn.close();
