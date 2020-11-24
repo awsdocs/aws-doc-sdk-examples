04fc3f8b28ef517b18c01ac2a6dec0d2af575de3
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-24 08:47:22 -0500

    Add files via upload

--- javav2/example_code/sts/src/main/java/com/example/sts/GetAccessKeyInfo.java
+++ javav2/example_code/sts/src/main/java/com/example/sts/GetAccessKeyInfo.java
@@ -1,27 +1,16 @@
 // snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-sourcedescription:[GetAccessKeyInfo.java demonstrates how to return the account identifier for the specified access key ID.]
-// snippet-service:[AWS Security Token Service]
-// snippet-keyword:[Java]
-// snippet-keyword:[AWS Security Token Service]
+// snippet-sourcedescription:[GetAccessKeyInfo.java demonstrates how to return the account identifier for the specified access key ID by using AWS Security Token Service (AWS STS).]
+// snippet-keyword:[AWS SDK for Java v2]
+// snippet-keyword:[AWS Security Token Service (AWS STS)]
 // snippet-keyword:[Code Sample]
 // snippet-sourcetype:[full-example]
-// snippet-sourcedate:[2020-09-21]
+// snippet-sourcedate:[11/06/2020]
 // snippet-sourceauthor:[AWS - scmacdon]
 
-/**
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- */
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 
 package com.example.sts;
 
@@ -38,24 +27,24 @@ public class GetAccessKeyInfo {
     public static void main(String[] args) {
 
         final String USAGE = "\n" +
-                "To run this example, supply the accessKey Id value.  \n" +
-                "\n" +
-                "Ex: GetAccessKeyInfo <accessKeyId>\n";
+                "Usage:\n" +
+                "    GetAccessKeyInfo <accessKeyId> \n\n" +
+                "Where:\n" +
+                "    accessKeyId - the identifier of an access key (for example, XXXXX3JWY3BXW7POHDLA). \n";
 
-        if (args.length < 1) {
+        if (args.length != 1) {
              System.out.println(USAGE);
              System.exit(1);
         }
 
-        /* Read the name from command args*/
         String accessKeyId = args[0];
-
         Region region = Region.US_EAST_1;
         StsClient stsClient = StsClient.builder()
                 .region(region)
                 .build();
 
         getKeyInfo(stsClient, accessKeyId );
+        stsClient.close();
     }
 
     // snippet-start:[sts.java2.get_access_key.main]
