08c7c1809232eabe35dc1b4aca9b04421156813b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-11-25 08:56:00 -0500

    Add files via upload

--- javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribe/Microphone.java
+++ javav2/example_code/transcribe/src/main/java/com/amazonaws/transcribe/Microphone.java
@@ -1,21 +1,14 @@
 //snippet-sourcedescription:[Microphone.java is a helper class to the BidirectionalStreaming example that setups the app to listen on the device's microphone for audio input.]
-//snippet-keyword:[SDK for Java 2.0]
-//snippet-keyword:[Code Sample]
+// snippet-keyword:[AWS SDK for Java v2]
 //snippet-keyword:[Amazon Transcribe]
-//snippet-keyword:[bidirectional streaming]
-//snippet-service:[transcribe]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[2019-04-18]
-//snippet-sourceauthor:[AWS]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[11/06/2020]
+// snippet-sourceauthor:[scmacdon - AWS]
+
 /*
-   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
-    http://aws.amazon.com/apache2.0/
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
 */
 
 package com.amazonaws.transcribe;
