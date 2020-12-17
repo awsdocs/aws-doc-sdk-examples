0b9a472d0e01fd0e049db3531fca6f50ec8799a4
Irene Smith <igsmith@amazon.com>
2020-12-17 13:21:43 -0500

    First commit for Synthesize Speech example.

---
+++ dotnet3.5/Polly/SynthesizeSpeechExample/SynthesizeSpeech/SynthesizeSpeech.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier:  Apache - 2.0
+
+using Amazon;
+using Amazon.Polly;
+using System;
+using System.Threading.Tasks;
+
+namespace SynthesizeSpeech
+{
+    class SynthesizeSpeech
+    {
+        // The following example text to speech and saves it to an .mp3 file
+        // using Amazon Polly. It was created using the AWS SDK for .NET 3.5
+        // and .NET 5.0.
+
+        // Specify your AWS Region (an example Region is shown).
+        private static readonly RegionEndpoint SERVICE_REGION = RegionEndpoint.USWest2;
+        private static IAmazonPolly _PollyClient;
+
+        static async Task Main(string[] args)
+        {
+            _PollyClient = new AmazonPollyClient(SERVICE_REGION);
+        }
+    }
+}
---
+++ dotnet3.5/Polly/SynthesizeSpeechExample/SynthesizeSpeechTest/SynthesizeSpeechTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace SynthesizeSpeechTest
+{
+    public class SynthesizeSpeechTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
