1df29e41769fd5b747413412ee8e5bac67f7e02a
Irene Smith <igsmith@amazon.com>
2020-12-22 06:23:13 -0500

    Working on Polly example.

--- dotnet3.5/Polly/SynthesizeSpeechExample/SynthesizeSpeech/SynthesizeSpeech.cs
+++ dotnet3.5/Polly/SynthesizeSpeechExample/SynthesizeSpeech/SynthesizeSpeech.cs
@@ -3,6 +3,7 @@
 
 using Amazon;
 using Amazon.Polly;
+using Amazon.Polly.Model;
 using System;
 using System.Threading.Tasks;
 
@@ -18,9 +19,46 @@ namespace SynthesizeSpeech
         private static readonly RegionEndpoint SERVICE_REGION = RegionEndpoint.USWest2;
         private static IAmazonPolly _PollyClient;
 
+        private const string _OutputFileName = "speech.mp3";
+        private const string _Text = "Twas brillig, and the slithy toves did gyre and gimbol in the wabe";
+
         static async Task Main(string[] args)
         {
             _PollyClient = new AmazonPollyClient(SERVICE_REGION);
+            var response = await PollySynthesizeSpeech(_PollyClient, _Text);
+
         }
+
+        static async Task<SynthesizeSpeechResponse> PollySynthesizeSpeech (IAmazonPolly client, string text)
+        {
+            var synthesizeSpeechRequest = new SynthesizeSpeechRequest()
+            {
+                OutputFormat = OutputFormat.Mp3,
+                VoiceId = VoiceId.Joanna,
+                Text = text
+            };
+
+            var synthesizeSpeechResponse =
+                await client.SynthesizeSpeechAsync(synthesizeSpeechRequest);
+
+            return synthesizeSpeechResponse;
+        }
+
+        public static async Task WriteSpechToFile()
+        {
+            //var audioStream = response.Result.AudioStream;
+            //var outputStream = new FileStream(outputFileName, FileMode.Create,
+            //    FileAccess.Write);
+            //byte[] buffer = new byte[2 * 1024];
+            //int readBytes;
+
+            //while ((readBytes = audioStream.Read(buffer, 0, 2 * 1024)) > 0)
+            //    outputStream.Write(buffer, 0, readBytes);
+            //// If we don't flush the buffer, we lose the last second or so of
+            //// the syntesized text.
+            //outputStream.Flush();
+
+        }
+
     }
 }
