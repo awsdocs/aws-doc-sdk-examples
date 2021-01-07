277142650c41c9f1ab2e635462e353b64db1c570
Paul Cornell <pccornel@amazon.com>
2021-01-07 15:54:55 -0800

    Continued cleaning up Ruby examples for Amazon DynamoDB: update item. Also started adding unit tests.

--- ruby/example_code/dynamodb/GettingStarted/MoviesItemOps03.rb
+++ ruby/example_code/dynamodb/GettingStarted/MoviesItemOps03.rb
@@ -1,63 +1,56 @@
-# snippet-sourcedescription:[ ]
-# snippet-service:[dynamodb]
-# snippet-keyword:[Ruby]
-# snippet-sourcesyntax:[ruby]
-# snippet-keyword:[Amazon DynamoDB]
-# snippet-keyword:[Code Sample]
-# snippet-keyword:[ ]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[ ]
-# snippet-sourceauthor:[AWS]
-# snippet-start:[dynamodb.Ruby.CodeExample.MoviesItemOps03] 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
-#
-#  Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-#  This file is licensed under the Apache License, Version 2.0 (the "License").
-#  You may not use this file except in compliance with the License. A copy of
-#  the License is located at
-# 
-#  http://aws.amazon.com/apache2.0/
-# 
-#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#  CONDITIONS OF ANY KIND, either express or implied. See the License for the
-#  specific language governing permissions and limitations under the License.
-#
-require "aws-sdk"
+# snippet-start:[dynamodb.Ruby.CodeExample.MoviesItemOps03]
+require 'aws-sdk-dynamodb'
 
-Aws.config.update({
-  region: "us-west-2",
-  endpoint: "http://localhost:8000"
-})
+def table_item_updated?(dynamodb_client, table_item)
+  dynamodb_client.update_item(table_item)
+  true
+rescue StandardError => e
+  puts "Error updating item: #{e.message}"
+  false
+end
 
-dynamodb = Aws::DynamoDB::Client.new
+def run_me
+  region = 'us-west-2'
+  table_name = 'Movies'
+  title = 'The Big New Movie'
+  year = 2015
 
-table_name = 'Movies'
+  # To use the downloadable version of Amazon DynamoDB,
+  # uncomment the endpoint statement.
+  Aws.config.update(
+    # endpoint: "http://localhost:8000",
+    region: region
+  )
 
-year = 2015
-title = "The Big New Movie"
+  dynamodb_client = Aws::DynamoDB::Client.new
 
-params = {
+  table_item = {
     table_name: table_name,
     key: {
-        year: year,
-        title: title
+      year: year,
+      title: title
     },
-    update_expression: "set info.rating = :r, info.plot=:p, info.actors=:a",
+    update_expression: 'SET info.rating = :r, info.plot = :p, info.actors = :a',
     expression_attribute_values: {
-        ":r" => 5.5,
-        ":p" => "Everything happens all at once.", # value <Hash,Array,String,Numeric,Boolean,IO,Set,nil>
-        ":a" => ["Larry", "Moe", "Curly"]
+      ':r': 5.5,
+      ':p': 'Everything happens all at once.',
+      ':a': [ "Larry", "Moe", "Curly" ]
     },
     return_values: "UPDATED_NEW"
-}
+  }
 
-begin
-    dynamodb.update_item(params)
-    puts "Added item: #{year}  - #{title}"
+  puts "Updating table '#{table_name}' with information about " \
+    "'#{title} (#{year})'..."
 
-rescue  Aws::DynamoDB::Errors::ServiceError => error
-    puts "Unable to add item:"
-    puts "#{error.message}"
+  if table_item_updated?(dynamodb_client, table_item)
+    puts 'Table updated.'
+  else
+    puts 'Table not updated.'
+  end
 end
+
+run_me if $PROGRAM_NAME == __FILE__
 # snippet-end:[dynamodb.Ruby.CodeExample.MoviesItemOps03]
