1ab8b23a923faa28119ffc26d90708c3ec68cf5f
Paul Cornell <pccornel@amazon.com>
2021-01-08 16:26:58 -0800

    Add docs to Ruby code examples for Amazon DynamoDB.

--- ruby/example_code/dynamodb/GettingStarted/MoviesItemOps05.rb
+++ ruby/example_code/dynamodb/GettingStarted/MoviesItemOps05.rb
@@ -1,69 +1,73 @@
-# snippet-sourcedescription:[ ]
-# snippet-service:[dynamodb]
-# snippet-keyword:[Ruby]
-# snippet-sourcesyntax:[ruby]
-# snippet-keyword:[Amazon DynamoDB]
-# snippet-keyword:[Code Sample]
-# snippet-keyword:[ ]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[ ]
-# snippet-sourceauthor:[AWS]
-# snippet-start:[dynamodb.Ruby.CodeExample.MoviesItemOps05] 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
-#
-#  Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-#  This file is licensed under the Apache License, Version 2.0 (the "License").
-#  You may not use this file except in compliance with the License. A copy of
-#  the License is located at
-# 
-#  http://aws.amazon.com/apache2.0/
-# 
-#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#  CONDITIONS OF ANY KIND, either express or implied. See the License for the
-#  specific language governing permissions and limitations under the License.
-#
-require "aws-sdk"
+# This code example demonstrates how to update information about an
+# existing item in an existing table in Amazon DynamoDB named 'Movies'.
+# If an item with the specified attributes exists in the table,
+# information about that item is updated. In this example, the item
+# must have a 'year' attribute value of 2015 and a 'title' attribute value
+# of 'The Big New Movie'. If so, and if the 'actors' value of the item's
+# 'info' attribute contains more than three, then value of the first
+# actor is removed. Only information about the updated attribute is
+# returned, as specified by 'UPDATED_NEW'.
 
-Aws.config.update({
-  region: "us-west-2",
-  endpoint: "http://localhost:8000"
-})
+# snippet-start:[dynamodb.Ruby.CodeExample.MoviesItemOps05]
+require 'aws-sdk-dynamodb'
 
-dynamodb = Aws::DynamoDB::Client.new
+def table_item_updated?(dynamodb_client, table_item)
+  result = dynamodb_client.update_item(table_item)
+  puts "Table item updated with the following attributes for 'info':"
+  result.attributes['info'].each do |key, value|
+    if key == 'rating'
+      puts "#{key}: #{value.to_f}"
+    else
+      puts "#{key}: #{value}"
+    end
+  end
+  true
+rescue StandardError => e
+  puts "Error updating item: #{e.message}"
+  false
+end
 
-table_name = 'Movies'
+def run_me
+  region = 'us-west-2'
+  table_name = 'Movies'
+  title = 'The Big New Movie'
+  year = 2015
 
-year = 2015
-title = "The Big New Movie"
+  # To use the downloadable version of Amazon DynamoDB,
+  # uncomment the endpoint statement.
+  Aws.config.update(
+    # endpoint: "http://localhost:8000",
+    region: region
+  )
 
-params = {
+  dynamodb_client = Aws::DynamoDB::Client.new
+
+  table_item = {
     table_name: table_name,
     key: {
-        year: year,
-        title: title
+      year: year,
+      title: title
     },
-    update_expression: "remove info.actors[0]",
-    condition_expression: "size(info.actors) > :num",
+    update_expression: 'REMOVE info.actors[0]',
+    condition_expression: 'size(info.actors) > :num',
     expression_attribute_values: {
-        ":num" => 3
+      ':num': 3
     },
-    return_values: "UPDATED_NEW"
-}
+    return_values: 'UPDATED_NEW'
+  }
 
-begin
-    result = dynamodb.update_item(params)
-    puts "Updated item. ReturnValues are:"
-    result.attributes["info"].each do |key, value| 
-        if key == "rating" 
-            puts "#{key}: #{value.to_f}" 
-        else
-            puts "#{key}: #{value}" 
-        end
-    end
+  puts "Updating table '#{table_name}' with information about " \
+    "'#{title} (#{year})'..."
 
-rescue  Aws::DynamoDB::Errors::ServiceError => error
-    puts "Unable to update item:"
-    puts "#{error.message}"
+  if table_item_updated?(dynamodb_client, table_item)
+    puts 'Table updated.'
+  else
+    puts 'Table not updated.'
+  end
 end
+
+run_me if $PROGRAM_NAME == __FILE__
 # snippet-end:[dynamodb.Ruby.CodeExample.MoviesItemOps05]
