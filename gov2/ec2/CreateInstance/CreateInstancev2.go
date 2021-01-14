d12a5e06d2bc71f6c2bc34c43a673629c1e70103
Sean McGrail <mcgrails@amazon.com>
2021-01-13 17:59:45 -0800

    Update Go V2 EC2 Examples

--- gov2/ec2/CreateInstance/CreateInstancev2.go
+++ gov2/ec2/CreateInstance/CreateInstancev2.go
@@ -4,26 +4,26 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
+	"context"
+	"flag"
+	"fmt"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/config"
-    "github.com/aws/aws-sdk-go-v2/service/ec2"
-    "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 // EC2CreateInstanceAPI defines the interface for the RunInstances and CreateTags functions.
 // We use this interface to test the functions using a mocked service.
 type EC2CreateInstanceAPI interface {
-    RunInstances(ctx context.Context,
-        params *ec2.RunInstancesInput,
-        optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
+	RunInstances(ctx context.Context,
+		params *ec2.RunInstancesInput,
+		optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error)
 
-    CreateTags(ctx context.Context,
-        params *ec2.CreateTagsInput,
-        optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
+	CreateTags(ctx context.Context,
+		params *ec2.CreateTagsInput,
+		optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error)
 }
 
 // MakeInstance creates an Amazon Elastic Compute Cloud (Amazon EC2) instance.
@@ -35,9 +35,7 @@ type EC2CreateInstanceAPI interface {
 //     If success, a RunInstancesOutput object containing the result of the service call and nil.
 //     Otherwise, nil and an error from the call to RunInstances.
 func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunInstancesInput) (*ec2.RunInstancesOutput, error) {
-    result, err := api.RunInstances(c, input)
-
-    return result, err
+	return api.RunInstances(c, input)
 }
 
 // MakeTags creates tags for an Amazon Elastic Compute Cloud (Amazon EC2) instance.
@@ -49,59 +47,58 @@ func MakeInstance(c context.Context, api EC2CreateInstanceAPI, input *ec2.RunIns
 //     If success, a CreateTagsOutput object containing the result of the service call and nil.
 //     Otherwise, nil and an error from the call to CreateTags.
 func MakeTags(c context.Context, api EC2CreateInstanceAPI, input *ec2.CreateTagsInput) (*ec2.CreateTagsOutput, error) {
-    result, err := api.CreateTags(c, input)
-
-    return result, err
+	return api.CreateTags(c, input)
 }
 
 func main() {
-    name := flag.String("n", "", "The name of the tag to attach to the instance")
-    value := flag.String("v", "", "The value of the tag to attach to the instance")
-    flag.Parse()
+	name := flag.String("n", "", "The name of the tag to attach to the instance")
+	value := flag.String("v", "", "The value of the tag to attach to the instance")
+	flag.Parse()
 
-    if *name == "" || *value == "" {
-        fmt.Println("You must supply a name and value for the tag (-n NAME -v VALUE)")
-        return
-    }
+	if *name == "" || *value == "" {
+		fmt.Println("You must supply a name and value for the tag (-n NAME -v VALUE)")
+		return
+	}
 
-    cfg, err := config.LoadDefaultConfig()
-    if err != nil {
-        panic("configuration error, " + err.Error())
-    }
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
 
-    client := ec2.NewFromConfig(cfg)
+	client := ec2.NewFromConfig(cfg)
 
-    input := &ec2.RunInstancesInput{
-        ImageId:      aws.String("ami-e7527ed7"),
-        InstanceType: types.InstanceTypeT2Micro,
-        MinCount:     aws.Int32(1),
-        MaxCount:     aws.Int32(1),
-    }
+	input := &ec2.RunInstancesInput{
+		ImageId:      aws.String("ami-e7527ed7"),
+		InstanceType: types.InstanceTypeT2Micro,
+		MinCount:     1,
+		MaxCount:     1,
+	}
 
-    result, err := MakeInstance(context.Background(), client, input)
-    if err != nil {
-        fmt.Println("Got an error creating an instance:")
-        fmt.Println(err)
-        return
-    }
+	result, err := MakeInstance(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error creating an instance:")
+		fmt.Println(err)
+		return
+	}
 
-    tagInput := &ec2.CreateTagsInput{
-        Resources: []*string{result.Instances[0].InstanceId},
-        Tags: []*types.Tag{
-            {
-                Key:   name,
-                Value: value,
-            },
-        },
-    }
+	tagInput := &ec2.CreateTagsInput{
+		Resources: []string{*result.Instances[0].InstanceId},
+		Tags: []types.Tag{
+			{
+				Key:   name,
+				Value: value,
+			},
+		},
+	}
 
-    _, err = MakeTags(context.Background(), client, tagInput)
-    if err != nil {
-        fmt.Println("Got an error tagging the instance:")
-        fmt.Println(err)
-        return
-    }
+	_, err = MakeTags(context.TODO(), client, tagInput)
+	if err != nil {
+		fmt.Println("Got an error tagging the instance:")
+		fmt.Println(err)
+		return
+	}
 
-    fmt.Println("Created tagged instance with ID " + *result.Instances[0].InstanceId)
+	fmt.Println("Created tagged instance with ID " + *result.Instances[0].InstanceId)
 }
+
 // snippet-end:[ec2.go-v2.CreateInstance]
--- gov2/ec2/CreateInstance/CreateInstancev2_test.go
+++ gov2/ec2/CreateInstance/CreateInstancev2_test.go
@@ -1,49 +1,45 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "errors"
-    "io/ioutil"
-    "testing"
-    "time"
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"testing"
+	"time"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/service/ec2"
-    "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
 
 type EC2CreateInstanceImpl struct{}
 
 func (dt EC2CreateInstanceImpl) RunInstances(ctx context.Context,
-    params *ec2.RunInstancesInput,
-    optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error) {
+	params *ec2.RunInstancesInput,
+	optFns ...func(*ec2.Options)) (*ec2.RunInstancesOutput, error) {
 
-    // Create a dummy instance and populate the InstanceId value
-    instances := make([]*types.Instance, 1)
-    instances[0] = &types.Instance{
-        InstanceId: aws.String("aws-docs-example-instanceID"),
-    }
+	// Create a dummy instance and populate the InstanceId value
+	instances := []types.Instance{
+		{InstanceId: aws.String("aws-docs-example-instanceID")},
+	}
 
-    output := &ec2.RunInstancesOutput{
-        Instances: instances,
-    }
+	output := &ec2.RunInstancesOutput{
+		Instances: instances,
+	}
 
-    return output, nil
+	return output, nil
 }
 
 func (dt EC2CreateInstanceImpl) CreateTags(ctx context.Context,
-    params *ec2.CreateTagsInput,
-    optFns ...func(*ec2.Options)) (*ec2.CreateTagsOutput, error) {
-
-    output := &ec2.CreateTagsOutput{}
-
-    return output, nil
+	params *ec2.CreateTagsInput,
+	optFns ...func(*ec2.Options)) (output *ec2.CreateTagsOutput, err error) {
+	return output, err
 }
 
 type Config struct {
-    TagName  string `json:"TagName"`
-    TagValue string `json:"TagValue"`
+	TagName  string `json:"TagName"`
+	TagValue string `json:"TagValue"`
 }
 
 var configFileName = "config.json"
@@ -51,68 +47,68 @@ var configFileName = "config.json"
 var globalConfig Config
 
 func populateConfiguration(t *testing.T) error {
-    content, err := ioutil.ReadFile(configFileName)
-    if err != nil {
-        return err
-    }
+	content, err := ioutil.ReadFile(configFileName)
+	if err != nil {
+		return err
+	}
 
-    text := string(content)
+	text := string(content)
 
-    err = json.Unmarshal([]byte(text), &globalConfig)
-    if err != nil {
-        return err
-    }
+	err = json.Unmarshal([]byte(text), &globalConfig)
+	if err != nil {
+		return err
+	}
 
-    if globalConfig.TagName == "" || globalConfig.TagValue == "" {
-        msg := "You must specify a value for TagName and TagValue in " + configFileName
-        return errors.New(msg)
-    }
+	if globalConfig.TagName == "" || globalConfig.TagValue == "" {
+		msg := "You must specify a value for TagName and TagValue in " + configFileName
+		return errors.New(msg)
+	}
 
-    return nil
+	return nil
 }
 
 func TestCreateInstance(t *testing.T) {
-    thisTime := time.Now()
-    nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
-    t.Log("Starting unit test at " + nowString)
+	thisTime := time.Now()
+	nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
+	t.Log("Starting unit test at " + nowString)
 
-    err := populateConfiguration(t)
-    if err != nil {
-        t.Fatal(err)
-    }
+	err := populateConfiguration(t)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    api := &EC2CreateInstanceImpl{}
+	api := &EC2CreateInstanceImpl{}
 
-    input := &ec2.RunInstancesInput{
-        ImageId:      aws.String("ami-e7527ed7"),
-        InstanceType: types.InstanceTypeT2Micro,
-        MinCount:     aws.Int32(1),
-        MaxCount:     aws.Int32(1),
-    }
+	input := &ec2.RunInstancesInput{
+		ImageId:      aws.String("ami-e7527ed7"),
+		InstanceType: types.InstanceTypeT2Micro,
+		MinCount:     1,
+		MaxCount:     1,
+	}
 
-    result, err := MakeInstance(context.Background(), api, input)
-    if err != nil {
-        t.Log("Got an error creating an instance:")
-        t.Log(err)
-        return
-    }
+	result, err := MakeInstance(context.Background(), api, input)
+	if err != nil {
+		t.Log("Got an error creating an instance:")
+		t.Log(err)
+		return
+	}
 
-    tagInput := &ec2.CreateTagsInput{
-        Resources: []*string{result.Instances[0].InstanceId},
-        Tags: []*types.Tag{
-            {
-                Key:   &globalConfig.TagName,
-                Value: &globalConfig.TagValue,
-            },
-        },
-    }
+	tagInput := &ec2.CreateTagsInput{
+		Resources: []string{*result.Instances[0].InstanceId},
+		Tags: []types.Tag{
+			{
+				Key:   &globalConfig.TagName,
+				Value: &globalConfig.TagValue,
+			},
+		},
+	}
 
-    _, err = MakeTags(context.Background(), api, tagInput)
-    if err != nil {
-        t.Log("Got an error tagging the instance:")
-        t.Log(err)
-        return
-    }
+	_, err = MakeTags(context.Background(), api, tagInput)
+	if err != nil {
+		t.Log("Got an error tagging the instance:")
+		t.Log(err)
+		return
+	}
 
-    t.Log("Created tagged instance with ID " + *result.Instances[0].InstanceId)
+	t.Log("Created tagged instance with ID " + *result.Instances[0].InstanceId)
 }
