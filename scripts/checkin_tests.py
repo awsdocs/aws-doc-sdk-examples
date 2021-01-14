8e4a63228065716a3c7e5bc5611203915b343187
Brian Murray <brmur@amazon.com>
2021-01-14 19:04:39 +0000

    edits implemented

--- scripts/checkin_tests.py
+++ scripts/checkin_tests.py
@@ -194,7 +194,8 @@ def verify_no_secret_keys(file_contents, file_location):
                      "access key. If not, add it to the allow list in https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/scripts/checkin_tests.py, "
                       "and in and in src/allow.list.xml in the AWSDocsChecklistCodeSampleCatalog GitFarm repo.", {word[1:-1]}, file_location)
         error_count += 1
-    return error_count
+
+     return error_count
 
 
 def verify_snippet_start_end(file_contents, file_location):
