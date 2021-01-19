402cf4bea97b590fd30884c53d244b4eb382643c
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-01-19 11:32:33 -0500

    Add files via upload

--- javav2/example_code/secretsmanager/src/main/java/com/example/secrets/CreateSecret.java
+++ javav2/example_code/secretsmanager/src/main/java/com/example/secrets/CreateSecret.java
@@ -21,6 +21,15 @@ import software.amazon.awssdk.services.secretsmanager.model.CreateSecretResponse
 import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
 //snippet-end:[secretsmanager.java2.create_secret.import]
 
+/**
+ * To run this AWS code example, ensure that you have setup your development environment, including your AWS credentials.
+ *
+ * For information, see this documentation topic:
+ *
+ *https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+
+
 public class CreateSecret {
 
     public static void main(String[] args) {
