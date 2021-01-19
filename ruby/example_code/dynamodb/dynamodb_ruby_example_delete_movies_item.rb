33af169e44833317f6f9d476ba4eb3ae9236f82c
Paul Cornell <pccornel@amazon.com>
2021-01-19 14:32:33 -0800

    Cleaning up next set of Ruby code examples for Amazon DynamoDB

--- ruby/example_code/dynamodb/dynamodb_ruby_example_delete_movies_item.rb
+++ ruby/example_code/dynamodb/dynamodb_ruby_example_delete_movies_item.rb
@@ -1,43 +1,57 @@
-# snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-# snippet-sourceauthor:[Doug-AWS]
-# snippet-sourcedescription:[Removes an item from a DynamoDB table.]
-# snippet-keyword:[Amazon DynamoDB]
-# snippet-keyword:[delete_item method]
-# snippet-keyword:[Ruby]
-# snippet-sourcesyntax:[ruby]
-# snippet-service:[dynamodb]
-# snippet-keyword:[Code Sample]
-# snippet-sourcetype:[full-example]
-# snippet-sourcedate:[2018-03-16]
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+require 'aws-sdk-dynamodb'
+
+# Deletes an item from a table in Amazon DynamoDB.
 #
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-
-require 'aws-sdk-dynamodb'  # v2: require 'aws-sdk'
-
-# Create dynamodb client in us-west-2 region
-dynamodb = Aws::DynamoDB::Client.new(region: 'us-west-2')
-
-params = {
-    table_name: 'Movies',
-    key: {
-        year: 2015,
-        title: 'The Big New Movie'
-    }
-}
-
-begin
-  dynamodb.delete_item(params)
-  puts 'Deleted movie'
-rescue  Aws::DynamoDB::Errors::ServiceError => error
-  puts 'Unable to delete movie:'
-  puts error.message
+# @param dynamodb_client [Aws::DynamoDB::Client] An initialized
+#   Amazon DynamoDB client.
+# @param table_item [Hash] The properties of the item, in the correct format.
+# @return [Boolean] true if the item was deleted; otherwise, false.
+# @example
+#   ext 1 unless item_deleted_from_table?(
+#     Aws::DynamoDB::Client.new(region, 'us-west-2'),
+#     {
+#       table_name: 'Movies',
+#       key: {
+#         year: 2015,
+#         title: 'The Big New Movie'
+#       }
+#     }
+#   )
+def item_deleted_from_table?(dynamodb_client, table_item)
+  dynamodb_client.delete_item(table_item)
+  true
+rescue StandardError => e
+  puts "Error deleting item: #{e.message}"
+  false
 end
+
+# Full example call:
+def run_me
+  region = 'us-west-2'
+  table_name = 'Movies'
+  year = 2015
+  title = 'The Big New Movie'
+
+  dynamodb_client = Aws::DynamoDB::Client.new(region: region)
+
+  table_item = {
+    table_name: table_name,
+    key: {
+      year: year,
+      title: title
+    }
+  }
+
+  puts "Deleting movie '#{title} (#{year})' from the '#{table_name}' table..."
+
+  if item_deleted_from_table?(dynamodb_client, table_item)
+    puts 'Item deleted.'
+  else
+    puts 'Item not deleted.'
+  end
+end
+
+run_me if $PROGRAM_NAME == __FILE__
