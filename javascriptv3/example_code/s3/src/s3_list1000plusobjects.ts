ec7813b623b3dd1b1c852f0a33f031422524f6d9
April Polubiec <51533324+AprilPolubiec@users.noreply.github.com>
2021-01-21 12:37:31 -0600

    Update s3_list1000plusobjects.ts
    Fixes infinite loop created by never adding pageMarker to the ListObjectsCommand input params

--- javascriptv3/example_code/s3/src/s3_list1000plusobjects.ts
+++ javascriptv3/example_code/s3/src/s3_list1000plusobjects.ts
@@ -41,6 +41,7 @@ async function run() {
       // If 'truncated' is true, assign the key of the final element in the response to our variable 'pageMarker'
       if (truncated) {
         pageMarker = response.Contents.slice(-1)[0].Key;
+        bucketParams.Marker = pageMarker;
       }
       // At end of the list, response.truncated is false and our function exits the while loop.
     } catch (err) {
