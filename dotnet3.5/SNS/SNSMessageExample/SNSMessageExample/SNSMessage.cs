7ca55f7c343b8a98729a2ed1f74d125960380a14
Irene Smith <igsmith@amazon.com>
2021-01-27 12:39:37 -0500

    Adding an SNS example.

---
+++ dotnet3.5/SNS/SNSMessageExample/SNSMessageExample/SNSMessage.cs
@@ -0,0 +1,61 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace SNSMessageExample
+{
+    class SNSMessage
+    {
+        private AmazonSimpleNotificationServiceClient snsClient;
+
+        /// <summary>
+        /// Constructs a new SNSMessage object initializing the Amazon Simple
+        /// the Notification Service (Amazon SNS) client using the supplied
+        /// region endpoint.
+        /// </summary>
+        /// <param name="regionEndpoint">The Amazon region endpoint to use in
+        /// sending test messages with this object.</param>
+        public SNSMessage(RegionEndpoint regionEndpoint)
+        {
+            snsClient = new AmazonSimpleNotificationServiceClient(regionEndpoint);
+        }
+
+        /// <summary>
+        /// Sends the SMS message passed in the text parameter to the phone number
+        /// in phoneNum.
+        /// </summary>
+        /// <param name="phoneNum">The ten-digit phone number to which the text
+        /// message will be sent.</param>
+        /// <param name="text">The text of the message to send.</param>
+        /// <returns></returns>
+        public async Task SendTextMessageAsync(string phoneNum, string text)
+        {
+            if (string.IsNullOrEmpty(phoneNum) || string.IsNullOrEmpty(text))
+            {
+                return;
+            }
+
+            // Now actually send the message.
+            var request = new PublishRequest
+            {
+                Message = text,
+                PhoneNumber = phoneNum
+            };
+
+            try
+            {
+                var response = await snsClient.PublishAsync(request);
+            }
+            catch (AmazonSimpleNotificationServiceException ex)
+            {
+                Console.WriteLine($"Error sending message: {ex}");
+            }
+        }
+
+    }
+}
