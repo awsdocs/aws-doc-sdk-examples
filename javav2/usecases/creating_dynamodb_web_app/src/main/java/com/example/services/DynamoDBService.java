74250816ad2a5f65a493ff42ffbe7a69f140bda1
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-02-03 11:04:40 -0500

    Update DynamoDBService.java

--- javav2/usecases/creating_dynamodb_web_app/src/main/java/com/example/services/DynamoDBService.java
+++ javav2/usecases/creating_dynamodb_web_app/src/main/java/com/example/services/DynamoDBService.java
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 package com.example.services;
 
 import com.example.entities.WorkItem;
