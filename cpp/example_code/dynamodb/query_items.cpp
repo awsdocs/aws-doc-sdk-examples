e1be50d3bd4c54419cd4725b16a9612c8ace59d5
Sarfraz Siddiqui <grandmaster.carlson@outlook.com>
2021-02-05 22:46:06 +0530

    Update license text

--- cpp/example_code/dynamodb/query_items.cpp
+++ cpp/example_code/dynamodb/query_items.cpp
@@ -10,19 +10,9 @@
 //snippet-sourceauthor:[Sarfraz]
 
 
-/*
-Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0 
 
-This file is licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License. A copy of
-the License is located at
-
-http://aws.amazon.com/apache2.0/
-
-This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-*/
 //snippet-start:[dynamodb.cpp.query_items.inc]
 #include <aws/core/Aws.h>
 #include <aws/core/utils/Outcome.h> 
