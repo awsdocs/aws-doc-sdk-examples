dfb902d747dfa9f22feef71030e951eeb772c52a
Brian Murray <brmur@amazon.com>
2021-02-17 16:45:25 +0000

    updates to Amazon Kinesis example

--- resources/cdk/kenesis-example-create-stream/setup.ts
+++ resources/cdk/kenesis-example-create-stream/setup.ts
@@ -3,23 +3,26 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// TODO: Customize this AWS CDK code for your specific AWS CDK solution.
 // For more information, see the AWS CDK Developer Guide at
 // https://docs.aws.amazon.com/cdk/latest/guide
 
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
+import { CfnOutput } from "@aws-cdk/core";
 import * as cognito from "@aws-cdk/aws-cognito";
 import * as iam from "@aws-cdk/aws-iam";
 import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
-import { Stream } from "@aws-cdk/aws-kinesis";
+import {Stream} from "@aws-cdk/aws-kinesis";
 
 export class SetupStackCreateTable extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myStream = new Stream(this, "MyFirstStream", {
-      streamName: "my-awesome-stream",
-    });
+   const myStream =   new Stream(this, "MyFirstStream", {
+          streamName: `my-stream-${Math.ceil(Math.random() * 10 ** 10)}`
+      });
+
 
     const myIdentityPool = new cognito.CfnIdentityPool(
       this,
@@ -50,19 +53,17 @@ export class SetupStackCreateTable extends cdk.Stack {
 
     unauthenticatedRole.addToPolicy(
       new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: ["mobileanalytics:PutEvents", "cognito-sync:*"],
-        resources: ["*"],
-      })
-    );
+            effect: Effect.ALLOW,
+            actions: ["mobileanalytics:PutEvents", "cognito-sync:*"],
+            resources: ["*"],
+          }));
 
-    unauthenticatedRole.addToPolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: ["kinesis:Put*"],
-        resources: [myStream.streamArn],
-      })
-    );
+      unauthenticatedRole.addToPolicy(
+          new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ["kinesis:Put*"],
+              resources: [myStream.streamArn],
+          }));
 
     const defaultPolicy = new cognito.CfnIdentityPoolRoleAttachment(
       this,
@@ -74,6 +75,9 @@ export class SetupStackCreateTable extends cdk.Stack {
         },
       }
     );
+      new CfnOutput(this, 'Identity pool id', {value: myIdentityPool.ref})
+      new CfnOutput(this, 'Stack name', {value: myStream.streamName})
+      new CfnOutput(this, 'Stack arn', {value: myStream.streamArn})
   }
 }
 
