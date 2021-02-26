5990601e01ce0e18a98fa777986cd36172e177d1
Brian Murray <brmur@amazon.com>
2021-02-26 12:33:55 +0000

    - minor changes to lambda-api-gateway example (JS) - fixed link, and deleted duplicated content from CDK readme

--- javascriptv3/example_code/cross-services/lambda-api-gateway/src/helper-functions/lambda-function-setup.ts
+++ javascriptv3/example_code/cross-services/lambda-api-gateway/src/helper-functions/lambda-function-setup.ts
@@ -7,7 +7,7 @@ https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/cross-service-
 
 Purpose:
 lambda-function-setup.ts demonstrates how to create an AWS Lambda function.
-It is part of a tutorial demonstrates how to create a REST API using API Gateway that triggers a AWS Lambda function that scans an
+It is part of a tutorial demonstrates how to create a REST API using API Gateway that triggers a Lambda function that scans an
 Amazon DynamoDB table of employees' information and send an Amazon Simple Notification Service (Amazon SNS)
 message based on the results. It demonstrates how toTo run the full tutorial, see
 https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/cross-service-example-scan-and-publish-message.html.
@@ -24,7 +24,7 @@ ts-node lambda-function-setup.ts
 */
 
 // snippet-start:[lambda.JavaScript.general-examples-dynamodb-lambda.LambdaFunctionSetUpV3]
-// Load the required AWS Lambda client and commands.
+// Load the required Lambda client and commands.
 const {
   LambdaClient,
   CreateFunctionCommand,
@@ -33,7 +33,7 @@ const {
 // Set the AWS Region.
 const REGION = "REGION"; //e.g. "us-east-1"
 
-// Instantiate an AWS Lambda client service object.
+// Instantiate an Lambda client service object.
 const lambda = new LambdaClient({ region: REGION });
 
 // Set the parameters.
