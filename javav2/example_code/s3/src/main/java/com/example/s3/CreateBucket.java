b751ad56024d4cb1c36014864338e37c21a967fb
Doug Schwartz <dougsch@amazon.com>
2021-03-09 11:20:10 -0800

    Go CI test that should PASS

--- javav2/example_code/s3/src/main/java/com/example/s3/CreateBucket.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/CreateBucket.java
@@ -8,7 +8,7 @@
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
+   SPDX-License-Identifier: Apache-2.0 
 */
 
 package com.example.s3;
