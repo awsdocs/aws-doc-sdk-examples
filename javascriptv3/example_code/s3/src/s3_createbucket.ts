e087722725ca14030e9ab819ce5d99fc1c07c765
Doug Schwartz <dougsch@amazon.com>
2021-03-09 11:18:04 -0800

    Go CI test that should PASS

--- javascriptv3/example_code/s3/src/s3_createbucket.ts
+++ javascriptv3/example_code/s3/src/s3_createbucket.ts
@@ -1,4 +1,4 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
 SPDX-License-Identifier: Apache-2.0
 ABOUT THIS NODE.JS EXAMPLE: This example works with AWS SDK for JavaScript version 3 (v3),
 which is available at https://github.com/aws/aws-sdk-js-v3. This example is in the 'AWS SDK for JavaScript v3 Developer Guide' at
