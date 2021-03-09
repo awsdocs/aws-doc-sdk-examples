3eaa8c610eaa1b2ddd57728047e69c5301562db1
Doug Schwartz <dougsch@amazon.com>
2021-03-09 09:57:02 -0800

    Go CI test that should PASS

--- dotnet3.5/S3/CreateBucketExample/CreateBucket/CreateBucket.cs
+++ dotnet3.5/S3/CreateBucketExample/CreateBucket/CreateBucket.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX - License - Identifier: Apache - 2.0
+// SPDX-License-Identifier: Apache-2.0
 
 // snippet-start:[S3.dotnet35.CreateBucket]
 
--- dotnet3.5/S3/CreateBucketExample/CreateBucketTest/CreateBucketTest.cs
+++ dotnet3.5/S3/CreateBucketExample/CreateBucketTest/CreateBucketTest.cs
@@ -1,5 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX - License - Identifier: Apache - 2.0
+// SPDX-License-Identifier: Apache-2.0
 
 using Amazon.S3;
 using Amazon.S3.Model;
