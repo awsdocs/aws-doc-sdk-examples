0b7d52ac10ebfa72b8ef0efd3dfb935298e348c8
Brian Murray <brmur@amazon.com>
2021-03-18 12:23:55 +0000

    - Add multipartupload to S3 example

---
+++ javascriptv3/example_code/tests/s3/s3_multipartupload.test.js
@@ -0,0 +1,41 @@
+const mockCreateMultipartUpload = jest.fn();
+jest.mock("@aws-sdk/client-s3/Commands/CreateMultipartUploadCommand", () => ({
+    S3: function S3() {
+        this.CreateMultipartUploadCommand = mockCreateMultipartUpload;
+    },
+}));
+const { run } = require("../../s3/s3_multipartupload");
+
+test("has to mock S3#CreateMultiPartUpload", async (done) => {
+    await run();
+    expect(mockCreateMultipartUpload).toHaveBeenCalled;
+    done();
+});
+
+const mockUploadPartCommand = jest.fn();
+jest.mock("@aws-sdk/client-s3/Commands/UploadPartCommand", () => ({
+    S3: function S3() {
+        this.UploadPartCommand = mockUploadPartCommand;
+    },
+}));
+const { run } = require("../../s3/s3_multipartupload");
+
+test("has to mock S3#mUploadPart", async (done) => {
+    await run();
+    expect(mockUploadPartCommand).toHaveBeenCalled;
+    done();
+});
+
+const mockCompleteMultipartUploadCommand = jest.fn();
+jest.mock("@aws-sdk/client-s3/Commands/CompleteMultipartUploadCommand", () => ({
+    S3: function S3() {
+        this.CompleteMultipartUploadCommand = mockCompleteMultipartUploadCommand;
+    },
+}));
+const { run } = require("../../s3/s3_multipartupload");
+
+test("has to mock S3#mCompleteUploadPart", async (done) => {
+    await run();
+    expect(mockCompleteMultipartUploadCommand).toHaveBeenCalled;
+    done();
+});
