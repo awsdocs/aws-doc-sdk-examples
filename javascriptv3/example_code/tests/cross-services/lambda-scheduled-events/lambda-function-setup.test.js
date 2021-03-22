5a05939d8e4756cb105a47fd8dd240a2e179fa13
Brian Murray <brmur@amazon.com>
2021-03-22 15:20:56 +0000

    Add cross-service example demonstrating how to execute Lambda functions using Amazon CloudWatch scheduled events.

--- javascriptv3/example_code/tests/cross-services/lambda-scheduled-events/lambda-function-setup.test.js
+++ javascriptv3/example_code/tests/cross-services/lambda-scheduled-events/lambda-function-setup.test.js
@@ -4,7 +4,7 @@ jest.mock("@aws-sdk/client-lambda/commands/CreateFunctionCommand", () => ({
         this.CreateFunctionCommand = mockCreateFunction;
     },
 }));
-const { run } = require("../../../cross-services/lambda-api-gateway/src/helper-functions/lambda-function-setup");
+const { run } = require("../../../cross-services/lambda-scheduled-events/src/helper-functions/lambda-function-setup");
 
 test("has to mock db#createfunction", async (done) => {
     await run();
