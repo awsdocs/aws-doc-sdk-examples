7926caa823fc682f981ab5fe9453c5a5e100b965
Irene Smith <igsmith@amazon.com>
2021-03-23 09:47:46 -0400

    Creating SQS examples.

---
+++ dotnet3.5/SQS/SQSExample1/SQSExample/SQSExample.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier:  Apache-2.0
+
+using System;
+
+namespace SQSExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ dotnet3.5/SQS/SQSExample1/SQSExampleTests/SQSExampleUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SQSExampleTests
+{
+    public class SQSExampleUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
