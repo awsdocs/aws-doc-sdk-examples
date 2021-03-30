851197f7fde8967cbed4eaa26de9e50a4adfc646
Irene Smith <igsmith@amazon.com>
2021-03-30 11:15:45 -0400

    Added tests to examples. Ready for merge.

---
+++ dotnet3.5/SQS/ReceiveDeleteExample/ReceiveDeleteExample/ReceiveDeleteExample.cs
@@ -0,0 +1,91 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier:  Apache-2.0
+
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace ReceiveDeleteExample
+{
+    class ReceiveDeleteExample
+    {
+        // This example will retrieve a single message from an Amazon Simple
+        // Queue Service (Amazon SQS) queue and then will delete the message.
+        // This example was creating using the AWS SDK for .NET version 3.x
+        // and .NET 5.0.
+
+        // Specify your AWS Region (an example Region is shown).
+        private static readonly RegionEndpoint _endpoint = RegionEndpoint.USEast2; // RegionEndpoint.USWest2;
+        private static IAmazonSQS _client;
+
+        static async Task Main()
+        {
+            _client = new AmazonSQSClient(_endpoint);
+            string queueName = "Example_Queue";
+
+            var queueUrl = await GetQueueUrl(_client, queueName);
+            Console.WriteLine($"The SQS queue's URL is {queueUrl}");
+
+            var response = await ReceiveAndDeleteMessage(_client, queueUrl);
+
+            Console.WriteLine($"Message: {response.Messages[0]}");
+        }
+
+        /// <summary>
+        /// Retrieve the queue URL for the queue named in the queueName
+        /// property using the client object.
+        /// </summary>
+        /// <param name="client">The Amazon SQS client used to retrieve the
+        /// queue URL.</param>
+        /// <param name="queueName">A string representing  name of the queue
+        /// for which to retrieve the URL.</param>
+        /// <returns></returns>
+        private static async Task<string> GetQueueUrl(IAmazonSQS client, string queueName)
+        {
+            var request = new GetQueueUrlRequest
+            {
+                QueueName = queueName
+            };
+
+            GetQueueUrlResponse response = await client.GetQueueUrlAsync(request);
+            return response.QueueUrl;
+        }
+
+        /// <summary>
+        /// Retrieves the message from the quque at the URL pssed in the
+        /// queueURL parameters using the client.
+        /// </summary>
+        /// <param name="client">The SQS client used to retrieve a message.</param>
+        /// <param name="queueUrl">The URL of the queue from which to retrieve
+        /// a message.</param>
+        /// <returns></returns>
+        public static async Task<ReceiveMessageResponse> ReceiveAndDeleteMessage(IAmazonSQS client, string queueUrl)
+        {
+            // Receive a single message from the queue.
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                AttributeNames = { "SentTimestamp" },
+                MaxNumberOfMessages = 1,
+                MessageAttributeNames = { "All" },
+                QueueUrl = queueUrl,
+                VisibilityTimeout = 0,
+                WaitTimeSeconds = 0
+            };
+
+            var receiveMessageResponse = await client.ReceiveMessageAsync(receiveMessageRequest);
+
+            // Delete the received message from the queue.
+            var deleteMessageRequest = new DeleteMessageRequest
+            {
+                QueueUrl = queueUrl,
+                ReceiptHandle = receiveMessageResponse.Messages[0].ReceiptHandle
+            };
+
+            await client.DeleteMessageAsync(deleteMessageRequest);
+
+            return receiveMessageResponse;
+        }
+    }
+}
