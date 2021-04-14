91ed03da6618371acf66385e92766c9dd19865bb
Paul Bartos <pbart@amazon.com>
2021-04-14 17:34:55 +0000

    Pulling file from primary repo in hopes that it will fix errors in PR checks.

---
+++ scripts/run_go_tests.sh
@@ -0,0 +1,38 @@
+TestGoFile () {
+    echo Testing $1
+    pushd $1
+
+    declare RESULT=(`go test`)  # (..) = array
+
+    popd
+    
+#    echo RESULT in TestGoFile: $RESULT
+    
+    if [ "${RESULT[0]}" != "PASS" ]
+    then
+        exit 1
+    fi
+}
+
+for f in $@ ; do
+#    echo Looking at $f
+    # Do any end with "_test.go"?
+    path="$(dirname $f)"
+    file="$(basename $f)"
+
+#    echo Path is $path
+#    echo filename is $file
+
+    # If it's a go test file
+    # test it
+    case $file in
+        *[a-zA-Z]*_test.go)
+#    	    echo It IS a go test file
+            TestGoFile "$path"
+	    ;;
+        *)
+#    	    echo It is NOT a go test file
+    esac
+done
+
+exit 0
