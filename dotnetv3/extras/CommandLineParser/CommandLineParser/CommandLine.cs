c70c9061a1f7851bfaa4290af8337736f61c0217
Paul Bartos <pbart@amazon.com>
2021-04-27 01:36:58 +0000

    Adding code for a command-line parser used in .NET console apps.

---
+++ dotnetv3/extras/CommandLineParser/CommandLineParser/CommandLine.cs
@@ -0,0 +1,90 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Purpose:
+// CommandLine.cs contains a class for parsing command lines in .NET console applications.
+// Include this class in your namespace and use it in a manner that's similar to
+//  the code shown in the following companion test file:
+//  "github.com/awsdocs/aws-doc-sdk-examples/dotnetv3/extras/CommandLineParser/CommandLineParserTestClass.cs"
+
+using System;
+using System.Collections.Generic;
+
+namespace CommandLineParser
+{
+  // snippet-start:[none.dotnet.CommandLineParserClass]
+
+  // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+  // Class that represents a command line on the console or terminal.
+  // (This is the same for all examples. When you have seen it once, you can ignore it.)
+  static class CommandLine
+  {
+    //
+    // Method to parse a command line of the form: "--key value" or "-k value".
+    //
+    // Parameters:
+    // - args: The command-line arguments passed into the application by the system.
+    //
+    // Returns:
+    // A Dictionary with string Keys and Values.
+    //
+    // If a key is found without a matching value, Dictionary.Value is set to the key
+    //  (including the dashes).
+    // If a value is found without a matching key, Dictionary.Key is set to "--NoKeyN",
+    //  where "N" represents sequential numbers.
+    public static Dictionary<string,string> Parse(string[] args)
+    {
+      var parsedArgs = new Dictionary<string,string>();
+      int i = 0, n = 0;
+      while(i < args.Length)
+      {
+        // If the first argument in this iteration starts with a dash it's an option.
+        if(args[i].StartsWith("-"))
+        {
+          var key = args[i++];
+          var value = key;
+
+          // Check to see if there's a value that goes with this option?
+          if((i < args.Length) && (!args[i].StartsWith("-"))) value = args[i++];
+          parsedArgs.Add(key, value);
+        }
+
+        // If the first argument in this iteration doesn't start with a dash, it's a value
+        else
+        {
+          parsedArgs.Add("--NoKey" + n.ToString(), args[i++]);
+          n++;
+        }
+      }
+
+      return parsedArgs;
+    }
+
+    //
+    // Method to get an argument from the parsed command-line arguments
+    //
+    // Parameters:
+    // - parsedArgs: The Dictonary object returned from the Parse() method (shown above).
+    // - defaultValue: The default string to return if the specified key isn't in parsedArgs.
+    // - keys: An array of keys to look for in parsedArgs.
+    public static string GetArgument(
+      Dictionary<string,string> parsedArgs, string defaultReturn, params string[] keys)
+    {
+      string retval = null;
+      foreach(var key in keys)
+        if(parsedArgs.TryGetValue(key, out retval)) break;
+      return retval ?? defaultReturn;
+    }
+
+    //
+    // Method to exit the applicaiton with an error.
+    public static void ErrorExit(string msg, int code=1)
+    {
+      Console.WriteLine("\nError");
+      Console.WriteLine(msg);
+      Environment.Exit(code);
+    }
+  }
+  // snippet-end:[none.dotnet.CommandLineParserClass]
+
+}
