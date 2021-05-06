6bbefc0168132ab5e5c69223d1cb059e4efff714
Brian Murray <brmur@amazon.com>
2021-05-06 16:47:16 +0100

    updating DDB Doc Client example to use lib

--- javascriptv3/example_code/dynamodb/src/ddbdoc_query_item.ts
+++ javascriptv3/example_code/dynamodb/src/ddbdoc_query_item.ts
@@ -6,7 +6,7 @@ which is available at https://github.com/aws/aws-sdk-js-v3. This example is in t
 https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html.
 
 Purpose:
-ddbdoc_update_query.ts demonstrates how to use Amazon DynamoDB utilities to retrieve items from an Amazon DynamoDB table.
+ddbdoc_update_query.ts demonstrates how to use the Amazon DynamoDB document client to query items from an Amazon DynamoDB table.
 
 Inputs (replace in code):
 - TABLE_NAME
@@ -16,10 +16,33 @@ Running the code:
 ts-node ddbdoc_update_query.ts
 */
 // snippet-start:[dynamodb.JavaScript.docClient.queryV3]
+const {
+  DynamoDBDocumentClient,
+  QueryCommand,
+} = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
-// Import the required AWS SDK clients and command for Node.js
-const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
-const { marshall } = require("@aws-sdk/util-dynamodb");
+const REGION = "eu-west-1";
+
+const marshallOptions = {
+  // Whether to automatically convert empty strings, blobs, and sets to `null`.
+  convertEmptyValues: false, // false, by default.
+  // Whether to remove undefined values while marshalling.
+  removeUndefinedValues: false, // false, by default.
+  // Whether to convert typeof object to map attribute.
+  convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+  // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+  wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+
+// Create the clients.
+const client = new DynamoDBClient({ region: REGION });
+const ddbDocClient = DynamoDBDocumentClient.from(client, translateConfig); // client is DynamoDB client
 
 // Set the parameters
 const params = {
@@ -35,29 +58,24 @@ const params = {
   MapAttribute: { foo: "bar" },
   NullAttribute: null
    */
-  ExpressionAttributeValues: marshall({
-    ":s": 2,
-    ":e": 9,
-    ":topic": "The Return",
-  }),
+  ExpressionAttributeValues: {
+    ":s": 1,
+    ":e": 1,
+    ":topic": "Title2",
+  },
   // Specifies the values that define the range of the retrieved items. In this case, items in Season 2 before episode 9.
   KeyConditionExpression: "Season = :s and Episode > :e",
   // Filter that returns only episodes that meet previous criteria and have the subtitle 'The Return'
   FilterExpression: "contains (Subtitle, :topic)",
 };
 
-// Create DynamoDB client
-const client = new DynamoDBClient({ region: "REGION" });
-
 const run = async () => {
   try {
-    const data = await client.send(new QueryCommand(params));
-    console.log("Success - query");
-    console.log(data.Items);
+    const data = await ddbDocClient.send(new QueryCommand(params));
+    console.log("Success. Item details: ", data.Items);
   } catch (err) {
     console.log("Error", err);
   }
 };
 run();
-
 // snippet-end:[dynamodb.JavaScript.docClient.queryV3]
