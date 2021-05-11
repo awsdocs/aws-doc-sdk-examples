4bf63fb9a08448442391424a7fb5c0a7ae5f571b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-05-11 08:38:22 -0400

    Add files via upload

--- javav2/example_code/comprehend/src/main/java/com/example/comprehend/DetectLanguage.java
+++ javav2/example_code/comprehend/src/main/java/com/example/comprehend/DetectLanguage.java
@@ -24,7 +24,13 @@ import java.util.Iterator;
 import java.util.List;
 //snippet-end:[comprehend.java2.detect_language.import]
 
-
+/**
+ * To run this Java V2 code example, ensure that you have setup your development environment, including your credentials.
+ *
+ * For information, see this documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
 public class DetectLanguage {
 
     public static void main(String[] args) {
