4efc6c343f876041dde961e5b4dbe5f2526117f3
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-05-11 08:45:39 -0400

    Add files via upload

--- javav2/example_code/eventbridge/src/main/java/com/example/eventbridge/DeleteRule.java
+++ javav2/example_code/eventbridge/src/main/java/com/example/eventbridge/DeleteRule.java
@@ -21,6 +21,13 @@ import software.amazon.awssdk.services.eventbridge.model.DeleteRuleRequest;
 import software.amazon.awssdk.services.eventbridge.model.DisableRuleRequest;
 // snippet-end:[eventbridge.java2._delete_rule.import]
 
+/**
+ * To run this Java V2 code example, ensure that you have setup your development environment, including your credentials.
+ *
+ * For information, see this documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
 
 public class DeleteRule {
 
