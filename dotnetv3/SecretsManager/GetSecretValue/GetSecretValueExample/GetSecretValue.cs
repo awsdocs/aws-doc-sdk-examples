f8923ab8de498c8bc44418435710e62496bfc95d
Irene Smith <igsmith@amazon.com>
2021-05-11 09:58:14 -0400

    Creating an example of getting a Secret Value from AWS Secrets Manager.

---
+++ dotnetv3/SecretsManager/GetSecretValue/GetSecretValueExample/GetSecretValue.cs
@@ -0,0 +1,141 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+
+namespace GetSecretValueExample
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Amazon;
+    using Amazon.SecretsManager;
+    using Amazon.SecretsManager.Model;
+
+    /// <summary>
+    /// This example retrieves the secret value for the provided secret name.
+    /// This example was created using the AWS SDK for .NET v3.7 and
+    /// .NET Core 5.
+    /// </summary>
+    public class GetSecretValue
+    {
+        public static async Task Main()
+        {
+            string secretName = "<<{{MySecretName}}>>";
+            string secret;
+
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient();
+
+            var response = await GetSecretAsync(client, secretName);
+
+            if (response is not null)
+            {
+                secret = DecodeString(response);
+
+                if (!string.IsNullOrEmpty(secret))
+                {
+                    Console.WriteLine($"The decoded secret value is: {secret}.");
+                }
+                else
+                {
+                    Console.WriteLine("No secret value was returned.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the secret value given the name of the secret to
+        /// retrieve.
+        /// </summary>
+        /// <param name="client">The client object used to retrieve the secret
+        /// value for the given secret name.</param>
+        /// <param name="secretName">The name of the secret value to retrieve.</param>
+        /// <returns>Returns the response from calling GetSecretValueAsync.</returns>
+        public static async Task<GetSecretValueResponse> GetSecretAsync(
+            IAmazonSecretsManager client,
+            string secretName)
+        {
+            GetSecretValueRequest request = new ();
+            request.SecretId = secretName;
+            request.VersionStage = "AWSCURRENT"; // VersionStage defaults to AWSCURRENT if unspecified.
+
+            GetSecretValueResponse response = null;
+
+            // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+            // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            // We rethrow the exception by default.
+
+            try
+            {
+                response = await client.GetSecretValueAsync(request);
+            }
+            catch (DecryptionFailureException e)
+            {
+                // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (InternalServiceErrorException e)
+            {
+                // An error occurred on the server side.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (InvalidParameterException e)
+            {
+                // You provided an invalid value for a parameter.
+                // Deal with the exception here, and/or rethrow at your discretion
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (InvalidRequestException e)
+            {
+                // You provided a parameter value that is not valid for the current state of the resource.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (ResourceNotFoundException e)
+            {
+                // We can't find the resource that you asked for.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (AggregateException e)
+            {
+                // More than one of the above exceptions were triggered.
+                // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (AmazonSecretsManagerException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Decodes the secret returned by the call to GetSecretValueAsync and
+        /// returns it to the calling program.
+        /// </summary>
+        /// <param name="response">A GetSecretValueResponse object containing
+        /// the requested secret value returned by GetSecretValueAsync.</param>
+        /// <returns>A string representing the decoded secret string.</returns>
+        public static string DecodeString(GetSecretValueResponse response)
+        {
+            // Decrypts secret using the associated KMS CMK.
+            // Depending on whether the secret is a string or binary, one of these fields will be populated.
+            MemoryStream memoryStream = new ();
+
+            if (response.SecretString != null)
+            {
+                var secret = response.SecretString;
+                return secret;
+            }
+            else
+            {
+                memoryStream = response.SecretBinary;
+                StreamReader reader = new StreamReader(memoryStream);
+                string decodedBinarySecret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+                return decodedBinarySecret;
+            }
+        }
+    }
+}
---
+++ dotnetv3/SecretsManager/GetSecretValue/GetSecretValueExampleTests/GetSecretValueTests.cs
@@ -0,0 +1,51 @@
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using Moq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GetSecretValueExampleTests
+{
+    public class GetSecretValueTests
+    {
+        [Fact]
+        public void TestGetSecretValueAsync()
+        {
+            var mockClient = new Mock<IAmazonSecretsManager>();
+            mockClient.Setup(client => client.GetSecretValueAsync(
+                    It.IsAny<GetSecretValueRequest>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns((GetSecretValueRequest r,
+                    CancellationToken token) =>
+                {
+                    return Task.FromResult(new GetSecretValueResponse()
+                    {
+                        HttpStatusCode = HttpStatusCode.OK,
+                    });
+                });
+
+            var client = mockClient.Object;
+
+            GetSecretValueRequest request = new();
+            request.SecretId = "SecretTest";
+            request.VersionStage = "AWSCURRENT"; // VersionStage defaults to AWSCURRENT if unspecified.
+
+            var response = client.GetSecretValueAsync(request);
+        }
+
+        [Fact]
+        public void TestDecodeSecretValueBinary()
+        {
+
+        }
+
+        [Fact]
+        public void TestDecodeSecretValueString()
+        {
+
+        }
+    }
+}
