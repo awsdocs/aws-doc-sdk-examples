1846ecfb96fbe6e9785db1efe46537002613dd19
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-05-11 16:08:28 -0400

    Add files via upload

--- javav2/example_code/kms/src/main/java/com/example/kms/DescribeKey.java
+++ javav2/example_code/kms/src/main/java/com/example/kms/DescribeKey.java
@@ -21,6 +21,13 @@ import software.amazon.awssdk.services.kms.model.DescribeKeyResponse;
 import software.amazon.awssdk.services.kms.model.KmsException;
 // snippet-end:[kms.java2_describe_key.import]
 
+/**
+ * To run this Java V2 code example, ensure that you have setup your development environment, including your credentials.
+ *
+ * For information, see this documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
 public class DescribeKey {
 
     public static void main(String[] args) {
