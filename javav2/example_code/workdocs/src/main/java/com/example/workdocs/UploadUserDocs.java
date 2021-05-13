e561f86e46bf7126b3af98ef037544205c541112
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-05-13 09:32:06 -0400

    Add files via upload

--- javav2/example_code/workdocs/src/main/java/com/example/workdocs/UploadUserDocs.java
+++ javav2/example_code/workdocs/src/main/java/com/example/workdocs/UploadUserDocs.java
@@ -44,6 +44,13 @@ import software.amazon.awssdk.services.workdocs.model.UpdateDocumentVersionReque
 import software.amazon.awssdk.services.workdocs.model.DocumentVersionStatus;
 // snippet-end:[workdocs.java2.upload_user_doc.import]
 
+/**
+ * To run this Java V2 code example, ensure that you have setup your development environment, including your credentials.
+ *
+ * For information, see this documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
 public class UploadUserDocs {
 
     public static void main(String[] args) {
