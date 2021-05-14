e6777b445c93bdc9fbbed6a0bb6706e307cc9147
Irene Smith <igsmith@amazon.com>
2021-05-14 08:52:42 -0400

    Updated examples to use the default account region.

--- dotnetv3/S3/CreateBucketExample/CreateBucket/CreateBucket.cs
+++ dotnetv3/S3/CreateBucketExample/CreateBucket/CreateBucket.cs
@@ -1,12 +1,8 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX - License - Identifier: Apache - 2.0
 
-// snippet-start:[S3.dotnet35.CreateBucket]
-
-using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
-using Amazon.S3.Util;
 using System;
 using System.Threading.Tasks;
 
@@ -18,17 +14,15 @@ namespace CreateBucket
         // to create a new Amazon S3 bucket. The examples uses AWS SDK for .NET 3.5 and
         // .NET 5.0.
 
-        // Specify your AWS Region (an example Region is shown).
-        private static readonly RegionEndpoint BUCKET_REGION = RegionEndpoint.USEast1; // RegionEndpoint.USWest2;
         private static IAmazonS3 _s3Client;
 
         // Specify the name of the new bucket.
-        private const string NEW_BUCKET_NAME = "igsmith-bucket2"; // "doc-example-bucket";
+        private const string NEW_BUCKET_NAME = "doc-example-bucket";
 
         static async Task Main()
         {
 
-            _s3Client = new AmazonS3Client(BUCKET_REGION);
+            _s3Client = new AmazonS3Client();
             Console.WriteLine($"\nCreating a new bucket, named: {NEW_BUCKET_NAME}.");
 
             await CreatingBucketAsync(_s3Client, NEW_BUCKET_NAME);
@@ -61,4 +55,3 @@ namespace CreateBucket
         }
     }
 }
-// snippet-end:[S3.dotnet35.CreateBucket]
