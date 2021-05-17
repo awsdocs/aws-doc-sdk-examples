837f5bdd4f97d86bc89612ef1c50a0bd1a5367f3
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-05-17 09:38:16 -0400

    Add files via upload

---
+++ javav2/usecases/creating_sns_sample_app/src/main/java/com/spring/sns/SubApplication.java
@@ -0,0 +1,12 @@
+package com.spring.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubApplication.class, args);
+    }
+}
