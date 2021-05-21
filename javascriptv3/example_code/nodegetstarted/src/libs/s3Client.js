59568da98baedab7374600ecb84df1cb68f1b494
Brian Murray <brmur@amazon.com>
2021-05-21 13:55:27 +0100

    updates node getting started examples:
    removed client generation to separate file to accomodate tests
    changed to ES6 from commonJS
    changed from TypeScript to JavaScript
    rewrote tests
    updated readmes

--- javascriptv3/example_code/nodegetstarted/src/libs/s3Client.js
+++ javascriptv3/example_code/nodegetstarted/src/libs/s3Client.js
@@ -14,7 +14,7 @@ Inputs (replace in code):
 // snippet-start:[GettingStarted.JavaScript.createclientv3]
 import { S3Client } from "@aws-sdk/client-s3";
 // Set the AWS Region.
-const REGION = "eu-west-1"; //e.g. "us-east-1"
+const REGION = "REGION"; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({ region: REGION });
 export { s3Client };
