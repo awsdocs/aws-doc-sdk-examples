bb94438a11a3915b558a65a663544b88856f7474
Brian Murray <brmur@amazon.com>
2021-06-08 19:08:25 +0100

    update CloudWatch tests

---
+++ javascriptv3/example_code/cloudwatch-events/tests/putRule.test.js
@@ -0,0 +1,24 @@
+jest.mock("../src/libs/cloudWatchEventsClient.js");
+jest.mock("@aws-sdk/client-cloudwatch-events");
+
+// Get service clients module and commands.
+import 'regenerator-runtime/runtime'
+import { run, params } from "../src/putRule.js";
+import { cweClient } from "../src/libs/cloudWatchEventsClient.js";
+
+describe("@aws-sdk/client-cloudwatch-events mock", () => {
+    it("should run async equal", async (done) => {
+        expect(1).toBe(1);
+        done();
+    });
+
+    it("should be equal", () => {
+        expect(1).toBe(1);
+    });
+
+    it("should successfully mock CloudWatch Events client", async () => {
+        cweClient.send.mockResolvedValue({ isMock: true });
+        const response = await run(params);
+        expect(response.isMock).toEqual(true);
+    });
+});
---
+++ javascriptv3/example_code/cloudwatch-logs/tests/deleteSubscriptionFilter.test.js
@@ -0,0 +1,24 @@
+jest.mock("../src/libs/cloudWatchLogsClient.js");
+jest.mock("@aws-sdk/client-cloudwatch-logs");
+
+// Get service clients module and commands.
+import "regenerator-runtime/runtime";
+import { run, params } from "../src/deleteSubscriptionFilter.js";
+import { cwlClient } from "../src/libs/cloudWatchLogsClient";
+
+describe("@aws-sdk/client-cloudwatch-logs mock", () => {
+  it("should run async equal", async (done) => {
+    expect(1).toBe(1);
+    done();
+  });
+
+  it("should be equal", () => {
+    expect(1).toBe(1);
+  });
+
+  it("should successfully mock CloudWatch Logs client", async () => {
+    cwlClient.send.mockResolvedValue({ isMock: true });
+    const response = await run(params);
+    expect(response.isMock).toEqual(true);
+  });
+});
