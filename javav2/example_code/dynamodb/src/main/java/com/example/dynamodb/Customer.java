c8daf41e2afedb1472b17991ab9a7c997095a036
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-06-14 08:44:37 -0400

    Add the  Amazon Aurora and Amazon Translation Services example (#1833)
    * Update README.rst
    
    * Create Readme.md
    
    * Create f.txt
    
    * Add files via upload
    
    * Update Readme.md
    
    * Add files via upload
    
    * Delete EnhancedScanRecordsWithExpressionTest.java
    
    * Update Readme.md
    
    * Update Readme.md
    
    * Create BlogApp.java
    
    * Add files via upload
    
    * Update ConnectionHelper.java
    
    * Update ConnectionHelper.java
    
    * Add files via upload
    
    Co-authored-by: Irene Smith <73654695+irenepsmith@users.noreply.github.com>

---
+++ javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/Customer.java
@@ -0,0 +1,69 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.example.dynamodb;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteItemEnhancedRequest;
+import software.amazon.awssdk.enhanced.dynamodb.model.WriteBatch;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+/**
+ * This class is used by the Enhanced Client examples.
+ */
+
+ @DynamoDbBean
+ public class Customer {
+
+        private String id;
+        private String name;
+        private String email;
+        private Instant regDate;
+
+        @DynamoDbPartitionKey
+        public String getId() {
+            return this.id;
+        };
+
+        public void setId(String id) {
+
+            this.id = id;
+        }
+
+        public String getCustName() {
+            return this.name;
+        }
+
+        public void setCustName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return this.email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Instant getRegistrationDate() {
+            return regDate;
+        }
+        public void setRegistrationDate(Instant registrationDate) {
+
+            this.regDate = registrationDate;
+        }
+    }
