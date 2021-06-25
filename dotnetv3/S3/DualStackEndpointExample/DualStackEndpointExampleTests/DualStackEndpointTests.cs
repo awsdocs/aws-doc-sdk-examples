a3bda2fba758d452bd995533f506e0205dc40edb
Irene Smith <igsmith@amazon.com>
2021-06-25 12:37:40 -0400

    Adding three new S3 examples.

---
+++ dotnetv3/S3/DualStackEndpointExample/DualStackEndpointExampleTests/DualStackEndpointTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using DualStackEndpointExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualStackEndpointExample.Tests
+{
+    public class DualStackEndpointTests
+    {
+        [Fact()]
+        public void MainTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
