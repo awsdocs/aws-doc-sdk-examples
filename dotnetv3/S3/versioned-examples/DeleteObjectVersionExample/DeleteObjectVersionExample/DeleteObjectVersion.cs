ae7998cc3a3b2084c60a8572fc9e042304a2360c
Irene Smith <igsmith@amazon.com>
2021-07-01 09:02:21 -0400

    Adding three examples that work with version enabled buckets.

--- dotnetv3/S3/versioned-examples/DeleteObjectVersionExample/DeleteObjectVersionExample/DeleteObjectVersion.cs
+++ dotnetv3/S3/versioned-examples/DeleteObjectVersionExample/DeleteObjectVersionExample/DeleteObjectVersion.cs
@@ -1,76 +1,79 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier:  Apache-2.0
-
-using Amazon;
-using Amazon.S3;
-using Amazon.S3.Model;
-using System;
-using System.Threading.Tasks;
+// SPDX - License - Identifier: Apache - 2.0
 
 namespace DeleteObjectVersionExample
 {
-    class DeleteObjectVersion
+    using System;
+    using System.Threading.Tasks;
+    using Amazon.S3;
+    using Amazon.S3.Model;
+
+    /// <summary>
+    /// This example creates an object in an Amazon Simple Storage Service
+    /// (Amazon S3) bucket and then deletes the object version that was
+    /// created. The example uses the AWS SDK for .NET version 3.7 and
+    /// .NET Core 5.0.
+    /// </summary>
+    public class DeleteObjectVersion
     {
-        static async Task Main()
+        public static async Task Main()
         {
             string bucketName = "doc-example-bucket";
-            string keyName = "example.txt";
+            string keyName = "verstioned-object.txt";
 
-            // Specify your bucket region (an example region is shown).
-            RegionEndpoint bucketRegion = RegionEndpoint.USWest2;
+            // If the AWS region of the default user is different from the AWS
+            // region of the S3 bucket, pass the bucket region to the S3 client
+            // object's constructor. Define it like this:
+            //      RegionEndpoint bucketRegion = RegionEndpoint.USWest2;
             IAmazonS3 client = new AmazonS3Client();
 
-            // Add a sample object which has a version.
-            var versionID = await PutAnObject(client, bucketName, keyName);
-
-            // Now that the object has been created, we will delete the version.
-            await DeleteObjectVersionAsync(client, bucketName, keyName, versionID);
+            await CreateAndDeleteObjectVersionAsync(client, bucketName, keyName);
         }
 
         /// <summary>
-        /// Removes the temporary version created by the call to PutAnObject.
+        /// This method creates and then deletes a versioned object.
         /// </summary>
-        /// <param name="client">The initialized S3 client object used to delete
-        /// the object version.</param>
-        /// <param name="bucketName">The name of the bucket where the versioned
-        /// object is stored.</param>
-        /// <param name="keyName">The name of the object for which we will
-        /// delete a version.</param>
-        /// <param name="versionID">The ID of the version to delete.</param>
-        static async Task DeleteObjectVersionAsync(IAmazonS3 client, string bucketName, string keyName, string versionID)
+        /// <param name="client">The initialized S3 client object used to
+        /// create and delete the object.</param>
+        /// <param name="bucketName">The name of the S3 bucket where the will
+        /// be created and deleted.</param>
+        /// <param name="keyName">The key name of the object to create.</param>
+        public static async Task CreateAndDeleteObjectVersionAsync(IAmazonS3 client, string bucketName, string keyName)
         {
             try
             {
+                // Add a sample object.
+                string versionID = await PutAnObject(client, bucketName, keyName);
+
                 // Delete the object by specifying an object key and a version ID.
-                DeleteObjectRequest request = new DeleteObjectRequest
+                DeleteObjectRequest request = new ()
                 {
                     BucketName = bucketName,
                     Key = keyName,
                     VersionId = versionID,
                 };
+
                 Console.WriteLine("Deleting an object");
                 await client.DeleteObjectAsync(request);
             }
             catch (AmazonS3Exception ex)
             {
-                Console.WriteLine($"Error:'{ex.Message}'");
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
 
         /// <summary>
-        /// This method creates a new version of an Amazon S3 object using
-        /// PutObject on a bucket which has versioning enabled.
+        /// This method is used to create the temporary S3 object.
         /// </summary>
-        /// <param name="client">The initialized Amazon S3 client used to
-        /// create the temporary object.</param>
-        /// <param name="bucketName">The name of the bucket where the temporary
-        /// object version will be created.</param>
-        /// <param name="objectKey">The name of the object for which we will
-        /// create a temporary version.</param>
-        /// <returns>A string that represents the version ID we just created.</returns>
+        /// <param name="client">The initialized S3 object which will be used
+        /// to create the temporary S3 object.</param>
+        /// <param name="bucketName">The name of the S3 bucket where the object
+        /// will be created.</param>
+        /// <param name="objectKey">The name of the S3 object co create.</param>
+        /// <returns>The Version ID of the created object.</returns>
         public static async Task<string> PutAnObject(IAmazonS3 client, string bucketName, string objectKey)
         {
-            PutObjectRequest request = new PutObjectRequest
+            PutObjectRequest request = new ()
             {
                 BucketName = bucketName,
                 Key = objectKey,
