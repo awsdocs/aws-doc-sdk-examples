08ca2b66a65217395b4ef3bb0bfd1ce03a435062
Morgan Gangwere <gangwere@amazon.com>
2021-07-13 11:23:09 -0700

    Add Golang AppRunner example

---
+++ resources/cdk/go-apprunner/app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
