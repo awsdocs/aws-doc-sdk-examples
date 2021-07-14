7b6a06bc23b7f87c30575935c408165124d2b039
Brian Murray <brmur@amazon.com>
2021-07-14 12:57:44 +0100

    add AWS Rekognition photo analyzer cross-serivce example (JS)

--- javascriptv3/example_code/cross-services/photo_analyzer/libs/s3Client.js
+++ javascriptv3/example_code/cross-services/photo_analyzer/libs/s3Client.js
@@ -1,8 +1,7 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 ABOUT THIS NODE.JS EXAMPLE: This example works with AWS SDK for JavaScript version 3 (v3),
-which is available at https://github.com/aws/aws-sdk-js-v3. This example is in the 'AWS SDK for JavaScript v3 Developer Guide' at
-https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/video-analyzer.html.
+which is available at https://github.com/aws/aws-sdk-js-v3.
 
 Purpose:
 s3Client.js is a helper function that creates the Amazon Simple Storage Solution (Amazon S3) service clients.
@@ -11,7 +10,7 @@ Inputs (replace in code):
 - REGION
 - IDENTITY_POOL_ID - an Amazon Cognito Identity Pool ID.
 */
-// snippet-start:[video-analyzer.JavaScript.s3Client]
+// snippet-start:[photo-analyzer.JavaScript.s3Client]
 import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { S3Client } from "@aws-sdk/client-s3";
@@ -21,12 +20,12 @@ const IDENTITY_POOL_ID = "IDENTITY_POOL_ID"; // An Amazon Cognito Identity Pool 
 
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({
-  region: REGION,
-  credentials: fromCognitoIdentityPool({
-    client: new CognitoIdentityClient({ region: REGION }),
-    identityPoolId: IDENTITY_POOL_ID,
-  }),
+    region: REGION,
+    credentials: fromCognitoIdentityPool({
+        client: new CognitoIdentityClient({ region: REGION }),
+        identityPoolId: IDENTITY_POOL_ID,
+    }),
 });
 
-export { s3Client };
-// snippet-end:[video-analyzer.JavaScript.s3Client]
+export { REGION, s3Client };
+// snippet-end:[photo-analyzer.JavaScript.s3Client]
