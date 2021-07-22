67bc19e03788713aa8a0d78c6a549d1cfc99e7d8
Doug <dougsch@amazon.com>
2021-07-22 07:17:37 -0700

    Added Snowball code examples in Rust

--- .rust_alpha/snowball/src/bin/create-address.rs
+++ .rust_alpha/snowball/src/bin/create-address.rs
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_sdk_snowball::model::Address;
+use aws_sdk_snowball::{Client, Config, Error, Region, PKG_VERSION};
 use aws_types::region::{self, ProvideRegion};
-use snowball::model::Address;
-use snowball::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -96,7 +96,7 @@ async fn main() -> Result<(), Error> {
         verbose,
     } = Opt::from_args();
 
-    let region_provider = region::ChainProvider::first_try(region.map(Region::new))
+    let region = region::ChainProvider::first_try(region.map(Region::new))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
 
@@ -106,7 +106,7 @@ async fn main() -> Result<(), Error> {
         println!("Snowball version:       {}", PKG_VERSION);
         println!(
             "Region:                 {}",
-            region_provider.region().unwrap().as_ref()
+            region.region().unwrap().as_ref()
         );
         println!("City:                   {}", &city);
         println!("Company:                {:?}", &company);
@@ -139,7 +139,7 @@ async fn main() -> Result<(), Error> {
         .set_is_restricted(Some(false))
         .build();
 
-    let conf = Config::builder().region(region_provider).build();
+    let conf = Config::builder().region(region).build();
     let client = Client::from_conf(conf);
 
     let result = client.create_address().address(new_address).send().await?;
--- .rust_alpha/snowball/src/bin/list-jobs.rs
+++ .rust_alpha/snowball/src/bin/list-jobs.rs
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_sdk_snowball::{Client, Config, Error, Region, PKG_VERSION};
 use aws_types::region::{self, ProvideRegion};
-use snowball::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -31,22 +31,22 @@ async fn main() -> Result<(), Error> {
 
     let Opt { region, verbose } = Opt::from_args();
 
-    let region_provider = region::ChainProvider::first_try(region.map(Region::new))
+    let region = region::ChainProvider::first_try(region.map(Region::new))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
 
     println!();
 
     if verbose {
-        println!("Snowball version: {}", PKG_VERSION);
+        println!("Snowball client version: {}", PKG_VERSION);
         println!(
-            "Region:           {}",
-            region_provider.region().unwrap().as_ref()
+            "Region:                  {}",
+            region.region().unwrap().as_ref()
         );
         println!();
     }
 
-    let conf = Config::builder().region(region_provider).build();
+    let conf = Config::builder().region(region).build();
     let client = Client::from_conf(conf);
 
     println!("Jobs:");
