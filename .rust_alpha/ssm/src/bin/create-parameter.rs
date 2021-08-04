51a28494c9398aa9113d29d69c062f5198042fb0
Doug <dougsch@amazon.com>
2021-08-04 14:01:16 -0700

    Updated SSM code examples in Rust to use alpha 0.0.13 bits

--- .rust_alpha/ssm/src/bin/create-parameter.rs
+++ .rust_alpha/ssm/src/bin/create-parameter.rs
@@ -3,92 +3,88 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use std::process;
-
-use ssm::model::ParameterType;
-use ssm::{Client, Config, Region};
-
-use aws_types::region::{EnvironmentProvider, ProvideRegion};
-
+use aws_sdk_ssm::model::ParameterType;
+use aws_sdk_ssm::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
+use aws_types::region::ProvideRegion;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region
+    /// The AWS Region.
     #[structopt(short, long)]
     region: Option<String>,
 
-    /// The parameter name
+    /// The parameter name.
     #[structopt(short, long)]
     name: String,
 
-    /// The parameter value
+    /// The parameter value.
     #[structopt(short, long)]
     parameter_value: String,
 
-    /// The parameter description
+    /// The parameter title (description).
     #[structopt(short, long)]
-    description: String,
+    title: String,
 
-    /// Whether to display additional information
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Creates a new AWS Systems Manager parameter.
+/// Creates a new AWS Systems Manager parameter in the Region.
 /// # Arguments
 ///
 /// * `-n NAME` - The name of the parameter.
 /// * `-p PARAMETER_VALUE` - The value of the parameter.
-/// * `-d DESCRIPTION` - The description of the parameter.
-/// * `[-d DEFAULT-REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+/// * `-t TITLE` - The description of the parameter.
+/// * `[-r REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
     let Opt {
         name,
         parameter_value,
-        description,
+        title,
         region,
         verbose,
     } = Opt::from_args();
 
-    let region = EnvironmentProvider::new()
-        .region()
-        .or_else(|| region.as_ref().map(|region| Region::new(region.clone())))
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
 
     if verbose {
-        println!("SSM client version:   {}", ssm::PKG_VERSION);
-        println!("Region:               {:?}", &region);
-        println!("Parameter name:       {}", name);
-        println!("Paramter value:       {}", parameter_value);
-        println!("Paramter description: {}", description);
-
-        tracing_subscriber::fmt::init();
+        println!("SQS client version:   {}", PKG_VERSION);
+        println!(
+            "Region:               {}",
+            region.region().unwrap().as_ref()
+        );
+        println!("Parameter name:       {}", &name);
+        println!("Paramter value:       {}", &parameter_value);
+        println!("Paramter description: {}", &title);
+        println!();
     }
 
     let config = Config::builder().region(region).build();
     let client = Client::from_conf(config);
 
-    match client
+    let resp = client
         .put_parameter()
         .overwrite(true)
         .r#type(ParameterType::String)
         .name(name)
         .value(parameter_value)
-        .description(description)
+        .description(title)
         .send()
-        .await
-    {
-        Ok(response) => {
-            println!("Success! Parameter now has version: {}", response.version)
-        }
-        Err(error) => {
-            println!("Got an error putting the parameter: {}", error);
-            process::exit(1);
-        }
-    }
+        .await?;
+
+    println!("Success! Parameter now has version: {}", resp.version);
+
+    Ok(())
 }
