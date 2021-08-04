3085b476babd42133e409859ea9ae906cea8af00
Doug <dougsch@amazon.com>
2021-08-04 06:24:48 -0700

    Updated QLDB code examples in Rust to use alpha 0.0.13 bits

--- .rust_alpha/qldb/README.md
+++ .rust_alpha/qldb/README.md
@@ -4,7 +4,7 @@ Amazon Quantum Ledger Database (Amazon QLDB) is a fully managed ledger database 
 
 ## create-ledger
 
-This code example creates an Amazon QLDB ledger.
+This code example creates an Amazon QLDB ledger in the Region.
 
 ### Usage
 
@@ -14,13 +14,13 @@ where:
 
 - _LEDGER_ is the name of the ledger to create.
 - _REGION_ is the region in which the client is created.
-  If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+  If not supplied, uses the value of the **AWS_REGION** environment variable.
   If the environment variable is not set, defaults to **us-west-2**.
 - __-v__ enables displaying additional information.
 
 ## list-ledgers
 
-This code example lists your Amazon QLDB ledgers.
+This code example lists your Amazon QLDB ledgers in the Region.
 
 ### Usage
 
@@ -29,13 +29,13 @@ This code example lists your Amazon QLDB ledgers.
 where:
 
 - _REGION_ is the region in which the client is created.
-  If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+  If not supplied, uses the value of the **AWS_REGION** environment variable.
   If the environment variable is not set, defaults to **us-west-2**.
 - __-v__ enables displaying additional information.
 
 ## qldb-helloworld
 
-This code example creates a low-level Amazon QLDB session against a ledger.
+This code example creates a low-level Amazon QLDB session against a ledger in the Region.
 
 Avoid using the QldbSession API directly. Instead, use a higher-level driver, such as the Amazon QLDB Driver for Rust.
 
@@ -47,7 +47,7 @@ where:
 
 - _LEDGER_ is the name of the ledger to create the session against.
 - _REGION_ is the region in which the client is created.
-  If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+  If not supplied, uses the value of the **AWS_REGION** environment variable.
   If the environment variable is not set, defaults to **us-west-2**.
 - __-v__ enables displaying additional information.
 
--- .rust_alpha/qldb/src/bin/create-ledger.rs
+++ .rust_alpha/qldb/src/bin/create-ledger.rs
@@ -3,60 +3,55 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_sdk_qldb::model::PermissionsMode;
+use aws_sdk_qldb::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
 use aws_types::region::ProvideRegion;
-
-use qldb::model::PermissionsMode;
-use qldb::{Client, Config, Error, Region};
-
 use structopt::StructOpt;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region. Overrides environment variable AWS_DEFAULT_REGION.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
     /// The name of the ledger.
     #[structopt(short, long)]
     ledger: String,
 
-    /// Whether to display additional runtime information
+    /// Whether to display additional runtime information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Creates an Amazon QLDB ledger.
+/// Creates an Amazon QLDB ledger in the Region.
 /// # Arguments
 ///
 /// * `-l LEDGER` - The name of the ledger.
-/// * `[-d DEFAULT-REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+/// * `[-r REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+
     let Opt {
-        default_region,
         ledger,
+        region,
         verbose,
     } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
 
     if verbose {
-        println!("QLDB client version: {}\n", qldb::PKG_VERSION);
-        println!("Region:              {:?}", &region);
-
-        SubscriberBuilder::default()
-            .with_env_filter("info")
-            .with_span_events(FmtSpan::CLOSE)
-            .init();
+        println!("QLDB client version: {}", PKG_VERSION);
+        println!("Region:              {}", region.region().unwrap().as_ref());
+        println!();
     }
 
     let conf = Config::builder().region(region).build();
