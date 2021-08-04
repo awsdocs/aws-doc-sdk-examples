e93ab52f2a11cc0b5498e3c560cfe13ef691b634
Doug <dougsch@amazon.com>
2021-08-04 12:35:16 -0700

    Updated SageMaker code examples in Rust to use alpha 0.0.13 bits

--- .rust_alpha/sagemaker/src/bin/sagemaker-helloworld.rs
+++ .rust_alpha/sagemaker/src/bin/sagemaker-helloworld.rs
@@ -3,63 +3,66 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_sdk_sagemaker::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
 use aws_types::region::ProvideRegion;
-
-use sagemaker::{Client, Config, Region};
-
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region. Overrides environment variable AWS_DEFAULT_REGION.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
-    /// Whether to display additional runtime information
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Lists the name, status, and type of your SageMaker instances in an AWS Region.
+/// Lists the name, status, and type of your SageMaker instances in the Region.
 /// /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+/// * `[-r REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), sagemaker::Error> {
+async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
-    let Opt {
-        default_region,
-        verbose,
-    } = Opt::from_args();
+    let Opt { region, verbose } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
 
     if verbose {
-        println!("SageMaker client version: {}", sagemaker::PKG_VERSION);
-        println!("Region:                   {:?}", &region);
+        println!("SageMaker client version: {}", PKG_VERSION);
+        println!(
+            "Region:                   {}",
+            region.region().unwrap().as_ref()
+        );
+        println!();
     }
 
     let conf = Config::builder().region(region).build();
     let client = Client::from_conf(conf);
+
     let notebooks = client.list_notebook_instances().send().await?;
+
+    println!("Notebooks:");
 
     for n in notebooks.notebook_instances.unwrap_or_default() {
         let n_instance_type = n.instance_type.unwrap();
         let n_status = n.notebook_instance_status.unwrap();
         let n_name = n.notebook_instance_name.as_deref().unwrap_or_default();
 
-        println!(
-            "Notebook Name : {}, Notebook Status : {:#?}, Notebook Instance Type : {:#?}",
-            n_name, n_status, n_instance_type
-        );
+        println!("  Name :          {}", n_name);
+        println!("  Status :        {}", n_status.as_ref());
+        println!("  Instance Type : {}", n_instance_type.as_ref());
+        println!();
     }
 
     Ok(())
