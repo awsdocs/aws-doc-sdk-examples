715865de63a242123912caed02e0306909f48d1d
Doug <dougsch@amazon.com>
2021-08-16 13:25:12 -0700

    Updated Kinesis code examples in Rust to use alpha 0.0.13 bits (#1967)

--- .rust_alpha/kinesis/src/bin/describe-stream.rs
+++ .rust_alpha/kinesis/src/bin/describe-stream.rs
@@ -2,77 +2,79 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-use std::process;
 
-use kinesis::{Client, Config, Region};
-
-use aws_types::region::{EnvironmentProvider, ProvideRegion};
-
+use aws_sdk_kinesis::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
+use aws_types::region::ProvideRegion;
 use structopt::StructOpt;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region
+    /// The AWS Region.
     #[structopt(short, long)]
     region: Option<String>,
 
-    /// The name of the stream
+    /// The name of the stream.
     #[structopt(short, long)]
-    name: String,
+    stream_name: String,
 
-    /// Whether to display additional information
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
+/// Lists your Amazon Kinesis data streams in the Region.
+/// # Arguments
+///
+/// * `[-r REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
+///    If the environment variable is not set, defaults to **us-west-2**.
+/// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
     let Opt {
-        name,
+        stream_name,
         region,
         verbose,
     } = Opt::from_args();
 
-    let region = EnvironmentProvider::new()
-        .region()
-        .or_else(|| region.as_ref().map(|region| Region::new(region.clone())))
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
 
     if verbose {
-        println!("Kinesis client version: {}\n", kinesis::PKG_VERSION);
-        println!("Region:      {:?}", &region);
-        println!("Stream name: {}", name);
-
-        SubscriberBuilder::default()
-            .with_env_filter("info")
-            .with_span_events(FmtSpan::CLOSE)
-            .init();
+        println!("Kinesis client version: {}", PKG_VERSION);
+        println!(
+            "Region:                 {}",
+            region.region().unwrap().as_ref()
+        );
+        println!("Stream name:            {}", &stream_name);
+        println!();
     }
 
     let config = Config::builder().region(region).build();
-
     let client = Client::from_conf(config);
 
-    match client.describe_stream().stream_name(name).send().await {
-        Ok(resp) => {
-            let desc = resp.stream_description.unwrap();
+    let resp = client
+        .describe_stream()
+        .stream_name(stream_name)
+        .send()
+        .await?;
 
-            println!("Stream description:");
-            println!("  Name:              {}:", desc.stream_name.unwrap());
-            println!("  Status:            {:?}", desc.stream_status.unwrap());
-            println!("  Open shards:       {:?}", desc.shards.unwrap().len());
-            println!(
-                "  Retention (hours): {}",
-                desc.retention_period_hours.unwrap()
-            );
-            println!("  Encryption:        {:?}", desc.encryption_type.unwrap());
-        }
-        Err(e) => {
-            println!("Got an error describing stream");
-            println!("{}", e);
-            process::exit(1);
-        }
-    };
+    let desc = resp.stream_description.unwrap();
+
+    println!("Stream description:");
+    println!("  Name:              {}:", desc.stream_name.unwrap());
+    println!("  Status:            {:?}", desc.stream_status.unwrap());
+    println!("  Open shards:       {:?}", desc.shards.unwrap().len());
+    println!(
+        "  Retention (hours): {}",
+        desc.retention_period_hours.unwrap()
+    );
+    println!("  Encryption:        {:?}", desc.encryption_type.unwrap());
+
+    Ok(())
 }
