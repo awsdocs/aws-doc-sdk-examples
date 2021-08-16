27e837dd77c5e37dc47fbe9076973a524a0ac0b9
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2021-08-16 13:14:07 -0400

    New SNS Examples (#1998)
    * New SNS Examples
    
    * New SNS examples
    
    * Updated according to reviewer comments.

---
+++ dotnetv3/SNS/DeleteSNSTopicExample/DeleteSNSTopicExample/DeleteSNSTopic.cs
@@ -0,0 +1,29 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+
+namespace DeleteSNSTopicExample
+{
+    using System;
+    using System.Threading.Tasks;
+    using Amazon.SimpleNotificationService;
+
+    // snippet-start:[SNS.dotnetv3.DeleteSNSTopicExample]
+
+    /// <summary>
+    /// This example deletes an existing Amazon Simple Notification Service
+    /// (Amazon SNS) topic. The example was created using the AWS SDK for .NET
+    /// version 3.7 and .NET Core 5.0.
+    /// </summary>
+    public class DeleteSNSTopic
+    {
+        public static async Task Main()
+        {
+            string topicArn = "arn:aws:sns:us-east-2:704825161248:ExampleSNSTopic";
+            IAmazonSimpleNotificationService client = new AmazonSimpleNotificationServiceClient();
+
+            var response = await client.DeleteTopicAsync(topicArn);
+        }
+    }
+
+    // snippet-end:[SNS.dotnetv3.DeleteSNSTopicExample]
+}
