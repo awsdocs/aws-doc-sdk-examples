2471c8b41cfc9e95e50c2067972c5a487f0a45ab
Doug <dougsch@amazon.com>
2021-08-17 14:37:04 -0700

    Updated Rust code examples to use 0.0.15 alpha version

--- .rust_alpha/iam/src/bin/create-role.rs
+++ .rust_alpha/iam/src/bin/create-role.rs
@@ -7,29 +7,29 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-  /// The AWS Region.
-  #[structopt(short, long)]
-  region: Option<String>,
+    /// The AWS Region.
+    #[structopt(short, long)]
+    region: Option<String>,
 
-  /// Your account ID.
-  #[structopt(short, long)]
-  account: String,
+    /// Your account ID.
+    #[structopt(short, long)]
+    account: String,
 
-  /// The name of the bucket.
-  #[structopt(short, long)]
-  bucket: String,
+    /// The name of the bucket.
+    #[structopt(short, long)]
+    bucket: String,
 
-  /// The name of the role.
-  #[structopt(short, long)]
-  name: String,
+    /// The name of the role.
+    #[structopt(short, long)]
+    name: String,
 
-  /// The name of the file containing the policy document.
-  #[structopt(short, long)]
-  policy_file: String,
+    /// The name of the file containing the policy document.
+    #[structopt(short, long)]
+    policy_file: String,
 
-  /// Whether to display additional information.
-  #[structopt(short, long)]
-  verbose: bool,
+    /// Whether to display additional information.
+    #[structopt(short, long)]
+    verbose: bool,
 }
 
 /// Creates an IAM role in the Region.
@@ -46,47 +46,47 @@ struct Opt {
 /// * `[-v]` - Whether to display information.
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-  tracing_subscriber::fmt::init();
-  let Opt {
-    account,
-    bucket,
-    name,
-    policy_file,
-    region,
-    verbose,
-  } = Opt::from_args();
+    tracing_subscriber::fmt::init();
+    let Opt {
+        account,
+        bucket,
+        name,
+        policy_file,
+        region,
+        verbose,
+    } = Opt::from_args();
 
-  let region = region::ChainProvider::first_try(region.map(Region::new))
-    .or_default_provider()
-    .or_else(Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
 
-  println!();
-
-  if verbose {
-    println!("IAM client version: {}", PKG_VERSION);
-    println!("Region:             {}", region.region().unwrap().as_ref());
-    println!("Account ID:         {}", &account);
-    println!("Bucket:             {}", &bucket);
-    println!("Role name:          {}", &name);
-    println!("Policy doc filename {}", &policy_file);
     println!();
-  }
 
-  // Read policy doc from file as a string
-  let doc = fs::read_to_string(policy_file).expect("Unable to read file");
-  //let doc: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
+    if verbose {
+        println!("IAM client version: {}", PKG_VERSION);
+        println!("Region:             {}", region.region().unwrap().as_ref());
+        println!("Account ID:         {}", &account);
+        println!("Bucket:             {}", &bucket);
+        println!("Role name:          {}", &name);
+        println!("Policy doc filename {}", &policy_file);
+        println!();
+    }
 
-  let conf = Config::builder().region(region).build();
-  let client = Client::from_conf(conf);
+    // Read policy doc from file as a string
+    let doc = fs::read_to_string(policy_file).expect("Unable to read file");
+    //let doc: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
 
-  let resp = client
-    .create_role()
-    .assume_role_policy_document(doc)
-    .role_name(name)
-    .send()
-    .await?;
+    let conf = Config::builder().region(region).build();
+    let client = Client::from_conf(conf);
 
-  println!("Created role with ARN {}", resp.role.unwrap().arn.unwrap());
+    let resp = client
+        .create_role()
+        .assume_role_policy_document(doc)
+        .role_name(name)
+        .send()
+        .await?;
 
-  Ok(())
+    println!("Created role with ARN {}", resp.role.unwrap().arn.unwrap());
+
+    Ok(())
 }
