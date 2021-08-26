8a58784bab1731e348c78930b119d1d10dcb1043
Doug <dougsch@amazon.com>
2021-08-26 13:09:52 -0700

    Added filter to S3 ListBuckets operation in Rust to only see buckets in region

--- .rust_alpha/s3/src/bin/list-buckets.rs
+++ .rust_alpha/s3/src/bin/list-buckets.rs
@@ -14,14 +14,19 @@ struct Opt {
     #[structopt(short, long)]
     region: Option<String>,
 
+    /// Whether to only get buckets in the Region.
+    #[structopt(short, long)]
+    strict: bool,
+
     /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Lists your Amazon S3 buckets in the Region.
+/// Lists your Amazon S3 buckets, or just the buckets in the Region.
 /// # Arguments
 ///
+/// * `[-s]` - Only list bucket in the Region.
 /// * `[-r REGION]` - The Region in which the client is created.
 ///   If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///   If the environment variable is not set, defaults to **us-west-2**.
@@ -30,33 +35,66 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
-    let Opt { region, verbose } = Opt::from_args();
+    let Opt {
+        region,
+        strict,
+        verbose,
+    } = Opt::from_args();
 
     let region = region::ChainProvider::first_try(region.map(Region::new))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
 
-    println!();
+    let region_str: String = String::from(region.region().unwrap().as_ref());
 
     if verbose {
         println!("S3 client version: {}", PKG_VERSION);
-        println!("Region:            {}", region.region().unwrap().as_ref());
+        println!("Region:            {}", region_str);
+
+        if strict {
+            println!("Only lists buckets in the Region.");
+        } else {
+            println!("Lists all buckets.");
+        }
+
         println!();
     }
 
-    let conf = Config::builder().region(region).build();
+    let conf = Config::builder().region(region.region()).build();
     let client = Client::from_conf(conf);
 
     let resp = client.list_buckets().send().await?;
     let buckets = resp.buckets.unwrap_or_default();
     let num_buckets = buckets.len();
 
+    let mut in_region = 0;
+
     for bucket in &buckets {
-        println!("{}", bucket.name.as_deref().unwrap_or_default());
+        if strict {
+            let r = client
+                .get_bucket_location()
+                .bucket(bucket.name.as_deref().unwrap_or_default())
+                .send()
+                .await?;
+
+            if r.location_constraint.unwrap().as_ref() == region_str {
+                println!("{}", bucket.name.as_deref().unwrap_or_default());
+                in_region += 1;
+            }
+        } else {
+            println!("{}", bucket.name.as_deref().unwrap_or_default());
+        }
     }
 
     println!();
-    println!("Found {} buckets", num_buckets);
+    if strict {
+        println!(
+            "Found {} buckets in the {} region out of a total of {} buckets.",
+            in_region, region_str, num_buckets
+        );
+    } else {
+        println!("Found {} buckets in all regions.", num_buckets);
+    }
 
     Ok(())
 }
