22aeeb42c212df5f7904b555b533daa50959737e
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-08-31 14:39:54 -0400

    Add files via upload

---
+++ .kotlin_alpha/services/firehose/src/main/kotlin/com/kotlin/firehose/PutBatchRecords.kt
@@ -0,0 +1,85 @@
+//snippet-sourcedescription:[PutBatchRecords.kt demonstrates how to write multiple data records into a delivery stream and check each record using the response object.]
+//snippet-keyword:[AWS SDK for Kotlin]
+//snippet-keyword:[Code Sample]
+//snippet-service:[Amazon Kinesis Data Firehose]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[06/24/2021]
+//snippet-sourceauthor:[scmacdon - aws]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.kotlin.firehose
+
+// snippet-start:[firehose.kotlin.put_batch_records.import]
+import aws.sdk.kotlin.services.firehose.FirehoseClient
+import aws.sdk.kotlin.services.firehose.model.FirehoseException
+import aws.sdk.kotlin.services.firehose.model.PutRecordBatchRequest
+import aws.sdk.kotlin.services.firehose.model.Record
+import com.example.firehose.StockTradeGenerator
+import kotlinx.coroutines.delay
+import java.util.ArrayList
+import kotlin.system.exitProcess
+// snippet-end:[firehose.kotlin.put_batch_records.import]
+
+suspend fun main(args:Array<String>) {
+
+    val usage = """
+        Usage:
+            <streamName> 
+        Where:
+            streamName - the data stream name. 
+    """
+
+     if (args.size != 1) {
+         println(usage)
+         exitProcess(0)
+     }
+
+    val streamName = args[0]
+    val firehoseClient = FirehoseClient{region="us-east-1"}
+    addStockTradeData(firehoseClient, streamName)
+}
+
+// snippet-start:[firehose.kotlin.put_batch_records.main]
+suspend fun addStockTradeData(firehoseClient: FirehoseClient, streamName: String?) {
+    val recordList: MutableList<Record> = ArrayList()
+    try {
+
+        // Repeatedly send stock trades with a 100 milliseconds wait in between.
+        val stockTradeGenerator = StockTradeGenerator()
+        val index = 100
+
+        // Populate the list with StockTrade data.
+        for (x in 0 until index) {
+            val trade = stockTradeGenerator.randomTrade
+            val bytes = trade.toJsonAsBytes()
+            val myRecord = Record {
+                 data = bytes
+            }
+
+            println("Adding trade: $trade")
+            recordList.add(myRecord)
+            delay(100)
+        }
+        val recordBatchRequest = PutRecordBatchRequest {
+             deliveryStreamName = streamName
+             records = recordList
+        }
+
+        val recordResponse = firehoseClient.putRecordBatch(recordBatchRequest)
+        println("The number of records added is ${recordResponse.requestResponses?.size}")
+
+
+    } catch (ex: FirehoseException) {
+        println(ex.message)
+        firehoseClient.close()
+        exitProcess(0)
+    } catch (e: InterruptedException) {
+        println(e.localizedMessage)
+        exitProcess(0)
+    }
+}
+// snippet-end:[firehose.kotlin.put_batch_records.main]
---
+++ .kotlin_alpha/services/firehose/src/main/kotlin/com/kotlin/firehose/PutRecord.kt
@@ -0,0 +1,68 @@
+//snippet-sourcedescription:[PutRecord.kt demonstrates how to write a data record into a delivery stream.]
+//snippet-keyword:[AWS SDK for Kotlin]
+//snippet-keyword:[Code Sample]
+//snippet-service:[Amazon Kinesis Data Firehose]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[06/24/2021]
+//snippet-sourceauthor:[scmacdon - aws]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.kotlin.firehose
+
+// snippet-start:[firehose.kotlin.put_record.import]
+import aws.sdk.kotlin.services.firehose.FirehoseClient
+import aws.sdk.kotlin.services.firehose.model.FirehoseException
+import aws.sdk.kotlin.services.firehose.model.PutRecordRequest
+import aws.sdk.kotlin.services.firehose.model.Record
+import kotlin.system.exitProcess
+// snippet-end:[firehose.kotlin.put_record.import]
+
+suspend fun main(args:Array<String>) {
+
+    val usage = """
+        Usage:
+            <textValue> <streamName> 
+        Where:
+            textValue - the text used as the data to write to the data stream. 
+            streamName - the data stream name. 
+    """
+
+    if (args.size != 2) {
+         println(usage)
+         exitProcess(0)
+    }
+
+    val textValue = args[0]
+    val streamName = args[1]
+    val firehoseClient = FirehoseClient{region="us-east-1"}
+    putSingleRecord(firehoseClient, textValue, streamName)
+}
+
+// snippet-start:[firehose.kotlin.put_record.main]
+suspend fun putSingleRecord(firehoseClient: FirehoseClient, textValue: String, streamName: String?) {
+    try {
+        val bytes = textValue.toByteArray()
+
+        val recordOb = Record {
+            data = bytes
+        }
+
+        val recordRequest = PutRecordRequest {
+            deliveryStreamName = streamName
+            record = recordOb
+        }
+
+        val recordResponse = firehoseClient.putRecord(recordRequest)
+        println("The record ID is ${recordResponse.recordId}")
+
+    } catch (ex: FirehoseException) {
+        println(ex.message)
+        firehoseClient.close()
+        exitProcess(0)
+    }
+}
+// snippet-end:[firehose.kotlin.put_record.main]
