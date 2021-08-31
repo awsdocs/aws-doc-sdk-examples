68c655b679c533d76574e8c99bed98db491fb7f5
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-08-31 13:05:55 -0400

    Add files via upload

---
+++ .kotlin_alpha/services/ecs/src/main/kotlin/com/kotlin/ecs/ListTaskDefinitions.kt
@@ -0,0 +1,70 @@
+//snippet-sourcedescription:[ListTaskDefinitions.kt demonstrates how to list task definitions.]
+//snippet-keyword:[AWS SDK for Kotlin]
+//snippet-keyword:[Code Sample]
+//snippet-service:[Amazon Elastic Container Service]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[06/20/2021]
+//snippet-sourceauthor:[scmacdon-aws]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.kotlin.ecs
+
+// snippet-start:[ecs.kotlin.list_tasks.import]
+import aws.sdk.kotlin.services.ecs.EcsClient
+import aws.sdk.kotlin.services.ecs.model.DescribeTasksRequest
+import aws.sdk.kotlin.services.ecs.model.EcsException
+import kotlin.system.exitProcess
+// snippet-end:[ecs.kotlin.list_tasks.import]
+
+suspend fun main(args:Array<String>){
+
+    val usage = """
+    
+    Usage:
+        <clusterArn> <taskId>
+
+    Where:
+        clusterArn - the ARN of an ECS cluster..
+        taskId - the task Id value.
+    """
+
+    if (args.size != 2) {
+        println(usage)
+        exitProcess(0)
+     }
+
+    val clusterArn = args[0]
+    val taskId =   args[1]
+    val ecsClient = EcsClient{ region = "us-east-1"}
+    getAllTasks(ecsClient, clusterArn, taskId)
+    ecsClient.close()
+}
+
+// snippet-start:[ecs.kotlin.list_tasks.main]
+suspend fun getAllTasks(ecsClient: EcsClient, clusterArn: String, taskId: String) {
+    try {
+        val tasksRequest = DescribeTasksRequest {
+             cluster = clusterArn
+             tasks = listOf(taskId)
+        }
+
+        val response = ecsClient.describeTasks(tasksRequest)
+
+        val tasks = response.tasks
+        if (tasks != null) {
+            for (task in tasks) {
+                System.out.println("The task ARN is " + task.taskDefinitionArn)
+            }
+        }
+
+    } catch (ex: EcsException) {
+        println(ex.message)
+        ecsClient.close()
+        exitProcess(0)
+    }
+}
+// snippet-end:[ecs.kotlin.list_tasks.main]
