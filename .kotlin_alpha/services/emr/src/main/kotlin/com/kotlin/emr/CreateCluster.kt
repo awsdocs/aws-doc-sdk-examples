64e643cf5f8a23040d70a02d8faf627f45d3c2b8
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-09-01 14:26:37 -0400

    Update CreateCluster.kt

--- .kotlin_alpha/services/emr/src/main/kotlin/com/kotlin/emr/CreateCluster.kt
+++ .kotlin_alpha/services/emr/src/main/kotlin/com/kotlin/emr/CreateCluster.kt
@@ -12,7 +12,7 @@
 
 package com.kotlin.emr
 
-//snippet-start:[erm.kotlin.create_cluster.import]
+//snippet-start:[emr.kotlin.create_cluster.import]
 import aws.sdk.kotlin.services.emr.EmrClient
 import aws.sdk.kotlin.services.emr.model.HadoopJarStepConfig
 import aws.sdk.kotlin.services.emr.model.Application
@@ -23,7 +23,7 @@ import aws.sdk.kotlin.services.emr.model.RunJobFlowRequest
 import aws.sdk.kotlin.services.emr.model.RunJobFlowResponse
 import aws.sdk.kotlin.services.emr.model.EmrException
 import kotlin.system.exitProcess
-//snippet-end:[erm.kotlin.create_cluster.import]
+//snippet-end:[emr.kotlin.create_cluster.import]
 
 /**
 To run this Kotlin code example, ensure that you have setup your development environment,
@@ -62,7 +62,7 @@ suspend fun main(args:Array<String>) {
     println("The job flow id is $jobFlowId")
 }
 
-//snippet-start:[erm.kotlin.create_cluster.main]
+//snippet-start:[emr.kotlin.create_cluster.main]
 suspend fun createAppCluster(
     emrClient: EmrClient,
     jarVal: String?,
@@ -127,4 +127,4 @@ suspend fun createAppCluster(
         exitProcess(0)
     }
  }
-//snippet-end:[erm.kotlin.create_cluster.main]
+//snippet-end:[emr.kotlin.create_cluster.main]
