e4399e19244d2cae694e0bcd3fbc043d78e32dd2
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-09-01 10:16:13 -0400

    Update DeleteStack.kt

--- .kotlin_alpha/services/cloudformation/src/main/kotlin/com/kotlin/cloudformation/DeleteStack.kt
+++ .kotlin_alpha/services/cloudformation/src/main/kotlin/com/kotlin/cloudformation/DeleteStack.kt
@@ -22,6 +22,14 @@ import aws.sdk.kotlin.services.cloudformation.model.CloudFormationException
 import kotlin.system.exitProcess
 // snippet-end:[cf.kotlin.delete_stack.import]
 
+/**
+To run this Kotlin code example, ensure that you have setup your development environment,
+including your credentials.
+
+For information, see this documentation topic:
+https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+ */
+
 suspend fun main(args:Array<String>) {
 
     val usage = """
