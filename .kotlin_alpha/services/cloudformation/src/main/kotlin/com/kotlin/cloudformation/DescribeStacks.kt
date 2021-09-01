545cbeba4263e3fcf4643033b2c6c09678d4926d
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-09-01 10:16:33 -0400

    Update DescribeStacks.kt

--- .kotlin_alpha/services/cloudformation/src/main/kotlin/com/kotlin/cloudformation/DescribeStacks.kt
+++ .kotlin_alpha/services/cloudformation/src/main/kotlin/com/kotlin/cloudformation/DescribeStacks.kt
@@ -23,6 +23,14 @@ import aws.sdk.kotlin.services.cloudformation.model.DescribeStacksRequest
 import kotlin.system.exitProcess
 // snippet-end:[cf.kotlin.get_stacks.import]
 
+/**
+To run this Kotlin code example, ensure that you have setup your development environment,
+including your credentials.
+
+For information, see this documentation topic:
+https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+ */
+
 suspend fun main() {
 
     val cfClient = CloudFormationClient{region="us-east-1"}
