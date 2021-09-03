5402d87185d13eb260a410a015f58d32c4b6b2d0
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-09-03 13:19:12 -0400

    Add files via upload

---
+++ .kotlin_alpha/services/sts/src/main/kotlin/com/kotlin/sts/GetCallerIdentity.kt
@@ -0,0 +1,51 @@
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourcedescription:[GetCallerIdentity.kt demonstrates how to obtain details about the IAM user whose credentials are used to call the operation.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-keyword:[AWS Security Token Service (AWS STS)]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[06/07/2021]
+// snippet-sourceauthor:[AWS - scmacdon]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.kotlin.sts
+
+// snippet-start:[sts.kotlin.get_call_id.import]
+import aws.sdk.kotlin.services.sts.StsClient
+import aws.sdk.kotlin.services.sts.model.GetCallerIdentityRequest
+import aws.sdk.kotlin.services.sts.model.StsException
+import kotlin.system.exitProcess
+// snippet-end:[sts.kotlin.get_call_id.import]
+
+
+/**
+To run this Kotlin code example, ensure that you have setup your development environment,
+including your credentials.
+For information, see this documentation topic:
+https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+ */
+suspend fun main() {
+
+    val stsClient = StsClient{region ="us-east-1"}
+    getCallerId(stsClient)
+    stsClient.close()
+}
+
+// snippet-start:[sts.kotlin.get_call_id.main]
+suspend fun getCallerId(stsClient: StsClient) {
+    try {
+        val response = stsClient.getCallerIdentity(GetCallerIdentityRequest{})
+        println("The user id is ${response.userId}")
+        println("The ARN value is ${response.arn}")
+
+    } catch (e: StsException) {
+        println(e.message)
+        stsClient.close()
+        exitProcess(0)
+    }
+}
+// snippet-end:[sts.kotlin.get_call_id.main]
