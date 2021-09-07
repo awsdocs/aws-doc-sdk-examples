77daf77b6ef32ba4623dffb871b0b48bf21267f5
Doug <dougsch@amazon.com>
2021-09-07 13:41:34 -0700

    Updated Cognito Identity Provider code examples in Rust to 0.0.17 alpha bits.

--- .rust_alpha/cognitoidentityprovider/src/bin/list-user-pools.rs
+++ .rust_alpha/cognitoidentityprovider/src/bin/list-user-pools.rs
@@ -2,9 +2,9 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
-use aws_sdk_cognitoidentityprovider::{Client, Config, Error, Region, PKG_VERSION};
-use aws_types::region;
-use aws_types::region::ProvideRegion;
+
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_cognitoidentityprovider::{Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -31,21 +31,23 @@ async fn main() -> Result<(), Error> {
 
     let Opt { region, verbose } = Opt::from_args();
 
-    let region = region::ChainProvider::first_try(region.map(Region::new))
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
+    println!();
 
     if verbose {
         println!("Cognito client version: {}", PKG_VERSION);
         println!(
             "Region:                 {}",
-            region.region().unwrap().as_ref()
+            region_provider.region().await.unwrap().as_ref()
         );
+
         println!();
     }
 
-    let config = Config::builder().region(region).build();
-    let client = Client::from_conf(config);
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
 
     let response = client.list_user_pools().max_results(10).send().await?;
     if let Some(pools) = response.user_pools {
