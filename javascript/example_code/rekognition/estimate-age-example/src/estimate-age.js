ca6c9dcec81e77c46e40877a6e081d003507ae0a
Brian Murray <brmur@amazon.com>
2021-09-15 10:59:30 +0100

    Updating Rekognition examples

--- javascript/example_code/rekognition/estimate-age-example/src/estimate-age.js
+++ javascript/example_code/rekognition/estimate-age-example/src/estimate-age.js
@@ -52,7 +52,7 @@ function ProcessImage() {
     reader.readAsArrayBuffer(file);
 }
 
-//snippet-start:[rekognition.JavaScript.alternative.detect_faces_v2]
+//snippet-end:[rekognition.JavaScript.alternative.detect_faces_v2]
 
 //Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
