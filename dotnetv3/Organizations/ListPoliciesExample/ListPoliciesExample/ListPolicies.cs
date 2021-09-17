a6c389eaf60ef4e5e10066fef2be9db6c3002285
Irene Smith <igsmith@amazon.com>
2021-09-16 21:00:01 -0400

    Added new examples for Organizations Service.

---
+++ dotnetv3/Organizations/ListPoliciesExample/ListPoliciesExample/ListPolicies.cs
@@ -0,0 +1,71 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
+
+namespace ListPoliciesExample
+{
+    using System;
+    using System.Threading.Tasks;
+    using Amazon.Organizations;
+    using Amazon.Organizations.Model;
+
+    /// <summary>
+    /// Shows how to list the Amazon Organizations policies associated with an
+    /// Organization. The example was created with the AWS SDK for .NET version
+    /// 3.7 and .NET Core 5.0.
+    /// </summary>
+    public class ListPolicies
+    {
+        /// <summary>
+        /// Initializes an Organizations client object, and then calls its
+        /// ListPolliciesAsync method.
+        /// </summary>
+        public static async Task Main()
+        {
+            // Create the client object using the default account.
+            IAmazonOrganizations client = new AmazonOrganizationsClient();
+
+            // The value for the Filter parameter is required and must must be
+            // one of the following:
+            //     AISERVICES_OPT_OUT_POLICY
+            //     BACKUP_POLICY
+            //     SERVICE_CONTROL_POLICY
+            //     TAG_POLICY
+            var request = new ListPoliciesRequest
+            {
+                Filter = "SERVICE_CONTROL_POLICY",
+                MaxResults = 5,
+            };
+
+            var response = new ListPoliciesResponse();
+            try
+            {
+                do
+                {
+                    response = await client.ListPoliciesAsync(request);
+                    response.Policies.ForEach(p => DisplayPolicies(p));
+                    if (response.NextToken is not null)
+                    {
+                        request.NextToken = response.NextToken;
+                    }
+                } while (response.NextToken is not null);
+            }
+            catch (AWSOrganizationsNotInUseException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Displays information about the Organizations policies associated
+        /// with an Organization.
+        /// </summary>
+        /// <param name="policy">An Organizations Policy Summary to display
+        /// information on the console.</param>
+        private static void DisplayPolicies(PolicySummary policy)
+        {
+            string policyInfo = $"{policy.Id} {policy.Name}\t{policy.Description}";
+
+            Console.WriteLine(policyInfo);
+        }
+    }
+}
