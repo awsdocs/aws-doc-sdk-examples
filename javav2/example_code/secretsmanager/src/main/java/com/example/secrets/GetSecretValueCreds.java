d3ed0d6b8b95c19474f235ae489adfc80db8e1ae
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-09-29 08:11:00 -0400

    Add files via upload

--- javav2/example_code/secretsmanager/src/main/java/com/example/secrets/GetSecretValueCreds.java
+++ javav2/example_code/secretsmanager/src/main/java/com/example/secrets/GetSecretValueCreds.java
@@ -1,25 +1,10 @@
-//snippet-sourcedescription:[GetSecretValue.java demonstrates how to get the value of a secret from AWS Secrets Manager.]
-//snippet-keyword:[AWS SDK for Java v2]
-//snippet-keyword:[Code Sample]
-//snippet-service:[AWS Secrets Manager]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[09/27/2021]
-//snippet-sourceauthor:[scmacdon-AWS]
-
-/*
-   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
-*/
-
 package com.example.secrets;
 
-//snippet-start:[secretsmanager.java2.get_secret.import]
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
-//snippet-end:[secretsmanager.java2.get_secret.import]
 
 /**
  * To run this AWS code example, ensure that you have setup your development environment, including your AWS credentials.
@@ -29,7 +14,7 @@ import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerExcept
  *https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
  */
 
-public class GetSecretValue {
+public class GetSecretValueCreds {
 
     public static void main(String[] args) {
 
@@ -39,10 +24,10 @@ public class GetSecretValue {
                 "Where:\n" +
                 "    secretName - the name of the secret (for example, tutorials/MyFirstSecret). \n";
 
-       if (args.length != 1) {
+        if (args.length != 1) {
             System.out.println(USAGE);
-           System.exit(1);
-       }
+            System.exit(1);
+        }
 
         String secretName = args[0];
         Region region = Region.US_EAST_1;
@@ -54,13 +39,12 @@ public class GetSecretValue {
         secretsClient.close();
     }
 
-    //snippet-start:[secretsmanager.java2.get_secret.main]
     public static void getValue(SecretsManagerClient secretsClient,String secretName) {
 
         try {
             GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
-                .secretId(secretName)
-                .build();
+                    .secretId(secretName)
+                    .build();
 
             GetSecretValueResponse valueResponse = secretsClient.getSecretValue(valueRequest);
             String secret = valueResponse.secretString();
@@ -71,5 +55,4 @@ public class GetSecretValue {
             System.exit(1);
         }
     }
-    //snippet-end:[secretsmanager.java2.get_secret.main]
 }
