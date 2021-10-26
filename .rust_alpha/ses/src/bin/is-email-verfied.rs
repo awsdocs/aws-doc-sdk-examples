20ea7a0b095aeb68475f1b5a8cd9b4d76097b43e
Doug <dougsch@amazon.com>
2021-10-26 06:59:21 -0700

    Verified Rust code examples on alpha 0.0.22 release on crates.io

--- .rust_alpha/ses/src/bin/is-email-verfied.rs
+++ .rust_alpha/ses/src/bin/is-email-verfied.rs
@@ -4,7 +4,7 @@
  */
 
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_ses::{Client, Error, Region, PKG_VERSION};
+use aws_sdk_sesv2::{Client, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
