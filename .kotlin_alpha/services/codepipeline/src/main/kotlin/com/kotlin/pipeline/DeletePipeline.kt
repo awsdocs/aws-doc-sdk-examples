dba6112aad5a85d299a53bdaed5431fa9eedc047
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-10-27 08:49:45 -0400

    Add files via upload

---
+++ .kotlin_alpha/services/codepipeline/src/main/kotlin/com/kotlin/pipeline/DeletePipeline.kt
@@ -0,0 +1,59 @@
+//snippet-sourcedescription:[DeletePipeline.kt demonstrates how to delete a pipeline.]
+//snippet-keyword:[AWS SDK for Kotlin]
+//snippet-keyword:[Code Sample]
+//snippet-service:[AWS CodePipeline]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[10/19/2021]
+//snippet-sourceauthor:[scmacdon-aws]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.kotlin.pipeline
+
+// snippet-start:[pipeline.kotlin.delete_pipeline.import]
+import aws.sdk.kotlin.services.codepipeline.CodePipelineClient
+import aws.sdk.kotlin.services.codepipeline.model.CodePipelineException
+import aws.sdk.kotlin.services.codepipeline.model.DeletePipelineRequest
+import kotlin.system.exitProcess
+// snippet-end:[pipeline.kotlin.delete_pipeline.import]
+
+
+suspend fun main(args:Array<String>) {
+
+    val usage = """
+        Usage:
+            <name> 
+        Where:
+           name - the name of the pipeline to delete. 
+   """
+     if (args.size != 1) {
+         println(usage)
+         exitProcess(1)
+    }
+
+    val name =  args[0]
+    val pipelineClient = CodePipelineClient{region = "us-east-1"}
+    deleteSpecificPipeline(pipelineClient, name)
+    pipelineClient.close()
+}
+
+// snippet-start:[pipeline.kotlin.delete_pipeline.main]
+suspend fun deleteSpecificPipeline(pipelineClient: CodePipelineClient, nameVal: String) {
+    try {
+        val deletePipelineRequest = DeletePipelineRequest {
+            name = nameVal
+        }
+
+        pipelineClient.deletePipeline(deletePipelineRequest)
+        println("$nameVal was successfully deleted")
+
+    } catch (e: CodePipelineException) {
+        println(e.message)
+        pipelineClient.close()
+        exitProcess(0)
+    }
+}
+// snippet-end:[pipeline.kotlin.delete_pipeline.main]
