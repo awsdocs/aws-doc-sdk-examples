a38e1311c5d9f55755cfddb920c94fcb0f3e66e8
Doug <dougsch@amazon.com>
2021-10-27 12:05:59 -0700

    Refactored RDS code example in Rust: moved operation out of main and into separate function with snippet tags; updated readme

--- .rust_alpha/rds/src/bin/rds-helloworld.rs
+++ .rust_alpha/rds/src/bin/rds-helloworld.rs
@@ -18,36 +18,9 @@ struct Opt {
     verbose: bool,
 }
 
-/// Displays information about your Amazon Relational Database Service (Amazon RDS) instances in the Region.
-/// # Arguments
-///
-/// * `[-r REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_REGION** environment variable.
-///    If the environment variable is not set, defaults to **us-west-2**.
-/// * `[-v]` - Whether to display additional information.
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt::init();
-
-    let Opt { region, verbose } = Opt::from_args();
-
-    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
-        .or_default_provider()
-        .or_else(Region::new("us-west-2"));
-    println!();
-
-    if verbose {
-        println!("RDS client version: {}", PKG_VERSION);
-        println!(
-            "Region:             {}",
-            region_provider.region().await.unwrap().as_ref()
-        );
-        println!();
-    }
-
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
-
+// Lists your instances.
+// snippet-start:[rds.rust.rds-helloworld]
+async fn show_instances(client: &Client) -> Result<(), Error> {
     let result = client.describe_db_instances().send().await?;
 
     for db_instance in result.db_instances.unwrap_or_default() {
@@ -81,4 +54,38 @@ async fn main() -> Result<(), Error> {
     }
 
     Ok(())
+}
+// snippet-end:[rds.rust.rds-helloworld]
+
+/// Displays information about your Amazon Relational Database Service (Amazon RDS) instances in the Region.
+/// # Arguments
+///
+/// * `[-r REGION]` - The region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
+///    If the environment variable is not set, defaults to **us-west-2**.
+/// * `[-v]` - Whether to display additional information.
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+
+    let Opt { region, verbose } = Opt::from_args();
+
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+    println!();
+
+    if verbose {
+        println!("RDS client version: {}", PKG_VERSION);
+        println!(
+            "Region:             {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
+        println!();
+    }
+
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    show_instances(&client).await
 }
