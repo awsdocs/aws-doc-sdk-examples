2eb549f8033c016382c49732585804d36b615323
Irene Smith <igsmith@amazon.com>
2021-10-28 10:31:02 -0400

    Initial commit for SubscribePublishTranslate example.

---
+++ dotnetv3/cross-service/SubscribePublishTranslate/SubscribePublishTranslate/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿namespace SNSExample.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using SNSExample.Models;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PublishMessage(string body, string lang)
+        {
+            var snsService = new SnsService();
+            var id = await snsService.PubTopic(body, lang);
+            return Content($"Message {id} was successfully published");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RemoveEmailSub(string email)
+        {
+            var snsService = new SnsService();
+            var msg = await snsService.UnSubEmail(email);
+            return Content(msg);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmailSub(string email)
+        {
+            var snsService = new SnsService();
+            var arn = await snsService.SubEmail(email);
+            return Content(arn);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAjaxValue()
+        {
+            var snsService = new SnsService();
+            var xml = await snsService.GetSubs();
+            return Content(xml);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
