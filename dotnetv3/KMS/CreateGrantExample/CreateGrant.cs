dd53c81cc5975e0bc360e721f683fd3882066deb
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2021-11-02 16:01:36 -0400

    Adding examples and readme for KMS (#2313)
    * Adding examples and readme for KMS
    
    * Updated according to reviewer's comments.

---
+++ dotnetv3/KMS/CreateGrantExample/CreateGrant.cs
@@ -0,0 +1,54 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+/// <summary>
+/// Create a new AWS Key Management Service grant (AWS KMS). This example
+/// was created using the AWS SDK for .NET version 3.7 and .NET Core 5.0.
+/// </summary>
+namespace CreateGrantExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Amazon.KeyManagementService;
+    using Amazon.KeyManagementService.Model;
+
+    public class CreateGrant
+    {
+        // snippet-start:[KMS.dotnetv3.CreateGrantExample]
+        public static async Task Main()
+        {
+            var client = new AmazonKeyManagementServiceClient();
+
+            // The identity that is given permission to perform the operations
+            // specified in the grant.
+            var grantee = "arn:aws:iam::111122223333:role/ExampleRole";
+
+            // The identifier of the AWS KMS key to which the grant applies. You
+            // can use the key ID or the Amazon Resource Name (ARN) of the KMS key.
+            var keyId = "7c9eccc2-38cb-4c4f-9db3-766ee8dd3ad4";
+
+            var request = new CreateGrantRequest
+            {
+                GranteePrincipal = grantee,
+                KeyId = keyId,
+
+                // A list of operations that the grant allows.
+                Operations = new List<string>
+                {
+                    "Encrypt",
+                    "Decrypt",
+                },
+            };
+
+            var response = await client.CreateGrantAsync(request);
+
+            string grantId = response.GrantId; // The unique identifier of the grant.
+            string grantToken = response.GrantToken; // The grant token.
+
+            Console.WriteLine($"Id: {grantId}, Token: {grantToken}");
+        }
+    }
+
+    // snippet-end:[KMS.dotnetv3.CreateGrantExample]
+}
