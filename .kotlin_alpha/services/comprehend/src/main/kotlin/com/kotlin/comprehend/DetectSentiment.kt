6dbfe2b0a3b27803662df47073f93d22bfc93a06
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-11-02 12:00:20 -0400

    Add files via upload

--- .kotlin_alpha/services/comprehend/src/main/kotlin/com/kotlin/comprehend/DetectSentiment.kt
+++ .kotlin_alpha/services/comprehend/src/main/kotlin/com/kotlin/comprehend/DetectSentiment.kt
@@ -30,7 +30,9 @@ https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
 suspend fun main() {
 
-    val comprehendClient = ComprehendClient({region="us-east-1"})
+    val comprehendClient = ComprehendClient{
+        region="us-east-1"
+    }
     val text = "Amazon.com, Inc. is located in Seattle, WA and was founded July 5th, 1994 by Jeff Bezos, allowing customers to buy everything from books to blenders. Seattle is north of Portland and south of Vancouver, BC. Other notable Seattle - based companies are Starbucks and Boeing."
     detectSentiments(comprehendClient,text)
     comprehendClient.close()
