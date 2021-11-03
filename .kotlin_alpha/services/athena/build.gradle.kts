a9d59e9014efb9f70f0f9ba0ab745be3e4804707
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-11-03 16:42:25 -0400

    Add files via upload

--- .kotlin_alpha/services/athena/build.gradle.kts
+++ .kotlin_alpha/services/athena/build.gradle.kts
@@ -10,11 +10,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
-    api("aws.sdk.kotlin:athena:0.4.0-alpha")
+    api("aws.sdk.kotlin:athena:0.9.0-alpha")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 
