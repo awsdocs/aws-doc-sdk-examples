4f298ca755274b65fe2dab34679b054b7731fe97
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-11-04 15:11:37 -0400

    Add files via upload

--- .kotlin_alpha/services/glue/build.gradle.kts
+++ .kotlin_alpha/services/glue/build.gradle.kts
@@ -1,23 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.31"
+    id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
     application
 }
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
+buildscript {
+    repositories {
+        jcenter()
+    }
+}
+
 repositories {
     mavenCentral()
-    mavenLocal()
+    jcenter()
 }
 
 dependencies {
-    api("aws.sdk.kotlin:glue:0.4.0-alpha")
+    implementation("aws.sdk.kotlin:glacier:0.9.0-alpha")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
