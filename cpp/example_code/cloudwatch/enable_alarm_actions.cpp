5b150c145443cc9f519e902d4277b7d4599eb157
Brian Murray <brmur@amazon.com>
2021-11-15 13:31:25 +0000

    Cloudwatch updates (C++)

--- cpp/example_code/cloudwatch/enable_alarm_actions.cpp
+++ cpp/example_code/cloudwatch/enable_alarm_actions.cpp
@@ -1,27 +1,10 @@
- 
-//snippet-sourcedescription:[enable_alarm_actions.cpp demonstrates how to enable actions on an Amazon CloudWatch alarm.]
-//snippet-keyword:[C++]
-//snippet-sourcesyntax:[cpp]
-//snippet-keyword:[Code Sample]
-//snippet-keyword:[Amazon CloudWatch]
-//snippet-service:[cloudwatch]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[]
-//snippet-sourceauthor:[AWS]
-
-
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX - License - Identifier: Apache - 2.0
 /*
-   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Purpose:
+enable_alarm_actions.cpp demonstrates how to enable actions on an Amazon CloudWatch alarm.
 
-   This file is licensed under the Apache License, Version 2.0 (the "License").
-   You may not use this file except in compliance with the License. A copy of
-   the License is located at
 
-    http://aws.amazon.com/apache2.0/
-
-   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied. See the License for the
-   specific language governing permissions and limitations under the License.
 */
 //snippet-start:[cw.cpp.enable_alarm_actions.inc]
 #include <aws/core/Aws.h>
