11f5e553323d6a3de336e82f08f411ae1a98d743
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2021-11-18 09:13:44 -0500

    Update AnalyzePhotos.kt

--- .kotlin_alpha/usecases/creating_photo_analyzer_app/src/main/kotlin/com/aws/photo/AnalyzePhotos.kt
+++ .kotlin_alpha/usecases/creating_photo_analyzer_app/src/main/kotlin/com/aws/photo/AnalyzePhotos.kt
@@ -8,56 +8,37 @@ package com.aws.photo
 import aws.sdk.kotlin.services.rekognition.RekognitionClient
 import aws.sdk.kotlin.services.rekognition.model.DetectLabelsRequest
 import aws.sdk.kotlin.services.rekognition.model.Image
-import aws.sdk.kotlin.services.rekognition.model.RekognitionException
 import org.springframework.stereotype.Component
-import kotlin.system.exitProcess
 
 @Component
 class AnalyzePhotos {
 
-    fun getClient() :RekognitionClient  {
-        val rekognitionClient = RekognitionClient { region = "us-west-2" }
-        return rekognitionClient
-    }
-
     suspend fun DetectLabels(bytesVal: ByteArray?, key: String?): MutableList<WorkItem>? {
 
-        val rekClient =  getClient()
+        // Create an Image object for the source image.
+        val souImage = Image {
+            bytes = bytesVal
+        }
 
-        try {
+        val detectLabelsRequest = DetectLabelsRequest {
+            image = souImage
+            maxLabels = 10
+        }
 
-            // Create an Image object for the source image.
-            val souImage = Image {
-                bytes = bytesVal
-            }
-
-            val detectLabelsRequest  = DetectLabelsRequest {
-                image = souImage
-                maxLabels = 10
-            }
-
-            val labelsResponse = rekClient.detectLabels(detectLabelsRequest)
+        RekognitionClient { region = "us-west-2" }.use { rekClient ->
+            val response = rekClient.detectLabels(detectLabelsRequest)
 
             // Write the results to a WorkItem instance.
-            val labels = labelsResponse.labels
-            println("Detected labels for the given photo")
             val list = mutableListOf<WorkItem>()
-            var item: WorkItem
-
-            if (labels != null) {
-                for (label in labels) {
-                    item = WorkItem()
-                    item.key = key // identifies the photo.
-                    item.confidence = label.confidence.toString()
-                    item.name = label.name
-
-                    list.add(item)
-                }
+            println("Detected labels for the given photo")
+            response.labels?.forEach { label ->
+                val item = WorkItem()
+                item.key = key // identifies the photo.
+                item.confidence = label.confidence.toString()
+                item.name = label.name
+                list.add(item)
             }
             return list
-        } catch (e: RekognitionException) {
-            println(e.message)
-            exitProcess(0)
         }
     }
 }
