c5b952a1453000a8c97154d2a87b0b926b31b3f6
Morgan Gangwere <gangwere@amazon.com>
2021-11-18 15:42:26 -0800

    [no ci][Do not merge]Rebuild go test runner
    This starts the process of rebuilding the Go test runner. More to come.

--- scripts/run_go_tests.sh
+++ scripts/run_go_tests.sh
@@ -1,38 +1,33 @@
-TestGoFile () {
-    echo Testing $1
-    pushd $1
+#!/usr/bin/env bash
+# Run tests
 
-    declare RESULT=(`go test`)  # (..) = array
-
-    popd
-    
-#    echo RESULT in TestGoFile: $RESULT
-    
-    if [ "${RESULT[0]}" != "PASS" ]
-    then
-        exit 1
-    fi
-}
+hasgotest=0
 
 for f in $@ ; do
 #    echo Looking at $f
     # Do any end with "_test.go"?
     path="$(dirname $f)"
     file="$(basename $f)"
-
-#    echo Path is $path
-#    echo filename is $file
-
-    # If it's a go test file
-    # test it
-    case $file in
-        *[a-zA-Z]*_test.go)
-#    	    echo It IS a go test file
-            TestGoFile "$path"
-	    ;;
-        *)
-#    	    echo It is NOT a go test file
-    esac
+    echo $pa
+    if [[ $f =~ /gov2/.+ ]]; then
+        hasgotest=1
+    fi
 done
+
+echo $hasgotest
+
+if [[ $hasgotest -eq 1 ]]; then
+    echo "Go files have been modified. Running tests..."
+    for mod in $( find . -name go.mod ); do
+        moddir=$(dirname $mod)
+        echo "Testing ${moddir}"
+        pushd $moddir
+        go test ./...
+        if [[ $? -ne 0 ]]; then
+            exit 0
+        fi
+        popd
+    done
+fi
 
 exit 0
