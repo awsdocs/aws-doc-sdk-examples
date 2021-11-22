f44be8a44cb3781f8843b3f99fdfac78fa3552e2
miguelcss <miguelcorreiasousa@gmail.com>
2021-11-22 17:31:18 +0000

    Revert "Refactor file names to be more explicit, removed duplicates"
    This reverts commit bf73d0c117fd3fa43dcb6b16cfddece0127b7d9b.

--- typescript/example_code/cdk/hello-cdk-stack2.ts
+++ typescript/example_code/cdk/hello-cdk-stack2.ts
@@ -1,8 +1,9 @@
 // snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-comment:[This is a full sample when you include s3-example-stack.ts, which goes in the lib dir.]
+// snippet-comment:[This is a full sample when you include HelloCdk.ts, which goes in the bin dir.]
 // snippet-sourceauthor:[Doug-AWS]
-// snippet-sourcedescription:[Instantiates the stack in s3-example-stack.ts.]
+// snippet-sourcedescription:[Creates an S3 bucket with encryption and versioning enabled.]
 // snippet-keyword:[CDK V1.0.0]
+// snippet-keyword:[s3.Bucket function]
 // snippet-keyword:[TypeScript]
 // snippet-sourcesyntax:[javascript]
 // snippet-service:[cdk]
@@ -20,11 +21,21 @@
 // This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
 // OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
-// snippet-start:[cdk.typescript.s3-example]
+// snippet-start:[cdk.typescript.hello-cdk-stack.version2]
 import * as core from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 
-import { S3ExampleStack } from '../lib/s3-example-stack';
+export class HelloCdkStack extends core.Stack {
+  constructor(scope: core.App, id: string, props?: core.StackProps) {
+    super(scope, id, props);
 
-const app = new core.App();
-new S3ExampleStack(app, 'S3ExampleStack');
-// snippet-end:[cdk.typescript.s3-example]
+    // Don't change the formatting of this section
+    // snippet-start:[cdk.typescript.hello-cdk-stack.version2_bucket]
+new s3.Bucket(this, 'MyFirstBucket', {
+  versioned: true,
+  encryption: s3.BucketEncryption.KMS_MANAGED
+});
+    // snippet-end:[cdk.typescript.hello-cdk-stack.version2_bucket]    
+  }
+}
+// snippet-end:[cdk.typescript.hello-cdk-stack.version2]
--- typescript/example_code/cdk/hello-cdk2.ts
+++ typescript/example_code/cdk/hello-cdk2.ts
@@ -1,9 +1,8 @@
 // snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-comment:[This is a full sample when you include s3-example.ts, which goes in the bin dir.]
+// snippet-comment:[This is a full sample when you include hello-cdk-stack.ts, which goes in the lib dir.]
 // snippet-sourceauthor:[Doug-AWS]
-// snippet-sourcedescription:[Creates an S3 bucket with encryption and versioning enabled.]
+// snippet-sourcedescription:[Instantiates the stack in hello-cdk-stack.ts.]
 // snippet-keyword:[CDK V1.0.0]
-// snippet-keyword:[s3.Bucket function]
 // snippet-keyword:[TypeScript]
 // snippet-sourcesyntax:[javascript]
 // snippet-service:[cdk]
@@ -21,21 +20,11 @@
 // This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
 // OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
-// snippet-start:[cdk.typescript.s3-example-stack]
+// snippet-start:[cdk.typescript.hello-cdk.version2]
 import * as core from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
 
-export class S3ExampleStack extends core.Stack {
-  constructor(scope: core.App, id: string, props?: core.StackProps) {
-    super(scope, id, props);
+import { HelloCdkStack } from '../lib/hello-cdk-stack2';
 
-    // Don't change the formatting of this section
-    // snippet-start:[cdk.typescript.s3-example-stack_bucket]
-new s3.Bucket(this, 'MyFirstBucket', {
-  versioned: true,
-  encryption: s3.BucketEncryption.KMS_MANAGED
-});
-    // snippet-end:[cdk.typescript.s3-example-stack_bucket]
-  }
-}
-// snippet-end:[cdk.typescript.s3-example-stack]
+const app = new core.App();
+new HelloCdkStack(app, 'HelloCdkStack');
+// snippet-end:[cdk.typescript.hello-cdk.version2]
