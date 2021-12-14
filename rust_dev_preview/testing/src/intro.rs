39266834708dfea88fb7f58bbba988b5a9df670f
Doug <dougsch@amazon.com>
2021-12-14 13:27:39 -0800

    Added testing code examples in Rust

--- rust_dev_preview/testing/src/intro.rs
+++ rust_dev_preview/testing/src/intro.rs
@@ -4,9 +4,11 @@
  */
 
 use aws_config::meta::region::RegionProviderChain;
-// So we can refer to the S3 SDK as s3 for the rest of this example.
+// snippet-start:[testing.rust.intro-import]
+// So we can refer to the S3 package as s3 for the rest of the example.
 use aws_sdk_s3 as s3;
-use aws_sdk_s3::{Client, Error, Region, PKG_VERSION};
+// snippet-end:[testing.rust.intro-import]
+use std::error::Error;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -19,7 +21,7 @@ struct Opt {
     #[structopt(short, long)]
     bucket: String,
 
-    /// The prefix to the bucket name.
+    /// The bucket prefix.
     #[structopt(short, long)]
     prefix: String,
 
@@ -28,12 +30,17 @@ struct Opt {
     verbose: bool,
 }
 
-// Get the size of the files.
+// The two testing approaches imported as modules below
+mod enums;
+mod traits;
+
+// snippet-start:[testing.rust.intro-function]
+// Lists all objects in an S3 bucket with the given prefix, and adds up their size.
 async fn determine_prefix_file_size(
-    s3: &s3::Client,
+    s3: s3::Client,
     bucket: &str,
     prefix: &str,
-) -> Result<usize, Error> {
+) -> Result<usize, Box<dyn Error + Send + Sync + 'static>> {
     let mut next_token: Option<String> = None;
     let mut total_size_bytes = 0;
     loop {
@@ -45,33 +52,25 @@ async fn determine_prefix_file_size(
             .send()
             .await?;
 
+        // Add up the file sizes we got back
         if let Some(contents) = response.contents() {
             for object in contents {
                 total_size_bytes += object.size() as usize;
             }
         }
 
-        // Handle pagination, and break out of the loop if there are no more pages.
+        // Handle pagination, and break the loop if there are no more pages
         next_token = response.continuation_token().map(|t| t.to_string());
         if !response.is_truncated() {
             break;
         }
     }
-
     Ok(total_size_bytes)
 }
+// snippet-end:[testing.rust.intro-function]
 
-/// Get the size of the files with a name starting with a prefix.
-/// # Arguments
-///
-/// * `-b BUCKET` - The name of the bucket containing the files.
-/// * `-p PREFIX` - The prefix.
-/// * `[-r REGION]` - The Region in which the client is created.
-///   If not supplied, uses the value of the **AWS_REGION** environment variable.
-///   If the environment variable is not set, defaults to **us-west-2**.
-/// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     tracing_subscriber::fmt::init();
 
     let Opt {
@@ -81,14 +80,14 @@ async fn main() -> Result<(), Error> {
         verbose,
     } = Opt::from_args();
 
-    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+    let region_provider = RegionProviderChain::first_try(region.map(s3::Region::new))
         .or_default_provider()
-        .or_else(Region::new("us-west-2"));
+        .or_else(s3::Region::new("us-west-2"));
 
     println!();
 
     if verbose {
-        println!("S3 client version: {}", PKG_VERSION);
+        println!("S3 client version: {}", s3::PKG_VERSION);
         println!(
             "Region:            {}",
             region_provider.region().await.unwrap().as_ref()
@@ -99,11 +98,10 @@ async fn main() -> Result<(), Error> {
     }
 
     let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
+    let client = s3::Client::new(&shared_config);
 
-    let size = determine_prefix_file_size(&client, &bucket, &prefix).await?;
-
-    println!("The # of bytes: {}", size);
+    let total_size = determine_prefix_file_size(client, &bucket, &prefix).await?;
+    println!("Total size: {}", total_size);
 
     Ok(())
 }
