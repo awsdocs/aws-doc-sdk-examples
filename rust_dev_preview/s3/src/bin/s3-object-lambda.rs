218b2513e42b4f4ce83b7cd32cc559d9f3a76681
Doug <dougsch@amazon.com>
2021-12-15 11:24:37 -0800

    Added accessors where available to Rust code example for S3 Object Lambda

--- rust_dev_preview/s3/src/bin/s3-object-lambda.rs
+++ rust_dev_preview/s3/src/bin/s3-object-lambda.rs
@@ -31,11 +31,11 @@ struct Opt {
 // Shows your buckets in the endpoint.
 async fn show_buckets(client: &Client) -> Result<(), Error> {
     let resp = client.list_buckets().send().await?;
-    let buckets = resp.buckets.unwrap_or_default();
+    let buckets = resp.buckets().unwrap_or_default();
     let num_buckets = buckets.len();
 
-    for bucket in &buckets {
-        println!("{}", bucket.name.as_deref().unwrap_or_default());
+    for bucket in buckets {
+        println!("{}", bucket.name().as_deref().unwrap_or_default());
     }
 
     println!();
