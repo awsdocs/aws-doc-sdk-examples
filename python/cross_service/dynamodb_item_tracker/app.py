ddaa36953d18ebef40361ea105be54b0cad6f1b5
Laren-AWS <larencra@amazon.com>
2022-01-13 17:47:30 -0800

    Added error handling and other polish.

--- python/cross_service/dynamodb_item_tracker/app.py
+++ python/cross_service/dynamodb_item_tracker/app.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 import logging
 import os
-from storage import Storage
+from storage import Storage, StorageError
 
 app = Flask(__name__)
+app.secret_key = 'change-for-production!'
 _STORAGE = None
 logger = logging.getLogger(__name__)
 
@@ -24,49 +25,60 @@ def add_formatted_date(work_item):
 
 @app.route('/')
 @app.route('/items')
-def items():
+def items(error=None):
     storage = get_storage()
     logger.info("Getting items for table %s.", storage.table.name)
-    work_items = storage.get_items()
+    status_filter = request.args.get('status_filter', 'All')
+
+    work_items = []
+    try:
+        work_items = storage.get_items(status_filter)
+    except StorageError as err:
+        error = str(err)
     for work_item in work_items:
         add_formatted_date(work_item)
     return render_template(
-        'items.html', table=storage.table.name, items=work_items)
+        'items.html', table=storage.table.name, items=work_items,
+        status_filter=status_filter, error=error)
 
 
 @app.route('/item/', methods=['GET', 'POST'])
 @app.route('/item/<item_id>', methods=['GET', 'POST'])
 def item(item_id=None):
     if request.method == 'GET':
+        work_item = {'item_id': item_id}
+        error = None
         if item_id is not None:
-            work_item = get_storage().get_item(item_id)
-            add_formatted_date(work_item)
-            return render_template('item.html', **work_item)
-        else:
-            return render_template('item.html')
+            try:
+                work_item = get_storage().get_item(item_id)
+                add_formatted_date(work_item)
+            except StorageError as err:
+                error = str(err)
+        return render_template('item.html', **work_item, action='get', error=error)
     elif request.method == 'POST':
-        get_storage().add_or_update_item({
+        work_item = {
             'item_id': request.form.get('item_id'),
             'name': request.form.get('name'),
             'created_date': request.form.get('created_date'),
             'description': request.form.get('description'),
-            'status': request.form.get('status')})
-        return redirect(url_for('items'))
+            'status': request.form.get('status')}
+        try:
+            get_storage().add_or_update_item(work_item)
+        except StorageError as err:
+            return render_template(
+                'item.html', **work_item, action='add or update', error=err)
+        else:
+            return redirect(url_for('items'))
 
 
-@app.route('/item/delete/<item_id>')
+@app.route('/items/delete/<item_id>')
 def delete_item(item_id):
-    get_storage().delete_item(item_id)
+    try:
+        get_storage().delete_item(item_id)
+    except StorageError as err:
+        flash(f"Couldn't delete item '{item_id}'.")
+        flash(str(err))
     return redirect(url_for('items'))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('hello', name=user))
-    else:
-        return render_template('login.html')
 
 
 if __name__ == '__main__':
--- python/cross_service/dynamodb_item_tracker/storage.py
+++ python/cross_service/dynamodb_item_tracker/storage.py
@@ -6,10 +6,15 @@ import logging
 import os
 from uuid import uuid4
 import boto3
+from boto3.dynamodb.conditions import Attr
 from botocore.exceptions import ClientError
 
 
 logger = logging.getLogger(__name__)
+
+
+class StorageError(Exception):
+    pass
 
 
 class Storage:
@@ -28,33 +33,60 @@ class Storage:
         logger.info("Table %s ready.", table.name)
         return cls(table, ddb_resource)
 
-    def get_items(self):
-        return self.table.scan().get('Items', [])
+    def get_items(self, status_filter='All'):
+        try:
+            if status_filter == 'All':
+                work_items = self.table.scan().get('Items', [])
+            else:
+                work_items = self.table.scan(
+                    FilterExpression=Attr('status').eq(status_filter)).get('Items', [])
+        except ClientError as err:
+            logger.exception(
+                "Couldn't get items from table %s with status %s.", self.table.name,
+                status_filter)
+            raise StorageError(err)
+        else:
+            return work_items
 
     def get_item(self, item_id):
-        return self.table.get_item(Key={'item_id': item_id}).get('Item', {})
+        try:
+            item = self.table.get_item(Key={'item_id': item_id}).get('Item', {})
+        except ClientError as err:
+            logger.exception(
+                "Couldn't get item %s from table %s.", item_id, self.table.name)
+            raise StorageError(err)
+        else:
+            return item
 
     def add_or_update_item(self, item):
-        if item['item_id'] is None:
-            item['item_id'] = str(uuid4())
-            item['created_date'] = str(datetime.now())
-            self.table.put_item(Item=item)
-        else:
-            self.table.update_item(
-                Key={'item_id': item['item_id']},
-                AttributeUpdates={
-                    'name': {
-                        'Value': item['name'],
-                        'Action': 'PUT'
-                    },
-                    'description': {
-                        'Value': item['description'],
-                        'Action': 'PUT'
-                    },
-                    'status': {
-                        'Value': item['status'],
-                        'Action': 'PUT'
-                    }})
+        try:
+            if item['item_id'] is None:
+                item['item_id'] = str(uuid4())
+                item['created_date'] = str(datetime.now())
+                self.table.put_item(Item=item)
+            else:
+                self.table.update_item(
+                    Key={'item_id': item['item_id']},
+                    AttributeUpdates={
+                        'name': {
+                            'Value': item['name'],
+                            'Action': 'PUT'
+                        },
+                        'description': {
+                            'Value': item['description'],
+                            'Action': 'PUT'
+                        },
+                        'status': {
+                            'Value': item['status'],
+                            'Action': 'PUT'
+                        }})
+        except ClientError as err:
+            logger.exception("Couldn't add or update item %s in table %s.")
+            raise StorageError(err)
 
     def delete_item(self, item_id):
-        self.table.delete_item(Key={'item_id': item_id})
+        try:
+            self.table.delete_item(Key={'item_id': item_id})
+        except ClientError as err:
+            logger.exception("Couldn't delete item %s from table %s.")
+            raise StorageError(err)
