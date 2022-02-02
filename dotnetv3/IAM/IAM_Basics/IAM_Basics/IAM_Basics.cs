772f47397dc0cc7174b5ef5b9035af65b046c21d
Irene Smith <igsmith@amazon.com>
2022-02-02 14:20:13 -0500

    Creating IAM Scenario

---
+++ dotnetv3/IAM/IAM_Basics/IAM_Basics/IAM_Basics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IAM_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
