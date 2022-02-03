19f268f45ce64501573a81181b01e8940c42860e
Brian Murray <40031786+brmur@users.noreply.github.com>
2022-02-03 16:49:10 +0000

    S3_basics scenario

--- javascriptv3/example_code/s3/scenarios/s3_basics/src/s3_basics.js
+++ javascriptv3/example_code/s3/scenarios/s3_basics/src/s3_basics.js
@@ -12,48 +12,148 @@ resources at the end of the demo.
 
 Inputs (in command line):
 node s3_basics.js <the bucket name> <the AWS Region to use> <object name> <object content>
-*/
 
-import { CreateBucketCommand } from "@aws-sdk/client-s3";
+Running the code:
+node s3_basics.js
+*/
+// snippet-start:[s3_basics.JavaScript.s3_basics]
+import {
+  CreateBucketCommand,
+  PutObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  DeleteBucketCommand,
+} from "@aws-sdk/client-s3";
 import { s3Client, REGION } from "../libs/s3Client.js"; // Helper function that creates Amazon S3 service client module.
-import { uploadObject } from "./helpers/uploadObject.js"; // Helper function that creates and uploads and object to Amazon.
 
 if (process.argv.length < 5) {
   console.log(
-    "Usage: node s3_basics.js <the bucket name> <the AWS Region to use> <object name> <object content>\n" +
-      "Example: node s3_basics.js test-bucket test.txt 'Test Content'"
+      "Usage: node s3_basics.js <the bucket name> <the AWS Region to use> <object name> <object content>\n" +
+      "Example: node s3_basics_full.js test-bucket test.txt 'Test Content'"
   );
-  // process.exit(1);
 }
+const bucket_name = process.argv[2];
+const object_key = process.argv[3];
+const object_content = process.argv[4];
 
-// Create constants from the command line inputs.
-export const bucket_name = process.argv[2];
-export const object_key = process.argv[3];
-export const object_content = process.argv[4];
-
-// Set the parameters for creating a bucket.
-export const create_bucket_params = {
-  Bucket: bucket_name,
-  CreateBucketConfiguration: {
-    LocationConstraint: REGION,
-  },
-};
-
-export const createBucket = async () => {
+export const run = async (bucket_name, object_key, object_content) => {
   try {
+    const create_bucket_params = {
+      Bucket: bucket_name,
+    };
     console.log("\nCreating the bucket, named " + bucket_name + "...\n");
+    console.log("about to create");
     const data = await s3Client.send(
-      new CreateBucketCommand(create_bucket_params)
+        new CreateBucketCommand(create_bucket_params)
     );
     console.log("Bucket created at ", data.Location);
     try {
-      uploadObject();
+      console.log(
+          "\nCreated and uploaded an object named" +
+          object_key +
+          " to first bucket " +
+          bucket_name +
+          " ...\n"
+      );
+      // Set the parameters for the object to upload.
+      const object_upload_params = {
+        Bucket: bucket_name,
+        // Specify the name of the new object. For example, 'test.html'.
+        // To create a directory for the object, use '/'. For example, 'myApp/package.json'.
+        Key: object_key,
+        // Content of the new object.
+        Body: object_content,
+      };
+      // Create and upload the object to the first Amazon S3 bucket.
+      await s3Client.send(new PutObjectCommand(object_upload_params));
+      console.log(
+          "Successfully uploaded object: " +
+          object_upload_params.Bucket +
+          "/" +
+          object_upload_params.Key
+      );
+      try {
+        // Copy the object from the first bucket to the second bucket.
+        const copy_object_params = {
+          Bucket: bucket_name,
+          CopySource: "/" + bucket_name + "/" + object_key,
+          Key: "copy-destination/" + object_key,
+        };
+        console.log(
+            "\nCopying " +
+            object_key +
+            " from" +
+            bucket_name +
+            " to " +
+            bucket_name +
+            "/" +
+            copy_object_params.Key +
+            " ...\n"
+        );
+        await s3Client.send(new CopyObjectCommand(copy_object_params));
+        console.log("Success, object copied to folder.");
+        try {
+          console.log("\nDeleting " + object_key + " from" + bucket_name);
+          const delete_object_from_bucket_params = {
+            Bucket: bucket_name,
+            Key: object_key,
+          };
+
+          await s3Client.send(
+              new DeleteObjectCommand(delete_object_from_bucket_params)
+          );
+          console.log("Success. Object deleted from bucket.");
+          try {
+            console.log(
+                "\nDeleting " +
+                object_key +
+                " from " +
+                bucket_name +
+                "/copy-destination folder"
+            );
+            const delete_object_from_folder_params = {
+              Bucket: bucket_name,
+              Key: "copy-destination/" + object_key,
+            };
+
+            await s3Client.send(
+                new DeleteObjectCommand(delete_object_from_folder_params)
+            );
+            console.log("Success. Object deleted from folder.");
+            return "Run successfully"; // For unit tests.
+            try {
+              console.log(
+                  "\nDeleting the bucket named " + bucket_name + "...\n"
+              );
+              const delete_bucket_params = { Bucket: bucket_name };
+              await s3Client.send(
+                  new DeleteBucketCommand(delete_bucket_params)
+              );
+              console.log("Success. First bucket deleted.");
+            } catch (err) {
+              console.log("Error deleting object from folder.", err);
+              process.exit(1);
+            }
+          } catch (err) {
+            console.log("Error deleting  bucket.", err);
+            process.exit(1);
+          }
+        } catch (err) {
+          console.log("Error deleting object from  bucket.", err);
+          process.exit(1);
+        }
+      } catch (err) {
+        console.log("Error copying object from to folder", err);
+        process.exit(1);
+      }
     } catch (err) {
-      console.log("Error uploading  object", err);
+      console.log("Error creating and upload object to  bucket", err);
+      process.exit(1);
     }
-    return data; // For unit tests.
+    console.log("works");
   } catch (err) {
-    console.log("Error creating  bucket", err);
+    console.log("Error creating bucket", err);
   }
 };
-createBucket();
+run(bucket_name, object_content, object_key);
+// snippet-start:[s3_basics.JavaScript.s3_basics]
--- javascriptv3/example_code/s3/scenarios/s3_basics/tests/s3_basics.test.js
+++ javascriptv3/example_code/s3/scenarios/s3_basics/tests/s3_basics.test.js
@@ -1,53 +1,20 @@
-jest.mock("../libs/s3Client");
-jest.mock("@aws-sdk/client-s3");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+const value = getRandomInt(1000);
+const bucket_name = "bucket-" + value;
+process.argv.push("node", "s3_basics_full.js", bucket_name, "test.txt", "Test Content");
+console.log('argv', process.argv[4], process.argv[5], process.argv[6])
+const expected = "Run successfully";
 
-// Get service clients module and commands.
 import "regenerator-runtime/runtime";
-import { createBucket, bucket_name, object_key, object_content } from "../src/s3_basics.js";
-import { uploadObject } from "../src/helpers/uploadObject.js";
-import { copyObject } from "../src/helpers/copyObject.js";
-import {REGION, s3Client} from "../libs/s3Client.js";
+import { run } from "../src/s3_basics.js";
 
-const create_bucket_params = {
-  Bucket: bucket_name,
-  CreateBucketConfiguration: {
-    LocationConstraint: REGION,
-  },
-};
-
-describe("@aws-sdk/client-s3 mock", () => {
-  it("should successfully mock S3 client", async () => {
-    s3Client.send.mockResolvedValue({ isMock: true });
-    const response = await createBucket(create_bucket_params);
-    expect(response.isMock).toEqual(true);
+describe("Test function runs", () => {
+  it("should successfully run",  async() => {
+    /*    console.log(value);*/
+    const response = await run(process.argv[4], process.argv[5], process.argv[6]);
+    console.log("Response ", Promise.resolve(response));
+    expect(response).toEqual((expected));
   });
 });
-
-const upload_object_params = {
-  Bucket: bucket_name,
-  CopySource: "/" + bucket_name + "/" + object_key,
-  Key: "copy-destination/" + object_key,
-};
-
-describe("@aws-sdk/client-s3 mock", () => {
-  it("should successfully mock S3 client", async () => {
-    s3Client.send.mockResolvedValue({ isMock: true });
-    const response = await uploadObject(upload_object_params);
-    expect(response.isMock).toEqual(true);
-  });
-});
-
-const copy_object_params = {
-  Bucket: bucket_name,
-  CopySource: "/" + bucket_name + "/" + object_key,
-  Key: "copy-destination/" + object_key,
-};
-
-describe("@aws-sdk/client-s3 mock", () => {
-  it("should successfully mock S3 client", async () => {
-    s3Client.send.mockResolvedValue({ isMock: true });
-    const response = await copyObject(copy_object_params);
-    expect(response.isMock).toEqual(true);
-  });
-});
-
