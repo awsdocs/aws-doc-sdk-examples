17c5d78b6f559d601abf0027a8d31ae2b8c46519
Irene Smith <irenesmith13@gmail.com>
2022-02-07 16:40:44 -0500

    Adding another Polly example as a test.

---
+++ dotnetv3/Polly/DescribeVoicesExample/DescribeVoices.cs
@@ -0,0 +1,73 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+/// <summary>
+/// This example shows how to retrieve a list of all voices available for
+/// Amazon Polly. It first lists all voices and then only those voices that
+/// work with language code "en-US" or United States English.
+/// </summary>
+namespace DescribeVoicesExample
+{
+    using System;
+    using System.Threading.Tasks;
+    using Amazon.Polly;
+    using Amazon.Polly.Model;
+
+    public class DescribeVoices
+    {
+        // snippet-start:[Polly.dotnetv3.DescribeVoicesExample]
+        public static async Task Main()
+        {
+            var client = new AmazonPollyClient();
+
+            var allVoicesRequest = new DescribeVoicesRequest();
+            var enUsVoicesRequest = new DescribeVoicesRequest()
+            {
+                LanguageCode = "en-US",
+            };
+
+            try
+            {
+                string nextToken;
+                do
+                {
+                    var allVoicesResponse = await client.DescribeVoicesAsync(allVoicesRequest);
+                    nextToken = allVoicesResponse.NextToken;
+                    allVoicesRequest.NextToken = nextToken;
+
+                    Console.WriteLine("\nAll voices: ");
+                    allVoicesResponse.Voices.ForEach(voice =>
+                    {
+                        DisplayVoiceInfo(voice);
+                    });
+                }
+                while (nextToken is not null);
+
+                do
+                {
+                    var enUsVoicesResponse = await client.DescribeVoicesAsync(enUsVoicesRequest);
+                    nextToken = enUsVoicesResponse.NextToken;
+                    enUsVoicesRequest.NextToken = nextToken;
+
+                    Console.WriteLine("\nen-US voices: ");
+                    enUsVoicesResponse.Voices.ForEach(voice =>
+                    {
+                        DisplayVoiceInfo(voice);
+                    });
+                }
+                while (nextToken is not null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error listing Polly voices: {ex.Message}");
+            }
+        }
+
+        public static void DisplayVoiceInfo(Voice voice)
+        {
+            Console.WriteLine($" Name: {voice.Name}\tGender: {voice.Gender}\tLanguageName: {voice.LanguageName}");
+        }
+
+        // snippet-end:[Polly.dotnetv3.DescribeVoicesExample]
+    }
+}
