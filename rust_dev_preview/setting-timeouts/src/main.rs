7276427ddf4eb0f97ed3aec0ca5160a39c056e24
Zelda Hessler <zhessler@amazon.com>
2022-02-09 10:57:36 -0600

    update: timeouts example for TimeoutConfig breaking change

--- rust_dev_preview/setting-timeouts/src/main.rs
+++ rust_dev_preview/setting-timeouts/src/main.rs
@@ -27,43 +27,45 @@ use std::time::Duration;
 /// `Duration::from_secs(2)` to `Duration::from_millis(2)`.
 ///
 /// You could _also_ trigger the timeouts by replacing the HTTPS connector _(`conns::https()`)_
-/// with a `NeverConnector`. That's how we manually trigger timeouts when writing tests.
+/// with a `NeverConnector`. That's how we consistently trigger timeouts during testing.
 #[tokio::main]
 async fn main() -> Result<(), aws_sdk_s3::Error> {
     tracing_subscriber::fmt::init();
 
-    // Here we set our "first group" timeouts
-    let request_timeout_config = TimeoutConfig::new()
+    // Here we create an object that holds timeout-related configuration. We'll have to pass this
+    // config into two places. This is because different timeouts are handled at different
+    // "levels" of the AWS SDK Client networking stack. We'll note which timeouts are getting
+    // set each time we pass in this cofiguration.
+    let timeout_config = TimeoutConfig::new()
         // This timeout acts at the "Request to a service" level. When the SDK makes a request to a
         // service, that "request" may actually comprise of several HTTP requests in order to retry
         // failures that are likely spurious or to refresh credentials.
         .with_api_call_timeout(Some(Duration::from_secs(2)))
         // This timeout acts at the "HTTP request" level and will set a separate timeout for each
         // HTTP request made as part of a "service request"
-        .with_api_call_attempt_timeout(Some(Duration::from_secs(2)));
-
-    // Timeouts can also be defined in your environment or AWS profile but in this example we
-    // overrule any that happen to be set
-    let shared_config = aws_config::from_env()
-        .timeout_config(request_timeout_config)
-        .load()
-        .await;
-
-    // Here we set our "second group" timeouts
-    let http_timeout_config = timeout::Settings::new()
+        .with_api_call_attempt_timeout(Some(Duration::from_secs(2)))
         // A limit on the amount of time an application takes to attempt to read the first byte over
         // an established, open connection after write request.
         // Also known as the "time to first byte" timeout
-        .with_read_timeout(Duration::from_secs(2))
+        .with_read_timeout(Some(Duration::from_secs(2)))
         // A limit on the amount of time after making an initial connect attempt on a socket to
         // complete the connect-handshake
-        .with_connect_timeout(Duration::from_secs(2));
+        .with_connect_timeout(Some(Duration::from_secs(2)));
 
-    // These timeouts must be passed to create the `Connector` that will handle our HTTP requests.
+    // Timeouts can be defined in your environment or AWS profile but in this example we
+    // overrule any that happen to be set
+    // NOTE: The two API call timeouts get set here
+    let shared_config = aws_config::from_env()
+        .timeout_config(timeout_config.clone())
+        .load()
+        .await;
+
+    // These timeouts must also be passed to create the `Connector` that will handle our HTTP requests.
     // If a timeout needs to be changed after this, we'd have to create a new `Connector`.
+    // NOTE: The read and connect timeouts get set here
     let conn = DynConnector::new(
         hyper_ext::Adapter::builder()
-            .timeout(&http_timeout_config)
+            .timeout(&timeout_config)
             .build(conns::https()),
     );
     let s3_config = aws_sdk_s3::Config::from(&shared_config);
