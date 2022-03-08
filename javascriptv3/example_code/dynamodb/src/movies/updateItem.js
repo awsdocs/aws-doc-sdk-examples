935a39ba7b394e26292420a0e253c844017303e1
Brian Murray <40031786+brmur@users.noreply.github.com>
2022-03-08 19:32:26 +0000

    Add DDB MVP for JS (V3) (#2968)
    * DDB MVP for JS (V3)
    
    Co-authored-by: Morgan Gangwere <470584+indrora@users.noreply.github.com>

---
+++ javascriptv3/example_code/dynamodb/src/movies/updateItem.js
@@ -0,0 +1,49 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+
+ABOUT THIS NODE.JS EXAMPLE: This example works with the AWS SDK for JavaScript version 3 (v3),
+which is available at https://github.com/aws/aws-sdk-js-v3.
+
+Purpose:
+updateItem.js demonstrates how to use the Amazon DynamoDB document client to create or update an item in an Amazon DynamoDB table.
+
+Inputs (replace in code):
+- TABLE_NAME
+- MOVIE_NAME
+- MOVIE_YEAR
+- MOVIE_PLOT
+- MOVIE_RANK
+
+Running the code:
+node updateItem.js
+*/
+// snippet-start:[dynamodb.JavaScript.movies.updateItemV3]
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { ddbDocClient } from "../libs/ddbDocClient.js";
+
+export const updateItem = async () => {
+  // Set the parameters.
+  const params = {
+    TableName: "TABLE_NAME",
+    Key: {
+      title: "MOVIE_NAME",
+      year: "MOVIE_YEAR",
+    },
+    ProjectionExpression: "#r",
+    ExpressionAttributeNames: { "#r": "rank" },
+    UpdateExpression: "set info.plot = :p, info.#r = :r",
+    ExpressionAttributeValues: {
+      ":p": "MOVIE_PLOT",
+      ":r": "MOVIE_RANK",
+    },
+  };
+  try {
+    const data = await ddbDocClient.send(new UpdateCommand(params));
+    console.log("Success - item added or updated", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+updateItem();
+// snippet-end:[dynamodb.JavaScript.movies.updateItemV3]
