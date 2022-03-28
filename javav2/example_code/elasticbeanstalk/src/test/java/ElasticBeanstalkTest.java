782208083afbc21d33ddf8cf7f70e74d924fbca3
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-03-28 09:18:59 -0400

    Update ElasticBeanstalkTest.java

--- javav2/example_code/elasticbeanstalk/src/test/java/ElasticBeanstalkTest.java
+++ javav2/example_code/elasticbeanstalk/src/test/java/ElasticBeanstalkTest.java
@@ -1,3 +1,7 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 import com.aws.example.*;
 import software.amazon.awssdk.services.elasticbeanstalk.ElasticBeanstalkClient;
 import org.junit.jupiter.api.*;
