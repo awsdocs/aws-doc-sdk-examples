5cbec8a715bea8fda3e5ae492dbf924e2061f9bf
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-04 08:36:01 -0400

    Update AWSService.cs

--- dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/AWSService.cs
+++ dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/AWSService.cs
@@ -1,3 +1,7 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
