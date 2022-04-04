60f6e8d9c074937ce700acda476657a6652fe13b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-04 08:36:38 -0400

    Update WorkItem.cs

--- dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/WorkItem.cs
+++ dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/WorkItem.cs
@@ -1,4 +1,8 @@
-﻿using System;
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
