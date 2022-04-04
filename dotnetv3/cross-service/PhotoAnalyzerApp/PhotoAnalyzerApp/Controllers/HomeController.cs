6dd3322ee4b05176f3e18828b013775e5f629050
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-04 10:54:12 -0400

    Update HomeController.cs

--- dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/HomeController.cs
+++ dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/HomeController.cs
@@ -1,8 +1,3 @@
-﻿/*
-   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
-*/
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
