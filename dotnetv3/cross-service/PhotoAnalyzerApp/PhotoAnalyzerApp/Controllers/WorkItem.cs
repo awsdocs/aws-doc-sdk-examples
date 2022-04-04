756cc65835a2f6fe121333802648eb760f0f30f7
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-04 09:56:20 -0400

    Update WorkItem.cs

--- dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/WorkItem.cs
+++ dotnetv3/cross-service/PhotoAnalyzerApp/PhotoAnalyzerApp/Controllers/WorkItem.cs
@@ -11,38 +11,8 @@ namespace PhotoAnalyzerApp.Controllers
 {
     public class WorkItem
     {
-        private String key;
-        private String name;
-        private String confidence;
-
-        public void setKey(String key)
-        {
-            this.key = key;
-        }
-
-        public String getKey()
-        {
-            return this.key;
-        }
-
-        public void setName(String name)
-        {
-            this.name = name;
-        }
-
-        public String getName()
-        {
-            return this.name;
-        }
-
-        public void setConfidence(String confidence)
-        {
-            this.confidence = confidence;
-        }
-
-        public String getConfidence()
-        {
-            return this.confidence;
-        }
+        public String Key { get; set; }
+        public String Name { get; set; }
+        public String Confidence { get; set; }
     }
 }
