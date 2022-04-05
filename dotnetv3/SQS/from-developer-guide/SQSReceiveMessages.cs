a1e869b668a7f8939da1f8d655adebc36748d6db
Paul Bartos <pbart@amazon.com>
2022-04-05 01:06:33 +0000

    Some formatting.

--- dotnetv3/SQS/from-developer-guide/SQSReceiveMessages.cs
+++ dotnetv3/SQS/from-developer-guide/SQSReceiveMessages.cs
@@ -19,85 +19,86 @@ using Amazon.SQS.Model;
 
 namespace SQSReceiveMessages
 {
-  class Program
-  {
-    private const int MaxMessages = 1;
-    private const int WaitTime = 2;
-
-    static async Task Main(string[] args)
+    class Program
     {
-      // Do some checks on the command-line.
-      if(args.Length == 0)
-      {
-        Console.WriteLine("\nUsage: SQSReceiveMessages queue_url");
-        Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
-        return;
-      }
-      if(!args[0].StartsWith("https://sqs."))
-      {
-        Console.WriteLine("\nThe command-line argument isn't a queue URL:");
-        Console.WriteLine($"{args[0]}");
-        return;
-      }
+        private const int MaxMessages = 1;
+        private const int WaitTime = 2;
 
-      // Create the Amazon SQS client.
-      var sqsClient = new AmazonSQSClient();
-
-      // (Could verify that the queue exists.)
-      // Read messages from the queue and perform appropriate actions.
-      Console.WriteLine($"Reading messages from queue\n  {args[0]}");
-      Console.WriteLine("Press any key to stop. (Response might be slightly delayed.)");
-      do
-      {
-        var msg = await GetMessage(sqsClient, args[0], WaitTime);
-        if(msg.Messages.Count != 0)
+        static async Task Main(string[] args)
         {
-          if(ProcessMessage(msg.Messages[0]))
-            await DeleteMessage(sqsClient, msg.Messages[0], args[0]);
+            // Do some checks on the command-line.
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\nUsage: SQSReceiveMessages queue_url");
+                Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
+                return;
+            }
+            if (!args[0].StartsWith("https://sqs."))
+            {
+                Console.WriteLine("\nThe command-line argument isn't a queue URL:");
+                Console.WriteLine($"{args[0]}");
+                return;
+            }
+
+            // Create the Amazon SQS client.
+            var sqsClient = new AmazonSQSClient();
+
+            // (Could verify that the queue exists.)
+            // Read messages from the queue and perform appropriate actions.
+            Console.WriteLine($"Reading messages from queue\n  {args[0]}");
+            Console.WriteLine("Press any key to stop. (Response might be slightly delayed.)");
+            do
+            {
+                var msg = await GetMessage(sqsClient, args[0], WaitTime);
+                if (msg.Messages.Count != 0)
+                {
+                    if (ProcessMessage(msg.Messages[0]))
+                        await DeleteMessage(sqsClient, msg.Messages[0], args[0]);
+                }
+            } while (!Console.KeyAvailable);
         }
-      } while(!Console.KeyAvailable);
+
+
+        // snippet-start:[SQS.dotnetv3.SQSReceiveMessages.GetMessage]
+        //
+        // Method to read a message from the given queue.
+        // In this example, it gets one message at a time.
+        private static async Task<ReceiveMessageResponse> GetMessage(
+          IAmazonSQS sqsClient, string qUrl, int waitTime = 0)
+        {
+            return await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = qUrl,
+                MaxNumberOfMessages = MaxMessages,
+                WaitTimeSeconds = waitTime
+                // (Could also request attributes, set visibility timeout, etc.)
+            });
+        }
+        // snippet-end:[SQS.dotnetv3.SQSReceiveMessages.GetMessage]
+
+
+        //
+        // Method to process a message.
+        // In this example, it simply prints the message.
+        private static bool ProcessMessage(Message message)
+        {
+            Console.WriteLine($"\nMessage body of {message.MessageId}:");
+            Console.WriteLine($"{message.Body}");
+            return true;
+        }
+
+
+        // snippet-start:[SQS.dotnetv3.SQSReceiveMessages.DeleteMessage]
+        //
+        // Method to delete a message from a queue.
+        private static async Task DeleteMessage(
+          IAmazonSQS sqsClient, Message message, string qUrl)
+        {
+            Console.WriteLine($"\nDeleting message {message.MessageId} from queue...");
+            await sqsClient.DeleteMessageAsync(qUrl, message.ReceiptHandle);
+        }
+        // snippet-end:[SQS.dotnetv3.SQSReceiveMessages.DeleteMessage]
+
     }
-
-
-    // snippet-start:[SQS.dotnetv3.SQSReceiveMessages.GetMessage]
-    //
-    // Method to read a message from the given queue.
-    // In this example, it gets one message at a time.
-    private static async Task<ReceiveMessageResponse> GetMessage(
-      IAmazonSQS sqsClient, string qUrl, int waitTime=0)
-    {
-      return await sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest{
-        QueueUrl=qUrl,
-        MaxNumberOfMessages=MaxMessages,
-        WaitTimeSeconds=waitTime
-        // (Could also request attributes, set visibility timeout, etc.)
-      });
-    }
-    // snippet-end:[SQS.dotnetv3.SQSReceiveMessages.GetMessage]
-
-
-    //
-    // Method to process a message.
-    // In this example, it simply prints the message.
-    private static bool ProcessMessage(Message message)
-    {
-      Console.WriteLine($"\nMessage body of {message.MessageId}:");
-      Console.WriteLine($"{message.Body}");
-      return true;
-    }
-
-
-    // snippet-start:[SQS.dotnetv3.SQSReceiveMessages.DeleteMessage]
-    //
-    // Method to delete a message from a queue.
-    private static async Task DeleteMessage(
-      IAmazonSQS sqsClient, Message message, string qUrl)
-    {
-      Console.WriteLine($"\nDeleting message {message.MessageId} from queue...");
-      await sqsClient.DeleteMessageAsync(qUrl, message.ReceiptHandle);
-    }
-    // snippet-end:[SQS.dotnetv3.SQSReceiveMessages.DeleteMessage]
-
-  }
 }
 // snippet-end:[SQS.dotnetv3.SQSReceiveMessages.complete]
--- dotnetv3/SQS/from-developer-guide/SQSSendMessages.cs
+++ dotnetv3/SQS/from-developer-guide/SQSSendMessages.cs
@@ -21,117 +21,117 @@ using Amazon.SQS.Model;
 
 namespace SQSSendMessages
 {
-  class Program
-  {
-    // Some example messages to send to the queue.
-    private const string JsonMessage = "{\"product\":[{\"name\":\"Product A\",\"price\": \"32\"},{\"name\": \"Product B\",\"price\": \"27\"}]}";
-    private const string XmlMessage = "<products><product name=\"Product A\" price=\"32\" /><product name=\"Product B\" price=\"27\" /></products>";
-    private const string CustomMessage = "||product|Product A|32||product|Product B|27||";
-    private const string TextMessage = "Just a plain text message.";
-
-    static async Task Main(string[] args)
+    class Program
     {
-      // Do some checks for the command-line.
-      if(args.Length == 0)
-      {
-        Console.WriteLine("\nUsage: SQSSendMessages queue_url");
-        Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
-        return;
-      }
-      if(!args[0].StartsWith("https://sqs."))
-      {
-        Console.WriteLine("\nThe command-line argument isn't a queue URL:");
-        Console.WriteLine($"{args[0]}");
-        return;
-      }
+        // Some example messages to send to the queue.
+        private const string JsonMessage = "{\"product\":[{\"name\":\"Product A\",\"price\": \"32\"},{\"name\": \"Product B\",\"price\": \"27\"}]}";
+        private const string XmlMessage = "<products><product name=\"Product A\" price=\"32\" /><product name=\"Product B\" price=\"27\" /></products>";
+        private const string CustomMessage = "||product|Product A|32||product|Product B|27||";
+        private const string TextMessage = "Just a plain text message.";
 
-      // Create the Amazon SQS client.
-      var sqsClient = new AmazonSQSClient();
+        static async Task Main(string[] args)
+        {
+            // Do some checks for the command-line.
+            if (args.Length == 0)
+            {
+                Console.WriteLine("\nUsage: SQSSendMessages queue_url");
+                Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
+                return;
+            }
+            if (!args[0].StartsWith("https://sqs."))
+            {
+                Console.WriteLine("\nThe command-line argument isn't a queue URL:");
+                Console.WriteLine($"{args[0]}");
+                return;
+            }
 
-      // (Could verify that the queue exists.)
-      // Send some example messages to the given queue.
-      // A single message:
-      await SendMessage(sqsClient, args[0], JsonMessage);
+            // Create the Amazon SQS client.
+            var sqsClient = new AmazonSQSClient();
 
-      // A batch of messages:
-      var batchMessages = new List<SendMessageBatchRequestEntry>{
-        new SendMessageBatchRequestEntry("xmlMsg", XmlMessage),
-        new SendMessageBatchRequestEntry("customeMsg", CustomMessage),
-        new SendMessageBatchRequestEntry("textMsg", TextMessage)};
-      await SendMessageBatch(sqsClient, args[0], batchMessages);
+            // (Could verify that the queue exists.)
+            // Send some example messages to the given queue.
+            // A single message:
+            await SendMessage(sqsClient, args[0], JsonMessage);
 
-      // Let the user send their own messages or quit:
-      await InteractWithUser(sqsClient, args[0]);
+            // A batch of messages:
+            var batchMessages = new List<SendMessageBatchRequestEntry>{
+                new SendMessageBatchRequestEntry("xmlMsg", XmlMessage),
+                new SendMessageBatchRequestEntry("customeMsg", CustomMessage),
+                new SendMessageBatchRequestEntry("textMsg", TextMessage)};
+            await SendMessageBatch(sqsClient, args[0], batchMessages);
 
-      // Delete all messages that are still in the queue.
-      await DeleteAllMessages(sqsClient, args[0]);
+            // Let the user send their own messages or quit:
+            await InteractWithUser(sqsClient, args[0]);
+
+            // Delete all messages that are still in the queue.
+            await DeleteAllMessages(sqsClient, args[0]);
+        }
+
+
+        // snippet-start:[SQS.dotnetv3.SQSSendMessages.SendMessage]
+        //
+        // Method to put a message on a queue.
+        // Could be expanded to include message attributes, etc., in a
+        // SendMessageRequest object.
+        private static async Task SendMessage(
+          IAmazonSQS sqsClient, string qUrl, string messageBody)
+        {
+            SendMessageResponse responseSendMsg =
+              await sqsClient.SendMessageAsync(qUrl, messageBody);
+            Console.WriteLine($"Message added to queue\n  {qUrl}");
+            Console.WriteLine($"HttpStatusCode: {responseSendMsg.HttpStatusCode}");
+        }
+        // snippet-end:[SQS.dotnetv3.SQSSendMessages.SendMessage]
+
+
+        // snippet-start:[SQS.dotnetv3.SQSSendMessages.SendMessageBatch]
+        //
+        // Method to put a batch of messages on a queue.
+        // Could be expanded to include message attributes, etc., in the
+        // SendMessageBatchRequestEntry objects.
+        private static async Task SendMessageBatch(
+          IAmazonSQS sqsClient, string qUrl, List<SendMessageBatchRequestEntry> messages)
+        {
+            Console.WriteLine($"\nSending a batch of messages to queue\n  {qUrl}");
+            SendMessageBatchResponse responseSendBatch =
+              await sqsClient.SendMessageBatchAsync(qUrl, messages);
+            // Could test responseSendBatch.Failed here
+            foreach (SendMessageBatchResultEntry entry in responseSendBatch.Successful)
+                Console.WriteLine($"Message {entry.Id} successfully queued.");
+        }
+        // snippet-end:[SQS.dotnetv3.SQSSendMessages.SendMessageBatch]
+
+
+        //
+        // Method to get input from the user.
+        // They can provide messages to put in the queue or exit the application.
+        private static async Task InteractWithUser(IAmazonSQS sqsClient, string qUrl)
+        {
+            string response;
+            while (true)
+            {
+                // Get the user's input.
+                Console.WriteLine("\nType a message for the queue or \"exit\" to quit:");
+                response = Console.ReadLine();
+                if (response.ToLower() == "exit") break;
+
+                // Put the user's message in the queue.
+                await SendMessage(sqsClient, qUrl, response);
+            }
+        }
+
+
+        // snippet-start:[SQS.dotnetv3.SQSSendMessages.DeleteAllMessages]
+        //
+        // Method to delete all messages from the queue.
+        private static async Task DeleteAllMessages(IAmazonSQS sqsClient, string qUrl)
+        {
+            Console.WriteLine($"\nPurging messages from queue\n  {qUrl}...");
+            PurgeQueueResponse responsePurge = await sqsClient.PurgeQueueAsync(qUrl);
+            Console.WriteLine($"HttpStatusCode: {responsePurge.HttpStatusCode}");
+        }
+        // snippet-end:[SQS.dotnetv3.SQSSendMessages.DeleteAllMessages]
+
     }
-
-
-    // snippet-start:[SQS.dotnetv3.SQSSendMessages.SendMessage]
-    //
-    // Method to put a message on a queue.
-    // Could be expanded to include message attributes, etc., in a
-	// SendMessageRequest object.
-    private static async Task SendMessage(
-      IAmazonSQS sqsClient, string qUrl, string messageBody)
-    {
-      SendMessageResponse responseSendMsg =
-        await sqsClient.SendMessageAsync(qUrl, messageBody);
-      Console.WriteLine($"Message added to queue\n  {qUrl}");
-      Console.WriteLine($"HttpStatusCode: {responseSendMsg.HttpStatusCode}");
-    }
-    // snippet-end:[SQS.dotnetv3.SQSSendMessages.SendMessage]
-
-
-    // snippet-start:[SQS.dotnetv3.SQSSendMessages.SendMessageBatch]
-    //
-    // Method to put a batch of messages on a queue.
-    // Could be expanded to include message attributes, etc., in the
-    // SendMessageBatchRequestEntry objects.
-    private static async Task SendMessageBatch(
-      IAmazonSQS sqsClient, string qUrl, List<SendMessageBatchRequestEntry> messages)
-    {
-      Console.WriteLine($"\nSending a batch of messages to queue\n  {qUrl}");
-      SendMessageBatchResponse responseSendBatch =
-        await sqsClient.SendMessageBatchAsync(qUrl, messages);
-      // Could test responseSendBatch.Failed here
-      foreach(SendMessageBatchResultEntry entry in responseSendBatch.Successful)
-        Console.WriteLine($"Message {entry.Id} successfully queued.");
-    }
-    // snippet-end:[SQS.dotnetv3.SQSSendMessages.SendMessageBatch]
-
-
-    //
-    // Method to get input from the user.
-    // They can provide messages to put in the queue or exit the application.
-    private static async Task InteractWithUser(IAmazonSQS sqsClient, string qUrl)
-    {
-      string response;
-      while (true)
-      {
-        // Get the user's input.
-        Console.WriteLine("\nType a message for the queue or \"exit\" to quit:");
-        response = Console.ReadLine();
-        if(response.ToLower() == "exit") break;
-
-        // Put the user's message in the queue.
-        await SendMessage(sqsClient, qUrl, response);
-      }
-    }
-
-
-    // snippet-start:[SQS.dotnetv3.SQSSendMessages.DeleteAllMessages]
-    //
-    // Method to delete all messages from the queue.
-    private static async Task DeleteAllMessages(IAmazonSQS sqsClient, string qUrl)
-    {
-      Console.WriteLine($"\nPurging messages from queue\n  {qUrl}...");
-      PurgeQueueResponse responsePurge = await sqsClient.PurgeQueueAsync(qUrl);
-      Console.WriteLine($"HttpStatusCode: {responsePurge.HttpStatusCode}");
-    }
-    // snippet-end:[SQS.dotnetv3.SQSSendMessages.DeleteAllMessages]
-
-  }
 }
 // snippet-end:[SQS.dotnetv3.SQSSendMessages.complete]
