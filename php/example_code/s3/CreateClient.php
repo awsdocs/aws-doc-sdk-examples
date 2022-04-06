38b6c2fbef4084fd6eb5b748bc317eb5377709ca
Jason Quesenberry <beqqrry@amazon.com>
2022-04-06 09:55:52 -0700

    Removes extra snippet tags, reduces copyright footprint, and fixes a typo.

--- php/example_code/s3/CreateClient.php
+++ php/example_code/s3/CreateClient.php
@@ -1,22 +1,7 @@
 <?php
 
-/**
- * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * This file is licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License. A copy of
- * the License is located at
- *
- * http://aws.amazon.com/apache2.0/
- *
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- *  ABOUT THIS PHP SAMPLE: This sample is part of the SDK for PHP Developer Guide topic at
- * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/getting-started/basic-usage.html
- *
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 // snippet-start:[s3.php.create_client.complete]
 // snippet-start:[s3.php.create_client.import]
@@ -34,7 +19,7 @@ use Aws\Exception\AwsException;
  */
 // snippet-start:[s3.php.create_client.main]
 // snippet-start:[s3.php.create_client.client]
-//Create a S3Client
+//Create an S3Client
 $s3 = new Aws\S3\S3Client([
     'version' => 'latest',
     'region' => 'us-east-2'
@@ -55,14 +40,3 @@ $client = $sdk->createS3();
 // snippet-end:[s3.php.create_client.sdk]
 // snippet-end:[s3.php.create_client.main]
 // snippet-end:[s3.php.create_client.complete]
-// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
-// snippet-sourcedescription:[CreateClient.php demonstrates how to create an Amazon S3 client object for the PHP SDK.]
-// snippet-keyword:[PHP]
-// snippet-sourcesyntax:[php]
-// snippet-keyword:[AWS SDK for PHP v3]
-// snippet-keyword:[Code Sample]
-// snippet-keyword:[Amazon S3]
-// snippet-service:[s3]
-// snippet-sourcetype:[full-example]
-// snippet-sourcedate:[2018-09-20]
-// snippet-sourceauthor:[jschwarzwalder (AWS)]
