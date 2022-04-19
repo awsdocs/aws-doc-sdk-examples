7afbcee22542d04a932031da8db80fe3a05ed129
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-19 07:43:00 -0400

    Update LambdaTest.kt

--- kotlin/services/lambda/src/test/kotlin/LambdaTest.kt
+++ kotlin/services/lambda/src/test/kotlin/LambdaTest.kt
@@ -1,132 +1,226 @@
+// snippet-sourcedescription:[LambdaScenario.kt demonstrates how to perform various operations by using the LambdaClient object.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-keyword:[AWS Lambda]
+// snippet-keyword:[Code Sample]
+// snippet-sourcetype:[full-example]
+// snippet-sourcedate:[04/14/2022]
+// snippet-sourceauthor:[AWS-scmacdon]
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
 */
-import com.kotlin.lambda.UpdateFunctionConfiguration
-import com.kotlin.lambda.createNewFunction
-import com.kotlin.lambda.createScFunction
-import com.kotlin.lambda.delFunction
-import com.kotlin.lambda.delLambdaFunction
-import com.kotlin.lambda.getFunction
-import com.kotlin.lambda.getSettings
-import com.kotlin.lambda.invokeFunctionSc
-import com.kotlin.lambda.listFunctions
-import com.kotlin.lambda.listFunctionsSc
-import com.kotlin.lambda.updateFunctionCode
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestMethodOrder
-import java.io.InputStream
-import java.util.Properties
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(OrderAnnotation::class)
-class LambdaTest {
+package com.kotlin.lambda
 
-    var functionName: String = ""
-    var functionARN: String = "" // Gets set in a test.
-    var s3BucketName: String = ""
-    var updatedBucketName: String = ""
-    var functionNameSc: String = ""
-    var s3Key: String = ""
-    var role: String = ""
-    var handler: String = ""
+// snippet-start:[lambda.kotlin.scenario.import]
+import aws.sdk.kotlin.services.lambda.LambdaClient
+import aws.sdk.kotlin.services.lambda.model.CreateFunctionRequest
+import aws.sdk.kotlin.services.lambda.model.DeleteFunctionRequest
+import aws.sdk.kotlin.services.lambda.model.FunctionCode
+import aws.sdk.kotlin.services.lambda.model.GetFunctionRequest
+import aws.sdk.kotlin.services.lambda.model.InvokeRequest
+import aws.sdk.kotlin.services.lambda.model.ListFunctionsRequest
+import aws.sdk.kotlin.services.lambda.model.LogType
+import aws.sdk.kotlin.services.lambda.model.Runtime
+import aws.sdk.kotlin.services.lambda.model.UpdateFunctionCodeRequest
+import aws.sdk.kotlin.services.lambda.model.UpdateFunctionConfigurationRequest
+import aws.sdk.kotlin.services.lambda.waiters.waitUntilFunctionActive
+import aws.sdk.kotlin.services.lambda.waiters.waitUntilFunctionUpdated
+import kotlin.system.exitProcess
+// snippet-end:[lambda.kotlin.scenario.import]
 
-    @BeforeAll
-    fun setup() {
+/**
+ Before running this code example, set up your development environment, including your credentials.
 
-        val input: InputStream = this.javaClass.getClassLoader().getResourceAsStream("config.properties")
-        val prop = Properties()
+ For more information, see this documentation topic:
 
-        prop.load(input)
-        functionName = prop.getProperty("functionName")
-        functionNameSc = prop.getProperty("functionNameSc")
-        s3BucketName = prop.getProperty("s3BucketName")
-        updatedBucketName = prop.getProperty("updatedBucketName")
-        s3Key = prop.getProperty("s3Key")
-        role = prop.getProperty("role")
-        handler = prop.getProperty("handler")
+ https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+
+ This example performs the following tasks:
+
+ 1. Creates a Lambda function.
+ 2. Gets a specific Lambda function.
+ 3. Lists all Lambda functions.
+ 4. Invokes a Lambda function.
+ 5. Updates a Lambda function's code.
+ 6. Updates a Lambda function's configuration value.
+ 7. Deletes a Lambda function.
+ */
+
+// snippet-start:[lambda.kotlin.scenario.main]
+suspend fun main(args: Array<String>) {
+
+    val usage = """
+        Usage:
+            <functionName> <role> <handler> <bucketName> <updatedBucketName> <key> 
+
+        Where:
+            functionName - The name of the AWS Lambda function. 
+            role - The AWS Identity and Access Management (IAM) service role that has AWS Lambda permissions. 
+            handler - The fully qualified method name (for example, example.Handler::handleRequest). 
+            bucketName - The Amazon Simple Storage Service (Amazon S3) bucket name that contains the ZIP or JAR used for the Lambda function's code.
+            updatedBucketName - The Amazon S3 bucket name that contains the .zip or .jar used to update the Lambda function's code. 
+            key - The Amazon S3 key name that represents the .zip or .jar file (for example, LambdaHello-1.0-SNAPSHOT.jar).
+            """
+
+    if (args.size != 6) {
+        println(usage)
+        exitProcess(1)
     }
 
-    @Test
-    @Order(1)
-    fun whenInitializingAWSService_thenNotNull() {
-        Assertions.assertTrue(!functionName.isEmpty())
-        Assertions.assertTrue(!s3BucketName.isEmpty())
-        Assertions.assertTrue(!s3Key.isEmpty())
-        Assertions.assertTrue(!role.isEmpty())
-        Assertions.assertTrue(!handler.isEmpty())
-        println("Test 1 passed")
+    val functionName = args[0]
+    val role = args[1]
+    val handler = args[2]
+    val bucketName = args[3]
+    val updatedBucketName = args[4]
+    val key = args[5]
+
+    println("Creating a Lambda function named $functionName.")
+    val funArn = createScFunction(functionName, bucketName, key, handler, role)
+    println("The AWS Lambda ARN is $funArn")
+
+    // Get a specific Lambda function.
+    println("Getting the $functionName AWS Lambda function.")
+    getFunction(functionName)
+
+    // List the Lambda functions.
+    println("Listing all AWS Lambda functions.")
+    listFunctionsSc()
+
+    // Invoke the Lambda function.
+    println("*** Invoke the Lambda function.")
+    invokeFunctionSc(functionName)
+
+    // Update the AWS Lambda function code.
+    println("*** Update the Lambda function code.")
+    updateFunctionCode(functionName, updatedBucketName, key)
+
+    // println("*** Invoke the function again after updating the code.")
+    invokeFunctionSc(functionName)
+
+    // Update the AWS Lambda function configuration.
+    println("Update the run time of the function.")
+    UpdateFunctionConfiguration(functionName, handler)
+
+    // Delete the AWS Lambda function.
+    println("Delete the AWS Lambda function.")
+    delFunction(functionName)
+}
+
+suspend fun createScFunction(
+    myFunctionName: String,
+    s3BucketName: String,
+    myS3Key: String,
+    myHandler: String,
+    myRole: String
+): String {
+
+    val functionCode = FunctionCode {
+        s3Bucket = s3BucketName
+        s3Key = myS3Key
     }
 
-    @Test
-    @Order(2)
-    fun createFunctionTest() = runBlocking {
-
-        functionARN = createNewFunction(functionName, s3BucketName, s3Key, handler, role).toString()
-        Assertions.assertTrue(!functionARN.isEmpty())
-        println("Test 2 passed")
+    val request = CreateFunctionRequest {
+        functionName = myFunctionName
+        code = functionCode
+        description = "Created by the Lambda Kotlin API"
+        handler = myHandler
+        role = myRole
+        runtime = Runtime.Java8
     }
 
-    @Test
-    @Order(3)
-    fun listLambdaTest() = runBlocking {
-        listFunctions()
-        println("Test 3 passed")
-    }
-
-    @Test
-    @Order(4)
-    fun getAccountSettings() = runBlocking {
-        getSettings()
-        println("Test 4 passed")
-    }
-
-    @Test
-    @Order(5)
-    fun deleteFunctionTest() = runBlocking {
-        delLambdaFunction(functionName)
-        println("Test 5 passed")
-    }
-
-    @Test
-    @Order(5)
-    fun testLambdaScenario() = runBlocking {
-        println("Creating a Lambda function named $functionNameSc.")
-        val funArn = createScFunction(functionNameSc, s3BucketName, s3Key, handler, role)
-        println("The AWS Lambda ARN is $funArn")
-
-        // Get a specific Lambda function.
-        println("Getting the $functionNameSc AWS Lambda function.")
-        getFunction(functionNameSc)
-
-        // List the Lambda functions.
-        println("Listing all AWS Lambda functions.")
-        listFunctionsSc()
-
-        // Invoke the Lambda function.
-        println("*** Invoke the Lambda function.")
-        invokeFunctionSc(functionNameSc)
-
-        // Update the AWS Lambda function code.
-        println("*** Update the Lambda function code.")
-        updateFunctionCode(functionNameSc, updatedBucketName, s3Key)
-
-       // println("*** Invoke the function again after updating the code.")
-       invokeFunctionSc(functionNameSc)
-
-        // Update the AWS Lambda function configuration.
-        println("Update the run time of the function.")
-        UpdateFunctionConfiguration(functionNameSc, handler)
-
-        // Delete the AWS Lambda function.
-        println("Delete the AWS Lambda function.")
-        delFunction(functionNameSc)
+    // Create a Lambda function using a waiter
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        val functionResponse = awsLambda.createFunction(request)
+        awsLambda.waitUntilFunctionActive {
+            functionName = myFunctionName
+        }
+        return functionResponse.functionArn.toString()
     }
 }
+
+suspend fun getFunction(functionNameVal: String) {
+
+    val functionRequest = GetFunctionRequest {
+        functionName = functionNameVal
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        val response = awsLambda.getFunction(functionRequest)
+        println("The runtime of this Lambda function is ${response.configuration?.runtime}")
+    }
+}
+
+suspend fun listFunctionsSc() {
+
+    val request = ListFunctionsRequest {
+        maxItems = 10
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        val response = awsLambda.listFunctions(request)
+        response.functions?.forEach { function ->
+            println("The function name is ${function.functionName}")
+        }
+    }
+}
+
+suspend fun invokeFunctionSc(functionNameVal: String) {
+
+    val json = """{"inputValue":"1000"}"""
+    val byteArray = json.trimIndent().encodeToByteArray()
+    val request = InvokeRequest {
+        functionName = functionNameVal
+        payload = byteArray
+        logType = LogType.Tail
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        val res = awsLambda.invoke(request)
+        println("The function payload is ${res.payload?.toString(Charsets.UTF_8)}")
+    }
+}
+
+suspend fun updateFunctionCode(functionNameVal: String?, bucketName: String?, key: String?) {
+
+    val functionCodeRequest = UpdateFunctionCodeRequest {
+        functionName = functionNameVal
+        publish = true
+        s3Bucket = bucketName
+        s3Key = key
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        val response = awsLambda.updateFunctionCode(functionCodeRequest)
+        awsLambda.waitUntilFunctionUpdated {
+            functionName = functionNameVal
+        }
+        println("The last modified value is " + response.lastModified)
+    }
+}
+
+suspend fun UpdateFunctionConfiguration(functionNameVal: String?, handlerVal: String?) {
+
+    val configurationRequest = UpdateFunctionConfigurationRequest {
+        functionName = functionNameVal
+        handler = handlerVal
+        runtime = Runtime.Java11
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        awsLambda.updateFunctionConfiguration(configurationRequest)
+    }
+}
+
+suspend fun delFunction(myFunctionName: String) {
+
+    val request = DeleteFunctionRequest {
+        functionName = myFunctionName
+    }
+
+    LambdaClient { region = "us-west-2" }.use { awsLambda ->
+        awsLambda.deleteFunction(request)
+        println("$myFunctionName was deleted")
+    }
+}
+// snippet-end:[lambda.kotlin.scenario.main]
