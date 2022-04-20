11766684b8a211c4fb9e67a276bb09ef9b2df9dd
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:56:04 -0400

    Update build.gradle.kts

--- kotlin/services/apigateway/build.gradle.kts
+++ kotlin/services/apigateway/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
     application
 }
+
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
@@ -24,8 +25,8 @@ dependencies {
     implementation("aws.sdk.kotlin:apigateway:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
