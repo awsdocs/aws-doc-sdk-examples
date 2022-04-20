48d265c424b528214e6c9239f460b4c105e1eb9e
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 08:22:32 -0400

    Update build.gradle.kts

--- kotlin/services/rds/build.gradle.kts
+++ kotlin/services/rds/build.gradle.kts
@@ -20,8 +20,8 @@ repositories {
 }
 
 dependencies {
-    implementation("aws.sdk.kotlin:rds:0.9.4-beta")
-    implementation("aws.sdk.kotlin:rdsdata:0.9.4-beta")
+    implementation("aws.sdk.kotlin:rds:0.14.3-beta")
+    implementation("aws.sdk.kotlin:rdsdata:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
