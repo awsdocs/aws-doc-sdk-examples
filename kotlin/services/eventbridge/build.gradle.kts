5f1dbfff8f94a6419666172bbbc7874227f9b3a8
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 08:15:21 -0400

    Update build.gradle.kts

--- kotlin/services/eventbridge/build.gradle.kts
+++ kotlin/services/eventbridge/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation("aws.sdk.kotlin:eventbridge:0.9.4-beta")
+    implementation("aws.sdk.kotlin:eventbridge:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
