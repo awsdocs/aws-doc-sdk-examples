66fc05ca436cfdde6ef112eb5cc7d354aedcb218
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:26:01 -0400

    Update build.gradle.kts

--- kotlin/services/cloudwatch/build.gradle.kts
+++ kotlin/services/cloudwatch/build.gradle.kts
@@ -15,7 +15,6 @@ buildscript {
     }
 }
 
-
 repositories {
     mavenCentral()
     jcenter()
@@ -27,8 +26,8 @@ dependencies {
     implementation("aws.sdk.kotlin:cloudwatchlogs:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
