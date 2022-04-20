f4fef366d2f84395658dd4c721cb9b6cf20ce50c
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 08:21:03 -0400

    Update build.gradle.kts

--- kotlin/services/personalize/build.gradle.kts
+++ kotlin/services/personalize/build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 }
 
 dependencies {
-    implementation("aws.sdk.kotlin:personalize:0.9.4-beta")
-    implementation("aws.sdk.kotlin:personalizeruntime:0.9.4-beta")
+    implementation("aws.sdk.kotlin:personalize:0.14.3-beta")
+    implementation("aws.sdk.kotlin:personalizeruntime:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 
