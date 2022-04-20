88279c4c0052c971eff4fec31ff79db7a068614b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 08:24:57 -0400

    Update build.gradle.kts

--- kotlin/services/secretsmanager/build.gradle.kts
+++ kotlin/services/secretsmanager/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("aws.sdk.kotlin:secretsmanager:0.9.4-beta")
+    implementation("aws.sdk.kotlin:secretsmanager:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
