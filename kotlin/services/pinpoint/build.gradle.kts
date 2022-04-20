cbc867dd58049fe138171c14336f2fbd9c39dcd6
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 08:21:36 -0400

    Update build.gradle.kts

--- kotlin/services/pinpoint/build.gradle.kts
+++ kotlin/services/pinpoint/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("aws.sdk.kotlin:pinpoint:0.9.4-beta")
+    implementation("aws.sdk.kotlin:pinpoint:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("com.google.code.gson:gson:2.8.6")
