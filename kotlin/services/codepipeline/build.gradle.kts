dd3ce13dc41840b7245312cdaeab5e2082352501
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:26:23 -0400

    Update build.gradle.kts

--- kotlin/services/codepipeline/build.gradle.kts
+++ kotlin/services/codepipeline/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation("aws.sdk.kotlin:codepipeline:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
