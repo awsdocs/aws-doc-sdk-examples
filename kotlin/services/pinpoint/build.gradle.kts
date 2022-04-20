c9804e82ca25731780e9307ec547f71807ed5416
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 12:22:27 -0400

    Update build.gradle.kts

--- kotlin/services/pinpoint/build.gradle.kts
+++ kotlin/services/pinpoint/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
     id("io.gitlab.arturbosch.detekt").version("1.16.0-RC1")
-     application
+    application
 }
 
 group = "me.scmacdon"
