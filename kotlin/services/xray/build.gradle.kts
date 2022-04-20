6e762dd94194cbf4c304149724e08e15d6f299aa
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:43:10 -0400

    Update build.gradle.kts

--- kotlin/services/xray/build.gradle.kts
+++ kotlin/services/xray/build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
     api("aws.sdk.kotlin:xray:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
