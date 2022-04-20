226e10f7fd84e7b85806bc362f877326b192f31a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:17:49 -0400

    Update build.gradle.kts

--- kotlin/services/forecast/build.gradle.kts
+++ kotlin/services/forecast/build.gradle.kts
@@ -10,15 +10,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
+    jcenter()
 }
 
 dependencies {
     implementation("aws.sdk.kotlin:forecast:0.14.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
