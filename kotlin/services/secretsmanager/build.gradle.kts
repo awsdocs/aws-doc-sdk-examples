ce8d503ddc3390d7f6359afafa6be4af76801a3a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-04-20 11:39:47 -0400

    Update build.gradle.kts

--- kotlin/services/secretsmanager/build.gradle.kts
+++ kotlin/services/secretsmanager/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
 }
+
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
@@ -14,6 +15,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
