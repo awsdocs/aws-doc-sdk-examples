7d53a687a8b1b8c2d38c4610f9badacb346421d8
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-05-05 15:24:21 -0400

    Update GreetingItems.java

--- javav2/usecases/creating_first_project/src/main/java/com/example/handlingformsubmission/GreetingItems.java
+++ javav2/usecases/creating_first_project/src/main/java/com/example/handlingformsubmission/GreetingItems.java
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 package com.example.handlingformsubmission;
 
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
