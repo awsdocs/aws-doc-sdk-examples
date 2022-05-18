f9d9c9f7cc25fbf776e1e7fa5a0c69130bc37ed9
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-05-18 09:30:54 -0400

    Add files via upload

--- javav2/example_code/emr/src/main/java/aws/example/emr/TerminateJobFlow.java
+++ javav2/example_code/emr/src/main/java/aws/example/emr/TerminateJobFlow.java
@@ -3,8 +3,7 @@
 //snippet-keyword:[Code Sample]
 //snippet-keyword:[Amazon EMR]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[07/19/2021]
-//snippet-sourceauthor:[scmacdon AWS]
+//snippet-sourcedate:[05/18/2022]
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
@@ -12,39 +11,40 @@
 package aws.example.emr;
 
 // snippet-start:[emr.java2.terminate_job.import]
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.emr.EmrClient;
-import software.amazon.awssdk.services.emr.model.*;
 import software.amazon.awssdk.services.emr.model.TerminateJobFlowsRequest;
+import software.amazon.awssdk.services.emr.model.EmrException;
 // snippet-end:[emr.java2.terminate_job.import]
 
-/*
- *   Ensure that you have setup your development environment, including your credentials.
- *   For information, see this documentation topic:
+/**
+ * Before running this Java V2 code example, set up your development environment, including your credentials.
  *
- *   https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ * For more information, see the following documentation topic:
  *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
  */
 public class TerminateJobFlow {
 
     public static void main(String[] args){
 
-        final String USAGE = "\n" +
+        final String usage = "\n" +
                 "Usage: " +
                 "   <id>\n\n" +
                 "Where:\n" +
-                "   id - an id of a job flow to shut down.\n\n" ;
+                "   id - An id of a job flow to shut down.\n\n" ;
 
         if (args.length != 1) {
-              System.out.println(USAGE);
+              System.out.println(usage);
               System.exit(1);
-          }
-
+        }
 
         String id = args[0] ;
         Region region = Region.US_WEST_2;
         EmrClient emrClient = EmrClient.builder()
                 .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
 
         terminateFlow(emrClient, id);
