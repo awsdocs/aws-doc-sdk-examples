602d5b7a5103e000ac14d6b27bff146d4b744cd1
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-05-19 10:17:26 -0400

    Add files via upload

--- javav2/example_code/sns/src/main/java/com/example/sns/SubscribeTextSMS.java
+++ javav2/example_code/sns/src/main/java/com/example/sns/SubscribeTextSMS.java
@@ -3,8 +3,7 @@
 //snippet-keyword:[Code Sample]
 //snippet-keyword:[Amazon Simple Notification Service]
 //snippet-sourcetype:[full-example]
-//snippet-sourcedate:[09-27-2021]
-//snippet-sourceauthor:[scmacdon- AWS]
+//snippet-sourcedate:[05/19/2022]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -14,6 +13,7 @@
 package com.example.sns;
 
 //snippet-start:[sns.java2.SubscribeTextSMS.import]
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.SnsException;
@@ -22,31 +22,33 @@ import software.amazon.awssdk.services.sns.model.SubscribeResponse;
 //snippet-end:[sns.java2.SubscribeTextSMS.import]
 
 /**
- * To run this Java V2 code example, ensure that you have setup your development environment, including your credentials.
+ * Before running this Java V2 code example, set up your development environment, including your credentials.
  *
- * For information, see this documentation topic:
+ * For more information, see the following documentation topic:
  *
  * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
  */
 public class SubscribeTextSMS {
 
     public static void main(String[] args) {
-        final String USAGE = "\n" +
+
+        final String usage = "\n" +
                 "Usage: " +
                 "   <topicArn> <phoneNumber>\n\n" +
                 "Where:\n" +
-                "   topicArn - the ARN of the topic to subscribe.\n\n" +
-                "   phoneNumber - a mobile phone number that receives notifications (for example, +1XXX5550100).\n\n";
+                "   topicArn - The ARN of the topic to subscribe.\n\n" +
+                "   phoneNumber - A mobile phone number that receives notifications (for example, +1XXX5550100).\n\n";
 
         if (args.length < 2) {
-            System.out.println(USAGE);
+            System.out.println(usage);
             System.exit(1);
         }
 
         String topicArn = args[0];
         String phoneNumber = args[1];
         SnsClient snsClient = SnsClient.builder()
-                .region(Region.US_WEST_2)
+                .region(Region.US_EAST_1)
+                .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
 
         subTextSNS(snsClient, topicArn, phoneNumber);
