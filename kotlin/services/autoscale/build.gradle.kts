fece808f0f6858225f468c00b56f84bce018d768
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-06-21 16:22:10 -0400

    Update build.gradle.kts

--- kotlin/services/autoscale/build.gradle.kts
+++ kotlin/services/autoscale/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
 }
-group = "me.scmacdon"
+group = "me.test"
 version = "1.0-SNAPSHOT"
 
 buildscript {
