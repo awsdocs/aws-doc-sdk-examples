97bdbf5501c163a89cba67720e525e998327af4e
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2022-07-07 09:27:41 -0400

    Creating Auto Scale MVP for .NET

---
+++ dotnetv3/AutoScale/scenarios/AutoScale_Basics/AutoScale_Basics/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// snippet-start:[autoscale.dotnetv3.create_scaling_scenario.import]
+global using Amazon;
+global using Amazon.AutoScaling;
+global using Amazon.AutoScaling.Model;
+// snippet-end:[autoscale.dotnetv3.create_scaling_scenario.import]
