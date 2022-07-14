5a17628e1a467b156fc09e901bcd2fdc066e5ffb
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2022-07-14 08:53:42 -0400

    Missing tag in code.

---
+++ dotnetv3/dynamodb/scenarios/PartiQL_Basics_Scenario/PartiQL_Basics_Scenario/PartiQLMethods.cs
@@ -0,0 +1,274 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+// snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-Complete]
+namespace PartiQL_Basics_Scenario
+{
+    public class PartiQLMethods
+    {
+        private static readonly AmazonDynamoDBClient Client = new AmazonDynamoDBClient();
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-InsertMovies]
+
+        /// <summary>
+        /// Inserts movies imported from a JSON file into the movie table by
+        /// using an Amazon DynamoDB PartiQL INSERT statement.
+        /// </summary>
+        /// <param name="tableName">The name of the table where the movie
+        /// information will be inserted.</param>
+        /// <param name="movieFileName">The name of the JSON file that contains
+        /// movie information.</param>
+        /// <returns>A Boolean value that indicates the success or failure of
+        /// the insert operation.</returns>
+        public static async Task<bool> InsertMovies(string tableName, string movieFileName)
+        {
+            // Get the list of movies from the JSON file.
+            var movies = ImportMovies(movieFileName);
+
+            var success = false;
+
+            if (movies is not null)
+            {
+                // Insert the movies in a batch using PartiQL. Because the
+                // batch can contain a maximum of 25 items, insert 25 movies
+                // at a time.
+                string insertBatch = $"INSERT INTO {tableName} VALUE {{'title': ?, 'year': ?}}";
+                var statements = new List<BatchStatementRequest>();
+
+                try
+                {
+                    for (var indexOffset = 0; indexOffset < 250; indexOffset += 25)
+                    {
+                        for (var i = indexOffset; i < indexOffset + 25; i++)
+                        {
+                            statements.Add(new BatchStatementRequest
+                            {
+                                Statement = insertBatch,
+                                Parameters = new List<AttributeValue>
+                                {
+                                    new AttributeValue { S = movies[i].Title },
+                                    new AttributeValue { N = movies[i].Year.ToString() },
+                                },
+                            });
+                        }
+
+                        var response = await Client.BatchExecuteStatementAsync(new BatchExecuteStatementRequest
+                        {
+                            Statements = statements,
+                        });
+
+                        // Wait between batches for movies to be successfully added.
+                        System.Threading.Thread.Sleep(3000);
+
+                        success = response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+
+                        // Clear the list of statements for the next batch.
+                        statements.Clear();
+                    }
+                }
+                catch (AmazonDynamoDBException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return success;
+        }
+
+        /// <summary>
+        /// Loads the contents of a JSON file into a list of movies to be
+        /// added to the DynamoDB table.
+        /// </summary>
+        /// <param name="movieFileName">The full path to the JSON file.</param>
+        /// <returns>A generic list of movie objects.</returns>
+        public static List<Movie> ImportMovies(string movieFileName)
+        {
+            if (!File.Exists(movieFileName))
+            {
+                return null;
+            }
+
+            using var sr = new StreamReader(movieFileName);
+            string json = sr.ReadToEnd();
+            var allMovies = JsonConvert.DeserializeObject<List<Movie>>(json);
+
+            if (allMovies is not null)
+            {
+                // Return the first 250 entries.
+                return allMovies.GetRange(0, 250);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-InsertMovies]
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-GetSingleMovie]
+
+        /// <summary>
+        /// Uses a PartiQL SELECT statement to retrieve a single movie from the
+        /// movie database.
+        /// </summary>
+        /// <param name="tableName">The name of the movie table.</param>
+        /// <param name="movieTitle">The title of the movie to retrieve.</param>
+        /// <returns>A list of movie data. If no movie matches the supplied
+        /// title, the list is empty.</returns>
+        public static async Task<List<Dictionary<string, AttributeValue>>> GetSingleMovie(string tableName, string movieTitle)
+        {
+            string selectSingle = $"SELECT * FROM {tableName} WHERE title = ?";
+            var parameters = new List<AttributeValue>
+            {
+                new AttributeValue { S = movieTitle },
+            };
+
+            var response = await Client.ExecuteStatementAsync(new ExecuteStatementRequest
+            {
+                Statement = selectSingle,
+                Parameters = parameters,
+            });
+
+            return response.Items;
+        }
+
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-GetSingleMovie]
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-GetMovies]
+
+        /// <summary>
+        /// Retrieve multiple movies by year using a SELECT statement.
+        /// </summary>
+        /// <param name="tableName">The name of the movie table.</param>
+        /// <param name="year">The year the movies were released.</param>
+        /// <returns></returns>
+        public static async Task<List<Dictionary<string, AttributeValue>>> GetMovies(string tableName, int year)
+        {
+            string selectSingle = $"SELECT * FROM {tableName} WHERE year = ?";
+            var parameters = new List<AttributeValue>
+            {
+                new AttributeValue { N = year.ToString() },
+            };
+
+            var response = await Client.ExecuteStatementAsync(new ExecuteStatementRequest
+            {
+                Statement = selectSingle,
+                Parameters = parameters,
+            });
+
+            return response.Items;
+        }
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-GetMovies]
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-InsertSingleMovie]
+
+        /// <summary>
+        /// Inserts a single movie into the movies table.
+        /// </summary>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="movieTitle">The title of the movie to insert.</param>
+        /// <param name="year">The year that the movie was released.</param>
+        /// <returns>A Boolean value that indicates the success or failure of
+        /// the INSERT operation.</returns>
+        public static async Task<bool> InsertSingleMovie(string tableName, string movieTitle, int year)
+        {
+            string insertBatch = $"INSERT INTO {tableName} VALUE {{'title': ?, 'year': ?}}";
+
+            var response = await Client.ExecuteStatementAsync(new ExecuteStatementRequest
+            {
+                Statement = insertBatch,
+                Parameters = new List<AttributeValue>
+                {
+                    new AttributeValue { S = movieTitle },
+                    new AttributeValue { N = year.ToString() },
+                },
+            });
+
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-InsertSingleMovie]
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-UpdateSingleMovie]
+
+        /// <summary>
+        /// Updates a single movie in the table, adding information for the
+        /// producer.
+        /// </summary>
+        /// <param name="tableName">the name of the table.</param>
+        /// <param name="producer">The name of the producer.</param>
+        /// <param name="movieTitle">The movie title.</param>
+        /// <param name="year">The year the movie was released.</param>
+        /// <returns>A Boolean value that indicates the success of the
+        /// UPDATE operation.</returns>
+        public static async Task<bool> UpdateSingleMovie(string tableName, string producer, string movieTitle, int year)
+        {
+            string insertSingle = $"UPDATE {tableName} SET Producer=? WHERE title = ? AND year = ?";
+
+            var response = await Client.ExecuteStatementAsync(new ExecuteStatementRequest
+            {
+                Statement = insertSingle,
+                Parameters = new List<AttributeValue>
+                {
+                    new AttributeValue { S = producer },
+                    new AttributeValue { S = movieTitle },
+                    new AttributeValue { N = year.ToString() },
+                },
+            });
+
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-UpdateSingleMovie]
+
+        // snippet-start:[PartiQL.dotnetv3.PartiQLBasicsScenario-DeleteSingleMovie]
+
+        /// <summary>
+        /// Deletes a single movie from the table.
+        /// </summary>
+        /// <param name="tableName">The name of the table.</param>
+        /// <param name="movieTitle">The title of the movie to delete.</param>
+        /// <param name="year">The year that the movie was released.</param>
+        /// <returns>A Boolean value that indicates the success of the
+        /// DELETE operation.</returns>
+        public static async Task<bool> DeleteSingleMovie(string tableName, string movieTitle, int year)
+        {
+            var deleteSingle = $"DELETE FROM {tableName} WHERE title = ? AND year = ?";
+
+            var response = await Client.ExecuteStatementAsync(new ExecuteStatementRequest
+            {
+                Statement = deleteSingle,
+                Parameters = new List<AttributeValue>
+                {
+                    new AttributeValue { S = movieTitle },
+                    new AttributeValue { N = year.ToString() },
+                },
+            });
+
+            return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        // snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-DeleteSingleMovie]
+
+        /// <summary>
+        /// Displays the list of movies returned from a database query.
+        /// </summary>
+        /// <param name="items">The list of movie information to display.</param>
+        private static void DisplayMovies(List<Dictionary<string, AttributeValue>> items)
+        {
+            if (items.Count > 0)
+            {
+                Console.WriteLine($"Found {items.Count} movies.");
+                items.ForEach(item => Console.WriteLine($"{item["year"].N}\t{item["title"].S}"));
+            }
+            else
+            {
+                Console.WriteLine($"Didn't find a movie that matched the supplied criteria.");
+            }
+        }
+
+
+    }
+}
+
+// snippet-end:[PartiQL.dotnetv3.PartiQLBasicsScenario-Complete]
