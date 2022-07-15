80989c594420f11c3c9eaa5559c10bae1f2cb20b
ford prior <fprior@amazon.com>
2022-07-15 11:26:15 -0400

    fixing linting complaints

--- lambda_functions/blank-ruby/function/lambda_function.test.rb
+++ lambda_functions/blank-ruby/function/lambda_function.test.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
-require_relative 'lambda_function'
-require 'test/unit'
-require 'json'
-require 'logger'
-require 'aws-xray-sdk/lambda'
+require_relative "lambda_function"
+require "test/unit"
+require "json"
+require "logger"
+require "aws-xray-sdk/lambda"
 
-XRay.recorder.configure({ context_missing: 'LOG_ERROR' })
+XRay.recorder.configure({ context_missing: "LOG_ERROR" })
 
 class TestFunction < Test::Unit::TestCase
   logger = Logger.new($stdout)
 
   def test_invoke
-    file = File.read('event.json')
+    file = File.read("event.json")
     event = JSON.parse(file)
     context = {}
     result = lambda_handler(event: event, context: context)
-    assert_match('function_count', result.to_s, 'Should match')
+    assert_match("function_count", result.to_s, "Should match")
   end
 end
