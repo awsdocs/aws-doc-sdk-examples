ac65dee43d4d89bd17619e056ce2aa0149e8f91f
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-23 22:38:47 -0400

    Add files via upload

--- javav2/example_code/codepipeline/src/main/java/com/example/pipeline/DeletePipeline.java
+++ javav2/example_code/codepipeline/src/main/java/com/example/pipeline/DeletePipeline.java
@@ -1,10 +1,6 @@
 //snippet-sourcedescription:[DeletePipeline.java demonstrates how to delete a pipeline.]
 //snippet-keyword:[SDK for Java 2.0]
-//snippet-keyword:[Code Sample]
 //snippet-service:[AWS CodePipeline]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[05/17/2022]
-
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
@@ -29,37 +25,36 @@ import software.amazon.awssdk.services.codepipeline.model.DeletePipelineRequest;
  */
 public class DeletePipeline {
 
-
     public static void main(String[] args) {
-    final String usage = "\n" +
+        final String usage = "\n" +
             "Usage: " +
             "   <name>\n\n" +
             "Where:\n" +
             "   name - The name of the pipeline to delete \n\n" ;
 
-    if (args.length != 1) {
-         System.out.println(usage);
-         System.exit(1);
-    }
+        if (args.length != 1) {
+            System.out.println(usage);
+            System.exit(1);
+        }
 
-    String name = args[0];
-    Region region = Region.US_EAST_1;
-    CodePipelineClient pipelineClient = CodePipelineClient.builder()
+        String name = args[0];
+        Region region = Region.US_EAST_1;
+        CodePipelineClient pipelineClient = CodePipelineClient.builder()
             .region(region)
             .credentialsProvider(ProfileCredentialsProvider.create())
             .build();
 
-   deleteSpecificPipeline(pipelineClient, name);
-   pipelineClient.close();
-}
+        deleteSpecificPipeline(pipelineClient, name);
+        pipelineClient.close();
+    }
 
- // snippet-start:[pipeline.java2.delete_pipeline.main]
- public static void deleteSpecificPipeline(CodePipelineClient pipelineClient, String name) {
+    // snippet-start:[pipeline.java2.delete_pipeline.main]
+    public static void deleteSpecificPipeline(CodePipelineClient pipelineClient, String name) {
 
         try {
             DeletePipelineRequest deletePipelineRequest = DeletePipelineRequest.builder()
-                    .name(name)
-                    .build() ;
+                .name(name)
+                .build() ;
 
             pipelineClient.deletePipeline(deletePipelineRequest);
             System.out.println(name +" was successfully deleted");
