f0f1bacaa16350e9efeceee5ef55ee505c8bed6e
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-24 12:25:25 -0400

    Add files via upload

--- javav2/example_code/s3/src/main/java/com/example/s3/DeleteObjects.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/DeleteObjects.java
@@ -1,10 +1,6 @@
 //snippet-sourcedescription:[DeleteObjects.java demonstrates how to delete multiple objects from an Amazon Simple Storage Service (Amazon S3) bucket.]
 //snippet-keyword:[AWS SDK for Java v2]
-//snippet-keyword:[Code Sample]
 //snippet-service:[Amazon S3]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[05/16/2022]
-
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
@@ -35,11 +31,11 @@ public class DeleteObjects {
     public static void main(String[] args) {
 
         final String usage = "\n" +
-                "Usage: " +
-                "   <bucketName> <objectName>\n\n" +
-                "Where:\n" +
-                "   bucketName - The Amazon S3 bucket to delete the website configuration from.\n"+
-                "   objectName - The object name.\n" ;
+            "Usage: " +
+            "   <bucketName> <objectName>\n\n" +
+            "Where:\n" +
+            "   bucketName - The Amazon S3 bucket to delete the website configuration from.\n"+
+            "   objectName - The object name.\n" ;
 
         if (args.length != 2) {
             System.out.println(usage);
@@ -49,13 +45,13 @@ public class DeleteObjects {
         String bucketName = args[0];
         String objectName = args[1];
         System.out.println("Deleting "+objectName +" from the Amazon S3 bucket: " + bucketName);
-
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
         Region region = Region.US_EAST_1;
         S3Client s3 = S3Client.builder()
-                .region(region)
-                .credentialsProvider(credentialsProvider)
-                .build();
+            .region(region)
+            .credentialsProvider(credentialsProvider)
+            .build();
+
         deleteBucketObjects(s3, bucketName, objectName);
         s3.close();
     }
@@ -64,19 +60,24 @@ public class DeleteObjects {
     public static void deleteBucketObjects(S3Client s3, String bucketName, String objectName) {
 
         ArrayList<ObjectIdentifier> toDelete = new ArrayList<>();
-        toDelete.add(ObjectIdentifier.builder().key(objectName).build());
+        toDelete.add(ObjectIdentifier.builder()
+            .key(objectName)
+            .build());
 
         try {
             DeleteObjectsRequest dor = DeleteObjectsRequest.builder()
-                    .bucket(bucketName)
-                    .delete(Delete.builder().objects(toDelete).build())
-                    .build();
+                .bucket(bucketName)
+                .delete(Delete.builder()
+                .objects(toDelete).build())
+                .build();
+            
             s3.deleteObjects(dor);
 
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
+        
         System.out.println("Done!");
     }
     // snippet-end:[s3.java2.delete_objects.main]
