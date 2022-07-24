788959990803b308ce7681c065ea5bd6520e9594
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-24 11:37:24 -0400

    Add files via upload

--- javav2/example_code/lex/src/main/java/com/example/lex/GetIntent.java
+++ javav2/example_code/lex/src/main/java/com/example/lex/GetIntent.java
@@ -1,9 +1,6 @@
 //snippet-sourcedescription:[GetIntent.java demonstrates how to return information about an intent.]
 //snippet-keyword:[AWS SDK for Java v2]
-//snippet-keyword:[Code Sample]
 //snippet-service:[Amazon Lex]
-//snippet-sourcetype:[full-example]
-// snippet-sourcedate:[05/18/2022]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -33,25 +30,25 @@ public class GetIntent {
     public static void main(String[] args) {
 
         final String usage = "\n" +
-                "Usage: " +
-                "   <intentName> <intentVersion>\n\n" +
-                "Where:\n" +
-                "   intentName - The name of the intent (for example, BookHotel).\n\n" +
-                "   intentVersion - The version of the intent (for example, 1).\n\n";
+            "Usage: " +
+            "   <intentName> <intentVersion>\n\n" +
+            "Where:\n" +
+            "   intentName - The name of the intent (for example, BookHotel).\n\n" +
+            "   intentVersion - The version of the intent (for example, 1).\n\n";
 
         if (args.length != 2) {
              System.out.println(usage);
              System.exit(1);
-         }
+        }
 
         String intentName = args[0];
         String intentVersion = args[1];
 
         Region region = Region.US_WEST_2;
         LexModelBuildingClient lexClient = LexModelBuildingClient.builder()
-                .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
+            .region(region)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
 
         getSpecificIntent(lexClient, intentName, intentVersion);
         lexClient.close();
@@ -62,9 +59,9 @@ public class GetIntent {
 
         try {
             GetIntentRequest intentRequest = GetIntentRequest.builder()
-                    .name(intentName)
-                    .version(intentVersion)
-                    .build();
+                .name(intentName)
+                .version(intentVersion)
+                .build();
 
             GetIntentResponse intentResponse = lexClient.getIntent(intentRequest);
             System.out.println("The description is "+ intentResponse.description());
