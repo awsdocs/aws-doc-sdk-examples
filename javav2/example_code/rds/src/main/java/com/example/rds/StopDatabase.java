ed7a9225b21e47151a7d0a57abae051cec8ab879
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-24 12:14:05 -0400

    Add files via upload

--- javav2/example_code/rds/src/main/java/com/example/rds/StopDatabase.java
+++ javav2/example_code/rds/src/main/java/com/example/rds/StopDatabase.java
@@ -1,22 +1,16 @@
-//snippet-sourcedescription:[DeleteDBInstance.java demonstrates how to delete an Amazon Relational Database Service (RDS) snapshot.]
+//snippet-sourcedescription:[StopDatabase.java demonstrates how to stop an Amazon Relational Database Service (RDS) instance.]
 //snippet-keyword:[AWS SDK for Java v2]
 //snippet-service:[Amazon Relational Database Service]
 
-/*
-   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-   SPDX-License-Identifier: Apache-2.0
-*/
-
 package com.example.rds;
 
-// snippet-start:[rds.java2.delete_instance.import]
+// snippet-start:[rds.java2.stop_instance.import]
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rds.RdsClient;
-import software.amazon.awssdk.services.rds.model.DeleteDbInstanceRequest;
-import software.amazon.awssdk.services.rds.model.DeleteDbInstanceResponse;
 import software.amazon.awssdk.services.rds.model.RdsException;
-// snippet-end:[rds.java2.delete_instance.import]
+import software.amazon.awssdk.services.rds.model.StopDbInstanceRequest;
+// snippet-end:[rds.java2.stop_instance.import]
 
 /**
  * Before running this Java V2 code example, set up your development environment, including your credentials.
@@ -25,7 +19,7 @@ import software.amazon.awssdk.services.rds.model.RdsException;
  *
  * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
  */
-public class DeleteDBInstance {
+public class StopDatabase {
 
     public static void main(String[] args) {
 
@@ -33,7 +27,7 @@ public class DeleteDBInstance {
             "Usage:\n" +
             "    <dbInstanceIdentifier> \n\n" +
             "Where:\n" +
-            "    dbInstanceIdentifier - The database instance identifier \n";
+            "    dbInstanceIdentifier - The database instance identifier \n" ;
 
         if (args.length != 1) {
             System.out.println(usage);
@@ -47,26 +41,26 @@ public class DeleteDBInstance {
             .credentialsProvider(ProfileCredentialsProvider.create())
             .build();
 
-        deleteDatabaseInstance(rdsClient, dbInstanceIdentifier);
+        stopInstance(rdsClient, dbInstanceIdentifier) ;
         rdsClient.close();
     }
 
-    // snippet-start:[rds.java2.delete_instance.main]
-    public static void deleteDatabaseInstance( RdsClient rdsClient, String dbInstanceIdentifier) {
+    // snippet-start:[rds.java2.stop_instance.main]
+    public static void stopInstance(RdsClient rdsClient, String dbInstanceIdentifier ) {
+
         try {
-            DeleteDbInstanceRequest deleteDbInstanceRequest = DeleteDbInstanceRequest.builder()
+            StopDbInstanceRequest stopDbInstanceRequest = StopDbInstanceRequest.builder()
                 .dbInstanceIdentifier(dbInstanceIdentifier)
-                .deleteAutomatedBackups(true)
-                .skipFinalSnapshot(true)
                 .build();
 
-            DeleteDbInstanceResponse response = rdsClient.deleteDBInstance(deleteDbInstanceRequest);
-            System.out.print("The status of the database is " + response.dbInstance().dbInstanceStatus());
+            rdsClient.stopDBInstance(stopDbInstanceRequest);
+            System.out.print("The database was stopped!");
 
         } catch (RdsException e) {
             System.out.println(e.getLocalizedMessage());
             System.exit(1);
         }
     }
-    // snippet-end:[rds.java2.delete_instance.main]
+    // snippet-end:[rds.java2.stop_instance.main]
 }
+
