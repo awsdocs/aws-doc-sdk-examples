f813f408d33e1a6e6040b1a7e201e437050802e8
Laren-AWS <larencra@amazon.com>
2022-07-26 15:30:08 -0700

    Clean up obsolete Lambda handler examples.

--- lambda_functions/blank-csharp/2-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-ARTIFACT_BUCKET=$(cat bucket-name.txt)
-cd src/blank-csharp
-dotnet lambda package
-cd ../../
-aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name blank-csharp --capabilities CAPABILITY_NAMED_IAM
--- lambda_functions/blank-python/2-build-layer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-rm -rf package
-cd function
-pip install --target ../package/python -r requirements.txt
