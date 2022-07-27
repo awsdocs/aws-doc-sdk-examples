e9c31d150cca7d15fc61baebbf2e65f0ad7ea8eb
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-27 18:45:29 -0400

    Add files via upload

--- kotlin/services/ec2/src/main/kotlin/com/kotlin/ec2/FindRunningInstances.kt
+++ kotlin/services/ec2/src/main/kotlin/com/kotlin/ec2/FindRunningInstances.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[FindRunningInstances.kt demonstrates how to find running Amazon Elastic Compute Cloud (Amazon EC2) instances by using a filter.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-service:[Amazon EC2]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/04/2021]
-//snippet-sourceauthor:[scmacdon-aws]
+// snippet-sourcedescription:[FindRunningInstances.kt demonstrates how to find running Amazon Elastic Compute Cloud (Amazon EC2) instances by using a filter.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-service:[Amazon EC2]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -20,13 +16,12 @@ import aws.sdk.kotlin.services.ec2.model.Filter
 // snippet-end:[ec2.kotlin.running_instances.import]
 
 /**
-To run this Kotlin code example, ensure that you have setup your development environment,
+Before running this Kotlin code example, set up your development environment,
 including your credentials.
 
-For information, see this documentation topic:
+For more information, see the following documentation topic:
 https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
-
 suspend fun main() {
     findRunningEC2Instances()
 }
@@ -34,19 +29,19 @@ suspend fun main() {
 // snippet-start:[ec2.kotlin.running_instances.main]
 suspend fun findRunningEC2Instances() {
 
-        val filter = Filter {
-                name = "instance-state-name"
-                values = listOf("running")
-         }
+    val filter = Filter {
+        name = "instance-state-name"
+        values = listOf("running")
+    }
 
-        val request = DescribeInstancesRequest {
-           filters = listOf(filter)
-        }
+    val request = DescribeInstancesRequest {
+        filters = listOf(filter)
+    }
 
-        Ec2Client { region = "us-west-2" }.use { ec2 ->
-            val response = ec2.describeInstances(request)
-            response.reservations?.forEach { reservation ->
-                reservation.instances?.forEach { instance ->
+    Ec2Client { region = "us-west-2" }.use { ec2 ->
+        val response = ec2.describeInstances(request)
+        response.reservations?.forEach { reservation ->
+            reservation.instances?.forEach { instance ->
                 println("Found Reservation with id: ${instance.instanceId}, type: ${instance.instanceType} state: ${instance.state?.name} and monitoring state: ${instance.monitoring?.state}")
             }
         }
