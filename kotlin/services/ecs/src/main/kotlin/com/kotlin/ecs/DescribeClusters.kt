ebd311c4fb21dca58851525d18853f5de78f3f80
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-27 18:47:05 -0400

    Add files via upload

--- kotlin/services/ecs/src/main/kotlin/com/kotlin/ecs/DescribeClusters.kt
+++ kotlin/services/ecs/src/main/kotlin/com/kotlin/ecs/DescribeClusters.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[DescribeClusters.kt demonstrates how to describe a cluster for the Amazon Elastic Container Service (Amazon ECS) service.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-service:[Amazon Elastic Container Service]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/04/2021]
-//snippet-sourceauthor:[scmacdon-aws]
+// snippet-sourcedescription:[DescribeClusters.kt demonstrates how to describe a cluster for the Amazon Elastic Container Service (Amazon ECS) service.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-service:[Amazon Elastic Container Service]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -20,14 +16,14 @@ import kotlin.system.exitProcess
 // snippet-end:[ecs.kotlin.des_cluster.import]
 
 /**
-To run this Kotlin code example, ensure that you have setup your development environment,
+Before running this Kotlin code example, set up your development environment,
 including your credentials.
 
-For information, see this documentation topic:
+For more information, see the following documentation topic:
 https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
 
-suspend fun main(args:Array<String>){
+suspend fun main(args: Array<String>) {
 
     val usage = """
     
@@ -35,17 +31,17 @@ suspend fun main(args:Array<String>){
         <clusterArn> 
 
     Where:
-        clusterArn - the ARN of the ECS cluster.
+        clusterArn - The ARN of the ECS cluster.
     """
 
     if (args.size != 1) {
         println(usage)
         exitProcess(0)
-     }
+    }
 
     val clusterArn = args[0]
     descCluster(clusterArn)
-    }
+}
 
 // snippet-start:[ecs.kotlin.des_cluster.main]
 suspend fun descCluster(clusterArn: String) {
@@ -54,7 +50,7 @@ suspend fun descCluster(clusterArn: String) {
         clusters = listOf(clusterArn)
     }
 
-    EcsClient {region = "us-east-1" }.use { ecsClient ->
+    EcsClient { region = "us-east-1" }.use { ecsClient ->
         val response = ecsClient.describeClusters(request)
         response.clusters?.forEach { cluster ->
             println("The cluster name is ${cluster.clusterName}.")
