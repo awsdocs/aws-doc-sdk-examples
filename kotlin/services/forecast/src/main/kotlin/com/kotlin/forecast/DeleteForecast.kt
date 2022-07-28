ffef8dbf7aa44edeb235e35bc7ce3029780b3590
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-28 09:11:58 -0400

    Add files via upload

--- kotlin/services/forecast/src/main/kotlin/com/kotlin/forecast/DeleteForecast.kt
+++ kotlin/services/forecast/src/main/kotlin/com/kotlin/forecast/DeleteForecast.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[DeleteForecast.kt demonstrates how to delete a forecast that belongs to the Amazon Forecast service.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-service:[Amazon Forecast]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/04/2021]
-//snippet-sourceauthor:[scmacdon-aws]
+// snippet-sourcedescription:[DeleteForecast.kt demonstrates how to delete a forecast that belongs to the Amazon Forecast service.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-service:[Amazon Forecast]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -20,27 +16,27 @@ import kotlin.system.exitProcess
 // snippet-end:[forecast.kotlin.delete_forecast.import]
 
 /**
-To run this Kotlin code example, ensure that you have setup your development environment,
+Before running this Kotlin code example, set up your development environment,
 including your credentials.
 
-For information, see this documentation topic:
+For more information, see the following documentation topic:
 https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
 
-suspend fun main(args:Array<String>) {
+suspend fun main(args: Array<String>) {
 
     val usage = """
     Usage:
         <forecastArn>
 
     Where:
-       forecastArn - the ARN that belongs to the forecast to delete. 
+       forecastArn - The ARN that belongs to the forecast to delete. 
       """
 
-      if (args.size != 1) {
-          println(usage)
-          exitProcess(0)
-      }
+    if (args.size != 1) {
+        println(usage)
+        exitProcess(0)
+    }
 
     val forecastArn = args[0]
     delForecast(forecastArn)
@@ -49,13 +45,13 @@ suspend fun main(args:Array<String>) {
 // snippet-start:[forecast.kotlin.delete_forecast.main]
 suspend fun delForecast(forecastArnVal: String) {
 
-          val request = DeleteForecastRequest {
-              forecastArn = forecastArnVal
-          }
+    val request = DeleteForecastRequest {
+        forecastArn = forecastArnVal
+    }
 
-          ForecastClient { region = "us-west-2" }.use { forecast ->
-            forecast.deleteForecast(request)
-            println("$forecastArnVal was successfully deleted")
-          }
+    ForecastClient { region = "us-west-2" }.use { forecast ->
+        forecast.deleteForecast(request)
+        println("$forecastArnVal was successfully deleted")
+    }
 }
 // snippet-end:[forecast.kotlin.delete_forecast.main]
