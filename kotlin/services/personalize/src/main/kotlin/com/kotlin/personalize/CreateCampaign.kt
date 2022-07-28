488a0e823f8833ee06a9a81e365f5847dd43e408
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-28 10:04:31 -0400

    Add files via upload

--- kotlin/services/personalize/src/main/kotlin/com/kotlin/personalize/CreateCampaign.kt
+++ kotlin/services/personalize/src/main/kotlin/com/kotlin/personalize/CreateCampaign.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[CreateCampaign.kt demonstrates how to create an Amazon Personalize campaign.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-service:[Amazon Personalize]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/05/2021]
-//snippet-sourceauthor:[scmacdon - AWS]
+// snippet-sourcedescription:[CreateCampaign.kt demonstrates how to create an Amazon Personalize campaign.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-service:[Amazon Personalize]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -13,21 +9,21 @@
 
 package com.kotlin.personalize
 
-//snippet-start:[personalize.kotlin.create_campaign.import]
+// snippet-start:[personalize.kotlin.create_campaign.import]
 import aws.sdk.kotlin.services.personalize.PersonalizeClient
 import aws.sdk.kotlin.services.personalize.model.CreateCampaignRequest
 import kotlin.system.exitProcess
-//snippet-end:[personalize.kotlin.create_campaign.import]
+// snippet-end:[personalize.kotlin.create_campaign.import]
 
 /**
-To run this Kotlin code example, ensure that you have setup your development environment,
+Before running this Kotlin code example, set up your development environment,
 including your credentials.
 
-For information, see this documentation topic:
+For more information, see the following documentation topic:
 https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
 
-suspend fun main(args:Array<String>){
+suspend fun main(args: Array<String>) {
 
     val usage = """
     Usage:
@@ -41,26 +37,26 @@ suspend fun main(args:Array<String>){
     if (args.size != 2) {
         println(usage)
         exitProcess(0)
-     }
+    }
 
     val solutionVersionArn = args[0]
     val campaignName = args[1]
     val campaignARN = createPersonalCompaign(solutionVersionArn, campaignName)
     println("The campaign ARN is $campaignARN")
+}
+
+// snippet-start:[personalize.kotlin.create_campaign.main]
+suspend fun createPersonalCompaign(solutionVersionArnVal: String?, campaignName: String?): String? {
+
+    val request = CreateCampaignRequest {
+        minProvisionedTps = 1
+        solutionVersionArn = solutionVersionArnVal
+        name = campaignName
     }
 
-//snippet-start:[personalize.kotlin.create_campaign.main]
-suspend fun createPersonalCompaign(solutionVersionArnVal: String?, campaignName: String?) : String? {
-
-      val request = CreateCampaignRequest {
-          minProvisionedTps = 1
-          solutionVersionArn = solutionVersionArnVal
-          name = campaignName
-      }
-
-      PersonalizeClient { region = "us-east-1" }.use { personalizeClient ->
+    PersonalizeClient { region = "us-east-1" }.use { personalizeClient ->
         val campaignResponse = personalizeClient.createCampaign(request)
         return campaignResponse.campaignArn
-     }
+    }
 }
-//snippet-end:[personalize.kotlin.create_campaign.main]
+// snippet-end:[personalize.kotlin.create_campaign.main]
