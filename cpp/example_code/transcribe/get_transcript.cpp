3bdbff1b4349980871d862ffaab248f2b33ca8f9
annapan-aws <annapan@amazon.com>
2022-07-29 13:23:35 -0400

    updating copyright

--- cpp/example_code/transcribe/get_transcript.cpp
+++ cpp/example_code/transcribe/get_transcript.cpp
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 #include <aws/core/Aws.h>
 #include <aws/core/utils/threading/Semaphore.h>
 #include <aws/core/auth/AWSCredentialsProviderChain.h>
