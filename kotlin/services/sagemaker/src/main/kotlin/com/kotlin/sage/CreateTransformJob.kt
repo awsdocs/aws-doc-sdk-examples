706e1d3e4fece24473c1deb3aa2c08c985e6a517
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-29 15:12:38 -0400

    Add files via upload

--- kotlin/services/sagemaker/src/main/kotlin/com/kotlin/sage/CreateTransformJob.kt
+++ kotlin/services/sagemaker/src/main/kotlin/com/kotlin/sage/CreateTransformJob.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[CreateTransformJob.kt demonstrates how to start a transform job that uses a trained model to get inferences on a dataset.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-keyword:[Amazon SageMaker]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/05/2021]
-//snippet-sourceauthor:[scmacdon - AWS]
+// snippet-sourcedescription:[CreateTransformJob.kt demonstrates how to start a transform job that uses a trained model to get inferences on a dataset.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-keyword:[Amazon SageMaker]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -13,53 +9,53 @@
 
 package com.kotlin.sage
 
-//snippet-start:[sagemaker.kotlin.transform_job.import]
+// snippet-start:[sagemaker.kotlin.transform_job.import]
 import aws.sdk.kotlin.services.sagemaker.SageMakerClient
-import aws.sdk.kotlin.services.sagemaker.model.TransformS3DataSource
+import aws.sdk.kotlin.services.sagemaker.model.CreateTransformJobRequest
 import aws.sdk.kotlin.services.sagemaker.model.S3DataType
+import aws.sdk.kotlin.services.sagemaker.model.SplitType
 import aws.sdk.kotlin.services.sagemaker.model.TransformDataSource
 import aws.sdk.kotlin.services.sagemaker.model.TransformInput
-import aws.sdk.kotlin.services.sagemaker.model.SplitType
+import aws.sdk.kotlin.services.sagemaker.model.TransformInstanceType
 import aws.sdk.kotlin.services.sagemaker.model.TransformOutput
 import aws.sdk.kotlin.services.sagemaker.model.TransformResources
-import aws.sdk.kotlin.services.sagemaker.model.TransformInstanceType
-import aws.sdk.kotlin.services.sagemaker.model.CreateTransformJobRequest
+import aws.sdk.kotlin.services.sagemaker.model.TransformS3DataSource
 import kotlin.system.exitProcess
-//snippet-end:[sagemaker.kotlin.transform_job.import]
+// snippet-end:[sagemaker.kotlin.transform_job.import]
 
 /**
-To run this Kotlin code example, ensure that you have setup your development environment,
+Before running this Kotlin code example, set up your development environment,
 including your credentials.
 
-For information, see this documentation topic:
+For more information, see the following documentation topic:
 https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
-suspend fun main(args:Array<String>) {
+suspend fun main(args: Array<String>) {
 
     val usage = """
     Usage:
         <s3Uri> <s3OutputPath> <modelName> <transformJobName>
 
     Where:
-        s3Uri - identifies the key name of an Amazon S3 object that contains the data (ie, s3://mybucket/churn.txt).
-        s3OutputPath - the Amazon S3 location where the results are stored.
-        modelName - the name of the model.
-        transformJobName - the name of the transform job.
+        s3Uri - Identifies the key name of an Amazon S3 object that contains the data (ie, s3://mybucket/churn.txt).
+        s3OutputPath - The Amazon S3 location where the results are stored.
+        modelName - The name of the model.
+        transformJobName - The name of the transform job.
     """
 
-   if (args.size != 4) {
+    if (args.size != 4) {
         println(usage)
         exitProcess(1)
-   }
+    }
 
     val s3Uri = args[0]
     val s3OutputPath = args[1]
     val modelName = args[2]
     val transformJobName = args[3]
     transformJob(s3Uri, s3OutputPath, modelName, transformJobName)
-    }
+}
 
-//snippet-start:[sagemaker.kotlin.transform_job.main]
+// snippet-start:[sagemaker.kotlin.transform_job.main]
 suspend fun transformJob(
     s3UriVal: String?,
     s3OutputPathVal: String?,
@@ -67,41 +63,41 @@ suspend fun transformJob(
     transformJobNameVal: String?
 ) {
 
-        val s3DataSourceOb = TransformS3DataSource {
-            s3DataType = S3DataType.S3Prefix
-            s3Uri = s3UriVal
-        }
+    val s3DataSourceOb = TransformS3DataSource {
+        s3DataType = S3DataType.S3Prefix
+        s3Uri = s3UriVal
+    }
 
-        val dataSourceOb = TransformDataSource {
-            s3DataSource = s3DataSourceOb
-        }
+    val dataSourceOb = TransformDataSource {
+        s3DataSource = s3DataSourceOb
+    }
 
-        val input = TransformInput {
-            dataSource = dataSourceOb
-            contentType = "text/csv"
-            splitType = SplitType.Line
-        }
+    val input = TransformInput {
+        dataSource = dataSourceOb
+        contentType = "text/csv"
+        splitType = SplitType.Line
+    }
 
-        val output = TransformOutput {
-            s3OutputPath = s3OutputPathVal
-        }
+    val output = TransformOutput {
+        s3OutputPath = s3OutputPathVal
+    }
 
-        val resources = TransformResources {
-            instanceCount = 1
-            instanceType = TransformInstanceType.MlC4_4_Xlarge
-        }
+    val resources = TransformResources {
+        instanceCount = 1
+        instanceType = TransformInstanceType.MlC4_4_Xlarge
+    }
 
-        val request = CreateTransformJobRequest {
-            transformJobName = transformJobNameVal
-            modelName = modelNameVal
-            transformInput = input
-            transformOutput = output
-            transformResources = resources
-        }
+    val request = CreateTransformJobRequest {
+        transformJobName = transformJobNameVal
+        modelName = modelNameVal
+        transformInput = input
+        transformOutput = output
+        transformResources = resources
+    }
 
-        SageMakerClient { region = "us-west-2" }.use { sageMakerClient ->
-          val jobResponse = sageMakerClient.createTransformJob(request)
-          println("Response ${jobResponse.transformJobArn}")
-        }
+    SageMakerClient { region = "us-west-2" }.use { sageMakerClient ->
+        val jobResponse = sageMakerClient.createTransformJob(request)
+        println("Response ${jobResponse.transformJobArn}")
+    }
 }
-//snippet-end:[sagemaker.kotlin.transform_job.main]
+// snippet-end:[sagemaker.kotlin.transform_job.main]
