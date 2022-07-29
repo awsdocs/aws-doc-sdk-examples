13355b9a2dd368d99a05b1ed17cb1bbaa2f4aec1
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-29 15:26:14 -0400

    Add files via upload

--- kotlin/services/secretsmanager/src/main/kotlin/com/kotlin/secrets/UpdateSecret.kt
+++ kotlin/services/secretsmanager/src/main/kotlin/com/kotlin/secrets/UpdateSecret.kt
@@ -1,10 +1,6 @@
-//snippet-sourcedescription:[UpdateSecret.kt demonstrates how to update a secret for AWS Secrets Manager.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-keyword:[AWS Secrets Manager]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/05/2021]
-//snippet-sourceauthor:[scmacdon-aws]
+// snippet-sourcedescription:[UpdateSecret.kt demonstrates how to update a secret for AWS Secrets Manager.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-keyword:[AWS Secrets Manager]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -13,12 +9,19 @@
 
 package com.kotlin.secrets
 
-//snippet-start:[secretsmanager.kotlin.update_secret.import]
+// snippet-start:[secretsmanager.kotlin.update_secret.import]
 import aws.sdk.kotlin.services.secretsmanager.SecretsManagerClient
 import aws.sdk.kotlin.services.secretsmanager.model.UpdateSecretRequest
 import kotlin.system.exitProcess
-//snippet-end:[secretsmanager.kotlin.update_secret.import]
+// snippet-end:[secretsmanager.kotlin.update_secret.import]
 
+/**
+Before running this Kotlin code example, set up your development environment,
+including your credentials.
+
+For more information, see the following documentation topic:
+https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+*/
 suspend fun main(args: Array<String>) {
 
     val usage = """
@@ -26,8 +29,8 @@ suspend fun main(args: Array<String>) {
         <secretName> <secretValue>
 
     Where:
-         secretName - the name of the secret (for example, tutorials/MyFirstSecret).
-         secretValue - the secret value.
+         secretName - The name of the secret (for example, tutorials/MyFirstSecret).
+         secretValue - The secret value.
       """
 
     if (args.size != 2) {
@@ -38,19 +41,19 @@ suspend fun main(args: Array<String>) {
     val secretName = args[0]
     val secretValue = args[1]
     updateMySecret(secretName, secretValue)
-    }
+}
 
-//snippet-start:[secretsmanager.kotlin.update_secret.main]
+// snippet-start:[secretsmanager.kotlin.update_secret.main]
 suspend fun updateMySecret(secretName: String?, secretValue: String?) {
 
-        val request = UpdateSecretRequest {
-            secretId = secretName
-            secretString = secretValue
-        }
+    val request = UpdateSecretRequest {
+        secretId = secretName
+        secretString = secretValue
+    }
 
-        SecretsManagerClient { region = "us-east-1" }.use { secretsClient ->
-            secretsClient.updateSecret(request)
-            println("The secret value was updated")
-        }
- }
-//snippet-end:[secretsmanager.kotlin.update_secret.main]
+    SecretsManagerClient { region = "us-east-1" }.use { secretsClient ->
+        secretsClient.updateSecret(request)
+        println("The secret value was updated")
+    }
+}
+// snippet-end:[secretsmanager.kotlin.update_secret.main]
