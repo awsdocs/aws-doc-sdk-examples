a36702edd6587f60b6441b65643e52496c32cd08
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-07-31 09:05:45 -0400

    Add files via upload

--- kotlin/services/stepfunctions/src/main/kotlin/com/kotlin/stepfunctions/ListStateMachines.kt
+++ kotlin/services/stepfunctions/src/main/kotlin/com/kotlin/stepfunctions/ListStateMachines.kt
@@ -1,11 +1,6 @@
-//snippet-sourcedescription:[ListStateMachines.kt demonstrates how to list existing state machines for AWS Step Functions.]
-//snippet-keyword:[AWS SDK for Kotlin]
-//snippet-keyword:[Code Sample]
-//snippet-service:[AWS Step Functions]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[11/05/2021]
-//snippet-sourceauthor:[scmacdon-AWS]
-
+// snippet-sourcedescription:[ListStateMachines.kt demonstrates how to list existing state machines for AWS Step Functions.]
+// snippet-keyword:[AWS SDK for Kotlin]
+// snippet-service:[AWS Step Functions]
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
@@ -14,9 +9,17 @@
 package com.kotlin.stepfunctions
 
 // snippet-start:[stepfunctions.kotlin.list_machines.import]
-import  aws.sdk.kotlin.services.sfn.SfnClient
+import aws.sdk.kotlin.services.sfn.SfnClient
 import aws.sdk.kotlin.services.sfn.model.ListStateMachinesRequest
 // snippet-end:[stepfunctions.kotlin.list_machines.import]
+
+/**
+Before running this Kotlin code example, set up your development environment,
+including your credentials.
+
+For more information, see the following documentation topic:
+https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
+ */
 
 suspend fun main() {
     listMachines()
@@ -25,12 +28,12 @@ suspend fun main() {
 // snippet-start:[stepfunctions.kotlin.list_machines.main]
 suspend fun listMachines() {
 
-       SfnClient { region = "us-east-1" }.use { sfnClient ->
-         val response = sfnClient.listStateMachines(ListStateMachinesRequest{})
-          response.stateMachines?.forEach { machine ->
-                    println("The name of the state machine is ${machine.name}")
-                    println("The ARN value is ${machine.stateMachineArn}")
-                }
+    SfnClient { region = "us-east-1" }.use { sfnClient ->
+        val response = sfnClient.listStateMachines(ListStateMachinesRequest {})
+        response.stateMachines?.forEach { machine ->
+            println("The name of the state machine is ${machine.name}")
+            println("The ARN value is ${machine.stateMachineArn}")
         }
- }
+    }
+}
 // snippet-end:[stepfunctions.kotlin.list_machines.main]
