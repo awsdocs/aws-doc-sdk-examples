bf03da7f60726666bc63190ad12a87ed209e1cd6
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-08-02 08:38:44 -0400

    Update InjectWorkService.java

--- javav2/usecases/Creating_Spring_Redshift_ Rest/src/main/java/com/aws/rest/InjectWorkService.java
+++ javav2/usecases/Creating_Spring_Redshift_ Rest/src/main/java/com/aws/rest/InjectWorkService.java
@@ -1,3 +1,8 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 package com.aws.rest;
 
 import org.springframework.stereotype.Component;
