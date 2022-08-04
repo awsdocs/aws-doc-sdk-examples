51fed315adc8f690f0ff95b5b832654281f2fdda
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-08-04 15:33:49 -0400

    Add files via upload

---
+++ javav2/example_code/ses/src/main/java/com/example/sesv2/ListTemplates.java
@@ -0,0 +1,52 @@
+// snippet-comment:[These are tags for the AWS doc team's sample catalog. Do not remove.]
+// snippet-sourcedescription:[SendEmailTemplate.java demonstrates how to send an email message based on a template by using the SesV2Client.]
+// snippet-keyword:[AWS SDK for Java v2]
+// snippet-keyword:[Amazon Simple Email Service]
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.example.sesv2;
+
+// snippet-start:[ses.java2.list.templates.sesv2.import]
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.ListEmailTemplatesRequest;
+import software.amazon.awssdk.services.sesv2.model.ListEmailTemplatesResponse;
+import software.amazon.awssdk.services.sesv2.model.SesV2Exception;
+// snippet-end:[ses.java2.list.templates.sesv2.import]
+
+public class ListTemplates {
+
+    public static void main(String[] args) {
+        Region region = Region.US_EAST_1;
+        SesV2Client sesv2Client = SesV2Client.builder()
+            .region(region)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
+
+        listALlTemplates(sesv2Client);
+    }
+
+    // snippet-start:[ses.java2.list.templates.sesv2.main]
+    public static void listALlTemplates(SesV2Client sesv2Client) {
+
+        try {
+            ListEmailTemplatesRequest templatesRequest = ListEmailTemplatesRequest.builder()
+                .pageSize(1)
+                .build();
+
+            ListEmailTemplatesResponse response = sesv2Client.listEmailTemplates(templatesRequest);
+            response.templatesMetadata().forEach(template ->
+                    System.out.println("Template name: " + template.templateName()));
+
+        } catch (SesV2Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+    // snippet-end:[ses.java2.list.templates.sesv2.main]
+}
