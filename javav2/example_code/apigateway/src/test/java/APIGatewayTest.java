7559fa9d15304849e2b80189a99d35a2ad94eabb
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2022-08-04 11:14:41 +0530

    Adding the tests

--- javav2/example_code/apigateway/src/test/java/APIGatewayTest.java
+++ javav2/example_code/apigateway/src/test/java/APIGatewayTest.java
@@ -14,12 +14,15 @@ public class APIGatewayTest {
 
     private static ApiGatewayClient apiGateway;
     private static String restApiId = "";
+    private static String swaggerFilePath = "";
     private static String resourceId = "";
     private static String httpMethod = "";
     private static String restApiName = "";
     private static String stageName = "";
     private static String newApiId = ""; // Gets dynamically set
     private static String deploymentId = "";  // Gets dynamically set
+    private static String newImportedRestApiId = ""; // Gets dynamically set
+    private static String deploymentIdForImported = "";  // Gets dynamically set
 
 
     @BeforeAll
@@ -42,6 +45,7 @@ public class APIGatewayTest {
 
             // Populate the data members required for all tests
             restApiId = prop.getProperty("restApiId");
+            swaggerFilePath = prop.getProperty("swaggerFilePath");
             resourceId = prop.getProperty("resourceId");
             httpMethod = prop.getProperty("httpMethod");
             restApiName = prop.getProperty("restApiName");
@@ -67,40 +71,76 @@ public class APIGatewayTest {
         assertTrue(!newApiId.isEmpty());
         System.out.println("Test 2 passed");
     }
-
+    
     @Test
     @Order(3)
-    public void CreateDeployment() {
-        deploymentId = CreateDeployment.createNewDeployment(apiGateway, newApiId, stageName);
-        assertTrue(!deploymentId.isEmpty());
+    public void ImportRestApi() {
+
+    	newImportedRestApiId = ImportRestApi.importAPI(apiGateway, swaggerFilePath);
+        assertTrue(!newImportedRestApiId.isEmpty());
         System.out.println("Test 3 passed");
     }
 
     @Test
     @Order(4)
-    public void GetDeployments() {
-        GetDeployments.getAllDeployments(apiGateway, newApiId);
+    public void CreateDeployment() {
+        deploymentId = CreateDeployment.createNewDeployment(apiGateway, newApiId, stageName);
+        assertTrue(!deploymentId.isEmpty());
         System.out.println("Test 4 passed");
     }
-
+    
     @Test
-    @Order(6)
-    public void GetMethod() {
-        GetMethod.getSpecificMethod(apiGateway, restApiId, resourceId, httpMethod);
+    @Order(5)
+    public void CreateDeploymentForImported() throws InterruptedException {
+    	Thread.sleep(1000);//to avoid too many requests
+    	deploymentIdForImported = CreateDeployment.createNewDeployment(apiGateway, newImportedRestApiId, stageName);
+        assertTrue(!deploymentIdForImported.isEmpty());
         System.out.println("Test 5 passed");
     }
 
     @Test
+    @Order(6)
+    public void GetDeployments() {
+        GetDeployments.getAllDeployments(apiGateway, newApiId);
+        System.out.println("Test 6 passed");
+    }
+    
+    @Test
     @Order(7)
-    public void GetStages() {
-
-        GetStages.getAllStages(apiGateway, newApiId);
+    public void GetDeploymentsForImported() {
+        GetDeployments.getAllDeployments(apiGateway, newImportedRestApiId);
         System.out.println("Test 7 passed");
     }
 
 
     @Test
     @Order(8)
+    public void GetMethod() {
+        GetMethod.getSpecificMethod(apiGateway, restApiId, resourceId, httpMethod);
+        System.out.println("Test 8 passed");
+    }
+    
+    
+
+    @Test
+    @Order(9)
+    public void GetStages() {
+
+        GetStages.getAllStages(apiGateway, newApiId);
+        System.out.println("Test 9 passed");
+    }
+    
+    @Test
+    @Order(10)
+    public void GetStagesForImported() {
+
+        GetStages.getAllStages(apiGateway, newImportedRestApiId);
+        System.out.println("Test 10 passed");
+        }
+
+
+    @Test
+    @Order(11)
     public void DeleteRestApi() {
 
         Region region = Region.US_EAST_1;
@@ -109,6 +149,19 @@ public class APIGatewayTest {
                 .build();
 
         DeleteRestApi.deleteAPI(apiGateway2, newApiId);
-        System.out.println("Test 9 passed");
+        System.out.println("Test 11 passed");
+    }
+    
+    @Test
+    @Order(12)
+    public void DeleteImportedRestApi() {
+
+        Region region = Region.US_EAST_1;
+        ApiGatewayClient apiGateway2 = ApiGatewayClient.builder()
+                .region(region)
+                .build();
+
+        DeleteRestApi.deleteAPI(apiGateway2, newImportedRestApiId);
+        System.out.println("Test 12 passed");
     }
 }
