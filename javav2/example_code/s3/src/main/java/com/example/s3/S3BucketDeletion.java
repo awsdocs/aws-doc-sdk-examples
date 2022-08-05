e7e55cbe108625a0662fba40f601aa1e65975cbc
Todd Hill <tkhill@amazon.com>
2022-08-05 09:59:49 -0400

    Refactorings that rename name methods to better describe their function. Extracted the method that actually deletes the bucked from the method the deletes the objects.
    These refactorings will allow better use in the Java2 Dev Guide.

--- javav2/example_code/s3/src/main/java/com/example/s3/S3BucketDeletion.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/S3BucketDeletion.java
@@ -35,10 +35,10 @@ public class S3BucketDeletion {
     public static void main(String[] args) throws Exception {
 
         final String usage = "\n" +
-            "Usage:\n" +
-            "    <bucket>\n\n" +
-            "Where:\n" +
-            "    bucket - The bucket to delete (for example, bucket1). \n\n" ;
+                "Usage:\n" +
+                "    <bucket>\n\n" +
+                "Where:\n" +
+                "    bucket - The bucket to delete (for example, bucket1). \n\n" ;
 
         if (args.length != 1) {
             System.out.println(usage);
@@ -49,49 +49,49 @@ public class S3BucketDeletion {
         ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create();
         Region region = Region.US_EAST_1;
         S3Client s3 = S3Client.builder()
-            .region(region)
-            .credentialsProvider(credentialsProvider)
-            .build();
+                .region(region)
+                .credentialsProvider(credentialsProvider)
+                .build();
 
-        listAllObjects(s3,bucket) ;
+        deleteObjectsInBucket(s3,bucket);
+        deleteBucket(s3, bucket);
+
         s3.close();
     }
 
-    // snippet-start:[s3.java2.s3_bucket_ops.delete_bucket]
-    public static void listAllObjects(S3Client s3, String bucket) {
+    // snippet-start:[s3.java2.s3_bucket_ops.delete_objects]
+    public static void deleteObjectsInBucket (S3Client s3, String bucket) {
 
         try {
             // To delete a bucket, all the objects in the bucket must be deleted first.
             ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
-                .bucket(bucket)
-                .build();
+                    .bucket(bucket)
+                    .build();
             ListObjectsV2Response listObjectsV2Response;
 
             do {
                 listObjectsV2Response = s3.listObjectsV2(listObjectsV2Request);
                 for (S3Object s3Object : listObjectsV2Response.contents()) {
                     s3.deleteObject(DeleteObjectRequest.builder()
-                        .bucket(bucket)
-                        .key(s3Object.key())
-                        .build());
+                            .bucket(bucket)
+                            .key(s3Object.key())
+                            .build());
                 }
-
-                listObjectsV2Request = ListObjectsV2Request.builder()
-                        .bucket(bucket)
-                        .continuationToken(listObjectsV2Response.nextContinuationToken())
-                        .build();
-
             } while(listObjectsV2Response.isTruncated());
-            // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket]
-
-            DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucket).build();
-            s3.deleteBucket(deleteBucketRequest);
+            // snippet-end:[s3.java2.s3_bucket_ops.delete_objects]
 
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
     }
+
+    // snippet-start:[s3.java2.s3_bucket_ops.delete_bucket]
+    private static void deleteBucket(S3Client s3, String bucket) {
+        DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucket).build();
+        s3.deleteBucket(deleteBucketRequest);
+    }
+    // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket]
     // snippet-end:[s3.java2.bucket_deletion.main]
 }
 
