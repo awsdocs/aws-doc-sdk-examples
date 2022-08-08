09021a2efac66f5be2486b9cff2be26f6e3d8378
Steven Meyer <meyertst@amazon.com>
2022-08-08 17:12:15 -0400

    test does fail

--- cpp/example_code/s3/test.cpp
+++ cpp/example_code/s3/test.cpp
@@ -7,8 +7,10 @@
 
 // Create an Amazon S3 bucket and then delete it. 
 // Before and after creating the bucket, and then after deleting the bucket, 
-// try to determine whether that bucket still exists. 
+// try to determine whether that bucket still exists.
+
+
 int main(int argc, char* argv[]) {
-    return 0; 
+    int result = 0;   //something
+    return 0;
 }
-// snippet-end:[s3.cpp.bucket_operations.list_create_delete]
