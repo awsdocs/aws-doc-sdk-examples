71a1862a0a902e8a5b682c43a3b1ef8254300b71
Steven Meyer <meyertst@amazon.com>
2022-08-08 17:31:20 -0400

    rust change

--- rust_dev_preview/s3/src/bin/s3-helloworld.rs
+++ rust_dev_preview/s3/src/bin/s3-helloworld.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
+ * SPDX-License-Identifier: Apache-2.0. 
  */
 
 use aws_config::meta::region::RegionProviderChain;
