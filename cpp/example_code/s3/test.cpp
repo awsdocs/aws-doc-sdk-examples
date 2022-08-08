910ecf130a1c46d32c60cbc512ddd1ab1524be6c
Steven Meyer <meyertst@amazon.com>
2022-08-08 16:51:36 -0400

    test again

--- cpp/example_code/s3/test.cpp
+++ cpp/example_code/s3/test.cpp
@@ -1,20 +1,14 @@
-//snippet-sourcedescription:[s3-demo.cpp demonstrates how to perform various operations for Amazon Simple Storage Service (Amazon S3).]
-//snippet-keyword:[AWS SDK for C++]
-//snippet-keyword:[Code Sample]
-//snippet-service:[Amazon S3]
-//snippet-sourcetype:[full-example]
-//snippet-sourcedate:[12/15/2021]
-//snippet-sourceauthor:[scmacdon - aws]
 
 /*
    Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: Apache-2.0
 */
 
+
 // Create an Amazon S3 bucket and then delete it. 
 // Before and after creating the bucket, and then after deleting the bucket, 
 // try to determine whether that bucket still exists. 
 int main(int argc, char* argv[]) {
-    return 0;
+    return 0; 
 }
 // snippet-end:[s3.cpp.bucket_operations.list_create_delete]
