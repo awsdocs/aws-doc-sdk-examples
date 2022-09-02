73d3d1fd58de1030b8b5d47313914f5b75357fd2
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-09-02 10:51:56 -0400

    Create App.kt

---
+++ kotlin/usecases/creating_redshift_application/src/main/kotlin/com/aws/rest/App.kt
@@ -0,0 +1,87 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.aws.rest
+
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import java.io.IOException
+
+@SpringBootApplication
+open class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
+
+@CrossOrigin(origins = ["*"])
+@RestController
+@RequestMapping("api/")
+class MessageResource {
+    // Add a new item.
+    @RequestMapping(value = ["/add"], method = [RequestMethod.POST])
+    @ResponseBody
+    fun addItems(@RequestBody payLoad: Map<String, Any>): String = runBlocking {
+        val injectWorkService = InjectWorkService()
+        val nameVal = "user"
+        val guideVal = payLoad.get("guide").toString()
+        val descriptionVal = payLoad.get("description").toString()
+        val statusVal = payLoad.get("status").toString()
+
+        // Create a Work Item object.
+        val myWork = WorkItem()
+        myWork.guide = guideVal
+        myWork.description = descriptionVal
+        myWork.status = statusVal
+        myWork.name = nameVal
+        val id = injectWorkService.injestNewSubmission(myWork)
+        return@runBlocking "Item $id added successfully!"
+    }
+
+    // Retrieve items.
+    @GetMapping("items/{state}")
+    fun getItems(@PathVariable state: String): MutableList<WorkItem> = runBlocking {
+        val retrieveItems = RetrieveItems()
+        val list: MutableList<WorkItem>
+        if (state.compareTo("archive") == 0) {
+            list = retrieveItems.getData(1)!!
+        } else {
+            list = retrieveItems.getData(0)!!
+        }
+        return@runBlocking list
+    }
+
+    // Flip an item from Active to Archive.
+    @PutMapping("mod/{id}")
+    fun modUser(@PathVariable id: String): String = runBlocking {
+        val retrieveItems = RetrieveItems()
+        retrieveItems.flipItemArchive(id)
+        return@runBlocking id
+    }
+
+    // Send a report through Amazon SES.
+    @PutMapping("report/{email}")
+    fun sendReport(@PathVariable email: String): String = runBlocking {
+        val retrieveItems = RetrieveItems()
+        val sendMsg = SendMessage()
+        val xml= retrieveItems.getDataXML(0)
+        try {
+            sendMsg.send(email, xml)
+        } catch (e: IOException) {
+            e.stackTrace
+        }
+        return@runBlocking "Report was sent"
+    }
+}
