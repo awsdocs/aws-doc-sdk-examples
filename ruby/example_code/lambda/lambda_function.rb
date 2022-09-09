9f19ba9e775b22ff96b01f5c3fa2e6439f4678a1
ford prior <fprior@amazon.com>
2022-09-09 17:02:32 -0600

    ugga

--- ruby/example_code/lambda/lambda_function.rb
+++ ruby/example_code/lambda/lambda_function.rb
@@ -1,10 +1,8 @@
-require 'json'
-require 'aws-sdk-s3'
-
 def lambda_handler(event:, context:)
-  bucket = event["Records"][0]["s3"]["bucket"]["name"]
-  key = event["Records"][0]["s3"]["object"]["key"]
-  s3 = Aws::S3::Object.new(bucket, key)
-  type = s3.content_type
-  puts "CONTENT TYPE: #{type}"
+  # bucket = event["Records"][0]["s3"]["bucket"]["name"]
+  # key = event["Records"][0]["s3"]["object"]["key"]
+  # s3 = Aws::S3::Object.new(bucket, key)
+  # type = s3.content_type
+  # puts "CONTENT TYPE: #{type}"
+  puts "Hello world"
 end
