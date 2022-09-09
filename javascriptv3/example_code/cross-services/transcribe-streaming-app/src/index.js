bb6a9462244df96f3fd05d02552d5d6fa8fa8efd
Norbert Nemes <nnemes@amazon.com>
2022-09-09 21:02:21 +0900

    - Adds webpack 5 support - Updates dependencies to latest versions - Cleans up UI - Adds all supported languages both for streaming and translation - Fixes bug that causes the closure of the web socket in the case of an invalid audio chunk size - Makes the README a bit clearer - Refactors code to separate out concerns

--- javascriptv3/example_code/cross-services/transcribe-streaming-app/src/index.js
+++ javascriptv3/example_code/cross-services/transcribe-streaming-app/src/index.js
@@ -13,189 +13,91 @@ index.js is part of a tutorial demonstrating how to:
 */
 
 // snippet-start:[transcribe.JavaScript.streaming.indexv3]
-import { transcribeClient } from "./libs/transcribeClient.js";
-import { translateClient } from "./libs/translateClient.js";
-import { sesClient } from "./libs/sesClient.js";
-import { comprehendClient } from "./libs/comprehendClient.js";
-import { DetectDominantLanguageCommand } from "@aws-sdk/client-comprehend";
-import { StartStreamTranscriptionCommand } from "@aws-sdk/client-transcribe-streaming";
-import { TranslateTextCommand } from "@aws-sdk/client-translate";
-import { SendEmailCommand } from "@aws-sdk/client-ses";
-import MicrophoneStream from "microphone-stream";
-import getUserMedia from "get-user-media-promise";
+import * as TranscribeClient from "./libs/transcribeClient.js";
+import * as TranslateClient from "./libs/translateClient.js";
+import * as EmailClient from "./libs/emailClient.js";
 
-// Helper function to encode PCM audio.
-const pcmEncodeChunk = (chunk) => {
-  const input = MicrophoneStream.toRaw(chunk);
-  var offset = 0;
-  var buffer = new ArrayBuffer(input.length * 2);
-  var view = new DataView(buffer);
-  for (var i = 0; i < input.length; i++, offset += 2) {
-    var s = Math.max(-1, Math.min(1, input[i]));
-    view.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+window.onRecordPress = () => {
+  let recordingButton = document.getElementById("record");
+  if (recordingButton.getAttribute("class") === "recordInactive") {
+    startRecording();
+  } else {
+    stopRecording();
   }
-  return Buffer.from(buffer);
 };
 
-window.startRecord = async () => {
+const startRecording = async() => {
+  window.clearTranscription();
+  const recordingButton = document.getElementById("record");
+  const languageList = document.getElementById("inputLanguageList");
+  const selectedLanguage = languageList.value;
+  if (selectedLanguage === "nan") {
+    alert("Please select a language");
+    return;
+  }
+  languageList.disabled = true;
+  recordingButton.setAttribute("class", "recordActive");
   try {
-    console.log("Recording started");
-    var record = document.getElementById("record");
-    var stop = document.getElementById("stopRecord");
-    record.disabled = true;
-    record.style.backgroundColor = "blue";
-    stop.disabled = false;
-
-    // Start the browser microphone.
-    const micStream = new MicrophoneStream();
-    micStream.setStream(
-      await window.navigator.mediaDevices.getUserMedia({
-        video: false,
-        audio: true,
-      })
-    );
-
-    // Acquire the microphone audio stream.
-    const audioStream = async function* () {
-      for await (const chunk of micStream) {
-        yield {
-          AudioEvent: {
-            AudioChunk: pcmEncodeChunk(
-              chunk
-            ) /* pcm Encoding is optional depending on the source. */,
-          },
-        };
-      }
-    };
-
-    const command = new StartStreamTranscriptionCommand({
-      // The language code for the input audio. Valid values are en-GB, en-US, es-US, fr-CA, and fr-FR.
-      LanguageCode: "en-US",
-      // The encoding used for the input audio. The only valid value is pcm.
-      MediaEncoding: "pcm",
-      // The sample rate of the input audio in Hertz.
-      MediaSampleRateHertz: 44100,
-      AudioStream: audioStream(),
-    });
-
-    // Send the speech stream to Amazon Transcribe.
-    const data = await transcribeClient.send(command);
-    console.log("Success", data.TranscriptResultStream);
-    // return data; //For unit tests only.
-    for await (const event of data.TranscriptResultStream) {
-      for (const result of event.TranscriptEvent.Transcript.Results || []) {
-        if (result.IsPartial === false) {
-          const noOfResults = result.Alternatives[0].Items.length;
-          // Print results to browser window.
-          for (let i = 0; i < noOfResults; i++) {
-            console.log(result.Alternatives[0].Items[i].Content);
-            const outPut = result.Alternatives[0].Items[i].Content + " ";
-            const outputDiv = document.getElementById("output");
-            outputDiv.insertAdjacentHTML("beforeend", outPut);
-          }
-        }
-      }
-    }
-    console.log("Success. ", data);
-    client.destroy();
-  } catch (err) {
-    console.log("Error. ", err);
+    await TranscribeClient.startRecording(selectedLanguage, onTranscriptionDataReceived);
+  } catch(error) {
+    alert("An error occurred while recording: " + error.message);
+    stopRecording();
   }
 };
 
-window.stopRecord = function () {
-  window.location.reload();
+const onTranscriptionDataReceived = (data) => {
+  const outputDiv = document.getElementById("output");
+  outputDiv.insertAdjacentHTML("beforeend", data);
+}
+
+const stopRecording = function () {
+  let recordingButton = document.getElementById("record");
+  let languageList = document.getElementById("inputLanguageList");
+  languageList.disabled = false;
+  recordingButton.setAttribute("class", "recordInactive");
+  TranscribeClient.stopRecording();
 };
 
 window.translateText = async () => {
+  const sourceText = document.getElementById("output").innerHTML;
+  if (sourceText.length === 0) {
+    alert("No text to translate!");
+    return;
+  }
+  const targetLanguage = document.getElementById("translationLanguageList").value;
+  if (targetLanguage === "nan") {
+    alert("Please select a language to translate to!");
+    return;
+  }
   try {
-    const outPut = document.getElementById("output").innerHTML;
-    const data = await comprehendClient.send(
-      new DetectDominantLanguageCommand({ Text: outPut })
-    );
-    // return data; // Uncomment this for unit tests.
-    const langCode = data.Languages[0].LanguageCode;
-    try {
-      const selectedValue = document.getElementById("list").value;
-      const translateParams = {
-        Text: outPut,
-        SourceLanguageCode: langCode /* required */,
-        TargetLanguageCode: selectedValue /* required */,
-      };
-      const data = await translateClient.send(
-        new TranslateTextCommand(translateParams)
-      );
-      document.getElementById("translated").innerHTML = data.TranslatedText;
-    } catch (err) {
-      console.log("Error translating language. ", err);
+    let translation = await TranslateClient.translateTextToLanguage(sourceText, targetLanguage);
+    if (translation) {
+      document.getElementById("translated").innerHTML = translation;
     }
-  } catch (err) {
-    console.log("Error detecting language of text. ", err);
+  } catch (error) {
+    alert("There was an error translating the text: " + error.message);
   }
 };
 
-window.clearTranscription = async () => {
+window.clearTranscription = () => {
   document.getElementById("output").innerHTML = "";
+  document.getElementById("translated").innerHTML = "";
 };
 
-// Helper function to send an email to user.
 window.sendEmail = async () => {
-  const toEmail = document.getElementById("email").value;
-  const outputDiv = document.getElementById("output").innerHTML;
-  const translatedDiv = document.getElementById("translated").innerHTML;
-  const fromEmail = "SENDER_ADDRESS";
+  const receiver = document.getElementById("email").value;
+  if (receiver.length === 0) {
+    alert("Please enter an email address!");
+    return;
+  }
+  const originalText = document.getElementById("output").innerHTML;
+  const translatedText = document.getElementById("translated").innerHTML;
+  const sender = receiver;
   try {
-    // Set the parameters
-    const params = {
-      Destination: {
-        /* required */
-        CcAddresses: [
-          /* more items */
-        ],
-        ToAddresses: [
-          toEmail, //RECEIVER_ADDRESS
-          /* more To-email addresses */
-        ],
-      },
-      Message: {
-        /* required */
-        Body: {
-          /* required */
-          Html: {
-            Charset: "UTF-8",
-            Data:
-              "<h1>Hello!</h1><p>Here is your Amazon Transcribe recording:</p>" +
-              "<h1>Original</h1>" +
-              "<p>" +
-              outputDiv +
-              "</p>" +
-              "<h1>Translation (if available)</h1>" +
-              "<p>" +
-              translatedDiv +
-              "</p>",
-          },
-          Text: {
-            Charset: "UTF-8",
-            Data:
-              "Hello,\\r\\n" +
-              "Here is your Amazon Transcribe transcription:" +
-              "\n" +
-              outputDiv,
-          },
-        },
-        Subject: {
-          Charset: "UTF-8",
-          Data: "Your Amazon Transcribe transcription.",
-        },
-      },
-      Source: fromEmail, // Sender's email address (required)
-    };
-    const data = await sesClient.send(new SendEmailCommand(params));
-    // return data; // Uncomment this for unit tests.
-    alert("Success. Email sent.");
-  } catch (err) {
-    console.log("Error", err);
+    await EmailClient.sendEmail(sender, receiver, originalText, translatedText);
+    alert("Success! Email sent!");
+  } catch (error) {
+    alert("There was an error sending the email: " + error);
   }
 };
 // snippet-end:[transcribe.JavaScript.streaming.indexv3]
-// module.exports = {sendEmail, translateText, startRecord }
