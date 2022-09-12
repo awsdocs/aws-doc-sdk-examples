f80ed560492eccadf4de194cad503cf36ad59916
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2022-09-12 09:07:04 -0500

    Adding Transcribe examples for .NET
    Adding Transcribe examples for .NET

---
+++ dotnetv3/Transcribe/Actions/ActionExamples.cs
@@ -0,0 +1,328 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+using System.Net;
+using System.Text.Json;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.TranscribeService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace TranscribeActions;
+
+/// <summary>
+/// Amazon Transcribe action examples.
+/// </summary>
+public class ActionExamples
+{
+    public static ActionExamples CreateInstance()
+    {
+        return new ActionExamples();
+    }
+
+    private static readonly string sepBar = new('-', 80);
+    private static ILogger logger = null!;
+    private static TranscribeWrapper transcribeWrapper = null!;
+    static readonly HttpClient httpClient = new HttpClient();
+
+    // Set this value to the S3 location of a media file.
+    // A sample media file is provided in the media folder of this solution.
+    private static readonly string transcriptionMediaLocation = 
+        "https://[example-media-bucket].s3.amazonaws.com/Jabberwocky.mp3";
+    // Set this value to an S3 folder name where the output can be stored.
+    private static readonly string transcriptionMediaOutputLocation = "example-output-bucket";
+    private static readonly string customVocabularyName = "Example-jabber-vocabulary";
+
+    static async Task Main(string[] args)
+    {
+        // Set up dependency injection for the Amazon Transcribe service.
+        using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+                logging.AddFilter("System", LogLevel.Debug)
+                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Information)
+                    .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Trace))
+            .ConfigureServices((_, services) =>
+                services.AddAWSService<IAmazonTranscribeService>()
+                    .AddScoped<TranscribeWrapper>()
+            )
+            .Build();
+
+        logger = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        }).CreateLogger<ActionExamples>();
+
+        transcribeWrapper = host.Services.GetRequiredService<TranscribeWrapper>();
+
+        try
+        {
+            Console.WriteLine(sepBar);
+            Console.WriteLine("Welcome to the Amazon Transcribe examples!");
+            Console.WriteLine(sepBar);
+
+            await CreateVocabulary();
+
+            var transcriptionName = await StartTranscription();
+
+            await CleanupTranscription(transcriptionName);
+
+            var medicalTranscriptionName = await StartMedicalTranscription();
+
+            await CleanupMedicalTranscription(medicalTranscriptionName);
+
+            await CleanupVocabulary();
+
+            Console.WriteLine(sepBar);
+            Console.WriteLine("Finished running examples.");
+            Console.WriteLine(sepBar);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "There was a problem executing the examples.");
+        }
+    }
+
+    /// <summary>
+    /// Run the wrapper methods to start a transcription job and get information about the job.
+    /// </summary>
+    /// <returns>The name of the new transcription job.</returns>
+    public static async Task<string> StartTranscription()
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("Start a transcription.");
+        
+        var transcriptionName = $"ExampleTranscription_{DateTime.Now.Ticks}";
+
+        var transcriptionJob = await transcribeWrapper.StartTranscriptionJob(
+            transcriptionName, transcriptionMediaLocation, MediaFormat.Mp3,
+            LanguageCode.EnUS, null);
+
+        Console.WriteLine($"Transcription started: {transcriptionJob.TranscriptionJobName}, " +
+                          $"status: {transcriptionJob.TranscriptionJobStatus}.");
+
+        Console.WriteLine($"Waiting for transcription {transcriptionName}.");
+
+        while (transcriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.COMPLETED
+               && transcriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.FAILED)
+        {
+            transcriptionJob =
+                await transcribeWrapper.GetTranscriptionJob(transcriptionName);
+            Thread.Sleep(5000);
+        }
+
+        Console.WriteLine($"Transcription status:  {transcriptionJob.TranscriptionJobStatus}, " +
+                          $"completed at: {transcriptionJob.CompletionTime}.");
+
+        if (transcriptionJob.TranscriptionJobStatus == TranscriptionJobStatus.COMPLETED)
+        {
+            var url = transcriptionJob.Transcript.TranscriptFileUri;
+
+            var transcriptionResult = await httpClient.GetAsync(url);
+            var resultStream = await transcriptionResult.Content.ReadAsStreamAsync();
+
+            var transcriptionObject = await JsonDocument.ParseAsync(resultStream);
+            var transcriptionText = transcriptionObject.RootElement
+                .GetProperty("results").GetProperty("transcripts")[0]
+                .GetProperty("transcript");
+
+            Console.WriteLine(sepBar);
+            Console.WriteLine($"Transcript:");
+            Console.WriteLine(transcriptionText);
+        }
+
+        Console.WriteLine(sepBar);
+        return transcriptionName;
+    }
+
+    /// <summary>
+    /// Run the wrapper methods to list jobs and delete a transcription job.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    public static async Task CleanupTranscription(string transcriptionName)
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("List the transcriptions for the current account.");
+
+        var transcriptionJobs = await transcribeWrapper.ListTranscriptionJobs();
+
+        transcriptionJobs.ForEach(j =>
+            Console.WriteLine($"Transcription job {j.TranscriptionJobName}" +
+                              $"status {j.TranscriptionJobStatus}, created on " +
+                              $"{j.CreationTime.ToShortDateString()} at " +
+                              $"{j.CreationTime.ToShortTimeString()}."));
+
+        Console.WriteLine("Delete a transcription job.");
+
+        var success = await transcribeWrapper.DeleteTranscriptionJob(transcriptionName);
+
+        Console.WriteLine(success ? $"Transcription {transcriptionName} deleted." 
+            : $"Unable to delete transcription {transcriptionName}.");
+        Console.WriteLine(sepBar);
+    }
+
+    /// <summary>
+    /// Run the wrapper methods to create and update a custom vocabulary.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    public static async Task CreateVocabulary()
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("Create a custom vocabulary to try to improve transcription accuracy.");
+
+        var phrases = new List<string>
+        {
+            "brillig", "slithy", "borogoves", "mome", "raths", "Jub-Jub", "frumious",
+            "manxome", "Tumtum", "uffish", "whiffling", "tulgey", "thou", "frabjous",
+            "callooh","callay"
+        };
+
+        await transcribeWrapper.CreateCustomVocabulary(LanguageCode.EnUS,
+            phrases, customVocabularyName);
+
+        Console.WriteLine("Get the custom vocabulary state and wait until it is ready.");
+        VocabularyState vocabularyState = null!;
+
+        while (vocabularyState != VocabularyState.FAILED && vocabularyState != VocabularyState.READY)
+        {
+            vocabularyState = await transcribeWrapper.GetCustomVocabulary(customVocabularyName);
+            Thread.Sleep(5000);
+        }
+
+        Console.WriteLine("Update the vocabulary.");
+
+        // add a phrase to the list
+        phrases.Add("chortled");
+
+        vocabularyState = await transcribeWrapper.UpdateCustomVocabulary(LanguageCode.EnUS,
+            phrases, customVocabularyName);
+
+        while (vocabularyState != VocabularyState.PENDING)
+        {
+            vocabularyState = await transcribeWrapper.GetCustomVocabulary(customVocabularyName);
+            Thread.Sleep(5000);
+        }
+
+        Console.WriteLine($"Transcription vocabulary status: {vocabularyState}.");
+        Console.WriteLine(sepBar);
+    }
+
+    /// <summary>
+    /// Run the wrapper methods to list and delete a vocabulary.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    public static async Task CleanupVocabulary()
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("List the vocabularies for the current account.");
+
+        var vocabularies = await transcribeWrapper.ListCustomVocabularies();
+
+        vocabularies.ForEach(v =>
+            Console.WriteLine($"Vocabulary {v.VocabularyName}" +
+                              $"status {v.VocabularyState}, last modified on " +
+                              $"{v.LastModifiedTime} at {v.LastModifiedTime.ToShortTimeString()}."));
+
+        Console.WriteLine("Delete the custom vocabulary.");
+
+        var success = await transcribeWrapper.DeleteCustomVocabulary(customVocabularyName);
+
+        Console.WriteLine(success ? $"Custom vocabulary {customVocabularyName} deleted."
+            : $"Unable to delete vocabulary {customVocabularyName}");
+
+        Console.WriteLine(sepBar);
+    }
+
+    /// <summary>
+    /// Run the wrapper methods to start a medical transcription job and get information about the job.
+    /// </summary>
+    /// <returns>The name of the new medical transcription job.</returns>
+    public static async Task<string> StartMedicalTranscription()
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("Start a medical transcription.");
+
+        var medicalTranscriptionName = $"ExampleMedicalTranscription_{DateTime.Now.Ticks}";
+
+        var medicalTranscriptionJob = await transcribeWrapper.StartMedicalTranscriptionJob(
+            medicalTranscriptionName, transcriptionMediaLocation, MediaFormat.Mp3,
+            transcriptionMediaOutputLocation, Amazon.TranscribeService.Type.DICTATION);
+
+        Console.WriteLine($"Medical transcription started: {medicalTranscriptionJob}, " +
+                          $"status: {medicalTranscriptionJob.TranscriptionJobStatus}.");
+
+        Console.WriteLine($"Waiting for medical transcription {medicalTranscriptionName}.");
+
+        while (medicalTranscriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.COMPLETED
+               && medicalTranscriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.FAILED)
+        {
+            medicalTranscriptionJob =
+                await transcribeWrapper.GetMedicalTranscriptionJob(medicalTranscriptionName);
+            Thread.Sleep(5000);
+        }
+
+        Console.WriteLine($"Transcription status:  {medicalTranscriptionJob.TranscriptionJobStatus}, " +
+                          $"completed at: {medicalTranscriptionJob.CompletionTime}.");
+
+        if (medicalTranscriptionJob.TranscriptionJobStatus == TranscriptionJobStatus.COMPLETED)
+        {
+            Console.WriteLine(sepBar);
+            Console.WriteLine($"Transcription result url:");
+            Console.WriteLine(medicalTranscriptionJob.Transcript.TranscriptFileUri);
+
+            var s3client = new AmazonS3Client();
+            var output = await s3client.GetObjectAsync(new GetObjectRequest()
+            {
+                BucketName = transcriptionMediaOutputLocation,
+                Key = medicalTranscriptionName
+            });
+
+            if (output.HttpStatusCode == HttpStatusCode.OK)
+            {
+                var transcriptionObject =
+                    await JsonDocument.ParseAsync(output.ResponseStream);
+                var transcriptionText = transcriptionObject.RootElement
+                    .GetProperty("results").GetProperty("transcripts")[0]
+                    .GetProperty("transcript");
+
+                Console.WriteLine(sepBar);
+                Console.WriteLine($"Medical transcript:");
+                Console.WriteLine(transcriptionText);
+            }
+        }
+
+        Console.WriteLine(sepBar);
+        return medicalTranscriptionName;
+    }
+
+
+    /// <summary>
+    /// Run the wrapper methods to list medical transcription jobs and delete one job.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    public static async Task CleanupMedicalTranscription(string medicalTranscriptionName)
+    {
+        Console.WriteLine(sepBar);
+        Console.WriteLine("List the medical transcriptions for the current account.");
+
+        var transcriptionJobs = await transcribeWrapper.ListMedicalTranscriptionJobs();
+
+        transcriptionJobs.ForEach(j =>
+            Console.WriteLine($"Medical transcription job {j.MedicalTranscriptionJobName}" +
+                              $"status {j.TranscriptionJobStatus}, created on " +
+                              $"{j.CreationTime.ToShortDateString()} at " +
+                              $"{j.CreationTime.ToShortTimeString()}."));
+
+        Console.WriteLine("Delete a medical transcription job.");
+
+        var success = await transcribeWrapper.DeleteMedicalTranscriptionJob(medicalTranscriptionName);
+
+        Console.WriteLine(success ? $"Medical transcription {medicalTranscriptionName} deleted."
+            : $"Unable to delete transcription {medicalTranscriptionName}.");
+
+        Console.WriteLine(sepBar);
+    }
+}
---
+++ dotnetv3/Transcribe/Tests/TranscribeExampleTests.cs
@@ -0,0 +1,304 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+using Amazon.TranscribeService;
+using Microsoft.Extensions.Configuration;
+using TranscribeActions;
+
+namespace TranscribeTests;
+
+public class TranscribeExampleTests
+{
+    private readonly IConfiguration _configuration;
+    private readonly TranscribeWrapper _wrapper;
+
+    public TranscribeExampleTests()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("testsettings.json") // Load test settings from json file.
+            .AddJsonFile("testsettings.local.json",
+                true) // Optionally load local settings.
+            .Build();
+
+        _wrapper = new TranscribeWrapper(new AmazonTranscribeServiceClient());
+    }
+
+    /// <summary>
+    /// Create a valid vocabulary. Should not fail.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(1)]
+    public async Task VerifyCreateVocabulary_ValidName_ShouldSucceed()
+    {
+        var vocabularyName = _configuration["customVocabularyName"];
+        var phrases = new List<string> { "testPhrase" };
+        var vocabularyState = await _wrapper.CreateCustomVocabulary(LanguageCode.EnUS, phrases, vocabularyName);
+
+        while (vocabularyState != VocabularyState.FAILED && vocabularyState != VocabularyState.READY)
+        {
+            Thread.Sleep(5000);
+            vocabularyState = await _wrapper.GetCustomVocabulary(vocabularyName);
+        }
+
+        Assert.NotEqual(VocabularyState.FAILED, vocabularyState);
+    }
+
+    /// <summary>
+    /// Create an invalid vocabulary. Should throw an exception for a bad request.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(2)]
+    public async Task VerifyCreateVocabulary_InValidName_ShouldThrowException()
+    {
+        var vocabularyName = " ";
+        var phrases = new List<string> { "testPhrase" };
+
+        await Assert.ThrowsAsync<Amazon.TranscribeService.Model.BadRequestException>(async () =>
+        {
+            await _wrapper.CreateCustomVocabulary(LanguageCode.EnUS, phrases,
+                vocabularyName);
+        });
+    }
+
+    /// <summary>
+    /// Get an existing vocabulary. Should not fail.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(3)]
+    public async Task GetVocabulary_ValidName_ShouldSucceed()
+    {
+        var vocabularyName = _configuration["customVocabularyName"];
+
+        var vocabularyState = await _wrapper.GetCustomVocabulary(vocabularyName);
+
+        Assert.Contains(vocabularyState, new List<VocabularyState>()
+             {
+                 VocabularyState.FAILED,
+                 VocabularyState.PENDING,
+                 VocabularyState.READY
+             });
+    }
+
+    /// <summary>
+    /// Try to get a vocabulary with the wrong name. Should fail with an exception for a bad name.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(4)]
+    public async Task GetVocabulary_WrongName_ShouldThrowException()
+    {
+        await Assert.ThrowsAsync<Amazon.TranscribeService.Model.BadRequestException>(async () =>
+        {
+            await _wrapper.GetCustomVocabulary("wrongName");
+        });
+    }
+
+    /// <summary>
+    /// Update a custom vocabulary.Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(5)]
+    public async Task UpdateVocabulary_Should_Succeed()
+    {
+        var vocabularyName = _configuration["customVocabularyName"];
+        var phrases = new List<string> { "testPhraseUpdate" };
+        var vocabularyState = await _wrapper.UpdateCustomVocabulary(LanguageCode.EnUS, phrases, vocabularyName);
+
+        while (vocabularyState != VocabularyState.FAILED && vocabularyState != VocabularyState.READY)
+        {
+            vocabularyState = await _wrapper.GetCustomVocabulary(vocabularyName);
+            Thread.Sleep(5000);
+        }
+
+        Assert.NotEqual(VocabularyState.FAILED, vocabularyState);
+    }
+
+    /// <summary>
+    /// Update a custom vocabulary with the wrong name. Should fail with an exception.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(6)]
+    public async Task UpdateVocabulary_WrongName_Should_ThrowException()
+    {
+        var phrases = new List<string> { "testPhraseUpdate" };
+
+        await Assert.ThrowsAsync<Amazon.TranscribeService.Model.BadRequestException>(async () =>
+        {
+            await _wrapper.UpdateCustomVocabulary(LanguageCode.EnUS, phrases, "wrongName");
+        });
+    }
+
+    /// <summary>
+    /// List vocabularies. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(7)]
+    public async Task ListVocabulary_ShouldSucceed()
+    {
+        var result = await _wrapper.ListCustomVocabularies();
+        Assert.NotEmpty(result);
+    }
+
+    /// <summary>
+    /// Delete a custom vocabulary. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(8)]
+    public async Task DeleteVocabulary_Should_Succeed()
+    {
+        var vocabularyName = _configuration["customVocabularyName"];
+        var result = await _wrapper.DeleteCustomVocabulary(vocabularyName);
+        Assert.True(result);
+    }
+
+    /// <summary>
+    /// Create a valid transcription job. Should not fail.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(9)]
+    public async Task VerifyCreateTranscriptionJob_ValidMedia_ShouldSucceed()
+    {
+        var mediaLocation = _configuration["transcriptionMediaLocation"];
+        var transcriptionJobName = _configuration["transcriptionJobName"];
+
+        var transcriptionJob = await _wrapper.StartTranscriptionJob(
+            transcriptionJobName, mediaLocation, MediaFormat.Mp3,
+            LanguageCode.EnUS, null);
+
+        while (transcriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.COMPLETED
+               && transcriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.FAILED)
+        {
+            transcriptionJob =
+                await _wrapper.GetTranscriptionJob(transcriptionJobName);
+            Thread.Sleep(5000);
+        }
+
+        Assert.Equal(TranscriptionJobStatus.COMPLETED, transcriptionJob.TranscriptionJobStatus);
+    }
+
+    /// <summary>
+    /// Create an invalid vocabulary. Should throw an exception for a bad request.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(10)]
+    public async Task VerifyCreateTranscriptionJob_MissingMedia_ShouldFail()
+    {
+        var mediaLocation = _configuration["transcriptionMediaLocation"];
+        var transcriptionJobName = _configuration["transcriptionJobName"];
+
+        await Assert.ThrowsAsync<Amazon.TranscribeService.Model.ConflictException>(async () =>
+        {
+            var transcriptionJob = await _wrapper.StartTranscriptionJob(
+                transcriptionJobName, mediaLocation, MediaFormat.Mp3,
+                LanguageCode.EnUS, null);
+
+        });
+    }
+
+    /// <summary>
+    /// List Transcription jobs. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(11)]
+    public async Task ListTranscriptionJobs_Should_Succeed()
+    {
+        var result = await _wrapper.ListTranscriptionJobs();
+        Assert.NotEmpty(result);
+    }
+
+    /// <summary>
+    /// Delete a transcription job. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(11)]
+    public async Task DeleteTranscriptionJob_Should_Succeed()
+    {
+        var transcriptionJobName = _configuration["transcriptionJobName"];
+        var result = await _wrapper.DeleteTranscriptionJob(transcriptionJobName);
+        Assert.True(result);
+    }
+
+    /// <summary>
+    /// Create a valid medical transcription job. Should not fail.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(12)]
+    public async Task VerifyCreateMedicalTranscriptionJob_ValidMedia_ShouldSucceed()
+    {
+        var mediaLocation = _configuration["transcriptionMediaLocation"];
+        var medicalTranscriptionJobName = _configuration["medicalTranscriptionJobName"];
+        var outputLocation = _configuration["outputLocation"];
+
+        var medicalTranscriptionJob = await _wrapper.StartMedicalTranscriptionJob(
+            medicalTranscriptionJobName, mediaLocation, MediaFormat.Mp3,
+            outputLocation, Amazon.TranscribeService.Type.DICTATION);
+
+        while (medicalTranscriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.COMPLETED
+               && medicalTranscriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.FAILED)
+        {
+            medicalTranscriptionJob =
+                await _wrapper.GetMedicalTranscriptionJob(medicalTranscriptionJobName);
+            Thread.Sleep(5000);
+        }
+
+        Assert.Equal(TranscriptionJobStatus.COMPLETED, medicalTranscriptionJob.TranscriptionJobStatus);
+    }
+
+    /// <summary>
+    /// Create an duplicate medical transcription job. Should throw an exception for a conflict.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(13)]
+    public async Task VerifyCreateMedicalTranscriptionJob_DuplicateName_ShouldFail()
+    {
+        var mediaLocation = _configuration["transcriptionMediaLocation"];
+        var medicalTranscriptionJobName = _configuration["medicalTranscriptionJobName"];
+        var outputLocation = _configuration["outputLocation"];
+
+        await Assert.ThrowsAsync<Amazon.TranscribeService.Model.ConflictException>(async () =>
+        {
+            var medicalTranscriptionJob = await _wrapper.StartMedicalTranscriptionJob(
+                medicalTranscriptionJobName, mediaLocation, MediaFormat.Mp3,
+                outputLocation, Amazon.TranscribeService.Type.DICTATION);
+        });
+    }
+
+    /// <summary>
+    /// List Medical Transcription jobs. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(14)]
+    public async Task ListMedicalTranscriptionJobs_Should_Succeed()
+    {
+        var result = await _wrapper.ListMedicalTranscriptionJobs();
+        Assert.NotEmpty(result);
+    }
+
+    /// <summary>
+    /// Delete a medical transcription job. Should succeed.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(15)]
+    public async Task DeleteMedicalTranscriptionJob_Should_Succeed()
+    {
+        var medicalTranscriptionJobName = _configuration["medicalTranscriptionJobName"];
+        var result = await _wrapper.DeleteMedicalTranscriptionJob(medicalTranscriptionJobName);
+        Assert.True(result);
+    }
+}
