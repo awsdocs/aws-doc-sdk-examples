3226de0eee1e8d4433511324e122cf4423ee625d
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2022-09-13 10:03:11 -0400

    Fixed another snippet-tag typo.

--- dotnetv3/Cognito/scenarios/Cognito_Basics/Cognito_MVP/GlobalUsings.cs
+++ dotnetv3/Cognito/scenarios/Cognito_Basics/Cognito_MVP/GlobalUsings.cs
@@ -1,4 +1,7 @@
-﻿// snippet-start:[dotnetv3.Cognito_MVP.Usings]
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+// snippet-start:[dotnetv3.Cognito_MVP.Usings]
 global using Amazon;
 global using Amazon.CognitoIdentityProvider;
 global using Amazon.CognitoIdentityProvider.Model;
