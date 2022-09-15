5bb68b1fc3d6f98339da8abe4113c060560f78da
Steven Meyer <meyertst@amazon.com>
2022-09-15 17:16:42 -0400

    clang-tidy changes

--- cpp/example_code/transcribe/get_transcript.cpp
+++ cpp/example_code/transcribe/get_transcript.cpp
@@ -14,13 +14,14 @@
 #include <cstdio>
 #include <chrono>
 #include <thread>
+#include <array>
 
 using namespace Aws;
 using namespace Aws::TranscribeStreamingService;
 using namespace Aws::TranscribeStreamingService::Model;
 
-//TODO: Update path to location of local .wav test file.
-static const char FILE_NAME[] = "../transcribe-test-file.wav";
+//TODO(User): Update path to location of local .wav test file.
+static const Aws::String FILE_NAME{"../transcribe-test-file.wav"};
 
 int main()
 {
@@ -28,7 +29,7 @@ int main()
 	options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Trace;
 	Aws::InitAPI(options);
 	{
-		//TODO: Set to the region of your AWS account.
+		//TODO(User): Set to the region of your AWS account.
 		const Aws::String region = Aws::Region::US_WEST_2;
 
 		Aws::Utils::Threading::Semaphore received(0 /*initialCount*/, 1 /*maxCount*/);
@@ -48,19 +49,19 @@ int main()
 		TranscribeStreamingServiceClient client(config);
 		StartStreamTranscriptionHandler handler;
 		handler.SetOnErrorCallback([](const Aws::Client::AWSError<TranscribeStreamingServiceErrors>& error) {
-			printf("ERROR: %s", error.GetMessage().c_str());
+            std::cerr << "ERROR: " + error.GetMessage() << std::endl;
 			});
 		//SetTranscriptEventCallback called for every 'chunk' of file transcripted. Partial results are returned in real time.
 		handler.SetTranscriptEventCallback([&received](const TranscriptEvent& ev) {
 			for (auto&& r : ev.GetTranscript().GetResults()) {
 				if (r.GetIsPartial()) {
-					printf("[partial] ");
+                    std::cout << "[partial] ";
 				}
 				else {
-					printf("[Final] ");
+                    std::cout << "[Final] ";
 				}
 				for (auto&& alt : r.GetAlternatives()) {
-					printf("%s\n", alt.GetTranscript().c_str());
+                    std::cout << alt.GetTranscript() << std::endl;
 				}
 			}
 			received.Release();
@@ -76,22 +77,22 @@ int main()
 		{
 			Aws::FStream file(FILE_NAME, std::ios_base::in | std::ios_base::binary);
 			if (!file.is_open()) {
-				std::cout << "Failed to open " << FILE_NAME << '\n';
+				std::cerr << "Failed to open " << FILE_NAME << '\n';
 			}
-			char buf[1024];
+			std::array<char,1024> buf;
 			int i = 0;
 			while (file)
 			{
-				file.read(buf, sizeof(buf));
+				file.read(buf.begin(), buf.size());
 
 				if (!file)
-					std::cout << "File: only " << file.gcount() << " could be read";
+					std::cout << "File: only " << file.gcount() << " could be read" << std::endl;
 
-				Aws::Vector<unsigned char> bits{ buf, buf + file.gcount() };
+				Aws::Vector<unsigned char> bits{ buf.begin(), buf.begin() + file.gcount() };
 				AudioEvent event(std::move(bits));
 				if (!stream)
 				{
-					printf("Failed to create a stream\n");
+                    std::cerr << "Failed to create a stream" << std::endl;
 					break;
 				}
 				//The std::basic_istream::gcount() is used to count the characters in the given string. It returns
@@ -99,8 +100,8 @@ int main()
 				if (file.gcount() > 0) {
 					if (!stream.WriteAudioEvent(event))
 					{
-						printf("Failed to write an audio event\n");
-						break;
+                        std::cerr << "Failed to write an audio event" << std::endl;
+ 						break;
 					}
 				}
 				else {
@@ -109,28 +110,28 @@ int main()
 				std::this_thread::sleep_for(std::chrono::milliseconds(25)); // Slow down since we are streaming from a file.
 			}
 			if (!stream.WriteAudioEvent(AudioEvent())) { // Per the spec, we have to send an empty event (i.e. without a payload) at the end.
-				printf("Failed to send an empty frame\n");
+				std::cerr << "Failed to send an empty frame" << std::endl;
 			}
 			else {
-				printf("Successfully sent the empty frame\n");
+                std::cout << "Successfully sent the empty frame" << std::endl;
 			}
 			stream.flush();
-			std::this_thread::sleep_for(std::chrono::milliseconds(10000)); // Workaround to prevent an error at the end of the stream for this contrived example.
+			std::this_thread::sleep_for(std::chrono::seconds (10)); // Workaround to prevent an error at the end of the stream for this contrived example.
 			stream.Close();
 		};
 
 		Aws::Utils::Threading::Semaphore signaling(0 /*initialCount*/, 1 /*maxCount*/);
-		auto OnResponseCallback = [&signaling](const TranscribeStreamingServiceClient*,
-			const Model::StartStreamTranscriptionRequest&,
-			const Model::StartStreamTranscriptionOutcome&,
-			const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
+		auto OnResponseCallback = [&signaling](const TranscribeStreamingServiceClient* /*unused*/,
+			const Model::StartStreamTranscriptionRequest& /*unused*/,
+			const Model::StartStreamTranscriptionOutcome& /*unused*/,
+			const std::shared_ptr<const Aws::Client::AsyncCallerContext>& /*unused*/) {
 				signaling.Release();
 		};
 
-		printf("Starting...\n");
+        std::cout << "Starting..." << std::endl;
 		client.StartStreamTranscriptionAsync(request, OnStreamReady, OnResponseCallback, nullptr /*context*/);
 		signaling.WaitOne(); // Prevent the application from exiting until we're done.
-		printf("Done\n");
+        std::cout << "Done" << std::endl;
 	}
 
 	Aws::ShutdownAPI(options);
