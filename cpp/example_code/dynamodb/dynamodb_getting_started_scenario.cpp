ad04cc845792190ebaa1f1fd5a55a61b7812c6cf
Steven Meyer <meyertst@amazon.com>
2022-09-20 09:09:18 -0400

    further scenario implementation

--- cpp/example_code/dynamodb/dynamodb_getting_started_scenario.cpp
+++ cpp/example_code/dynamodb/dynamodb_getting_started_scenario.cpp
@@ -10,21 +10,24 @@
 #include <aws/dynamodb/DynamoDBClient.h>
 #include <aws/dynamodb/model/CreateTableRequest.h>
 #include <aws/dynamodb/model/DeleteTableRequest.h>
+#include <aws/dynamodb/model/DescribeTableRequest.h>
 
 namespace AwsDoc {
     namespace DynamoDB {
         static const Aws::String MOVIE_TABLE_NAME("doc-example-table-movies");
         static const int PROVISIONED_THROUGHPUT_UNITS = 10;
 
-        static bool deleteDynamoTable(const Aws::String& tableName,
-                                      const Aws::Client::ClientConfiguration& clientConfiguration);
+        static bool deleteDynamoTable(const Aws::String &tableName,
+                                      const Aws::Client::ClientConfiguration &clientConfiguration);
 
-        static Aws::String askQuestion(const Aws::String& string);
+        static Aws::String askQuestion(const Aws::String &string);
+
+        static bool waitTableActive(const Aws::String &tableName,
+                                         const Aws::Client::ClientConfiguration &clientConfiguration);
     } //  namespace DynamoDB
 } // namespace AwsDoc
 
-bool AwsDoc::DynamoDB::dynamodbGettingStartedScenario(const Aws::Client::ClientConfiguration& clientConfiguration)
-{
+bool AwsDoc::DynamoDB::dynamodbGettingStartedScenario(const Aws::Client::ClientConfiguration &clientConfiguration) {
     std::cout << std::setfill('*') << std::setw(88) << " " << std::endl;
     std::cout << "Welcome to the Amazon DynamoDB getting started demo." << std::endl;
     std::cout << std::setfill('*') << std::setw(88) << " " << std::endl;
@@ -56,58 +59,60 @@ bool AwsDoc::DynamoDB::dynamodbGettingStartedScenario(const Aws::Client::ClientC
     request.SetProvisionedThroughput(throughput);
     request.SetTableName(MOVIE_TABLE_NAME);
 
-    const Aws::DynamoDB::Model::CreateTableOutcome& result = dynamoClient.CreateTable(request);
-    if (result.IsSuccess())
-    {
-        std::cout << "Table \"" << result.GetResult().GetTableDescription().GetTableName() <<
-                  " created!" << std::endl;
+    std::cout << "Creating table '" << MOVIE_TABLE_NAME << "'..." << std::endl;
+    const Aws::DynamoDB::Model::CreateTableOutcome &result = dynamoClient.CreateTable(request);
+    if (!result.IsSuccess()) {
+        if (result.GetError().GetErrorType() == Aws::DynamoDB::DynamoDBErrors::RESOURCE_IN_USE)
+        {
+            std::cout << "Table already exists." << std::endl;
+        }
+        else
+        {
+            std::cerr << "Failed to create table: " << result.GetError().GetMessage();
+            return false;
+        }
     }
-    else
+    std::cout << "Waiting for table '" << MOVIE_TABLE_NAME << "' to become active...." << std::endl;
+    if (!AwsDoc::DynamoDB::waitTableActive(MOVIE_TABLE_NAME, clientConfiguration))
     {
-        std::cerr << "Failed to create table: " << result.GetError().GetMessage();
+        deleteDynamoTable(MOVIE_TABLE_NAME, clientConfiguration);
     }
 
+    std::cout << "Table '" << MOVIE_TABLE_NAME << "' created and active." << std::endl;
+
     Aws::String title = askQuestion("Enter the title of a movie you want to add to the table: ");
-    if (title.empty())
-    {
+    if (title.empty()) {
         deleteDynamoTable(MOVIE_TABLE_NAME, clientConfiguration);
         return false;
     }
 
-
-
-    return deleteDynamoTable(MOVIE_TABLE_NAME, clientConfiguration);;
+    return deleteDynamoTable(MOVIE_TABLE_NAME, clientConfiguration);
 }
 
-bool AwsDoc::DynamoDB::deleteDynamoTable(const Aws::String& tableName,
-                                         const Aws::Client::ClientConfiguration& clientConfiguration)
-{
+bool AwsDoc::DynamoDB::deleteDynamoTable(const Aws::String &tableName,
+                                         const Aws::Client::ClientConfiguration &clientConfiguration) {
     Aws::DynamoDB::DynamoDBClient dynamoClient(clientConfiguration);
 
     Aws::DynamoDB::Model::DeleteTableRequest request;
     request.SetTableName(tableName);
 
-    const Aws::DynamoDB::Model::DeleteTableOutcome& result = dynamoClient.DeleteTable(request);
-    if (result.IsSuccess())
-    {
+    const Aws::DynamoDB::Model::DeleteTableOutcome &result = dynamoClient.DeleteTable(request);
+    if (result.IsSuccess()) {
         std::cout << "Your Table \"" << result.GetResult().GetTableDescription().GetTableName() << " was deleted!\n";
     }
-    else
-    {
+    else {
         std::cerr << "Failed to delete table: " << result.GetError().GetMessage();
     }
 
     return result.IsSuccess();
 }
 
-Aws::String AwsDoc::DynamoDB::askQuestion(const Aws::String& string)
-{
+Aws::String AwsDoc::DynamoDB::askQuestion(const Aws::String &string) {
     Aws::String result;
     do {
         std::cout << string << std::endl;
         std::cin >> result;
-        if (result.empty())
-        {
+        if (result.empty()) {
             std::cout << "Please enter some text." << std::endl;
         }
     } while (result.empty());
@@ -115,17 +120,46 @@ Aws::String AwsDoc::DynamoDB::askQuestion(const Aws::String& string)
     return result;
 }
 
-#ifndef TESTING_BUILD
-int main(int argc, char** argv)
-{
-    Aws::SDKOptions options;
-    InitAPI(options);
+bool AwsDoc::DynamoDB::waitTableActive(const Aws::String &tableName,
+                                            const Aws::Client::ClientConfiguration &clientConfiguration) {
+    Aws::DynamoDB::DynamoDBClient client(clientConfiguration);
+    // Repeatedly call DescribeTable until table is ACTIVE
+    Aws::DynamoDB::Model::DescribeTableRequest request;
+    request.SetTableName(tableName);
 
-    {
-        Aws::Client::ClientConfiguration clientConfig;
-        AwsDoc::DynamoDB::dynamodbGettingStartedScenario(clientConfig);
+    int count = 0;
+    while (count < 20) {
+        const Aws::DynamoDB::Model::DescribeTableOutcome &result = client.DescribeTable(request);
+        if (!result.IsSuccess()) {
+            std::cerr << "Error DynamoDB::waitTableActiveconst "
+                      << result.GetError().GetMessage() << std::endl;
+            return false;
+        }
+        else {
+            Aws::DynamoDB::Model::TableStatus status = result.GetResult().GetTable().GetTableStatus();
+
+            if (Aws::DynamoDB::Model::TableStatus::ACTIVE == status) {
+                return true;
+            }
+            else {
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+        }
+        count++;
     }
-   return 0;
+    return false;
 }
+
+#ifndef TESTING_BUILD
+    int main(int argc, char **argv) {
+        Aws::SDKOptions options;
+        InitAPI(options);
+
+        {
+            Aws::Client::ClientConfiguration clientConfig;
+            AwsDoc::DynamoDB::dynamodbGettingStartedScenario(clientConfig);
+        }
+        return 0;
+    }
 
 #endif // TESTING_BUILD
