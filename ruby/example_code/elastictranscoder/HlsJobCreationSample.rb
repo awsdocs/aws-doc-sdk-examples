40466c48e0daa6a951dd20989fe544b77e7ca5f6
ford prior <fprior@amazon.com>
2022-09-20 17:48:02 -0600

    fixing transcoder examples

--- ruby/example_code/elastictranscoder/HlsJobCreationSample.rb
+++ ruby/example_code/elastictranscoder/HlsJobCreationSample.rb
@@ -2,32 +2,31 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Purpose:
-# eb_list_stacks.rb demonstrates how to create an HLS job using the AWS SDK for Ruby.
-
+# Demonstrates how to create an HLS job using the AWS SDK for Ruby.
 
 # snippet-start:[elastictranscoder.ruby.create_hls_job.import]
 require 'aws-sdk-elastictranscoder'
+require 'aws-sdk-s3'
 
-client = Aws::ElasticTranscoder::Client.new
+transcoder_client = Aws::ElasticTranscoder::Client.new
+transcoder_pipeline = "transcoder-pipeline-#{rand(10**4)}"
 
-# resp = client.test_role({
-#                           role: "arn:aws:iam:260778392212::role/Elastic_Transcoder_Default_Role", # required
-#                           input_bucket: "example-code-bucket-171", # required
-#                           output_bucket: "example-code-bucket-171", # required
-#                           topics: ["arn:aws:sns:260778392212::aws-doc-sdk-examples-topic"] # required
-#                         })
+# create bucket for input and output
+s3_resource = Aws::S3::Resource.new
+bucket_name = "transcoder-bucket-#{rand(10**4)}"
+s3_resource.create_bucket(bucket: bucket_name)
 
-resp = client.create_pipeline({
-                                name: "test1", # required
-                                input_bucket: "example-code-bucket-171", # required
-                                output_bucket: "example-code-bucket-171",
+resp = transcoder_client.create_pipeline({
+                                name: transcoder_pipeline, # required
+                                input_bucket: bucket_name, # required
+                                output_bucket: bucket_name,
                                 role: "arn:aws:iam::260778392212:role/Elastic_Transcoder_Default_Role" # required
                               })
 
 pipeline_id = resp["pipeline"]["id"]
 
 # This is the name of the input key that you would like to transcode.
-input_key = 'movie.mp4'
+input_key = 'Jabberwocky.mp3'
 
 # HLS Presets that will be used to create an adaptive bitrate playlist.
 hls_64k_audio_preset_id = '1351620000001-200071'
@@ -37,20 +36,13 @@ hls_1000k_preset_id     = '1351620000001-200030'
 hls_1500k_preset_id     = '1351620000001-200020'
 hls_2000k_preset_id     = '1351620000001-200010'
 
-# HLS Segment duration that will be targeted.
-segment_duration = '2'
+# Additional variables
+segment_duration        = '2'
+output_key_prefix       = 'elastic-transcoder-samples/output/hls/'
 
-# All outputs will have this prefix prepended to their output key.
-output_key_prefix = 'elastic-transcoder-samples/output/hls/'
-
-# Create the client for Elastic Transcoder.
-transcoder_client = Aws::ElasticTranscoder::Client.new
-
-# Setup the job input using the provided input key.
 input = { key: input_key }
+output_key = OpenSSL::Digest::SHA256.new(input_key.encode('UTF-8')).to_s
 
-# Setup the job outputs using the HLS presets.
-output_key = OpenSSL::Digest::SHA256.new(input_key.encode('UTF-8'))
 
 hls_audio = {
   key: 'hlsAudio/' + output_key,
