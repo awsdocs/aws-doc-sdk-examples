a7a035f1255b235b453264f25553417ddd91fae3
Laren-AWS <larencra@amazon.com>
2022-09-26 15:07:38 -0700

    Fix typo.

--- python/example_code/aurora/scenario_get_started_aurora.py
+++ python/example_code/aurora/scenario_get_started_aurora.py
@@ -1,5 +1,4 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 """
