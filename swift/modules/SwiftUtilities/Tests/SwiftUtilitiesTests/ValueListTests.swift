694355f48e4d76349ecb4ad80eeed815bf9ffd39
Eric Shepherd <ericsh@amazon.com>
2022-09-27 12:44:39 -0400

    Clean up tests for ValueList class

--- swift/modules/SwiftUtilities/Tests/SwiftUtilitiesTests/ValueListTests.swift
+++ swift/modules/SwiftUtilities/Tests/SwiftUtilitiesTests/ValueListTests.swift
@@ -6,65 +6,61 @@ import XCTest
 
 final class ValueListTests: XCTestCase {
     let testHeader = "Table of random nonsense"
-    
+    let dividerNoContent = "========================"
+    let dividerWithContent = "==========================================="
+
+    /// List of each row's title strings.
+    let rowTitles = [
+        "King of Rock and Roll",
+        "Life, the Universe, and everything",
+        "Earth is round"
+    ]
+
+    /// List of each row's value strings.
+    let rowValues = [
+        "Elvis",
+        "42",
+        "Yes"
+    ]
+
+    /// Each complete row in text format.
+    let textRows = [
+        "King of Rock and Roll                 Elvis",
+        "Life, the Universe, and everything    42",
+        "Earth is round                        Yes"
+    ]
+
     /***** UTILITY FUNCTIONS *****/
 
-    /// Create a test list using the `ValueList` type.
+    /// Concatenate the specified strings with newlines separating them.
     ///
-    /// - Parameter withHeader: Whether or not the list should have a header.
-    ///   Default: `false`.
-    /// - Returns: A `ValueList` with the test list's contents.
-    func createTestList(withHeader: Bool = false) -> ValueList {
-        let vl: ValueList
-        
-        if withHeader == false {
-            vl = ValueList()
-        } else {
-            vl = ValueList(header: self.testHeader)
-        }
+    /// - Parameter strings: One or more strings to concatenate with newlines
+    ///   after them.
+    ///
+    /// - Returns: A `String` containing each of the specified string values,
+    ///   separated by newline characters.
+    func concatStrings(_ strings: String...) -> String {
+        var output = ""
 
-        vl.addItem(title: "King of Rock and Roll", value: "Elvis")
-        vl.addItem(title: "Life, the Universe, and everything", value: 42)
-        vl.addItem(title: "Earth is round", value: true)
-        return vl
+        for str in strings {
+            output += "\(str)\n"
+        }
+        return output
     }
 
-    /// Create a text representation of a list's header.
+    /// Concatenate each row in an array of strings, separated by newlines.
     ///
-    /// - Parameters:
-    ///   - itemCount: The number of items the list will hold.
-    ///   - withHeader: Whether or not the list should have a header. Default:
-    ///     `false`.
-    /// - Returns: A `String` with the text of the list's header block.
-    func createTestHeaderBlock(itemCount: Int = 0, withHeader: Bool = false) -> String {
-        if withHeader == false {
-            return ""
-        }
-        
-        var divider = ""
-        if itemCount > 0 {
-            divider = "==========================================="
-        } else {
-            divider = "========================"
-        }
-
-        return "\(self.testHeader)\n\(divider)\n"
-    }
-
-    /// Returns a text representation of the test list's contents.
+    /// - Parameter array: The array of `String` values to concatenate.
     ///
-    /// - Parameter withHeader: Whether or not the list should have a header.
-    ///   Default: `false`.
-    /// - Returns: A `String` containing the entire text representation of the
-    ///   test list.
-    func createExpectedOutput(withHeader: Bool = false) -> String {
-        let output = """
-        King of Rock and Roll                 Elvis
-        Life, the Universe, and everything    42
-        Earth is round                        Yes\n
-        """
+    /// - Returns: A `String` containing each of the source array's values
+    ///   separated by newline characters.
+    func concatArrayStrings(_ array: [String]) -> String {
+        var output = ""
 
-        return "\(self.createTestHeaderBlock(itemCount: 3, withHeader: withHeader))\(output)"
+        for str in array {
+            output += "\(str)\n"
+        }
+        return output
     }
 
     /***** TESTS *****/
@@ -80,26 +76,39 @@ final class ValueListTests: XCTestCase {
     /// Test that a list with a header but no data is generated correctly.
     func testEmptyWithHeader() {
         let vl = ValueList(header: self.testHeader)
-        let output = vl.getFormattedOutput()
+        let expectedOutput = concatStrings(self.testHeader, self.dividerNoContent)
 
-        let expectedOutput = createTestHeaderBlock(withHeader: true)
+        // Get the formatted output and check it.
+        let output = vl.getFormattedOutput()
         XCTAssertEqual(output, expectedOutput, "Empty table with header doesn't match expected string")
     }
 
     /// Test that a list with content but no header is generated correctly.
     func testOutputNoHeader() {
-        let vl = createTestList()
-        let expectedOutput = createExpectedOutput()
-        let output = vl.getFormattedOutput()
+        let vl = ValueList()
+        let expectedOutput = concatArrayStrings(self.textRows)
 
+        // Add the content to the ValueList.
+        for index in 0...(rowTitles.count - 1) {
+            vl.addItem(title: rowTitles[index], value: rowValues[index])
+        }
+
+        // Get the formatted output and check it.
+        let output = vl.getFormattedOutput()
         XCTAssertEqual(output, expectedOutput, "Table with no header doesn't match expected string")
     }
 
     /// Test that a list with content and a header is generated correctly.
     func testOutputWithHeader() {
-        let vl = createTestList(withHeader: true)
-        let expectedOutput = createExpectedOutput(withHeader: true)
+        let vl = ValueList(header: self.testHeader)
+        let expectedOutput = concatStrings(self.testHeader, self.dividerWithContent) + concatArrayStrings(self.textRows)
 
+        // Add the content to the ValueList.
+        for index in 0...(rowTitles.count - 1) {
+            vl.addItem(title: rowTitles[index], value: rowValues[index])
+        }
+
+        // Get the formatted output and check it.
         let output = vl.getFormattedOutput()
         XCTAssertEqual(output, expectedOutput, "Table with header doesn't match expected string")
     }
