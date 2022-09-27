937510dc6acf0b366c75bf7df6cc0bd0b768ea4e
ford prior <fprior@amazon.com>
2022-09-27 15:10:56 -0600

    Edited file

--- ruby/example_code/rds/list_cluster_snapshots.rb
+++ ruby/example_code/rds/list_cluster_snapshots.rb
@@ -4,12 +4,12 @@
 # snippet-start:[rds.ruby.listClusterSnapshots]
 require "aws-sdk-rds"  # v2: require 'aws-sdk'
 
-# List all snapshots for an Amazon Relational Database Service (Amazon RDS)
-# Multi-AZ DB cluster.
+# List snapshots for all Amazon Relational Database Service (Amazon RDS)
+# RDS clusters.
 #
 # @param rds_resource [Aws::RDS::Resource] An SDK for Ruby Amazon RDS resource
 # @return [Array, nil] A list of snapshots or nil if error
-def list_multi_az_db_cluster_snapshots(rds_resource)
+def list_cluster_snapshots(rds_resource)
   cluster_snapshots = []
   rds_resource.db_clusters.each do |c|
     c.snapshots.each do |s|
@@ -22,11 +22,12 @@ def list_multi_az_db_cluster_snapshots(rds_resource)
   end
   cluster_snapshots
 rescue Aws::Errors::ServiceError => e
-  puts "Couldn't list Multi-AZ DB cluster snapshots:\n #{e.message}"
+  puts "Couldn't list cluster snapshots:\n #{e.message}"
 end
 # snippet-end:[rds.ruby.listClusterSnapshots]
 
 if __FILE__ == $0
   rds_resource = Aws::RDS::Resource.new
-  list_multi_az_db_cluster_snapshots(rds_resource)
+  test = list_cluster_snapshots(rds_resource)
+  puts test.to_s
 end
