2229dc859f0f30f4132e82b56d38bea5ad218399
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-10-04 09:59:07 -0400

    Add files via upload

--- javav2/usecases/CreatingSpringRedshiftRest/src/main/java/com/aws/rest/App.java
+++ javav2/usecases/CreatingSpringRedshiftRest/src/main/java/com/aws/rest/App.java
@@ -7,10 +7,118 @@ package com.aws.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementRequest;
+import software.amazon.awssdk.services.redshiftdata.model.DescribeStatementResponse;
+import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementRequest;
+import software.amazon.awssdk.services.redshiftdata.model.ExecuteStatementResponse;
+import software.amazon.awssdk.services.redshiftdata.model.GetStatementResultRequest;
+import software.amazon.awssdk.services.redshiftdata.model.GetStatementResultResponse;
+import software.amazon.awssdk.services.redshiftdata.model.RedshiftDataException;
+import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient;
+import software.amazon.awssdk.services.redshiftdata.model.SqlParameter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) throws Throwable {
+
+    // Specify the database name, the database user, and the cluster Id value.
+    private static final String database = "dev";
+    private static final String dbUser ="awsuser";
+    private static final String clusterId = "redshift-cluster-1";
+
+    static RedshiftDataClient getClient() {
+
+        Region region = Region.US_WEST_2;
+        return RedshiftDataClient.builder()
+            .region(region)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
+    }
+
+    static List<WorkItem> getResults(String statementId) {
+        try {
+            GetStatementResultRequest resultRequest = GetStatementResultRequest.builder()
+                .id(statementId)
+                .build();
+
+            GetStatementResultResponse response = App.getClient().getStatementResult(resultRequest);
+            return response
+                .records()
+                .stream()
+                .map(WorkItem::from)
+                .collect(Collectors.toUnmodifiableList());
+
+        } catch (RedshiftDataException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+
+    // Update the work table.
+    static void flipItemArchive(String sqlStatement,  List<SqlParameter> parameters ) {
+
+        RedshiftDataClient redshiftDataClient = getClient();
+        String arc = "1";
+
+        try {
+            ExecuteStatementRequest statementRequest = ExecuteStatementRequest.builder()
+                .clusterIdentifier(clusterId)
+                .database(database)
+                .dbUser(dbUser)
+                .sql(sqlStatement)
+                .parameters(parameters)
+                .build();
+
+            App.getClient().executeStatement(statementRequest);
+
+        } catch (RedshiftDataException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    static void checkStatement(String sqlId ) {
+        try {
+            DescribeStatementRequest statementRequest = DescribeStatementRequest.builder()
+                .id(sqlId)
+                .build() ;
+
+            // Wait until the sql statement processing is finished.
+            String status;
+            while (true) {
+                DescribeStatementResponse response = App.getClient().describeStatement(statementRequest);
+                status = response.statusAsString();
+                System.out.println("..."+status);
+
+                if (status.compareTo("FINISHED") == 0) {
+                    break;
+                }
+                Thread.sleep(500);
+            }
+            System.out.println("The statement is finished!");
+
+        } catch (RedshiftDataException | InterruptedException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    static ExecuteStatementResponse execute(String sqlStatement, List<SqlParameter> parameters) {
+        ExecuteStatementRequest sqlRequest = ExecuteStatementRequest.builder()
+            .clusterIdentifier(clusterId)
+            .database(database)
+            .dbUser(dbUser)
+            .sql(sqlStatement)
+            .parameters(parameters)
+            .build();
+        return App.getClient().executeStatement(sqlRequest);
+    }
+
+   public static void main(String[] args) throws Throwable {
         SpringApplication.run(App.class, args);
     }
 }
