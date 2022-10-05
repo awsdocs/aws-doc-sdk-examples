76423abba479a236d0afbec6f241070b7f29370b
David Souther <davidsouther@gmail.com>
2022-10-04 18:01:24 -0700

    Sanding

--- rust_dev_preview/dynamodb/src/bin/movies.rs
+++ rust_dev_preview/dynamodb/src/bin/movies.rs
@@ -8,42 +8,22 @@ use std::net::SocketAddr;
 use aws_sdk_dynamodb::{Client, Error};
 use axum::extract::Extension;
 use dynamodb_code_examples::scenario::movies::server::{make_app, movies_in_year};
-use dynamodb_code_examples::scenario::movies::{
-    shutdown::remove_table, startup::initialize, TABLE_NAME,
-};
-use tracing_subscriber::layer::SubscriberExt;
+use dynamodb_code_examples::scenario::movies::{startup::initialize, TABLE_NAME};
 
-struct Server<'a> {
-    client: &'a Client,
+async fn run(client: Client) {
+    let app = make_app()
+        .layer(Extension(client))
+        .layer(Extension(TABLE_NAME));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    eprintln!("Listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Server crashed");
 }
 
-impl<'a> Drop for Server<'a> {
-    fn drop(&mut self) {
-        remove_table(self.client, TABLE_NAME).await?;
-    }
-}
-
-/// A partial reimplementation of
-/// <https://docs.amazonaws.cn/en_us/amazondynamodb/latest/developerguide/GettingStarted.Ruby.html>
-/// in Rust
-///
-/// - Create table
-/// - Wait for table to be ready
-/// - Add a couple of rows
-/// - Query for those rows
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "axum_api=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer());
-
-    let config = aws_config::from_env().load().await;
-    let client = Client::new(&config);
-
-    initialize(&client, TABLE_NAME).await?;
-
+async fn verify(client: &Client) -> Result<(), Error> {
     let films_2222 = movies_in_year(&client, TABLE_NAME, 2222)
         .await
         .expect("Query should succeed");
@@ -57,15 +37,28 @@ async fn main() -> Result<(), Error> {
 
     assert!(films_2013.len() > 0, "should get movies back");
 
-    let app = make_app()
-        .layer(Extension(client))
-        .layer(Extension(TABLE_NAME));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("Listening on {addr}");
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Ok(())
+}
+
+/// A DynamoDB backed movie database. Loads a bunch of movies
+///
+/// - Create table
+/// - Wait for table to be ready
+/// - Add a bunch of data
+/// - Check the data is there
+/// - Start an HTTP server to return subsets of those rows
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+
+    let config = aws_config::from_env().load().await;
+    let client = Client::new(&config);
+
+    initialize(&client, TABLE_NAME).await?;
+
+    verify(&client).await?;
+
+    run(client).await;
 
     Ok(())
 }
