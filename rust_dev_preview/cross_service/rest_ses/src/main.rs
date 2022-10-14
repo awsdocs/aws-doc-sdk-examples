57b266ef1d80b1d9af201175f02b31e5f77cd072
David Souther <davidsouther@gmail.com>
2022-10-14 10:09:05 -0700

    Comments

--- rust_dev_preview/cross_service/rest_ses/src/main.rs
+++ rust_dev_preview/cross_service/rest_ses/src/main.rs
@@ -6,11 +6,16 @@ use rest_ses::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // App Settings come from configuration.yaml
     let settings = get_settings().expect("Failed to load configuration.");
+    // AWS Settings (region & role) come from the environment
     let config = aws_config::from_env().load().await;
     let rds = RdsClient::new(settings.sdk_config, config);
+
     let address = format!("127.0.0.1:{}", settings.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind a TcpListener!");
+
     println!("Listening on {addr}", addr = listener.local_addr()?);
+
     run(listener, rds)?.await
 }
