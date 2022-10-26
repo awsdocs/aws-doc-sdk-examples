68ec64ac8d377beaf7a2917b02292961b4a9c506
Corey Pyle <corepyle@amazon.com>
2022-10-26 09:20:51 -0400

    Add copyright to test files.

--- javascriptv3/example_code/cognito/tests/actions.integration.test.js
+++ javascriptv3/example_code/cognito/tests/actions.integration.test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { describe, expect } from "@jest/globals";
 import { createUserPool } from "../actions/create-user-pool.js";
 import { getUniqueName } from "../../libs/utils/util-string.js";
