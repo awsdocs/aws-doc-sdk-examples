d7f14434f2d4f412375831c015cc0c7fe2b6c4f3
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-10-27 16:23:50 -0400

    Add files via upload

---
+++ javav2/usecases/Creating_rds_item_tracker/src/main/java/com/aws/rest/WorkItem.java
@@ -0,0 +1,73 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.aws.rest;
+
+public class WorkItem {
+
+    private String id;
+    private String name;
+    private String guide ;
+    private String date;
+    private String description;
+    private String status;
+    private boolean archived ;
+
+    public void setId (String id) {
+        this.id = id;
+    }
+
+    public boolean getArchived() {
+        return this.archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setStatus (String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDate (String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setGuide (String guide) {
+        this.guide = guide;
+    }
+
+    public String getGuide() {
+        return this.guide;
+    }
+}
---
+++ javav2/usecases/Creating_rds_item_tracker/src/main/java/com/aws/rest/WriteExcel.java
@@ -0,0 +1,191 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.aws.rest;
+
+import jxl.CellView;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.format.UnderlineStyle;
+import jxl.write.Label;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.RawMessage;
+import software.amazon.awssdk.services.ses.model.SendRawEmailRequest;
+import software.amazon.awssdk.services.ses.model.SesException;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Locale;
+import java.util.Properties;
+
+@Component
+public class WriteExcel {
+    static WritableCellFormat times ;
+    static WritableCellFormat timesBoldUnderline;
+
+    static {
+        try {
+            WritableFont times10pt = new WritableFont(WritableFont.TIMES, 10);
+            times = new WritableCellFormat(times10pt);
+            times.setWrap(true);
+
+            WritableFont times10ptBoldUnderline = new WritableFont(WritableFont.TIMES, 10, WritableFont.BOLD, false, UnderlineStyle.SINGLE);
+            timesBoldUnderline = new WritableCellFormat(times10ptBoldUnderline);
+            timesBoldUnderline.setWrap(true);
+        } catch (WriteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public InputStream write(Iterable<WorkItem> items) throws IOException, WriteException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        WorkbookSettings wbSettings = new WorkbookSettings();
+        wbSettings.setLocale(new Locale("en", "US"));
+
+        WritableWorkbook workbook = Workbook.createWorkbook(os, wbSettings);
+        workbook.createSheet("Work Item Report", 0);
+        WritableSheet excelSheet = workbook.getSheet(0);
+
+        addLabels(excelSheet);
+        fillContent(excelSheet, items);
+
+        workbook.write();
+        workbook.close();
+
+        return new ByteArrayInputStream(os.toByteArray());
+    }
+
+    private void addLabels(WritableSheet sheet) throws WriteException {
+        CellView cv = new CellView();
+        cv.setFormat(timesBoldUnderline);
+        cv.setAutosize(true);
+
+        addCaption(sheet, 0, 0, "Writer");
+        addCaption(sheet, 1, 0, "Date");
+        addCaption(sheet, 2, 0, "Guide");
+        addCaption(sheet, 3, 0, "Description");
+        addCaption(sheet, 4, 0, "Status");
+    }
+
+    private void addCaption(WritableSheet sheet, int column, int row, String s) throws WriteException {
+        Label label = new Label(column, row, s, timesBoldUnderline);
+        int cc = s.length();
+        sheet.setColumnView(column, cc);
+        sheet.addCell(label);
+    }
+
+    private void addField(WritableSheet sheet, int column, int row, String s) throws WriteException {
+        Label label = new Label(column, row, s, timesBoldUnderline);
+        int cc = s.length();
+        cc = cc > 200 ? 150 : cc + 6;
+        sheet.setColumnView(column, cc);
+        sheet.addCell(label);
+    }
+
+    private void fillContent(WritableSheet sheet, Iterable<WorkItem> items) throws WriteException {
+        int row = 2;
+        for (WorkItem item : items) {
+            addField(sheet, 0, row, item.getName());
+            addField(sheet, 1, row, item.getDate());
+            addField(sheet, 2, row, item.getGuide());
+            addField(sheet, 3, row, item.getDescription());
+            addField(sheet, 4, row, item.getStatus());
+            row += 1;
+        }
+    }
+
+    @Component
+    public static class SendMessages {
+        private static String sender = "scmacdon@amazon.com";
+        private static String subject = "Weekly AWS Status Report";
+        private static String bodyText = "Hello,\r\n\r\nPlease see the attached file for a weekly update.";
+        private static String bodyHTML = "<!DOCTYPE html><html lang=\"en-US\"><body><h1>Hello!</h1><p>Please see the attached file for a weekly update.</p></body></html>";
+        private static String attachmentName = "WorkReport.xls";
+
+        public void sendReport(InputStream is, String emailAddress) throws IOException {
+            byte[] fileContent = IOUtils.toByteArray(is);
+
+            try {
+                send(makeEmail(fileContent, emailAddress));
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void send(MimeMessage message) throws MessagingException, IOException {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            message.writeTo(outputStream);
+            ByteBuffer buf = ByteBuffer.wrap(outputStream.toByteArray());
+            byte[] arr = new byte[buf.remaining()];
+            buf.get(arr);
+            SdkBytes data = SdkBytes.fromByteArray(arr);
+            RawMessage rawMessage = RawMessage.builder().data(data).build();
+            SendRawEmailRequest rawEmailRequest = SendRawEmailRequest.builder().rawMessage(rawMessage).build();
+
+            try {
+                System.out.println("Attempting to send an email through Amazon SES...");
+                SesClient client = SesClient.builder().region(Region.US_WEST_2).build();
+                client.sendRawEmail(rawEmailRequest);
+            } catch (SesException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private MimeMessage makeEmail(byte[] attachment, String emailAddress) throws MessagingException {
+            Session session = Session.getDefaultInstance(new Properties());
+            MimeMessage message = new MimeMessage(session);
+
+            message.setSubject(subject, "UTF-8");
+            message.setFrom(new InternetAddress(sender));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAddress));
+
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setContent(bodyText, "text/plain; charset=UTF-8");
+
+            MimeBodyPart htmlPart = new MimeBodyPart();
+            htmlPart.setContent(bodyHTML, "text/html; charset=UTF-8");
+
+            MimeMultipart msgBody = new MimeMultipart("alternative");
+            msgBody.addBodyPart(textPart);
+            msgBody.addBodyPart(htmlPart);
+
+            MimeBodyPart wrap = new MimeBodyPart();
+            wrap.setContent(msgBody);
+
+            MimeMultipart msg = new MimeMultipart("mixed");
+            msg.addBodyPart(wrap);
+
+            MimeBodyPart att = new MimeBodyPart();
+            DataSource fds = new ByteArrayDataSource(attachment, "application/vnc.openxmlformats-officedocument.spreadsheetml.sheet");
+            att.setDataHandler(new DataHandler(fds));
+            att.setFileName(attachmentName);
+
+            msg.addBodyPart(att);
+            message.setContent(msg);
+            return message;
+        }
+    }
+}
