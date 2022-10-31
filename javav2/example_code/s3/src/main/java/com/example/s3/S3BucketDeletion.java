bf2bd74f9bf5387f3c5b3eef8cc7326895b8c25b
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-10-31 12:21:47 -0400

    Revert "New example file for s3 transfer mgr with snippets created for Java Dev Guide"

--- javav2/example_code/s3/src/main/java/com/example/s3/S3BucketDeletion.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/S3BucketDeletion.java
@@ -13,12 +13,7 @@ package com.example.s3;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Exception;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.*;
 // snippet-end:[s3.java2.s3_bucket_ops.delete_bucket.import]
 // snippet-end:[s3.java2.bucket_deletion.import]
 // snippet-start:[s3.java2.bucket_deletion.main]
