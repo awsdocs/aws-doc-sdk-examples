97c67e03fa7db36d27ef54dc6bd7b882543d066e
Zelda Hessler <zhessler@amazon.com>
2022-11-04 13:30:52 -0500

    update: ca-certs example to work with changes from smithy-rs#1918 rename: "ca-certs" example to more explicit "custom-root-certificates"

--- rust_dev_preview/custom-root-certificates/src/main.rs
+++ rust_dev_preview/custom-root-certificates/src/main.rs
@@ -27,13 +27,14 @@ async fn main() {
     let provider_config = ProviderConfig::default().with_tcp_connector(rustls_connector.clone());
     let shared_conf = aws_config::from_env()
         .configure(provider_config)
+        .http_connector(hyper_ext::Adapter::builder().build(rustls_connector))
         .load()
         .await;
-    let s3_config = aws_sdk_s3::Config::from(&shared_conf);
-    // however, for generated clients, they are constructred from a Hyper adapter directly:
-    let s3_client = aws_sdk_s3::Client::from_conf_conn(
-        s3_config,
-        hyper_ext::Adapter::builder().build(rustls_connector),
-    );
-    let _ = s3_client.list_buckets().send().await;
+    // however, for generated clients, they are constructed from a Hyper adapter directly:
+    let s3_client = aws_sdk_s3::Client::new(&shared_conf);
+    
+    match s3_client.list_buckets().send().await {
+        Ok(res) => { println!("Your buckets: {:?}", res.buckets().unwrap_or_default()) },
+        Err(err) => { println!("an error occurred when trying to list buckets: {}", err) },
+    }
 }
