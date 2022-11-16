c73e3900149a26e8dbb918c726e3d5ee68c003f1
Corey Pyle <corepyle@amazon.com>
2022-11-16 15:39:56 -0500

    Elwing Item Tracker QOL and JS fixes.
    - Differentiate between 200 and 204 responses in the EWP (Elwing plugin). 200's will be parsed and throw an error on empty content.
    - Update the typings for the EWP RestService to account for void responses.
    - Update JS aurora app to return a 204 response for the email report.
    - Add an error banner to the 'Item Tracker' EWP.
    - Show error banner when report call fails in EWP.
    - Wrap column lines in 'Item Tracker' EWP work item list.
    - Clear form data after sending a work item in 'Item Tracker' EWP.
    - Remove 'Archive' column, make items in the table selectable, and allow archiving multiple items.

--- resources/clients/react/elwing/src/plugins/item-tracker/ItemTrackerComponent.tsx
+++ resources/clients/react/elwing/src/plugins/item-tracker/ItemTrackerComponent.tsx
@@ -1,22 +1,34 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Container, Grid } from "@cloudscape-design/components";
+import { Alert, Container, Grid } from "@cloudscape-design/components";
 import { Readme } from "./ItemTrackerReadmeComponent";
-import { AddWorkItem } from "./work-item/AddWorkItemComponent";
+import { useItemTrackerAction, useItemTrackerState } from "./ItemTrackerStore";
 import { WorkItemControls, WorkItems } from "./work-item/WorkItemListComponent";
 
 const Component = () => {
+  const error = useItemTrackerState(({ error }) => error);
+  const { setError } = useItemTrackerAction();
+
   return (
     <>
       <Readme />
       <Container>
         <h2>Work Items</h2>
+        <Alert
+          dismissible
+          visible={!!error}
+          type="error"
+          onDismiss={() => {
+            setError("");
+          }}
+        >
+          {error}
+        </Alert>
         <Grid gridDefinition={[{ colspan: 9 }, { colspan: 3 }]}>
           <WorkItems />
           <WorkItemControls />
         </Grid>
-        <AddWorkItem />
       </Container>
     </>
   );
