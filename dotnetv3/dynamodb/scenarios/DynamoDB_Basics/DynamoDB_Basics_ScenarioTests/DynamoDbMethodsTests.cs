793969792ef707084257b6775d2aa9855f8c6d1e
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2022-11-18 08:09:10 -0500

    One final change to testing variables.

--- dotnetv3/dynamodb/scenarios/DynamoDB_Basics/DynamoDB_Basics_ScenarioTests/DynamoDbMethodsTests.cs
+++ dotnetv3/dynamodb/scenarios/DynamoDB_Basics/DynamoDB_Basics_ScenarioTests/DynamoDbMethodsTests.cs
@@ -8,15 +8,15 @@ namespace DynamoDB_Basics_Scenario.Tests
     public class DynamoDbMethodsTests
     {
         readonly AmazonDynamoDBClient client = new();
-        readonly string tableName = "movie_table";
-        readonly string movieFileName = @"..\..\..\..\..\..\..\..\resources\sample_files\movies.json";
-        readonly string testMovieFileName = "testmoviedata.json";
+        readonly string _tableName = "movie_table";
+        readonly string _movieFileName = @"..\..\..\..\..\..\..\..\resources\sample_files\movies.json";
+        readonly string _badMovieFile = "notareaddatafile";
 
         [Fact()]
         [Order(1)]
         public async Task CreateMovieTableAsyncTest()
         {
-            var success = await DynamoDbMethods.CreateMovieTableAsync(client, tableName);
+            var success = await DynamoDbMethods.CreateMovieTableAsync(client, _tableName);
 
             Assert.True(success, "Failed to create table.");
         }
@@ -31,7 +31,7 @@ namespace DynamoDB_Basics_Scenario.Tests
                 Title = "North by Northwest",
             };
 
-            var success = await DynamoDbMethods.PutItemAsync(client, newMovie, tableName);
+            var success = await DynamoDbMethods.PutItemAsync(client, newMovie, _tableName);
             Assert.True(success, "Couldn't add the movie.");
         }
 
@@ -51,7 +51,7 @@ namespace DynamoDB_Basics_Scenario.Tests
                 Rank = 5,
             };
 
-            var success = await DynamoDbMethods.UpdateItemAsync(client, updateMovie, updateMovieInfo, tableName);
+            var success = await DynamoDbMethods.UpdateItemAsync(client, updateMovie, updateMovieInfo, _tableName);
             Assert.True(success, $"Couldn't update {updateMovie.Title}.");
         }
 
@@ -59,7 +59,7 @@ namespace DynamoDB_Basics_Scenario.Tests
         [Order(4)]
         public void ImportMoviesWithBadFileNameShouldReturnNullTest()
         {
-            var movies = DynamoDbMethods.ImportMovies(testMovieFileName);
+            var movies = DynamoDbMethods.ImportMovies(_badMovieFile);
             Assert.Null(movies);
         }
 
@@ -67,7 +67,7 @@ namespace DynamoDB_Basics_Scenario.Tests
         [Order(5)]
         public async Task BatchWriteItemsAsyncTest()
         {
-            var itemCount = await DynamoDbMethods.BatchWriteItemsAsync(client, movieFileName);
+            var itemCount = await DynamoDbMethods.BatchWriteItemsAsync(client, _movieFileName);
             Assert.Equal(250, itemCount);
         }
 
@@ -81,7 +81,7 @@ namespace DynamoDB_Basics_Scenario.Tests
                 Year = 2013,
             };
 
-            var item = await DynamoDbMethods.GetItemAsync(client, lookupMovie, tableName);
+            var item = await DynamoDbMethods.GetItemAsync(client, lookupMovie, _tableName);
 
             Assert.True(item["title"].S == lookupMovie.Title, $"Couldn't find {lookupMovie.Title}.");
         }
@@ -96,7 +96,7 @@ namespace DynamoDB_Basics_Scenario.Tests
                 Year = 2013,
             };
 
-            var success = await DynamoDbMethods.DeleteItemAsync(client, tableName, movieToDelete);
+            var success = await DynamoDbMethods.DeleteItemAsync(client, _tableName, movieToDelete);
             Assert.True(success, "Couldn't delete the item.");
         }
 
@@ -106,7 +106,7 @@ namespace DynamoDB_Basics_Scenario.Tests
         {
             // Use Query to find all the movies released in 2010.
             int findYear = 2013;
-            var queryCount = await DynamoDbMethods.QueryMoviesAsync(client, tableName, findYear);
+            var queryCount = await DynamoDbMethods.QueryMoviesAsync(client, _tableName, findYear);
             Assert.True(queryCount > 0, "Couldn't find any movies that match.");
         }
 
@@ -116,7 +116,7 @@ namespace DynamoDB_Basics_Scenario.Tests
         {
             int startYear = 2001;
             int endYear = 2011;
-            var scanCount = await DynamoDbMethods.ScanTableAsync(client, tableName, startYear, endYear);
+            var scanCount = await DynamoDbMethods.ScanTableAsync(client, _tableName, startYear, endYear);
             Assert.True(scanCount > 0, "Couldn't find any movies released in those years.");
         }
 
@@ -124,7 +124,7 @@ namespace DynamoDB_Basics_Scenario.Tests
         [Order(11)]
         public async void DeleteTableAsyncTest()
         {
-            var success = await DynamoDbMethods.DeleteTableAsync(client, tableName);
+            var success = await DynamoDbMethods.DeleteTableAsync(client, _tableName);
 
             Assert.True(success, "Failed to delete table.");
         }
