8e03ecc4c38c2a8a6d3e297fecf7bb1db4b4cbbf
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-11-25 12:31:34 -0500

    Update Route53Test.java

--- javav2/example_code/route53/src/test/java/Route53Test.java
+++ javav2/example_code/route53/src/test/java/Route53Test.java
@@ -1,7 +1,15 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 import com.example.route.*;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.route53.Route53Client;
 import org.junit.jupiter.api.*;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.route53domains.Route53DomainsClient;
+
 import java.io.*;
 import java.net.URISyntaxException;
 import java.util.*;
@@ -12,11 +20,16 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Route53Test {
-
+    public static final String DASHES = new String(new char[80]).replace("\0", "-");
     private static String domainName = "";
     private static String healthCheckId = "";
     private static String hostedZoneId = "";
     private static Route53Client route53Client;
+    private static Route53DomainsClient route53DomainsClient;
+
+    private static String domainSuggestionSc = "" ;
+    private static String domainTypeSc = "" ;
+
 
     @BeforeAll
     public static void setUp() throws IOException, URISyntaxException {
@@ -26,6 +39,11 @@ public class Route53Test {
                 .region(region)
                 .credentialsProvider(ProfileCredentialsProvider.create())
                 .build();
+
+        route53DomainsClient = Route53DomainsClient.builder()
+            .region(Region.US_EAST_1)
+            .credentialsProvider(ProfileCredentialsProvider.create())
+            .build();
 
         try (InputStream input = Route53Test.class.getClassLoader().getResourceAsStream("config.properties")) {
 
@@ -41,7 +59,8 @@ public class Route53Test {
 
             // Populate the data members required for all tests
             domainName = prop.getProperty("domainName");
-
+            domainSuggestionSc = prop.getProperty("domainSuggestionSc");
+            domainTypeSc = prop.getProperty("domainTypeSc");
 
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -119,5 +138,56 @@ public class Route53Test {
     public void DeleteHostedZone() {
         DeleteHostedZone.delHostedZone(route53Client, hostedZoneId);
         System.out.println("Test 9 passed");
+    }
+
+    @Test
+    @Order(10)
+    public void ScenarioTest() {
+        System.out.println(DASHES);
+        System.out.println("1. List current domains.");
+        Route53Scenario.listDomains(route53DomainsClient);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("2. List operations in the past year.");
+        Route53Scenario.listOperations(route53DomainsClient);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("3. View billing for the account in the past year.");
+        Route53Scenario.listBillingRecords(route53DomainsClient);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("4. View prices for domain types.");
+        Route53Scenario.listPrices(route53DomainsClient, domainTypeSc);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("5. Get domain suggestions.");
+        Route53Scenario.listDomainSuggestions(route53DomainsClient, domainSuggestionSc);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("6. Check domain availability.");
+        Route53Scenario.checkDomainAvailability(route53DomainsClient, domainSuggestionSc);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("7. Check domain transferability.");
+        Route53Scenario.checkDomainTransferability(route53DomainsClient, domainSuggestionSc);
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("8. Request a domain registration.");
+        String opId = Route53Scenario.requestDomainRegistration(route53DomainsClient, domainSuggestionSc);
+        assertFalse(opId.isEmpty());
+        System.out.println(DASHES);
+
+        System.out.println(DASHES);
+        System.out.println("9. Get an operation detail.");
+        Route53Scenario.getOperationalDetail(route53DomainsClient, opId);
+        System.out.println(DASHES);
+        System.out.println("Test 10 Passed");
     }
 }
