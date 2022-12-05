5930a0a18da3df7e8da63d2d95769fa16c4035c4
ford prior <fprior@amazon.com>
2022-12-05 11:38:26 -0600

    more

--- ruby/cross_service_examples/item_tracker/env/setup_scripts/create_table.rb
+++ ruby/cross_service_examples/item_tracker/env/setup_scripts/create_table.rb
@@ -3,14 +3,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-require "multi_json"
 require "yaml"
-require "json"
-require "rspec"
 require "aws-sdk-rdsdataservice"
 require "aws-sdk-rds"
 require "aws-sdk-ses"
 
+# A simple class for checking for databases and tables in an Amazon Aurora cluster
 class SetupDatabase
 
   def initialize
@@ -34,12 +32,14 @@ class SetupDatabase
   # Checks if table exists in database
   # @return [Boolean] true if table exists, false if not.
   def table_exists?
-    resp = @data_client.execute_statement({
-                                      resource_arn: @config["resource_arn"],
-                                      secret_arn: @config["secret_arn"],
-                                      sql: "show tables;",
-                                      database: @config["database"],
-                                    })
+    resp = @data_client.execute_statement(
+      {
+        resource_arn: @config["resource_arn"],
+        secret_arn: @config["secret_arn"],
+        sql: "show tables;",
+        database: @config["database"]
+      }
+    )
 
     resp[0].each { |table|
       if table[0].string_value == @config["table_name"]
@@ -52,13 +52,14 @@ class SetupDatabase
   # NOTE: This code contains raw string substitution and is therefore vulnerable to SQL injection
   # TODO: Refactor to use Rails ActiveRecord
   def create_table
-    sql = "CREATE TABLE #{@config['table_name']} (work_item_id INT AUTO_INCREMENT PRIMARY KEY, description VARCHAR(400), guide VARCHAR(45), status VARCHAR(400), username VARCHAR(45), archived TINYINT(4));"
-    @data_client.execute_statement({
-                               resource_arn: @config["resource_arn"],
-                               secret_arn: @config["secret_arn"],
-                               sql: sql,
-                               database: @config["database"],
-                             })
+    @data_client.execute_statement(
+      {
+        resource_arn: @config["resource_arn"],
+        secret_arn: @config["secret_arn"],
+        sql: "CREATE TABLE work_items (work_item_id INT AUTO_INCREMENT PRIMARY KEY, description VARCHAR(400), guide VARCHAR(45), status VARCHAR(400), username VARCHAR(45), archived TINYINT(4));",
+        database: @config["database"],
+      }
+    )
   end
 
 end
--- ruby/cross_service_examples/item_tracker/env/setup_scripts/populate_table.rb
+++ ruby/cross_service_examples/item_tracker/env/setup_scripts/populate_table.rb
@@ -7,19 +7,34 @@ require "yaml"
 require "aws-sdk-rdsdataservice"
 require_relative "../../src/aurora"
 
-client = Aws::RDSDataService::Client.new
-config = YAML.safe_load(File.open(File.join(File.dirname(__FILE__), "./../", "config.yml")))
-wrapper = AuroraActions.new(config, client)
+# A simple class for creating items in the database
+class PopulateTable
+  def initialize
+    client = Aws::RDSDataService::Client.new
+    config = YAML.safe_load(File.open(File.join(File.dirname(__FILE__), "./../", "config.yml")))
+    @wrapper = AuroraActions.new(config, client)
+  end
 
-10.times do
-  username = ["ltolstoy", "jsteinbeck", "jkerouac", "wkhalifa"].sample
-  item_data = {
-    description: ["New feature", "Quick bugfix", "User research", "Tech debt"].sample,
-    guide: ["cpp", "python", "go", "ruby", "dotnet", "js", "php"].sample,
-    status: ["backlog", "icebox", "unrefined", "done", "in-progress"].sample,
-    username: username,
-    name: username,
-    archived: [0, 1].sample
-  }
-  wrapper.add_work_item(item_data)
+  def add_records
+    10.times do
+      username = ["ltolstoy", "jsteinbeck", "jkerouac", "wkhalifa"].sample
+      item_data = {
+        description: ["New feature", "Quick bugfix", "User research", "Tech debt"].sample,
+        guide: ["cpp", "python", "go", "ruby", "dotnet", "js", "php"].sample,
+        status: ["backlog", "icebox", "unrefined", "done", "in-progress"].sample,
+        username: username,
+        name: username,
+        archived: [0, 1].sample
+      }
+      @wrapper.add_work_item(item_data)
+    end
+  end
+end
+
+# check for database cluster & create table if none exists
+begin
+  setup = PopulateTable.new
+  setup.add_records
+rescue StandardError => e
+  raise "Failed while creating new database records:\n#{e}"
 end
