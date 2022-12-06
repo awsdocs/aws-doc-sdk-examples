52421e87f862ff7bed80cb973ee73aaf35926078
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-12-06 13:45:17 -0500

    Add files via upload

--- kotlin/services/route53/src/main/kotlin/com/kotlin/route/HelloRoute53.kt
+++ kotlin/services/route53/src/main/kotlin/com/kotlin/route/HelloRoute53.kt
@@ -9,9 +9,12 @@ package com.kotlin.route
 
 import aws.sdk.kotlin.services.route53domains.Route53DomainsClient
 import aws.sdk.kotlin.services.route53domains.model.ListPricesRequest
+import aws.sdk.kotlin.services.route53domains.paginators.listPricesPaginated
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.transform
 import kotlin.system.exitProcess
 
-// snippet-start:[route.kotlin.hello.main]
+//snippet-start:[route.kotlin.hello.main]
 /**
  Before running this Kotlin code example, set up your development environment,
  including your credentials.
@@ -28,29 +31,30 @@ suspend fun main(args: Array<String>) {
            domainType - The domain type (for example, com). 
     """
 
-    if (args.size != 1) {
-        println(usage)
-        exitProcess(0)
-    }
-    val domainType = args[0]
-    println("Invokes ListPrices for at least one domain type.")
-    listPrices(domainType)
+   // if (args.size != 1) {
+   //     println(usage)
+   //     exitProcess(0)
+   // }
+    val domainType = "com" //args[0]
+    println("Invokes ListPrices using a Paginated method.")
+    listPricesPaginated(domainType)
 }
 
-suspend fun listPrices(domainType: String) {
+suspend fun listPricesPaginated(domainType: String) {
     val pricesRequest = ListPricesRequest {
         maxItems = 10
         tld = domainType
     }
 
     Route53DomainsClient { region = "us-east-1" }.use { route53DomainsClient ->
-        val response = route53DomainsClient.listPrices(pricesRequest)
-        response.prices?.forEach { pr ->
-            println("Registration: ${pr.registrationPrice?.price} ${pr.registrationPrice?.currency}")
-            println("Renewal: ${pr.renewalPrice?.price} ${pr.renewalPrice?.currency}")
-            println("Transfer: ${pr.transferPrice?.price} ${pr.transferPrice?.currency}")
-            println("Restoration: ${pr.restorationPrice?.price} ${pr.restorationPrice?.currency}")
+        route53DomainsClient.listPricesPaginated(pricesRequest)
+            .transform { it.prices?.forEach { obj -> emit(obj) } }
+            .collect { pr ->
+                println("Registration: ${pr.registrationPrice} ${pr.registrationPrice?.currency}")
+                println("Renewal: ${pr.renewalPrice?.price} ${pr.renewalPrice?.currency}")
+                println("Transfer: ${pr.transferPrice?.price} ${pr.transferPrice?.currency}")
+                println("Restoration: ${pr.restorationPrice?.price} ${pr.restorationPrice?.currency}")
         }
     }
 }
-// snippet-end:[route.kotlin.hello.main]
+//snippet-end:[route.kotlin.hello.main]
