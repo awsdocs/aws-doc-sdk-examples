ec1719f2f97b368054b74533ef135942a7393037
Eric Shepherd <ericsh@amazon.com>
2022-12-06 16:56:43 -0500

    Add maximum length option for unique names
    This patch adds the optional `maxDigits` parameter to the
    `String.uniqueName()` function. This option lets you specify
    the maximum number of characters of the UUID to include in
    the returned unique string.

--- swift/modules/SwiftUtilities/Sources/SwiftUtilities/String_uniqueName.swift
+++ swift/modules/SwiftUtilities/Sources/SwiftUtilities/String_uniqueName.swift
@@ -8,23 +8,34 @@
 import Foundation
 
 public extension String {
-    /// Returns a unique file name for use in testing. The file name is a UUID.
-    /// You can optionally add a period and an extension to it. If `inValid` is
-    /// true, the returned name isn't valid for use with Amazon Simple Storage
-    /// Service (Amazon S3).
+    /// Returns a unique file name for use in testing. The file name is a
+    /// UUID. You can optionally add a period and an extension to it. If
+    /// `inValid` is true, the returned name isn't valid for use with Amazon
+    /// Simple Storage Service (Amazon S3).
     ///
     /// - Parameters:
     ///   - prefix: String - A prefix to add to the name. If empty, none is
     ///     added.
+    ///   - maxDigits: Int - The maximum number of random characters to use.
+    ///     If this value is 0 or exceeds the UUID length, the entire UUID is
+    ///     used.
     ///   - ext: String - The file extension to add. If empty, no extension is
     ///     added.
-    ///   - isValid: Bool - If true, the returned filename is invalid for Amazon
-    ///     S3 use.
-    /// - Returns: A string containing a unique filename for Amazon S3 testing.
+    ///   - isValid: Bool - If true, the returned filename is invalid for
+    ///     Amazon S3 use.
+    /// - Returns: A string containing a unique filename for Amazon S3
+    ///   testing.
     static func uniqueName(withPrefix prefix: String = "",
+                           maxDigits: Int = 0,
                            withExtension ext: String = "",
                            isValid: Bool = true) -> String {
         var name = UUID().uuidString
+
+        if maxDigits != 0 {
+            if maxDigits < name.count {
+                name = String(name.prefix(maxDigits))
+            }
+        }
 
         if prefix != "" {
             name = "\(prefix)-\(name)"
