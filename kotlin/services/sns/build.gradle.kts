73b26731b46cba09963962775b6f647e3c69eb3a
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2022-12-13 10:17:40 -0500

    Add files via upload

--- kotlin/services/sns/build.gradle.kts
+++ kotlin/services/sns/build.gradle.kts
@@ -26,10 +26,7 @@ dependencies {
 	implementation("aws.sdk.kotlin:sns:0.19.0-beta")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-	implementation("com.fasterxml.jackson.core:jackson-core:2.14.0")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
 }
 tasks.withType<KotlinCompile>() {
 	kotlinOptions.jvmTarget = "1.8"
 }
-
