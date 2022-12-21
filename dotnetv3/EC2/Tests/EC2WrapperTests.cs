3d66415bddc954f4cd60ef474f71e7c48f6b7eeb
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2022-12-21 11:14:35 -0500

    Re-ordered integration tests.

--- dotnetv3/EC2/Tests/EC2WrapperTests.cs
+++ dotnetv3/EC2/Tests/EC2WrapperTests.cs
@@ -56,48 +56,11 @@ public class EC2WrapperTests
     }
 
     /// <summary>
-    /// Test the allocation of an Elastic IP address.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(11)]
-    public async Task AllocateAddressTest()
-    {
-        _allocationId = await _ec2Wrapper.AllocateAddress();
-        Assert.NotNull(_allocationId);
-    }
-
-    /// <summary>
-    /// Test the association of an elastic IP address to an instance.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(12)]
-    public async Task AssociateAddressTest()
-    {
-        _associationId = await _ec2Wrapper.AssociateAddress(_allocationId, _ec2InstanceId);
-        Assert.NotNull(_associationId);
-    }
-
-    /// <summary>
-    /// Test the authorization of the local computer for ingress to an
-    /// EC2 instance.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(5)]
-    public async Task AuthorizeSecurityGroupIngressTest()
-    {
-        var success = await _ec2Wrapper.AuthorizeSecurityGroupIngress(_groupName);
-        Assert.True(success, "Could not authorize the group for ingress.");
-    }
-
-    /// <summary>
     /// Test the creation of an Amazon EC2 key pair.
     /// </summary>
     /// <returns></returns>
     [Fact()]
-    [Order(2)]
+    [Order(1)]
     public async Task CreateKeyPairTest()
     {
         _keyPair = await _ec2Wrapper.CreateKeyPair(_keyPairName);
@@ -105,119 +68,16 @@ public class EC2WrapperTests
     }
 
     /// <summary>
-    /// Test the creation of an Amazon EC2 security group.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(4)]
-    public async Task CreateSecurityGroupTest()
-    {
-        _secGroupId = await _ec2Wrapper.CreateSecurityGroup(_groupName, _groupDescription);
-        Assert.NotNull(_secGroupId);
-    }
-
-    /// <summary>
-    /// Test the deletion of an Amazon EC2 key pair.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(19)]
-    public async Task DeleteKeyPairTest()
-    {
-        var success = await _ec2Wrapper.DeleteKeyPair(_keyPair.KeyName);
-        Assert.True(success, "Could not delete the key pair.");
-    }
-
-    /// <summary>
-    /// Test the deletion of an Amazon EC2 security group.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(18)]
-    public async Task DeleteSecurityGroupTest()
-    {
-        var success = await _ec2Wrapper.DeleteSecurityGroup(_secGroupId);
-        Assert.True(success, "Couldn't delete the security group.");
-    }
-
-    /// <summary>
-    /// Test the deletion of the temporary PEM file.
+    /// Test the ability to save the values of an Amazon EC2 key pair to a
+    /// temporary file.
     /// </summary>
     [Fact()]
-    [Order(20)]
-    public void DeleteTempFileTest()
+    [Order(2)]
+    public void SaveKeyPairTest()
     {
-        try
-        {
-            _ec2Wrapper.DeleteTempFile(_tempFileName);
-        }
-        catch (Exception ex)
-        {
-            Assert.True(false, $"Could not delete the temporary file. Error: {ex.Message}");
-        }
-    }
+        _tempFileName = _ec2Wrapper.SaveKeyPair(_keyPair);
+        Assert.NotNull(_tempFileName);
 
-    /// <summary>
-    /// Test the method which retrieves a list of images.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(7)]
-    public async Task DescribeImagesTest()
-    {
-        // Get list of available Amazon Linux 2 Amazon Machine Images (AMIs).
-        var parameters = await _ssmWrapper.GetParametersByPath("/aws/service/ami-amazon-linux-latest");
-
-        List<string> imageIds = parameters.Select(param => param.Value).ToList();
-
-        _images = await _ec2Wrapper.DescribeImages(imageIds);
-        _imageId = _images[0].ImageId;
-        _architecture = _images[0].Architecture;
-        Assert.NotEmpty(_images);
-    }
-
-    /// <summary>
-    /// Test the retrieval of information about an EC2 instance.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(5)]
-    public async Task DescribeInstanceTest()
-    {
-        var instance = await _ec2Wrapper.DescribeInstance(_ec2InstanceId);
-        Assert.NotNull(instance);
-
-    }
-
-    /// <summary>
-    /// Test the retrieval of information about multiple EC2 instances.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(9)]
-    public async Task DescribeInstancesTest()
-    {
-        try
-        {
-            await _ec2Wrapper.DescribeInstances();
-        }
-        catch (Exception ex)
-        {
-            Assert.True(false, $"Describe instances failed. Error: {ex.Message}");
-        }
-    }
-
-    /// <summary>
-    /// Test the ability to retrieve information about available instance
-    /// types.
-    /// </summary>
-    /// <returns>Async task.</returns>
-    [Fact()]
-    [Order(8)]
-    public async Task DescribeInstanceTypesTest()
-    {
-        _instanceTypes = await _ec2Wrapper.DescribeInstanceTypes(_architecture);
-        Assert.NotEmpty(_instanceTypes);
     }
 
     /// <summary>
@@ -234,6 +94,31 @@ public class EC2WrapperTests
     }
 
     /// <summary>
+    /// Test the creation of an Amazon EC2 security group.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(4)]
+    public async Task CreateSecurityGroupTest()
+    {
+        _secGroupId = await _ec2Wrapper.CreateSecurityGroup(_groupName, _groupDescription);
+        Assert.NotNull(_secGroupId);
+    }
+
+    /// <summary>
+    /// Test the authorization of the local computer for ingress to an
+    /// EC2 instance.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(5)]
+    public async Task AuthorizeSecurityGroupIngressTest()
+    {
+        var success = await _ec2Wrapper.AuthorizeSecurityGroupIngress(_groupName);
+        Assert.True(success, "Could not authorize the group for ingress.");
+    }
+
+    /// <summary>
     /// Test the ability to retrieve a list of existing Amazon EC2 security
     /// groups.
     /// </summary>
@@ -247,40 +132,48 @@ public class EC2WrapperTests
     }
 
     /// <summary>
-    /// Test the ability to disassociate an IP address from an Amazon EC2
-    /// Elastic IP address.
+    /// Test the retrieval of information about an EC2 instance.
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(15)]
-    public async Task DisassociateIpTest()
+    [Order(7)]
+    public async Task DescribeInstanceTest()
     {
-        var success = await _ec2Wrapper.DisassociateIp(_associationId);
-        Assert.True(success, "Could not disassociate IP address.");
+        var instance = await _ec2Wrapper.DescribeInstance(_ec2InstanceId);
+        Assert.NotNull(instance);
+
     }
 
     /// <summary>
-    /// Test the ability to retrieve a list of Amazon EC2 AMIs.
+    /// Test the method which retrieves a list of images.
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(24)]
-    public async Task GetEc2AmiListTest()
+    [Order(8)]
+    public async Task DescribeImagesTest()
     {
-        _images = await _ec2Wrapper.GetEC2AmiList();
+        // Get list of available Amazon Linux 2 Amazon Machine Images (AMIs).
+        var parameters = await _ssmWrapper.GetParametersByPath("/aws/service/ami-amazon-linux-latest");
+
+        List<string> imageIds = parameters.Select(param => param.Value).ToList();
+
+        _images = await _ec2Wrapper.DescribeImages(imageIds);
+        _imageId = _images[0].ImageId;
+        _architecture = _images[0].Architecture;
         Assert.NotEmpty(_images);
     }
 
     /// <summary>
-    /// Test the ability to release an Amazon EC2 Elastic IP address.
+    /// Test the ability to retrieve information about available instance
+    /// types.
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(16)]
-    public async Task ReleaseAddressTest()
+    [Order(9)]
+    public async Task DescribeInstanceTypesTest()
     {
-        var success = await _ec2Wrapper.ReleaseAddress(_allocationId);
-        Assert.True(success, "Could not releases the address.");
+        _instanceTypes = await _ec2Wrapper.DescribeInstanceTypes(_architecture);
+        Assert.NotEmpty(_instanceTypes);
     }
 
     /// <summary>
@@ -304,16 +197,70 @@ public class EC2WrapperTests
     }
 
     /// <summary>
-    /// Test the ability to save the values of an Amazon EC2 key pair to a
-    /// temporary file.
+    /// Test the retrieval of information about multiple EC2 instances.
     /// </summary>
+    /// <returns>Async task.</returns>
     [Fact()]
-    [Order(2)]
-    public void SaveKeyPairTest()
+    [Order(11)]
+    public async Task DescribeInstancesTest()
     {
-        _tempFileName = _ec2Wrapper.SaveKeyPair(_keyPair);
-        Assert.NotNull(_tempFileName);
+        try
+        {
+            await _ec2Wrapper.DescribeInstances();
+        }
+        catch (Exception ex)
+        {
+            Assert.True(false, $"Describe instances failed. Error: {ex.Message}");
+        }
+    }
 
+    /// <summary>
+    /// Test the allocation of an Elastic IP address.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(12)]
+    public async Task AllocateAddressTest()
+    {
+        _allocationId = await _ec2Wrapper.AllocateAddress();
+        Assert.NotNull(_allocationId);
+    }
+
+    /// <summary>
+    /// Test the association of an elastic IP address to an instance.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(13)]
+    public async Task AssociateAddressTest()
+    {
+        _associationId = await _ec2Wrapper.AssociateAddress(_allocationId, _ec2InstanceId);
+        Assert.NotNull(_associationId);
+    }
+
+    /// <summary>
+    /// Test the ability to stop a running EC2 instance.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(14)]
+    public async Task StopInstancesTest()
+    {
+        try
+        {
+            await _ec2Wrapper.StopInstances(_ec2InstanceId);
+            var hasStopped = false;
+            do
+            {
+                hasStopped = await _ec2Wrapper.WaitForInstanceState(_ec2InstanceId, InstanceStateName.Stopped);
+            } while (!hasStopped);
+
+            Assert.True(hasStopped);
+        }
+        catch (Exception ex)
+        {
+            Assert.True(false, $"Could not stop the instance. Error: {ex.Message}");
+        }
     }
 
     /// <summary>
@@ -321,7 +268,7 @@ public class EC2WrapperTests
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(14)]
+    [Order(15)]
     public async Task StartInstancesTest()
     {
         try
@@ -344,28 +291,28 @@ public class EC2WrapperTests
     }
 
     /// <summary>
-    /// Test the ability to stop a running EC2 instance.
+    /// Test the ability to disassociate an IP address from an Amazon EC2
+    /// Elastic IP address.
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(13)]
-    public async Task StopInstancesTest()
+    [Order(16)]
+    public async Task DisassociateIpTest()
     {
-        try
-        {
-            await _ec2Wrapper.StopInstances(_ec2InstanceId);
-            var hasStopped = false;
-            do
-            {
-                hasStopped = await _ec2Wrapper.WaitForInstanceState(_ec2InstanceId, InstanceStateName.Stopped);
-            } while (!hasStopped);
+        var success = await _ec2Wrapper.DisassociateIp(_associationId);
+        Assert.True(success, "Could not disassociate IP address.");
+    }
 
-            Assert.True(hasStopped);
-        }
-        catch (Exception ex)
-        {
-            Assert.True(false, $"Could not stop the instance. Error: {ex.Message}");
-        }
+    /// <summary>
+    /// Test the ability to release an Amazon EC2 Elastic IP address.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(17)]
+    public async Task ReleaseAddressTest()
+    {
+        var success = await _ec2Wrapper.ReleaseAddress(_allocationId);
+        Assert.True(success, "Could not releases the address.");
     }
 
     /// <summary>
@@ -373,7 +320,7 @@ public class EC2WrapperTests
     /// </summary>
     /// <returns>Async task.</returns>
     [Fact()]
-    [Order(17)]
+    [Order(18)]
     public async Task TerminateInstanceTest()
     {
         var stateChange = await _ec2Wrapper.TerminateInstances(_ec2InstanceId);
@@ -386,5 +333,57 @@ public class EC2WrapperTests
         } while (!hasTerminated);
 
         Assert.True(hasTerminated);
+    }
+    /// <summary>
+    /// Test the deletion of an Amazon EC2 security group.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(19)]
+    public async Task DeleteSecurityGroupTest()
+    {
+        var success = await _ec2Wrapper.DeleteSecurityGroup(_secGroupId);
+        Assert.True(success, "Couldn't delete the security group.");
+    }
+
+    /// <summary>
+    /// Test the deletion of an Amazon EC2 key pair.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(20)]
+    public async Task DeleteKeyPairTest()
+    {
+        var success = await _ec2Wrapper.DeleteKeyPair(_keyPair.KeyName);
+        Assert.True(success, "Could not delete the key pair.");
+    }
+
+    /// <summary>
+    /// Test the deletion of the temporary PEM file.
+    /// </summary>
+    [Fact()]
+    [Order(21)]
+    public void DeleteTempFileTest()
+    {
+        try
+        {
+            _ec2Wrapper.DeleteTempFile(_tempFileName);
+        }
+        catch (Exception ex)
+        {
+            Assert.True(false, $"Could not delete the temporary file. Error: {ex.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Test the ability to retrieve a list of Amazon EC2 AMIs.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact()]
+    [Order(22)]
+    public async Task GetEc2AmiListTest()
+    {
+        _images = await _ec2Wrapper.GetEC2AmiList();
+        Assert.NotEmpty(_images);
     }
 }
