13aef84b65fec6106b9781f079cfeab67191c1df
ford prior <108086978+ford-at-aws@users.noreply.github.com>
2022-12-26 14:08:10 -0600

    Fords branch (#4118)
    * Stack for batch fargate.
    
    The cluster isn't stabilizing.
    
    * updates for working version
    
    * Delete cdk.context.json
    
    * Update batch_fargate-stack.ts
    
    * Delete package-lock.json
    
    Co-authored-by: David Souther <davidsouther@gmail.com>

---
+++ resources/cdk/batch_fargate/test/batch_fargate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BatchFargate from '../lib/batch_fargate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/batch_fargate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BatchFargate.BatchFargateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
