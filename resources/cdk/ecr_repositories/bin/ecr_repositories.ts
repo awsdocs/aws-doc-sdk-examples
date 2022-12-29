9aea0f5a47122748c5844e953dfa2c0226743ebf
ford prior <fprior@amazon.com>
2022-12-29 15:11:52 -0600

    actually, no batch

--- resources/cdk/ecr_repositories/bin/ecr_repositories.ts
+++ resources/cdk/ecr_repositories/bin/ecr_repositories.ts
@@ -6,14 +6,5 @@ import { ContainerImage } from "../lib/ecr_repositories-stack";
 
 const app = new cdk.App();
 new ContainerImage(app, "EcrRepositoriesStack", {
-    /* If you don't specify 'env', this stack will be environment-agnostic.
-     * Account/Region-dependent features and context lookups will not work,
-     * but a single synthesized template can be deployed anywhere. */
-    /* Uncomment the next line to specialize this stack for the AWS Account
-     * and Region that are implied by the current CLI configuration. */
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-    /* Uncomment the next line if you know exactly what Account and Region you
-     * want to deploy the stack to. */
-    // env: { account: '123456789012', region: 'us-east-1' }
-    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
