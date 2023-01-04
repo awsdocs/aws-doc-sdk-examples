24229b8a205d984d30801bde644351df5dea9773
Eric Shepherd <ericsh@amazon.com>
2023-01-04 14:28:32 -0500

    Require minimum of SDK 0.8.0

--- swift/example_code/s3/DeleteObjects/Package.swift
+++ swift/example_code/s3/DeleteObjects/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(
             url: "https://github.com/awslabs/aws-sdk-swift",
-            from: "0.7.0"
+            from: "0.8.0"
         ),
         .package(
             url: "https://github.com/apple/swift-argument-parser.git",
