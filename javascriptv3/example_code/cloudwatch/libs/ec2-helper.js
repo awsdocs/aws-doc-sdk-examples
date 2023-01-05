2cb399728e12c9591ce54bafe871c6b448602e92
Corey Pyle <corepyle@amazon.com>
2023-01-05 11:30:05 -0500

    add libs package

--- javascriptv3/example_code/cloudwatch/libs/ec2-helper.js
+++ javascriptv3/example_code/cloudwatch/libs/ec2-helper.js
@@ -7,8 +7,8 @@ import {
   RunInstancesCommand,
   TerminateInstancesCommand,
 } from "@aws-sdk/client-ec2";
-import { retry } from '../../libs/utils/util-timers.js';
-import { DEFAULT_REGION } from "../../libs/utils/util-aws-sdk.js";
+import { retry } from 'libs/utils/util-timers.js';
+import { DEFAULT_REGION } from "libs/utils/util-aws-sdk.js";
 
 const client = new EC2Client({ region: DEFAULT_REGION });
 const retryEvery3Seconds = retry({ intervalInMs: 3000, maxRetries: 5 });
