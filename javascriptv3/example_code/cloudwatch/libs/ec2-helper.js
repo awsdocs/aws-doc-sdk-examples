7cc423d4eb30818d9e3848083b430ce7f7983718
Corey Pyle <corepyle@amazon.com>
2023-01-05 10:31:48 -0500

    update vite config and fix some test issues

--- javascriptv3/example_code/cloudwatch/libs/ec2-helper.js
+++ javascriptv3/example_code/cloudwatch/libs/ec2-helper.js
@@ -7,19 +7,22 @@ import {
   RunInstancesCommand,
   TerminateInstancesCommand,
 } from "@aws-sdk/client-ec2";
+import { retry } from '../../libs/utils/util-timers.js';
 import { DEFAULT_REGION } from "../../libs/utils/util-aws-sdk.js";
 
 const client = new EC2Client({ region: DEFAULT_REGION });
+const retryEvery3Seconds = retry({ intervalInMs: 3000, maxRetries: 5 });
 
 export const runEC2Instance = async () => {
   // Free Tier - Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
-  const imageId = "ami-0b0dcb5067f052a63";
+  const imageId = "ami-0b5eea76982371e91";
   const command = new RunInstancesCommand({
     MinCount: 1,
     MaxCount: 1,
     ImageId: imageId,
+    InstanceType: "t2.micro"
   });
-  const { Instances } = await client.send(command);
+  const { Instances } = await retryEvery3Seconds(() => client.send(command));
   return Instances[0].InstanceId;
 };
 
