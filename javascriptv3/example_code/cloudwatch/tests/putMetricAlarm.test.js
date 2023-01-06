5ae13e6669555e719620466fa8080cd94cd59518
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-01-06 08:23:00 -0500

    Revert "Cloudwatch Cleanup (#4191)" (#4195)
    This reverts commit e5278759da3992a9aca6bfb5177388da966b02ee.

---
+++ javascriptv3/example_code/cloudwatch/tests/putMetricAlarm.test.js
@@ -0,0 +1,16 @@
+
+jest.mock("../src/libs/cloudWatchClient.js");
+jest.mock("@aws-sdk/client-cloudwatch");
+
+// Get service clients module and commands.
+import 'regenerator-runtime/runtime'
+import { run, params } from "../src/putMetricAlarm.js";
+import { cwClient } from "../src/libs/cloudWatchClient";
+
+describe("@aws-sdk/client-cloudwatch mock", () => {
+  it("should successfully mock CloudWatch client", async () => {
+    cwClient.send.mockResolvedValue({ isMock: true });
+    const response = await run(params);
+    expect(response.isMock).toEqual(true);
+  });
+});
