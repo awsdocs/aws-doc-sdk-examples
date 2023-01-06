b6c23279911b22cf54c252a684b24f5cd203ba32
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-01-06 08:19:43 -0500

    Revert "Cloudwatch Cleanup (#4191)"
    This reverts commit e5278759da3992a9aca6bfb5177388da966b02ee.

---
+++ javascriptv3/example_code/cloudwatch/tests/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleDirectories: ["node_modules"],
+  testMatch: ["**/tests/*.test.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  }
+};
