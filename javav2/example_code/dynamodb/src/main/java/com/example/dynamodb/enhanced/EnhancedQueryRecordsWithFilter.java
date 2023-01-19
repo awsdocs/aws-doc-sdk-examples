d76f0c7c1a9633b3419594bd5012fee8d1a3b6c2
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2023-01-19 15:07:21 -0500

    Add files via upload

---
+++ javav2/example_code/dynamodb/src/main/java/com/example/dynamodb/enhanced/EnhancedQueryRecordsWithFilter.java
@@ -0,0 +1,22 @@
+package com.example.dynamodb;
+
+// snippet-start:[dynamodb.java2.mapping.queryfilter.import]
+// snippet-end:[dynamodb.java2.mapping.queryfilter.import]
+
+/*
+ * This example is depreciated, see the Scenario example.
+ */
+public class EnhancedQueryRecordsWithFilter {
+
+    // Query the Customer table using a filter.
+    public static void main(String[] args) {
+
+        System.out.println("This example is depreciated, see the Scenario example");
+    }
+
+    // snippet-start:[dynamodb.java2.mapping.queryfilter.main]
+    public static void queryTableFilter() {
+        System.out.println("This example is depreciated, see the Scenario example");
+    }
+    // snippet-end:[dynamodb.java2.mapping.queryfilter.main]
+}
