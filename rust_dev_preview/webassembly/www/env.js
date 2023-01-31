c0240c2e6c1de56c76fd085bc7db004a52713b8d
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-02-01 00:10:07 +0100

    chore: retrieve credentials from browser cookie

--- rust_dev_preview/webassembly/www/env.js
+++ rust_dev_preview/webassembly/www/env.js
@@ -7,10 +7,33 @@ export const now = () => {
     return Date.now();
 };
 
-export const retrieveCredentials = () => {
-    return {
-        accessKeyId: "access_key",
-        secretAccessKey: "secret_key",
-        sessionToken: "session_token"
+const parseCookie = str =>
+  str
+  .split(';')
+  .map(v => v.split('='))
+  .reduce((acc, v) => {
+    if (v[0] && v[1]) {
+        acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
     }
+    return acc;
+  }, {});
+
+const EXAMPLE_CREDENTIALS = {
+    accessKeyId: "access_key",
+    secretAccessKey: "secret_key",
+    sessionToken: "session_token"
+};
+
+export const retrieveCredentials = () => {
+    let cookie = parseCookie(document.cookie || "");
+    return cookie.credentials_aws ? JSON.parse(cookie.credentials_aws) : EXAMPLE_CREDENTIALS;
 }
+
+export const setCredentials = () => {
+    const credentials = encodeURIComponent(JSON.stringify({
+        ...EXAMPLE_CREDENTIALS,
+    }));
+    document.cookie = `credentials_aws=${credentials}; max-age=43200; path=/;`;
+}
+
+document.getElementById("set-credentials").addEventListener("click", setCredentials)
