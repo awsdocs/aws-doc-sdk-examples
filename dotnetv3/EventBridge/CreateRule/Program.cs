86d92387f633563f2f98250004f7c277b60b139a
Emre Ceyhan <a@emreceyhan.com>
2023-02-03 21:49:33 +0300

    add eventbirdge crate rule example for dotnetv3

---
+++ dotnetv3/EventBridge/CreateRule/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.EventBridge;
+using Amazon.EventBridge.Model;
+
+var client = new AmazonEventBridgeClient();
+
+var request = new PutRuleRequest
+{
+    Name = "ExampleScheduledRule",
+    ScheduleExpression = "rate(1 hour)",
+    State = RuleState.ENABLED
+};
+
+var response = await client.PutRuleAsync(request);
+
+
