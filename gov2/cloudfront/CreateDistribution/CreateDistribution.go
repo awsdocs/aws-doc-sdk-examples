caf206239e18a639b64de800fa9c90600ab090d2
tae2089 <tae2089002@gmail.com>
2023-02-05 17:04:13 +0900

    add copyright notice

--- gov2/cloudfront/CreateDistribution/CreateDistribution.go
+++ gov2/cloudfront/CreateDistribution/CreateDistribution.go
@@ -1,3 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[gov2.cloudfront.CreateDistribution]
+
 package main
 
 import (
