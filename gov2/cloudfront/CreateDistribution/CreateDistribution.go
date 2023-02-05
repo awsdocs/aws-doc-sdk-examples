167af0f1a16ab84a87c4358d11ca1a82a5eb6e71
tae2089 <tae2089002@gmail.com>
2023-02-05 17:01:21 +0900

    create cloud front code

---
+++ gov2/cloudfront/CreateDistribution/CreateDistribution.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
+	cloudfrontTypes "github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	awshttp "github.com/aws/smithy-go/transport/http"
+)
+
+// main uses the AWS SDK for Go V2 to create an Amazon CloudFront distribution.
+// This example uses the default settings specified in your shared credentials
+// and config files.
+func CreateDistribution(s3Client *s3.Client, cloudfrontClient *cloudfront.Client, bucketName, originAccessIdentityID, certificateSSLArn, domain string) (*cloudfront.CreateDistributionOutput, error) {
+	bucket, err := s3Client.HeadBucket(context.TODO(), &s3.HeadBucketInput{
+		Bucket: &bucketName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	bucketResponseMetadata := middleware.GetRawResponse(bucket.ResultMetadata).(*awshttp.Response)
+
+	region := bucketResponseMetadata.Header.Get("x-amz-bucket-region")
+	originDomain := bucketName + ".s3." + region + ".amazonaws.com"
+
+	if err != nil {
+		return nil, err
+	}
+
+	cloudfrontResponse, err := cloudfrontClient.CreateDistribution(context.TODO(), &cloudfront.CreateDistributionInput{
+		DistributionConfig: &cloudfrontTypes.DistributionConfig{
+			Enabled:           aws.Bool(true),
+			CallerReference:   &originDomain,
+			Comment:           &originDomain,
+			IsIPV6Enabled:     aws.Bool(false),
+			PriceClass:        cloudfrontTypes.PriceClassPriceClass100,
+			HttpVersion:       cloudfrontTypes.HttpVersionHttp11,
+			DefaultRootObject: aws.String("index.html"),
+			Aliases: &cloudfrontTypes.Aliases{
+				Quantity: aws.Int32(1),
+				Items:    []string{domain},
+			},
+			ViewerCertificate: &cloudfrontTypes.ViewerCertificate{
+				ACMCertificateArn: aws.String(certificateSSLArn),
+				SSLSupportMethod:  cloudfrontTypes.SSLSupportMethodSniOnly,
+			},
+			CustomErrorResponses: &cloudfrontTypes.CustomErrorResponses{
+				Quantity: aws.Int32(1),
+				Items: []cloudfrontTypes.CustomErrorResponse{
+					{
+						ErrorCode:          aws.Int32(403),
+						ResponseCode:       aws.String("200"),
+						ErrorCachingMinTTL: aws.Int64(10),
+						ResponsePagePath:   aws.String("/index.html"),
+					},
+				},
+			},
+			Origins: &cloudfrontTypes.Origins{
+				Quantity: aws.Int32(1),
+				Items: []cloudfrontTypes.Origin{
+					{
+						DomainName: aws.String(originDomain),
+						Id:         aws.String(originDomain),
+						S3OriginConfig: &cloudfrontTypes.S3OriginConfig{
+							OriginAccessIdentity: aws.String("origin-access-identity/cloudfront/" + originAccessIdentityID),
+						},
+					},
+				},
+			},
+			CacheBehaviors: nil,
+			DefaultCacheBehavior: &cloudfrontTypes.DefaultCacheBehavior{
+				TargetOriginId:       aws.String(originDomain),
+				Compress:             aws.Bool(true),
+				MinTTL:               aws.Int64(200),
+				ViewerProtocolPolicy: cloudfrontTypes.ViewerProtocolPolicyRedirectToHttps,
+				ForwardedValues: &cloudfrontTypes.ForwardedValues{
+					Cookies: &cloudfrontTypes.CookiePreference{
+						Forward: cloudfrontTypes.ItemSelectionNone,
+					},
+					QueryString: aws.Bool(true),
+				},
+				AllowedMethods: &cloudfrontTypes.AllowedMethods{
+					Quantity: aws.Int32(2),
+					Items: []cloudfrontTypes.Method{
+						cloudfrontTypes.MethodGet,
+						cloudfrontTypes.MethodHead,
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cloudfrontResponse, nil
+}
+
+func main() {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+
+	s3Client := s3.NewFromConfig(sdkConfig)
+	cloudfrontClient := cloudfront.NewFromConfig(sdkConfig)
+	bucketName := "example-bucket-name"
+	originAccessIdentityID := "E12345678" // example id
+	certificateSSLArn := "arn:aws:us-east-1:1234567890:certificate/7a4c4086-706d-4f6f-a8a2-2c7cebad7264"
+	domain := "aws.example.com"
+	result, err := CreateDistribution(s3Client, cloudfrontClient, bucketName, originAccessIdentityID, certificateSSLArn, domain)
+	fmt.Println(result.Distribution.ARN)
+}
---
+++ gov2/cloudfront/go.mod
@@ -0,0 +1,26 @@
+module example.aws/go-v2/examples/cloudfront
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.17.4 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.12 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.12 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.22 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.28 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.22 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.29 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.19 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudfront v1.24.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.22 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.22 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.30.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.3 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
