c78ebd25e305af72f43ed5f273fbf2b1490ff7ef
Corey Pyle <corepyle@amazon.com>
2023-02-08 17:10:22 -0500

    add authorize-security-group-ingress, add tests, onboard to sos

---
+++ javascriptv3/example_code/ec2/tests/authorize-security-group-ingress.unit.test.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { describe, it, expect, vi } from "vitest";
+
+const send = vi.fn();
+
+vi.doMock("@aws-sdk/client-ec2", async () => {
+  const actual = await vi.importActual("@aws-sdk/client-ec2");
+  return {
+    ...actual,
+    EC2Client: class {
+      send = send;
+    },
+  };
+});
+
+import { main } from "../actions/authorize-security-group-ingress.js";
+
+describe("authorize-security-group-ingress", () => {
+  it("should log the formatted security group rules", async () => {
+    const logSpy = vi.spyOn(console, "log");
+    const rules = [{ foo: "bar" }];
+    send.mockResolvedValueOnce({
+      SecurityGroupRules: rules,
+    });
+
+    await main();
+
+    expect(logSpy).toHaveBeenCalledWith(JSON.stringify(rules, null, 2));
+  });
+});
