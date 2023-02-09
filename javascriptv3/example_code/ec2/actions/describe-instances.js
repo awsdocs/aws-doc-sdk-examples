76d39193fbd4693167c8b7af50d720086251c673
Corey Pyle <corepyle@amazon.com>
2023-02-09 16:34:42 -0500

    update describe-instances, add tests, onboard to sos

--- javascriptv3/example_code/ec2/actions/describe-instances.js
+++ javascriptv3/example_code/ec2/actions/describe-instances.js
@@ -1,31 +1,47 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-
-ABOUT THIS NODE.JS EXAMPLE: This example works with the AWS SDK for JavaScript version 3 (v3),
-which is available at https://github.com/aws/aws-sdk-js-v3. This example is in the 'AWS SDK for JavaScript v3 Developer Guide' at
-https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/ec2-example-managing-instances.html
-
-Purpose:
-ec2_describeinstances.js demonstrates how to retrieve information about one or more Amazon EC2 instances.
-
-Running the code:
-node ec2_describeinstances.js
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-// snippet-start:[ec2.JavaScript.Instances.describeInstancesV3]
-// Import required AWS SDK clients and commands for Node.js
+import { fileURLToPath } from "url";
+
+// snippet-start: [ec2.JavaScript.Instances.describeInstancesV3]
 import { DescribeInstancesCommand } from "@aws-sdk/client-ec2";
-import { ec2Client } from "./libs/ec2Client";
-const run = async () => {
+
+import { client } from "../libs/client.js";
+
+// List all of your EC2 instances running with x86_64 architecture that were
+// launched this this month.
+export const main = async () => {
+  const d = new Date();
+  const year = d.getFullYear();
+  const month = `0${d.getMonth() + 1}`.slice(-2);
+  const launchTimePattern = `${year}-${month}-*`;
+  const command = new DescribeInstancesCommand({
+    Filters: [
+      { Name: "architecture", Values: ["x86_64"] },
+      { Name: "instance-state-name", Values: ["running"] },
+      {
+        Name: "launch-time",
+        Values: [launchTimePattern],
+      },
+    ],
+  });
+
   try {
-    const data = await ec2Client.send(new DescribeInstancesCommand({}));
-    console.log("Success", JSON.stringify(data));
-    return data;
+    const { Reservations } = await client.send(command);
+    const instanceList = Reservations.reduce((prev, current) => {
+      return prev.concat(current.Instances);
+    }, []);
+
+    console.log(instanceList);
   } catch (err) {
-    console.log("Error", err);
+    console.error(err);
   }
 };
-run();
-// snippet-end:[ec2.JavaScript.Instances.describeInstancesV3]
-// For unit tests only.
-// module.exports ={run, params};
+// snippet-end: [ec2.JavaScript.Instances.describeInstancesV3]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main();
+}
