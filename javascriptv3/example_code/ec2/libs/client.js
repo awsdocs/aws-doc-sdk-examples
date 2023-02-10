b469504f1c38e0945a870ac58680e38c07f981e6
Corey Pyle <corepyle@amazon.com>
2023-02-10 11:30:02 -0500

    update allocate-address, add tests, onboard to sos

--- javascriptv3/example_code/ec2/libs/client.js
+++ javascriptv3/example_code/ec2/libs/client.js
@@ -5,6 +5,6 @@
 
 // snippet-start:[ec2.JavaScript.createclientv3]
 import { EC2Client } from "@aws-sdk/client-ec2";
-const REGION = "us-east-1";
+export const REGION = "us-east-1";
 export const client = new EC2Client({ region: REGION });
 // snippet-end:[ec2.JavaScript.createclientv3]
