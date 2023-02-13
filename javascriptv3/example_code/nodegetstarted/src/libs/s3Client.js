7c948993bf8b8db454b2acff183da1fb9593f72c
Michael McBride <108891717+MichaelsEngineering@users.noreply.github.com>
2023-02-13 14:01:53 -0800

    S3: create sample_key.txt

--- javascriptv3/example_code/nodegetstarted/src/libs/s3Client.js
+++ javascriptv3/example_code/nodegetstarted/src/libs/s3Client.js
@@ -12,9 +12,9 @@ Inputs (replace in code):
 
 */
 // snippet-start:[GettingStarted.JavaScript.createclientv3]
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client } from '@aws-sdk/client-s3';
 // Set the AWS Region.
-const REGION = "REGION"; //e.g. "us-east-1"
+const REGION = 'us-west-2'; //e.g. "us-east-1"
 // Create an Amazon S3 service client object.
 const s3Client = new S3Client({ region: REGION });
 export { s3Client };
