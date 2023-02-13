bc6c3531f7375593694ef6ed4b7378b7cd4b4bea
ford prior <fprior@amazon.com>
2023-02-13 17:47:26 -0600

    changes

--- ruby/example_code/sqs/scenario_getting_started.rb
+++ ruby/example_code/sqs/scenario_getting_started.rb
@@ -39,6 +39,55 @@ end
 queue_url = sqs.get_queue_url(queue_name: queue_name).queue_url
 puts queue_url
 
+begin
+  # Create a message with three custom attributes: Title, Author, and WeeksOn.
+  send_message_result = sqs.send_message({
+                                           queue_url: queue_url,
+                                           message_body: "Information about current NY Times fiction bestseller for week of 2016-12-11.",
+                                           message_attributes: {
+                                             "Title" => {
+                                               string_value: "The Whistler",
+                                               data_type: "String"
+                                             },
+                                             "Author" => {
+                                               string_value: "John Grisham",
+                                               data_type: "String"
+                                             },
+                                             "WeeksOn" => {
+                                               string_value: "6",
+                                               data_type: "Number"
+                                             }
+                                           }
+                                         })
+  puts send_message_result.message_id
+rescue Aws::SQS::Errors::NonExistentQueue
+  puts "A queue named '#{queue_name}' does not exist."
+  exit(false)
+end
+
+# Receive the message in the queue.
+receive_message_result = sqs.receive_message({
+                                               queue_url: queue_url,
+                                               message_attribute_names: ["All"], # Receive all custom attributes.
+                                               max_number_of_messages: 1, # Receive at most one message.
+                                               wait_time_seconds: 0 # Do not wait to check for the message.
+                                             })
+
+# Display information about the message.
+# Display the message's body and each custom attribute value.
+receive_message_result.messages.each do |message|
+  puts message.body
+  puts "Title: #{message.message_attributes["Title"]["string_value"]}"
+  puts "Author: #{message.message_attributes["Author"]["string_value"]}"
+  puts "WeeksOn: #{message.message_attributes["WeeksOn"]["string_value"]}"
+
+  # Delete the message from the queue.
+  sqs.delete_message({
+                       queue_url: queue_url,
+                       receipt_handle: message.receipt_handle
+                     })
+end
+
 # Delete the queue.
 sqs.delete_queue(queue_url: queue_url)
 # snippet-end:[ruby.example_code.sqs.ScenarioGettingStarted]
