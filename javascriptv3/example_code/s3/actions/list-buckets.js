673028621efc420789124fd9aec5cfba0812b713
Corey Pyle <corepyle@amazon.com>
2023-02-20 15:39:00 -0500

    refactor list-buckets, add test, update metadata

--- javascriptv3/example_code/s3/actions/list-buckets.js
+++ javascriptv3/example_code/s3/actions/list-buckets.js
@@ -1,34 +1,33 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-ABOUT THIS NODE.JS EXAMPLE: This example works with the AWS SDK for JavaScript version 3 (v3),
-which is pending release.  The preview version of the SDK is available
-at https://github.com/aws/aws-sdk-js-v3. This example is in the 'AWS SDK for JavaScript v3 Developer Guide' at
-https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/s3-example-creating-buckets.html.
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-Purpose:
-s3_listbuckets.js demonstrates how to list all the buckets in an AWS account.
+import { fileURLToPath } from "url";
 
-Inputs (replace in code):
-- BUCKET_NAME
-
-Running the code:
-s3_getbucketwebsite s3_listobjects.js
-*/
 // snippet-start:[s3.JavaScript.buckets.listBucketsV3]
-// Import required AWS SDK clients and commands for Node.js.
-import { ListBucketsCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./libs/s3Client.js"; // Helper function that creates an Amazon S3 service client module.
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 
-export const run = async () => {
+const client = new S3Client({});
+
+export const main = async () => {
+  const command = new ListBucketsCommand({});
+
   try {
-    const data = await s3Client.send(new ListBucketsCommand({}));
-    console.log("Success", data.Buckets);
-    return data; // For unit tests.
+    const { Owner, Buckets } = await client.send(command);
+    console.log(
+      `${Owner.DisplayName} owns ${Buckets.length} bucket${
+        Buckets.length === 1 ? "" : "s"
+      }:`
+    );
+    console.log(`${Buckets.map((b) => ` • ${b.Name}`).join("\n")}`);
   } catch (err) {
-    console.log("Error", err);
+    console.error(err);
   }
 };
-run();
 // snippet-end:[s3.JavaScript.buckets.listBucketsV3]
-// For unit testing only.
-// module.exports ={run, bucketParams};
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main();
+}
