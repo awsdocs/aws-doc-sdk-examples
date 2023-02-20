c7ab4b4d1ce52a47934a1b38af59783314716896
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-02-20 18:02:48 -0500

    4245 c++ rds mvp (#4397)
    C++ RDS MVP

--- cpp/example_code/rds/tests/rds_gtests.cpp
+++ cpp/example_code/rds/tests/rds_gtests.cpp
@@ -3,17 +3,16 @@
    SPDX-License-Identifier: Apache-2.0
 */
 
-#include "cognito_gtests.h"
+#include "rds_gtests.h"
 #include <fstream>
 #include <aws/core/client/ClientConfiguration.h>
 #include <aws/testing/mocks/http/MockHttpClient.h>
-#include <aws/core/http/standard/StandardHttpRequest.h>
 
-Aws::SDKOptions AwsDocTest::Cognito_GTests::s_options;
-std::unique_ptr<Aws::Client::ClientConfiguration> AwsDocTest::Cognito_GTests::s_clientConfig;
-static const char ALLOCATION_TAG[] = "COGNITO_GTEST";
+Aws::SDKOptions AwsDocTest::RDS_GTests::s_options;
+std::unique_ptr<Aws::Client::ClientConfiguration> AwsDocTest::RDS_GTests::s_clientConfig;
+static const char ALLOCATION_TAG[] = "RDS_GTEST";
 
-void AwsDocTest::Cognito_GTests::SetUpTestSuite() {
+void AwsDocTest::RDS_GTests::SetUpTestSuite() {
     InitAPI(s_options);
 
     // s_clientConfig must be a pointer because the client config must be initialized
@@ -21,24 +20,24 @@ void AwsDocTest::Cognito_GTests::SetUpTestSuite() {
     s_clientConfig = std::make_unique<Aws::Client::ClientConfiguration>();
 }
 
-void AwsDocTest::Cognito_GTests::TearDownTestSuite() {
+void AwsDocTest::RDS_GTests::TearDownTestSuite() {
     ShutdownAPI(s_options);
+
 }
 
-void AwsDocTest::Cognito_GTests::SetUp() {
+void AwsDocTest::RDS_GTests::SetUp() {
     m_savedBuffer = std::cout.rdbuf();
     std::cout.rdbuf(&m_coutBuffer);
 
     m_savedInBuffer = std::cin.rdbuf();
     std::cin.rdbuf(&m_cinBuffer);
 
-
     // The following code is needed for the AwsDocTest::MyStringBuffer::underflow exception.
     // Otherwise, an infinite loop occurs when looping for a result on an empty buffer.
     std::cin.exceptions(std::ios_base::badbit);
 }
 
-void AwsDocTest::Cognito_GTests::TearDown() {
+void AwsDocTest::RDS_GTests::TearDown() {
     if (m_savedBuffer != nullptr) {
         std::cout.rdbuf(m_savedBuffer);
         m_savedBuffer = nullptr;
@@ -51,7 +50,11 @@ void AwsDocTest::Cognito_GTests::TearDown() {
     }
 }
 
-void AwsDocTest::Cognito_GTests::AddCommandLineResponses(
+Aws::String AwsDocTest::RDS_GTests::preconditionError() {
+    return "Failed to meet precondition.";
+}
+
+void AwsDocTest::RDS_GTests::AddCommandLineResponses(
         const std::vector<std::string> &responses) {
 
     std::stringstream stringStream;
@@ -61,9 +64,14 @@ void AwsDocTest::Cognito_GTests::AddCommandLineResponses(
     m_cinBuffer.str(stringStream.str());
 }
 
+int AwsDocTest::MyStringBuffer::underflow() {
+    int result = basic_stringbuf::underflow();
+    if (result == EOF) {
+        std::cerr << "Error AwsDocTest::MyStringBuffer::underflow." << std::endl;
+        throw std::underflow_error("AwsDocTest::MyStringBuffer::underflow");
+    }
 
-Aws::String AwsDocTest::Cognito_GTests::preconditionError() {
-    return "Failed to meet precondition.";
+    return result;
 }
 
 AwsDocTest::MockHTTP::MockHTTP() {
@@ -81,22 +89,22 @@ AwsDocTest::MockHTTP::~MockHTTP() {
     Aws::Http::InitHttp();
 }
 
-void AwsDocTest::MockHTTP::addResponseWithBody(const std::string &body) {
-    std::shared_ptr<Aws::Http::Standard::StandardHttpResponse> goodResponse = Aws::MakeShared<Aws::Http::Standard::StandardHttpResponse>(
-            ALLOCATION_TAG, requestTmp);
-    goodResponse->AddHeader("Content-Type", "text/json");
-    goodResponse->SetResponseCode(Aws::Http::HttpResponseCode::OK);
-    goodResponse->GetResponseBody() << body;
-    mockHttpClient->AddResponseToReturn(goodResponse);
-}
+bool AwsDocTest::MockHTTP::addResponseWithBody(const std::string &fileName,
+                                               Aws::Http::HttpResponseCode httpResponseCode) {
 
-
-int AwsDocTest::MyStringBuffer::underflow() {
-    int result = basic_stringbuf::underflow();
-    if (result == EOF) {
-        std::cerr << "Error AwsDocTest::MyStringBuffer::underflow." << std::endl;
-        throw std::underflow_error("AwsDocTest::MyStringBuffer::underflow");
+    std::ifstream inStream(std::string(SRC_DIR) + "/" + fileName);
+    if (inStream) {
+        std::shared_ptr<Aws::Http::Standard::StandardHttpResponse> goodResponse = Aws::MakeShared<Aws::Http::Standard::StandardHttpResponse>(
+                ALLOCATION_TAG, requestTmp);
+        goodResponse->AddHeader("Content-Type", "text/xml");
+        goodResponse->SetResponseCode(httpResponseCode);
+        goodResponse->GetResponseBody() << inStream.rdbuf();
+        mockHttpClient->AddResponseToReturn(goodResponse);
+        return true;
     }
 
-    return result;
+    std::cerr << "MockHTTP::addResponseWithBody open file error '" << fileName << "'."
+              << std::endl;
+
+    return false;
 }
