782d51814270872d63c72979182f496303608722
Corey Pyle <corepyle@amazon.com>
2023-02-20 09:46:04 -0500

    refactor put-bucket-cors example, add tests, clean up metadata

--- javascriptv3/example_code/s3/actions/put-bucket-cors.js
+++ javascriptv3/example_code/s3/actions/put-bucket-cors.js
@@ -1,77 +1,51 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-
-ABOUT THIS NODE.JS EXAMPLE: This sample is part of the SDK for JavaScript Developer Guide (scheduled for release September 2020) top
-https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/s3-example-configuring-buckets.html.
-
-Purpose:
-s3_setcors.js demonstrates how to set the CORS configuration of an Amazon S3 bucket.
-
-Inputs:
-- BUCKET_NAME (into command line below)
-
-Running the code:
-node s3_setcors.js BUCKET_NAME REGION
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
+
+import { fileURLToPath } from "url";
+
 // snippet-start:[s3.JavaScript.v3.cors.putBucketCors]
-// Import required AWS-SDK clients and commands for Node.js.
-import { PutBucketCorsCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./libs/s3Client.js"; // Helper function that creates an Amazon S3 service client module.
+import { PutBucketCorsCommand, S3Client } from "@aws-sdk/client-s3";
 
-// Set parameters.
-// Create initial parameters JSON for putBucketCors.
-const thisConfig = {
-  AllowedHeaders: ["Authorization"],
-  AllowedMethods: [],
-  AllowedOrigins: ["*"],
-  ExposeHeaders: [],
-  MaxAgeSeconds: 3000,
-};
+const client = new S3Client({})
 
-// Assemble the list of allowed methods based on command line parameters
-const allowedMethods = [];
-process.argv.forEach(function (val) {
-  if (val.toUpperCase() === "POST") {
-    allowedMethods.push("POST");
-  }
-  if (val.toUpperCase() === "GET") {
-    allowedMethods.push("GET");
-  }
-  if (val.toUpperCase() === "PUT") {
-    allowedMethods.push("PUT");
-  }
-  if (val.toUpperCase() === "PATCH") {
-    allowedMethods.push("PATCH");
-  }
-  if (val.toUpperCase() === "DELETE") {
-    allowedMethods.push("DELETE");
-  }
-  if (val.toUpperCase() === "HEAD") {
-    allowedMethods.push("HEAD");
-  }
-});
+// By default Amazon S3 does not allow cross-origin requests. Use this command
+// to explicitly allow cross-origin requests.
+export const main = async () => {
+  const command = new PutBucketCorsCommand({
+    Bucket: "test-bucket",
+    CORSConfiguration: {
+      CORSRules: [
+        {
+          // Allow all headers to be sent to this bucket.
+          AllowedHeaders: ["*"],
+          // Allow only GET and PUT methods to be sent to this bucket.
+          AllowedMethods: ["GET", "PUT"],
+          // Allow only requests from the specified origin.
+          AllowedOrigins: ["https://www.example.com"],
+          // Allow the entity tag (ETag) header to be returned in the response. The ETag header
+          // The entity tag represents a specific version of the object. The ETag reflects
+          // changes only to the contents of an object, not its metadata.
+          ExposeHeaders: ["ETag"],
+          // How long the requesting browser should cache the preflight response. After
+          // this time, the preflight request will have to be made again.
+          MaxAgeSeconds: 3600,
+        },
+      ],
+    },
+  });
 
-// Copy the array of allowed methods into the config object
-thisConfig.AllowedMethods = allowedMethods;
-
-// Create an array of configs then add the config object to it.
-const corsRules = new Array(thisConfig);
-
-// Create CORS parameters.
-export const corsParams = {
-  Bucket: "BUCKET_NAME",
-  CORSConfiguration: { CORSRules: corsRules },
-};
-export async function run() {
   try {
-    const data = await s3Client.send(new PutBucketCorsCommand(corsParams));
-    console.log("Success", data);
-    return data; // For unit tests.
+    const response = await client.send(command);
+    console.log(response);
   } catch (err) {
-    console.log("Error", err);
+    console.error(err);
   }
-}
-run();
+};
 // snippet-end:[s3.JavaScript.v3.cors.putBucketCors]
-// For unit testing only.
-// module.exports ={run, corsParams};
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main();
+}
