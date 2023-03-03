40383b4e5bdc2724dab0aa78eeca52df74d95e12
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-03-03 15:07:31 -0500

    initial object dependency

---
+++ cpp/example_code/aurora/rds_samples.h
@@ -0,0 +1,25 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+#pragma once
+#ifndef RDS_EXAMPLES_RDS_SAMPLES_H
+#define RDS_EXAMPLES_RDS_SAMPLES_H
+
+namespace AwsDoc {
+    namespace Aurora {
+        //! Routine which creates an Amazon Relational Database Service (Amazon Aurora)
+        //! instance and demonstrates several operations on that instance.
+        /*!
+         \sa gettingStartedWithDBClusters()
+         \param clientConfiguration: AWS client configuration.
+         \return bool: Successful completion.
+         */
+        bool gettingStartedWithDBClusters(
+                const Aws::Client::ClientConfiguration &clientConfig);
+
+    } // Aurora
+} // AwsDoc
+
+#endif //RDS_EXAMPLES_RDS_SAMPLES_H
