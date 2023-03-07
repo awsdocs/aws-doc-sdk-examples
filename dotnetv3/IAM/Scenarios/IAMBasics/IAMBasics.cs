37301dbdc4ad9e4be5b1d287daa43f7f0851e52b
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2023-03-07 06:41:36 -0500

    Refactoring scenarios.

--- dotnetv3/IAM/Scenarios/IAMBasics/IAMBasics.cs
+++ dotnetv3/IAM/Scenarios/IAMBasics/IAMBasics.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier:  Apache-2.0
 
+// snippet-start:[IAM.dotnetv3.IAMBasics]
 namespace IAMBasics;
 
 public class IAMBasics
