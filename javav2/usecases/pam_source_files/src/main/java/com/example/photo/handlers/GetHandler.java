0f97a7e70efaffe8a3380f34ee484961467e8ba3
Corey Pyle <corepyle@amazon.com>
2023-03-09 20:07:30 -0500

    fix proxy response for java GET labels java lambda

--- javav2/usecases/pam_source_files/src/main/java/com/example/photo/handlers/GetHandler.java
+++ javav2/usecases/pam_source_files/src/main/java/com/example/photo/handlers/GetHandler.java
@@ -2,6 +2,7 @@ package com.example.photo.handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.example.photo.WorkCount;
 import com.example.photo.services.DynamoDBService;
 import com.google.gson.Gson;
@@ -9,9 +10,9 @@ import com.google.gson.Gson;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class GetHandler implements RequestHandler<Object, String> {
-    @Override
-    public String handleRequest(Object o, Context context) {
+public class GetHandler implements RequestHandler<Map<String, Object>, APIGatewayProxyResponseEvent> {
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(Map<String, Object> event, Context context) {
         context.getLogger().log("In Labels handler");
         DynamoDBService dbService = new DynamoDBService();
         Map<String, WorkCount> map = dbService.scanPhotoTable();
@@ -19,6 +20,10 @@ public class GetHandler implements RequestHandler<Object, String> {
         Gson gson = new Gson();
         Map<String, Map<String, WorkCount>> m = new TreeMap<>();
         m.put("labels", map);
-        return gson.toJson(m);
-    }
+
+    return new APIGatewayProxyResponseEvent()
+        .withStatusCode(200)
+        .withBody(gson.toJson(m))
+        .withIsBase64Encoded(false);
+  }
 }
