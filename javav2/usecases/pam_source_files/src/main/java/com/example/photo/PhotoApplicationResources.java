ececb4f0cceacd8825d8a13027521c8a99ad2bfa
scmacdon <scmacdon@amazon.com>
2023-03-15 15:36:11 -0400

    added new topic data member

--- javav2/usecases/pam_source_files/src/main/java/com/example/photo/PhotoApplicationResources.java
+++ javav2/usecases/pam_source_files/src/main/java/com/example/photo/PhotoApplicationResources.java
@@ -5,18 +5,42 @@
 
 package com.example.photo;
 
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+
 import software.amazon.awssdk.regions.Region;
 
 public class PhotoApplicationResources {
-    public static final Region REGION = Region.US_EAST_1;
 
-    public static final String STORAGE_BUCKET = "photoassetmanagementpoc";
-    public static final String MANIFEST_BUCKET = "photoassetmanagementpoc-manifest";
 
-    public static final String TAGS_TABLE = "Photo";
-    public static final String JOBS_TABLE = "JobTopics";
+    public static final Region REGION = Region.of(System.getenv("AWS_REGION"));
+
+    public static final String STORAGE_BUCKET = System.getenv("STORAGE_BUCKET_NAME");
+    public static final String WORKING_BUCKET = System.getenv("WORKING_BUCKET_NAME");
+
+    public static final String TAGS_TABLE = System.getenv("LABELS_TABLE_NAME");
+    public static final String JOBS_TABLE = System.getenv("JOBS_TABLE_NAME");
 
     public static final String REKOGNITION_TAG_KEY = "rekognition";
     public static final String REKOGNITION_TAG_VALUE = "complete";
-}
 
+    public static final String topicARN = "<Enter Topic ARN>";
+
+    public static final Map<String, String> CORS_HEADER_MAP = Map.of(
+        "Access-Control-Allow-Origin", "*");
+    public static final Gson gson = new Gson();
+
+    public static String toJson(Object src) {
+        return gson.toJson(src);
+    }
+
+    public static APIGatewayProxyResponseEvent makeResponse(Object src) {
+        return new APIGatewayProxyResponseEvent()
+            .withStatusCode(200)
+            .withHeaders(CORS_HEADER_MAP)
+            .withBody(toJson(src))
+            .withIsBase64Encoded(false);
+    }
+}
