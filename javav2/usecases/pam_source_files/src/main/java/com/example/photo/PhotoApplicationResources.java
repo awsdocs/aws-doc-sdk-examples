304beb2daba7d38da622d58a7c8351efbb921330
scmacdon <scmacdon@amazon.com>
2023-03-15 15:48:59 -0400

    added new topic data member again

--- javav2/usecases/pam_source_files/src/main/java/com/example/photo/PhotoApplicationResources.java
+++ javav2/usecases/pam_source_files/src/main/java/com/example/photo/PhotoApplicationResources.java
@@ -26,7 +26,7 @@ public class PhotoApplicationResources {
     public static final String REKOGNITION_TAG_KEY = "rekognition";
     public static final String REKOGNITION_TAG_VALUE = "complete";
 
-    public static final String topicARN = "<Enter Topic ARN>";
+    public static final String topicARN = "<Enter topic ARN>";
 
     public static final Map<String, String> CORS_HEADER_MAP = Map.of(
         "Access-Control-Allow-Origin", "*");
