e16cc44f14f83d2d1ba253da07454c810bd1630c
ford prior <fprior@amazon.com>
2023-03-27 15:17:48 -0600

    working with hardcoded Lambda values

--- resources/cdk/sqs_lambda_to_batch_fargate/consumer_stack/consumer_stack.py
+++ resources/cdk/sqs_lambda_to_batch_fargate/consumer_stack/consumer_stack.py
@@ -15,6 +15,7 @@ from aws_cdk import (
     aws_ecs as ecs,
     aws_ecr as ecr,
     aws_batch as batch,
+    aws_batch_alpha as batch_alpha,
     Stack
 )
 from constructs import Construct
@@ -34,6 +35,7 @@ class ConsumerStack(Stack):
         fanout_topic_name = "ProducerStack-MyTopic86869434-71bWvzg3W1sQ"
         fanout_topic_arn = f'arn:aws:sns:us-east-1:808326389482:{fanout_topic_name}'
         sns_topic = sns.Topic.from_topic_arn(self, fanout_topic_name, topic_arn=fanout_topic_arn)
+        container_image = ecs.EcrImage.from_registry("public.ecr.aws/b4v4v1s0/ruby:latest")
 
         # Define the SQS queue in this account
         sqs_queue = sqs.Queue(self, 'BatchJobQueue')
@@ -77,82 +79,85 @@ class ConsumerStack(Stack):
             }
         )
 
-        # Define compute environment
-        vpc = ec2.Vpc.from_lookup(self, "Vpc", is_default=True)
-        compute_environment = batch.CfnComputeEnvironment(self,'CfnComputeEnvironmentBatchMaterializeQuery',
-            type="MANAGED",
-            compute_environment_name="BatchMaterializeQueryCE",
-            state="Enabled",
-            compute_resources=batch.CfnComputeEnvironment.ComputeResourcesProperty(
-                maxv_cpus=8,
-                type="FARGATE",
-                subnets=vpc.select_subnets(
-                    subnet_type=ec2.SubnetType.PUBLIC
-                ).subnet_ids,
-                security_group_ids=[
-                    ec2.SecurityGroup(self, "DefaultSG",
-                        vpc=vpc
-                    ).security_group_id
-                ]
-            )
-        )
-
-        # Define the job queue
-        batch_job_queue = batch.CfnJobQueue(self, "CfnJobQueueBatchMaterializeQuery",
-            compute_environment_order=[
-                batch.CfnJobQueue.ComputeEnvironmentOrderProperty(
-                    compute_environment=compute_environment.attr_compute_environment_arn,
-                    order=1
-                )],
-            priority=1,
-            state="ENABLED"
-        )
-
+        # # Batch resources commented out due to bug: https://github.com/aws/aws-cdk/issues/24783
+        #
+        # # Define compute environment
+        # vpc = ec2.Vpc.from_lookup(self, "Vpc", is_default=True)
+        # compute_environment = batch.CfnComputeEnvironment(self,'CfnComputeEnvironmentBatchMaterializeQuery',
+        #     type="MANAGED",
+        #     compute_environment_name="BatchMaterializeQueryCE",
+        #     state="Enabled",
+        #     compute_resources=batch.CfnComputeEnvironment.ComputeResourcesProperty(
+        #         maxv_cpus=8,
+        #         type="FARGATE",
+        #         subnets=vpc.select_subnets(
+        #             subnet_type=ec2.SubnetType.PUBLIC
+        #         ).subnet_ids,
+        #         security_group_ids=[
+        #             ec2.SecurityGroup(self, "DefaultSG",
+        #                 vpc=vpc
+        #             ).security_group_id
+        #         ]
+        #     )
+        # )
+        #
+        # # Define the job queue
+        # batch_job_queue = batch.CfnJobQueue(self, "CfnJobQueueBatchMaterializeQuery",
+        #     compute_environment_order=[
+        #         batch.CfnJobQueue.ComputeEnvironmentOrderProperty(
+        #             compute_environment=compute_environment.attr_compute_environment_arn,
+        #             order=1
+        #         )],
+        #     priority=1,
+        #     state="ENABLED"
+        # )
+        #
         # Define the job definition
-        cfn_job_definition = batch.CfnJobDefinition(self, "MyCfnJobDefinition",
-            type="container",
-            job_definition_name='test',
-            container_properties=batch.CfnJobDefinition.ContainerPropertiesProperty(
-                # environment=compute_environment,
-                image="public.ecr.aws/b4v4v1s0/ruby:latest",
-                # network_configuration=batch.CfnJobDefinition.NetworkConfigurationProperty(
-                #     assign_public_ip="assignPublicIp"
-                # ),
-                job_role_arn="arn:aws:iam::260778392212:role/sdk-code-examples-batch",
-                execution_role_arn="arn:aws:iam::260778392212:role/sdk-code-examples-batch",
-                vcpus=123,
-                memory=123,
-                command=["rspec","/src/ruby/example_code/dynamodb/spec/scenario_getting_started_movies_spec.rb"],
-            ),
-            network_configuration = batch.CfnJobDefinition.NetworkConfigurationProperty(
-                assign_public_ip="ENABLED"
+        # cfn_job_definition = batch.CfnJobDefinition(self, "MyCfnJobDefinition",
+        #     type="fargate",
+        #     job_definition_name='test',
+        #     container_properties=batch.CfnJobDefinition.ContainerPropertiesProperty(
+        #         # environment=compute_environment,
+        #         image="public.ecr.aws/b4v4v1s0/ruby:latest",
+        #         # network_configuration=batch.CfnJobDefinition.NetworkConfigurationProperty(
+        #         #     assign_public_ip="assignPublicIp"
+        #         # ),
+        #         job_role_arn="arn:aws:iam::260778392212:role/sdk-code-examples-batch",
+        #         execution_role_arn="arn:aws:iam::260778392212:role/sdk-code-examples-batch",
+        #         vcpus=123,
+        #         memory=123,
+        #         command=["rspec","/src/ruby/example_code/dynamodb/spec/scenario_getting_started_movies_spec.rb"],
+        #     )
+        # )
+
+        vpc = ec2.Vpc.from_lookup(self, "Vpc", is_default=True)
+
+        fargate_environment = batch_alpha.ComputeEnvironment(self, "MyFargateEnvironment",
+            compute_resources=batch_alpha.ComputeResources(
+                type=batch_alpha.ComputeResourceType.FARGATE,
+                vpc=vpc
             )
         )
 
-        # vpc = ec2.Vpc(self, "VPC")
-        # fargate_spot_environment = batch.ComputeEnvironment(self, "MyFargateEnvironment",
-        #     compute_resources=batch.ComputeResources(
-        #         type=batch.ComputeResourceType.FARGATE_SPOT,
-        #         vpc=vpc
-        #     )
-        # )
-        #
-        # job_queue = batch.JobQueue(self, "JobQueue",
-        #    compute_environments=[batch.JobQueueComputeEnvironment(
-        #        # Defines a collection of compute resources to handle assigned batch jobs
-        #        compute_environment=fargate_spot_environment,
-        #        # Order determines the allocation order for jobs (i.e. Lower means higher preference for job assignment)
-        #        order=1
-        #    )]
-        # )
-        #
-        # repo = ecr.Repository.from_repository_arn(self, "b4v4v1s0", "arn:aws:ecr:us-east-1:260778392212:repository/b4v4v1s0/ruby")
-        #
-        # batch.JobDefinition(self, "batch-job-def-from-ecr",
-        #     container=batch.JobDefinitionContainer(
-        #         image=ecs.EcrImage(repo, "latest")
-        #     )
-        # )
+        repo = ecr.Repository.from_repository_arn(self, "b4v4v1s0", "arn:aws:ecr:us-east-1:260778392212:repository/b4v4v1s0/ruby")
+
+        batch_alpha.JobDefinition(self, "batch-job-def-from-ecr",
+            container=batch_alpha.JobDefinitionContainer(
+                # image=ecs.EcrImage(repo, "latest"),
+                image=container_image,
+                execution_role=iam.Role.from_role_arn(self, 'sdk-code-examples-batch', "arn:aws:iam::260778392212:role/sdk-code-examples-batch")
+            ),
+            platform_capabilities=[ batch_alpha.PlatformCapabilities.FARGATE ]
+        )
+
+        job_queue = batch_alpha.JobQueue(self, "JobQueue",
+            compute_environments=[batch_alpha.JobQueueComputeEnvironment(
+               # Defines a collection of compute resources to handle assigned batch jobs
+               compute_environment=fargate_environment,
+               # Order determines the allocation order for jobs (i.e. Lower means higher preference for job assignment)
+               order=1
+            )]
+        )
 
         #################################################
         ##                                             ##
@@ -175,9 +180,6 @@ class ConsumerStack(Stack):
 
         # Add SNS-SQS policy to the IAM role
         sns_topic_role.add_to_policy(sns_topic_policy)
-
-        # Grant permissions to allow the function to receive messages from the queue
-        sqs_queue.grant_consume_messages(function)
 
         # Grant permissions to allow the function to receive messages from the queue
         sqs_queue.grant_consume_messages(function)
