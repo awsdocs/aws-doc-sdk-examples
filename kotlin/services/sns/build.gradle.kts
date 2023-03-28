cffa5adc85c7fd8abc21eab6146cd0c0a8028fcb
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2023-03-28 09:43:01 -0400

    Add files via upload

--- kotlin/services/sns/build.gradle.kts
+++ kotlin/services/sns/build.gradle.kts
@@ -12,6 +12,7 @@ buildscript {
 	repositories {
 		maven("https://plugins.gradle.org/m2/")
 	}
+
 	dependencies {
 		classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
 	}
@@ -22,10 +23,10 @@ repositories {
 	jcenter()
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
 dependencies {
 	implementation("aws.sdk.kotlin:sns-jvm:0.21.3-beta")
-	implementation("com.googlecode.json-simple:json-simple:1.1.1")
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
