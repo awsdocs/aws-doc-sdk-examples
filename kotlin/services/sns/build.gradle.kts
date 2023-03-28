565751b29986ca22f4e9a0c8401cbe696a09f0e1
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2023-03-28 09:05:37 -0400

    Add files via upload

--- kotlin/services/sns/build.gradle.kts
+++ kotlin/services/sns/build.gradle.kts
@@ -24,10 +24,9 @@ repositories {
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
 	implementation("aws.sdk.kotlin:sns-jvm:0.21.3-beta")
+	implementation("com.googlecode.json-simple:json-simple:1.1.1")
 	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-	implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 }
 tasks.withType<KotlinCompile>() {
 	kotlinOptions.jvmTarget = "1.8"
