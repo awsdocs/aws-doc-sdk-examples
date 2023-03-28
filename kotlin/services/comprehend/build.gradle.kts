8ef08d1d88804d942759f3755566c93a04484862
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2023-03-28 08:06:19 -0400

    Add files via upload

--- kotlin/services/comprehend/build.gradle.kts
+++ kotlin/services/comprehend/build.gradle.kts
@@ -12,8 +12,10 @@ buildscript {
     repositories {
         maven("https://plugins.gradle.org/m2/")
     }
+
     dependencies {
         classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+
     }
 }
 
@@ -22,11 +24,19 @@ repositories {
     jcenter()
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
 dependencies {
-    implementation("aws.sdk.kotlin:comprehend:0.19.0-beta")
+    implementation("aws.sdk.kotlin:comprehend-jvm:0.21.3-beta")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 }
