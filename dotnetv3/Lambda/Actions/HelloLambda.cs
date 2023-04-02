81a9bf243657acb550e14426e034187c510aea62
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2023-04-02 12:59:22 -0400

    Updated Lambda examples to use service solution format.
    Updated SoS
    Adding the Hello (ListFunctions) example to SoS
    Refactored Lambda examples
    
    Fixing linting error.
    
    Fixing another linting error.

---
+++ dotnetv3/Lambda/Actions/HelloLambda.cs
@@ -0,0 +1,24 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+// snippet-start:[Lambda.dotnetv3.LambdaActions.HelloLambda]
+namespace ServiceActions;
+
+using Amazon.Lambda;
+
+public class HelloLambda
+{
+
+    static async Task Main(string[] args)
+    {
+        var lambdaClient = new AmazonLambdaClient();
+
+        var response = await lambdaClient.ListFunctionsAsync();
+        response.Functions.ForEach(function =>
+        {
+            Console.WriteLine($"{function.FunctionName}\t{function.Description}");
+        });
+    }
+}
+
+// snippet-end:[Lambda.dotnetv3.LambdaActions.HelloLambda]
--- dotnetv3/Lambda/Tests/Usings.cs
+++ dotnetv3/Lambda/Tests/Usings.cs
@@ -1,10 +1,6 @@
-﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier:  Apache-2.0
-
-global using Amazon;
-global using Amazon.Lambda;
-global using Microsoft.Extensions.Configuration;
 global using Xunit;
+global using Xunit.Extensions.Ordering;
+global using LambdaActions;
 
 // Optional.
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
