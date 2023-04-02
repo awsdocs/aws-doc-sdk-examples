1145d845a1ce009db19c452076c641d5bde500df
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2023-04-02 18:42:24 -0400

    Updating Glacier examples.
    Rearchitect examples to use Actions and Tests
    Coded integration tests
    Updating SoS and README

---
+++ dotnetv3/Glacier/Actions/HelloGlacier.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+namespace ServiceActions;
+
+using Amazon.Glacier;
+using Amazon.Glacier.Model;
+
+public class HelloGlacier
+{
+    static async Task Main()
+    {
+        var glacierService = new AmazonGlacierClient();
+
+        Console.WriteLine("Hello, Amazon Glacier");
+        Console.WriteLine("Let's list your Glacier vaults:");
+
+        var glacierVaultPaginator = glacierService.Paginators.ListVaults(
+            new ListVaultsRequest { AccountId = "-" });
+
+        await foreach (var vault in glacierVaultPaginator.VaultList)
+        {
+            Console.WriteLine($"{vault.CreationDate,-24}{vault.VaultName,-30}\t{vault.VaultARN}");
+        }
+    }
+}
