19aed6efa5762ec7ee7e2184c868d1e7a35a56c1
Irene Smith <73654695+irenepsmith@users.noreply.github.com>
2023-04-10 12:29:06 -0400

    Updated Lambda examples to use service solution format. (#4629)
    * Updated Lambda examples to use service solution format.
    * Updated SoS
    * Adding the Hello (ListFunctions) example to SoS
    * Refactored Lambda examples
    * All reviewer comments incorporated.

---
+++ dotnetv3/Lambda/Actions/LambdaRoleWrapper.cs
@@ -0,0 +1,73 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+// snippet-start:[Lambda.dotnetv3.LambdaActions.LambdaRoleWrapper]
+namespace LambdaActions;
+
+using Amazon.IdentityManagement;
+using Amazon.IdentityManagement.Model;
+
+public class LambdaRoleWrapper
+{
+    private readonly IAmazonIdentityManagementService _lambdaRoleService;
+
+    public LambdaRoleWrapper(IAmazonIdentityManagementService lambdaRoleService)
+    {
+        _lambdaRoleService = lambdaRoleService;
+    }
+
+    /// <summary>
+    /// Attach an AWS Identity and Access Management (IAM) role policy to the
+    /// IAM role to be assumed by the AWS Lambda functions created for the scenario.
+    /// </summary>
+    /// <param name="policyArn">The Amazon Resource Name (ARN) of the IAM policy.</param>
+    /// <param name="roleName">The name of the IAM role to attach the IAM policy to.</param>
+    /// <returns>A Boolean value indicating the success of the action.</returns>
+    public async Task<bool> AttachLambdaRolePolicyAsync(string policyArn, string roleName)
+    {
+        var response = await _lambdaRoleService.AttachRolePolicyAsync(new AttachRolePolicyRequest { PolicyArn = policyArn, RoleName = roleName });
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+    }
+
+    /// <summary>
+    /// Create a new IAM role.
+    /// </summary>
+    /// <param name="roleName">The name of the IAM role to create.</param>
+    /// <param name="policyDocument">The policy document for the new IAM role.</param>
+    /// <returns>A string representing the ARN for newly created role.</returns>
+    public async Task<string> CreateLambdaRoleAsync(string roleName, string policyDocument)
+    {
+        var request = new CreateRoleRequest
+        {
+            AssumeRolePolicyDocument = policyDocument,
+            RoleName = roleName,
+        };
+
+        var response = await _lambdaRoleService.CreateRoleAsync(request);
+        return response.Role.Arn;
+    }
+
+    /// <summary>
+    /// Deletes an IAM role.
+    /// </summary>
+    /// <param name="roleName">The name of the role to delete.</param>
+    /// <returns>A Boolean value indicating the success of the operation.</returns>
+    public async Task<bool> DeleteLambdaRoleAsync(string roleName)
+    {
+        var request = new DeleteRoleRequest
+        {
+            RoleName = roleName,
+        };
+
+        var response = await _lambdaRoleService.DeleteRoleAsync(request);
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+    }
+
+    public async Task<bool> DetachLambdaRolePolicyAsync(string policyArn, string roleName)
+    {
+        var response = await _lambdaRoleService.DetachRolePolicyAsync(new DetachRolePolicyRequest { PolicyArn = policyArn, RoleName = roleName });
+        return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+    }
+}
+
+// snippet-end:[Lambda.dotnetv3.LambdaActions.LambdaRoleWrapper]
