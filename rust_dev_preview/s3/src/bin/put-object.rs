c105938c957869bcc12e96528368eb3f63f0d0f4
David Souther <davidsouther+github@gmail.com>
2023-04-11 13:27:23 -0700

    s3 put object with tracking

--- rust_dev_preview/s3/src/bin/put-object.rs
+++ rust_dev_preview/s3/src/bin/put-object.rs
@@ -1,5 +1,6 @@
 use std::{
-    fs::File,
+    convert::Infallible,
+    mem,
     path::PathBuf,
     pin::Pin,
     process::exit,
@@ -8,11 +9,15 @@ use std::{
 
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{
+    error::DisplayErrorContext,
     primitives::{ByteStream, SdkBody},
     Client,
 };
+use aws_smithy_http::body::BoxBody;
+use bytes::Bytes;
 use clap::Parser;
-use tracing::trace;
+use http_body::{Body, SizeHint};
+use tracing::debug;
 
 #[derive(Debug, Parser)]
 struct Opt {
@@ -33,8 +38,6 @@ pub struct ProgressBody<InnerBody> {
 }
 
 impl ProgressBody<SdkBody> {
-    /// Given an `SdkBody`, a `Box<dyn HttpChecksum>`, and a precalculated checksum represented
-    /// as `Bytes`, create a new `ChecksumBody<SdkBody>`.
     pub fn new(body: SdkBody, content_length: u64) -> Self {
         Self {
             inner: body,
@@ -42,20 +45,27 @@ impl ProgressBody<SdkBody> {
             bytes_written: 0,
         }
     }
+}
 
-    fn poll_inner(
+impl<InnerBody> Body for ProgressBody<InnerBody>
+where
+    InnerBody: Body<Data = Bytes, Error = aws_smithy_http::body::Error>,
+{
+    type Data = Bytes;
+
+    type Error = aws_smithy_http::body::Error;
+
+    fn poll_data(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
-    ) -> Poll<Option<Result<(), aws_smithy_http::body::Error>>> {
-        use http_body::Body;
-
+    ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
         let this = self.project();
 
         match this.inner.poll_data(cx) {
             Poll::Ready(Some(Ok(data))) => {
                 *this.bytes_written += data.len() as u64;
                 let progress = *this.bytes_written as f64 / *this.content_length as f64;
-                tracing::trace!(
+                println!(
                     "Read {} bytes, progress: {:.2}%",
                     data.len(),
                     progress * 100.0
@@ -64,33 +74,65 @@ impl ProgressBody<SdkBody> {
                 Poll::Ready(Some(Ok(data)))
             }
             Poll::Ready(None) => {
-                tracing::trace!("done");
+                tracing::debug!("done");
                 Poll::Ready(None)
             }
             Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(e))),
             Poll::Pending => Poll::Pending,
         }
     }
+
+    fn poll_trailers(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<Option<http::HeaderMap>, Self::Error>> {
+        self.project().inner.poll_trailers(cx)
+    }
+
+    fn size_hint(&self) -> http_body::SizeHint {
+        SizeHint::with_exact(self.content_length)
+    }
 }
 
 // snippet-start:[s3.rust.put-object]
 // Uploads a local file to a bucket.
 async fn put_object(client: &Client, opts: &Opt) -> Result<(), anyhow::Error> {
-    trace!("bucket: {}", opts.bucket);
-    trace!("object: {}", opts.object);
-    trace!("source: {}", opts.source.display());
+    debug!("bucket: {}", opts.bucket);
+    debug!("object: {}", opts.object);
+    debug!("source: {}", opts.source.display());
 
-    let file = File::open(opts.source.clone())?;
+    let body = ByteStream::read_from()
+        .path(opts.source.clone())
+        .buffer_size(2048)
+        .build()
+        .await?;
 
-    let body = ByteStream::from(SdkBody::from(file).map(move |body| {
-        let body = ProgressBody::new(
-            body,
-            file.metadata().expect("Could not load file metadata").len(),
-        );
-        aws_smithy_http::body::SdkBody::from_dyn(aws_smithy_http::body::BoxBody::new(body))
-    }));
+    let request = client
+        .put_object()
+        .bucket(opts.bucket.clone())
+        .key(opts.object.clone())
+        .body(body);
 
-    Ok(())
+    let customized = request.customize().await?.map_request(|mut req| {
+        let body = mem::replace(req.body_mut(), SdkBody::taken()).map(|body| {
+            let len = body.content_length().expect("upload body sized"); // TODO - panics
+            let body = ProgressBody::new(body, len);
+            // Warning: A from_dyn loses `Once`, so it can't be sigv4'd. There is an upcoming
+            // special body map to handle this block's operation while keeping signing.
+            // See https://github.com/awslabs/smithy-rs/pull/2567
+            SdkBody::from_dyn(BoxBody::new(body))
+        });
+        let _ = mem::replace(req.body_mut(), body);
+        Ok::<http::Request<SdkBody>, Infallible>(req)
+    })?;
+
+    match customized.send().await {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("{}", DisplayErrorContext(e));
+            Ok(())
+        }
+    }
 }
 // snippet-end:[s3.rust.put-object-presigned]
 
