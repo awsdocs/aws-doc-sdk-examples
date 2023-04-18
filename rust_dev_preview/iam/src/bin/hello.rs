3744b8d0801fb5ab7db4395843f9118afebe8491
Corey Pyle <corepyle@amazon.com>
2023-04-18 12:49:12 -0400

    update hello example and add to SOS

--- rust_dev_preview/iam/src/bin/hello.rs
+++ rust_dev_preview/iam/src/bin/hello.rs
@@ -1,21 +1,28 @@
+use aws_sdk_iam::types::PolicyScopeType;
 use futures::StreamExt;
 
 use aws_sdk_iam::error::SdkError;
 use aws_sdk_iam::operation::list_policies::ListPoliciesError;
 use iam_service::clients::IAM_CLIENT;
 
+// snippet-start:[rust.example_code.iam.hello]
 #[tokio::main]
 async fn main() -> Result<(), SdkError<ListPoliciesError>> {
     let client = IAM_CLIENT.get().await;
 
-    let mut list_policies = client.list_policies().into_paginator().send();
+    let mut list_policies = client
+        .list_policies()
+        // Get policies created by the user.
+        .set_scope(Some(PolicyScopeType::Local))
+        .into_paginator()
+        .send();
 
     while let Some(list_policies_output) = list_policies.next().await {
         match list_policies_output {
             Ok(list_policies) => {
                 if let Some(policies) = list_policies.policies() {
-                    for policy in policies.iter().enumerate() {
-                        println!("{:?}", policy.policy_name())
+                    for policy in policies.iter() {
+                        println!("{:?}", policy.policy_name().unwrap())
                     }
                 }
             }
@@ -26,6 +33,7 @@ async fn main() -> Result<(), SdkError<ListPoliciesError>> {
 
     Ok(())
 }
+// snippet-end:[rust.example_code.iam.hello]
 
 #[cfg(test)]
 mod test {
@@ -33,6 +41,6 @@ mod test {
 
     #[test]
     fn test_hello() {
-        assert!(main().is_err(), "main should not throw an error")
+        assert_eq!(main().is_err(), false, "don't panic")
     }
 }
