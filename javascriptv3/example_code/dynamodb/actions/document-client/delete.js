f4cf184c3fd300880f4d815ff0f684bdb01029e2
Corey Pyle <corepyle@amazon.com>
2023-05-01 17:48:23 -0400

    update document-delete; add tests

--- javascriptv3/example_code/dynamodb/actions/document-client/delete.js
+++ javascriptv3/example_code/dynamodb/actions/document-client/delete.js
@@ -1,57 +1,32 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-ABOUT THIS NODE.JS EXAMPLE: This example works with the AWS SDK for JavaScript (v3),
-which is available at https://github.com/aws/aws-sdk-js-v3. This example is in the 'AWS SDK for JavaScript v3 Developer Guide' at
-https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html.
-Purpose:
-ddbdoc_get_item.js demonstrates how to use the Amazon DynamoDB document client to delete an item from an Amazon DynamoDB table.
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-Inputs (replace in code):
-- TABLE_NAME
-- primaryKey - The name of the primary key. For example, "id".
-- VALUE_1: Value for the primary key (The format for the datatype must match the schema. For example, if the primaryKey is a number, VALUE_1 has no inverted commas.)
-- sortKey - The name of the sort key. Only required if table has sort key. For example, "firstName".
-- VALUE_2: Value for the primary key (The format for the datatype must match the schema.)
+import { fileURLToPath } from "url";
 
-Running the code:
-node ddbdoc_delete_item.js
-*/
 // snippet-start:[dynamodb.JavaScript.docClient.deleteV3]
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
-import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
-import { ddbDocClient } from "../src/libs/ddbDocClient";
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
-// Set the parameters
-export const params = {
-  TableName: "TABLE_NAME",
-  /*
-  Convert the key JavaScript object you are deleting to the
-  required Amazon DynamoDB record. The format of values specifies
-  the datatype. The following list demonstrates different
-  datatype formatting requirements:
-  String: "String",
-  NumAttribute: 1,
-  BoolAttribute: true,
-  ListAttribute: [1, "two", false],
-  MapAttribute: { foo: "bar" },
-  NullAttribute: null
-   */
-  Key: {
-    primaryKey: "VALUE_1", // For example, 'Season': 2.
-    sortKey: "VALUE_2", // For example,  'Episode': 1; (only required if table has sort key).
-  },
+export const main = async () => {
+  const command = new DeleteCommand({
+    TableName: "Sodas",
+    Key: {
+      Flavor: "Cola",
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
 };
-
-export const run = async () => {
-  try {
-    const data = await ddbDocClient.send(new DeleteCommand(params));
-    console.log("Success - item deleted");
-    return data;
-  } catch (err) {
-    console.log("Error", err);
-  }
-};
-run();
 // snippet-end:[dynamodb.JavaScript.docClient.deleteV3]
-// For unit tests only.
-// module.exports ={run, params};
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main();
+}
