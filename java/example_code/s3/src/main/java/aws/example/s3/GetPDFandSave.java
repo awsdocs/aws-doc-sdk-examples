8b6079acb41bb4db7a55b920f4aaafbbb6203256
scmacdon <scmacdon@amazon.com>
2023-05-01 14:01:30 -0400

    addressed another ticket

--- java/example_code/s3/src/main/java/aws/example/s3/GetPDFandSave.java
+++ java/example_code/s3/src/main/java/aws/example/s3/GetPDFandSave.java
@@ -1,7 +1,7 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0 */
 `
-package main.java.com.example.s3;
+package aws.example.s3;
 
 import java.io.InputStream;
 import java.io.File;
