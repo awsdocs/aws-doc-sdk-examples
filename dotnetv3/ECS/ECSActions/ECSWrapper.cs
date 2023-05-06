91431b1e5f3284700debbe1a1af459932d1bdca2
Emre Ceyhan <a@emreceyhan.com>
2023-05-06 11:57:55 +0100

    ecs examples updated for .net core

---
+++ dotnetv3/ECS/ECSActions/ECSWrapper.cs
@@ -0,0 +1,133 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.ECS;
+using Amazon.ECS.Model;
+using Microsoft.Extensions.Logging;
+
+namespace ECSActions;
+
+public class ECSWrapper
+{
+
+    private readonly AmazonECSClient _ecsClient;
+    private readonly ILogger<ECSWrapper> _logger;
+
+    /// <summary>
+    /// Constructor for the CloudWatch wrapper.
+    /// </summary>
+    /// <param name="ecsClient">The injected ECS client.</param>
+    /// <param name="logger">The injected logger for the wrapper.</param>
+    public ECSWrapper(AmazonECSClient ecsClient, ILogger<ECSWrapper> logger)
+
+    {
+        _logger = logger;
+        _ecsClient = ecsClient;
+    }
+
+    /// <summary>
+    /// List cluster ARNs available.
+    /// </summary>
+    /// <returns>The ARN list of clusters.</returns>
+    public async Task<List<string>> GetClusterARNSAsync()
+    {
+
+        Console.WriteLine("Getting a list of all the clusters in your AWS account...");
+        List<string> clusterArnList = new List<string>();
+        // Get a list of all the clusters in your AWS account
+        try
+        {
+            var listClustersResponse = await _ecsClient.ListClustersAsync(new ListClustersRequest
+            {
+            });
+
+            var clusterArns = listClustersResponse.ClusterArns;
+
+            // Print the ARNs of the clusters
+            foreach (var clusterArn in clusterArns)
+            {
+                clusterArnList.Add(clusterArn);
+            }
+
+            if (clusterArns.Count == 0)
+            {
+                _logger.LogWarning("No clusters found in your AWS account.");
+            }
+
+
+            return clusterArnList;
+
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"An error occurred while getting a list of all the clusters in your AWS account. {e.InnerException}");
+            throw new Exception($"An error occurred while getting a list of all the clusters in your AWS account. {e.InnerException}");
+        }
+
+    }
+
+    /// <summary>
+    /// List service ARNs available.
+    /// </summary>
+    /// <param name="clusterARN">The arn of the ECS cluster.</param>
+    /// <returns>The ARN list of services in given cluster.</returns>
+    public async Task<List<string>> GetServiceARNSAsync(string clusterARN)
+    {
+        List<string> serviceArns = new List<string>();
+
+        var request = new ListServicesRequest
+        {
+            Cluster = clusterARN
+        };
+
+        string nextToken = null;
+        do
+        {
+            request.NextToken = nextToken;
+            var response = await _ecsClient.ListServicesAsync(request);
+            serviceArns.AddRange(response.ServiceArns);
+            nextToken = response.NextToken;
+        } while (nextToken != null);
+
+
+        return serviceArns;
+
+    }
+
+    /// <summary>
+    /// List tasks ARNs available.
+    /// </summary>
+    /// <param name="clusterARN">The arn of the ECS cluster.</param>
+    /// <returns>The ARN list of tasks in given cluster.</returns>
+    public async Task<List<string>> GetTaskARNsAsync(string clusterARN)
+    {
+        // Set up the request to describe the tasks in the service
+        var listTasksRequest = new ListTasksRequest
+        {
+            Cluster = clusterARN
+        };
+
+        // Call the ListTasks API operation and get the list of task ARNs
+        List<string> taskArns = new List<string>();
+        string nextToken = null;
+        do
+        {
+            listTasksRequest.NextToken = nextToken;
+            var listTasksResponse = await _ecsClient.ListTasksAsync(listTasksRequest);
+            taskArns.AddRange(listTasksResponse.TaskArns);
+            nextToken = listTasksResponse.NextToken;
+        } while (nextToken != null);
+
+        if (taskArns.Count == 0)
+        {
+            _logger.LogWarning("No tasks found in cluster: " + clusterARN);
+        }
+
+
+        return taskArns;
+    }
+
+}
---
+++ dotnetv3/ECS/ECSTests/ECSTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+using Amazon.Runtime;
+using ECSActions;
+using Amazon.ECS;
+
+namespace ECSTests;
+
+/// <summary>
+/// ECS tests.
+/// </summary>
+public class CloudWatchTests
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ECSWrapper _ecsWrapper;
+
+    /// <summary>
+    /// Constructor for the test class.
+    /// </summary>
+    public CloudWatchTests()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .Build();
+
+        _loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+
+        _ecsWrapper = new ECSWrapper(
+            new AmazonECSClient(),
+            new Logger<ECSWrapper>(_loggerFactory)
+        );
+    }
+
+    /// <summary>
+    /// List the metrics. Should not be null.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Fact]
+    [Order(1)]
+    [Trait("Category", "Integration")]
+    public async Task ListClusters_ShouldNotBeNull()
+    {
+        var result = await _ecsWrapper.GetClusterARNSAsync();
+
+        Assert.NotEmpty(result);
+    }
+}
