c04b6fa176122dab227e18dcc333bfe0d0fbdd05
scmacdon <scmacdon@amazon.com>
2023-05-16 09:32:39 -0400

    fixed linter

--- kotlin/usecases/creating_pam/src/main/kotlin/com/example/photo/PhotoApplicationResources.kt
+++ kotlin/usecases/creating_pam/src/main/kotlin/com/example/photo/PhotoApplicationResources.kt
@@ -10,14 +10,11 @@ import java.util.Map
 class PhotoApplicationResources {
 
     companion object {
-        val LABELS_TABLE = "lam200-Java-PAM-PamTablesLabelsTable6998F453-1AY3UH5OVH24H"
-        val STORAGE_BUCKET = "lam200-java-pam-pambucketsstoragebucket08049213-1h3fcp6kz7kjf"
-        val WORKING_BUCKET = "lam200-java-pam-pambucketsworkingbucket9940330b-17j4gk7djl1sz"
-        val TOPIC_ARN = "arn:aws:sns:us-east-1:814548047983:lam200-Java-PAM-PamNotifier9D8DBE72-hIIAi9PvosRe"
-
-        val CORS_HEADER_MAP = Map.of(
-            "Access-Control-Allow-Origin", "*"
-        )
+        val LABELS_TABLE = "<Enter value>"
+        val STORAGE_BUCKET = "<Enter value>"
+        val WORKING_BUCKET = "<Enter value>"
+        val TOPIC_ARN = "<Enter value>"
+        val CORS_HEADER_MAP = Map.of("Access-Control-Allow-Origin", "*")
         val gson = Gson()
     }
 }
