fbb171f691860fb134a4162ca33a0fc308dbfea4
Todd Hill <tkhill@amazon.com>
2023-05-17 12:42:59 -0400

    add missing snippet start tag

--- javav2/example_code/s3/src/main/java/com/example/s3/S3ObjectOperations.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/S3ObjectOperations.java
@@ -116,7 +116,7 @@ public class S3ObjectOperations {
                     .build();
         }
         // snippet-end:[s3.java2.s3_object_operations.pagination]
-
+        // snippet-start:[s3.java2.s3_object_operations.iterative]
         ListObjectsV2Request listReq = ListObjectsV2Request.builder()
                 .bucket(bucketName)
                 .maxKeys(1)
