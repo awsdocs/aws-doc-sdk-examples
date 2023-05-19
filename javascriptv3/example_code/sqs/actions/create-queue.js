78760448cb3e0c75dd39ed672bf6713be5d68b83
Corey Pyle <corepyle@amazon.com>
2023-05-19 09:21:25 -0400

    refactor and organize SQS examples

---
+++ javascriptv3/example_code/sqs/actions/create-queue.js
@@ -0,0 +1,31 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import { fileURLToPath } from "url";
+
+// snippet-start:[sqs.JavaScript.queues.createQueueV3]
+import { CreateQueueCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+const client = new SQSClient({});
+const SQS_QUEUE_NAME = "test-queue";
+
+export const main = async (sqsQueueName = SQS_QUEUE_NAME) => {
+  const command = new CreateQueueCommand({
+    QueueName: sqsQueueName, 
+    Attributes: {
+      DelaySeconds: "60", 
+      MessageRetentionPeriod: "86400", 
+    },
+  });
+
+  const response = await client.send(command);
+  console.log(response);
+  return response;
+};
+// snippet-end:[sqs.JavaScript.queues.createQueueV3]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  main();
+}
---
+++ javascriptv3/example_code/sqs/client.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// snippet-start[sqs.JavaScript.createclientv3]
+import { SQSClient } from "@aws-sdk/client-sqs";
+
+export const client = new SQSClient({ region: "us-east-1" });
+// snippet-end:[sqs.JavaScript.createclientv3]
