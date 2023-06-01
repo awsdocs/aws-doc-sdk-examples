74c7be6a596c7585535141851c3f216d9fb46951
David Souther <davidsouther+github@gmail.com>
2023-06-01 14:02:15 -0700

    Rust: Implement PAM cross service example.
    This includes the Rust code, as well as the CDK strategy to run it.
    
    The rust lambdas are compiled using [](https://cargo-lambda.info).
    
    Use to standard overview for readme.

---
+++ rust_dev_preview/cross_service/photo_asset_management/src/bin/download.rs
@@ -0,0 +1,19 @@
+use photo_asset_management::{
+    common::{init_tracing_subscriber, Common},
+    handlers::download,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), lambda_runtime::Error> {
+    eprintln!("Inside download::main");
+
+    init_tracing_subscriber();
+
+    let common = Common::load_from_env().await;
+
+    lambda_runtime::run(lambda_runtime::service_fn(|request| async {
+        eprintln!("Inside download::main::run");
+        download::handler(&common, request).await
+    }))
+    .await
+}
