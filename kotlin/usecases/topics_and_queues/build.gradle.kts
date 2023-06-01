66f88be99c6f9b15ec1173dfd2ff829497975569
scmacdon <scmacdon@amazon.com>
2023-06-01 13:49:05 -0400

    add Kotlin SNS/SQS example

---
+++ kotlin/usecases/topics_and_queues/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "me.scmacdon"
+version = "1.0-SNAPSHOT"
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+    }
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+dependencies {
+    implementation("aws.sdk.kotlin:sns-jvm:0.21.3-beta")
+    implementation("aws.sdk.kotlin:sqs-jvm:0.21.3-beta")
+    implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
