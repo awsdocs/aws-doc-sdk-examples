7b45a6a37d0908cd8b870f9cbdea8c5c5b4c3c24
Corey Pyle <corepyle@amazon.com>
2023-06-20 10:03:25 -0400

    JavaScript (v3): Enforce SSL on buckets created with the CDK.

--- resources/cdk/javascript_example_code_transcribe_demo/setup.ts
+++ resources/cdk/javascript_example_code_transcribe_demo/setup.ts
@@ -19,154 +19,195 @@
 // For instructions on how to run any of these apps, see
 // https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/resources/cdk/README.md#Running-a-CDK-app
 
-import * as cdk from '@aws-cdk/core';
-import * as cognito from '@aws-cdk/aws-cognito';
-import * as iam from '@aws-cdk/aws-iam';
-import {Effect, PolicyStatement} from '@aws-cdk/aws-iam';
-import * as s3 from '@aws-cdk/aws-s3';
-import {Bucket, BucketAccessControl, BlockPublicAccess, BlockPublicAccessOptions} from '@aws-cdk/aws-s3';
+import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
+import * as iam from "@aws-cdk/aws-iam";
+import { Effect, PolicyStatement } from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
+import {
+  Bucket,
+  BucketAccessControl,
+  BlockPublicAccess,
+  BlockPublicAccessOptions,
+} from "@aws-cdk/aws-s3";
 
-
-export class  SetupStackTranscription extends cdk.Stack {
+export class SetupStackTranscription extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const transcriptionBucket = new Bucket(this, 'transciptions', {
+    const transcriptionBucket = new Bucket(this, "transcriptions", {
+      enforceSSL: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       publicReadAccess: true,
       accessControl: BucketAccessControl.PUBLIC_READ,
-
     });
 
-    transcriptionBucket.addToResourcePolicy(new iam.PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["*"],
-      resources: [transcriptionBucket.arnForObjects('*')],
-      principals: [new iam.AccountRootPrincipal()],
-    }));
-    transcriptionBucket.grantPublicAccess('*', 's3:GetObject');
-    transcriptionBucket.grantPublicAccess('*', 's3:PutObject');
-    transcriptionBucket.grantPublicAccess('*', 's3:DeleteObject');
+    transcriptionBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["*"],
+        resources: [transcriptionBucket.arnForObjects("*")],
+        principals: [new iam.AccountRootPrincipal()],
+      })
+    );
+    transcriptionBucket.grantPublicAccess("*", "s3:GetObject");
+    transcriptionBucket.grantPublicAccess("*", "s3:PutObject");
+    transcriptionBucket.grantPublicAccess("*", "s3:DeleteObject");
 
-    const cfnBucket = transcriptionBucket.node.findChild('Resource') as s3.CfnBucket
-    cfnBucket.addPropertyOverride('CorsConfiguration', {
+    const cfnBucket = transcriptionBucket.node.findChild(
+      "Resource"
+    ) as s3.CfnBucket;
+    cfnBucket.addPropertyOverride("CorsConfiguration", {
       CorsRules: [
         {
-          "AllowedHeaders": [
-            "*"
-          ],
-          "AllowedMethods": [
-            "GET",
-            "POST",
-            "PUT",
-            "DELETE"
-          ],
-          "AllowedOrigins": [
-            "*"
-          ],
-        }
-      ]
+          AllowedHeaders: ["*"],
+          AllowedMethods: ["GET", "POST", "PUT", "DELETE"],
+          AllowedOrigins: ["*"],
+        },
+      ],
     });
 
-    const appBucket = new s3.Bucket(this, 'appbucket',{
+    const appBucket = new s3.Bucket(this, "appbucket", {
+      enforceSSL: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
-      blockPublicAccess:  {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "index.html",
+      blockPublicAccess: {
         blockPublicAcls: true,
         blockPublicPolicy: false,
         ignorePublicAcls: true,
-        restrictPublicBuckets: false
-      }
+        restrictPublicBuckets: false,
+      },
     });
-    appBucket.grantPublicAccess('*', 's3:GetObject');
-    appBucket.grantPublicAccess('*', 's3:PutObject');
+    appBucket.grantPublicAccess("*", "s3:GetObject");
+    appBucket.grantPublicAccess("*", "s3:PutObject");
 
-    const randomUserPoolName = `ExampleUserPoolName-${Math.ceil(Math.random() * 10 ** 10)}`;
+    const randomUserPoolName = `ExampleUserPoolName-${Math.ceil(
+      Math.random() * 10 ** 10
+    )}`;
 
-    const myUserPool = new cognito.UserPool(this, 'exampleuserpoolid', {
+    const myUserPool = new cognito.UserPool(this, "exampleuserpoolid", {
       userPoolName: randomUserPoolName,
       selfSignUpEnabled: true,
       autoVerify: {
-        email: true
+        email: true,
       },
       userVerification: {
-        emailSubject: 'Verify your email for our awesome app!',
-        emailBody: 'Hello, Thanks for signing up to our awesome app! Your verification code is {####}',
-        smsMessage: 'Hello, Thanks for signing up to our awesome app! Your verification code is {####}',
+        emailSubject: "Verify your email for our awesome app!",
+        emailBody:
+          "Hello, Thanks for signing up to our awesome app! Your verification code is {####}",
+        smsMessage:
+          "Hello, Thanks for signing up to our awesome app! Your verification code is {####}",
       },
       standardAttributes: {
         email: {
           required: true,
           mutable: false,
-        }
-      }
+        },
+      },
     });
 
-    const randomUserPoolClientName = `ExampleUserPoolName-${Math.ceil(Math.random() * 10 ** 10)}`;
+    const randomUserPoolClientName = `ExampleUserPoolName-${Math.ceil(
+      Math.random() * 10 ** 10
+    )}`;
 
-    const userPoolClient = new cognito.UserPoolClient(this, 'ExampleUserPoolClient', {
-      userPool: myUserPool,
-      userPoolClientName: randomUserPoolClientName,
-      oAuth: {
-        callbackUrls: [appBucket.urlForObject('index.html')]
+    const userPoolClient = new cognito.UserPoolClient(
+      this,
+      "ExampleUserPoolClient",
+      {
+        userPool: myUserPool,
+        userPoolClientName: randomUserPoolClientName,
+        oAuth: {
+          callbackUrls: [appBucket.urlForObject("index.html")],
+        },
       }
-    });
+    );
 
-    const randomDomain = `exampleuserpoolname-${Math.ceil(Math.random() * 10 ** 10)}`;
+    const randomDomain = `exampleuserpoolname-${Math.ceil(
+      Math.random() * 10 ** 10
+    )}`;
 
-    myUserPool.addDomain('CognitoDomain', {
+    myUserPool.addDomain("CognitoDomain", {
       cognitoDomain: {
-        domainPrefix: randomDomain
-      }
+        domainPrefix: randomDomain,
+      },
     });
 
-    const myIdentityPool = new cognito.CfnIdentityPool(this, "ExampleIdentityPool", {
-      allowUnauthenticatedIdentities: false,
-      cognitoIdentityProviders: [{
-        clientId: userPoolClient.userPoolClientId,
-        providerName: myUserPool.userPoolProviderName,
-      }]
-    });
-    const authenticatedRole = new iam.Role(this, 'ExampleCognitoDefaultAuthenticatedRole', {
-      assumedBy: new iam.FederatedPrincipal('cognito-identity.amazonaws.com', {
-        "StringEquals": {"cognito-identity.amazonaws.com:aud": myIdentityPool.ref},
-        "ForAnyValue:StringLike": {"cognito-identity.amazonaws.com:amr": "authenticated"},
-      }, "sts:AssumeRoleWithWebIdentity"),
-    });
-    authenticatedRole.addToPolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        "mobileanalytics:PutEvents",
-        "cognito-sync:*",
-        "cognito-identity:*"
-      ],
-      resources: ["*"]
-    }));
-
-    authenticatedRole.addToPolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["sns:*"],
-      resources: ["*"]
-    }));
-    authenticatedRole.addToPolicy(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: ["transcribe:StartTranscriptionJob",
-        "transcribe:GetTranscriptionJob"],
-      resources: ["*"]
-    }));
-    const defaultPolicy = new cognito.CfnIdentityPoolRoleAttachment(this, 'DefaultValid', {
-      identityPoolId: myIdentityPool.ref,
-      roles: {
-        'authenticated': authenticatedRole.roleArn
+    const myIdentityPool = new cognito.CfnIdentityPool(
+      this,
+      "ExampleIdentityPool",
+      {
+        allowUnauthenticatedIdentities: false,
+        cognitoIdentityProviders: [
+          {
+            clientId: userPoolClient.userPoolClientId,
+            providerName: myUserPool.userPoolProviderName,
+          },
+        ],
       }
-    });
+    );
+    const authenticatedRole = new iam.Role(
+      this,
+      "ExampleCognitoDefaultAuthenticatedRole",
+      {
+        assumedBy: new iam.FederatedPrincipal(
+          "cognito-identity.amazonaws.com",
+          {
+            StringEquals: {
+              "cognito-identity.amazonaws.com:aud": myIdentityPool.ref,
+            },
+            "ForAnyValue:StringLike": {
+              "cognito-identity.amazonaws.com:amr": "authenticated",
+            },
+          },
+          "sts:AssumeRoleWithWebIdentity"
+        ),
+      }
+    );
+    authenticatedRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "mobileanalytics:PutEvents",
+          "cognito-sync:*",
+          "cognito-identity:*",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    authenticatedRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["sns:*"],
+        resources: ["*"],
+      })
+    );
+    authenticatedRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "transcribe:StartTranscriptionJob",
+          "transcribe:GetTranscriptionJob",
+        ],
+        resources: ["*"],
+      })
+    );
+    const defaultPolicy = new cognito.CfnIdentityPoolRoleAttachment(
+      this,
+      "DefaultValid",
+      {
+        identityPoolId: myIdentityPool.ref,
+        roles: {
+          authenticated: authenticatedRole.roleArn,
+        },
+      }
+    );
   }
-};
+}
 
-
-const stackName = 'SetupStack'
+const stackName = "SetupStack";
 
 const app = new cdk.App();
 
-new SetupStack(app, stackName);
+new SetupStackTranscription(app, stackName);
