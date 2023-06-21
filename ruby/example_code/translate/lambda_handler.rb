443ac556fffc38208595bcfd0ad91af87149be3b
ford-at-aws <fprior@amazon.com>
2023-06-21 15:24:03 -0600

    Added code for AWS Polly integration in the lambda_handler.rb file. Deleted code for AWS Rekognition integration in the lambda_handler.rb file. Modified code for AWS Translate integration in the lambda_handler.rb file.

---
+++ ruby/example_code/translate/lambda_handler.rb
@@ -0,0 +1,25 @@
+require 'json'
+require 'aws-sdk-translate'
+require 'logger'
+# require 'pry'
+
+def lambda_handler(event:, context:)
+
+  logger = Logger.new($stdout)
+
+  logger.info("event:\n #{event}")
+  logger.info("context:\n #{context}")
+
+  # Create an instance of the Textract client
+  client = Aws::Translate::Client.new(region: "us-east-1")
+
+  client.translate_text({
+     text: event['Payload'], # required
+     source_language_code: "fr", # required
+     target_language_code: "en", # required
+   })
+
+end
+
+test = lambda_handler(event: nil, context: nil)
+puts test
