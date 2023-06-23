d3e91ca5d77a1c0c6acd24f59676cf9f3d8d545b
Corey Pyle <corepyle@amazon.com>
2023-06-23 10:57:16 -0400

    FSA: (client) Fix failing sign out call.

--- applications/feedback_sentiment_analyzer/client/src/store.ts
+++ applications/feedback_sentiment_analyzer/client/src/store.ts
@@ -70,7 +70,7 @@ export const useStore = create<Store>((set, get) => ({
     }
   },
   signOut() {
-    location.assign(`${COGNITO_SIGN_OUT_URL}&logout_uri=${location}`); // append ?redirect_uri=...
+    location.assign(`${COGNITO_SIGN_OUT_URL}&logout_uri=${location.origin}`);
   },
   async uploadFile(file: File) {
     return get().autoLogout(() => API.uploadFile(file, { token: get().token }));
