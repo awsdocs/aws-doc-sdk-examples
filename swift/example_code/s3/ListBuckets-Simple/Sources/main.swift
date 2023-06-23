0956c9e9c219f9f75e8d0029e81a78edc0689cbf
Eric Shepherd <ericsh@amazon.com>
2023-06-23 19:39:08 +0000

    Simplified example for Swift guide intro
    This is a simplified Amazon S3 "ListBuckets" example for the AWS SDK for
    Swift. There is no mocking support, no test suite, and no command line parsing
    or other "extra" features. All the code does is use your default credentials
    to dump a list of your Amazon S3 buckets to the screen.
    
    To test, build it and run. You should see a list of your Amazon S3 buckets.
    That's all there is to it.

---
+++ swift/example_code/s3/ListBuckets-Simple/Sources/main.swift
@@ -0,0 +1,61 @@
+/// A simple example that shows how to use the AWS SDK for Swift with the
+/// Amazon Simple Storage Service (Amazon S3) function `ListBuckets`.
+///
+/// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[s3.swift.intro.imports]
+import Foundation
+import ClientRuntime
+import AWSS3
+// snippet-end:[s3.swift.intro.imports]
+
+// snippet-start:[s3.swift.intro.getbucketnames]
+// Return an array containing the names of every available bucket.
+//
+// - Returns: An array of strings listing the available buckets.
+func getBucketNames() async throws -> [String] {
+    // Get an S3Client with which to access Amazon S3.
+    // snippet-start:[s3.swift.intro.client-init]
+    let client = try S3Client(region: "us-east-1")
+    try S3Client(config: S3Client.S3ClientConfiguration)
+    // snippet-end:[s3.swift.intro.client-init]
+
+    // snippet-start:[s3.swift.intro.listbuckets]
+    let output = try await client.listBuckets(
+        input: ListBucketsInput()
+    )
+    // snippet-end:[s3.swift.intro.listbuckets]
+    
+    // Get the bucket names.
+    var bucketNames: [String] = []
+
+    guard let buckets = output.buckets else {
+        return bucketNames
+    }
+    for bucket in buckets {
+        bucketNames.append(bucket.name ?? "<unknown>")
+    }
+
+    return bucketNames
+}
+// snippet-end:[s3.swift.intro.getbucketnames]
+
+// snippet-start:[s3.swift.intro.main]
+/// The program's asynchronous entry point.
+@main
+struct Main {
+    static func main() async {
+        do {
+            let names = try await getBucketNames()
+
+            print("Found \(names.count) buckets:")
+            for name in names {
+                print("  \(name)")
+            }
+        } catch {
+            dump(error, name: "Error getting the bucket list.")
+        }
+    }
+}
+// snippet-end:[s3.swift.intro.main]
