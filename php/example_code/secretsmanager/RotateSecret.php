1cfc57d04584c7e880893e9d35938cb89ada37e4
Todd Hill <tkhill@amazon.com>
2023-07-03 14:12:30 -0400

    Remove bad linke to lambda template. Change 'Secret' to 'Secrets' in 'Secret Manager'.

--- php/example_code/secretsmanager/RotateSecret.php
+++ php/example_code/secretsmanager/RotateSecret.php
@@ -24,7 +24,7 @@ use Aws\Exception\AwsException;
 // snippet-end:[secretsmanager.php.rotate_secret.import]
 
 /**
- * Immediately rotate a secret in an AWS Secret Manager Secret.
+ * Immediately rotate a secret in an AWS Secrets Manager Secret.
  *
  * This code expects that you have AWS credentials set up per:
  * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
