25b40c366b281e3bcc058561da77b779205fe29a
Eric Shepherd <ericsh@amazon.com>
2023-07-10 11:11:12 -0400

    Swift: Simplified example for Swift guide intro (#4983)
    This is a simplified Amazon S3 "ListBuckets" example for the AWS SDK for
    Swift. There is no mocking support, no test suite, and no command line parsing
    or other "extra" features. All the code does is use your default credentials
    to dump a list of your Amazon S3 buckets to the screen.

---
+++ swift/example_code/s3/ListBuckets-Simple/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.5
+//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ListBuckets-Simple",
+    // snippet-start:[s3.swift.intro.package-dependencies]
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/awslabs/aws-sdk-swift",
+            from: "0.17.0"
+        )
+    ],
+    // snippet-end:[s3.swift.intro.package-dependencies]
+    // snippet-start:[s3.swift.intro.package-targets]
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "ListBuckets-Simple",
+            dependencies: [
+                .product(name: "AWSS3", package: "aws-sdk-swift")
+            ],
+            path: "Sources")
+    ]
+    // snippet-end:[s3.swift.intro.package-targets]
+)
