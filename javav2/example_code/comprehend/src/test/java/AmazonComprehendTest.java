95849b635991351f40624dc252efa7ed0b773b57
scmacdon <scmacdon@amazon.com>
2023-07-10 12:25:27 -0400

    update tests

--- javav2/example_code/comprehend/src/test/java/AmazonComprehendTest.java
+++ javav2/example_code/comprehend/src/test/java/AmazonComprehendTest.java
@@ -1,13 +1,24 @@
-import com.example.comprehend.*;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import com.example.comprehend.*;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.*;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.comprehend.ComprehendClient;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import java.io.*;
-import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * To run these integration tests, you must set the required values
+ * in the config.properties file or AWS Secrets Manager.
+ */
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AmazonComprehendTest {
@@ -21,15 +32,23 @@ public class AmazonComprehendTest {
 
     @BeforeAll
     public static void setUp() throws IOException {
-
-        // Run tests on Real AWS Resources
-        Region region = Region.US_EAST_1;
         comClient = ComprehendClient.builder()
-                .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create())
+                .region(Region.US_EAST_1)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
 
+        // Get the values to run these tests from AWS Secrets Manager.
+        Gson gson = new Gson();
+        String json = getSecretValues();
+        SecretValues values = gson.fromJson(json, SecretValues.class);
+        dataAccessRoleArn = values.getDataAccessRoleArn();
+        s3Uri = values.getS3Uri();
+        documentClassifierName = values.getDocumentClassifier();
+
+        // Uncomment this code block if you prefer using a config.properties file to retrieve AWS values required for these tests.
+       /*
         try (InputStream input = AmazonComprehendTest.class.getClassLoader().getResourceAsStream("config.properties")) {
+
             Properties prop = new Properties();
 
             if (input == null) {
@@ -41,67 +60,88 @@ public class AmazonComprehendTest {
             prop.load(input);
 
             // Populate the data members required for all tests
-             dataAccessRoleArn = prop.getProperty("dataAccessRoleArn");
+            dataAccessRoleArn = prop.getProperty("dataAccessRoleArn");
             s3Uri = prop.getProperty("s3Uri");
             documentClassifierName = prop.getProperty("documentClassifier");
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        */
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(1)
-    public void whenInitializingAWSService_thenNotNull() {
-        assertNotNull(comClient);
+    public void DetectEntities() {
+        assertDoesNotThrow(() ->DetectEntities.detectAllEntities(comClient, text));
         System.out.println("Test 1 passed");
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(2)
-    public void DetectEntities() {
-
-        DetectEntities.detectAllEntities(comClient, text);
+    public void DetectKeyPhrases() {
+        assertDoesNotThrow(() ->DetectKeyPhrases.detectAllKeyPhrases(comClient, text));
         System.out.println("Test 2 passed");
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(3)
-    public void DetectKeyPhrases() {
-
-        DetectKeyPhrases.detectAllKeyPhrases(comClient, text);
+    public void DetectLanguage() {
+        assertDoesNotThrow(() ->DetectLanguage.detectTheDominantLanguage(comClient, frText));
         System.out.println("Test 3 passed");
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(4)
-    public void DetectLanguage() {
-
-        DetectLanguage.detectTheDominantLanguage(comClient, frText);
+    public void DetectSentiment() {
+        assertDoesNotThrow(() ->DetectSentiment.detectSentiments(comClient, text));
         System.out.println("Test 4 passed");
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(5)
-    public void DetectSentiment() {
-
-        DetectSentiment.detectSentiments(comClient, text);
+    public void DetectSyntax() {
+        assertDoesNotThrow(() -> DetectSyntax.detectAllSyntax(comClient, text));
         System.out.println("Test 5 passed");
     }
 
-    @Test
-    @Order(6)
-    public void DetectSyntax() {
+    private static String getSecretValues() {
+        SecretsManagerClient secretClient = SecretsManagerClient.builder()
+            .region(Region.US_EAST_1)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
+        String secretName = "test/comprehend";
 
-        DetectSyntax.detectAllSyntax(comClient, text);
-        System.out.println("Test 6 passed");
+        GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
+
+        GetSecretValueResponse valueResponse = secretClient.getSecretValue(valueRequest);
+        return valueResponse.secretString();
     }
 
-    @Test
-    @Order(7)
-    public void DocumentClassifierDemo() {
+    @Nested
+    @DisplayName("A class used to get test values from test/comprehend (an AWS Secrets Manager secret)")
+    class SecretValues {
+        private String dataAccessRoleArn;
+        private String s3Uri;
+        private String documentClassifier;
 
-      DocumentClassifierDemo.createDocumentClassifier(comClient, dataAccessRoleArn, s3Uri, documentClassifierName);
-     System.out.println("Test 7 passed");
+        public String getDataAccessRoleArn() {
+            return dataAccessRoleArn;
+        }
+
+        public String getS3Uri() {
+            return s3Uri;
+        }
+
+        public String getDocumentClassifier() {
+            return documentClassifier;
+        }
     }
 }
