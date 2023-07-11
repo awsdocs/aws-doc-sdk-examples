76315526fb5cef265fba1da8e28520192f058169
scmacdon <scmacdon@amazon.com>
2023-07-11 14:41:32 -0400

    updated Java tests

--- javav2/example_code/emr/src/test/java/EMRTest.java
+++ javav2/example_code/emr/src/test/java/EMRTest.java
@@ -1,17 +1,27 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
 import aws.example.emr.*;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.*;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.emr.EmrClient;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import java.io.*;
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * To run these integration tests, you must set the required values
+ * in the config.properties file or AWS Secrets Manager.
+ */
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EMRTest {
-
     private static EmrClient emrClient;
     private static String jar = "";
     private static String myClass = "" ;
@@ -23,27 +33,34 @@ public class EMRTest {
 
     @BeforeAll
     public static void setUp() throws IOException {
-
-        // Run tests on Real AWS Resources
-        Region region = Region.US_WEST_2;
         emrClient = EmrClient.builder()
-                .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
+            .region(Region.US_WEST_2)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
+
+        // Get the values to run these tests from AWS Secrets Manager.
+        Gson gson = new Gson();
+        String json = getSecretValues();
+        SecretValues values = gson.fromJson(json, SecretValues.class);
+        jar = values.getJar();
+        myClass = values.getMyClass();
+        keys = values.getKeys();
+        logUri = values.getLogUri();
+        name = values.getName();
+        existingClusterId= values.getExistingClusterId();
+
+
+        // Uncomment this code block if you prefer using a config.properties file to retrieve AWS values required for these tests.
+       /*
 
         try (InputStream input = EMRTest.class.getClassLoader().getResourceAsStream("config.properties")) {
-
             Properties prop = new Properties();
-
             if (input == null) {
                 System.out.println("Sorry, unable to find config.properties");
                 return;
             }
-
-            //load a properties file from class path, inside static method
+            // Populate the data members required for all tests.
             prop.load(input);
-
-            // Populate the data members required for all tests
             jar = prop.getProperty("jar");
             myClass = prop.getProperty("myClass");
             keys = prop.getProperty("keys");
@@ -54,80 +71,123 @@ public class EMRTest {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        */
     }
 
     @Test
     @Order(1)
-    public void whenInitializingAWSService_thenNotNull() {
-        assertNotNull(emrClient);
+    public void createClusterTest() {
+        jobFlowId = CreateCluster.createAppCluster(emrClient, jar, myClass, keys, logUri, name);
+        assertFalse(jobFlowId.isEmpty());
         System.out.println("Test 1 passed");
     }
 
     @Test
     @Order(2)
-    public void createClusterTest() {
-        jobFlowId =  CreateCluster.createAppCluster(emrClient, jar, myClass, keys, logUri, name);
-        assertTrue(!jobFlowId.isEmpty());
+    public void describeClusterTest() {
+        assertDoesNotThrow(() ->DescribeCluster.describeMyCluster(emrClient, existingClusterId));
         System.out.println("Test 2 passed");
     }
 
     @Test
     @Order(3)
-    public void describeClusterTest() {
-        DescribeCluster.describeMyCluster(emrClient, existingClusterId);
+    public void listClusterTest() {
+        assertDoesNotThrow(() ->ListClusters.listAllClusters(emrClient));
         System.out.println("Test 3 passed");
     }
 
     @Test
     @Order(4)
-    public void listClusterTest() {
-        ListClusters.listAllClusters(emrClient);
+    public void createEmrFleetTest() {
+        assertDoesNotThrow(() ->CreateEmrFleet.createFleet(emrClient));
         System.out.println("Test 4 passed");
     }
 
     @Test
     @Order(5)
-    public void createEmrFleetTest() {
-        CreateEmrFleet.createFleet(emrClient);
+    public void addStepsTest() {
+        assertDoesNotThrow(() -> AddSteps.addNewStep(emrClient, jobFlowId, jar, myClass));
         System.out.println("Test 5 passed");
     }
 
     @Test
     @Order(6)
-    public void addStepsTest() {
-        AddSteps.addNewStep(emrClient, jobFlowId, jar, myClass);
+    public void createSparkClusterTest(){
+        assertDoesNotThrow(() ->CreateSparkCluster.createCluster(emrClient, jar, myClass, keys, logUri, name));
         System.out.println("Test 6 passed");
     }
 
     @Test
     @Order(7)
-    public void createSparkClusterTest(){
-        CreateSparkCluster.createCluster(emrClient, jar, myClass, keys, logUri, name);
+    public void createHiveClusterTest() {
+        assertDoesNotThrow(() ->CreateHiveCluster.createCluster(emrClient, jar, myClass, keys, logUri, name));
         System.out.println("Test 7 passed");
+
     }
 
     @Test
     @Order(8)
-    public void createHiveClusterTest() {
-        CreateHiveCluster.createCluster(emrClient, jar, myClass, keys, logUri, name);
+    public void customEmrfsMaterialsTest(){
+        assertDoesNotThrow(() ->CustomEmrfsMaterials.createEmrfsCluster(emrClient, jar, myClass, keys, logUri, name));
         System.out.println("Test 8 passed");
-
     }
 
     @Test
     @Order(9)
-    public void customEmrfsMaterialsTest(){
-        CustomEmrfsMaterials.createEmrfsCluster(emrClient, jar, myClass, keys, logUri, name);
+    public void terminateJobFlowTest(){
+        assertDoesNotThrow(() ->TerminateJobFlow.terminateFlow(emrClient, existingClusterId));
         System.out.println("Test 9 passed");
     }
+    private static String getSecretValues() {
+        SecretsManagerClient secretClient = SecretsManagerClient.builder()
+            .region(Region.US_EAST_1)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
+        String secretName = "text/emr";
 
-    @Test
-    @Order(10)
-    public void terminateJobFlowTest(){
+        GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
 
-        TerminateJobFlow.terminateFlow(emrClient, existingClusterId);
-        System.out.println("Test 10 passed");
+        GetSecretValueResponse valueResponse = secretClient.getSecretValue(valueRequest);
+        return valueResponse.secretString();
     }
 
+    @Nested
+    @DisplayName("A class used to get test values from test/emr (an AWS Secrets Manager secret)")
+    class SecretValues {
+        private String existingClusterId;
+        private String jar;
+        private String myClass;
 
+        private String keys;
+
+        private String name;
+
+        private String logUri;
+
+
+        public String getLogUri() {
+            return logUri;
+        }
+        public String getJar() {
+            return jar;
+        }
+        public String getMyClass() {
+            return myClass;
+        }
+
+        public String getKeys() {
+            return keys;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getExistingClusterId() {
+            return existingClusterId;
+        }
+    }
 }
+
--- javav2/example_code/eventbridge/src/test/java/EventBridgeTest.java
+++ javav2/example_code/eventbridge/src/test/java/EventBridgeTest.java
@@ -3,7 +3,6 @@
    SPDX-License-Identifier: Apache-2.0
 */
 
-import com.example.eventbridge.EventbridgeMVP;
 import com.example.eventbridge.HelloEventBridge;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.MethodOrderer;
@@ -11,223 +10,30 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Order;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
-import software.amazon.awssdk.services.iam.IamClient;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.sns.SnsClient;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Scanner;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EventBridgeTest {
 
     private static  EventBridgeClient eventBrClient;
-    private static Region region;
-    private static String roleNameSc = "";
-    private static String bucketNameSc = "";
-    private static String topicNameSc = "";
-    private static String eventRuleNameSc = "";
-
-
-    public static final String DASHES = new String(new char[80]).replace("\0", "-");
 
     @BeforeAll
     public static void setUp() throws IOException {
-        region = Region.US_WEST_2;
         eventBrClient = EventBridgeClient.builder()
-                .region(region)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-
-        try (InputStream input = EventBridgeTest.class.getClassLoader().getResourceAsStream("config.properties")) {
-            Properties prop = new Properties();
-            if (input == null) {
-                System.out.println("Sorry, unable to find config.properties");
-                return;
-            }
-            prop.load(input);
-
-            // Populate the data members required for all tests.
-            roleNameSc = prop.getProperty("roleNameSc");
-            bucketNameSc = prop.getProperty("bucketNameSc");
-            topicNameSc = prop.getProperty("topicNameSc");
-            eventRuleNameSc = prop.getProperty("eventRuleNameSc");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+            .region(Region.US_WEST_2)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
     }
 
     @Test
     @Order(1)
-    public void whenInitializingService_thenNotNull() {
-        assertNotNull(eventBrClient);
+    public void helloEventBridge() {
+        HelloEventBridge.listBuses(eventBrClient);
         System.out.println("Test 1 passed");
     }
 
-    @Test
-    @Order(2)
-    public void helloEventBridge() {
-        HelloEventBridge.listBuses(eventBrClient);
-        System.out.println("Test 5 passed");
-    }
-
-    @Test
-    @Order(3)
-    public void eventBridgeTest() throws InterruptedException, IOException {
-        String polJSON = "{" +
-            "\"Version\": \"2012-10-17\"," +
-            "\"Statement\": [{" +
-            "\"Effect\": \"Allow\"," +
-            "\"Principal\": {" +
-            "\"Service\": \"events.amazonaws.com\"" +
-            "}," +
-            "\"Action\": \"sts:AssumeRole\"" +
-            "}]" +
-            "}";
-
-        Scanner sc = new Scanner(System.in);
-        Region region = Region.US_EAST_1;
-        S3Client s3Client = S3Client.builder()
-            .region(region)
-            .credentialsProvider(ProfileCredentialsProvider.create())
-            .build();
-
-        Region regionGl = Region.AWS_GLOBAL;
-        IamClient iam = IamClient.builder()
-            .region(regionGl)
-            .credentialsProvider(ProfileCredentialsProvider.create())
-            .build();
-
-        SnsClient snsClient = SnsClient.builder()
-            .region(region)
-            .credentialsProvider(ProfileCredentialsProvider.create())
-            .build();
-
-        System.out.println(DASHES);
-        System.out.println("1. Create an AWS Identity and Access Management (IAM) role to use with Amazon EventBridge.");
-        String roleArn = EventbridgeMVP.createIAMRole(iam, roleNameSc, polJSON);
-        assertFalse(roleArn.isEmpty());
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("2. Create an S3 bucket with EventBridge events enabled.");
-        if (EventbridgeMVP.checkBucket(s3Client, bucketNameSc)) {
-            System.out.println("Bucket "+ bucketNameSc +" already exists. Ending this scenario.");
-            System.exit(1);
-        }
-
-        EventbridgeMVP.createBucket(s3Client, bucketNameSc);
-        Thread.sleep(3000);
-        EventbridgeMVP.setBucketNotification(s3Client, bucketNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("3. Create a rule that triggers when an object is uploaded to Amazon S3.");
-        Thread.sleep(10000);
-        EventbridgeMVP.addEventRule(eventBrClient, roleArn, bucketNameSc, eventRuleNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("4. List rules on the event bus.");
-        EventbridgeMVP.listRules(eventBrClient);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("5. Create a new SNS topic for testing and let the user subscribe to the topic.");
-        String topicArn = EventbridgeMVP.createSnsTopic(snsClient, topicNameSc);
-        assertFalse(topicArn.isEmpty());
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("6. Add a target to the rule that sends an email to the specified topic");
-        System.out.println("Enter your email to subscribe to the Amazon SNS topic:");
-        String email = sc.nextLine();
-        EventbridgeMVP.subEmail(snsClient, topicArn, email);
-        System.out.println("Use the link in the email you received to confirm your subscription. Then, press Enter to continue.");
-        sc.nextLine();
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("7. Create an EventBridge event that sends an email when an Amazon S3 object is created.");
-        EventbridgeMVP.addSnsEventRule(eventBrClient, eventRuleNameSc, topicArn, topicNameSc, eventRuleNameSc, bucketNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println(" 8. List Targets.");
-        EventbridgeMVP.listTargets(eventBrClient, eventRuleNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println(" 9. List the rules for the same target.");
-        EventbridgeMVP.listTargetRules(eventBrClient, topicArn);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println(" 10. Trigger the rule by uploading a file to the S3 bucket.");
-        System.out.println("Press Enter to continue.");
-        sc.nextLine();
-        EventbridgeMVP.uploadTextFiletoS3(s3Client, bucketNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("11. Disable a specific rule.");
-        EventbridgeMVP.changeRuleState(eventBrClient, eventRuleNameSc, false);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("12. Check and print the state of the rule.");
-        EventbridgeMVP.checkRule(eventBrClient, eventRuleNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("13. Add a transform to the rule to change the text of the email.");
-        EventbridgeMVP.updateSnsEventRule(eventBrClient, topicArn, eventRuleNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("14. Enable a specific rule.");
-        EventbridgeMVP.changeRuleState(eventBrClient, eventRuleNameSc, true);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println(" 15. Trigger the updated rule by uploading a file to the S3 bucket.");
-        System.out.println("Press Enter to continue.");
-        sc.nextLine();
-        EventbridgeMVP.uploadTextFiletoS3(s3Client, bucketNameSc);
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println(" 16. Update the rule to be a custom rule pattern.");
-        EventbridgeMVP.updateToCustomRule(eventBrClient, eventRuleNameSc);
-        System.out.println("Updated event rule "+eventRuleNameSc +" to use a custom pattern.");
-        EventbridgeMVP.updateCustomRuleTargetWithTransform(eventBrClient, topicArn, eventRuleNameSc);
-        System.out.println("Updated event target "+topicArn +".");
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("17. Sending an event to trigger the rule. This will trigger a subscription email.");
-        EventbridgeMVP.triggerCustomRule(eventBrClient, email);
-        System.out.println("Events have been sent. Press Enter to continue.");
-        sc.nextLine();
-        System.out.println(DASHES);
-
-        System.out.println(DASHES);
-        System.out.println("18. Clean up resources.");
-        System.out.println("Do you want to clean up resources (y/n)");
-        String ans = sc.nextLine();
-        if (ans.compareTo("y") == 0) {
-            EventbridgeMVP.cleanupResources(eventBrClient, snsClient, s3Client, iam, topicArn, eventRuleNameSc, bucketNameSc, roleNameSc );
-        } else {
-            System.out.println("The resources will not be cleaned up. ");
-        }
-        System.out.println(DASHES);
-    }
 }
