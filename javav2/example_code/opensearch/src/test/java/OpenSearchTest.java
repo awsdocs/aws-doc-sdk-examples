f4612e15c2d0c47cbc3c8046b8dc1b2411c7c8fc
scmacdon <scmacdon@amazon.com>
2023-07-17 09:09:08 -0400

    update Java Tests

--- javav2/example_code/opensearch/src/test/java/OpenSearchTest.java
+++ javav2/example_code/opensearch/src/test/java/OpenSearchTest.java
@@ -4,15 +4,12 @@
 */
 
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.opensearch.OpenSearchClient;
 import java.io.*;
 import java.util.*;
 import com.example.search.*;
-
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -23,29 +20,21 @@ public class OpenSearchTest {
 
     @BeforeAll
     public static void setUp() throws IOException {
-
-        // Run tests on Real AWS Resources
         searchClient = OpenSearchClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-
+            .region(Region.US_EAST_1)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
 
         try (InputStream input = OpenSearchTest.class.getClassLoader().getResourceAsStream("config.properties")) {
-
             Properties prop = new Properties();
-
             if (input == null) {
                 System.out.println("Sorry, unable to find config.properties");
                 return;
             }
 
-            //load a properties file from class path, inside static method
-            prop.load(input);
-
             // Populate the data members required for all tests
-            domainName = prop.getProperty("domainName");
-
+            prop.load(input);
+            domainName ="testdomain";
 
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -53,38 +42,35 @@ public class OpenSearchTest {
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(1)
-    public void whenInitializingAWSService_thenNotNull() {
-        assertNotNull(searchClient);
+    public void createDomainTest() {
+        CreateDomain. createNewDomain(searchClient, domainName);
         System.out.println("Test 1 passed");
     }
 
+
     @Test
+    @Tag("IntegrationTest")
     @Order(2)
-    public void createDomainTest() {
-        CreateDomain. createNewDomain(searchClient, domainName);
+    public void listDomainNamesTest() {
+        ListDomainNames.listAllDomains(searchClient);
         System.out.println("Test 2 passed");
     }
 
-
     @Test
+    @Tag("IntegrationTest")
     @Order(3)
-    public void listDomainNamesTest() {
-        ListDomainNames.listAllDomains(searchClient);
+    public void UpdateDomain() {
+        UpdateDomain.updateSpecificDomain(searchClient, domainName);
         System.out.println("Test 3 passed");
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(4)
-    public void UpdateDomain() {
-        UpdateDomain.updateSpecificDomain(searchClient, domainName);
-        System.out.println("Test 4 passed");
-    }
-
-    @Test
-    @Order(5)
     public void DeleteDomain() {
         DeleteDomain.deleteSpecificDomain(searchClient, domainName);
-        System.out.println("Test 5 passed");
+        System.out.println("Test 4 passed");
     }
 }
