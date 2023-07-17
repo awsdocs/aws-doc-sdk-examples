60fabf9d7c143085cee67bdec244ec29aad2ae40
scmacdon <scmacdon@amazon.com>
2023-07-17 09:38:08 -0400

    update Java Tests

--- javav2/example_code/polly/src/test/java/AWSPollyTest.java
+++ javav2/example_code/polly/src/test/java/AWSPollyTest.java
@@ -2,8 +2,11 @@ import com.example.polly.DescribeVoicesSample;
 import com.example.polly.ListLexicons;
 import com.example.polly.PollyDemo;
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.polly.PollyClient;
@@ -13,46 +16,29 @@ import java.io.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 public class AWSPollyTest {
-
     private static PollyClient polly;
 
     @BeforeAll
-    public static void setUp() throws IOException {
-
+    public static void setUp() {
         polly = PollyClient.builder()
-                .region(Region.US_WEST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
+            .region(Region.US_WEST_2)
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .build();
     }
 
     @Test
+    @Tag("IntegrationTest")
     @Order(1)
-    public void whenInitializingAWSService_thenNotNull() {
-        assertNotNull(polly);
-        System.out.println("Running SNS Test 1");
-    }
-
-    @Test
-    @Order(2)
-    public void pollyDemo() {
-
-        PollyDemo.talkPolly(polly);
-        System.out.println("Test 2 passed");
-    }
-
-    @Test
-    @Order(3)
     public void describeVoicesSample() {
-
-        DescribeVoicesSample.describeVoice(polly);
-        System.out.println("Test 3 passed");
+        assertDoesNotThrow(() ->DescribeVoicesSample.describeVoice(polly));
+        System.out.println("describeVoicesSample test passed");
     }
 
     @Test
-    @Order(4)
+    @Tag("IntegrationTest")
+    @Order(2)
     public void listLexicons() {
-
-        ListLexicons.listLexicons(polly);
-        System.out.println("Test 4 passed");
+        assertDoesNotThrow(() ->ListLexicons.listLexicons(polly));
+        System.out.println("listLexicons test passed");
     }
 }
