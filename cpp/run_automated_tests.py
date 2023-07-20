94242c3477d766843f669fc634164950d95895e4
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-07-20 17:46:02 -0400

    Added hello service to tests

--- cpp/run_automated_tests.py
+++ cpp/run_automated_tests.py
@@ -28,13 +28,7 @@ import glob
 import re
 import datetime
 
-
-def build_tests(service="*"):
-    cmake_files = glob.glob( f"example_code/{service}/tests/CMakeLists.txt")
-    cmake_files.extend(glob.glob( f"example_code/{service}/gtests/CMakeLists.txt"))
-    cmake_files.extend(glob.glob(f"example_code/cross-service/{service}/tests/CMakeLists.txt"))
-
-
+def build_cmake_tests(cmake_files, executable_pattern) :
     run_files = []
 
     if len (cmake_files) == 0:
@@ -69,14 +63,23 @@ def build_tests(service="*"):
         if result_code != 0 :
             has_error = True
             continue
+        for executable in executable_pattern :
+            run_files.extend(glob.glob(f"{module_build_dir}{executable}"))
 
-        run_files.extend(glob.glob(f"{module_build_dir}/*_gtest"))
-        run_files.extend(glob.glob(f"{module_build_dir}/Debug/*_gtest.exe"))
-
-    if has_error :
+    os.chdir(base_dir)
+    if has_error:
         return [1, []]
     else:
         return [0, run_files]
+
+
+def build_tests(service="*"):
+    cmake_files = glob.glob( f"example_code/{service}/tests/CMakeLists.txt")
+    cmake_files.extend(glob.glob( f"example_code/{service}/gtests/CMakeLists.txt"))
+
+    executable_pattern = ["/*_gtest", "/Debug/*_gtest.exe"]
+
+    return build_cmake_tests(cmake_files, executable_pattern)
 
 
 def run_tests(run_files = [], type1=False, type2=False, type3=False):
@@ -118,15 +121,52 @@ def run_tests(run_files = [], type1=False, type2=False, type3=False):
         if proc.returncode != 0 :
             has_error = True
 
-    print('-'*88)
+    print('-' * 88)
     print(f"{passed_tests} tests passed.")
     print(f"{failed_tests} tests failed.")
 
     if has_error:
-        return 1
+        return [1, passed_tests, passed_tests]
     else :
-        return 0
+        return [0, passed_tests, failed_tests]
 
+def test_hello_service(service="*"):
+    print('-'*88)
+    print(f"Running hello tests for {service}.")
+
+    print(os.getcwd())
+    cmake_files = glob.glob(f"example_code/{service}/hello_{service}/CMakeLists.txt")
+
+    (err_code, run_files) = build_cmake_tests(cmake_files, ['/hello_*', '/Debug/hello_*.exe'])
+
+    if err_code != 0 :
+        print("Build hello tests failed.")
+        return [err_code, 0, 0]
+
+    passed_count = 0
+    failed_count = 0
+    has_error = False
+    for run_file in run_files :
+        path_split = os.path.splitext(run_file)
+        if (path_split[1] == '.exe') or (path_split[1] == ''):
+            print(f"Calling '{run_file}'.")
+            completedProcess = subprocess.run([run_file], stdout=subprocess.DEVNULL)
+            if completedProcess.returncode != 0 :
+                print(f"Error with {run_file}")
+                has_error = True
+                failed_count = failed_count + 1
+            else:
+                passed_count = passed_count + 1
+
+    print('-'*88)
+    print(f"{passed_count} tests passed.")
+    print(f"{failed_count} tests failed.")
+    print(f"Total cmake files - {len(cmake_files)}")
+
+    if has_error:
+        return [1,  passed_count, failed_count]
+    else :
+        return [0,  passed_count, failed_count]
 
 def main(argv):
     type1 = False
@@ -154,10 +194,20 @@ def main(argv):
             service = arg
 
     start_time = datetime.datetime.now()
+
     [err_code, run_files] = build_tests(service=service)
 
     if err_code == 0 :
-        err_code = run_tests(run_files = run_files, type1=type1, type2=type2, type3=type3)
+        [err_code, passed_count, failed_count]= run_tests(run_files = run_files, type1=type1, type2=type2, type3=type3)
+
+    if err_code == 0:
+        [err_code, hello_passed_count, hello_failed_count] = test_hello_service(service=service)
+        passed_count = passed_count + hello_passed_count
+        failed_count = failed_count + hello_failed_count
+
+    print('-'*88)
+    print(f"{passed_count} tests passed.")
+    print(f"{failed_count} tests failed.")
 
     print(f"Execution duration - {datetime.datetime.now() - start_time}")
 
@@ -165,8 +215,8 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    result = main(sys.argv[1:])
+     result = main(sys.argv[1:])
 
-    exit(result)
+     exit(result)
 
 
