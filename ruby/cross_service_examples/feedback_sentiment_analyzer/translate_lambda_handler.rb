4453ba2f35fef0679c57daeab4e79af297e81d32
ford-at-aws <fprior@amazon.com>
2023-07-24 11:33:03 -0600

    fixes for Ruby

--- ruby/cross_service_examples/feedback_sentiment_analyzer/translate_lambda_handler.rb
+++ ruby/cross_service_examples/feedback_sentiment_analyzer/translate_lambda_handler.rb
@@ -1,20 +1,21 @@
-require 'json'
-require 'aws-sdk-translate'
-require 'logger'
+# frozen_string_literal: true
+
+require "json"
+require "aws-sdk-translate"
+require "logger"
 
 def lambda_handler(event:, context:)
-
   logger = Logger.new($stdout)
 
   logger.info("event:\n #{event}")
   logger.info("context:\n #{context}")
 
   # Create an instance of the Textract client
-  client = Aws::Translate::Client.new(region: event['region'])
+  client = Aws::Translate::Client.new(region: event["region"])
 
   client.translate_text({
-                          text: event['extracted_text'], # required
-                          source_language_code: event['source_language_code'], # required
-                          target_language_code: 'en'
+                          text: event["extracted_text"], # required
+                          source_language_code: event["source_language_code"], # required
+                          target_language_code: "en"
                         })
 end
