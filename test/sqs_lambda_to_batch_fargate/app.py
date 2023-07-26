58fede4d5dcafbf67c83dc78878ade6f2ea55dbc
ford-at-aws <fprior@amazon.com>
2023-07-26 11:25:47 -0600

    pinned version of CDK

--- test/sqs_lambda_to_batch_fargate/app.py
+++ test/sqs_lambda_to_batch_fargate/app.py
@@ -8,7 +8,7 @@ from consumer_stack.consumer_stack import ConsumerStack
 import os
 
 app = App()
-ConsumerStack(app, "ConsumerStack4",
+ConsumerStack(app, "ConsumerStack",
               env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
               )
 
