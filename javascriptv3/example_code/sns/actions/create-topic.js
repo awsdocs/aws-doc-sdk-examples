493d268783f73f7bfba5e617017d1ae5e1e6eb88
Corey Pyle <corepyle@amazon.com>
2023-07-31 19:01:19 -0400

    JavaScript (v3): SNS - Cleanup (#5174)

---
+++ javascriptv3/example_code/sns/actions/create-topic.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from "url";
+
+// snippet-start:[sns.JavaScript.topics.createTopicV3]
+import { CreateTopicCommand } from "@aws-sdk/client-sns";
+import { snsClient } from "../libs/snsClient.js";
+
+/**
+ * @param {string} topicName - The name of the topic to create.
+ */
+export const createTopic = async (topicName = "TOPIC_NAME") => {
+  const response = await snsClient.send(
+    new CreateTopicCommand({ Name: topicName })
+  );
+  console.log(response);
+  // {
+  //   '$metadata': {
+  //     httpStatusCode: 200,
+  //     requestId: '087b8ad2-4593-50c4-a496-d7e90b82cf3e',
+  //     extendedRequestId: undefined,
+  //     cfId: undefined,
+  //     attempts: 1,
+  //     totalRetryDelay: 0
+  //   },
+  //   TopicArn: 'arn:aws:sns:us-east-1:xxxxxxxxxxxx:TOPIC_NAME'
+  // }
+  return response;
+};
+// snippet-end:[sns.JavaScript.topics.createTopicV3]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  createTopic();
+}
---
+++ javascriptv3/example_code/sns/actions/subscribe-app.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from "url";
+
+// snippet-start:[sns.JavaScript.subscriptions.subscribeAppV3]
+import { SubscribeCommand } from "@aws-sdk/client-sns";
+import { snsClient } from "../libs/snsClient.js";
+
+/**
+ * @param {string} topicArn - The ARN of the topic the subscriber is subscribing to.
+ * @param {string} endpoint - The Endpoint ARN of an application. This endpoint is created
+ *                            when an application registers for notifications.
+ */
+export const subscribeApp = async (topicArn = "TOPIC_ARN", endpoint = "ENDPOINT") => {
+  const response = await snsClient.send(
+    new SubscribeCommand({
+      Protocol: "application",
+      TopicArn: topicArn,
+      Endpoint: endpoint,
+    })
+  );
+  console.log(response);
+  // {
+  //   '$metadata': {
+  //     httpStatusCode: 200,
+  //     requestId: 'c8e35bcd-b3c0-5940-9f66-06f6fcc108f0',
+  //     extendedRequestId: undefined,
+  //     cfId: undefined,
+  //     attempts: 1,
+  //     totalRetryDelay: 0
+  //   },
+  //   SubscriptionArn: 'pending confirmation'
+  // }
+  return response;
+};
+// snippet-end:[sns.JavaScript.subscriptions.subscribeAppV3]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  subscribeApp();
+}
