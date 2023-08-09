f017e038ef16ceb505d617ccd9f1ce72b88ffee9
Corey Pyle <corepyle@amazon.com>
2023-08-09 15:57:21 -0400

    JavaScript (v3): Topics and Queues workflow (#5214)
    JavaScript (v3): Topics and Queues workflow

---
+++ javascriptv3/example_code/sqs/hello.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from "url";
+
+// snippet-start:[javascript.v3.sqs.hello]
+import { SQSClient, paginateListQueues } from "@aws-sdk/client-sqs";
+
+export const helloSqs = async () => {
+  // The configuration object (`{}`) is required. If the region and credentials
+  // are omitted, the SDK uses your local configuration if it exists.
+  const client = new SQSClient({});
+
+  // You can also use `ListQueuesCommand`, but to use that command you must
+  // handle the pagination yourself. You can do that by sending the `ListQueuesCommand`
+  // with the `NextToken` parameter from the previous request.
+  const paginatedQueues = paginateListQueues({ client }, {});
+  const queues = [];
+
+  for await (const page of paginatedQueues) {
+    if (page.QueueUrls?.length) {
+      queues.push(...page.QueueUrls);
+    }
+  }
+
+  const suffix = queues.length === 1 ? "" : "s";
+
+  console.log(
+    `Hello, Amazon SQS! You have ${queues.length} queue${suffix} in your account.`
+  );
+  console.log(queues.map((t) => `  * ${t}`).join("\n"));
+};
+// snippet-end:[javascript.v3.sqs.hello]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  helloSqs();
+}
