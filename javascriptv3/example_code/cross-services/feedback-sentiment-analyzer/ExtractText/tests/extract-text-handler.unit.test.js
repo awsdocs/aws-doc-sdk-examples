45ee189f7938548c4410858c7e5d3f6b1f9d0b9a
Corey Pyle <corepyle@amazon.com>
2023-08-11 10:22:30 -0400

    JavaScript (v3): FSA - Onboard to workspace and add ExtractText unit test.

---
+++ javascriptv3/example_code/cross-services/feedback-sentiment-analyzer/ExtractText/tests/extract-text-handler.unit.test.js
@@ -0,0 +1,39 @@
+import {describe, it, expect, vi} from "vitest";
+
+const send = vi.fn(() => Promise.resolve());
+
+vi.doMock("@aws-sdk/client-textract", async () => {
+  const actual = await vi.importActual("@aws-sdk/client-textract");
+  return {
+    ...actual,
+    TextractClient: class {
+      send = send;
+    },
+  };
+});
+
+const {handler} = await import("../src/index.js");
+
+describe("extract-text-handler", () => {
+  it("should create a single string from the word Blocks returned by textract", async () => {
+    send.mockResolvedValueOnce({
+      Blocks: [
+        {
+          BlockType: "LINE",
+          Text: "I love you.",
+        },
+        {
+          BlockType: "WORD",
+          Text: "I",
+        },
+        {
+          BlockType: "WORD",
+          Text: "know.",
+        },
+      ],
+    });
+
+    const response = await handler({bucket: "bucket", object: "object"});
+    expect(response).toEqual("I know.");
+  });
+});
