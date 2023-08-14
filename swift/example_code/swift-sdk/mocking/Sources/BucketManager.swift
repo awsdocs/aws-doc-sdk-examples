84ec90e9fc50eeda39b5398f1f09114f90d6be06
Eric Shepherd <ericsh@amazon.com>
2023-08-14 17:13:42 +0000

    Swift: Example showing how to mock the SDK This example demonstrates mocking the Swift SDK using a protocol and two implementations of the protocol: one that calls through to the SDK, and one that returns mock results.

---
+++ swift/example_code/swift-sdk/mocking/Sources/BucketManager.swift
@@ -0,0 +1,49 @@
+//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// A class to manage Amazon Simple Storage Service (Amazon S3) operations
+// using the ``S3Session`` class to access S3.
+//
+
+import Foundation
+import ClientRuntime
+import AWSS3
+
+/// A class that uses an object that uses an object of type
+/// ``S3SessionProtocol`` to access Amazon S3.
+// snippet-start:[mocking.swift.using-session.class]
+public class BucketManager {
+    /// The object based on the ``S3SessionProtocol`` protocol through which to
+    /// call SDK for swift functions. This may be either ``S3Session`` or
+    /// ``MockS3Session``.
+    var session: S3SessionProtocol
+
+    /// Initialize the ``S3Manager`` to call Amazon S3 functions using the
+    /// specified object that implements ``S3SessionProtocol``.
+    ///
+    /// - Parameter session: The session object to use when calling Amazon S3.
+    // snippet-start:[mocking.swift.using-session.init]
+    init(session: S3SessionProtocol) {
+        self.session = session
+    }
+    // snippet-end:[mocking.swift.using-session.init]
+
+    /// Return an array listing all of the user's buckets by calling the
+    /// ``S3SessionProtocol`` function `listBuckets()`.
+    /// 
+    /// - Returns: An array of bucket name strings.
+    ///
+    // snippet-start:[mocking.swift.using-session.calling]
+    public func getBucketNames() async throws -> [String] {
+        let output = try await session.listBuckets(input: ListBucketsInput())
+
+        guard let buckets = output.buckets else {
+            return []
+        }
+        
+        return buckets.map { $0.name ?? "<unknown>" }
+    }
+    // snippet-end:[mocking.swift.using-session.calling]
+}
+// snippet-end:[mocking.swift.using-session.class]
---
+++ swift/example_code/swift-sdk/mocking/Sources/mocking.swift
@@ -0,0 +1,59 @@
+//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// An example demonstrating how to mock AWS SDK for Swift functions using
+// protocols.
+//
+
+import Foundation
+import ClientRuntime
+import AWSS3
+
+/// The main entry point for the example is an asynchronous main function.
+@main
+struct MockingDemo {
+    /// The static, asynchronous entry point for the program.
+    static func main() async {
+        // snippet-start:[mocking.swift.main-setup]
+        /// An ``S3Session`` object that passes calls through to the SDK for
+        /// Swift.
+        let session: S3Session
+        /// A ``BucketManager`` object that will be initialized to call the
+        /// SDK using the session.
+        let bucketMgr: BucketManager
+        
+        // Create the ``S3Session`` and a ``BucketManager`` that calls the SDK
+        // using it.
+        do {
+            session = try S3Session(region: "us-east-1")
+            bucketMgr = BucketManager(session: session)
+        } catch {
+            print("Unable to initialize access to Amazon S3.")
+            return
+        }
+        // snippet-end:[mocking.swift.main-setup]
+
+        // snippet-start:[mocking.swift.main-call]
+        let bucketList: [String]
+
+        do {
+            bucketList = try await bucketMgr.getBucketNames()
+        } catch {
+            print("Unable to get the bucket list.")
+            return
+        }
+        // snippet-end:[mocking.swift.main-call]
+
+        // Print out a list of the bucket names.
+
+        if bucketList.count != 0 {
+            print("Found \(bucketList.count) buckets:")
+            for name in bucketList {
+                print("  \(name)")
+            }
+        } else {
+            print("No buckets found.")
+        }
+    }
+}
