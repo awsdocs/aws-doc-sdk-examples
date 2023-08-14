c5d5019a2addde3470315348bad4c9b889220c3c
Eric Shepherd <ericsh@amazon.com>
2023-08-14 20:16:29 +0000

    Swift: Add mocking example This example demonstrates how to use a protocol to mock the AWS SDK for Swift, since Swift lacks the readwrite reflection required for traditional mocking. Passes the test (which is also the exemplar).
    There is no build available on the web since this example is not one that corresponds to a particular function, but instead a technique for Swift developers using the SDK.
    
    Once this PR is submitted, I'll start writing the AWS SDK for Swift Developer Guide chapter that will embed these snippets as it explains the mocking technique used here.

---
+++ swift/example_code/swift-sdk/mocking/Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
+//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import PackageDescription
+
+let package = Package(
+    name: "mocking",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/awslabs/aws-sdk-swift",
+            from: "0.20.0"
+        )
+    ],
+    // snippet-start:[mocking.swift.package.targets]
+    targets: [
+        // A target defines a module or a test suite. A target can depend on
+        // other targets in this package. They can also depend on products in
+        // other packages that this package depends on.
+        .executableTarget(
+            name: "mocking",
+            dependencies: [
+                .product(name: "AWSS3", package: "aws-sdk-swift"),
+            ],
+            path: "./Sources"
+        ),
+        // snippet-start:[mocking.swift.package.testTarget]
+        .testTarget(
+            name: "mocking-tests",
+            dependencies: [
+                .product(name: "AWSS3", package: "aws-sdk-swift"),
+                "mocking"
+            ],
+            path: "./Tests"
+        )
+        // snippet-end:[mocking.swift.package.testTarget]
+    ]
+    // snippet-end:[mocking.swift.package.targets]
+)
