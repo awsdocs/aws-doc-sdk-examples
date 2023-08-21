a154753990c995551c0e43fba69a8e896ed030a1
Corey Pyle <corepyle@amazon.com>
2023-08-21 15:21:13 -0400

    JavaScript (v3): Add SES SendRawEmail example using nodemailer.

---
+++ javascriptv3/example_code/ses/src/send-with-attachments.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {fileURLToPath} from "url";
+
+// snippet-start:[javascript.v3.ses.attachment]
+import sesClientModule from "@aws-sdk/client-ses";
+/**
+ * nodemailer wraps the SES SDK and calls SendRawEmail. Use this for more advanced
+ * functionality like adding attachments to your email.
+ *
+ * https://nodemailer.com/transports/ses/
+ */
+import nodemailer from "nodemailer";
+
+/**
+ * @param {string} from An Amazon SES verified email address.
+ * @param {*} to An Amazon SES verified email address.
+ */
+export const sendEmailWithAttachments = (
+  from = "from@example.com",
+  to = "to@example.com"
+) => {
+  const ses = new sesClientModule.SESClient({});
+  const transporter = nodemailer.createTransport({
+    SES: {ses, aws: sesClientModule},
+  });
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        from,
+        to,
+        subject: "Hello World",
+        text: "Greetings from Amazon SES!",
+        attachments: [{content: "Hello World!", filename: "hello.txt"}],
+      },
+      (err, info) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      }
+    );
+  });
+};
+// snippet-end:[javascript.v3.ses.attachment]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  sendEmailWithAttachments();
+}
