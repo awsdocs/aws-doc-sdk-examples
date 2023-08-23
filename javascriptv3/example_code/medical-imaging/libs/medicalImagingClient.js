47a2b9138a7d34074db8fa6e83cbdd23a2efc68f
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-08-23 15:46:24 -0400

    added create datastore

---
+++ javascriptv3/example_code/medical-imaging/libs/medicalImagingClient.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// snippet-start:[sns.JavaScript.createclientv3]
+import { MedicalImagingClient } from "@aws-sdk/client-medical-imaging";
+
+// The AWS Region can be provided here using the `region` property. If you leave it blank
+// the SDK will default to the region set in your AWS config.
+export const medicalImagingClient = new MedicalImagingClient({});
+// snippet-end:[sns.JavaScript.createclientv3]
