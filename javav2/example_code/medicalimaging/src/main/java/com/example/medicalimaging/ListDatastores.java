d8ca9dcefddfbff59fa7836240f57c22611122d3
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-08-23 14:51:49 -0400

    Added 4 actions

---
+++ javav2/example_code/medicalimaging/src/main/java/com/example/medicalimaging/ListDatastores.java
@@ -0,0 +1,60 @@
+//snippet-sourcedescription:[ListDataStore.java demonstrates how to list the datastores in an AWS HealthImaging account.]
+//snippet-keyword:[AWS SDK for Java v2]
+//snippet-keyword:[AWS HealthImaging]
+
+package com.example.medicalimaging;
+
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+//snippet-start:[medicalimaging.java2.list_datastores.import]
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.medicalimaging.MedicalImagingClient;
+import software.amazon.awssdk.services.medicalimaging.model.*;
+
+import java.util.List;
+//snippet-end:[medicalimaging.java2.list_datastores.import]
+
+/**
+ * Before running this Java V2 code example, set up your development environment, including your credentials.
+ *
+ * For more information, see the following documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class ListDatastores {
+
+    public static void main(String[] args) {
+
+        Region region = Region.US_WEST_2;
+        MedicalImagingClient medicalImagingClient = MedicalImagingClient.builder()
+                .region(region)
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+
+        List<DatastoreSummary> datastoreSummaries = listMedicalImagingDatastores(medicalImagingClient);
+        if (datastoreSummaries != null) {
+            System.out.println("The medical imaging datastore properties are " + datastoreSummaries);
+        }
+        medicalImagingClient.close();
+    }
+
+    //snippet-start:[medicalimaging.java2.list_datastores.main]
+    public static List<DatastoreSummary> listMedicalImagingDatastores(MedicalImagingClient medicalImagingClient) {
+        try {
+            ListDatastoresRequest datastoreRequest = ListDatastoresRequest.builder()
+                     .build();
+            ListDatastoresResponse response = medicalImagingClient.listDatastores(datastoreRequest);
+            return  response.datastoreSummaries();
+        } catch (MedicalImagingException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
+        return null;
+    }
+    //snippet-end:[medicalimaging.java2.list_datastores.main]
+}
