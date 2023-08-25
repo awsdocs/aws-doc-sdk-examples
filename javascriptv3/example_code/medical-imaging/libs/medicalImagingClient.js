a1f274352281be632db48f55f6b7ca536ab4e9e5
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-08-25 09:11:30 -0400

    SOS fix

--- javascriptv3/example_code/medical-imaging/libs/medicalImagingClient.js
+++ javascriptv3/example_code/medical-imaging/libs/medicalImagingClient.js
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// snippet-start:[sns.JavaScript.createclientv3]
+// snippet-start:[medical-imaging.JavaScript.createclientv3]
 import { MedicalImagingClient } from "@aws-sdk/client-medical-imaging";
 
 // The AWS Region can be provided here using the `region` property. If you leave it blank
 // the SDK will default to the region set in your AWS config.
 export const medicalImagingClient = new MedicalImagingClient();
-// snippet-end:[sns.JavaScript.createclientv3]
+// snippet-end:[medical-imaging.JavaScript.createclientv3]
