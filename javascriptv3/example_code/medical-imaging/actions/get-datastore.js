889e00d409a2103f7b896d3233c6b64624cbed93
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-08-29 09:38:54 -0400

    Javascript: Added 4 actions for HealthImaging (#5327)

---
+++ javascriptv3/example_code/medical-imaging/actions/get-datastore.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {fileURLToPath} from "url";
+
+// snippet-start:[medical-imaging.JavaScript.datastore.getDatastoreV3]
+import {GetDatastoreCommand} from "@aws-sdk/client-medical-imaging";
+import {medicalImagingClient} from "../libs/medicalImagingClient.js";
+
+/**
+ * @param {string} datastoreID - The ID of the datastore to retrieve properties for.
+ */
+export const getDatastore = async (datastoreID = "DATASTORE_ID") => {
+    const response = await medicalImagingClient.send(
+        new GetDatastoreCommand({datastoreId: datastoreID})
+    );
+    console.log(response);
+    // {
+    //   '$metadata': {
+    //       httpStatusCode: 200,
+    //       requestId: '55ea7d2e-222c-4a6a-871e-4f591f40cadb',
+    //       extendedRequestId: undefined,
+    //       cfId: undefined,
+    //       attempts: 1,
+    //       totalRetryDelay: 0
+    //    },
+    //   datastoreProperties: {
+    //        createdAt: 2023-08-04T18:50:36.239Z,
+    //         datastoreArn: 'arn:aws:medical-imaging:us-east-1:123456789:datastore/1234567890abcdef01234567890abcde',
+    //         datastoreArn: 'arn:aws:medical-imaging:us-east-1:xxxxxxxxx:datastore/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    //         datastoreId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    //         datastoreName: 'my_datastore',
+    //         datastoreStatus: 'ACTIVE',
+    //         updatedAt: 2023-08-04T18:50:36.239Z
+    //   }
+    // }
+    return response.datastoreProperties;
+};
+// snippet-end:[medical-imaging.JavaScript.datastore.getDatastoreV3]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    getDatastore();
+}
---
+++ javascriptv3/example_code/medical-imaging/tests/create-delete-datastore.integration.test.js
@@ -0,0 +1,46 @@
+import {describe, it, expect} from "vitest";
+
+import {createDatastore} from "../actions/create-datastore.js";
+import {deleteDatastore} from "../actions/delete-datastore.js";
+import {getDatastore} from "../actions/get-datastore.js";
+import {listDatastores} from "../actions/list-datastores.js";
+import {wait} from "../../libs/utils/util-timers.js";
+
+
+describe("createDatastore/deleteDatastore", () => {
+        let datastoreID = "";
+        const datastoreName = "createDeleteDatastoreJSTest";
+
+        it("should create and delete a datastore", async () => {
+            // Create topic.
+            const createDatastoreCommandOutput = await createDatastore(datastoreName);
+            datastoreID = createDatastoreCommandOutput.datastoreId;
+
+            expect(datastoreID).toBeDefined();
+
+            let status = "NONE";
+            let counter = 1;
+            while ((counter < 20) && (status !== "ACTIVE")) {  // Redundant check with test timeout.
+                await wait(1);
+                const getDatastoreCommandOutput = await getDatastore(datastoreID);
+                status = getDatastoreCommandOutput.datastoreStatus; // eslint-disable-line
+                counter++;
+            }
+
+            const listDatastoresCommandOutput = await listDatastores();
+
+            let found = false;
+            for (const datastore of listDatastoresCommandOutput) {
+                if (datastore.datastoreId === datastoreID) { // eslint-disable-line
+                    found = true;
+                    break;
+                }
+            }
+
+            expect(found).toBe(true);
+
+            // Delete topic.
+            await deleteDatastore(datastoreID);
+        });
+    },
+    20000);  // 20 seconds test timeout.
