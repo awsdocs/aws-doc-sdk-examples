b7e5dbe73b910b8763538b78a1770b0ff727c452
scmacdon <scmacdon@amazon.com>
2023-09-01 11:34:48 -0400

    added updated kotlin build file

--- kotlin/services/emr/build.gradle.kts
+++ kotlin/services/emr/build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("aws.sdk.kotlin:secretsmanager-jvm:0.29.1-beta")
     implementation("com.google.code.gson:gson:2.10")
-
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
