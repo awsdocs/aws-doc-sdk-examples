f6d679e8aeecf1010e999eec6f3307ef0a2473f6
tae2089 <tae2089002@gmail.com>
2023-09-22 16:43:27 +0900

    using mocking

--- gov2/cloudfront/CreateDistribution/CreateDistribution_test.go
+++ gov2/cloudfront/CreateDistribution/CreateDistribution_test.go
@@ -4,13 +4,49 @@ package main
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+type MockCFDistributionAPI struct {
+	s3Client         *s3.Client
+	cloudfrontClient *cloudfront.Client
+}
+
+func (m *MockCFDistributionAPI) CreateDistribution(bucketName, certificateSSLArn, domain string) (*cloudfront.CreateDistributionOutput, error) {
+	if bucketName == "" || certificateSSLArn == "" || domain == "" {
+		return nil, errors.New("bucket name, certificate SSL ARN, and domain are required")
+	}
+	return &cloudfront.CreateDistributionOutput{
+		Distribution: &types.Distribution{
+			ARN:        aws.String("arn:aws:cloudfront::1234567890:distribution/AAAAAAAAAAAA"),
+			DomainName: aws.String(domain),
+			DistributionConfig: &types.DistributionConfig{
+				ViewerCertificate: &types.ViewerCertificate{
+					ACMCertificateArn: aws.String(certificateSSLArn),
+				},
+			},
+		},
+	}, nil
+}
+
+func (m *MockCFDistributionAPI) createoriginAccessIdentity(domainName string) (string, error) {
+	return domainName, nil
+}
+
+func createMockCFDistribution(s3client *s3.Client, cloudfront *cloudfront.Client) CFDistributionAPI {
+	return &MockCFDistributionAPI{
+		s3Client:         s3client,
+		cloudfrontClient: cloudfront,
+	}
+}
 
 func TestCreateDistribution(t *testing.T) {
 	thisTime := time.Now()
@@ -27,14 +63,19 @@ func TestCreateDistribution(t *testing.T) {
 
 	s3Client := s3.NewFromConfig(sdkConfig)
 	cloudfrontClient := cloudfront.NewFromConfig(sdkConfig)
-	bucketName := "<EXAMPLE-BUCKET-NAME>"
-	certificateSSLArn := "<AWS CERTIFICATE MANGER ARN>"
-	domain := "<YOUR DOMAIN>"
-	result, err := CreateDistribution(s3Client, cloudfrontClient, bucketName, certificateSSLArn, domain)
+
+	mockCFDistribution := createMockCFDistribution(s3Client, cloudfrontClient)
+
+	bucketName := "example-com"
+	certificateSSLArn := "arn:aws:acm:ap-northeast-2:123456789000:certificate/000000000-0000-0000-0000-000000000000"
+	domain := "example.com"
+
+	result, err := mockCFDistribution.CreateDistribution(bucketName, certificateSSLArn, domain)
 
 	if err != nil {
 		t.Error(err)
 		return
 	}
+
 	t.Log("Created Distribution with ARN: " + *result.Distribution.ARN + " for name: " + *result.Distribution.DomainName)
 }
