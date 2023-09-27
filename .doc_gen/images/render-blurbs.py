be0d8c7d6d9f021ce322c86aaa01762ccc1bd9c6
ford-at-aws <fprior@amazon.com>
2023-09-27 12:35:17 -0400

    fixing linting

--- .doc_gen/images/render-blurbs.py
+++ .doc_gen/images/render-blurbs.py
@@ -1,19 +1,19 @@
+import os
+
 import jinja2
 import yaml
-import os
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    autoescape=select_autoescape(enabled_extensions=('html', 'xml'), default_for_string=True)
+    autoescape=select_autoescape(
+        enabled_extensions=("html", "xml"), default_for_string=True
+    ),
 )
 
-with open('../metadata/sdks.yaml', 'r') as file:
+with open("../metadata/sdks.yaml", "r") as file:
     metadata = yaml.safe_load(file)
     for language in metadata.keys():
         metadata[language]
-        shortname = metadata[language]['property']
+        shortname = metadata[language]["property"]
         template = env.get_template("template.txt")
         print(template.render(language=language, shortname=shortname))
-
-
-
