e4834abb5226ab60cb659e7ae0f9ac8ee8c2d90e
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2023-09-29 17:06:14 -0400

    adding tests

--- javascriptv3/example_code/medical-imaging/tests/list-dicom-import-jobs.unit.test.js
+++ javascriptv3/example_code/medical-imaging/tests/list-dicom-import-jobs.unit.test.js
@@ -17,27 +17,28 @@ vi.doMock("@aws-sdk/client-medical-imaging", async () => {
     };
 });
 
-const { startDicomImportJob } = await import("../actions/start-dicom-import-job.js");
+const { listDICOMImportJobs } = await import("../actions/list-dicom-import-jobs.js");
 
-describe("start-dicom-import-job", () => {
+describe("list-dicom-import-job", () => {
     it("should log the response", async () => {
         const logSpy = vi.spyOn(console, "log");
         const datastoreId = "12345678901234567890123456789012";
 
-        const response = {
-            jobId: "12345678901234567890123456789012",
-            datastoreId: datastoreId,
-            jobStatus: 'CREATING',
-            submittedAt: '2019-01-01T00:00:00.000Z'
-        };
+        const response = {jobSummaries: [
+                {
+                    dataAccessRoleArn: 'arn:aws:iam::xxxxxxxxxxxx:role/dicom_import',
+                    datastoreId: datastoreId,
+                    endedAt: '2023-09-22T14:49:51.351Z',
+            jobId: '12345678901234567890123456789012',
+            jobName: 'test-1',
+            jobStatus: 'COMPLETED',
+            submittedAt: '2023-09-22T14:48:45.767Z'
+    }
+    ]};
 
         send.mockResolvedValueOnce(response);
 
-        await startDicomImportJob("test-1",
-            datastoreId,
-            "arn:aws:iam::xxxxxxxxxxxx:role/ImportJobDataAccessRole",
-             "s3://medical-imaging-dicom-input/dicom_input/",
-            "s3://medical-imaging-output/job_output/");
+        await listDICOMImportJobs(datastoreId);
 
         expect(logSpy).toHaveBeenCalledWith(response);
     });
