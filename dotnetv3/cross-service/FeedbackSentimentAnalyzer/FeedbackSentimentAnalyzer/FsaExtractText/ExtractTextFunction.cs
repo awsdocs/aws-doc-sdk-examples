316b0f85f17c5c9f92c8178b2003779c33d8a17d
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2023-10-03 10:36:48 -0500

    Adding new code files.

---
+++ dotnetv3/cross-service/FeedbackSentimentAnalyzer/FeedbackSentimentAnalyzer/FsaExtractText/ExtractTextFunction.cs
@@ -0,0 +1,65 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier:  Apache-2.0
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Util;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace FsaExtractText;
+
+public class ExtractTextFunction
+{
+    IAmazonS3 S3Client { get; set; }
+
+    /// <summary>
+    /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+    /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+    /// region the Lambda function is executed in.
+    /// </summary>
+    public ExtractTextFunction()
+    {
+        S3Client = new AmazonS3Client();
+    }
+
+    /// <summary>
+    /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
+    /// </summary>
+    /// <param name="s3Client"></param>
+    public ExtractTextFunction(IAmazonS3 s3Client)
+    {
+        this.S3Client = s3Client;
+    }
+
+    /// <summary>
+    /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+    /// to respond to S3 notifications.
+    /// </summary>
+    /// <param name="evnt"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async Task<string?> FunctionHandler(S3Event evnt, ILambdaContext context)
+    {
+        var s3Event = evnt.Records?[0].S3;
+        if (s3Event == null)
+        {
+            return null;
+        }
+
+        try
+        {
+            var response = await this.S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
+            return response.Headers.ContentType;
+        }
+        catch (Exception e)
+        {
+            context.Logger.LogInformation($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+            context.Logger.LogInformation(e.Message);
+            context.Logger.LogInformation(e.StackTrace);
+            throw;
+        }
+    }
+}
---
+++ dotnetv3/cross-service/FeedbackSentimentAnalyzer/FeedbackSentimentAnalyzer/FsaServices/Class1.cs
@@ -0,0 +1,10 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier:  Apache-2.0
+
+namespace FsaServices
+{
+    public class Class1
+    {
+
+    }
+}
