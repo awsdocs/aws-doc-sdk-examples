766395cf2a68d10d96c238af59ac49beba1a5a0f
David Souther <davidsouther+github@gmail.com>
2023-10-14 10:41:15 -0400

    Remove deprecated aws-smithy-client

--- rust_dev_preview/run_all.sh
+++ rust_dev_preview/run_all.sh
@@ -15,11 +15,11 @@ export APP_ENVIRONMENT="test"
 
 FAIL=()
 for f in ${DIRS[@]} ; do
-  "$HOME/.cargo/bin/cargo" fmt --manifest-path rust_dev_preview/$f/Cargo.toml --all --check || FAIL+=("fmt $f")
-  "$HOME/.cargo/bin/cargo" clippy --manifest-path rust_dev_preview/$f/Cargo.toml --all || FAIL+=("clippy $f")
-  "$HOME/.cargo/bin/cargo" test --manifest-path rust_dev_preview/$f/Cargo.toml --all || FAIL+=("clippy $f")
+  "$HOME/.cargo/bin/cargo" fmt --manifest-path rust_dev_preview/$f/Cargo.toml --all --check || FAIL+=("fmt:$f")
+  "$HOME/.cargo/bin/cargo" clippy --manifest-path rust_dev_preview/$f/Cargo.toml --all || FAIL+=("clippy:$f")
+  "$HOME/.cargo/bin/cargo" test --manifest-path rust_dev_preview/$f/Cargo.toml --all || FAIL+=("test:$f")
 done
 
-echo $FAIL
-
+echo $FAIL;
 exit "${#FAIL[@]}"
+
