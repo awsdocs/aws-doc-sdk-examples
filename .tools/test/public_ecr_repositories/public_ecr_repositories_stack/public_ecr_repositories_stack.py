133e0c2aff6212666806a1d04210f9527ee611aa
ford-at-aws <fprior@amazon.com>
2023-10-20 10:23:03 -0400

    Move Weathertop

--- .tools/test/public_ecr_repositories/public_ecr_repositories_stack/public_ecr_repositories_stack.py
+++ .tools/test/public_ecr_repositories/public_ecr_repositories_stack/public_ecr_repositories_stack.py
@@ -1,7 +1,8 @@
+import yaml
 from aws_cdk import Stack
 from aws_cdk import aws_ecr as ecr
 from constructs import Construct
-import yaml
+
 
 class PublicEcrRepositoriesStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
