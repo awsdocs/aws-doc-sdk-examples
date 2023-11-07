05bf5446722df2ef9e45ebb3869e5e6a88b28e54
scmacdon <scmacdon@amazon.com>
2023-11-07 10:55:05 -0500

    update the Kotlin build file

--- kotlin/services/cloudformation/build.gradle.kts
+++ kotlin/services/cloudformation/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -23,12 +23,25 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:cloudformation:0.30.1-beta")
-    implementation("aws.sdk.kotlin:secretsmanager:0.30.1-beta")
+    implementation("aws.sdk.kotlin:cloudformation:0.33.1-beta")
+    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test")
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
 }
