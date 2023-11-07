d2230e53743e76eff1b976686c4fc27a0cfe1ba4
scmacdon <scmacdon@amazon.com>
2023-11-07 12:36:23 -0500

    update the Kotlin build file

--- kotlin/services/eventbridge/build.gradle.kts
+++ kotlin/services/eventbridge/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -19,17 +19,29 @@ buildscript {
 
 repositories {
     mavenCentral()
-    jcenter()
 }
+
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:eventbridge-jvm:0.29.1-beta")
-    implementation("aws.sdk.kotlin:iam-jvm:0.29.1-beta")
-    implementation("aws.sdk.kotlin:sns-jvm:0.29.1-beta")
-    implementation("aws.sdk.kotlin:s3-jvm:0.29.1-beta")
+    implementation("aws.sdk.kotlin:eventbridge-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:iam-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:sns-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:s3-jvm:0.33.1-beta")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
+}
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test") // Change this path to match your project structure
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
 }
