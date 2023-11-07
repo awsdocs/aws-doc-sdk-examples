5798b0b0caf89e805e0e647d0e697d5172b31e60
scmacdon <scmacdon@amazon.com>
2023-11-07 11:31:06 -0500

    update the Kotlin build file

--- kotlin/services/cloudwatch/build.gradle.kts
+++ kotlin/services/cloudwatch/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -23,10 +23,12 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:cloudwatch-jvm:0.30.1-beta")
-    implementation("aws.sdk.kotlin:cloudwatchevents-jvm:0.30.1-beta")
-    implementation("aws.sdk.kotlin:cloudwatchlogs-jvm:0.30.1-beta")
-    implementation("aws.sdk.kotlin:secretsmanager:0.30.1-beta")
+    implementation("aws.sdk.kotlin:cloudwatch-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:cloudwatchevents-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:cloudwatchlogs-jvm:0.33.1-beta")
+    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
     implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
@@ -34,5 +36,16 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.test {
+    useJUnitPlatform() // Use JUnit 5 for running tests
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test") // Change this path to match your project structure
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
 }
