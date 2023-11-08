6393c8aa9cf48ecc1eeeada62cf23d6fe1c68012
Eric Shepherd <ericsh@amazon.com>
2023-11-08 15:48:09 +0000

    Fix copypasta

--- rust_dev_preview/examples/testing/src/replay.rs
+++ rust_dev_preview/examples/testing/src/replay.rs
@@ -125,7 +125,7 @@ mod test {
                     .unwrap(),
             );
         let replay_client = StaticReplayClient::new(vec![page_1, page_2]);
-        // snippet-start:[testing.rust.replay-create-client]
+        // snippet-end:[testing.rust.replay-create-client]
         let client: s3::Client = s3::Client::from_conf(
             s3::Config::builder()
                 .credentials_provider(make_s3_test_credentials())
