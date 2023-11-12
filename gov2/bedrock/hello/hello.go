3a7b2b98f2f4e5908079b5eac27e67ea6bbccfea
Dennis Traub <traubd@amazon.com>
2023-11-12 17:38:38 +0100

    Go: Add Bedrock ListFoundationModels

---
+++ gov2/bedrock/hello/hello.go
@@ -0,0 +1,42 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[gov2.bedrock.Hello]
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/bedrock"
+)
+
+const region = "us-east-1"
+
+// main uses the AWS SDK for Go (v2) to create an Amazon Bedrock client and
+// list the available foundation models in your account and the chosen region.
+// This example uses the default settings specified in your shared credentials
+// and config files.
+func main() {
+    sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+    if err != nil {
+        fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+        fmt.Println(err)
+        return
+    }
+    bedrockClient := bedrock.NewFromConfig(sdkConfig)
+    result, err := bedrockClient.ListFoundationModels(context.TODO(), &bedrock.ListFoundationModelsInput{})
+    if err != nil {
+		fmt.Printf("Couldn't list foundation models. Here's why: %v\n", err)
+		return
+    }
+    if len(result.ModelSummaries) == 0 {
+    fmt.Println("There are no foundation models.")}
+    for _, modelSummary := range result.ModelSummaries {
+        fmt.Println(fmt.Sprintf("%s", *modelSummary.ModelId))
+    }
+}
+
+// snippet-end:[gov2.bedrock.Hello]
