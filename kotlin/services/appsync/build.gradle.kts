a0ad10f3653106a49277588fc9715e0baae750d2
scmacdon <scmacdon@amazon.com>
2023-11-14 16:13:35 -0500

    updated to ga build

--- kotlin/services/appsync/build.gradle.kts
+++ kotlin/services/appsync/build.gradle.kts
@@ -22,10 +22,10 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:appsync:0.33.1-beta")
-    implementation("aws.sdk.kotlin:sts:0.33.1-beta")
-    implementation("aws.sdk.kotlin:s3:0.33.1-beta")
-    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.sdk.kotlin:appsync:1.0.0")
+    implementation("aws.sdk.kotlin:sts:1.0.0")
+    implementation("aws.sdk.kotlin:1.0.0")
+    implementation("aws.sdk.kotlin:secretsmanager:1.0.0")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
