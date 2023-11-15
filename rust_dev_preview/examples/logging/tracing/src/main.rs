3bcbde11a0d83aee9ccc3da7046a31a40b05c391
John DiSanti <jdisanti@amazon.com>
2023-11-15 15:47:23 -0800

    Use BV in SDK guide examples

--- rust_dev_preview/examples/logging/tracing/src/main.rs
+++ rust_dev_preview/examples/logging/tracing/src/main.rs
@@ -7,6 +7,7 @@
 
 // snippet-start:[tracing.rust.main]
 use aws_config::meta::region::RegionProviderChain;
+use aws_config::BehaviorVersion;
 use aws_sdk_dynamodb::{config::Region, meta::PKG_VERSION, Client, Error};
 use clap::Parser;
 
@@ -45,7 +46,10 @@ async fn main() -> Result<(), Error> {
         println!();
     }
 
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let shared_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
     let client = Client::new(&shared_config);
 
     let resp = client.list_tables().send().await?;
