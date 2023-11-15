6aa4d62b93fb1d23499815060d7ba921bf9807cd
scmacdon <scmacdon@amazon.com>
2023-11-15 11:13:49 -0500

    conflict fix

--- kotlin/services/forecast/build.gradle.kts
+++ kotlin/services/forecast/build.gradle.kts
@@ -8,11 +8,6 @@ plugins {
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 buildscript {
     repositories {
         maven("https://plugins.gradle.org/m2/")
@@ -27,12 +22,24 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:forecast:1.0.0")
-    implementation("aws.sdk.kotlin:secretsmanager:1.0.0")
+    implementation("aws.sdk.kotlin:forecast:0.33.1-beta")
+    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "17"
+}
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test")
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
 }
--- kotlin/services/iam/build.gradle.kts
+++ kotlin/services/iam/build.gradle.kts
@@ -8,11 +8,6 @@ plugins {
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 buildscript {
     repositories {
         maven("https://plugins.gradle.org/m2/")
@@ -25,13 +20,14 @@ buildscript {
 repositories {
     mavenCentral()
 }
-
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:iam:1.0.0")
-    implementation("aws.sdk.kotlin:sts:1.0.0")
-    implementation("aws.sdk.kotlin:s3:1.0.0")
-    implementation("aws.sdk.kotlin:secretsmanager:1.0.0)
+    implementation("aws.sdk.kotlin:iam:0.33.1-beta")
+    implementation("aws.sdk.kotlin:sts:0.33.1-beta")
+    implementation("aws.sdk.kotlin:s3:0.33.1-beta")
+    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
@@ -40,7 +36,6 @@ dependencies {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "17"
 }
-
 tasks.test {
     useJUnitPlatform()
     testLogging {
