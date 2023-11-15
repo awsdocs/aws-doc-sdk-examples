e60b38dbb408e1539abb4ebd62afc0c2c9f94bc9
scmacdon <scmacdon@amazon.com>
2023-11-15 16:06:48 -0500

    add GA Build

--- kotlin/usecases/serverless_rds/build.gradle.kts
+++ kotlin/usecases/serverless_rds/build.gradle.kts
@@ -27,8 +27,8 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:rdsdata:0.33.1-beta")
-    implementation("aws.sdk.kotlin:ses:0.33.1-beta")
+    implementation("aws.sdk.kotlin:rdsdata:1.0.0")
+    implementation("aws.sdk.kotlin:ses:1.0.0")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
