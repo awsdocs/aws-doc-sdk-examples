d3e4f8027d19f405952f0a5507d83f9bbf3e677e
scmacdon <scmacdon@amazon.com>
2023-11-15 08:47:53 -0500

    added release build to build file

--- kotlin/services/cloudwatch/build.gradle.kts
+++ kotlin/services/cloudwatch/build.gradle.kts
@@ -8,6 +8,11 @@ plugins {
 group = "me.scmacdon"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 buildscript {
     repositories {
         maven("https://plugins.gradle.org/m2/")
@@ -22,10 +27,10 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:cloudwatch-jvm:0.33.1-beta")
-    implementation("aws.sdk.kotlin:cloudwatchevents-jvm:0.33.1-beta")
-    implementation("aws.sdk.kotlin:cloudwatchlogs-jvm:0.33.1-beta")
-    implementation("aws.sdk.kotlin:secretsmanager:0.33.1-beta")
+    implementation("aws.sdk.kotlin:cloudwatch:1.0.0")
+    implementation("aws.sdk.kotlin:cloudwatchevents:1.0.0")
+    implementation("aws.sdk.kotlin:cloudwatchlogs:1.0.0")
+    implementation("aws.sdk.kotlin:secretsmanager:1.0.0")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
     implementation("com.google.code.gson:gson:2.10")
