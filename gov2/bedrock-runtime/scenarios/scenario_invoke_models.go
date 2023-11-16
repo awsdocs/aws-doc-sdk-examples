903e19806e756760ce000d276b11d777dea2fd7f
Dennis Traub <traubd@amazon.com>
2023-11-16 12:42:55 +0100

    Add variant with reponse stream

--- gov2/bedrock-runtime/scenarios/scenario_invoke_models.go
+++ gov2/bedrock-runtime/scenarios/scenario_invoke_models.go
@@ -21,23 +21,25 @@ import (
 // 1. Generate text with Anthropic Claude 2
 // 2. Generate text with AI21 Labs Jurassic-2
 // 3. Generate text with Meta Llama 2 Chat
-// 4. Generate an image with Stability.ai Stable Diffusion
+// 4. Generate text and asynchronously process the response stream with Anthropic Claude 2
 type InvokeModelsScenario struct {
 	sdkConfig aws.Config
-	wrapper actions.InvokeModelWrapper
+	invokeModelWrapper actions.InvokeModelWrapper
+	responseStreamWrapper actions.InvokeModelWithResponseStreamWrapper
 	questioner demotools.IQuestioner
 	isTestRun bool
 }
 
 // NewInvokeModelsScenario constructs an InvokeModelsScenarion instance from a configuration.
-// It uses the specified config to get a Bedrock Runtime client and create a wrapper for the
+// It uses the specified config to get a Bedrock Runtime client and create wrappers for the
 // actions used in the scenario.
 func NewInvokeModelsScenario(sdkConfig aws.Config, questioner demotools.IQuestioner) InvokeModelsScenario {
 	client := bedrockruntime.NewFromConfig(sdkConfig)
 	return InvokeModelsScenario{
-		sdkConfig: 	sdkConfig,
-		wrapper: 	actions.InvokeModelWrapper{client},
-		questioner: questioner,
+		sdkConfig:		 	   sdkConfig,
+		invokeModelWrapper:    actions.InvokeModelWrapper{client},
+		responseStreamWrapper: actions.InvokeModelWithResponseStreamWrapper{client},
+		questioner:            questioner,
 	}
 }
 
@@ -54,7 +56,7 @@ func (scenario InvokeModelsScenario) Run() {
 	log.Println("Welcome to the Amazon Bedrock Runtime model invocation demo.")
 	log.Println(strings.Repeat("-", 88))
 
-	prompt := "In one sentence, who are you?"
+	prompt := "In one paragraph, who are you?"
 
 	log.Printf("First, let's invoke a few large-language models:\n\n")
 	log.Printf("Prompt: %v\n", prompt)
@@ -66,25 +68,34 @@ func (scenario InvokeModelsScenario) Run() {
 
 	scenario.InvokeLlama2(prompt)
 
+	scenario.InvokeWithResponseStream(prompt)
+
 	log.Println(strings.Repeat("-", 88))
 	log.Println("Thanks for watching!")
 	log.Println(strings.Repeat("-", 88))
 }
 
 func (scenario InvokeModelsScenario) InvokeClaude(prompt string) {
-	completion, err := scenario.wrapper.InvokeClaude(prompt)
+	completion, err := scenario.invokeModelWrapper.InvokeClaude(prompt)
 	if err != nil { panic(err) }
 	log.Printf("\nClaude     : %v\n", strings.TrimSpace(completion))
 }
 
 func (scenario InvokeModelsScenario) InvokeJurassic2(prompt string) {
-	completion, err := scenario.wrapper.InvokeJurassic2(prompt)
+	completion, err := scenario.invokeModelWrapper.InvokeJurassic2(prompt)
 	if err != nil { panic(err) }
 	log.Printf("\nJurassic-2 : %v\n", strings.TrimSpace(completion))
 }
 
 func (scenario InvokeModelsScenario) InvokeLlama2(prompt string) {
-	completion, err := scenario.wrapper.InvokeLlama2(prompt)
+	completion, err := scenario.invokeModelWrapper.InvokeLlama2(prompt)
 	if err != nil { panic(err) }
 	log.Printf("\nLlama 2    : %v\n\n", strings.TrimSpace(completion))
+}
+
+func (scenario InvokeModelsScenario) InvokeWithResponseStream(prompt string) {
+	log.Println("\nClaude with response stream:\n")
+	_, err := scenario.responseStreamWrapper.InvokeModelWithResponseStream(prompt)
+	if err != nil { panic(err) }
+	log.Println()
 }
