2bdff9e894b02305bebd670697ab2b07a6484dd6
Dennis Traub <traubd@amazon.com>
2023-11-17 17:46:22 +0100

    Add test for invokeClaude

---
+++ php/example_code/bedrock-runtime/tests/BedrockRuntimeTests.php
@@ -0,0 +1,42 @@
+<?php
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# Integration tests for the Amazon Bedrock Runtime service.
+#
+
+namespace bedrockruntime\tests;
+
+use BedrockRuntime\BedrockRuntimeService;
+use Aws\BedrockRuntime\BedrockRuntimeClient;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . "/../BedrockRuntimeService.php";
+
+/**
+ * @group integ
+ */
+
+ class BedrockRuntimeTests extends TestCase {
+
+    protected BedrockRuntimeService $bedrockRuntimeService;
+
+    public function setup(): void
+    {
+        $this->clientArgs = [
+            'region' => 'us-west-2',
+            'version' => 'latest',
+            'profile' => 'default',
+        ];
+        $this->bedrockRuntimeService = new BedrockRuntimeService($this->clientArgs);
+    }
+
+    public function test_foundation_models_can_be_listed()
+    {
+        $prompt = "A test prompt";
+        $completion = $this->bedrockRuntimeService->invokeClaude($prompt);
+        self::assertNotEmpty($completion);
+    }
+ }
