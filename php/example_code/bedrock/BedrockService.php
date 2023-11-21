2ca97937ff5381fbb1062fd11f940302335342d6
Dennis Traub <dennis.traub@gmail.com>
2023-11-21 20:02:47 +0100

    PHP: Add Example for Listing Amazon Bedrock Foundation Models (#5683)
    * Add getting started with bedrock

---
+++ php/example_code/bedrock/BedrockService.php
@@ -0,0 +1,40 @@
+<?php
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+#snippet-start:[php.example_code.bedrock.service]
+namespace Bedrock;
+
+use Aws\Bedrock\BedrockClient;
+
+class BedrockService extends \AwsUtilities\AWSServiceClass
+{
+    protected BedrockClient $bedrockClient;
+
+    public function __construct(
+        $client = null,
+        $region = 'us-east-1',
+        $version = 'latest',
+        $profile = 'default'
+    ) {
+        if (gettype($client) == BedrockClient::class) {
+            $this->bedrockClient = $client;
+            return;
+        }
+        $this->bedrockClient = new BedrockClient([
+            'region' => $region,
+            'version' => $version,
+            'profile' => $profile,
+        ]);
+    }
+
+    #snippet-start:[php.example_code.bedrock.service.listFoundationModels]
+    public function listFoundationModels()
+    {
+        $result = $this->bedrockClient->listFoundationModels();
+        return $result;
+    }
+    #snippet-end:[php.example_code.bedrock.service.listFoundationModels]
+}
+#snippet-end:[php.example_code.bedrock.service]
---
+++ php/example_code/bedrock/tests/BedrockBasicsTests.php
@@ -0,0 +1,37 @@
+<?php
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# Integration test runner for Amazon Bedrock files.
+#
+
+namespace bedrock\tests;
+
+use Bedrock\BedrockService;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group integ
+ */
+class BedrockBasicsTests extends TestCase
+{
+    protected BedrockService $bedrockService;
+
+    public function setup(): void
+    {
+        $this->clientArgs = [
+            'region' => 'us-west-2',
+            'version' => 'latest',
+            'profile' => 'default',
+        ];
+        $this->bedrockService = new BedrockService($this->clientArgs);
+    }
+
+    public function test_foundation_models_can_be_listed()
+    {
+        $result = $this->bedrockService->listFoundationModels();
+        self::assertNotEmpty($result['modelSummaries']);
+    }
+}
