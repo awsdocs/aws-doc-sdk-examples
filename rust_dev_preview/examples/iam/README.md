3fc682b1381d81d9026eeae6a1e8119d9efac020
David Souther <davidsouther+github@gmail.com>
2023-11-20 19:40:51 -0500

    Rust: Add fake symlinks for all rust_dev_preview files.

---
+++ rust_dev_preview/examples/iam/README.md
@@ -0,0 +1,1 @@
+This file has been moved to https://github.com/awsdocs/aws-doc-sdk-examples/blob/rust_dev_preview/rustv1/examples/iam/README.md
---
+++ rust_dev_preview/examples/iot/src/bin/describe-endpoint.rs
@@ -0,0 +1,1 @@
+// This file has been moved to https://github.com/awsdocs/aws-doc-sdk-examples/blob/rust_dev_preview/rustv1/examples/iot/src/bin/describe-endpoint.rs
