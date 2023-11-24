d143327e5236ff671272d63f32cb02fb08238995
Dennis Traub <dennis.traub@gmail.com>
2023-11-24 21:08:47 +0100

    Kotlin: Add Example for Listing Amazon Bedrock Foundation Models (#5705)
    * Add .gitignore

---
+++ kotlin/services/bedrock/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "com.example.bedrock"
+version = "1.0-SNAPSHOT"
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.3.1")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+dependencies {
+    implementation("aws.sdk.kotlin:bedrock:0.33.1-beta")
+    implementation("aws.sdk.kotlin:sts:0.33.1-beta")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "17"
+}
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test")
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
+}
