196bde4e935812177e3deada3fd66b1703170dd2
scmacdon <scmacdon@amazon.com>
2023-11-26 20:13:58 -0500

    update smithy version

--- kotlin/services/polly/build.gradle.kts
+++ kotlin/services/polly/build.gradle.kts
@@ -28,8 +28,8 @@ repositories {
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
     implementation("aws.sdk.kotlin:polly:1.0.0")
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.28.0")
-    implementation("aws.smithy.kotlin:http-client-engine-crt:0.28.0")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("com.googlecode.soundlibs:jlayer:1.0.1.4")
