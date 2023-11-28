0ccfdca11cbde3f5f1c3dca62340f1abebe8c9be
David Souther <davidsouther+github@gmail.com>
2023-11-28 11:07:55 -0500

    Add shellcheck exception

--- rustv1/run_all.sh
+++ rustv1/run_all.sh
@@ -27,6 +27,8 @@ CARGO="$HOME/.cargo/bin/cargo"
 
 FAIL=()
 for f in "${DIRS[@]}" ; do
+  # we _do_ want to break out the flags in $a
+  # shellcheck disable=SC2086
   for a in "${ACTIONS[@]}" ; do
     "$CARGO" $a --manifest-path "$f/Cargo.toml" || FAIL+=("${a}:$f")
   done
