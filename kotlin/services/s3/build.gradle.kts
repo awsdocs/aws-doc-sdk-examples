ee1bf5f3d2fd5f23c72fd589776b655bc837956c
scmacdon <scmacdon@amazon.com>
2023-11-30 11:19:30 -0500

    addressed a ticket

--- kotlin/services/s3/build.gradle.kts
+++ kotlin/services/s3/build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
     implementation("aws.sdk.kotlin:secretsmanager:1.0.0")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("aws.smithy.kotlin:aws-signing-crt:0.30.0")
     implementation("com.google.code.gson:gson:2.10")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
