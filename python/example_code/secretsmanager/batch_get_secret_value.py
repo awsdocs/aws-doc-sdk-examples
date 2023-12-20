218bbfb5f29f219b72330d3f81aa46dc91dd5768
ford-at-aws <fprior@amazon.com>
2023-12-20 15:44:08 -0500

    adding tests

--- python/example_code/secretsmanager/batch_get_secret_value.py
+++ python/example_code/secretsmanager/batch_get_secret_value.py
@@ -15,15 +15,12 @@ def batch_get_secrets(filter_name):
 
     try:
         response = client.batch_get_secret_value(
-            Filters=[{
-                "Key": "name",
-                "Values": [f"{filter_name}"]
-            }]
+            Filters=[{"Key": "name", "Values": [f"{filter_name}"]}]
         )
         for secret in response["SecretValues"]:
             print(json.loads(secret["SecretString"]))
     except client.exceptions.ResourceNotFoundException:
-        return f"One or more requested secrets were not found"
+        return f"One or more requested secrets were not foun" f""
     except Exception as e:
         return f"An unknown error occurred:\n{str(e)}"
 
