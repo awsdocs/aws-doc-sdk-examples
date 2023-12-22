3ea21796c777a235b3cde92d4d92ba21b91f315f
François Bouteruche <bouteru@amazon.fr>
2023-12-22 08:08:51 -0800

    .NET v3: C# bedrock runtime new actions and bedrock runtime outline refactoring - Amazon Bedrock (#5792)
    * Refactor bedrock runtime action code example outline and add C# code example for invoking Jurassic-2 and Llama 2
    
    * Remove  duplicate snippet start

--- dotnetv3/BedrockRuntime/Tests/TextToTextAsyncTest.cs
+++ dotnetv3/BedrockRuntime/Tests/TextToTextAsyncTest.cs
@@ -19,4 +19,32 @@ public class TextToTextAsyncTest
         Assert.NotNull(generatedText);
         Assert.NotEmpty(generatedText);
     }
+
+    /// <summary>
+    /// InvokeJurassic2Async result should not be null or empty
+    /// </summary>
+    [Fact]
+    [Order(2)]
+    [Trait("Category", "Integration")]
+    public async Task InvokeJurassic2Async_ShouldNotBeNullOrEmpty()
+    {
+        var prompt = "In one sentence, what is a large-language model?";
+        var generatedText = await InvokeModelAsync.InvokeJurassic2Async(prompt);
+        Assert.NotNull(generatedText);
+        Assert.NotEmpty(generatedText);
+    }
+
+    /// <summary>
+    /// InvokeLlama2Async result should not be null or empty
+    /// </summary>
+    [Fact]
+    [Order(3)]
+    [Trait("Category", "Integration")]
+    public async Task InvokeLlama2Async_ShouldNotBeNullOrEmpty()
+    {
+        var prompt = "In one sentence, what is a large-language model?";
+        var generatedText = await InvokeModelAsync.InvokeLlama2Async(prompt);
+        Assert.NotNull(generatedText);
+        Assert.NotEmpty(generatedText);
+    }
 }
