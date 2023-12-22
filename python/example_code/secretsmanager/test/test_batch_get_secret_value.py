dfd95370ffdd02952e56cd1e480a319dbbe98260
ford-at-aws <fprior@amazon.com>
2023-12-22 15:58:22 -0500

    black

--- python/example_code/secretsmanager/test/test_batch_get_secret_value.py
+++ python/example_code/secretsmanager/test/test_batch_get_secret_value.py
@@ -1,8 +1,10 @@
 import sys
 import os
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import batch_get_secret_value as batch
+
 
 def test_batch():
     results = batch.batch_get_secrets("foo")
--- python/example_code/secretsmanager/test/test_get_secret_value.py
+++ python/example_code/secretsmanager/test/test_get_secret_value.py
@@ -1,9 +1,11 @@
 import sys
 import os
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import get_secret_value as secret
 
+
 def test_batch():
     results = secret.get_secret("foo")
-    assert 'not found' in results
+    assert "not found" in results
