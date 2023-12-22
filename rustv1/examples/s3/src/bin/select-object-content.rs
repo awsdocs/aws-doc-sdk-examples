5d9f56590f7394b49ed1aec8a1f290fe264e9809
Russell Cohen <rcoh@amazon.com>
2023-12-22 12:29:14 -0500

    Further updates to select-object content

--- rustv1/examples/s3/src/bin/select-object-content.rs
+++ rustv1/examples/s3/src/bin/select-object-content.rs
@@ -5,13 +5,15 @@
 
 #![allow(clippy::result_large_err)]
 
+use anyhow::bail;
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::types::{
     CompressionType, CsvInput, ExpressionType, FileHeaderInfo, InputSerialization, JsonOutput,
     OutputSerialization, SelectObjectContentEventStream,
 };
-use aws_sdk_s3::{config::Region, meta::PKG_VERSION, Client, Error};
+use aws_sdk_s3::{config::Region, meta::PKG_VERSION, Client};
 use clap::Parser;
+use serde::de::IgnoredAny;
 use serde::Deserialize;
 
 #[derive(Debug, Parser)]
@@ -27,13 +29,13 @@ struct Opt {
     /// The object key to scan. This example expects the object to be an uncompressed CSV file with:
 
     /// Name,PhoneNumber,City,Occupation
-    /// Person1,(nnn) nnn-nnnn,City1,Occupation1
+    /// Person1,(nnn) nnn-nnnn,City1,Occupation1,Comment
     /// ...
-    /// PersonN,(nnn) nnn-nnnn,CityN,OccupationN
+    /// PersonN,(nnn) nnn-nnnn,CityN,OccupationN,Comment
     #[structopt(short, long)]
     object: String,
 
-    /// The name of the person to scan for.
+    /// The name of the person to scan for. This used as a prefix search
     #[structopt(short, long)]
     name: String,
 
@@ -49,14 +51,25 @@ struct Record {
     phone_number: String,
     city: String,
     occupation: String,
+    description: String,
+}
+
+fn is_valid_json(data: impl AsRef<str>) -> bool {
+    serde_json::from_str::<IgnoredAny>(data.as_ref()).is_ok()
 }
 
 // Get object content.
 // snippet-start:[s3.rust.select-object-content]
-async fn get_content(client: &Client, bucket: &str, object: &str, name: &str) -> Result<(), Error> {
-    let mut person: String = "SELECT * FROM s3object s WHERE s.\"Name\" = '".to_owned();
-    person.push_str(name);
-    person.push('\'');
+async fn get_content(
+    client: &Client,
+    bucket: &str,
+    object: &str,
+    name: &str,
+) -> Result<(), anyhow::Error> {
+    // To escape a single quote, use two single quotes.
+    let name = name.replace("'", "''");
+    let person: String = format!("SELECT * FROM s3object s where s.Name like '{name}%'");
+    tracing::info!(query = %person);
 
     let mut output = client
         .select_object_content()
@@ -83,17 +96,17 @@ async fn get_content(client: &Client, bucket: &str, object: &str, name: &str) ->
         .send()
         .await?;
     let mut processed_records: Vec<Record> = vec![];
+    let mut buf: String = String::new();
 
     while let Some(event) = output.payload.recv().await? {
         match event {
             SelectObjectContentEventStream::Records(records) => {
-                let records_str =
-                    String::from_utf8(records.payload.map(|p| p.into_inner()).unwrap_or_default())
-                        .expect("invalid payload—not utf8");
+                let records_str = records.payload().map(|p| p.as_ref()).unwrap_or_default();
+                let records_str = std::str::from_utf8(records_str).expect("invalid utf8 from s3");
                 for line in records_str.lines() {
-                    match serde_json::from_str(line) {
-                        Ok(record) => processed_records.push(record),
-                        Err(_e) => eprintln!("failed to deserialize record {}\n{:?}", line, _e),
+                    // It's possible for one record to be split onto multiple lines
+                    if let Some(record) = parse_line_buffered(&mut buf, line)? {
+                        processed_records.push(record);
                     }
                 }
             }
@@ -116,6 +129,22 @@ async fn get_content(client: &Client, bucket: &str, object: &str, name: &str) ->
 
     Ok(())
 }
+
+/// Parse a new line &str, potentially using content from the previous line
+fn parse_line_buffered(buf: &mut String, line: &str) -> Result<Option<Record>, anyhow::Error> {
+    if buf.is_empty() && is_valid_json(line) {
+        return Ok(Some(serde_json::from_str(line)?));
+    } else {
+        buf.push_str(line);
+        if is_valid_json(&buf) {
+            let result = serde_json::from_str(&buf);
+            buf.clear();
+            Ok(Some(result?))
+        } else {
+            Ok(None)
+        }
+    }
+}
 // snippet-end:[s3.rust.select-object-content]
 
 /// Uses an SQL expression to retrieve content from an object in a bucket.
@@ -127,7 +156,7 @@ async fn get_content(client: &Client, bucket: &str, object: &str, name: &str) ->
 ///   If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt::init();
 
     let Opt {
