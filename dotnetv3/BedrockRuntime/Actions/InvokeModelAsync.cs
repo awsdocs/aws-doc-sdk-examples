708c53c022a614787fe00acabf9b159be355094d
François Bouteruche <bouteru@amazon.fr>
2023-12-22 16:12:07 +0100

    Refactor bedrock runtime action code example outline and add C# code example for invoking Jurassic-2 and Llama 2

--- dotnetv3/BedrockRuntime/Actions/InvokeModelAsync.cs
+++ dotnetv3/BedrockRuntime/Actions/InvokeModelAsync.cs
@@ -12,6 +12,8 @@ namespace BedrockRuntimeActions
 {
     public static class InvokeModelAsync
     {
+        // snippet-start:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Claude]
+
         /// <summary>
         /// Asynchronously invokes the Anthropic Claude 2 model to run an inference based on the provided input.
         /// </summary>
@@ -20,7 +22,7 @@ namespace BedrockRuntimeActions
         /// <remarks>
         /// The different model providers have individual request and response formats.
         /// For the format, ranges, and default values for Anthropic Claude, refer to:
-        ///     https://docs.anthropic.com/claude/reference/complete_post
+        ///     https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-claude.html
         /// </remarks>
         public static async Task<string> InvokeClaudeAsync(string prompt)
         {
@@ -65,6 +67,120 @@ namespace BedrockRuntimeActions
             }
             return generatedText;
         }
+
+        // snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Claude]
+
+        // snippet-start:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Jurassic2]
+
+        /// <summary>
+        /// Asynchronously invokes the AI21 Labs Jurassic-2 model to run an inference based on the provided input.
+        /// </summary>
+        /// <param name="prompt">The prompt that you want Claude to complete.</param>
+        /// <returns>The inference response from the model</returns>
+        /// <remarks>
+        /// The different model providers have individual request and response formats.
+        /// For the format, ranges, and default values for AI21 Labs Jurassic-2, refer to:
+        ///     https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-jurassic2.html
+        /// </remarks>
+        public static async Task<string> InvokeJurassic2Async(string prompt)
+        {
+            string jurassic2ModelId = "ai21.j2-mid-v1";
+
+            AmazonBedrockRuntimeClient client = new(RegionEndpoint.USEast1);
+
+            string payload = new JsonObject()
+            {
+                { "prompt", prompt },
+                { "maxTokens", 200 },
+                { "temperature", 0.5 }
+            }.ToJsonString();
+
+            string generatedText = "";
+            try
+            {
+                InvokeModelResponse response = await client.InvokeModelAsync(new InvokeModelRequest()
+                {
+                    ModelId = jurassic2ModelId,
+                    Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload),
+                    ContentType = "application/json",
+                    Accept = "application/json"
+                });
+
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return JsonNode.ParseAsync(response.Body)
+                        .Result?["completions"]?
+                        .AsArray()[0]?["data"]?
+                        .AsObject()["text"]?.GetValue<string>() ?? "";
+                }
+                else
+                {
+                    Console.WriteLine("InvokeModelAsync failed with status code " + response.HttpStatusCode);
+                }
+            }
+            catch (AmazonBedrockRuntimeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return generatedText;
+        }
+
+        // snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Jurassic2]
+
+        // snippet-start:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Llama2]
+
+        /// <summary>
+        /// Asynchronously invokes the Meta Llama 2 Chat model to run an inference based on the provided input.
+        /// </summary>
+        /// <param name="prompt">The prompt that you want Llama 2 to complete.</param>
+        /// <returns>The inference response from the model</returns>
+        /// <remarks>
+        /// The different model providers have individual request and response formats.
+        /// For the format, ranges, and default values for Meta Llama 2 Chat, refer to:
+        ///     https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-meta.html
+        /// </remarks>
+        public static async Task<string> InvokeLlama2Async(string prompt)
+        {
+            string llama2ModelId = "meta.llama2-13b-chat-v1";
+
+            AmazonBedrockRuntimeClient client = new(RegionEndpoint.USEast1);
+
+            string payload = new JsonObject()
+            {
+                { "prompt", prompt },
+                { "max_gen_len", 512 },
+                { "temperature", 0.5 },
+                { "top_p", 0.9 }
+            }.ToJsonString();
+
+            string generatedText = "";
+            try
+            {
+                InvokeModelResponse response = await client.InvokeModelAsync(new InvokeModelRequest()
+                {
+                    ModelId = llama2ModelId,
+                    Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload),
+                    ContentType = "application/json",
+                    Accept = "application/json"
+                });
+
+                if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return JsonNode.ParseAsync(response.Body)
+                        .Result?["generation"]?.GetValue<string>() ?? "";
+                }
+                else
+                {
+                    Console.WriteLine("InvokeModelAsync failed with status code " + response.HttpStatusCode);
+                }
+            }
+            catch (AmazonBedrockRuntimeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return generatedText;
+        }
+
+        // snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Llama2]
     }
 }
-// snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Claude]
