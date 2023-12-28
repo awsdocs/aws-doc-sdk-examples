8fd9701c0b007fc3bd3357622cf72ca13635a4fc
David Souther <davidsouther+github@gmail.com>
2023-12-28 16:34:38 -0500

    Tools: README updates for enforced languages.

--- .tools/readmes/config.py
+++ .tools/readmes/config.py
@@ -32,13 +32,25 @@ language = {
         }
     },
     "Go": {
+        1: {
+            "base_folder": "go",
+            "service_folder": 'go/{{service["name"]}}',
+            "deprecated": "true",
+            "sdk_api_ref": 'https://pkg.go.dev/github.com/aws/aws-sdk-go/service/{{service["name"]}}',
+        },
         2: {
             "base_folder": "gov2",
             "service_folder": 'gov2/{{service["name"]}}',
             "sdk_api_ref": 'https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/{{service["name"]}}',
-        }
+        },
     },
     "Java": {
+        1: {
+            "base_folder": "java",
+            "service_folder": 'java/example_code/{{service["name"]}}',
+            "sdk_api_ref": 'https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/{{service["name"]}}/package-summary.html',
+            "deprecated": "true",
+        },
         2: {
             "base_folder": "javav2",
             "service_folder": 'javav2/example_code/{{service["name"]}}',
@@ -46,13 +58,14 @@ language = {
             "service_folder_overrides": {
                 "medical-imaging": "javav2/example_code/medicalimaging",
             },
-        }
+        },
     },
     "JavaScript": {
         2: {
             "base_folder": "javascript",
             "service_folder": 'javascript/example_code/{{service["name"]}}',
             "sdk_api_ref": 'https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/{{service["name"] | capitalize}}.html',
+            "deprecated": "true",
         },
         3: {
             "base_folder": "javascriptv3",
@@ -160,6 +173,13 @@ language = {
         }
     },
     "Bash": {
+        2: {
+            "base_folder": "aws-cli",
+            "service_folder": 'aws-cli/bash-linux/{{service["name"]}}',
+            "sdk_api_ref": 'https://awscli.amazonaws.com/v2/documentation/api/latest/reference/{{service["name"]}}/index.html',
+        }
+    },
+    "CLI": {
         2: {
             "base_folder": "aws-cli",
             "service_folder": 'aws-cli/bash-linux/{{service["name"]}}',
