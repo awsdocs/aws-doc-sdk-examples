f0fe6cc9229e339f5e1a125fb54bf535ffa88193
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2024-01-03 10:03:55 -0500

    Revert "JavaScript (v3): CloudWatch Logs Wkflw" (#5855)
    Revert "JavaScript (v3): CloudWatch Logs Wkflw (#5785)"
    
    This reverts commit 83583fc144f7ed9b7debc2bb59e215fec916b9d2.

--- .tools/validation/validator_config.py
+++ .tools/validation/validator_config.py
@@ -188,7 +188,6 @@ ALLOW_LIST = {
     "iotanalytics/latest/APIReference/Welcome",
     "mediaconnect/latest/APIReference/Welcome",
     "cryptography/latest/APIReference/Welcome",
-    "com/AmazonCloudWatch/latest/logs/Working",
 }
 
 
