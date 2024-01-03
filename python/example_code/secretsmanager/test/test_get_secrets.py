8a6c4f1027fa486c3e40debe06a725d75321944c
ford-at-aws <fprior@amazon.com>
2024-01-03 12:35:53 -0500

    Tests updated

--- python/example_code/secretsmanager/test/test_get_secrets.py
+++ python/example_code/secretsmanager/test/test_get_secrets.py
@@ -11,17 +11,29 @@ from batch_get_secret_value import BatchGetSecretsWrapper
 from get_secret_value import GetSecretWrapper
 
 
+def test_scenario():
+    """
+    This test assumes the stack mentioned in the source code README has been successfully deployed.
+    This stack includes 7 secrets, all of which have names beginning with "mySecret".
+    """
+    # Retrieve secret using secret name
+    client = boto3.client("secretsmanager")
+    wrapper = GetSecretWrapper(client)
+    secret = wrapper.get_secret("nonexistentSecret")
+    assert "The requested secret nonexistentSecret was not found" in secret
+    secret = wrapper.get_secret("mySecret1")
+    assert secret
+
+
 def test_batch_scenario():
+    """
+    This test assumes the stack mentioned in the source code README has been successfully deployed.
+    This stack includes 7 secrets, all of which have names beginning with "mySecret".
+    """
     # Retrieve the secrets using the filter
     client = boto3.client("secretsmanager")
     wrapper = BatchGetSecretsWrapper(client)
-    secrets = wrapper.batch_get_secrets("nonexistentSecret")
+    secrets = wrapper.batch_get_secrets("nonexistentSecrets")
     assert not secrets
-
-
-def test_scenario():
-    # Retrieve the secrets using the filter
-    client = boto3.client("secretsmanager")
-    wrapper = GetSecretWrapper(client)
-    secrets = wrapper.get_secret("nonexistentSecret")
-    assert not secrets
+    secrets = wrapper.batch_get_secrets("mySecret")
+    assert secrets
