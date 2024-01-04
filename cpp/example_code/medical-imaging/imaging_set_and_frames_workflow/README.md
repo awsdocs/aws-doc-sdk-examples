5e37484681e1ad279c240bdeccd1cf84834e9fec
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2024-01-04 15:27:56 -0500

    HealthImaging workflow

---
+++ cpp/example_code/medical-imaging/imaging_set_and_frames_workflow/README.md
@@ -0,0 +1,106 @@
+# Import HealthImaging Image Sets and Download Image Frames using the SDK for C++
+
+## Overview
+
+This workflow shows how to use the SDK for C++ to import DICOM files into
+an AWS HealthImaging data store. It then shows how to download, decode and verify the image
+frames created by the DICOM import.
+
+Digital Imaging and Communications in Medicine (DICOM) is a technical standard for the digital storage and transmission of medical images and related information.
+
+This workflow runs as a command-line application prompting for user input.
+
+1. All the necessary resources are created from an AWS CloudFormation template.
+   1. A HealthImaging data store.
+   2. An Amazon Simple Storage Service (Amazon S3) input bucket for a DICOM import job.
+   3. An Amazon S3 output bucket for a DICOM import job.
+   4. An AWS Identity and Access Management (IAM) role with the appropriate permissions for a DICOM import job.
+
+![CloudFormation stack diagram](../../../../workflows/healthimaging_image_sets/.images/cfn_stack.png)
+
+2. The user chooses a DICOM study to copy from the [National Cancer Institute Imaging Data Commons (IDC) Collections](https://registry.opendata.aws/nci-imaging-data-commons/)' public S3 bucket.
+3. The chosen study is copied to the user's input S3 bucket.
+
+![DICOM copy diagram](../../../../workflows/healthimaging_image_sets/.images/copy_dicom.png)
+
+4. A HealthImaging DICOM import job is run.
+
+![DICOM import diagram](../../../../workflows/healthimaging_image_sets/.images/dicom_import.png)
+
+5. The workflow retrieves the IDs for the HealthImaging image frames created by the DICOM import job.
+
+![Image frame ID retrieval diagram](../../../../workflows/healthimaging_image_sets/.images/get_image_frame_ids.png)
+
+6. The HealthImaging image frames are downloaded, decoded to a bitmap format, and verified using a CRC32 checksum.
+7. The created resources can then be deleted, if the user chooses.
+
+## ⚠ Important
+
+* Running this code might result in charges to your AWS account.
+* Running the tests might result in charges to your AWS account.
+* We recommend that you grant your code least privilege. At most, grant only the minimum permissions required to perform the task. For more information, see [Grant least privilege](https://docs.aws.amazon.com/IAM/latest/UserGuide/best-practices.html#grant-least-privilege).
+* This code is not tested in every AWS Region. For more information, see [AWS Regional Services](https://aws.amazon.com/about-aws/global-infrastructure/regional-product-services).
+
+## Scenario
+
+### Prerequisites
+
+#### Build system requirements.
+
+* CMake - A C++ cross-platform build system.
+* AWS SDK for C++. 
+* vcpkg - An optional C/C++ dependency manager.
+
+For CMake installation instructions, go to the following link [Get the Software](https://cmake.org/download/).
+
+For AWS SDK for C++ installation instructions, go to the following link [Get started with the AWS SDK for C++](https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/getting-started.html).
+
+*Note: you can speed up the SDK build by only building the libraries needed by this scenario. To do this, pass the following argument to CMake `-DBUILD_ONLY="cloudformation;s3;medical-imaging;sts"`*
+
+vcpkg is the optional but recommended tool for installing the dependent libraries. The library dependencies can also be built and installed from the source code, or they can be installed using platform-specific package management software, such as `apt` for Linux.
+
+For vpckg installation instructions, go to the following link [Get started with vcpkg](https://vcpkg.io/en/getting-started).
+
+The following instructions assume vcpkg is installed.
+
+
+#### Library dependencies.
+
+* OpenJPEG - For decoding HTJ2K-encoded HealthImaging image frames.
+* jsoncons - For JMESPath queries of JSON data.
+* gzip-hpp - For decompressing downloaded HealthImaging image set metadata.
+* boost-crc - For verifying decoded HealthImaging image frames.
+
+CMake will install the library dependencies using the included [vckpg.json](vcpkg.json) file and vcpkg.
+
+
+### Build and Run the Scenario
+
+These instructions build and run the executable as a command-line application. 
+
+*Note: Many Integrated Development Environments (IDEs), such as Visual Studio and CLion, support CMake and can also be used to build and run the scenario.*
+
+Open a terminal in the directory `cpp/example_code/medical-imaging/imaging_set_and_frames_workflow`.
+
+Execute the following commands. You will need the path where vcpkg is installed.
+
+```shell
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=[path to vcpkg]/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
+cmake --build . --config=Debug
+./run_medical_image_sets_and_frames_workflow
+```
+
+
+## Additional resources
+
+* [HealthImaging User Guide](https://docs.aws.amazon.com/healthimaging/latest/devguide/what-is.html)
+* [HealthImaging API Reference](https://docs.aws.amazon.com/healthimaging/latest/APIReference/Welcome.html)
+* [SDK for C++ HealthImaging reference](https://sdk.amazonaws.com/cpp/api/LATEST/aws-cpp-sdk-medical-imaging/html/annotated.html)
+
+---
+
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
---
+++ cpp/example_code/medical-imaging/search_image_sets.cpp
@@ -0,0 +1,187 @@
+/*
+   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0
+*/
+
+#include <iostream>
+#include <aws/core/Aws.h>
+#include <aws/core/utils/DateTime.h>
+#include <aws/medical-imaging/MedicalImagingClient.h>
+#include <aws/medical-imaging/model/SearchImageSetsRequest.h>
+#include "medical-imaging_samples.h"
+
+/**
+ * Before running this C++ code example, set up your development environment, including your credentials.
+ *
+ * For more information, see the following documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/getting-started.html
+ *
+ * For information on the structure of the code examples and how to build and run the examples, see
+ * https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/getting-started-code-examples.html.
+ *
+ **/
+
+// snippet-start:[cpp.example_code.medical_imaging.SearchImageSets]
+//! Routine which searches for image sets based on defined input attributes.
+/*!
+  \param dataStoreID: The HealthImaging data store ID.
+  \param searchCriteria: A search criteria instance.
+  \param imageSetResults: Vector to receive the image set IDs.
+  \param clientConfig: Aws client configuration.
+  \return bool: Function succeeded.
+  */
+bool AwsDoc::Medical_Imaging::searchImageSets(
+        const Aws::String &dataStoreID,
+        const Aws::MedicalImaging::Model::SearchCriteria &searchCriteria,
+        Aws::Vector<Aws::String> &imageSetResults,
+        const Aws::Client::ClientConfiguration &clientConfig) {
+    Aws::MedicalImaging::MedicalImagingClient client(clientConfig);
+    Aws::MedicalImaging::Model::SearchImageSetsRequest request;
+    request.SetDatastoreId(dataStoreID);
+    request.SetSearchCriteria(searchCriteria);
+
+    Aws::String nextToken;
+    bool result = true;
+    do {
+        if (!nextToken.empty()) {
+            request.SetNextToken(nextToken);
+        }
+
+        Aws::MedicalImaging::Model::SearchImageSetsOutcome outcome = client.SearchImageSets(
+                request);
+        if (outcome.IsSuccess()) {
+            for (auto &imageSetMetadataSummary: outcome.GetResult().GetImageSetsMetadataSummaries()) {
+                imageSetResults.push_back(imageSetMetadataSummary.GetImageSetId());
+            }
+
+            nextToken = outcome.GetResult().GetNextToken();
+        }
+        else {
+            std::cout << "Error: " << outcome.GetError().GetMessage() << std::endl;
+            result = false;
+        }
+    } while (!nextToken.empty());
+
+    return result;
+}
+// snippet-end:[cpp.example_code.medical_imaging.SearchImageSets]
+
+
+/*
+ *
+ * main function
+ *
+ *  Usage: 'run_search_image_sets <datastore_id> <patient_id>'
+ *
+ *  Prerequisites: A HealthImaging data store containing image sets to search.
+ *
+*/
+
+#ifndef TESTING_BUILD
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout
+                << "Usage: 'run_search_image_sets <datastore_id> <patient_id>'"
+                << std::endl;
+        return 1;
+    }
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+    {
+        Aws::String dataStoreID = argv[1];
+        Aws::String patientID = argv[2];
+
+        Aws::Client::ClientConfiguration clientConfig;
+        // Optional: Set to the AWS Region in which the bucket was created (overrides config file).
+        // clientConfig.region = "us-east-1";
+
+        // Use case #1: EQUAL operator.
+        // snippet-start:[cpp.example_code.medical_imaging.SearchImageSets.use_case1]
+        Aws::Vector<Aws::String> imageIDsForPatientID;
+        Aws::MedicalImaging::Model::SearchCriteria searchCriteriaEqualsPatientID;
+        Aws::Vector<Aws::MedicalImaging::Model::SearchFilter> patientIDSearchFilters = {
+                Aws::MedicalImaging::Model::SearchFilter().WithOperator(Aws::MedicalImaging::Model::Operator::EQUAL)
+                .WithValues({Aws::MedicalImaging::Model::SearchByAttributeValue().WithDICOMPatientId(patientID)})
+        };
+        searchCriteriaEqualsPatientID.SetFilters(patientIDSearchFilters);
+        bool result = AwsDoc::Medical_Imaging::searchImageSets(dataStoreID, searchCriteriaEqualsPatientID, imageIDsForPatientID,
+                                                               clientConfig);
+        if (result) {
+            std::cout << imageIDsForPatientID.size() << " image sets found for the patient with ID '"
+            <<  patientID << "'." << std::endl;
+            for (auto &imageSetResult : imageIDsForPatientID) {
+                std::cout << "  Image set with ID '" << imageSetResult << std::endl;
+            }
+        }
+
+       //snippet-end:[cpp.example_code.medical_imaging.SearchImageSets.use_case1]
+
+       // Use case #2: BETWEEN operator using DICOMStudyDate and DICOMStudyTime.
+       // snippet-start:[cpp.example_code.medical_imaging.SearchImageSets.use_case2]
+         Aws::MedicalImaging::Model::SearchByAttributeValue useCase2StartDate;
+        useCase2StartDate.SetDICOMStudyDateAndTime(Aws::MedicalImaging::Model::DICOMStudyDateAndTime()
+        .WithDICOMStudyDate("19990101")
+        .WithDICOMStudyTime("000000.000"));
+
+        Aws::MedicalImaging::Model::SearchByAttributeValue useCase2EndDate;
+        useCase2EndDate.SetDICOMStudyDateAndTime(Aws::MedicalImaging::Model::DICOMStudyDateAndTime()
+        .WithDICOMStudyDate(Aws::Utils::DateTime(std::chrono::system_clock::now()).ToLocalTimeString("%Y%m%d"))
+        .WithDICOMStudyTime("000000.000"));
+
+        Aws::MedicalImaging::Model::SearchFilter useCase2SearchFilter;
+        useCase2SearchFilter.SetValues({useCase2StartDate, useCase2EndDate});
+        useCase2SearchFilter.SetOperator(Aws::MedicalImaging::Model::Operator::BETWEEN);
+
+        Aws::MedicalImaging::Model::SearchCriteria useCase2SearchCriteria;
+        useCase2SearchCriteria.SetFilters({useCase2SearchFilter});
+
+        Aws::Vector<Aws::String> usesCase2Results;
+        result = AwsDoc::Medical_Imaging::searchImageSets(dataStoreID, useCase2SearchCriteria, usesCase2Results,
+                                                               clientConfig);
+        if (result) {
+            std::cout << usesCase2Results.size() << " image sets found for between 1999/01/01 and present."
+                      <<  std::endl;
+            for (auto &imageSetResult : usesCase2Results) {
+                std::cout << "  Image set with ID '" << imageSetResult << std::endl;
+            }
+        }
+        //snippet-end:[cpp.example_code.medical_imaging.SearchImageSets.use_case2]
+
+        // Use case #3: BETWEEN operator using createdAt. Time studies were previously persisted.
+
+        //snippet-start:[cpp.example_code.medical_imaging.SearchImageSets.use_case3]
+        Aws::MedicalImaging::Model::SearchByAttributeValue useCase3StartDate;
+        useCase3StartDate.SetCreatedAt(Aws::Utils::DateTime("20231130T000000000Z",Aws::Utils::DateFormat::ISO_8601_BASIC));
+
+        Aws::MedicalImaging::Model::SearchByAttributeValue useCase3EndDate;
+        useCase3EndDate.SetCreatedAt(Aws::Utils::DateTime(std::chrono::system_clock::now()));
+
+
+        Aws::MedicalImaging::Model::SearchFilter useCase3SearchFilter;
+        useCase3SearchFilter.SetValues({useCase3StartDate, useCase3EndDate});
+        useCase3SearchFilter.SetOperator(Aws::MedicalImaging::Model::Operator::BETWEEN);
+
+        Aws::MedicalImaging::Model::SearchCriteria useCase3SearchCriteria;
+        useCase3SearchCriteria.SetFilters({useCase3SearchFilter});
+
+        Aws::Vector<Aws::String> usesCase3Results;
+        result = AwsDoc::Medical_Imaging::searchImageSets(dataStoreID, useCase3SearchCriteria, usesCase3Results,
+                                                          clientConfig);
+        if (result) {
+            std::cout << usesCase3Results.size() << " image sets found for created between 2023/12/01 and present."
+                      <<  std::endl;
+            for (auto &imageSetResult : usesCase3Results) {
+                std::cout << "  Image set with ID '" << imageSetResult << std::endl;
+            }
+        }
+        //snippet-end:[cpp.example_code.medical_imaging.SearchImageSets.use_case3]
+
+    }
+    Aws::ShutdownAPI(options);
+
+    return 0;
+}
+
+#endif // TESTING_BUILD
