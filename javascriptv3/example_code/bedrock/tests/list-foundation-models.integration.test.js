af4c42fac0b08425ab26ac12671b29d241c8dba5
Dennis Traub <traubd@amazon.com>
2024-01-09 13:42:48 +0100

    Add integration test for ListFoundationModels

---
+++ javascriptv3/example_code/bedrock/tests/list-foundation-models.integration.test.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { z } from 'zod';
+import { describe, it, expect } from "vitest";
+
+import { listFoundationModels } from "../actions/list-foundation-models.js";
+
+const ExpectedSchema = z.object({
+    modelId: z.string(),
+    modelName: z.string()
+});
+
+describe("list-foundation-models", () => {
+    it("should return model summaries", async () => {
+        let modelSummaries = await listFoundationModels();
+        expect(modelSummaries.length).toBeGreaterThan(0);
+
+        const isModelSummary = ExpectedSchema.safeParse(modelSummaries[0]).success;
+        expect(isModelSummary).toBe(true);
+    });
+});
