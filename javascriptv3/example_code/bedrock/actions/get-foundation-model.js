7d40eda5ac691bad491d3726b5c1632f52b6a484
Dennis Traub <traubd@amazon.com>
2024-01-09 16:26:31 +0100

    Add GetFoundationModel

---
+++ javascriptv3/example_code/bedrock/actions/get-foundation-model.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from 'url';
+
+// snippet-start:[javascript.v3.bedrock.actions.GetFoundationModel]
+import { BedrockClient, GetFoundationModelCommand  } from '@aws-sdk/client-bedrock';
+
+/**
+ * Get details about an Amazon Bedrock foundation model.
+ *
+ * @return {FoundationModelDetails} - The list of available bedrock foundation models.
+ */
+export const getFoundationModel = async () => {
+    const client = new BedrockClient();
+
+    const command = new GetFoundationModelCommand({
+        modelIdentifier: 'amazon.titan-embed-text-v1'
+    });
+
+    const response = await client.send(command);
+
+    return response.modelDetails;
+};
+
+// snippet-end:[javascript.v3.bedrock.actions.GetFoundationModel]
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    const model = await getFoundationModel();
+    console.log(model);
+}
--- javascriptv3/example_code/bedrock/tests/list-foundation-models.integration.test.js
+++ javascriptv3/example_code/bedrock/tests/list-foundation-models.integration.test.js
@@ -4,17 +4,17 @@
  */
 
 import { z } from 'zod';
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest';
 
-import { listFoundationModels } from "../actions/list-foundation-models.js";
+import { listFoundationModels } from '../actions/list-foundation-models.js';
 
 const ExpectedSchema = z.object({
     modelId: z.string(),
     modelName: z.string()
 });
 
-describe("list-foundation-models", () => {
-    it("should return model summaries", async () => {
+describe('list-foundation-models', () => {
+    it('should return model summaries', async () => {
         let modelSummaries = await listFoundationModels();
         expect(modelSummaries.length).toBeGreaterThan(0);
 
