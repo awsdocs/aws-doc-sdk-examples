7486a1a092aa8e16a21698ef26f9d524fef62e55
Corey Pyle <corepyle@amazon.com>
2024-01-10 15:54:00 -0500

    JavaScript (v3): Task - Fix outdated cert; test improvements. (#5890)

--- javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw.integration.test.js
+++ javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw.integration.test.js
@@ -4,8 +4,13 @@ import { scenarios } from "../index.js";
 
 describe("workflow", () => {
   it("should run without error", async () => {
-    await scenarios.deploy.run({ confirmAll: true, verbose: true });
-    await scenarios.demo.run({ confirmAll: true, verbose: true });
-    await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    try {
+      await scenarios.deploy.run({ confirmAll: true, verbose: true });
+      await scenarios.demo.run({ confirmAll: true, verbose: true });
+      await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    } catch (err) {
+      await scenarios.destroy.run({ confirmAll: true, verbose: true });
+      throw err;
+    }
   });
 });
