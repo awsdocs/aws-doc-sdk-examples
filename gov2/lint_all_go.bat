3a7a48642a69b7b321bbe0c9235ea0c81f9b8283
Laren-AWS <57545972+Laren-AWS@users.noreply.github.com>
2024-01-11 14:08:38 -0800

    [Go] Updated linting scripts to run on all Go V2 folders (#5904)

--- gov2/lint_all_go.bat
+++ gov2/lint_all_go.bat
@@ -1,7 +1,6 @@
 Rem Run this batch script to lint Go files in all subfolders.
 
-Rem When all Go examples have been updated, change this allow list to (*).
-for /d %%a in (aurora demotools, dynamodb, iam, lambda, s3, testtools) do (
+for /d %%a in (*) do (
     cd %%a
     call golangci-lint run
     cd ..
