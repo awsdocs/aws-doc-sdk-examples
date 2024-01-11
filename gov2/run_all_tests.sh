4b03e1158ad12619626440cfc429b0fb1e88f51e
ford-at-aws <fprior@amazon.com>
2024-01-11 17:25:38 -0500

    fix

--- gov2/run_all_tests.sh
+++ gov2/run_all_tests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 function runCommand() {
-  if [ "$@" ] && [ "$@" == 'integration' ]
-  then
+  if [ "$1" ] && [ "$1" == 'integration' ]; then
     kind='integration'
   else
     kind='unit'
   fi
-  echo Running $kind tests...
-  for d in /gov2/*/ ; do /bin/bash -c "(cd '$d' && go test -tags=$@  -timeout=60m ./...)"; done
+  echo "Running $kind tests..."
+  for d in /gov2/*/ ; do
+    /bin/bash -c "(cd '$d' && go test -tags='$1' -timeout=60m ./...)"
+  done
 }
 
 runCommand "$1"
