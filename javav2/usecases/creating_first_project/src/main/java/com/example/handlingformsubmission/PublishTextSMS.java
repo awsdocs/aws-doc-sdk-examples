9bd216eea861a0f1942a9a1e1232f4956fcbc6de
scmacdon <scmacdon@amazon.com>
2024-01-12 09:49:21 -0500

    update Java first app

--- javav2/usecases/creating_first_project/src/main/java/com/example/handlingformsubmission/PublishTextSMS.java
+++ javav2/usecases/creating_first_project/src/main/java/com/example/handlingformsubmission/PublishTextSMS.java
@@ -21,8 +21,7 @@ public class PublishTextSMS {
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
         String message = "A new item with ID value "+ id +" was added to the DynamoDB table";
-        String phoneNumber="<Specify a valid mobile number>" ; //Replace with a mobile phone number
-
+        String phoneNumber="Replace with a mobile phone number" ;
         try {
             PublishRequest request = PublishRequest.builder()
                     .message(message)
