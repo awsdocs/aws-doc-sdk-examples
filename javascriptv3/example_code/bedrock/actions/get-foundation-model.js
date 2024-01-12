633e5f538ea2df703017b9cf61676de71744b986
Dennis Traub <dennis.traub@gmail.com>
2024-01-12 21:01:03 +0100

    JavaScript v3: Add Examples for Basic Amazon Bedrock Actions (#5894)

---
+++ javascriptv3/example_code/bedrock/actions/get-foundation-model.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from 'url';
+
+import { BedrockClient, GetFoundationModelCommand  } from '@aws-sdk/client-bedrock';
+
+/**
+ * Get details about an Amazon Bedrock foundation model.
+ *
+ * @return {FoundationModelDetails} - The list of available bedrock foundation models.
+ */
+export const getFoundationModel = async () => {
+    const client = new BedrockClient();
+
+    const command = new GetFoundationModelCommand({
+        modelIdentifier: 'amazon.titan-embed-text-v1'
+    });
+
+    const response = await client.send(command);
+
+    return response.modelDetails;
+};
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    const model = await getFoundationModel();
+    console.log(model);
+}
---
+++ javascriptv3/example_code/bedrock/actions/list-foundation-models.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { fileURLToPath } from 'url';
+
+import { BedrockClient, ListFoundationModelsCommand } from '@aws-sdk/client-bedrock';
+
+/**
+ * List the available Amazon Bedrock foundation models.
+ *
+ * @return {FoundationModelSummary[]} - The list of available bedrock foundation models.
+ */
+export const listFoundationModels = async () => {
+    const client = new BedrockClient();
+
+    const input = {
+        // byProvider: 'STRING_VALUE',
+        // byCustomizationType: 'FINE_TUNING' || 'CONTINUED_PRE_TRAINING',
+        // byOutputModality: 'TEXT' || 'IMAGE' || 'EMBEDDING',
+        // byInferenceType: 'ON_DEMAND' || 'PROVISIONED',
+    };
+
+    const command = new ListFoundationModelsCommand(input);
+
+    const response = await client.send(command);
+
+    return response.modelSummaries;
+}
+
+// Invoke main function if this file was run directly.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    const models = await listFoundationModels();
+    console.log(models);
+}
