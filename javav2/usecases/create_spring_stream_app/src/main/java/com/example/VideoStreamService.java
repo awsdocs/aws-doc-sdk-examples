a150f338ed1cee58b71abd395b27b72ac991acb1
scmacdon <scmacdon@amazon.com>
2024-01-17 10:08:26 -0500

    updated the video render example

--- javav2/usecases/create_spring_stream_app/src/main/java/com/example/VideoStreamService.java
+++ javav2/usecases/create_spring_stream_app/src/main/java/com/example/VideoStreamService.java
@@ -6,10 +6,12 @@
 package com.example;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -35,23 +37,26 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedOutputStream;
 import java.io.StringWriter;
+import java.time.Duration;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.springframework.http.HttpHeaders;
 
 @Service
 public class VideoStreamService {
 
-    public static final String CONTENT_TYPE = "Content-Type";
-    public static final String CONTENT_LENGTH = "Content-Length";
     public static final String VIDEO_CONTENT = "video/";
 
     private S3Client getClient() {
 
         return S3Client.builder()
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-                .region(Region.US_WEST_2)
-                .build();
+            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+            .region(Region.US_WEST_2)
+            .build();
     }
 
     // Places a new video into an Amazon S3 bucket.
@@ -62,10 +67,10 @@ public class VideoStreamService {
             String theTags = "name="+fileName+"&description="+description;
 
             PutObjectRequest putOb = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(fileName)
-                    .tagging(theTags)
-                    .build();
+                .bucket(bucketName)
+                .key(fileName)
+                .tagging(theTags)
+                .build();
 
             s3.putObject(putOb, RequestBody.fromBytes(bytes));
 
@@ -74,6 +79,7 @@ public class VideoStreamService {
             System.exit(1);
         }
     }
+
 
     // Returns a schema that describes all tags for all videos in the given bucket.
     public String getTags(String bucketName){
@@ -84,88 +90,96 @@ public class VideoStreamService {
                 .bucket(bucketName)
                 .build();
 
-          ListObjectsResponse res = s3.listObjects(listObjects);
-          List<S3Object> objects = res.contents();
-          List<String> keys = new ArrayList<>();
-          for (S3Object myValue: objects) {
-              String key = myValue.key(); // We need the key to get the tags.
-              GetObjectTaggingRequest getTaggingRequest = GetObjectTaggingRequest.builder()
-                  .key(key)
-                  .bucket(bucketName)
-                  .build();
-
-              GetObjectTaggingResponse tags = s3.getObjectTagging(getTaggingRequest);
-              List<Tag> tagSet= tags.tagSet();
-              for (Tag tag : tagSet) {
-                  keys.add(tag.value());
-              }
-          }
-
-          List<Tags> tagList = modList(keys);
-          return convertToString(toXml(tagList));
-
-    } catch (S3Exception e) {
-        System.err.println(e.awsErrorDetails().errorMessage());
-        System.exit(1);
-    }
-        return "";
-    }
-
-    // Return a List where each element is a Tags object.
-    private List<Tags> modList(List<String> myList){
-        // Get the elements from the collection.
-        int count = myList.size();
-        List<Tags> allTags = new ArrayList<>();
-        Tags myTag ;
-        ArrayList<String> keys = new ArrayList<>();
-        ArrayList<String> values = new ArrayList<>();
-
-        for ( int index=0; index < count; index++) {
-            if (index % 2 == 0)
-                keys.add(myList.get(index));
-            else
-                values.add(myList.get(index));
-           }
-
-           // Create a list where each element is a Tags object.
-           for (int r=0; r<keys.size(); r++){
-               myTag = new Tags();
-               myTag.setName(keys.get(r));
-               myTag.setDesc(values.get(r));
-               allTags.add(myTag);
-           }
-        return allTags;
-    }
-
-
-    // Reads a video from a bucket and returns a ResponseEntity.
-    public ResponseEntity<byte[]> getObjectBytes (String bucketName, String keyName) {
-        S3Client s3 = getClient();
-        try {
-            // create a GetObjectRequest instance.
-            GetObjectRequest objectRequest = GetObjectRequest
-                    .builder()
-                    .key(keyName)
+            ListObjectsResponse res = s3.listObjects(listObjects);
+            List<S3Object> objects = res.contents();
+            List<String> keys = new ArrayList<>();
+            for (S3Object myValue: objects) {
+                String key = myValue.key(); // We need the key to get the tags.
+                GetObjectTaggingRequest getTaggingRequest = GetObjectTaggingRequest.builder()
+                    .key(key)
                     .bucket(bucketName)
                     .build();
 
-            // get the byte[] from this AWS S3 object and return a ResponseEntity.
-            ResponseBytes<GetObjectResponse> objectBytes = s3.getObjectAsBytes(objectRequest);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .header(CONTENT_TYPE, VIDEO_CONTENT + "mp4")
-                    .header(CONTENT_LENGTH, String.valueOf(objectBytes.asByteArray().length))
-                    .body(objectBytes.asByteArray());
+                GetObjectTaggingResponse tags = s3.getObjectTagging(getTaggingRequest);
+                List<Tag> tagSet= tags.tagSet();
+                for (Tag tag : tagSet) {
+                    keys.add(tag.value());
+                }
+            }
+
+            List<Tags> tagList = modList(keys);
+            return convertToString(toXml(tagList));
 
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-        return null;
+        return "";
+    }
+
+    // Return a List where each element is a Tags object.
+    private List<Tags> modList(List<String> myList) {
+        int count = myList.size();
+        return IntStream.range(0, count / 2)
+            .mapToObj(index -> {
+                Tags myTag = new Tags();
+                myTag.setName(myList.get(index * 2));
+                myTag.setDesc(myList.get(index * 2 + 1));
+                return myTag;
+            })
+            .collect(Collectors.toList());
     }
 
 
+    // Reads a video from a bucket and returns a ResponseEntity.
+    public ResponseEntity<StreamingResponseBody> getObjectBytes(String bucketName, String keyName) {
+        S3Client s3 = getClient();
+        try {
+            // Create an S3 object request.
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(keyName)
+                .build();
+
+            // Get the S3 object stream.
+            ResponseInputStream<GetObjectResponse> objectStream = s3.getObject(objectRequest);
+
+            // Set content type and length headers.
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.valueOf(VIDEO_CONTENT + "mp4"));
+
+            // Set content length if available.
+            Long contentLength = objectStream.response().contentLength();
+            if (contentLength != null) {
+                headers.setContentLength(contentLength);
+            }
+
+            // Set disposition as inline to display content in the browser.
+            headers.setContentDispositionFormData("inline", keyName);
+
+            // Create a StreamingResponseBody to stream the content.
+            StreamingResponseBody responseBody = outputStream -> {
+                try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream)) {
+                    byte[] buffer = new byte[1024 * 1024];
+                    int bytesRead;
+                    while ((bytesRead = objectStream.read(buffer)) != -1) {
+                        bufferedOutputStream.write(buffer, 0, bytesRead);
+                    }
+                    bufferedOutputStream.flush();
+                } finally {
+                    objectStream.close();
+                }
+            };
+
+            return new ResponseEntity<>(responseBody, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            // Handle exceptions and return an appropriate ResponseEntity.
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     // Convert a LIST to XML data.
-     private Document toXml(List<Tags> itemList) {
+    private Document toXml(List<Tags> itemList) {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
@@ -178,7 +192,6 @@ public class VideoStreamService {
 
             // Iterate through the list.
             for (Tags myItem: itemList) {
-
                 Element item = doc.createElement( "Tag" );
                 root.appendChild( item );
 
@@ -203,13 +216,14 @@ public class VideoStreamService {
     private String convertToString(Document xml) {
         try {
             TransformerFactory transformerFactory = getSecureTransformerFactory();
+            transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             Transformer transformer = transformerFactory.newTransformer();
             StreamResult result = new StreamResult(new StringWriter());
             DOMSource source = new DOMSource(xml);
             transformer.transform(source, result);
             return result.getWriter().toString();
 
-        } catch(TransformerException ex) {
+        } catch (TransformerException ex) {
             ex.printStackTrace();
         }
         return null;
