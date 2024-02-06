5fa2f02adc0a31eab39c65ed2570f9b6cce39d90
Jason Q <81179619+beqqrry-aws@users.noreply.github.com>
2024-02-06 09:28:06 -0700

    Adds the RDS snippets to metadata, with some minor cleanups. (#6048)

--- php/example_code/rds/DescribeEvents.php
+++ php/example_code/rds/DescribeEvents.php
@@ -2,20 +2,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// snippet-start:[rds.php.describe_events.complete]
-// snippet-start:[rds.php.describe_events.import]
+// snippet-start:[php.example_code.rds.describeEvents.complete]
+// snippet-start:[php.example_code.rds.describeEvents.import]
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Aws\Exception\AwsException;
 
-// snippet-end:[rds.php.describe_events.import]
+// snippet-end:[php.example_code.rds.describeEvents.import]
 
-// snippet-start:[rds.php.describe_events.main]
-//Create a RDSClient
+// snippet-start:[php.example_code.rds.describeEvents.main]
+
 $rdsClient = new Aws\Rds\RdsClient([
-    'profile' => 'default',
-    'version' => '2014-10-31',
     'region' => 'us-east-2'
 ]);
 
@@ -28,10 +26,9 @@ try {
     ]);
     var_dump($result);
 } catch (AwsException $e) {
-    // output error message if fails
     echo $e->getMessage();
     echo "\n";
 }
 
-// snippet-end:[rds.php.describe_events.main]
-// snippet-end:[rds.php.describe_events.complete]
+// snippet-end:[php.example_code.rds.describeEvents.main]
+// snippet-end:[php.example_code.rds.describeEvents.complete]
