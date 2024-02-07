5d54f2192db916b1c437ba8d5f9e95f819929421
scmacdon <scmacdon@amazon.com>
2024-02-07 17:01:35 -0500

    add cognito files

--- kotlin/services/cognito/build.gradle.kts
+++ kotlin/services/cognito/build.gradle.kts
@@ -27,9 +27,9 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:cognitoidentityprovider:1.0.0")
-    implementation("aws.sdk.kotlin:cognitoidentity:1.0.0")
-    implementation("aws.sdk.kotlin:secretsmanager:1.0.0")
+    implementation("aws.sdk.kotlin:cognitoidentityprovider:1.0.30")
+    implementation("aws.sdk.kotlin:cognitoidentity:1.0.30")
+    implementation("aws.sdk.kotlin:secretsmanager:1.0.30")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
