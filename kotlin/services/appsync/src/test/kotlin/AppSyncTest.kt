52aefc3930fa0026acb3e3e0c29942802aabb37e
scmacdon <scmacdon@amazon.com>
2024-02-07 11:32:03 -0500

    fixed linter

--- kotlin/services/appsync/src/test/kotlin/AppSyncTest.kt
+++ kotlin/services/appsync/src/test/kotlin/AppSyncTest.kt
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.Assertions.assertTrue
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
 class AppSyncTest {
