d2430970c59b3c20a18dd09da37b0e45a4d8952a
ford-at-aws <fprior@amazon.com>
2024-02-09 05:22:59 -0500

    woops

--- ruby/example_code/rds/list_subnet_groups.rb
+++ ruby/example_code/rds/list_subnet_groups.rb
@@ -1,8 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# snippet-start:[
-# ]
+# snippet-start:[rds.ruby.listSubnetGroups]
 require "aws-sdk-rds"  # v2: require 'aws-sdk'
 
 # List all Amazon Relational Database Service (Amazon RDS) subnet groups.
