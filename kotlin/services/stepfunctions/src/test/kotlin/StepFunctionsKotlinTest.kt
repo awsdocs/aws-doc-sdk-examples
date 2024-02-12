5658158690c0eec4972e0d9853434e4702041a3b
scmacdon <scmacdon@amazon.com>
2024-02-12 15:58:26 -0500

    updated stf files

--- kotlin/services/stepfunctions/src/test/kotlin/StepFunctionsKotlinTest.kt
+++ kotlin/services/stepfunctions/src/test/kotlin/StepFunctionsKotlinTest.kt
@@ -1,9 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import aws.sdk.kotlin.runtime.auth.credentials.EnvironmentCredentialsProvider
+import aws.sdk.kotlin.services.secretsmanager.SecretsManagerClient
+import aws.sdk.kotlin.services.secretsmanager.model.GetSecretValueRequest
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.google.gson.Gson
 import com.kotlin.stepfunctions.DASHES
 import com.kotlin.stepfunctions.GetStream
 import com.kotlin.stepfunctions.createActivity
@@ -20,30 +24,35 @@ import com.kotlin.stepfunctions.listStatemachinesPagnator
 import com.kotlin.stepfunctions.sendTaskSuccess
 import com.kotlin.stepfunctions.startWorkflow
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
-import java.io.InputStream
-import java.util.Properties
-import java.util.Scanner
+import java.util.UUID
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
 class StepFunctionsKotlinTest {
-    private var jsonFile = ""
-    private var jsonFileSM = ""
-    private var roleARN = ""
-    private var stateMachineName = ""
     private var roleNameSC = ""
     private var activityNameSC = ""
     private var stateMachineNameSC = ""
+    private var jsonFile = ""
 
     @BeforeAll
-    fun setup() {
+    fun setup() = runBlocking {
+        // Get the values to run these tests from AWS Secrets Manager.
+        val gson = Gson()
+        val json: String = getSecretValues()
+        val values = gson.fromJson(json, SecretValues::class.java)
+        roleNameSC = values.roleNameSC.toString() + UUID.randomUUID()
+        activityNameSC = values.activityNameSC.toString() + UUID.randomUUID()
+        stateMachineNameSC = values.stateMachineNameSC.toString() + UUID.randomUUID()
+        jsonFile = values.machineFile.toString()
+        /*
         val input: InputStream = this.javaClass.getClassLoader().getResourceAsStream("config.properties") as InputStream
         val prop = Properties()
         prop.load(input)
@@ -54,16 +63,7 @@ class StepFunctionsKotlinTest {
         roleNameSC = prop.getProperty("roleNameSC")
         activityNameSC = prop.getProperty("activityNameSC")
         stateMachineNameSC = prop.getProperty("stateMachineNameSC")
-    }
-
-    @Test
-    @Order(1)
-    fun whenInitializingAWSService_thenNotNull() {
-        Assertions.assertTrue(!jsonFile.isEmpty())
-        Assertions.assertTrue(!jsonFileSM.isEmpty())
-        Assertions.assertTrue(!roleARN.isEmpty())
-        Assertions.assertTrue(!stateMachineName.isEmpty())
-        println("Test 1 passed")
+        */
     }
 
     @Test
@@ -106,7 +106,7 @@ class StepFunctionsKotlinTest {
 
         // Get JSON to use for the state machine and place the activityArn value into it.
         val stream = GetStream()
-        val jsonString = stream.getStream()
+        val jsonString = stream.getStream(jsonFile)
 
         // Modify the Resource node.
         val objectMapper = ObjectMapper()
@@ -129,7 +129,7 @@ class StepFunctionsKotlinTest {
         println("Describe the state machine.")
         describeStateMachine(stateMachineArn)
         println("What should ChatSFN call you?")
-        val userName = sc.nextLine()
+        val userName = "foo"
         println("Hello $userName")
         println(DASHES)
 
@@ -145,11 +145,8 @@ class StepFunctionsKotlinTest {
             myList = getActivityTask(activityArn)
             println("ChatSFN: " + myList[1])
             println("$userName please specify a value.")
-            val myAction = sc.nextLine()
-            if (myAction.compareTo("done") == 0) {
-                action = true
-            }
-            println("You have selected $myAction")
+            val myAction = "done"
+            action = true
             val taskJson = "{ \"action\" : \"$myAction\" }"
             println(taskJson)
             sendTaskSuccess(myList[0], taskJson)
@@ -176,4 +173,25 @@ class StepFunctionsKotlinTest {
         println(DASHES)
         println("Test 4 passed")
     }
+
+    private suspend fun getSecretValues(): String {
+        val secretName = "test/stepfunctions"
+        val valueRequest= GetSecretValueRequest {
+            secretId = secretName
+        }
+        SecretsManagerClient { region = "us-east-1"; credentialsProvider = EnvironmentCredentialsProvider() }.use { secretClient ->
+            val valueResponse = secretClient.getSecretValue(valueRequest)
+            return valueResponse.secretString.toString()
+        }
+    }
+
+    @Nested
+    @DisplayName("A class used to get test values from test/stepfunctions (an AWS Secrets Manager secret)")
+    internal class SecretValues {
+        val roleNameSC: String? = null
+        val activityNameSC: String? = null
+        val stateMachineNameSC: String? = null
+        val machineFile: String? = null
+    }
+
 }
