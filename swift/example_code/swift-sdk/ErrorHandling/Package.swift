430797a28184804fb0704d1b1344532b1cd55772
Eric Shepherd <ericsh@amazon.com>
2024-02-13 19:05:33 +0000

    Complete Swift error handling example

--- swift/example_code/swift-sdk/ErrorHandling/Package.swift
+++ swift/example_code/swift-sdk/ErrorHandling/Package.swift
@@ -1,15 +1,24 @@
 // swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
+//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import PackageDescription
 
 let package = Package(
     name: "ErrorHandling",
+    // Let Xcode know the minimum Apple platforms supported.
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v13)
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(
             url: "https://github.com/awslabs/aws-sdk-swift",
-            from: "0.20.0"
+            from: "0.34.0"
         ),
     ],
     targets: [
---
+++ swift/example_code/swift-sdk/ErrorHandling/Sources/entry.swift
@@ -0,0 +1,64 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// An example showing how to handle service and HTTP errors when using the AWS
+// SDK for Swift.
+import Foundation
+import AwsCommonRuntimeKit
+import ClientRuntime
+import AWSS3
+
+/// Main entry point.
+@main
+struct ErrorHandlingExample {
+    static func main() async {
+        await SDKLoggingSystem.initialize(logLevel: .error)
+
+        print("Calling ListBuckets using a Region name that doesn't exist. This")
+        print("should result in a DNS resolution error.")
+
+        // snippet-start:[errors.swift.service-error]
+        do {
+            let client = try S3Client(region: "un-real-1")
+
+            _ = try await client.listBuckets(input: ListBucketsInput())
+            print("Done")
+        } catch let error as CommonRunTimeError {
+            switch error {
+                case .crtError(let error):
+                    print("Common RunTime error: (code \(error.code)) (\(error.name)): \(error.message)")
+                    break
+                default:
+                    // This should never happen in current versions of the
+                    // SDK, but is here to future-proof this error handler.
+                    dump(error, name: "Unknown type of CommonRunTimeError")
+            }
+        } catch let error as CRTError {
+            print("CRT Error (code \(error.code)) (\(error.name)): \(error.message)")
+        } catch {
+            print("Some other error")
+        }
+        // snippet-end:[errors.swift.service-error]
+
+        print("\n\n")
+        print("Calling GetObject with bucket and key names that likely don't")
+        print("exist. If that's the case, this will result in an HTTP error 403")
+        print("(Forbidden).")
+
+        // snippet-start:[errors.swift.http-error]
+        do {
+            let client = try S3Client(region: "us-east-1")
+
+            _ = try await client.getObject(input: GetObjectInput(
+                bucket: "not-a-real-bucket",
+                key: "not-a-real-key"
+            ))
+            print("Found a matching bucket but shouldn't have!")
+        } catch let error as HTTPError {
+            print("HTTP error; status code: \(error.httpResponse.statusCode.rawValue)")
+        } catch {
+            dump(error)
+        }
+        // snippet-end:[errors.swift.http-error]
+    }
+}
