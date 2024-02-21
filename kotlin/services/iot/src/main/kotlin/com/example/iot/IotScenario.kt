8f24926aa6adc2fd41d3eefced2d4465df9a7583
scmacdon <scmacdon@amazon.com>
2024-02-21 09:53:59 -0500

    add kotlin AWS IoT files

---
+++ kotlin/services/iot/src/main/kotlin/com/example/iot/IotScenario.kt
@@ -0,0 +1,493 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.iot
+
+import aws.sdk.kotlin.services.iot.IotClient
+import aws.sdk.kotlin.services.iot.model.Action
+import aws.sdk.kotlin.services.iot.model.AttachThingPrincipalRequest
+import aws.sdk.kotlin.services.iot.model.AttributePayload
+import aws.sdk.kotlin.services.iot.model.CreateThingRequest
+import aws.sdk.kotlin.services.iot.model.CreateTopicRuleRequest
+import aws.sdk.kotlin.services.iot.model.DeleteCertificateRequest
+import aws.sdk.kotlin.services.iot.model.DeleteThingRequest
+import aws.sdk.kotlin.services.iot.model.DescribeEndpointRequest
+import aws.sdk.kotlin.services.iot.model.DescribeThingRequest
+import aws.sdk.kotlin.services.iot.model.DetachThingPrincipalRequest
+import aws.sdk.kotlin.services.iot.model.ListTopicRulesRequest
+import aws.sdk.kotlin.services.iot.model.SearchIndexRequest
+import aws.sdk.kotlin.services.iot.model.SnsAction
+import aws.sdk.kotlin.services.iot.model.TopicRulePayload
+import aws.sdk.kotlin.services.iot.model.UpdateThingRequest
+import aws.sdk.kotlin.services.iotdataplane.IotDataPlaneClient
+import aws.sdk.kotlin.services.iotdataplane.model.GetThingShadowRequest
+import aws.sdk.kotlin.services.iotdataplane.model.UpdateThingShadowRequest
+import aws.smithy.kotlin.runtime.content.ByteStream
+import aws.smithy.kotlin.runtime.content.toByteArray
+import java.util.Scanner
+import java.util.regex.Pattern
+
+val DASHES = String(CharArray(80)).replace("\u0000", "-")
+val TOPIC = "your-iot-topic"
+suspend fun main() {
+    val usage = """
+                Usage:
+                    <roleARN> <snsAction> 
+
+                Where:
+                    roleARN - The ARN of an IAM role that has permission to work with AWS IOT.
+                    snsAction  - An ARN of an SNS topic.
+                
+    """.trimIndent()
+
+    // if (args.length != 2) {
+    //    System.out.println(usage);
+    //    System.exit(1);
+    // }
+
+    var thingName: String
+    val roleARN = "arn:aws:iam::814548047983:role/AssumeRoleSNS"
+    val snsAction = "arn:aws:sns:us-east-1:814548047983:scott1111"
+    val scanner = Scanner(System.`in`)
+
+    println(DASHES)
+    println("Welcome to the AWS IoT example scenario.")
+    println(
+        """
+            This example program demonstrates various interactions with the AWS Internet of Things (IoT) Core service. The program guides you through a series of steps, 
+            including creating an IoT Thing, generating a device certificate, updating the Thing with attributes, and so on. 
+            It utilizes the AWS SDK for Kotlin and incorporates functionality for creating and managing IoT Things, certificates, rules, 
+            shadows, and performing searches. The program aims to showcase AWS IoT capabilities and provides a comprehensive example for 
+            developers working with AWS IoT in a Kotlin environment.
+        """.trimIndent()
+    )
+
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    println(DASHES)
+
+    println(DASHES)
+    println("1. Create an AWS IoT Thing.")
+    println(
+        """
+            An AWS IoT Thing represents a virtual entity in the AWS IoT service that can be associated with a physical device.
+        """.trimIndent()
+    )
+    // Prompt the user for input.
+    print("Enter Thing name: ")
+    thingName = scanner.nextLine()
+    createIoTThing(thingName)
+    describeThing(thingName)
+    println(DASHES)
+
+    println(DASHES)
+    println("2. Generate a device certificate.")
+    println(
+        """
+        A device certificate performs a role in securing the communication between devices (Things) and the AWS IoT platform.
+        """.trimIndent()
+    )
+
+    print("Do you want to create a certificate for $thingName? (y/n)")
+    val certAns = scanner.nextLine()
+    var certificateArn: String? = ""
+    if (certAns != null && certAns.trim { it <= ' ' }.equals("y", ignoreCase = true)) {
+        certificateArn = createCertificate()
+        println("Attach the certificate to the AWS IoT Thing.")
+        attachCertificateToThing(thingName, certificateArn)
+    } else {
+        println("A device certificate was not created.")
+    }
+    println(DASHES)
+
+    println(DASHES)
+    println("3. Update an AWS IoT Thing with Attributes.")
+    println(
+        """
+        IoT Thing attributes, represented as key-value pairs, offer a pivotal advantage in facilitating efficient data 
+        management and retrieval within the AWS IoT ecosystem. 
+        """.trimIndent()
+    )
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    updateThing(thingName)
+    println(DASHES)
+
+    println(DASHES)
+    println("4. Return a unique endpoint specific to the Amazon Web Services account.")
+    println(
+        """
+        An IoT Endpoint refers to a specific URL or Uniform Resource Locator that serves as the entry point for communication between IoT devices and the AWS IoT service.
+        """.trimIndent()
+    )
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    val endpointUrl = describeEndpoint()
+    println(DASHES)
+
+    println(DASHES)
+    println("5. List your AWS IoT certificates")
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    if (certificateArn!!.isNotEmpty()) {
+        listCertificates()
+    } else {
+        println("You did not create a certificates. Skipping this step.")
+    }
+    println(DASHES)
+
+    println(DASHES)
+    println("6. Create an IoT shadow that refers to a digital representation or virtual twin of a physical IoT device")
+    println(
+        """
+            A Thing Shadow refers to a feature that enables you to create a virtual representation, or "shadow," 
+            of a physical device or thing. The Thing Shadow allows you to synchronize and control the state of a device between 
+            the cloud and the device itself. and the AWS IoT service. For example, you can write and retrieve JSON data from a Thing Shadow. 
+           
+        """.trimIndent()
+    )
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    updateShawdowThing(thingName)
+    println(DASHES)
+
+    println(DASHES)
+    println("7. Write out the state information, in JSON format.")
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    getPayload(thingName)
+    println(DASHES)
+
+    println(DASHES)
+    println("8. Creates a rule")
+    println(
+        """
+        Creates a rule that is an administrator-level action. 
+        Any user who has permission to create rules will be able to access data processed by the rule.
+        """.trimIndent()
+    )
+    print("Enter Rule name: ")
+    val ruleName = scanner.nextLine()
+    createIoTRule(roleARN, ruleName, snsAction)
+    println(DASHES)
+
+    println(DASHES)
+    println("9. List your rules.")
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    listIoTRules()
+    println(DASHES)
+
+    println(DASHES)
+    println("10. Search things using the Thing name.")
+    print("Press Enter to continue...")
+    scanner.nextLine()
+    val queryString = "thingName:$thingName"
+    searchThings(queryString)
+    println(DASHES)
+
+    println(DASHES)
+    if (certificateArn.length > 0) {
+        print("Do you want to detach and delete the certificate for $thingName? (y/n)")
+        val delAns = scanner.nextLine()
+        if (delAns != null && delAns.trim { it <= ' ' }.equals("y", ignoreCase = true)) {
+            println("11. You selected to detach amd delete the certificate.")
+            print("Press Enter to continue...")
+            scanner.nextLine()
+            detachThingPrincipal(thingName, certificateArn)
+            deleteCertificate(certificateArn)
+        } else {
+            println("11. You selected not to delete the certificate.")
+        }
+    } else {
+        println("11. You did not create a certificate so there is nothing to delete.")
+    }
+    println(DASHES)
+
+    println(DASHES)
+    println("12. Delete the AWS IoT Thing.")
+    print("Do you want to delete the IoT Thing? (y/n)")
+    val delAns = scanner.nextLine()
+    if (delAns != null && delAns.trim { it <= ' ' }.equals("y", ignoreCase = true)) {
+        deleteIoTThing(thingName)
+    } else {
+        println("The IoT Thing was not deleted.")
+    }
+    println(DASHES)
+
+    println(DASHES)
+    println("The AWS IoT workflow has successfully completed.")
+    println(DASHES)
+}
+
+suspend fun deleteIoTThing(thingNameVal: String) {
+    val deleteThingRequest = DeleteThingRequest {
+        thingName = thingNameVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        // Delete Thing Response.
+        iotClient.deleteThing(deleteThingRequest)
+
+        // Print ARN of the created thing.
+        println("Deleted Thing $thingNameVal")
+    }
+}
+
+suspend fun deleteCertificate(certificateArn: String) {
+    val certificateProviderRequest = DeleteCertificateRequest {
+        certificateId = extractCertificateId(certificateArn)
+    }
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        iotClient.deleteCertificate(certificateProviderRequest)
+        println("$certificateArn was successfully deleted.")
+    }
+}
+
+private fun extractCertificateId(certificateArn: String): String? {
+    // Example ARN: arn:aws:iot:region:account-id:cert/certificate-id
+    val arnParts = certificateArn.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    val certificateIdPart = arnParts[arnParts.size - 1]
+    return certificateIdPart.substring(certificateIdPart.lastIndexOf("/") + 1)
+}
+
+suspend fun detachThingPrincipal(thingNameVal: String, certificateArn: String) {
+    val thingPrincipalRequest = DetachThingPrincipalRequest {
+        principal = certificateArn
+        thingName = thingNameVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        iotClient.detachThingPrincipal(thingPrincipalRequest)
+        println("$certificateArn was successfully removed from $thingNameVal")
+    }
+}
+
+suspend fun searchThings(queryStringVal: String?) {
+    val searchIndexRequest = SearchIndexRequest {
+        queryString = queryStringVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        // Perform the search and get the result.
+        val searchIndexResponse = iotClient.searchIndex(searchIndexRequest)
+
+        // Process the result.
+        if (searchIndexResponse.things?.isEmpty() == true) {
+            println("No things found.")
+        } else {
+            searchIndexResponse.things
+                ?.forEach { thing -> println("Thing id found using search is ${thing.thingId}") }
+        }
+    }
+}
+
+suspend fun listIoTRules() {
+    val listTopicRulesRequest = ListTopicRulesRequest {}
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val listTopicRulesResponse = iotClient.listTopicRules(listTopicRulesRequest)
+        println("List of IoT Rules:")
+        val ruleList = listTopicRulesResponse.rules
+        ruleList?.forEach { rule ->
+            println("Rule Name: ${rule.ruleName}")
+            println("Rule ARN: ${rule.ruleArn}")
+            println("--------------")
+        }
+    }
+}
+
+suspend fun createIoTRule(roleARNVal: String?, ruleNameVal: String?, action: String?) {
+    // Set the rule SQL statement.
+    val sqlVal = "SELECT * FROM '$TOPIC '"
+    val action1 = SnsAction {
+        targetArn = action
+        roleArn = roleARNVal
+    }
+
+    // Create the action.
+    val myAction = Action {
+        sns = action1
+    }
+
+    // Create the topic rule payload.
+    val topicRulePayloadVal = TopicRulePayload {
+        sql = sqlVal
+        actions = listOf(myAction)
+    }
+
+    // Create the topic rule request.
+    val topicRuleRequest = CreateTopicRuleRequest {
+        ruleName = ruleNameVal
+        topicRulePayload = topicRulePayloadVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        iotClient.createTopicRule(topicRuleRequest)
+        println("IoT Rule created successfully.")
+    }
+}
+
+suspend fun getPayload(thingNameVal: String?) {
+    val getThingShadowRequest = GetThingShadowRequest {
+        thingName = thingNameVal
+    }
+
+    IotDataPlaneClient { region = "us-east-1" }.use { iotPlaneClient ->
+        val getThingShadowResponse = iotPlaneClient.getThingShadow(getThingShadowRequest)
+        val payload = getThingShadowResponse.payload
+        val payloadString = payload?.let { java.lang.String(it, Charsets.UTF_8) }
+        println("Received Shadow Data: $payloadString")
+    }
+}
+
+suspend fun listCertificates() {
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val response = iotClient.listCertificates()
+        val certList = response.certificates
+        certList?.forEach { cert ->
+            println("Cert id: ${cert.certificateId}")
+            println("Cert Arn: ${cert.certificateArn}")
+        }
+    }
+}
+
+// snippet-end:[iot.java2.update.thing.main]
+suspend fun describeEndpoint(): String? {
+    val request = DescribeEndpointRequest {}
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val endpointResponse = iotClient.describeEndpoint(request)
+        // Get the endpoint URL.
+        val endpointUrl: String? = endpointResponse.endpointAddress
+        val exString: String = getValue(endpointUrl)
+        val fullEndpoint = "https://$exString-ats.iot.us-east-1.amazonaws.com"
+        println("Full Endpoint URL: $fullEndpoint")
+        return fullEndpoint
+    }
+}
+
+// snippet-end:[iot.java2.create.thing.main]
+private fun getValue(input: String?): String {
+    // Define a regular expression pattern for extracting the subdomain.
+    val pattern = Pattern.compile("^(.*?)\\.iot\\.us-east-1\\.amazonaws\\.com")
+
+    // Match the pattern against the input string.
+    val matcher = pattern.matcher(input)
+
+    // Check if a match is found.
+    if (matcher.find()) {
+        // Extract the subdomain from the first capturing group.
+        val subdomain = matcher.group(1)
+        println("Extracted subdomain: $subdomain")
+        return subdomain
+    } else {
+        println("No match found")
+    }
+    return ""
+}
+
+suspend fun updateThing(thingNameVal: String?) {
+    // Specify the new attribute values.
+    val newLocation = "Office"
+    val newFirmwareVersion = "v2.0"
+    val attMap: MutableMap<String, String> = HashMap()
+    attMap["location"] = newLocation
+    attMap["firmwareVersion"] = newFirmwareVersion
+
+    // Build the update request
+    val attributePayloadVal = AttributePayload {
+        attributes = attMap
+    }
+
+    val updateThingRequest = UpdateThingRequest {
+        thingName = thingNameVal
+        attributePayload = attributePayloadVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        // Update the IoT Thing attributes.
+        iotClient.updateThing(updateThingRequest)
+        println("$thingNameVal attributes updated successfully.")
+    }
+}
+
+suspend fun updateShawdowThing(thingNameVal: String?) {
+    // Create Thing Shadow State Document.
+    val stateDocument = "{\"state\":{\"reported\":{\"temperature\":25, \"humidity\":50}}}"
+    val byteStream: ByteStream = ByteStream.fromString(stateDocument)
+    val byteArray: ByteArray = byteStream.toByteArray()
+
+    // Update Thing Shadow Request.
+    val updateThingShadowRequest = UpdateThingShadowRequest {
+        thingName = thingNameVal
+        payload = byteArray
+    }
+
+    IotDataPlaneClient { region = "us-east-1" }.use { iotPlaneClient ->
+        // Update Thing Shadow.
+        iotPlaneClient.updateThingShadow(updateThingShadowRequest)
+        println("Thing Shadow updated successfully.")
+    }
+}
+
+suspend fun attachCertificateToThing(thingNameVal: String?, certificateArn: String?) {
+    val principalRequest = AttachThingPrincipalRequest {
+        thingName = thingNameVal
+        principal = certificateArn
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        iotClient.attachThingPrincipal(principalRequest)
+        println("Certificate attached to Thing successfully.")
+    }
+}
+
+suspend fun describeThing(thingNameVal: String) {
+    // Describe the Thing to get more information.
+    val thingRequest = DescribeThingRequest {
+        thingName = thingNameVal
+    }
+
+    // Print Thing details.
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val describeResponse = iotClient.describeThing(thingRequest)
+        println("Thing Details:")
+        println("Thing Name: ${describeResponse.thingName}")
+        println("Thing ARN:  ${describeResponse.thingArn}")
+    }
+}
+
+suspend fun createCertificate(): String? {
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        // Create keys and certificate.
+        val response = iotClient.createKeysAndCertificate()
+
+        // Extract key, certificate, and certificate ARN.
+        val privateKey = response.keyPair?.privateKey
+        val certificatePem = response.certificatePem
+        val certificateArn = response.certificateArn
+
+        // Print the details.
+        println("Private Key:")
+        println(privateKey)
+        println("\nCertificate:")
+        println(certificatePem)
+        println("\nCertificate ARN:")
+        println(certificateArn)
+        return certificateArn
+    }
+}
+
+// snippet-start:[iot.kotlin.create.thing.main]
+suspend fun createIoTThing(thingNameVal: String) {
+    val createThingRequest = CreateThingRequest {
+        thingName = thingNameVal
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val createThingResponse = iotClient.createThing(createThingRequest)
+        println("Created $thingNameVal}")
+    }
+}
+// snippet-end:[iot.kotlin.create.thing.main]
