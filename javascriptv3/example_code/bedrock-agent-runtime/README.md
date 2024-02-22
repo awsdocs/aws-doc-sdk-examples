3dd97af63dc5978d35c720ed2c7537af4eb4f16a
Dinuda Yaggahavita <63149008+Dinuda@users.noreply.github.com>
2024-02-23 01:39:36 +0530

    Javascript (v3): Bedrock Runtime Agent - Invoke Agent (#6129)

--- javascriptv3/example_code/bedrock-agent-runtime/README.md
+++ javascriptv3/example_code/bedrock-agent-runtime/README.md
@@ -1,13 +1,13 @@
-# Amazon Bedrock Runtime code examples for the SDK for JavaScript (v3)
+# Agents for Amazon Bedrock Runtime code examples for the SDK for JavaScript (v3)
 
 ## Overview
 
-Shows how to use the AWS SDK for JavaScript (v3) to work with Amazon Bedrock Runtime.
+Shows how to use the AWS SDK for JavaScript (v3) to work with Agents for Amazon Bedrock Runtime.
 
 <!--custom.overview.start-->
 <!--custom.overview.end-->
 
-_Amazon Bedrock Runtime is a fully managed service that makes it easy to use foundation models from third-party providers and Amazon._
+_Agents for Amazon Bedrock Runtime offers you the ability to run autonomous agents in your application._
 
 ## ⚠ Important
 
@@ -28,7 +28,7 @@ For prerequisites, see the [README](../../README.md#Prerequisites) in the `javas
 
 <!--custom.prerequisites.start-->
 
-> ⚠ You must request access to a model before you can use it. If you try to use the model (with the API or console) before you have requested access to it, you will receive an error message. For more information, see [Model access](https://docs.aws.amazon.com/bedrock/latest/userguide/model-access.html).
+> ⚠ You must create an agent before you can invoke it. For more information, see [Create an agent](https://docs.aws.amazon.com/bedrock/latest/userguide/agents-create.html). Additionally, you must grant access to the model you specify during agent creation. For more information, see [Model access](https://us-east-1.console.aws.amazon.com/bedrock/home?region=us-east-1#/modelaccess).
 
 <!--custom.prerequisites.end-->
 
@@ -36,10 +36,7 @@ For prerequisites, see the [README](../../README.md#Prerequisites) in the `javas
 
 Code excerpts that show you how to call individual service functions.
 
-- [Text generation with AI21 Labs Jurassic-2](actions/invoke-jurassic2.js) (`InvokeModel`)
-- [Text generation with Amazon Titan Text G1](actions/invoke-titan-text-express-v1.js) (`InvokeModel`)
-- [Text generation with Anthropic Claude 2](actions/invoke-claude.js) (`InvokeModel`)
-- [Text generation with Meta Llama 2 Chat](actions/invoke-llama2.js) (`InvokeModel`)
+- [Invoke an agent](actions/invoke-agent.js) (`InvokeAgent`)
 
 
 <!--custom.examples.start-->
@@ -84,9 +81,9 @@ in the `javascriptv3` folder.
 
 ## Additional resources
 
-- [Amazon Bedrock Runtime User Guide](https://docs.aws.amazon.com/bedrock/latest/userguide/what-is-bedrock.html)
-- [Amazon Bedrock Runtime API Reference](https://docs.aws.amazon.com/bedrock/latest/APIReference/welcome.html)
-- [SDK for JavaScript (v3) Amazon Bedrock Runtime reference](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/bedrock-runtime)
+- [Agents for Amazon Bedrock Runtime User Guide](https://docs.aws.amazon.com/bedrock/latest/userguide/agents.html)
+- [Agents for Amazon Bedrock Runtime API Reference](https://docs.aws.amazon.com/bedrock/latest/APIReference/API_Operations_Agents_for_Amazon_Bedrock_Runtime.html)
+- [SDK for JavaScript (v3) Agents for Amazon Bedrock Runtime reference](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/bedrock-agent-runtime)
 
 <!--custom.resources.start-->
 <!--custom.resources.end-->
--- javascriptv3/example_code/bedrock-runtime/actions/invoke-llama2.js
+++ javascriptv3/example_code/bedrock-runtime/actions/invoke-llama2.js
@@ -1,9 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 
-import {BedrockRuntimeClient, InvokeModelCommand} from "@aws-sdk/client-bedrock-runtime";
+import {
+  AccessDeniedException,
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from "@aws-sdk/client-bedrock-runtime";
 
 /**
  * @typedef {Object} ResponseBody
@@ -18,50 +22,55 @@ import {BedrockRuntimeClient, InvokeModelCommand} from "@aws-sdk/client-bedrock-
  * @returns {string} The inference response (generation) from the model.
  */
 export const invokeLlama2 = async (prompt) => {
-    const client = new BedrockRuntimeClient( { region: 'us-east-1' } );
+  const client = new BedrockRuntimeClient({ region: "us-east-1" });
 
-    const modelId = 'meta.llama2-13b-chat-v1';
+  const modelId = "meta.llama2-13b-chat-v1";
 
-    /* The different model providers have individual request and response formats.
-     * For the format, ranges, and default values for Meta Llama 2 Chat, refer to:
-     * https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-meta.html
-     */
-    const payload = {
-        prompt,
-        temperature: 0.5,
-        top_p: 0.9,
-        max_gen_len: 512,
-    };
+  /* The different model providers have individual request and response formats.
+   * For the format, ranges, and default values for Meta Llama 2 Chat, refer to:
+   * https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-meta.html
+   */
+  const payload = {
+    prompt,
+    temperature: 0.5,
+    top_p: 0.9,
+    max_gen_len: 512,
+  };
 
-    const command = new InvokeModelCommand({
-        body: JSON.stringify(payload),
-        contentType: 'application/json',
-        accept: 'application/json',
-        modelId,
-    });
+  const command = new InvokeModelCommand({
+    body: JSON.stringify(payload),
+    contentType: "application/json",
+    accept: "application/json",
+    modelId,
+  });
 
-    try {
-        const response = await client.send(command);
-        const decodedResponseBody = new TextDecoder().decode(response.body);
+  try {
+    const response = await client.send(command);
+    const decodedResponseBody = new TextDecoder().decode(response.body);
 
-        /** @type {ResponseBody} */
-        const responseBody = JSON.parse(decodedResponseBody);
+    /** @type {ResponseBody} */
+    const responseBody = JSON.parse(decodedResponseBody);
 
-        return responseBody.generation;
-
-    } catch (err) {
-        console.error(err);
+    return responseBody.generation;
+  } catch (err) {
+    if (err instanceof AccessDeniedException) {
+      console.error(
+        `Access denied. Ensure you have the correct permissions to invoke ${modelId}.`,
+      );
+    } else {
+      throw err;
     }
+  }
 };
 
 // Invoke the function if this file was run directly.
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-    const prompt = 'Complete the following: "Once upon a time..."';
-    console.log('\nModel: Meta Llama 2 Chat');
-    console.log(`Prompt: ${prompt}`);
+  const prompt = 'Complete the following: "Once upon a time..."';
+  console.log("\nModel: Meta Llama 2 Chat");
+  console.log(`Prompt: ${prompt}`);
 
-    const completion = await invokeLlama2(prompt);
-    console.log('Completion:');
-    console.log(completion);
-    console.log('\n');
+  const completion = await invokeLlama2(prompt);
+  console.log("Completion:");
+  console.log(completion);
+  console.log("\n");
 }
