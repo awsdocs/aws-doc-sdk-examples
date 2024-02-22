d9d4fb979d65bb3e8097dedc14e6ecc4a9015ad9
Corey Pyle <corepyle@amazon.com>
2024-02-22 17:03:44 -0500

    JavaScript (v3): Add package validation script.

---
+++ javascriptv3/github_scripts/validate_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+errors_found=false
+packages=$(npm query --prefix javascriptv3 .workspace | jq -r '.[].name')
+for package_name in $packages; do
+    if [[ "$package_name" != *"@aws-doc-sdk-examples/"* ]]; then
+        echo "Error: Prefix '@aws-doc-sdk-examples/' is missing in package '$package_name'"
+        errors_found=true
+    fi
+done
+
+if [ "$errors_found" = true ]; then
+    exit 1
+fi
