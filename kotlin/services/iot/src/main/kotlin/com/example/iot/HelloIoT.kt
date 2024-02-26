869b7ddbc7c8f66c7c45acd5b813429aff37003e
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2024-02-26 12:03:42 -0500

    Kotlin Add AWS IoT Core Kotlin code examples (#6151)

---
+++ kotlin/services/iot/src/main/kotlin/com/example/iot/HelloIoT.kt
@@ -0,0 +1,31 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.iot
+
+// snippet-start:[iot.kotlin.hello_iot.main]
+import aws.sdk.kotlin.services.iot.IotClient
+import aws.sdk.kotlin.services.iot.model.ListThingsRequest
+
+suspend fun main() {
+    println("A listing of your AWS IoT Things:")
+    listAllThings()
+}
+
+suspend fun listAllThings() {
+    val thingsRequest = ListThingsRequest {
+        maxResults = 10
+    }
+
+    IotClient { region = "us-east-1" }.use { iotClient ->
+        val response = iotClient.listThings(thingsRequest)
+        val thingList = response.things
+        if (thingList != null) {
+            for (attribute in thingList) {
+                println("Thing name ${attribute.thingName}")
+                println("Thing ARN: ${attribute.thingArn}")
+            }
+        }
+    }
+}
+// snippet-end:[iot.kotlin.hello_iot.main]
