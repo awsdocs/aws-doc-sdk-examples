f1a058beba26df19def62ac29d32f5ae34328a65
François Bouteruche <bouteru@amazon.fr>
2024-03-04 14:24:04 +0100

    Add a C# code example for Anthropic Claude 2 with a response stream

--- dotnetv3/BedrockRuntime/Actions/InvokeModelAsync.cs
+++ dotnetv3/BedrockRuntime/Actions/InvokeModelAsync.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+using System;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading.Channels;
 using Amazon;
 using Amazon.BedrockRuntime;
 using Amazon.BedrockRuntime.Model;
+using Amazon.Runtime.EventStreams;
 using Amazon.Util;
 
 namespace BedrockRuntimeActions
@@ -68,6 +73,94 @@ namespace BedrockRuntimeActions
         }
 
         // snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Claude]
+
+        // snippet-start:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.ClaudeWithResponseStream]
+
+        /// <summary>
+        /// Asynchronously invokes the Anthropic Claude 2 model to run an inference based on the provided input and process the response stream.
+        /// </summary>
+        /// <param name="prompt">The prompt that you want Claude to complete.</param>
+        /// <returns>The inference response from the model</returns>
+        /// <remarks>
+        /// The different model providers have individual request and response formats.
+        /// For the format, ranges, and default values for Anthropic Claude, refer to:
+        ///     https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-claude.html
+        /// </remarks>
+        public static async IAsyncEnumerable<string> InvokeClaudeWithResponseStreamAsync(string prompt, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            string claudeModelId = "anthropic.claude-v2";
+
+            // Claude requires you to enclose the prompt as follows:
+            string enclosedPrompt = "Human: " + prompt + "\n\nAssistant:";
+
+            AmazonBedrockRuntimeClient client = new(RegionEndpoint.USEast1);
+
+            string payload = new JsonObject()
+             {
+                 { "prompt", enclosedPrompt },
+                 { "max_tokens_to_sample", 200 },
+                 { "temperature", 0.5 },
+                 { "stop_sequences", new JsonArray("\n\nHuman:") }
+             }.ToJsonString();
+
+            InvokeModelWithResponseStreamResponse? response = null;
+
+            try
+            {
+                response = await client.InvokeModelWithResponseStreamAsync(new InvokeModelWithResponseStreamRequest()
+                {
+                    ModelId = claudeModelId,
+                    Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload),
+                    ContentType = "application/json",
+                    Accept = "application/json"
+                });
+            }
+            catch (AmazonBedrockRuntimeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (response is not null && response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                // create a buffer to write the event in to move from a push mode to a pull mode
+                Channel<string> buffer = Channel.CreateUnbounded<string>();
+                bool isStreaming = true;
+
+                response.Body.ChunkReceived += BodyOnChunkReceived;
+                response.Body.StartProcessing();
+
+                while ((!cancellationToken.IsCancellationRequested && isStreaming) || (!cancellationToken.IsCancellationRequested && buffer.Reader.Count > 0))
+                {
+                   // pull the completion from the buffer and add it to the IAsyncEnumerable collection
+                    yield return await buffer.Reader.ReadAsync(cancellationToken);
+                }
+                response.Body.ChunkReceived -= BodyOnChunkReceived;
+
+                yield break;
+
+                // handle the ChunkReceived events
+                async void BodyOnChunkReceived(object? sender, EventStreamEventReceivedArgs<PayloadPart> e)
+                {
+                    var streamResponse = JsonSerializer.Deserialize<JsonObject>(e.EventStreamEvent.Bytes) ?? throw new NullReferenceException($"Unable to deserialize {nameof(e.EventStreamEvent.Bytes)}");
+
+                    if (streamResponse["stop_reason"]?.GetValue<string?>() != null)
+                    {
+                        isStreaming = false;
+                    }
+
+                    // write the received completion chunk into the buffer
+                    await buffer.Writer.WriteAsync(streamResponse["completion"]?.GetValue<string>(), cancellationToken);
+                }
+            }
+            else if(response is not null)
+            {
+                Console.WriteLine("InvokeModelAsync failed with status code " + response.HttpStatusCode);
+            }
+
+            yield break;
+        }
+
+        // snippet-end:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.ClaudeWithResponseStream]
 
         // snippet-start:[BedrockRuntime.dotnetv3.BedrockRuntimeActions.InvokeModelAsync.Jurassic2]
 
--- dotnetv3/BedrockRuntime/Tests/TextToTextAsyncTest.cs
+++ dotnetv3/BedrockRuntime/Tests/TextToTextAsyncTest.cs
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+using System.Text;
 using BedrockRuntimeActions;
 
 namespace BedrockRuntimeTests;
@@ -59,6 +60,24 @@ public class TextToTextAsyncTest
         var prompt = "In one sentence, what is a large-language model?";
         var generatedText = await InvokeModelAsync.InvokeTitanTextG1Async(prompt);
         Assert.NotNull(generatedText);
+        Assert.NotEmpty(generatedText);
+    }
+
+    /// <summary>
+    /// InvokeClaudeWithResponseStreamAsync result should not be empty
+    /// </summary>
+    [Fact]
+    [Order(5)]
+    [Trait("Category", "Integration")]
+    public async Task InvokeClaudeWithResponseStreamAsync_ShouldNotBeEmpty()
+    {
+        var prompt = "In one sentence, what is a large-language model?";
+        var generatedTextBuilder = new StringBuilder();
+        await foreach (var completionChunk in InvokeModelAsync.InvokeClaudeWithResponseStreamAsync(prompt))
+        {
+            generatedTextBuilder.Append(completionChunk);
+        }
+        var generatedText = generatedTextBuilder.ToString();
         Assert.NotEmpty(generatedText);
     }
 }
