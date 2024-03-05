5f8f09dcc220eafc6407c0394e2dae04fa8d2449
Brian Murray <40031786+brmur@users.noreply.github.com>
2024-03-05 13:16:07 +0000

    creating branch for electron version (windows)

---
+++ .doc_gen/my-app/my-app-win32-x64/resources/app/src/libs/utils/util-node.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const getEnv = (key) => process.env[key];
+export const setEnv = (key, value) => {
+  process.env[key] = value;
+};
