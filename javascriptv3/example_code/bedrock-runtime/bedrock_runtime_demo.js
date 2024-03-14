1c02389789dc5a7fcd15066c999d04319c9f67f9
Dennis Traub <traubd@amazon.com>
2024-03-14 17:07:14 +0100

    Typedef

--- javascriptv3/example_code/bedrock-runtime/bedrock_runtime_demo.js
+++ javascriptv3/example_code/bedrock-runtime/bedrock_runtime_demo.js
@@ -1,37 +1,46 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {fileURLToPath} from "url";
-import {askForPrompt, selectModel} from "./tools/user_input.js";
-import {FoundationModels} from "./foundation_models.js";
+import { fileURLToPath } from "url";
+import { askForPrompt, selectModel } from "./tools/user_input.js";
+import { FoundationModels } from "./foundation_models.js";
 
-const invokeModel = async (prompt, model) => {
-    try {
-        const modelModule = await model.module();
-        const invoker = model.invoker(modelModule);
-        return await invoker(prompt, model.modelId);
-    } catch (err) {
-        console.error(`Error invoking model ${model.modelId}:`, err);
-        throw err;
-    }
-}
+/**
+ * @typedef {Object} ModelConfig
+ * @property {Function} module
+ * @property {Function} invoker
+ * @property {string} modelId
+ * @property {string} modelName
+ */
+
+const invokeModel = async (prompt, /** @type {ModelConfig} */ model) => {
+  try {
+    const modelModule = await model.module();
+    const invoker = model.invoker(modelModule);
+    return await invoker(prompt, model.modelId);
+  } catch (err) {
+    console.error(`Error invoking model ${model.modelId}:`, err);
+    throw err;
+  }
+};
 
 const runDemo = async () => {
-    console.log("Welcome to the Amazon Bedrock Runtime client demo!");
+  console.log("Welcome to the Amazon Bedrock Runtime client demo!");
 
-    const model = await selectModel(Object.values(FoundationModels));
-    const prompt = await askForPrompt();
+  /** @type {ModelConfig} */
+  const model = await selectModel(Object.values(FoundationModels));
+  const prompt = await askForPrompt();
 
-    console.log(`Invoking ${model.modelName} with prompt '${prompt}'`);
+  console.log(`Invoking ${model.modelName} with prompt '${prompt}'`);
 
-    const response = await invokeModel(prompt, model);
-    if (Array.isArray(response)) {
-        response.forEach((str) => console.log(str));
-    } else {
-        console.log(response);
-    }
-}
+  const response = await invokeModel(prompt, model);
+  if (Array.isArray(response)) {
+    response.forEach((str) => console.log(str));
+  } else {
+    console.log(response);
+  }
+};
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-    runDemo();
+  runDemo();
 }
