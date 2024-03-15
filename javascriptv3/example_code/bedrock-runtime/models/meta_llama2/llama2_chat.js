305970c8910d15b17890f1342931825d0a3b120e
Dennis Traub <traubd@amazon.com>
2024-03-15 13:08:07 +0100

    Add text playground to readme

--- javascriptv3/example_code/bedrock-runtime/models/meta_llama2/llama2_chat.js
+++ javascriptv3/example_code/bedrock-runtime/models/meta_llama2/llama2_chat.js
@@ -3,11 +3,11 @@
 
 import { fileURLToPath } from "url";
 
-import {FoundationModels} from "../../foundation_models.js";
-import {defaultProvider} from "@aws-sdk/credential-provider-node";
+import { FoundationModels } from "../../tools/foundation_models.js";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
 import {
-    BedrockRuntimeClient,
-    InvokeModelCommand,
+  BedrockRuntimeClient,
+  InvokeModelCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 
 /**
@@ -23,49 +23,50 @@ import {
  * @returns {Promise<string>} The inference response from the model.
  */
 export const invokeModel = async (prompt, modelId) => {
-    // Create a new Bedrock Runtime client instance.
-    const client = new BedrockRuntimeClient({
-        region: "us-east-1",
-        credentialDefaultProvider: defaultProvider,
-    });
+  // Create a new Bedrock Runtime client instance.
+  const client = new BedrockRuntimeClient({
+    region: "us-east-1",
+    credentialDefaultProvider: defaultProvider,
+  });
 
-    // Use the provided model ID or fallback to Llama2 Chat 13B if not provided.
-    modelId = modelId || "meta.llama2-13b-chat-v1";
+  // Use the provided model ID or fallback to Llama2 Chat 13B if not provided.
+  modelId = modelId || "meta.llama2-13b-chat-v1";
 
-    // Prepare the payload for the model.
-    const payload = {
-        prompt,
-        temperature: 0.5,
-        max_gen_len: 1000,
-    };
+  // Prepare the payload for the model.
+  const payload = {
+    prompt,
+    temperature: 0.5,
+    max_gen_len: 1000,
+  };
 
-    // Invoke the model with the payload and wait for the response.
-    const command = new InvokeModelCommand({
-        contentType: "application/json",
-        body: JSON.stringify(payload),
-        modelId,
-    });
-    const apiResponse = await client.send(command);
+  // Invoke the model with the payload and wait for the response.
+  const command = new InvokeModelCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId,
+  });
+  const apiResponse = await client.send(command);
 
-    // Decode and return the response.
-    const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
-    /** @type {ResponseBody} */
-    const responseBody = JSON.parse(decodedResponseBody);
-    return responseBody.generation;
+  // Decode and return the response.
+  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+  /** @type {ResponseBody} */
+  const responseBody = JSON.parse(decodedResponseBody);
+  return responseBody.generation;
 };
 
 // Invoke the function if this file was run directly.
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-    const prompt = 'Complete the following in one sentence: "Once upon a time..."';
-    const modelId = FoundationModels.LLAMA2_CHAT_13B.modelId;
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Model ID: ${modelId}`);
+  const prompt =
+    'Complete the following in one sentence: "Once upon a time..."';
+  const modelId = FoundationModels.LLAMA2_CHAT_13B.modelId;
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Model ID: ${modelId}`);
 
-    try {
-        console.log("-".repeat(53));
-        const response = await invokeModel(prompt, modelId);
-        console.log(response);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    console.log("-".repeat(53));
+    const response = await invokeModel(prompt, modelId);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+  }
 }
--- javascriptv3/example_code/bedrock-runtime/models/mistral_ai/mistral_7b.js
+++ javascriptv3/example_code/bedrock-runtime/models/mistral_ai/mistral_7b.js
@@ -3,11 +3,11 @@
 
 import { fileURLToPath } from "url";
 
-import {FoundationModels} from "../../foundation_models.js";
-import {defaultProvider} from "@aws-sdk/credential-provider-node";
+import { FoundationModels } from "../../tools/foundation_models.js";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
 import {
-    BedrockRuntimeClient,
-    InvokeModelCommand,
+  BedrockRuntimeClient,
+  InvokeModelCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 
 /**
@@ -26,53 +26,54 @@ import {
  * @returns {Promise<string[]>} The inference response from the model.
  */
 export const invokeModel = async (prompt, modelId) => {
-    // Create a new Bedrock Runtime client instance.
-    const client = new BedrockRuntimeClient({
-        region: "us-east-1",
-        credentialDefaultProvider: defaultProvider,
-    });
+  // Create a new Bedrock Runtime client instance.
+  const client = new BedrockRuntimeClient({
+    region: "us-east-1",
+    credentialDefaultProvider: defaultProvider,
+  });
 
-    // Use the provided model ID or fallback to Mistral 7B Instruct if not provided.
-    modelId = modelId || "mistral.mistral-7b-instruct-v0:2";
+  // Use the provided model ID or fallback to Mistral 7B Instruct if not provided.
+  modelId = modelId || "mistral.mistral-7b-instruct-v0:2";
 
-    // Mistral instruct models provide optimal results when embedding
-    // the prompt into the following template:
-    const instruction = `<s>[INST] ${prompt} [/INST]`;
+  // Mistral instruct models provide optimal results when embedding
+  // the prompt into the following template:
+  const instruction = `<s>[INST] ${prompt} [/INST]`;
 
-    // Prepare the payload.
-    const payload = {
-        prompt: instruction,
-        max_tokens: 500,
-        temperature: 0.5,
-    };
+  // Prepare the payload.
+  const payload = {
+    prompt: instruction,
+    max_tokens: 500,
+    temperature: 0.5,
+  };
 
-    // Invoke the model with the payload and wait for the response.
-    const command = new InvokeModelCommand({
-        contentType: "application/json",
-        body: JSON.stringify(payload),
-        modelId,
-    });
-    const apiResponse = await client.send(command);
+  // Invoke the model with the payload and wait for the response.
+  const command = new InvokeModelCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId,
+  });
+  const apiResponse = await client.send(command);
 
-    // Decode and return the response.
-    const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
-    /** @type {ResponseBody} */
-    const responseBody = JSON.parse(decodedResponseBody);
-    return responseBody.outputs.map(output => output.text);
+  // Decode and return the response.
+  const decodedResponseBody = new TextDecoder().decode(apiResponse.body);
+  /** @type {ResponseBody} */
+  const responseBody = JSON.parse(decodedResponseBody);
+  return responseBody.outputs.map((output) => output.text);
 };
 
 // Invoke the function if this file was run directly.
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-    const prompt = 'Complete the following in one sentence: "Once upon a time..."';
-    const modelId = FoundationModels.MISTRAL_7B.modelId;
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Model ID: ${modelId}`);
+  const prompt =
+    'Complete the following in one sentence: "Once upon a time..."';
+  const modelId = FoundationModels.MISTRAL_7B.modelId;
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Model ID: ${modelId}`);
 
-    try {
-        console.log("-".repeat(53));
-        const responses = await invokeModel(prompt, modelId);
-        responses.forEach((response) => console.log(response));
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    console.log("-".repeat(53));
+    const responses = await invokeModel(prompt, modelId);
+    responses.forEach((response) => console.log(response));
+  } catch (err) {
+    console.log(err);
+  }
 }
