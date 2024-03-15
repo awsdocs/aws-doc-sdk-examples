8d85f007c10f0fb55c012ea29efe5e4577bec7f0
Dennis Traub <traubd@amazon.com>
2024-03-15 11:23:25 +0100

    Add simple hello scenario

---
+++ javascriptv3/example_code/bedrock-runtime/text_playground_cli.js
@@ -0,0 +1,46 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { fileURLToPath } from "url";
+import { askForPrompt, selectModel } from "./tools/user_input.js";
+import { FoundationModels } from "./foundation_models.js";
+
+/**
+ * @typedef {Object} ModelConfig
+ * @property {Function} module
+ * @property {Function} invoker
+ * @property {string} modelId
+ * @property {string} modelName
+ */
+
+const invokeModel = async (prompt, /** @type {ModelConfig} */ model) => {
+  try {
+    const modelModule = await model.module();
+    const invoker = model.invoker(modelModule);
+    return await invoker(prompt, model.modelId);
+  } catch (err) {
+    console.error(`Error invoking model ${model.modelId}:`, err);
+    throw err;
+  }
+};
+
+const runDemo = async () => {
+  console.log("Welcome to the Amazon Bedrock Runtime client demo!");
+
+  /** @type {ModelConfig} */
+  const model = await selectModel(Object.values(FoundationModels));
+  const prompt = await askForPrompt();
+
+  console.log(`Invoking ${model.modelName} with prompt '${prompt}'`);
+
+  const response = await invokeModel(prompt, model);
+  if (Array.isArray(response)) {
+    response.forEach((str) => console.log(str));
+  } else {
+    console.log(response);
+  }
+};
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  runDemo();
+}
