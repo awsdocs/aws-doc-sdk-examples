dbd4ca2ce630fd42e24cc17d1a2cc6e895b52c1c
Dennis Traub <traubd@amazon.com>
2024-03-18 14:32:39 +0100

    Add claude 3 variant with response stream

--- javascriptv3/example_code/bedrock-runtime/models/anthropic_claude/claude_3.js
+++ javascriptv3/example_code/bedrock-runtime/models/anthropic_claude/claude_3.js
@@ -8,16 +8,26 @@ import { defaultProvider } from "@aws-sdk/credential-provider-node";
 import {
   BedrockRuntimeClient,
   InvokeModelCommand,
+  InvokeModelWithResponseStreamCommand,
 } from "@aws-sdk/client-bedrock-runtime";
 
 /**
  * @typedef {Object} ResponseContent
  * @property {string} text
- */
-
-/**
+ *
  * @typedef {Object} MessagesResponseBody
  * @property {ResponseContent[]} content
+ *
+ * @typedef {Object} Delta
+ * @property {string} text
+ *
+ * @typedef {Object} Message
+ * @property {string} role
+ *
+ * @typedef {Object} Chunk
+ * @property {string} type
+ * @property {Delta} delta
+ * @property {Message} message
  */
 
 /**
@@ -67,19 +77,90 @@ export const invokeModel = async (prompt, modelId) => {
   return responseBody.content.map((content) => content.text);
 };
 
+/**
+ * Invokes Anthropic Claude 3 and processes the response stream.
+ *
+ * To learn more about the Anthropic Messages API, go to:
+ * https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-anthropic-claude-messages.html
+ *
+ * @param {string} prompt - The input text prompt for the model to complete.
+ * @param {string} [modelId] - The ID of the model to use. Defaults to "anthropic.claude-3-haiku-20240307-v1:0".
+ * @returns {Promise<Object[]>} The final response from the model.
+ */
+export const invokeModelWithResponseStream = async (prompt, modelId) => {
+  // Create a new Bedrock Runtime client instance.
+  const client = new BedrockRuntimeClient({
+    region: "us-east-1",
+    credentialDefaultProvider: defaultProvider,
+  });
+
+  // Use the provided model ID or fallback to Claude 3 Haiku if not provided.
+  modelId = modelId || "anthropic.claude-3-haiku-20240307-v1:0";
+
+  // Prepare the payload for the model.
+  const payload = {
+    anthropic_version: "bedrock-2023-05-31",
+    max_tokens: 1000,
+    messages: [
+      {
+        role: "user",
+        content: [{ type: "text", text: prompt }],
+      },
+    ],
+  };
+
+  const command = new InvokeModelWithResponseStreamCommand({
+    contentType: "application/json",
+    body: JSON.stringify(payload),
+    modelId,
+  });
+
+  const apiResponse = await client.send(command);
+
+  let role;
+  let final_message = "";
+
+  for await (const item of apiResponse.body) {
+    /** @type Chunk */
+    const chunk = JSON.parse(new TextDecoder().decode(item.chunk.bytes));
+    const chunk_type = chunk.type;
+
+    if (chunk_type === "message_start") {
+      role = chunk.message.role;
+    } else if (chunk_type === "content_block_delta") {
+      const text = chunk.delta.text;
+      final_message = final_message + text;
+      process.stdout.write(text);
+    }
+  }
+
+  const final_response = {
+    role: role,
+    content: [
+      {
+        type: "text",
+        text: final_message,
+      },
+    ],
+  };
+
+  return final_response;
+};
+
 // Invoke the function if this file was run directly.
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  const prompt =
-    'Complete the following in one sentence: "Once upon a time..."';
+  const prompt = 'Write a paragraph starting with: "Once upon a time..."';
   const modelId = FoundationModels.CLAUDE_3_HAIKU.modelId;
   console.log(`Prompt: ${prompt}`);
   console.log(`Model ID: ${modelId}`);
 
   try {
     console.log("-".repeat(53));
-    const responses = await invokeModel(prompt, modelId);
-    responses.forEach((response) => console.log(response));
+    const response = await invokeModelWithResponseStream(prompt, modelId);
+    console.log("\n" + "-".repeat(53));
+    console.log("Final structured response:");
+    console.log(response);
   } catch (err) {
-    console.log(err);
+    console.log(`\n${err}`);
   }
 }
