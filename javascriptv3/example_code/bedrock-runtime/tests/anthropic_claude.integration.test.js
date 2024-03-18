35c02fa08b74c02051318d1fda731236d11a50fe
Dennis Traub <traubd@amazon.com>
2024-03-18 14:32:39 +0100

    Add claude 3 variant with response stream

--- javascriptv3/example_code/bedrock-runtime/tests/anthropic_claude.integration.test.js
+++ javascriptv3/example_code/bedrock-runtime/tests/anthropic_claude.integration.test.js
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { describe, it } from "vitest";
+import { expect, describe, it } from "vitest";
 import { FoundationModels } from "../config/foundation_models.js";
 import { expectToBeANonEmptyString } from "./test_tools.js";
 import {
@@ -12,7 +12,10 @@ import {
   invokeTextCompletionsApi as invokeClaude2TextCompletionsApi,
   invokeMessagesApi as invokeClaude2MessagesApi,
 } from "../models/anthropic_claude/claude_2.js";
-import { invokeModel as invokeClaude3 } from "../models/anthropic_claude/claude_3.js";
+import {
+  invokeModel as invokeClaude3,
+  invokeModelWithResponseStream as invokeClaude3WithResponseStream,
+} from "../models/anthropic_claude/claude_3.js";
 
 const TEXT_PROMPT = "Hello, this is a test prompt";
 
@@ -86,5 +89,17 @@ describe("Invoke Anthropic Claude 3 Sonnet using the Messages API", () => {
     const modelId = FoundationModels.CLAUDE_3_SONNET.modelId;
     const response = await invokeClaude3(TEXT_PROMPT, modelId);
     expectToBeANonEmptyString(response[0]);
+  });
+});
+
+describe("Invoke Anthropic Claude 3 Haiku with a response stream", () => {
+  it("should return a response", async () => {
+    const modelId = FoundationModels.CLAUDE_3_HAIKU.modelId;
+    const response = await invokeClaude3WithResponseStream(
+      TEXT_PROMPT,
+      modelId,
+    );
+    expect(response).not.toBe(null);
+    expect(response.role).toBe("assistant");
   });
 });
