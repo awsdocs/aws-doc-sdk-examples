a38f884f7d184a637fc901ad65196e4de03fd85a
Brandon Cotter <brcotter@amazon.com>
2024-03-18 10:09:53 -0700

    Cognito Flutter app additional linter findings

--- kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
+++ kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
@@ -5,5 +5,4 @@ package com.example.android_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/windows/runner/win32_window.cpp
+++ kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/windows/runner/win32_window.cpp
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #include "win32_window.h"
 
 #include <dwmapi.h>
