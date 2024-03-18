b412516dba766fa4868115c4124dc8bab36a0e1f
Brandon Cotter <brcotter@amazon.com>
2024-03-18 10:17:54 -0700

    Cognito Flutter app fixed an underscore

--- kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
+++ kotlin/services/cognito/src/main/Cognito_Flutter_Mobile_App/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package com.example.android_app
+package com.example.android-app
 
 import io.flutter.embedding.android.FlutterActivity
 
