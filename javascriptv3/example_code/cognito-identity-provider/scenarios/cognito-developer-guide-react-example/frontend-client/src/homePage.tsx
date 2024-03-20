6a6503b4139734a9e21fa05c2a1d3a47e4538d26
Brandon Cotter <brcotter@amazon.com>
2024-03-20 12:29:30 -0700

    fixed missing brackets from app test

--- javascriptv3/example_code/cognito-identity-provider/scenarios/cognito-developer-guide-react-example/frontend-client/src/homePage.tsx
+++ javascriptv3/example_code/cognito-identity-provider/scenarios/cognito-developer-guide-react-example/frontend-client/src/homePage.tsx
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import useNavigate from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
   const navigate = useNavigate();
