9f952fb0da029289af735bcd08eea4fb45c4aacb
Brandon C (Amazon) <brcotter@amazon.com>
2024-03-22 10:40:44 -0700

    Example Cognito react app (#6261)
    * added Cognito example React application

---
+++ javascriptv3/example_code/cognito-identity-provider/scenarios/cognito-developer-guide-react-example/frontend-client/src/homePage.tsx
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default HomePage;
