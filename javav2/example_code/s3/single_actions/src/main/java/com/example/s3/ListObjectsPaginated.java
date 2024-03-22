4722ee2b38cea5b6aae6f71f471ca5d636b722d4
scmacdon <scmacdon@amazon.com>
2024-03-22 12:12:19 -0400

    updated folder structure for s3 scenario

---
+++ javav2/example_code/s3/single_actions/src/main/java/com/example/s3/ListObjectsPaginated.java
@@ -0,0 +1,57 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.s3;
+
+// snippet-start:[s3.java2.list_objects.pag.main]
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
+
+public class ListObjectsPaginated {
+    public static void main(String[] args) {
+        final String usage = """
+
+                Usage:
+                    <bucketName>\s
+
+                Where:
+                    bucketName - The Amazon S3 bucket from which objects are read.\s
+                """;
+
+        if (args.length != 1) {
+            System.out.println(usage);
+            System.exit(1);
+        }
+
+        String bucketName = args[0];
+        Region region = Region.US_EAST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        listBucketObjects(s3, bucketName);
+        s3.close();
+    }
+
+    public static void listBucketObjects(S3Client s3, String bucketName) {
+        try {
+            ListObjectsV2Request listReq = ListObjectsV2Request.builder()
+                    .bucket(bucketName)
+                    .maxKeys(1)
+                    .build();
+
+            ListObjectsV2Iterable listRes = s3.listObjectsV2Paginator(listReq);
+            listRes.stream()
+                    .flatMap(r -> r.contents().stream())
+                    .forEach(content -> System.out.println(" Key: " + content.key() + " size = " + content.size()));
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+}
+// snippet-end:[s3.java2.list_objects.pag.main]
---
+++ javav2/example_code/s3/single_actions/src/main/java/com/example/s3/SetAcl.java
@@ -0,0 +1,89 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.s3;
+
+// snippet-start:[s3.java2.set_acl.main]
+// snippet-start:[s3.java2.set_acl.import]
+import java.util.ArrayList;
+import java.util.List;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.services.s3.model.Permission;
+import software.amazon.awssdk.services.s3.model.Grant;
+import software.amazon.awssdk.services.s3.model.AccessControlPolicy;
+import software.amazon.awssdk.services.s3.model.Type;
+import software.amazon.awssdk.services.s3.model.PutBucketAclRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+// snippet-end:[s3.java2.set_acl.import]
+
+/**
+ * Before running this Java V2 code example, set up your development
+ * environment, including your credentials.
+ *
+ * For more information, see the following documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class SetAcl {
+    public static void main(String[] args) {
+        final String usage = """
+
+                Usage:
+                  <bucketName> <id>\s
+
+                Where:
+                  bucketName - The Amazon S3 bucket to grant permissions on.\s
+                  id - The ID of the owner of this bucket (you can get this value from the AWS Management Console).
+                """;
+
+        if (args.length != 2) {
+            System.out.println(usage);
+            System.exit(1);
+        }
+
+        String bucketName = args[0];
+        String id = args[1];
+        System.out.format("Setting access \n");
+        System.out.println(" in bucket: " + bucketName);
+        Region region = Region.US_EAST_1;
+        S3Client s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        setBucketAcl(s3, bucketName, id);
+        System.out.println("Done!");
+        s3.close();
+    }
+
+    public static void setBucketAcl(S3Client s3, String bucketName, String id) {
+        try {
+            Grant ownerGrant = Grant.builder()
+                    .grantee(builder -> builder.id(id)
+                            .type(Type.CANONICAL_USER))
+                    .permission(Permission.FULL_CONTROL)
+                    .build();
+
+            List<Grant> grantList2 = new ArrayList<>();
+            grantList2.add(ownerGrant);
+
+            AccessControlPolicy acl = AccessControlPolicy.builder()
+                    .owner(builder -> builder.id(id))
+                    .grants(grantList2)
+                    .build();
+
+            PutBucketAclRequest putAclReq = PutBucketAclRequest.builder()
+                    .bucket(bucketName)
+                    .accessControlPolicy(acl)
+                    .build();
+
+            s3.putBucketAcl(putAclReq);
+
+        } catch (S3Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
+// snippet-end:[s3.java2.set_acl.main]
