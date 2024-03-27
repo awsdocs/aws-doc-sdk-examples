68bd03572b9316fd5470f5a552766c3a0c2b7653
scmacdon <scmacdon@amazon.com>
2024-03-27 14:25:37 -0400

    updated a Java file

--- javav2/example_code/s3/s3/src/main/java/com/example/s3/GeneratePresignedUrlAndUploadObject.java
+++ javav2/example_code/s3/s3/src/main/java/com/example/s3/GeneratePresignedUrlAndUploadObject.java
@@ -12,7 +12,8 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
-import software.amazon.awssdk
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
