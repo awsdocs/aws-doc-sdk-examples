60d3385ccba0fa518a07bc7d03b1efcd43f8183d
Brandon Cotter <brcotter@amazon.com>
2024-04-01 15:06:09 -0700

    trying adding a filename declaration to mainactivity

--- kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
+++ kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/android-app/MainActivity.kt
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// MainActivity.kt
 package com.example.android-app
 
 import io.flutter.embedding.android.FlutterActivity
