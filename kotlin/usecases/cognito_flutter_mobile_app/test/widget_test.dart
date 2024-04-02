58fafc94bdac81e5a084bb9764ba67ec6faeec89
Brandon Cotter <brcotter@amazon.com>
2024-04-02 16:15:51 -0700

    updated application name to remove punctuation

--- kotlin/usecases/cognito_flutter_mobile_app/test/widget_test.dart
+++ kotlin/usecases/cognito_flutter_mobile_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:android_app/main.dart';
+import 'package:myapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
