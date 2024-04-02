14f8a8b98e4a851af36e391932b118799e8a477a
Brandon Cotter <brcotter@amazon.com>
2024-04-02 16:36:47 -0700

    fixed linter findings for Cognito Android app

--- kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -5,5 +5,4 @@ package com.example.myapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() { /* not used */ }
