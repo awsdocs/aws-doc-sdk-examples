698096a4982dcc8a02576b4307f182df05291e8f
Brandon C (Amazon) <brcotter@amazon.com>
2024-04-03 11:57:08 -0700

    Kotlin: Cognito - Update example Android app with rename (#6309)

--- kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++ kotlin/usecases/cognito_flutter_mobile_app/android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
@@ -1,9 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// MainActivity.kt
-package com.example.androidapp
+package com.example.myapp
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() { /*...*/ }
+class MainActivity : FlutterActivity() { /* not used */ }
