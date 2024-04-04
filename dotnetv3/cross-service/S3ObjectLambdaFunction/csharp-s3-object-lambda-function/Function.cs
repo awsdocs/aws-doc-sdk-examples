64056b3ca64cb5a5a5236548ca947a5557d0f8b7
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2024-04-04 08:40:54 -0700

    SPDX Update

--- dotnetv3/cross-service/S3ObjectLambdaFunction/csharp-s3-object-lambda-function/Function.cs
+++ dotnetv3/cross-service/S3ObjectLambdaFunction/csharp-s3-object-lambda-function/Function.cs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 using System.Text;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
