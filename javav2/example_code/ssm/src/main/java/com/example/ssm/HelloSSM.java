53006ec60e68c55f74f64efa5456cca90142e1a7
scmacdon <scmacdon@amazon.com>
2024-04-12 12:14:08 -0400

    updated Hello SSM

--- javav2/example_code/ssm/src/main/java/com/example/ssm/HelloSSM.java
+++ javav2/example_code/ssm/src/main/java/com/example/ssm/HelloSSM.java
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.example.ssm;
 
 // snippet-start:[ssm.java2.hello.main]
@@ -5,11 +8,25 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.DocumentFilter;
 import software.amazon.awssdk.services.ssm.model.ListDocumentsRequest;
+import software.amazon.awssdk.services.ssm.model.ListDocumentsResponse;
 import software.amazon.awssdk.services.ssm.paginators.ListDocumentsIterable;
 
 public class HelloSSM {
 
     public static void main(String[] args) {
+        final String usage = """
+
+                Usage:
+                    <awsAccount>
+
+                Where:
+                    awsAccount - Your AWS Account number.
+                """;
+
+       // if (args.length != 1) {
+       //     System.out.println(usage);
+      //      System.exit(1);
+       // }
 
         String awsAccount = "814548047983" ;
         Region region = Region.US_EAST_1;
@@ -20,19 +37,31 @@ public class HelloSSM {
         listDocuments(ssmClient, awsAccount);
     }
 
+    /*
+    This code automatically fetches the next set of results using the `nextToken` and
+    stops once the desired maxResults (20 in this case) have been reached.
+    */
     public static void listDocuments(SsmClient ssmClient, String awsAccount) {
-        ListDocumentsRequest request = ListDocumentsRequest.builder()
-            .documentFilterList(
-                DocumentFilter.builder()
-                    .key("Owner")
-                    .value(awsAccount)
-                    .build()
-            )
-            .maxResults(50)
-            .build();
+        String nextToken = null;
+        int totalDocumentsReturned = 0;
+        int maxResults = 20;
+        do {
+            ListDocumentsRequest request = ListDocumentsRequest.builder()
+                .documentFilterList(
+                    DocumentFilter.builder()
+                        .key("Owner")
+                        .value(awsAccount)
+                        .build()
+                    )
+                .maxResults(maxResults)
+                .nextToken(nextToken)
+                .build();
 
-        ListDocumentsIterable response = ssmClient.listDocumentsPaginator(request);
-        response.documentIdentifiers().forEach(identifier -> System.out.println("Document Name: " + identifier.name()));
+            ListDocumentsResponse response = ssmClient.listDocuments(request);
+            response.documentIdentifiers().forEach(identifier -> System.out.println("Document Name: " + identifier.name()));
+            nextToken = response.nextToken();
+            totalDocumentsReturned += response.documentIdentifiers().size();
+        } while (nextToken != null && totalDocumentsReturned < maxResults);
     }
 }
 // snippet-end:[ssm.java2.hello.main]
