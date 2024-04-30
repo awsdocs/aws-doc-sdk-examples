28c6ce3f965ef63015976d79236ec02f2dfb5883
Laren-AWS <larencra@amazon.com>
2024-04-30 13:42:16 -0700

    Add missed SPDX.

--- gov2/workflows/user_pools_and_lambda_triggers/cdk/lib/pools-and-triggers-base.ts
+++ gov2/workflows/user_pools_and_lambda_triggers/cdk/lib/pools-and-triggers-base.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import * as cdk from 'aws-cdk-lib';
 import {CfnOutput, RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
