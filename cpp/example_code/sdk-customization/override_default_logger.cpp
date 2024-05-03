53b9debe2326113b241403e64366ecbed7b197a8
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2024-05-03 16:24:03 -0400

    customizations first commit

---
+++ cpp/example_code/sdk-customization/override_default_logger.cpp
@@ -0,0 +1,14 @@
+//  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Before running this C++ code example, set up your development environment, including your credentials.
+ *
+ * For more information, see the following documentation topic:
+ *
+ * https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/getting-started.html
+ *
+ * For information on the structure of the code examples and how to build and run the examples, see
+ * https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/getting-started-code-examples.html.
+ *
+ **/
---
+++ cpp/example_code/sdk-customization/tests/customization_gtests.h
@@ -0,0 +1,52 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+#pragma once
+#ifndef S3_EXAMPLES_S3_GTESTS_H
+#define S3_EXAMPLES_S3_GTESTS_H
+
+#include <aws/core/Aws.h>
+#include <memory>
+#include <gtest/gtest.h>
+
+namespace AwsDocTest {
+
+    class MyStringBuffer : public std::stringbuf {
+        int underflow() override;
+    };
+
+    class S3_GTests : public testing::Test {
+    protected:
+
+        void SetUp() override;
+
+        void TearDown() override;
+
+        static void SetUpTestSuite();
+
+        static void TearDownTestSuite();
+
+        static Aws::String preconditionError();
+
+        void AddCommandLineResponses(const std::vector<std::string> &responses);
+
+        // s_clientConfig must be a pointer because the client config must be initialized
+        // after InitAPI.
+        static std::unique_ptr<Aws::Client::ClientConfiguration> s_clientConfig;
+
+    private:
+
+        static bool suppressStdOut();
+
+        static Aws::SDKOptions s_options;
+
+        std::stringbuf m_coutBuffer;  // Used to silence cout.
+        std::streambuf *m_savedBuffer = nullptr;
+
+        MyStringBuffer m_cinBuffer;
+        std::streambuf *m_savedInBuffer = nullptr;
+    }; // S3_GTests
+} // AwsDocTest
+
+#endif //S3_EXAMPLES_S3_GTESTS_H
