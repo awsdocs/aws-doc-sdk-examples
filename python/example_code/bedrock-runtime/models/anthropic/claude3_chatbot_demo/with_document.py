c1c905178eecd30a3928d86b7b517a10fa5a4bf7
ford prior <108086978+ford-at-aws@users.noreply.github.com>
2024-05-14 16:21:11 -0400

    Python - Bedrock: Remove FordGPT references and make chatbot application generic. (#6456)

--- python/example_code/bedrock-runtime/models/anthropic/claude3_chatbot_demo/with_document.py
+++ python/example_code/bedrock-runtime/models/anthropic/claude3_chatbot_demo/with_document.py
@@ -1,6 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 import os
+import yaml
 
 import boto3
 from langchain_community.chat_models import BedrockChat
@@ -9,7 +10,7 @@ from utils.custom_logging import setup_custom_logger
 from utils.timeit import timeit
 
 
-class FordGPTv2:
+class Claude3WithDocumentRAG:
     """
     A class encapsulating the functionality to run a conversational application
     simulating interactions as Ford Prior, using context from a resume loaded from
@@ -21,13 +22,15 @@ class FordGPTv2:
 
     def __init__(self):
         """
-        Initializes the conversational app with a custom logger, a Amazon Bedrock runtime client,
+        Initializes the conversational app with a custom logger, an Amazon Bedrock runtime client,
         and sets up the LangChain's BedrockChat model for conversation handling.
         """
+        with open("config.yaml", "r") as file:
+            self.data = yaml.safe_load(file)
         self.logger = setup_custom_logger(os.path.basename(__file__))
         self.boto3_bedrock = boto3.client(service_name="bedrock-runtime")
         self.llm = BedrockChat(
-            model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+            model_id=self.data["model"],
             client=self.boto3_bedrock,
         )
 
@@ -39,7 +42,7 @@ class FordGPTv2:
         user inputs.
 
         The conversation continues until "bye" is included in a user's reply, integrating
-        context from Ford Prior's resume throughout the interaction.
+        context from the user-provided document throughout the interaction.
 
         Exception Handling:
             Captures and logs any exceptions, indicating failure to run the application.
@@ -50,35 +53,19 @@ class FordGPTv2:
         from langchain_core.prompts import PromptTemplate
 
         try:
-            loader = S3FileLoader("bedrock-training-ford-gpt", "Profile.pdf")
-            resume = loader.load()
+            loader = S3FileLoader(self.data["bucket_name"], self.data["file_name"])
+            document = loader.load()
 
             template = PromptTemplate(
-                input_variables=["resume", "input_text"],
+                input_variables=["document", "input_text"],
                 template="""
-                    Imagine you are Ford Prior, a seasoned cloud and DevOps engineer with a
-                    rich background in cloud engineering, DevOps practices, and software development.
-
-                    Here is Ford's work history: {resume}
-
-                    Your response should reflect Ford's style: extremely concise, insightful, and
-                    very direct with a side of snark. Only help if asked; otherwise, be cheeky
-                    but truthful.
-
-                    If you are asked a question directly, ask one question to gain context,
-                    then dive in confidently with a local tip for getting into
-                    the Richmond tech scene based on your context and knowledge.
-
-                    Do not self refer or admit you are a bot. You are Ford Prior.
-
-                    User: {input_text}
-
-                    How would you reply? No intro; just reply as you would:
+                    Take into account the following document: {document}
+                    {input_text}
                 """,
             )
 
             prompt = template.format(
-                resume=resume, input_text="Introduce yourself in a single sentence."
+                document=document, input_text="Introduce yourself to start the chat."
             )
 
             conversation = ConversationChain(
@@ -89,12 +76,12 @@ class FordGPTv2:
             initial_prediction = conversation.predict(input=prompt)
             self.logger.warning(initial_prediction)
 
-            self.reply_with_document(template, "You:", conversation, resume)
+            self.reply_with_document(template, "User: ", conversation, document)
         except Exception as e:
             self.logger.error(f"Application failed to run: {e}")
             raise
 
-    def reply_with_document(self, template, reply_text, conversation, resume):
+    def reply_with_document(self, template, reply_text, conversation, document):
         """
         Facilitates a conversation using dynamically generated prompts, incorporating user input
         and document context. Logs responses until "bye" is detected in user input.
@@ -103,19 +90,20 @@ class FordGPTv2:
             template (PromptTemplate): The template for generating conversation prompts.
             reply_text (str): Initial text to prompt user input.
             conversation (ConversationChain): The conversation handler.
-            resume (str): Contextual document content to include in prompts.
+            document (str): Contextual document content to include in prompts.
         """
         reply = input(reply_text)
         while "bye" not in reply.lower():
             try:
-                prompt = template.format(resume=resume, input_text=reply)
+                prompt = template.format(document=document, input_text=reply)
+                breakpoint()
                 prediction = conversation.predict(input=prompt)
                 self.logger.warning(prediction)
-                reply = input("You:")
+                reply = input("User: ")
             except Exception as e:
                 self.logger.error(f"Error during conversation: {e}")
                 break
 
 
-app = FordGPTv2()
+app = Claude3WithDocumentRAG()
 app.run_app()
--- python/example_code/bedrock-runtime/models/anthropic/claude3_chatbot_demo/with_knowledgebase.py
+++ python/example_code/bedrock-runtime/models/anthropic/claude3_chatbot_demo/with_knowledgebase.py
@@ -1,34 +1,41 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 import os
-
+import boto3
+import yaml
+import argparse
 from langchain_community.chat_models import BedrockChat
-
 from utils.custom_logging import setup_custom_logger
 from utils.timeit import timeit
 
 
-class FordGPTv3:
+class Claude3WithKnowledgeBase:
     """
-    A conversational QA application simulating responses as Ford Prior, using a
+    A conversational QA application simulating responses using a
     RetrievalQA model integrated with a knowledge base and a large language model.
-    It processes user inputs in an interactive session, utilizing Ford's resume and
+    It processes user inputs in an interactive session, utilizing a contextual doc and
     user queries to generate contextually relevant answers.
+
+    This module relies on the existence of an S3 bucket with user-provided document
+    and a Knowledge Base (created using these instructions:
+    https://docs.aws.amazon.com/bedrock/latest/userguide/knowledge-base-create.html)
     """
 
-    def __init__(self):
+    def __init__(self, knowledge_base_id):
         """
         Initializes the QA application with necessary components, including a custom logger,
         the BedrockChat model for interaction with a large language model, and identifiers for
         knowledge base and document retrieval.
         """
+        with open("config.yaml", "r") as file:
+            self.data = yaml.safe_load(file)
         self.logger = setup_custom_logger(os.path.basename(__file__))
-        self.model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-        self.knowledge_base_id = "TB1WKZHMYK"
+        self.boto3_bedrock = boto3.client(service_name="bedrock-runtime")
         self.llm = BedrockChat(
-            model_id=self.model_id,
-            region_name="us-east-1",
+            model_id=self.data["model"],
+            client=self.boto3_bedrock,
         )
+        self.knowledge_base_id = knowledge_base_id
 
     @timeit
     def get_kb_answer(self, prompt, qa):
@@ -69,34 +76,16 @@ class FordGPTv3:
                 llm=self.llm, retriever=retriever, return_source_documents=False
             )
 
-            # loader = S3FileLoader("bedrock-training-ford-gpt", "Profile.pdf")
-            # resume = loader.load()
-            resume = "See file in context."
-
-            multi_var_prompt = PromptTemplate(
-                input_variables=["input", "resume"],
+            template = PromptTemplate(
+                input_variables=["input"],
                 template="""
-                         Imagine you are Ford Prior, a seasoned cloud and DevOps engineer with a
-                         rich background in cloud engineering, DevOps practices, and software development.
-
-                         His work experience is here: {resume}
-
-                         Your response should reflect Ford's style: extremely concise, insightful, and
-                         very direct with a side of snark. Only help if asked; otherwise, be cheeky
-                         but truthful.
-
-                         User: {input}
-
-                         How would you reply? No intro; just reply as you would:
+                         {input}
                      """,
             )
 
             input_text = input("You: ")
-            prompt = multi_var_prompt.format(input=input_text, resume=resume)
-
-            initial_response = self.get_kb_answer(
-                f"{prompt}\nIntroduce yourself in a sentence.", qa
-            )
+            prompt = template.format(input=input_text)
+            initial_response = self.get_kb_answer(f"{prompt}\nIntroduce yourself.", qa)
             self.logger.warning(initial_response)
             reply = input("\nYou: ")
 
@@ -106,7 +95,22 @@ class FordGPTv3:
                 reply = input("\nYou: ")
         except Exception as e:
             self.logger.error(f"Application failed to run: {e}")
+            raise
 
 
-app = FordGPTv3()
-app.run_app()
+if __name__ == "__main__":
+    # Argument parsing setup
+    parser = argparse.ArgumentParser(
+        description="Run Claude3WithKnowledgeBase with a specified knowledge base ID."
+    )
+    parser.add_argument(
+        "--knowledge_base_id",
+        type=str,
+        required=True,
+        help="The ID of the knowledge base to use.",
+    )
+    args = parser.parse_args()
+
+    # Create an instance of the app with the provided knowledge base ID and run it
+    app = Claude3WithKnowledgeBase(knowledge_base_id=args.knowledge_base_id)
+    app.run_app()
