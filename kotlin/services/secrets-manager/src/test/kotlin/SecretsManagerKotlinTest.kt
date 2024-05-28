853f5ed2f6ddc3cea0a1d2a2d62794f9671e087d
scmacdon <scmacdon@amazon.com>
2024-05-28 11:20:43 -0400

    fixed linter issue

--- kotlin/services/secrets-manager/src/test/kotlin/SecretsManagerKotlinTest.kt
+++ kotlin/services/secrets-manager/src/test/kotlin/SecretsManagerKotlinTest.kt
@@ -1,7 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-
-
 import com.kotlin.secrets.getValue
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
