07ec23a4a21e3a3257555a24f435cad3a3f2f15c
Brian Murray <40031786+brmur@users.noreply.github.com>
2024-06-06 13:54:28 +0100

    Add action to push updated repo to lambda layer for numbers count.

--- python/cross_service/aurora_item_tracker/config.py
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-CLUSTER_ARN = "NEED-CLUSTER-ARN"
-SECRET_ARN = "NEED-SECRET-ARN"
-DATABASE = "auroraappdb"
-TABLE_NAME = "work_items"
-SENDER_EMAIL = "NEED-SENDER-EMAIL"
-SECRET_KEY = "change-for-production!"
--- python/cross_service/dynamodb_item_tracker/config.py
+++
@@ -1,5 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-TABLE_NAME = "NEED-TABLE-NAME"
-SENDER_EMAIL = "NEED-SENDER-EMAIL"
-SECRET_KEY = "change-for-production!"
