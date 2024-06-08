e10272ebea810a3eb3c8d6862830c25b22343a6c
scmacdon <scmacdon@amazon.com>
2024-06-08 17:36:44 -0400

    added more lintered files

--- kotlin/services/cognito/src/main/kotlin/com/kotlin/cognito/SignUpUser.kt
+++ kotlin/services/cognito/src/main/kotlin/com/kotlin/cognito/SignUpUser.kt
@@ -83,13 +83,13 @@ suspend fun signUp(
 fun calculateSecretHash(
     userPoolClientId: String,
     userPoolClientSecret: String,
-    userName: String,
+    userName: String
 ): String {
     val macSha256Algorithm = "HmacSHA256"
     val signingKey =
         SecretKeySpec(
             userPoolClientSecret.toByteArray(StandardCharsets.UTF_8),
-            macSha256Algorithm,
+            macSha256Algorithm
         )
     try {
         val mac = Mac.getInstance(macSha256Algorithm)
