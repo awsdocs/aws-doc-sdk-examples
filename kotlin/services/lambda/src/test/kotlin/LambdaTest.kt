374e201689798a707063a88ffcdc5cc21ac54449
scmacdon <scmacdon@amazon.com>
2024-06-10 09:59:02 -0400

    added more lintered files

--- kotlin/services/lambda/src/test/kotlin/LambdaTest.kt
+++ kotlin/services/lambda/src/test/kotlin/LambdaTest.kt
@@ -5,7 +5,6 @@ import aws.sdk.kotlin.runtime.auth.credentials.EnvironmentCredentialsProvider
 import aws.sdk.kotlin.services.secretsmanager.SecretsManagerClient
 import aws.sdk.kotlin.services.secretsmanager.model.GetSecretValueRequest
 import com.google.gson.Gson
-import com.kotlin.lambda.updateFunctionConfiguration
 import com.kotlin.lambda.createNewFunction
 import com.kotlin.lambda.createScFunction
 import com.kotlin.lambda.delFunction
@@ -16,6 +15,8 @@ import com.kotlin.lambda.invokeFunctionSc
 import com.kotlin.lambda.listFunctions
 import com.kotlin.lambda.listFunctionsSc
 import com.kotlin.lambda.updateFunctionCode
+import com.kotlin.lambda.updateFunctionConfiguration
+import java.util.UUID
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
@@ -26,7 +27,6 @@ import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
-import java.util.UUID
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
