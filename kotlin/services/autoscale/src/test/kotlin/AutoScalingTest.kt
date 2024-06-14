cb92c21ffd838fa666d513eab7c2a5d9cc03bfad
scmacdon <scmacdon@amazon.com>
2024-06-14 14:56:54 -0400

    lintered kotlin files with ktlint v 12.1.1

--- kotlin/services/autoscale/src/test/kotlin/AutoScalingTest.kt
+++ kotlin/services/autoscale/src/test/kotlin/AutoScalingTest.kt
@@ -56,6 +56,7 @@ class AutoScalingTest {
         serviceLinkedRoleARN = values.serviceLinkedRoleARN.toString()
         groupNameSc = values.groupNameSc.toString() + randomNum
         // Uncomment this code block if you prefer using a config.properties file to retrieve AWS values required for these tests.
+
         /*
         try {
             AutoScalingTest::class.java.getClassLoader().getResourceAsStream("config.properties").use { input ->
