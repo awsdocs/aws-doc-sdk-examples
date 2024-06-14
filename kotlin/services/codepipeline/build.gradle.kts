8aea1eaf3a92f5b140ed6d23c31d64052156f42d
scmacdon <scmacdon@amazon.com>
2024-06-14 15:21:10 -0400

    lintered kotlin files with ktlint v 12.1.1

--- kotlin/services/codepipeline/build.gradle.kts
+++ kotlin/services/codepipeline/build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:12.1.1")
     }
 }
 
@@ -27,15 +27,15 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:codepipeline:1.0.30")
-    implementation("aws.sdk.kotlin:secretsmanager:1.0.30")
+    implementation("aws.sdk.kotlin:codepipeline:1.2.28")
+    implementation("aws.sdk.kotlin:secretsmanager:1.2.28")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
     implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
     implementation("com.google.code.gson:gson:2.10")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 
