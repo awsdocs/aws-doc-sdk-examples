06d676918fb5e551443b2aae8338de9e40fa526a
scmacdon <scmacdon@amazon.com>
2024-06-16 10:21:17 -0400

    lintered kotlin files with ktlint v 12.1.1

--- kotlin/services/ec2/build.gradle.kts
+++ kotlin/services/ec2/build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.3.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:12.1.1")
     }
 }
 
@@ -37,7 +37,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 tasks.test {
