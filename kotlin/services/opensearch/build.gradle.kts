71b9cd7b1bae6b0d36a722bce063437c6ac0c6c1
scmacdon <scmacdon@amazon.com>
2024-06-16 12:35:45 -0400

    lintered kotlin files with ktlint v 12.1.1

--- kotlin/services/opensearch/build.gradle.kts
+++ kotlin/services/opensearch/build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.5.1")
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:12.1.1")
     }
 }
 
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 tasks.test {
