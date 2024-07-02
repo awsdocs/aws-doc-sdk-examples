bcfff586f2c203d860774ac3de5b3239d818bc11
scmacdon <scmacdon@amazon.com>
2024-07-01 21:48:33 -0400

    added kotlin ecr files

---
+++ kotlin/services/ecr/src/main/kotlin/com/example/ecr/HelloECR.kt
@@ -0,0 +1,26 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// PDX-License-Identifier: Apache-2.0
+
+package com.example.ecr
+
+import aws.sdk.kotlin.services.ecr.EcrClient
+import aws.sdk.kotlin.services.ecr.model.ListImagesRequest
+
+suspend fun main(args: Array<String>) {
+    val repoName = "test61"
+    listImageTags(repoName)
+}
+
+suspend fun listImageTags(repoName: String?) {
+    val listImages =
+        ListImagesRequest {
+            repositoryName = repoName
+        }
+
+    EcrClient { region = "us-east-1" }.use { ecrClient ->
+        val imageResponse = ecrClient.listImages(listImages)
+        imageResponse.imageIds?.forEach { imageId ->
+            println("Image tag: ${imageId.imageTag}")
+        }
+    }
+}
