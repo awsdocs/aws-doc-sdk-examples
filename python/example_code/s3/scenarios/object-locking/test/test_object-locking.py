70d7569dbbed357f96a33e9e25a4ed3b37197380
ford-at-aws <fprior@amazon.com>
2024-07-16 12:41:44 -0400

    SPDX

--- python/example_code/s3/scenarios/object-locking/test/test_object-locking.py
+++ python/example_code/s3/scenarios/object-locking/test/test_object-locking.py
@@ -1,4 +1,6 @@
 import os
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import sys
 import unittest
 from unittest.mock import MagicMock, patch
