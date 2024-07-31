1ba3062fb88a51465a406fad5fde750f71209117
scmacdon <scmacdon@amazon.com>
2024-07-31 11:55:40 -0400

    updated Java example

--- javav2/example_code/batch/src/main/java/com/example/batch/scenario/BatchScenario.java
+++ javav2/example_code/batch/src/main/java/com/example/batch/scenario/BatchScenario.java
@@ -3,8 +3,14 @@
 
 package com.example.batch.scenario;
 
+import software.amazon.awssdk.services.batch.model.BatchException;
+import software.amazon.awssdk.services.batch.model.CreateComputeEnvironmentResponse;
+import software.amazon.awssdk.services.batch.model.JobSummary;
+
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
 public class BatchScenario {
@@ -29,24 +35,17 @@ public class BatchScenario {
         dockerImage = accId+"."+dockerImage; // Get the account Id into docker image.
 
         System.out.println("""
-            Amazon Batch is a fully managed batch processing service that enables developers, scientists, and engineers to run batch 
-            computing workloads of any scale. It dynamically provisions the optimal quantity and type of compute resources 
-            (e.g., CPU or memory-optimized instances) based on the volume and specific resource requirements of the batch jobs submitted.
+            AWS Batch is a fully managed batch processing service that dynamically provisions the required compute 
+            resources for batch computing workloads. The Java V2 SDK's `BatchAsyncClient` interface allows 
+            developers to automate the submission, monitoring, and management of batch jobs.
                         
-            The Java V2 SDK allows you to interact with various AWS services programmatically. The `BatchAsyncClient` interface 
-            in the Java V2 SDK enables developers to automate the submission, monitoring, and management of batch jobs on the Amazon Batch service.
-                        
-            In this scenario, we'll explore how to use the Java V2 SDK to interact with the Amazon Batch service, including:
-                        
-            1. **Create an AWS Batch Job Definition**: Define a job definition that is used to run your Docker container on Fargate. This job definition will include the Docker image location (ECR repository), the container properties, and the execution role that grants the necessary permissions.
-                        
-            2. **Submit an AWS Batch Job**: Submit a job to AWS Batch, specifying the job definition you created in the previous step. This will trigger the process of pulling the Docker image from ECR and launching the container on Fargate.
-                        
-            3. **Monitor the Job Execution**: AWS Batch will handle the execution of the job on Fargate, including pulling the Docker image from ECR, starting the container, and monitoring its execution.
-                        
+            This scenario provides an example of setting up a compute environment, job queue and job definition, 
+            and then submitting a job.
+            
             Let's get started...
                         
             You have two choices:
+            
             1 - Run the entire program.
             2 - Delete an existing Compute Environment (created from a previous execution of 
             this program that did not complete).
@@ -87,25 +86,25 @@ public class BatchScenario {
         System.out.println(DASHES);
 
         waitForInputToContinue(scanner);
-        System.out.println("Use CloudFormation to create two IAM roles that are required for this scenario.");
+        System.out.println("Use AWS CloudFormation to create two IAM roles that are required for this scenario.");
         CloudFormationHelper.deployCloudFormationStack(STACK_NAME);
         CloudFormationHelper.deployCloudFormationStack(STACK_ECR);
 
         Map<String, String> stackOutputs = CloudFormationHelper.getStackOutputs(STACK_NAME);
         Map<String, String> stackOutputECR = CloudFormationHelper.getStackOutputs(STACK_ECR);
-        String iamRole = stackOutputs.get("BatchRoleArn");
+        String batchIAMRole = stackOutputs.get("BatchRoleArn");
         String executionRoleARN = stackOutputECR.get("EcsRoleArn");
 
-        System.out.println("The IAM role needed to interact wit AWS Batch is "+iamRole);
+        System.out.println("The IAM role needed to interact wit AWS Batch is "+batchIAMRole);
         System.out.println("The second IAM role needed to interact wit AWS ECR is "+executionRoleARN);
         waitForInputToContinue(scanner);
 
-        System.out.println("1. Create a Batch compute Environment");
+        System.out.println(DASHES);
+        System.out.println("1. Create a Batch compute environment");
         System.out.println("""
-            An Amazon Batch compute environment is a resource where you can run your batch jobs. 
+            A compute environment is a resource where you can run your batch jobs. 
             After creating a compute environment, you can define job queues and job definitions to submit jobs for 
-            execution. Here’s an overview of what you can do with your compute environment and the types of jobs 
-            you can create:
+            execution. 
             
             The benefit of creating a compute environment is it allows you to easily configure and manage the compute 
             resources that will be used to run your Batch jobs. By separating the compute environment from the job definitions,
@@ -115,77 +114,104 @@ public class BatchScenario {
             """);
 
         waitForInputToContinue(scanner);
-        batchActions.createComputeEnvironment(computeEnvironmentName, iamRole, subnet, secGroup);
+        try {
+            CompletableFuture<CreateComputeEnvironmentResponse> future = batchActions.createComputeEnvironmentAsync(computeEnvironmentName, batchIAMRole, subnet, secGroup);
+            CreateComputeEnvironmentResponse response = future.join();
+            System.out.println("Compute Environment ARN: " + response.computeEnvironmentArn());
+        } catch (BatchException e) {
+            System.err.println("A Batch exception occurred: " + e.getMessage());
+            return;
+        } catch (RuntimeException e) {
+            System.err.println("An error occurred while creating the compute environment: " + e.getMessage());
+            return;
+        }
+        waitForInputToContinue(scanner);
         System.out.println(DASHES);
 
+        System.out.println(DASHES);
         System.out.println("2. Check the status of the "+computeEnvironmentName +" Compute Environment.");
         waitForInputToContinue(scanner);
-        batchActions.checkComputeEnvironmentsStatus(computeEnvironmentName).thenAccept(status -> {
-            System.out.println("Current Status: " + status);
-        }).join();
+        try {
+            CompletableFuture<String> future = batchActions.checkComputeEnvironmentsStatus(computeEnvironmentName);
+            String status = future.join();
+            System.out.println("Compute Environment Status: " + status);
+
+        } catch (BatchException e) {
+            System.err.println("A Batch exception occurred: " + e.getMessage());
+            return;
+        } catch (RuntimeException e) {
+            System.err.println("An error occurred while checking the compute environment status: " + e.getMessage());
+            return;
+        }
         waitForInputToContinue(scanner);
         System.out.println(DASHES);
 
-        System.out.println("3. What You Can Do with a Compute Environment?");
+        System.out.println(DASHES);
+        System.out.println("3. Create a job queue");
         System.out.println("""
-            Submit Jobs: You can submit batch jobs to the compute environment for execution. Jobs can be containerized applications or scripts.
-            Manage Job Queues: Define job queues to prioritize and manage jobs. Jobs submitted to a queue are evaluated by the scheduler to determine when, where, and how they run.
-            Define Job Definitions: Create job definitions that specify how jobs are to be run, including parameters, environment variables, and resource requirements.
-            Types of Jobs
-            Batch Processing Jobs: Process large volumes of data, such as ETL (Extract, Transform, Load) operations, image processing, and video transcoding.
-            Machine Learning Jobs: Train machine learning models or run inference tasks using frameworks like TensorFlow, PyTorch, or scikit-learn.
-            Compute-Intensive Jobs: Perform simulations, modeling, and other CPU/GPU-intensive tasks.
-            Data Analysis Jobs: Analyze large datasets, run statistical analyses, or perform data mining.
-            Setting Up Job Queues and Job Definitions.
-            
-            This scenario provides an example of setting up a job queue and job definition, and then submitting a job.
-            """);
+             A job queue is an essential component that helps manage the execution of your batch jobs. 
+             It acts as a buffer, where jobs are placed and then scheduled for execution based on their 
+             priority and the available resources in the compute environment. 
+             """);
         waitForInputToContinue(scanner);
-        String jobQueueArn = batchActions.createJobQueueAsync(jobQueueName, computeEnvironmentName).join();
-        System.out.println("Job Queue ARN returned: " + jobQueueArn);
+
+        String jobQueueArn="";
+        try {
+            CompletableFuture<String> jobQueueFuture = batchActions.createJobQueueAsync(jobQueueName, computeEnvironmentName);
+            jobQueueArn = jobQueueFuture.join();
+            System.out.println("Job Queue ARN: " + jobQueueArn);
+
+        } catch (BatchException e) {
+            System.err.println("A Batch exception occurred: " + e.getMessage());
+            return;
+        } catch (RuntimeException e) {
+            System.err.println("An error occurred while creating the job queue: " + e.getMessage());
+            return;
+        }
+
         waitForInputToContinue(scanner);
         System.out.println(DASHES);
 
         System.out.println("4. Register a Job Definition.");
         System.out.println("""
             Registering a job in AWS Batch using the Fargate launch type ensures that all
-            necessary parameters, such as the execution role, command to run, and so on 
-            are specified and reused across multiple job submissions. 
+            necessary parameters, such as the execution role, command to run, and so on
+            are specified and reused across multiple job submissions.
             
-            This promotes a standardized and efficient approach to managing containerized workloads 
-            in the cloud.
+             The job definition pulls a Docker image from Amazon ECR and executes the Docker image.
             """);
 
         waitForInputToContinue(scanner);
-        String[] jobARN = new String[1];
+        String jobARN = null;
         try {
-            jobARN[0] = batchActions.registerJobDefinitionAsync(jobDefinitionName, executionRoleARN, dockerImage)
+            jobARN = batchActions.registerJobDefinitionAsync(jobDefinitionName, executionRoleARN, dockerImage)
                 .exceptionally(ex -> {
                     System.err.println("Register job definition failed: " + ex.getMessage());
                     return null;
                 })
                 .join();
-            if (jobARN[0] != null) {
-                System.out.println("Job ARN: " + jobARN[0]);
+            if (jobARN != null) {
+                System.out.println("Job ARN: " + jobARN);
             }
         } catch (CompletionException ex) {
             System.err.println("Failed to register job definition: " + ex.getMessage());
+            return;
         }
         System.out.println(DASHES);
 
         System.out.println(DASHES);
         System.out.println("5. Submit an AWS Batch job from a job definition.");
         waitForInputToContinue(scanner);
-        String[] jobId = new String[1];
+        String jobId = null;
         try {
-            jobId[0] = batchActions.submitJobAsync(jobDefinitionName, jobQueueName, jobARN[0])
+            jobId = batchActions.submitJobAsync(jobDefinitionName, jobQueueName, jobARN)
                 .exceptionally(ex -> {
                     System.err.println("Submit job failed: " + ex.getMessage());
                     return null;
                 })
                 .join();
 
-            System.out.println("The job id is "+jobId[0]);
+            System.out.println("The job id is "+jobId);
 
         } catch (CompletionException ex) {
             System.err.println("Failed to submit job: " + ex.getMessage());
@@ -198,7 +224,11 @@ public class BatchScenario {
 
         waitForInputToContinue(scanner);
         try {
-            batchActions.listJobsAsync(jobDefinitionName, jobQueueName);
+            List<JobSummary> jobs =  batchActions.listJobsAsync(jobQueueName);
+            jobs.forEach(job ->
+                System.out.printf("Job ID: %s, Job Name: %s, Job Status: %s%n",
+                    job.jobId(), job.jobName(), job.status())
+            );
 
         } catch (CompletionException ex) {
             System.err.println("Failed to list jobs: " + ex.getMessage());
@@ -208,23 +238,19 @@ public class BatchScenario {
         System.out.println(DASHES);
 
         System.out.println(DASHES);
-        System.out.println("7. Check the status of job "+jobId[0]);
+        System.out.println("7. Check the status of job "+jobId);
         waitForInputToContinue(scanner);
         try {
-            String jobStatus = batchActions.describeJobAsync(jobId[0])
-                .exceptionally(ex -> {
-                    System.err.println("Describe job failed: " + ex.getMessage());
-                    return null;
-                })
-                .join();
+            CompletableFuture<String> future = batchActions.describeJobAsync(jobId);
+            String jobStatus = future.join();
+            System.out.println("Job Status: " + jobStatus);
 
-            if (jobStatus != null) {
-                System.out.println("Job status: " + jobStatus);
-            }
-
-        } catch (CompletionException ex) {
-            System.err.println("Failed to describe job: " + ex.getMessage());
+        } catch (BatchException e) {
+            System.err.println("A Batch exception occurred: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.err.println("An error occurred while describing the job: " + e.getMessage());
         }
+
         waitForInputToContinue(scanner);
         System.out.println(DASHES);
 
@@ -242,7 +268,7 @@ public class BatchScenario {
             System.out.println("First, we will deregister the Job Definition.");
             waitForInputToContinue(scanner);
             try {
-                batchActions.deregisterJobDefinitionAsync(jobARN[0])
+                batchActions.deregisterJobDefinitionAsync(jobARN)
                     .exceptionally(ex -> {
                         System.err.println("Deregister job definition failed: " + ex.getMessage());
                         return null;
@@ -309,7 +335,7 @@ public class BatchScenario {
         }
 
         System.out.println(DASHES);
-        System.out.println("This concludes the Amazon Batch SDK scenario");
+        System.out.println("This concludes the AWS Batch SDK scenario");
         System.out.println(DASHES);
     }
 
@@ -338,7 +364,6 @@ public class BatchScenario {
             System.out.print(String.format("\r%02d:%02d", displayMinutes, displaySeconds));
             Thread.sleep(1000); // Wait for 1 second
         }
-
         System.out.println("Countdown complete!");
     }
 }
