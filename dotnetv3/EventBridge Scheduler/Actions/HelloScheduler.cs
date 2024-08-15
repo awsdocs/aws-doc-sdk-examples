f56cd36e7a76a61de7fc5a8ffaba361032f81cf9
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-08-15 11:49:15 -0500

    .NET v3: Adding EventBridge Scheduler Workflow (#14)
    Adding workflow and related files.

---
+++ dotnetv3/EventBridge Scheduler/Actions/HelloScheduler.cs
@@ -0,0 +1,41 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using Amazon.Scheduler;
+using Amazon.Scheduler.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SchedulerActions;
+
+// snippet-start:[Scheduler.dotnetv3.HelloScheduler]
+public static class HelloScheduler
+{
+    static async Task Main(string[] args)
+    {
+        // Use the AWS .NET Core Setup package to set up dependency injection for the EventBridge Scheduler service.
+        // Use your AWS profile name, or leave it blank to use the default profile.
+        using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+                services.AddAWSService<IAmazonScheduler>()
+            ).Build();
+
+        // Now the client is available for injection.
+        var schedulerClient = host.Services.GetRequiredService<IAmazonScheduler>();
+
+        // You can use await and any of the async methods to get a response, or a paginator to list schedules or groups.
+        var results = new List<ScheduleSummary>();
+        var paginateSchedules = schedulerClient.Paginators.ListSchedules(
+            new ListSchedulesRequest());
+        Console.WriteLine(
+            $"Hello AWS Scheduler! Let's list schedules in your account.");
+        // Get the entire list using the paginator.
+        await foreach (var schedule in paginateSchedules.Schedules)
+        {
+            results.Add(schedule);
+        }
+        Console.WriteLine($"\tTotal of {results.Count} schedule(s) available.");
+        results.ForEach(s => Console.WriteLine($"\tSchedule: {s.Name}"));
+    }
+}
+// snippet-end:[Scheduler.dotnetv3.HelloScheduler]
