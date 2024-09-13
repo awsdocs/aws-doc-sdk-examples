5aa285e67299986770065a64e726c0eec5e28bca
ford-at-aws <fprior@amazon.com>
2024-09-13 16:37:35 -0400

    fixes

--- python/cross_service/apigateway_covid-19_tracker/test/conftest.py
+++ python/cross_service/apigateway_covid-19_tracker/test/conftest.py
@@ -7,5 +7,15 @@ Contains common test fixtures used to run AWS Lambda REST tests.
 
 import sys
 
-# This is needed so Python can find test_tools on the path.
-sys.path.append("../..")
+sys.path.append("../..") # noqa
+
+from test_tools.fixtures.common import (
+    pytest_configure,
+    fixture_make_stubber,
+    fixture_make_unique_name,
+    fixture_make_bucket,
+    StubRunner,
+    stub_runner,
+    InputMocker,
+    input_mocker
+)
