67c729830c764e08bc08733b41ee452cfdbaa190
Corey Pyle <corepyle@amazon.com>
2024-09-17 11:58:54 -0400

    JavaScript (v3): Kinesis - Replace web example with single action (#6872)
    The previous example was not configured with server-side encryption,
    which is the recommended best practice. It was also a web example
    that only showcase one SDK call, which we have decided doesn't meet
    the bar of a good example. This replaces the web example with a
    a single-action nodejs example and provides updated CDK/CFN scripts.

---
+++ javascriptv3/example_code/kinesis/kinesis-cdk/README.md
@@ -0,0 +1,13 @@
+# Kinesis example resources 
+
+The CDK resources necessary to run the Kinesis example code in this service directory.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ javascriptv3/example_code/kinesis/kinesis-cdk/lib/kinesis-cdk-stack.ts
@@ -0,0 +1,26 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as kinesis from "aws-cdk-lib/aws-kinesis";
+
+export class KinesisCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const stream = new kinesis.Stream(this, "ExampleStream", {
+      streamName: "example-stream",
+      encryption: kinesis.StreamEncryption.KMS,
+    });
+
+    new cdk.CfnOutput(this, "ExampleStreamName", {
+      key: "ExampleStreamName",
+      value: stream.streamName,
+    });
+    new cdk.CfnOutput(this, "ExampleStreamArn", {
+      key: "ExampleStreamArn",
+      value: stream.streamArn,
+    });
+  }
+}
