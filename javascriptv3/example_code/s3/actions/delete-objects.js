0474ae3994f2724ba055194ebdc24d21af01907f
Corey Pyle <corepyle@amazon.com>
2024-09-19 13:33:36 -0400

    JavaScript (v3): S3 - Standardize DeleteObjects example.

--- javascriptv3/example_code/s3/actions/delete-objects.js
+++ javascriptv3/example_code/s3/actions/delete-objects.js
@@ -1,34 +1,72 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { fileURLToPath } from "url";
-
 // snippet-start:[s3.JavaScript.buckets.deletemultipleobjectsV3]
-import { DeleteObjectsCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectsCommand,
+  S3Client,
+  S3ServiceException,
+  waitUntilObjectNotExists,
+} from "@aws-sdk/client-s3";
 
-const client = new S3Client({});
-
-export const main = async () => {
+/**
+ * Delete multiple objects from an S3 bucket.
+ * @param {{ bucketName: string, keys: string[] }}
+ */
+export const main = async ({ bucketName, keys }) => {
+  const client = new S3Client({});
   const command = new DeleteObjectsCommand({
-    Bucket: "test-bucket",
+    Bucket: bucketName,
     Delete: {
-      Objects: [{ Key: "object1.txt" }, { Key: "object2.txt" }],
+      Objects: keys.map((k) => ({ Key: k })),
     },
   });
 
   try {
     const { Deleted } = await client.send(command);
+    for (const key in keys) {
+      await waitUntilObjectNotExists(
+        { client },
+        { Bucket: bucketName, Key: key },
+      );
+    }
     console.log(
       `Successfully deleted ${Deleted.length} objects from S3 bucket. Deleted objects:`,
     );
     console.log(Deleted.map((d) => ` • ${d.Key}`).join("\n"));
-  } catch (err) {
-    console.error(err);
+  } catch (caught) {
+    if (
+      caught instanceof S3ServiceException &&
+      caught.name === "NoSuchBucket"
+    ) {
+      console.error(
+        `Error from S3 while deleting objects from ${bucketName}. The bucket doesn't exist.`,
+      );
+    } else if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while deleting objects from ${bucketName}.  ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
   }
 };
 // snippet-end:[s3.JavaScript.buckets.deletemultipleobjectsV3]
 
-// Invoke main function if this file was run directly.
+// Call function if run directly
+import { fileURLToPath } from "url";
+import { parseArgs } from "util";
+
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  main();
+  const options = {
+    bucketName: {
+      type: "string",
+      default: "my-bucket",
+    },
+  };
+  const { values, positionals } = parseArgs({
+    options,
+    allowPositionals: true,
+  });
+  main({ ...values, keys: positionals });
 }
--- javascriptv3/example_code/s3/tests/delete-objects.unit.test.js
+++ javascriptv3/example_code/s3/tests/delete-objects.unit.test.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { S3ServiceException } from "@aws-sdk/client-s3";
 import { describe, it, expect, vi } from "vitest";
 
 const send = vi.fn();
@@ -9,6 +10,7 @@ vi.doMock("@aws-sdk/client-s3", async () => {
   const actual = await vi.importActual("@aws-sdk/client-s3");
   return {
     ...actual,
+    waitUntilObjectNotExists: () => Promise.resolve(),
     S3Client: class {
       send = send;
     },
@@ -23,7 +25,7 @@ describe("delete-objects", () => {
 
     const spy = vi.spyOn(console, "log");
 
-    await main();
+    await main({ bucketName: "my-bucket", keys: ["foo", "bar"] });
 
     expect(spy).toHaveBeenNthCalledWith(
       1,
@@ -32,13 +34,42 @@ describe("delete-objects", () => {
     expect(spy).toHaveBeenNthCalledWith(2, ` • foo\n • bar`);
   });
 
-  it("should log errors", async () => {
-    send.mockRejectedValue("foo");
+  it("should log a relevant error when the bucket doesn't exist", async () => {
+    const error = new S3ServiceException("The specified bucket does not exist");
+    error.name = "NoSuchBucket";
+    const bucketName = "my-bucket";
+    send.mockRejectedValueOnce(error);
 
     const spy = vi.spyOn(console, "error");
 
-    await main();
+    await main({ bucketName, keys: ["foo"] });
 
-    expect(spy).toHaveBeenCalledWith("foo");
+    expect(spy).toHaveBeenCalledWith(
+      `Error from S3 while deleting objects from ${bucketName}. The bucket doesn't exist.`,
+    );
+  });
+
+  it("should indicate a failure came from S3 when the error isn't generic", async () => {
+    const error = new S3ServiceException("Some S3 service exception.");
+    error.name = "ServiceException";
+    const bucketName = "my-bucket";
+    send.mockRejectedValueOnce(error);
+
+    const spy = vi.spyOn(console, "error");
+
+    await main({ bucketName, keys: ["foo"] });
+
+    expect(spy).toHaveBeenCalledWith(
+      `Error from S3 while deleting objects from ${bucketName}.  ${error.name}: ${error.message}`,
+    );
+  });
+
+  it("should throw errors that are not S3 specific", async () => {
+    const bucketName = "my-bucket";
+    send.mockRejectedValueOnce(new Error());
+
+    await expect(() =>
+      main({ bucketName, keys: ["foo"] }),
+    ).rejects.toBeTruthy();
   });
 });
