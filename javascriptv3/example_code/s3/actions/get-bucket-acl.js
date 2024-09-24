7b911e1b1e600644e3de5e1964fdb6c262398e70
Corey Pyle <corepyle@amazon.com>
2024-09-24 15:31:37 -0400

    JavaScript (v3): S3 - Standardize GetBucketAcl example.

--- javascriptv3/example_code/s3/actions/get-bucket-acl.js
+++ javascriptv3/example_code/s3/actions/get-bucket-acl.js
@@ -1,28 +1,58 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { fileURLToPath } from "url";
-
 // snippet-start:[s3.JavaScript.perms.getBucketAclV3]
-import { GetBucketAclCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetBucketAclCommand,
+  S3Client,
+  S3ServiceException,
+} from "@aws-sdk/client-s3";
 
-const client = new S3Client({});
-
-export const main = async () => {
-  const command = new GetBucketAclCommand({
-    Bucket: "test-bucket",
-  });
+/**
+ * Retrieves the Access Control List (ACL) for an S3 bucket.
+ * @param {string} bucketName - The name of the S3 bucket.
+ */
+export const main = async ({ bucketName }) => {
+  const client = new S3Client({});
 
   try {
-    const response = await client.send(command);
-    console.log(response);
-  } catch (err) {
-    console.error(err);
+    const response = await client.send(
+      new GetBucketAclCommand({
+        Bucket: bucketName,
+      }),
+    );
+    console.log(`ACL for bucket "${bucketName}":`);
+    console.log(JSON.stringify(response, null, 2));
+  } catch (caught) {
+    if (
+      caught instanceof S3ServiceException &&
+      caught.name === "NoSuchBucket"
+    ) {
+      console.error(
+        `Error from S3 while getting ACL for ${bucketName}. The bucket doesn't exist.`,
+      );
+    } else if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while getting ACL for ${bucketName}. ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
   }
 };
 // snippet-end:[s3.JavaScript.perms.getBucketAclV3]
 
-// Invoke main function if this file was run directly.
+// Call function if run directly
+import { parseArgs } from "util";
+import { fileURLToPath } from "url";
+
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  main();
+  const options = {
+    bucketName: {
+      type: "string",
+      default: "amzn-s3-demo-bucket",
+    },
+  };
+  const { values } = parseArgs({ options });
+  main(values);
 }
