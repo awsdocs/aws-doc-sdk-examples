6705651ea15d6ff072074e64a4a35202d34d1f99
Corey Pyle <corepyle@amazon.com>
2024-09-24 14:42:15 -0400

    JavaScript (v3): S3 - Standardize PutBucketCors example.

--- javascriptv3/example_code/s3/actions/put-bucket-cors.js
+++ javascriptv3/example_code/s3/actions/put-bucket-cors.js
@@ -1,49 +1,77 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { fileURLToPath } from "url";
+// snippet-start:[s3.JavaScript.cors.putBucketCorsV3]
+import {
+  PutBucketCorsCommand,
+  S3Client,
+  S3ServiceException,
+} from "@aws-sdk/client-s3";
 
-// snippet-start:[s3.JavaScript.v3.cors.putBucketCors]
-import { PutBucketCorsCommand, S3Client } from "@aws-sdk/client-s3";
-
-const client = new S3Client({});
-
-// By default, Amazon S3 doesn't allow cross-origin requests. Use this command
-// to explicitly allow cross-origin requests.
-export const main = async () => {
-  const command = new PutBucketCorsCommand({
-    Bucket: "test-bucket",
-    CORSConfiguration: {
-      CORSRules: [
-        {
-          // Allow all headers to be sent to this bucket.
-          AllowedHeaders: ["*"],
-          // Allow only GET and PUT methods to be sent to this bucket.
-          AllowedMethods: ["GET", "PUT"],
-          // Allow only requests from the specified origin.
-          AllowedOrigins: ["https://www.example.com"],
-          // Allow the entity tag (ETag) header to be returned in the response. The ETag header
-          // The entity tag represents a specific version of the object. The ETag reflects
-          // changes only to the contents of an object, not its metadata.
-          ExposeHeaders: ["ETag"],
-          // How long the requesting browser should cache the preflight response. After
-          // this time, the preflight request will have to be made again.
-          MaxAgeSeconds: 3600,
-        },
-      ],
-    },
-  });
+/**
+ * Allows cross-origin requests to an S3 bucket by setting the CORS configuration.
+ * @param {string} bucketName - The name of the S3 bucket.
+ */
+export const main = async ({ bucketName }) => {
+  const client = new S3Client({});
 
   try {
-    const response = await client.send(command);
-    console.log(response);
-  } catch (err) {
-    console.error(err);
+    await client.send(
+      new PutBucketCorsCommand({
+        Bucket: bucketName,
+        CORSConfiguration: {
+          CORSRules: [
+            {
+              // Allow all headers to be sent to this bucket.
+              AllowedHeaders: ["*"],
+              // Allow only GET and PUT methods to be sent to this bucket.
+              AllowedMethods: ["GET", "PUT"],
+              // Allow only requests from the specified origin.
+              AllowedOrigins: ["https://www.example.com"],
+              // Allow the entity tag (ETag) header to be returned in the response. The ETag header
+              // The entity tag represents a specific version of the object. The ETag reflects
+              // changes only to the contents of an object, not its metadata.
+              ExposeHeaders: ["ETag"],
+              // How long the requesting browser should cache the preflight response. After
+              // this time, the preflight request will have to be made again.
+              MaxAgeSeconds: 3600,
+            },
+          ],
+        },
+      }),
+    );
+    console.log(`Successfully set CORS rules for bucket: ${bucketName}`);
+  } catch (caught) {
+    if (
+      caught instanceof S3ServiceException &&
+      caught.name === "NoSuchBucket"
+    ) {
+      console.error(
+        `Error from S3 while setting CORS rules for ${bucketName}. The bucket doesn't exist.`,
+      );
+    } else if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while setting CORS rules for ${bucketName}. ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
   }
 };
-// snippet-end:[s3.JavaScript.v3.cors.putBucketCors]
+// snippet-end:[s3.JavaScript.cors.putBucketCorsV3]
 
-// Invoke main function if this file was run directly.
+// Call function if run directly
+import { fileURLToPath } from "url";
+import { parseArgs } from "util";
+
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  main();
+  const { values } = parseArgs({
+    options: {
+      bucketName: {
+        type: "string",
+        default: "amzn-s3-demo-bucket",
+      },
+    },
+  });
+  main(values);
 }
