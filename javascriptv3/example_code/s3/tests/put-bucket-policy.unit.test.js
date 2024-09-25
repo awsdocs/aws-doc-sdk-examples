5609151e59f326ac2155582c21dbd3d0fa66ec37
Corey Pyle <corepyle@amazon.com>
2024-09-25 10:26:16 -0400

    JavaScript (v3): S3 - Standardize PutBucketPolicy example.

--- javascriptv3/example_code/s3/tests/put-bucket-policy.unit.test.js
+++ javascriptv3/example_code/s3/tests/put-bucket-policy.unit.test.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { S3ServiceException } from "@aws-sdk/client-s3";
 import { describe, it, expect, vi } from "vitest";
 
 const send = vi.fn();
@@ -20,21 +21,50 @@ const { main } = await import("../actions/put-bucket-policy.js");
 describe("put-bucket-policy", () => {
   it("should log the response from the service", async () => {
     send.mockResolvedValue("foo");
-
+    const bucketName = "amzn-s3-demo-bucket";
     const spy = vi.spyOn(console, "log");
 
-    await main();
+    await main({ bucketName });
 
-    expect(spy).toHaveBeenCalledWith("foo");
+    expect(spy).toHaveBeenCalledWith(
+      `GetObject access to the bucket "${bucketName}" was granted to the provided IAM role.`,
+    );
   });
 
-  it("should log errors", async () => {
-    send.mockRejectedValue("foo");
+  it("should log a relevant error when the bucket doesn't exist", async () => {
+    const error = new S3ServiceException("The specified bucket does not exist");
+    error.name = "NoSuchBucket";
+    const bucketName = "amzn-s3-demo-bucket";
+    send.mockRejectedValueOnce(error);
 
     const spy = vi.spyOn(console, "error");
 
-    await main();
+    await main({ bucketName });
 
-    expect(spy).toHaveBeenCalledWith("foo");
+    expect(spy).toHaveBeenCalledWith(
+      `Error from S3 while setting the bucket policy for the bucket "${bucketName}". The bucket doesn't exist.`,
+    );
+  });
+
+  it("should indicate a failure came from S3 when the error isn't generic", async () => {
+    const error = new S3ServiceException("Some S3 service exception.");
+    error.name = "ServiceException";
+    const bucketName = "amzn-s3-demo-bucket";
+    send.mockRejectedValueOnce(error);
+
+    const spy = vi.spyOn(console, "error");
+
+    await main({ bucketName });
+
+    expect(spy).toHaveBeenCalledWith(
+      `Error from S3 while setting the bucket policy for the bucket "${bucketName}". ${error.name}: ${error.message}`,
+    );
+  });
+
+  it("should throw errors that are not S3 specific", async () => {
+    const bucketName = "amzn-s3-demo-bucket";
+    send.mockRejectedValueOnce(new Error());
+
+    await expect(() => main({ bucketName })).rejects.toBeTruthy();
   });
 });
