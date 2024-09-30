7d2cb96434ea1b1f140abc2a0e068bdfd7fb4e09
Dennis Traub <dennis.traub@gmail.com>
2024-09-30 15:54:47 +0200

    [Fix] Amazon Bedrock Runtime: Fix bug that occurs when using Llama3 70B Instruct (#6942)
    Upgrade dependencies and fix Llama3 example in C#, Java, JavaScript, and Python

--- javav2/example_code/bedrock-runtime/src/main/java/com/example/bedrockruntime/models/metaLlama/Llama3_InvokeModel.java
+++ javav2/example_code/bedrock-runtime/src/main/java/com/example/bedrockruntime/models/metaLlama/Llama3_InvokeModel.java
@@ -22,11 +22,11 @@ public class Llama3_InvokeModel {
         // Replace the DefaultCredentialsProvider with your preferred credentials provider.
         var client = BedrockRuntimeClient.builder()
                 .credentialsProvider(DefaultCredentialsProvider.create())
-                .region(Region.US_EAST_1)
+                .region(Region.US_WEST_2)
                 .build();
 
-        // Set the model ID, e.g., Llama 3 8b Instruct.
-        var modelId = "meta.llama3-8b-instruct-v1:0";
+        // Set the model ID, e.g., Llama 3 70b Instruct.
+        var modelId = "meta.llama3-70b-instruct-v1:0";
 
         // The InvokeModel API uses the model's native payload.
         // Learn more about the available inference parameters and response fields at:
@@ -38,8 +38,7 @@ public class Llama3_InvokeModel {
 
         // Embed the prompt in Llama 3's instruction format.
         var instruction = (
-                "<|begin_of_text|>\\n" +
-                "<|start_header_id|>user<|end_header_id|>\\n" +
+                "<|begin_of_text|><|start_header_id|>user<|end_header_id|>\\n" +
                 "{{prompt}} <|eot_id|>\\n" +
                 "<|start_header_id|>assistant<|end_header_id|>\\n"
         ).replace("{{prompt}}", prompt);
--- python/example_code/bedrock-runtime/models/meta_llama/llama3_invoke_model_with_response_stream.py
+++ python/example_code/bedrock-runtime/models/meta_llama/llama3_invoke_model_with_response_stream.py
@@ -11,18 +11,17 @@ import json
 from botocore.exceptions import ClientError
 
 # Create a Bedrock Runtime client in the AWS Region of your choice.
-client = boto3.client("bedrock-runtime", region_name="us-east-1")
+client = boto3.client("bedrock-runtime", region_name="us-west-2")
 
-# Set the model ID, e.g., Llama 3 8b Instruct.
-model_id = "meta.llama3-8b-instruct-v1:0"
+# Set the model ID, e.g., Llama 3 70b Instruct.
+model_id = "meta.llama3-70b-instruct-v1:0"
 
 # Define the prompt for the model.
 prompt = "Describe the purpose of a 'hello world' program in one line."
 
 # Embed the prompt in Llama 3's instruction format.
 formatted_prompt = f"""
-<|begin_of_text|>
-<|start_header_id|>user<|end_header_id|>
+<|begin_of_text|><|start_header_id|>user<|end_header_id|>
 {prompt}
 <|eot_id|>
 <|start_header_id|>assistant<|end_header_id|>
