bdef9293b8ad5ea29092f0a353a018e05e3621fe
Corey Pyle <corepyle@amazon.com>
2024-10-04 15:44:23 -0400

    JavaScript (v3): S3 - Standardize multi-part download example.

--- javascriptv3/example_code/s3/scenarios/multipart-download.js
+++ javascriptv3/example_code/s3/scenarios/multipart-download.js
@@ -4,8 +4,8 @@
 import { fileURLToPath } from "url";
 
 // snippet-start:[javascript.v3.s3.scenarios.multipartdownload]
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { createWriteStream } from "fs";
+import { GetObjectCommand, NoSuchKey, S3Client } from "@aws-sdk/client-s3";
+import { createWriteStream, rmSync } from "fs";
 
 const s3Client = new S3Client({});
 const oneMB = 1024 * 1024;
@@ -35,9 +35,6 @@ export const getRangeAndLength = (contentRange) => {
 
 export const isComplete = ({ end, length }) => end === length - 1;
 
-// When downloading a large file, you might want to break it down into
-// smaller pieces. Amazon S3 accepts a Range header to specify the start
-// and end of the byte range to be downloaded.
 const downloadInChunks = async ({ bucket, key }) => {
   const writeStream = createWriteStream(
     fileURLToPath(new URL(`./${key}`, import.meta.url)),
@@ -49,28 +46,70 @@ const downloadInChunks = async ({ bucket, key }) => {
     const { end } = rangeAndLength;
     const nextRange = { start: end + 1, end: end + oneMB };
 
-    console.log(`Downloading bytes ${nextRange.start} to ${nextRange.end}`);
-
     const { ContentRange, Body } = await getObjectRange({
       bucket,
       key,
       ...nextRange,
     });
+    console.log(`Downloaded bytes ${nextRange.start} to ${nextRange.end}`);
 
     writeStream.write(await Body.transformToByteArray());
     rangeAndLength = getRangeAndLength(ContentRange);
   }
 };
 
-export const main = async () => {
-  await downloadInChunks({
-    bucket: "my-cool-bucket",
-    key: "my-cool-object.txt",
-  });
+/**
+ * Download a large object from and Amazon S3 bucket.
+ *
+ * When downloading a large file, you might want to break it down into
+ * smaller pieces. Amazon S3 accepts a Range header to specify the start
+ * and end of the byte range to be downloaded.
+ *
+ * @param {{ bucketName: string, key: string }}
+ */
+export const main = async ({ bucketName, key }) => {
+  try {
+    await downloadInChunks({
+      bucket: bucketName,
+      key: key,
+    });
+  } catch (caught) {
+    if (caught instanceof NoSuchKey) {
+      console.error(`Failed to download object. No such key "${key}".`);
+      rmSync(key);
+    }
+  }
 };
 // snippet-end:[javascript.v3.s3.scenarios.multipartdownload]
 
-// Invoke main function if this file was run directly.
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  main();
+// Call function if run directly
+import { parseArgs } from "util";
+import {
+  isMain,
+  validateArgs,
+} from "@aws-doc-sdk-examples/lib/utils/util-node.js";
+
+const loadArgs = () => {
+  const options = {
+    bucketName: {
+      type: "string",
+      required: true,
+    },
+    key: {
+      type: "string",
+      required: true,
+    },
+  };
+  const results = parseArgs({ options });
+  const { errors } = validateArgs({ options }, results);
+  return { errors, results };
+};
+
+if (isMain(import.meta.url)) {
+  const { errors, results } = loadArgs();
+  if (!errors) {
+    main(results.values);
+  } else {
+    console.error(errors.join("\n"));
+  }
 }
