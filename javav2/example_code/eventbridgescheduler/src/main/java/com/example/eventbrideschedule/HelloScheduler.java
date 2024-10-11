35527b5c6ec9f2ce0bfd4e68f68c2eb33b297185
scmacdon <scmacdon@amazon.com>
2024-10-11 09:59:42 -0400

    add eventbridge scheduler code

---
+++ javav2/example_code/eventbridgescheduler/src/main/java/com/example/eventbrideschedule/HelloScheduler.java
@@ -0,0 +1,4 @@
+package com.example.eventbrideschedule;
+
+public class HelloScheduler {
+}
---
+++ javav2/example_code/eventbridgescheduler/src/main/java/com/example/eventbridgeschedule/scenario/EventbridgeSchedulerScenario.java
@@ -0,0 +1,337 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+package com.example.eventbridgeschedule.scenario;
+
+// snippet-start:[scheduler.javav2.scenario.main]
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * This Java code example performs the following tasks for the Amazon EventBridge Scheduler workflow:
+ * <p>
+ * 1. Prepare the Application:
+ * - Prompt the user for an email address to use for the subscription for the SNS topic subscription.
+ * - Deploy the Cloud Formation template in resources/cfn_template.yaml for resource creation.
+ * - Store the outputs of the stack into variables for use in the workflow.
+ * - Create a schedule group for all workflow schedules.
+ * <p>
+ * 2. Create one-time Schedule:
+ * - Create a one-time schedule to send an initial event.
+ * - Use a Flexible Time Window and set the schedule to delete after completion.
+ * - Wait for the user to receive the event email from SNS.
+ * <p>
+ * 3. Create a time-based schedule:
+ * - Prompt the user for how many X times per Y hours a recurring event should be scheduled.
+ * - Create the scheduled event for X times per hour for Y hours.
+ * - Wait for the user to receive the event email from SNS.
+ * - Delete the schedule when the user is finished.
+ * <p>
+ * 4. Clean up:
+ * - Prompt the user for y/n answer if they want to destroy the stack and clean up all resources.
+ * - Delete the schedule group.
+ * - Destroy the Cloud Formation stack and wait until the stack has been removed.
+ */
+
+public class EventbridgeSchedulerScenario {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventbridgeSchedulerScenario.class);
+    private static Scanner scanner = new Scanner(System.in);
+    private static String STACK_NAME = "scott-workflow-stack-name11";
+    private static final String scheduleGroupName = "scott-schedules-group11";
+
+    private static final EventbridgeSchedulerActions eventbridgeActions = new EventbridgeSchedulerActions();
+
+    public static final String DASHES = new String(new char[80]).replace("\0", "-");
+
+    public static String roleArn = "";
+    public static String snsTopicArn = "";
+
+    public static void main(String[] args) {
+        logger.info(DASHES);
+        logger.info("Welcome to the Amazon EventBridge Scheduler Workflow.");
+        logger.info("""
+            Amazon EventBridge Scheduler is a fully managed service that helps you schedule and execute 
+            a wide range of tasks and events in the cloud. It's designed to simplify the process of 
+            scheduling and managing recurring or one-time events, making it easier for developers and 
+            businesses to automate various workflows and processes.
+                        
+            One of the key features of Amazon EventBridge Scheduler is its ability to schedule events 
+            based on a variety of triggers, including time-based schedules, custom event patterns, or 
+            even integration with other AWS services. For example, you can use EventBridge Scheduler 
+            to schedule a report generation task to run every weekday at 9 AM, or to trigger a 
+            Lambda function when a specific Amazon S3 object is created. 
+                        
+            This flexibility allows you to build complex and dynamic event-driven architectures 
+            that adapt to your business needs.
+                        
+            Lets get started... 
+            """);
+        waitForInputToContinue(scanner);
+        logger.info(DASHES);
+
+        logger.info(DASHES);
+        logger.info("1. Prepare the application.");
+        waitForInputToContinue(scanner);
+        try {
+            boolean prepareSuccess = prepareApplication();
+            logger.info(DASHES);
+
+            if (prepareSuccess) {
+                logger.info("2. Create one-time schedule.");
+                logger.info("""
+                    A one-time schedule in Amazon EventBridge Scheduler is an event trigger that allows 
+                    you to schedule a one-time event to run at a specific date and time. This is useful for 
+                    executing a specific task or workflow at a predetermined time, without the need for recurring 
+                    or complex scheduling.
+                    """);
+                waitForInputToContinue(scanner);
+                createOneTimeSchedule();
+                logger.info(DASHES);
+
+                logger.info("3. Create a reoccurring schedule.");
+                logger.info("""
+                    A recurring schedule is a feature that allows you to schedule and manage the execution 
+                    of your serverless applications or workloads on a recurring basis. For example, 
+                    with EventBridge Scheduler, you can create custom schedules for your AWS Lambda functions, 
+                    AWS Step Functions, and other supported event sources, enabling you to automate tasks and 
+                    workflows without the need for complex infrastructure management. T
+                    """);
+                waitForInputToContinue(scanner);
+                createRecurringSchedule();
+                logger.info(DASHES);
+            }
+        } catch (Exception ex) {
+            logger.info("There was a problem with the workflow {}, initiating cleanup...", ex.getMessage());
+            cleanUp();
+        }
+
+        logger.info(DASHES);
+        logger.info("4. Clean up the resources.");
+        logger.info("Do you want to delete these AWS resources (y/n) ?");
+        String delAns = scanner.nextLine().trim();
+        if (delAns.equalsIgnoreCase("y")) {
+            cleanUp();
+        } else {
+            logger.info("The AWS resources will not be deleted.");
+        }
+        logger.info("Amazon EventBridge Scheduler workflow completed.");
+        logger.info(DASHES);
+    }
+
+    public static void cleanUp() {
+        logger.info("First, delete the schedule groups name");
+        waitForInputToContinue(scanner);
+        eventbridgeActions.deleteScheduleGroupAsync(scheduleGroupName);
+        CloudFormationHelper.destroyCloudFormationStack(STACK_NAME);
+    }
+
+    /**
+     * Prepares the application by creating resources in a CloudFormation stack, including an SNS topic
+     * that will be subscribed to the EventBridge Scheduler events. The user will need to confirm the subscription
+     * in order to receive event emails.
+     *
+     * @return true if the application preparation was successful, false otherwise
+     */
+    public static boolean prepareApplication() {
+        logger.info("""
+            This example creates resources in a CloudFormation stack, including an SNS topic
+            that will be subscribed to the EventBridge Scheduler events. 
+            You will need to confirm the subscription in order to receive event emails. 
+             """);
+
+        String emailAddress = promptUserForEmail();
+        logger.info("You entered {}", emailAddress);
+
+        logger.info("Get the roleArn and snsTopicArn values using a Cloudformation template.");
+        waitForInputToContinue(scanner);
+        CloudFormationHelper.deployCloudFormationStack(STACK_NAME, emailAddress);
+        Map<String, String> stackOutputs = CloudFormationHelper.getStackOutputs(STACK_NAME);
+        roleArn = stackOutputs.get("RoleARN");
+        snsTopicArn = stackOutputs.get("SNStopicARN");
+
+        logger.info("The roleARN is {}", roleArn);
+        logger.info("The snsTopicArn is {}", snsTopicArn);
+
+        try {
+            eventbridgeActions.createScheduleGroup(scheduleGroupName).join();
+            logger.info("createScheduleGroupAsync completed successfully.");
+        } catch (RuntimeException e) {
+            logger.error("Error occurred: {} ", e.getMessage());
+            return false;
+        }
+        logger.info("Application preparation complete.");
+        return true;
+    }
+
+    /**
+     * Waits for the user to enter 'c' followed by <ENTER> to continue the program.
+     * This method is used to pause the program execution and wait for user input before
+     * proceeding.
+     *
+     * @param scanner the Scanner object to read user input from
+     */
+    private static void waitForInputToContinue(Scanner scanner) {
+        while (true) {
+            logger.info("");
+            logger.info("Enter 'c' followed by <ENTER> to continue:");
+            String input = scanner.nextLine();
+
+            if (input.trim().equalsIgnoreCase("c")) {
+                logger.info("Continuing with the program...");
+                logger.info("");
+                break;
+            } else {
+                // Handle invalid input.
+                logger.info("Invalid input. Please try again.");
+            }
+        }
+    }
+
+    /**
+     * Prompts the user to enter an email address and validates the input.
+     * If the provided email address is invalid, the method will prompt the user to try again.
+     *
+     * @return the valid email address entered by the user
+     */
+    private static String promptUserForEmail() {
+        logger.info("Enter an email address to use for event subscriptions: ");
+        String email = scanner.nextLine();
+        if (!isValidEmail(email)) {
+            logger.info("Invalid email address. Please try again.");
+            return promptUserForEmail();
+        }
+        return email;
+    }
+
+    /**
+     * Checks if the given email address is valid.
+     *
+     * @param email the email address to be validated
+     * @return {@code true} if the email address is valid, {@code false} otherwise
+     */
+    private static boolean isValidEmail(String email) {
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+            return true;
+
+        } catch (AddressException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Creates a one-time schedule to send an initial event in 1 minute with a flexible time window.
+     *
+     * @return {@code true} if the schedule was created successfully, {@code false} otherwise
+     */
+    public static Boolean createOneTimeSchedule() {
+        String scheduleName = promptUserForResourceName("Enter a name for the one-time schedule:");
+        logger.info("Creating a one-time schedule named {} to send an initial event in 1 minute with a flexible time window...", scheduleName);
+        LocalDateTime scheduledTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+        String scheduleExpression = "at(" + scheduledTime.format(formatter) + ")";
+        return eventbridgeActions.createScheduleAsync(
+            scheduleName,
+            scheduleExpression,
+            scheduleGroupName,
+            snsTopicArn,
+            roleArn,
+            "One time scheduled event test from schedule",
+            true,
+            true).join();
+    }
+
+
+    /**
+     * Creates a recurring schedule to send events based on a specific time.
+     *
+     * @return A {@link CompletableFuture} that completes with a boolean value indicating the success or failure of the operation.
+     */
+    public static Boolean createRecurringSchedule() {
+        logger.info("Creating a recurring schedule to send events for one hour...");
+        String scheduleName = promptUserForResourceName("Enter a name for the recurring schedule:");
+
+        // Prompt the user for the schedule rate (in minutes).
+        int scheduleRateInMinutes = promptUserForInteger("Enter the desired schedule rate (in minutes): ");
+        String scheduleExpression = "rate(" + scheduleRateInMinutes + " minutes)";
+        try {
+            // Call the asynchronous method and wait for it to complete
+            eventbridgeActions.createScheduleGroup(scheduleName).join(); // Use join() to block until completion
+
+            logger.info("createScheduleGroupAsync completed successfully.");
+        } catch (RuntimeException e) {
+            // Handle the exception thrown from the whenComplete block
+            logger.info("Error occurred: {} ", e.getMessage());
+        }
+
+        return eventbridgeActions.createScheduleAsync(
+            scheduleName,
+            scheduleExpression,
+            scheduleGroupName,
+            snsTopicArn,
+            roleArn,
+            "Recurrent event test from schedule " + scheduleName,
+            true,
+            true).join();
+    }
+
+    /**
+     * Prompts the user for a resource name and validates the input.
+     *
+     * @param prompt the message to display to the user when prompting for the resource name
+     * @return the valid resource name entered by the user
+     */
+    private static String promptUserForResourceName(String prompt) {
+        logger.info(prompt);
+        String resourceName = scanner.nextLine();
+        String regex = "[0-9a-zA-Z-_.]+";
+        if (!resourceName.matches(regex)) {
+            logger.info("Invalid resource name. Please use a name that matches the pattern " + regex + ".");
+            return promptUserForResourceName(prompt);
+        }
+        return resourceName;
+    }
+
+    /**
+     * Prompts the user for an integer input and returns the integer value.
+     *
+     * @param prompt the message to be displayed to the user when prompting for input
+     * @return the integer value entered by the user
+     */
+    private static int promptUserForInteger(String prompt) {
+        logger.info(prompt);
+        String stringResponse = scanner.nextLine();
+        if (stringResponse == null || stringResponse.trim().isEmpty() || !isInteger(stringResponse)) {
+            logger.info("Invalid integer.");
+            return promptUserForInteger(prompt);
+        }
+        return Integer.parseInt(stringResponse);
+    }
+
+    /**
+     * Checks if the given string represents a valid integer.
+     *
+     * @param str the string to be checked
+     * @return {@code true} if the string represents a valid integer, {@code false} otherwise
+     */
+    private static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
+// snippet-end:[scheduler.javav2.scenario.main]
