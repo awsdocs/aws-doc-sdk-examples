52abee06d7df077878da4585c475527e285676db
Corey Pyle <corepyle@amazon.com>
2024-10-15 15:39:50 -0400

    JavaScript (v3): Fix duplicate SPDX header.

--- javascriptv3/example_code/medical-imaging/actions/update-image-set-metadata.js
+++ javascriptv3/example_code/medical-imaging/actions/update-image-set-metadata.js
@@ -1,9 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
 
 import { fileURLToPath } from "node:url";
 
