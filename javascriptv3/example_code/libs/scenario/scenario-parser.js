efc7fd423c1aa4e64cb2288236318211b9c02c92
Corey Pyle <corepyle@amazon.com>
2024-10-21 16:05:50 -0400

    JavaScript (v3): Scenarios - Add metadata to parseScenarioArgs input and refactor.

--- javascriptv3/example_code/libs/scenario/scenario-parser.js
+++ javascriptv3/example_code/libs/scenario/scenario-parser.js
@@ -2,74 +2,67 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { parseArgs } from "node:util";
+import { printManPage } from "../utils/util-node.js";
+import { logger } from "../utils/util-log.js";
 
 /**
  * @param {Record<string, import('./scenario.js').Scenario>} scenarios
+ * @param {{name: string, synopsis: string, description: string}} [info] - High level info describing the suite of scenarios.
  */
-export function parseScenarioArgs(scenarios) {
-  const help = `Usage:
-node . -s <${Object.keys(scenarios).join("|")}>
-node . -h
-
-Options:
-[-s|--scenario, <scenario>] [-h|--help] [-y|--yes] [-v|--verbose]
-
--h, --help        Show this help message.
--s, --scenario    The name of a scenario to run.
--y, --yes         Assume "yes" to all prompts.
--v, --verbose     Show debug information.
-`;
-
-  const { values } = parseArgs({
-    options: {
-      help: {
-        type: "boolean",
-        short: "h",
-      },
-      scenario: {
-        short: "s",
-        type: "string",
-      },
-      yes: {
-        short: "y",
-        type: "boolean",
-      },
-      verbose: {
-        short: "v",
-        type: "boolean",
-      },
+export const parseScenarioArgs = (
+  scenarios,
+  { name = "", synopsis = "", description = "" } = {},
+) => {
+  const options = {
+    help: {
+      type: "boolean",
+      short: "h",
     },
-  });
+    scenario: {
+      short: "s",
+      type: "string",
+    },
+    yes: {
+      short: "y",
+      type: "boolean",
+    },
+    verbose: {
+      short: "v",
+      type: "boolean",
+    },
+  };
+
+  const { values } = parseArgs({ options });
+  const helpPage = () => {
+    printManPage(options, {
+      name,
+      synopsis: synopsis ?? `node . -s <${Object.keys(scenarios).join("|")}>`,
+      description,
+    });
+  };
 
   if (values.help) {
-    console.log(help);
+    helpPage();
     return;
   }
 
   if (!values.scenario) {
-    console.log(`Missing required argument: -s, --scenario\n\n${help}`);
+    logger.error("Missing required argument: -s, --scenario");
+    helpPage();
     return;
   }
 
   if (!(values.scenario in scenarios)) {
-    throw new Error(`Invalid scenario: ${values.scenario}\n${help}`);
+    logger.error(`Invalid scenario: ${values.scenario}`);
   }
 
   if (values.verbose) {
-    console.log(
-      `[DEBUG ${new Date().toISOString()}] Running scenario: ${
-        scenarios[values.scenario].name
-      }`,
-    );
-    console.log(
-      `[DEBUG ${new Date().toISOString()}] State: ${JSON.stringify(
-        scenarios[values.scenario].state,
-      )}`,
-    );
+    logger.debug(`Running scenario: ${scenarios[values.scenario].name}`);
+    logger.debug(`State: ${JSON.stringify(scenarios[values.scenario].state)}`);
   }
 
   return scenarios[values.scenario].run({
     confirmAll: values.yes,
     verbose: values.verbose,
   });
-}
+};
