3743fb3450915455e195387a8e36f1ff3694d40d
Dennis Traub <dennist.traub@gmail.com>
2024-11-12 11:38:24 +0100

    Remove Python, Go, CSharp, and JavaScript

--- javascriptv3/example_code/bedrock-runtime/tests/meta_llama.integration.test.js
+++ javascriptv3/example_code/bedrock-runtime/tests/meta_llama.integration.test.js
@@ -5,31 +5,8 @@ import { describe, expect, it, vi } from "vitest";
 
 /**
  * Integration tests for:
- * - models/meta/llama2/*.js
  * - models/meta/llama3/*.js
  */
-
-describe("Running the Llama2 InvokeModel quickstart", () => {
-  it("should run and log the model's response", async () => {
-    const log = vi.spyOn(console, "log").mockImplementation(() => {});
-    await import("../models/metaLlama/llama2/invoke_model_quickstart.js");
-    expect(log).toHaveBeenCalledTimes(1);
-    log.mockRestore();
-  });
-});
-
-describe("Running the Llama2 InvokeModelWithResponseStream quickstart", () => {
-  it("should run and log the model's response", async () => {
-    const write = vi
-      .spyOn(process.stdout, "write")
-      .mockImplementation(() => {});
-    await import(
-      "../models/metaLlama/llama2/invoke_model_with_response_stream_quickstart.js"
-    );
-    expect(write).toHaveBeenCalled();
-    write.mockRestore();
-  });
-});
 
 describe("Running the Llama3 InvokeModel quickstart", () => {
   it("should run and log the model's response", async () => {
