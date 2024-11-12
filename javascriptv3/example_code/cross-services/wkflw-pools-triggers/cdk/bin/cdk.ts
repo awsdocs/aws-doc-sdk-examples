dbd8f9d33c26f1c829e2830782c22553d6126c51
Corey Pyle <corepyle@amazon.com>
2024-11-12 10:24:54 -0500

    JavaScript (v3): Cognito - Add SPDX header to pools/triggers CDK.

--- javascriptv3/example_code/cross-services/wkflw-pools-triggers/cdk/bin/cdk.ts
+++ javascriptv3/example_code/cross-services/wkflw-pools-triggers/cdk/bin/cdk.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { PoolsAndTriggersStack } from "../lib/stack";
