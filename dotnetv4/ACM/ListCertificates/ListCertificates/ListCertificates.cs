86315630e459573e182b0123aa7d1b66f1b68f71
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-11-15 15:31:26 -0600

    WIP on project updates.

--- dotnetv4/ACM/ListCertificates/ListCertificates/ListCertificates.cs
+++ dotnetv4/ACM/ListCertificates/ListCertificates/ListCertificates.cs
@@ -5,51 +5,46 @@
 
 using System;
 using System.Threading.Tasks;
-using Amazon;
 using Amazon.CertificateManager;
 using Amazon.CertificateManager.Model;
 
-namespace ListCertificates
+namespace ListCertificates;
+
+// The following example retrieves and displays a list of the
+// certificates defined for the default account using the AWS
+// Certificate Manager (ACM) service.
+public class ListCertificates
 {
-    // The following example retrieves and displays a list of the
-    // certificates defined for the default account using the AWS
-    // Certificate Manager (ACM) service.
-    class ListCertificates
+    private static AmazonCertificateManagerClient _client;
+
+    static void Main(string[] args)
     {
-        // Specify your AWS Region (an example Region is shown).
+        _client = new AmazonCertificateManagerClient();
+        var certificateList = ListCertificatesResponseAsync(client: _client);
 
-        private static readonly RegionEndpoint ACMRegion = RegionEndpoint.USEast1;
-        private static AmazonCertificateManagerClient _client;
+        Console.WriteLine("Certificate Summary List\n");
 
-        static void Main(string[] args)
+        foreach (var certificate in certificateList.Result.CertificateSummaryList)
         {
-            _client = new AmazonCertificateManagerClient(ACMRegion);
-            var certificateList = ListCertificatesResponseAsync(client: _client);
-
-            Console.WriteLine("Certificate Summary List\n");
-
-            foreach (var certificate in certificateList.Result.CertificateSummaryList)
-            {
-                Console.WriteLine($"Certificate Domain: {certificate.DomainName}");
-                Console.WriteLine($"Certificate ARN: {certificate.CertificateArn}\n");
-            }
+            Console.WriteLine($"Certificate Domain: {certificate.DomainName}");
+            Console.WriteLine($"Certificate ARN: {certificate.CertificateArn}\n");
         }
+    }
 
-        /// <summary>
-        /// Retrieves a list of the certificates defined in this Region.
-        /// </summary>
-        /// <param name="client">The ACM client object passed to the
-        /// ListCertificateResAsync method call.</param>
-        /// <param name="request"></param>
-        /// <returns>The ListCertificatesResponse.</returns>
-        static async Task<ListCertificatesResponse> ListCertificatesResponseAsync(
-            AmazonCertificateManagerClient client)
-        {
-            var request = new ListCertificatesRequest();
+    /// <summary>
+    /// Retrieves a list of the certificates defined in this Region.
+    /// </summary>
+    /// <param name="client">The ACM client object passed to the
+    /// ListCertificateResAsync method call.</param>
+    /// <param name="request"></param>
+    /// <returns>The ListCertificatesResponse.</returns>
+    static async Task<ListCertificatesResponse> ListCertificatesResponseAsync(
+        AmazonCertificateManagerClient client)
+    {
+        var request = new ListCertificatesRequest();
 
-            var response = await client.ListCertificatesAsync(request);
-            return response;
-        }
+        var response = await client.ListCertificatesAsync(request);
+        return response;
     }
 }
 
