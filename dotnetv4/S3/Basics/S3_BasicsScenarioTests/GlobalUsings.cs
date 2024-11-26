97beaa8d91886eac10fca52a419eb210b8c28cd8
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-11-26 09:34:36 -0600

    Cleanup Aurora folder for updated versions.

--- dotnetv4/S3/Basics/S3_BasicsScenarioTests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Global using directives
-
-global using Amazon.S3;
-global using Amazon.S3.Model;
-global using Moq;
-global using Xunit;
