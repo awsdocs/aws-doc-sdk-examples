e1a45659158f79170dc4593bd0965825b85e6457
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-11-26 09:34:36 -0600

    Cleanup Aurora folder for updated versions.

--- dotnetv4/EC2/Scenarios/EC2_Basics/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-global using Amazon.EC2;
-global using Amazon.SimpleSystemsManagement;
-global using EC2Actions;
-global using Microsoft.Extensions.DependencyInjection;
--- dotnetv4/IAM/Actions/Usings.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// snippet-start:[IAM.dotnetv4.Actions.Usings]
-global using Amazon.IdentityManagement;
-global using Amazon.IdentityManagement.Model;
-
-// snippet-end:[IAM.dotnetv4.Actions.Usings]
