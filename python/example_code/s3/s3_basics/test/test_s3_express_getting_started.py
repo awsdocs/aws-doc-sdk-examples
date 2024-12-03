b892303a71845e7359b7bffe568821b0f6fb0f5c
Steven Meyer <108885656+meyertst-aws@users.noreply.github.com>
2024-12-03 18:00:43 -0500

    30 tests done

--- python/example_code/s3/s3_basics/test/test_s3_express_getting_started.py
+++ python/example_code/s3/s3_basics/test/test_s3_express_getting_started.py
@@ -20,16 +20,33 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(script_dir, ".."))
 import s3_express_getting_started
 
+number_of_uploads = 10
 
-@pytest.mark.parametrize(
-    "error_code, stop_on_index",
-    [
+stop_on_index = [
         ("TESTERROR-stub_create_vpc", 0),
         ("TESTERROR-stub_describe_route_tables", 1),
         ("TESTERROR-stub_create_vpc_endpoint", 2),
         ("TESTERROR-stub_create_stack", 3),
         ("TESTERROR-stub_describe_stacks", 4),
-    ],
+        ("TESTERROR-stub_create_access_key_regular", 5),
+        ("TESTERROR-stub_create_access_key_directory", 6),
+        ("TESTERROR-stub_describe_availability_zones", 7),
+        ("TESTERROR-stub_create_bucket_directory", 8),
+        ("TESTERROR-stub_create_bucket_regular", 9),
+        ("TESTERROR-stub_put_object", 10),
+        ("TESTERROR-stub_create_session", 11),
+        ("TESTERROR-stub_copy_object", 12),
+    ]
+
+for i in range(len(stop_on_index), len(stop_on_index) + number_of_uploads):
+    stop_on_index.append((f"TESTERROR-stub_get_object_directory", i))
+
+for i in range(len(stop_on_index), len(stop_on_index) + number_of_uploads):
+    stop_on_index.append((f"TESTERROR-stub_get_object_regular", i))
+
+@pytest.mark.parametrize(
+    "error_code, stop_on_index",
+    stop_on_index,
 )
 def test_s3_express_scenario(
     make_stubber, stub_runner, error_code   , stop_on_index, monkeypatch
@@ -67,8 +84,26 @@ def test_s3_express_scenario(
         {"OutputKey": "ExpressUser", "OutputValue": express_user_name},
     ]
 
+    availability_zone_filter = [{"Name": "region-name", "Values": [region]}]
+    availability_zone_ids = ["use1-az2"]
+
+    bucket_name_prefix = "amzn-s3-demo-bucket"
+    directory_bucket_name = f"{bucket_name_prefix}-{my_uuid}--{availability_zone_ids[0]}--x-s3"
+    regular_bucket_name = f"{bucket_name_prefix}-regular-{my_uuid}"
+
+    directory_bucket_configuration = { 'Bucket': { 'Type' : 'Directory',
+                                      'DataRedundancy' : 'SingleAvailabilityZone'},
+                          'Location' : { 'Name' : availability_zone_ids[0],
+                                         'Type' :  'AvailabilityZone'}
+                          }
+
+    object_name = "basic-text-object"
     inputs = [
         "y",
+        bucket_name_prefix,
+        "1",
+        "y",
+        number_of_uploads
     ]
     monkeypatch.setattr("builtins.input", lambda x: inputs.pop(0))
 
@@ -81,6 +116,20 @@ def test_s3_express_scenario(
         runner.add(ec2_stubber.stub_create_vpc_endpoint, vpc_id, route_table_id, service_name)
         runner.add(cloud_formation_stubber.stub_create_stack, stack_name, template, ["CAPABILITY_NAMED_IAM"], stack_id)
         runner.add(cloud_formation_stubber.stub_describe_stacks, stack_name, "CREATE_COMPLETE", outputs)
+        runner.add(iam_stubber.stub_create_access_key, regular_user_name)
+        runner.add(iam_stubber.stub_create_access_key, express_user_name)
+        runner.add(ec2_stubber.stub_describe_availability_zones, availability_zone_ids, availability_zone_filter, availability_zone_ids)
+        runner.add(s3_stubber.stub_create_bucket, directory_bucket_name, bucket_configuration = directory_bucket_configuration)
+        runner.add(s3_stubber.stub_create_bucket, regular_bucket_name)
+        runner.add (s3_stubber.stub_put_object, regular_bucket_name, object_name, "Look Ma, I'm a bucket!")
+        runner.add(s3_stubber.stub_create_session, directory_bucket_name)
+        runner.add (s3_stubber.stub_copy_object, regular_bucket_name, object_name, directory_bucket_name, object_name)
+
+        for _ in range(number_of_uploads):
+            runner.add (s3_stubber.stub_get_object, directory_bucket_name, object_name)
+
+        for _ in range(number_of_uploads):
+            runner.add(s3_stubber.stub_get_object, regular_bucket_name, object_name)
 
     def mock_wait(self, **kwargs):
         return
