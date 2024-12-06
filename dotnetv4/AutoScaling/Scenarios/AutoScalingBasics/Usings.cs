f7fb9d80eff759741bfc71f57ec2ab28cbbb2d8e
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-12-06 10:05:10 -0600

    .NET v4 preview: adding example folders for AutoScaling and Aurora (#7106)

---
+++ dotnetv4/AutoScaling/Scenarios/AutoScalingBasics/Usings.cs
@@ -0,0 +1,15 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[AutoScaling.dotnetv4.AutoScalingBasics.Usings]
+global using Amazon.AutoScaling;
+global using Amazon.AutoScaling.Model;
+global using Amazon.CloudWatch;
+global using AutoScalingActions;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Console;
+global using Microsoft.Extensions.Logging.Debug;
+
+// snippet-end:[AutoScaling.dotnetv4.AutoScalingBasics.Usings]
