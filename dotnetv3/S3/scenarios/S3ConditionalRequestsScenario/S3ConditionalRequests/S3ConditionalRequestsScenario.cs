d8bcbb31f1b75298f03936780a3af3b29c4260d0
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2024-12-24 10:45:31 -0600

    .NET v3: S3 Conditional Requests Feature Scenario Follow  (#7174)

---
+++ dotnetv3/S3/scenarios/S3ConditionalRequestsScenario/S3ConditionalRequests/S3ConditionalRequestsScenario.cs
@@ -0,0 +1,348 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[S3ConditionalRequests.dotnetv3.Scenario]
+
+using Amazon.S3;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace S3ConditionalRequestsScenario;
+
+public static class S3ConditionalRequestsScenario
+{
+    /*
+    Before running this .NET code example, set up your development environment, including your credentials.
+
+    This example demonstrates the use of conditional requests for S3 operations.
+    You can use conditional requests to add preconditions to S3 read requests to return or copy
+    an object based on its Entity tag (ETag), or last modified date. 
+    You can use a conditional write requests to prevent overwrites by ensuring 
+    there is no existing object with the same key. 
+   */
+
+    public static S3ActionsWrapper _s3ActionsWrapper = null!;
+    public static IConfiguration _configuration = null!;
+    public static string _resourcePrefix = null!;
+    public static string _sourceBucketName = null!;
+    public static string _destinationBucketName = null!;
+    public static string _sampleObjectKey = null!;
+    public static string _sampleObjectEtag = null!;
+    public static bool _interactive = true;
+
+
+    public static async Task Main(string[] args)
+    {
+        // Set up dependency injection for the Amazon service.
+        using var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+                logging.AddFilter("System", LogLevel.Debug)
+                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Information)
+                    .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Trace))
+            .ConfigureServices((_, services) =>
+                services.AddAWSService<IAmazonS3>()
+                    .AddTransient<S3ActionsWrapper>()
+            )
+            .Build();
+
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("settings.json") // Load settings from .json file.
+            .AddJsonFile("settings.local.json",
+                true) // Optionally, load local settings.
+            .Build();
+
+        ServicesSetup(host);
+
+        try
+        {
+            Console.WriteLine(new string('-', 80));
+            Console.WriteLine("Welcome to the Amazon Simple Storage Service (S3) Conditional Requests Feature Scenario.");
+            Console.WriteLine(new string('-', 80));
+            ConfigurationSetup();
+            _sampleObjectEtag = await Setup(_sourceBucketName, _destinationBucketName, _sampleObjectKey);
+
+            await DisplayDemoChoices(_sourceBucketName, _destinationBucketName, _sampleObjectKey, _sampleObjectEtag, 0);
+
+            Console.WriteLine(new string('-', 80));
+            Console.WriteLine("Cleaning up resources.");
+            Console.WriteLine(new string('-', 80));
+            await Cleanup(true);
+
+            Console.WriteLine(new string('-', 80));
+            Console.WriteLine("Amazon S3 Conditional Requests Feature Scenario is complete.");
+            Console.WriteLine(new string('-', 80));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(new string('-', 80));
+            Console.WriteLine($"There was a problem: {ex.Message}");
+            await CleanupScenario(_sourceBucketName, _destinationBucketName);
+            Console.WriteLine(new string('-', 80));
+        }
+    }
+
+    /// <summary>
+    /// Populate the services for use within the console application.
+    /// </summary>
+    /// <param name="host">The services host.</param>
+    private static void ServicesSetup(IHost host)
+    {
+        _s3ActionsWrapper = host.Services.GetRequiredService<S3ActionsWrapper>();
+    }
+
+    /// <summary>
+    /// Any setup operations needed.
+    /// </summary>
+    public static void ConfigurationSetup()
+    {
+        _resourcePrefix = _configuration["resourcePrefix"] ?? "dotnet-example";
+
+        _sourceBucketName = _resourcePrefix + "-source";
+        _destinationBucketName = _resourcePrefix + "-dest";
+        _sampleObjectKey = _resourcePrefix + "-sample-object.txt";
+    }
+
+    /// <summary>
+    /// Sets up the scenario by creating a source and destination bucket, and uploading a test file to the source bucket.
+    /// </summary>
+    /// <param name="sourceBucket">The name of the source bucket.</param>
+    /// <param name="destBucket">The name of the destination bucket.</param>
+    /// <param name="objectKey">The name of the test file to add to the source bucket.</param>
+    /// <returns>The ETag of the uploaded test file.</returns>
+    public static async Task<string> Setup(string sourceBucket, string destBucket, string objectKey)
+    {
+        Console.WriteLine(
+            "\nFor this scenario, we will use the AWS SDK for .NET to create several S3\n" +
+            "buckets and files to demonstrate working with S3 conditional requests.\n" +
+            "This example demonstrates the use of conditional requests for S3 operations.\r\n" +
+            "You can use conditional requests to add preconditions to S3 read requests to return or copy\r\n" +
+            "an object based on its Entity tag (ETag), or last modified date. \r\n" +
+            "You can use a conditional write requests to prevent overwrites by ensuring \r\n" +
+            "there is no existing object with the same key. \r\n\r\n" +
+            "This example will allow you to perform conditional reads\r\n" +
+            "and writes that will succeed or fail based on your selected options.\r\n\r\n" +
+            "Sample buckets and a sample object will be created as part of the example.");
+
+        Console.WriteLine(new string('-', 80));
+        Console.WriteLine("Press Enter when you are ready to start.");
+        if (_interactive)
+            Console.ReadLine();
+
+        await _s3ActionsWrapper.CreateBucketWithName(sourceBucket);
+        await _s3ActionsWrapper.CreateBucketWithName(destBucket);
+
+        var eTag = await _s3ActionsWrapper.PutObjectConditional(objectKey, sourceBucket,
+            "Test file content.");
+
+        return eTag;
+    }
+
+    /// <summary>
+    /// Cleans up the scenario by deleting the source and destination buckets.
+    /// </summary>
+    /// <param name="sourceBucket">The name of the source bucket.</param>
+    /// <param name="destBucket">The name of the destination bucket.</param>
+    public static async Task CleanupScenario(string sourceBucket, string destBucket)
+    {
+        await _s3ActionsWrapper.CleanupBucketByName(sourceBucket);
+        await _s3ActionsWrapper.CleanupBucketByName(destBucket);
+    }
+
+    /// <summary>
+    /// Displays a list of the objects in the test buckets.
+    /// </summary>
+    /// <param name="sourceBucket">The name of the source bucket.</param>
+    /// <param name="destBucket">The name of the destination bucket.</param>
+    public static async Task DisplayBuckets(string sourceBucket, string destBucket)
+    {
+        await _s3ActionsWrapper.ListBucketContentsByName(sourceBucket);
+        await _s3ActionsWrapper.ListBucketContentsByName(destBucket);
+    }
+
+    /// <summary>
+    /// Displays the menu of conditional request options for the user.
+    /// </summary>
+    /// <param name="sourceBucket">The name of the source bucket.</param>
+    /// <param name="destBucket">The name of the destination bucket.</param>
+    /// <param name="objectKey">The key of the test object in the source bucket.</param>
+    /// <param name="etag">The ETag of the test object in the source bucket.</param>
+    public static async Task DisplayDemoChoices(string sourceBucket, string destBucket, string objectKey, string etag, int defaultChoice)
+    {
+        var actions = new[]
+        {
+            "Print a list of bucket items.",
+            "Perform a conditional read.",
+            "Perform a conditional copy.",
+            "Perform a conditional write.",
+            "Clean up and exit."
+        };
+
+        var conditions = new[]
+        {
+            "If-Match: using the object's ETag. This condition should succeed.",
+            "If-None-Match: using the object's ETag. This condition should fail.",
+            "If-Modified-Since: using yesterday's date. This condition should succeed.",
+            "If-Unmodified-Since: using yesterday's date. This condition should fail."
+        };
+
+        var conditionTypes = new[]
+        {
+            S3ConditionType.IfMatch,
+            S3ConditionType.IfNoneMatch,
+            S3ConditionType.IfModifiedSince,
+            S3ConditionType.IfUnmodifiedSince,
+        };
+
+        var yesterdayDate = DateTime.UtcNow.AddDays(-1);
+
+        int choice;
+        while ((choice = GetChoiceResponse("\nExplore the S3 conditional request  features by selecting one of the following choices:", actions, defaultChoice)) != 4)
+        {
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Listing the objects and buckets.");
+                    await DisplayBuckets(sourceBucket, destBucket);
+                    break;
+                case 1:
+                    int conditionTypeIndex = GetChoiceResponse("Perform a conditional read:", conditions, 1);
+                    if (conditionTypeIndex == 0 || conditionTypeIndex == 1)
+                    {
+                        await _s3ActionsWrapper.GetObjectConditional(objectKey, sourceBucket, conditionTypes[conditionTypeIndex], null, _sampleObjectEtag);
+                    }
+                    else if (conditionTypeIndex == 2 || conditionTypeIndex == 3)
+                    {
+                        await _s3ActionsWrapper.GetObjectConditional(objectKey, sourceBucket, conditionTypes[conditionTypeIndex], yesterdayDate);
+                    }
+                    break;
+                case 2:
+                    int copyConditionTypeIndex = GetChoiceResponse("Perform a conditional copy:", conditions, 1);
+                    string destKey = GetStringResponse("Enter an object key:", "sampleObjectKey");
+                    if (copyConditionTypeIndex == 0 || copyConditionTypeIndex == 1)
+                    {
+                        await _s3ActionsWrapper.CopyObjectConditional(objectKey, destKey, sourceBucket, destBucket, conditionTypes[copyConditionTypeIndex], null, etag);
+                    }
+                    else if (copyConditionTypeIndex == 2 || copyConditionTypeIndex == 3)
+                    {
+                        await _s3ActionsWrapper.CopyObjectConditional(objectKey, destKey, sourceBucket, destBucket, conditionTypes[copyConditionTypeIndex], yesterdayDate);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Perform a conditional write using IfNoneMatch condition on the object key.");
+                    Console.WriteLine("If the key is a duplicate, the write will fail.");
+                    string newObjectKey = GetStringResponse("Enter an object key:", "newObjectKey");
+                    await _s3ActionsWrapper.PutObjectConditional(newObjectKey, sourceBucket, "Conditional write example data.");
+                    break;
+            }
+
+            if (!_interactive)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Proceeding to cleanup.");
+    }
+
+    // <summary>
+    /// Clean up the resources from the scenario.
+    /// </summary>
+    /// <param name="interactive">True to run as interactive.</param>
+    /// <returns>True if successful.</returns>
+    public static async Task<bool> Cleanup(bool interactive)
+    {
+        Console.WriteLine(new string('-', 80));
+
+        if (!interactive || GetYesNoResponse("Do you want to clean up all files and buckets? (y/n) "))
+        {
+            await _s3ActionsWrapper.CleanUpBucketByName(_sourceBucketName);
+            await _s3ActionsWrapper.CleanUpBucketByName(_destinationBucketName);
+
+        }
+        else
+        {
+            Console.WriteLine(
+                "Ok, we'll leave the resources intact.\n" +
+                "Don't forget to delete them when you're done with them or you might incur unexpected charges."
+            );
+        }
+
+        Console.WriteLine(new string('-', 80));
+        return true;
+    }
+
+    /// <summary>
+    /// Helper method to get a yes or no response from the user.
+    /// </summary>
+    /// <param name="question">The question string to print on the console.</param>
+    /// <returns>True if the user responds with a yes.</returns>
+    private static bool GetYesNoResponse(string question)
+    {
+        Console.WriteLine(question);
+        var ynResponse = Console.ReadLine();
+        var response = ynResponse != null && ynResponse.Equals("y", StringComparison.InvariantCultureIgnoreCase);
+        return response;
+    }
+
+    /// <summary>
+    /// Helper method to get a choice response from the user.
+    /// </summary>
+    /// <param name="question">The question string to print on the console.</param>
+    /// <param name="choices">The choices to print on the console.</param>
+    /// <returns>The index of the selected choice</returns>
+    private static int GetChoiceResponse(string? question, string[] choices, int defaultChoice)
+    {
+        if (question != null)
+        {
+            Console.WriteLine(question);
+
+            for (int i = 0; i < choices.Length; i++)
+            {
+                Console.WriteLine($"\t{i + 1}. {choices[i]}");
+            }
+        }
+
+        if (!_interactive)
+            return defaultChoice;
+
+        var choiceNumber = 0;
+        while (choiceNumber < 1 || choiceNumber > choices.Length)
+        {
+            var choice = Console.ReadLine();
+            Int32.TryParse(choice, out choiceNumber);
+        }
+
+        return choiceNumber - 1;
+    }
+
+    /// <summary>
+    /// Get a string response from the user.
+    /// </summary>
+    /// <param name="question">The question to print.</param>
+    /// <param name="defaultAnswer">A default answer to use when not interactive.</param>
+    /// <returns>The string response.</returns>
+    public static string GetStringResponse(string? question, string defaultAnswer)
+    {
+        string? answer = "";
+        if (_interactive)
+        {
+            do
+            {
+                Console.WriteLine(question);
+                answer = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(answer));
+        }
+        else
+        {
+            answer = defaultAnswer;
+        }
+
+        return answer;
+    }
+}
+// snippet-end:[S3ConditionalRequests.dotnetv3.Scenario]
