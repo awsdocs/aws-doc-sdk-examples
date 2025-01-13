7c5d254aceec722c065a8b86648597d03f54e32d
Corey Pyle <corepyle@amazon.com>
2025-01-13 14:43:12 -0500

    Add missing SPDX header.

--- javascriptv3/example_code/cross-services/textract-react/src/Config.js
+++ javascriptv3/example_code/cross-services/textract-react/src/Config.js
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export const Config = {
   ConfigError:
     "Demo resources not initialized. You must deploy AWS resources and demo elements before running this application. See the README for details.",
