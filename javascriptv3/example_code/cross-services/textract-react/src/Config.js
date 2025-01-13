9cd5430696f0e058e28a104124c3b3c88bd5308a
Corey Pyle <corepyle@amazon.com>
2025-01-13 12:45:38 -0500

    Add missing SPDX header and bump node version for linter.

--- javascriptv3/example_code/cross-services/textract-react/src/Config.js
+++ javascriptv3/example_code/cross-services/textract-react/src/Config.js
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export const Config = {
   StackName: "textract-react",
   DefaultImageName: "default_document_3.png",
