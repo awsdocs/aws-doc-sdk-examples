11e4ab0670bfe7983dc774002c657f0c1e4ea908
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-01-16 16:38:05 -0600

    Fix invalid paths.

--- python/example_code/firehose/scenarios/firehose-put-actions/config.py
+++ python/example_code/firehose/scenarios/firehose-put-actions/config.py
@@ -5,7 +5,7 @@ class Config:
         self.delivery_stream_name = "ENTER YOUR DELIVERY STREAM NAME HERE"
         self.region = "us-east-1"
         self.sample_data_file = (
-            "../../../../../scenarios/features/firehose//resources/sample_records.json"
+            "../../../../../scenarios/features/firehose/resources/sample_records.json"
         )
 
 
