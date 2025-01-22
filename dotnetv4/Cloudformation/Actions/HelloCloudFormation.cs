9143d2f8dce4581bdfaf9f0baa51bf91b4eb3fcc
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-01-22 08:22:17 -0600

    .NET v4 folders: adding CloudFormation and CloudWatch (#7197)

---
+++ dotnetv4/Cloudformation/Actions/HelloCloudFormation.cs
@@ -0,0 +1,116 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// snippet-start:[CloudFormation.dotnetv4.CloudFormationActions.HelloCloudFormation]
+using Amazon.CloudFormation;
+using Amazon.CloudFormation.Model;
+using Amazon.Runtime;
+
+namespace CloudFormationActions;
+
+public static class HelloCloudFormation
+{
+    public static IAmazonCloudFormation _amazonCloudFormation = null!;
+
+    static async Task Main(string[] args)
+    {
+        // Create the CloudFormation client
+        _amazonCloudFormation = new AmazonCloudFormationClient();
+        Console.WriteLine($"\nIn Region: {_amazonCloudFormation.Config.RegionEndpoint}");
+
+        // List the resources for each stack
+        await ListResources();
+    }
+
+    /// <summary>
+    /// Method to list stack resources and other information.
+    /// </summary>
+    /// <returns>True if successful.</returns>
+    public static async Task<bool> ListResources()
+    {
+        try
+        {
+            Console.WriteLine("Getting CloudFormation stack information...");
+
+            // Get all stacks using the stack paginator.
+            var paginatorForDescribeStacks =
+                _amazonCloudFormation.Paginators.DescribeStacks(
+                    new DescribeStacksRequest());
+            if (paginatorForDescribeStacks.Stacks != null)
+            {
+                await foreach (Stack stack in paginatorForDescribeStacks.Stacks)
+                {
+                    // Basic information for each stack
+                    Console.WriteLine(
+                        "\n------------------------------------------------");
+                    Console.WriteLine($"\nStack: {stack.StackName}");
+                    Console.WriteLine($"  Status: {stack.StackStatus.Value}");
+                    Console.WriteLine($"  Created: {stack.CreationTime}");
+
+                    // The tags of each stack (etc.)
+                    if (stack.Tags != null && stack.Tags.Count > 0)
+                    {
+                        Console.WriteLine("  Tags:");
+                        foreach (Tag tag in stack.Tags)
+                            Console.WriteLine($"    {tag.Key}, {tag.Value}");
+                    }
+
+                    // The resources of each stack
+                    DescribeStackResourcesResponse responseDescribeResources =
+                        await _amazonCloudFormation.DescribeStackResourcesAsync(
+                            new DescribeStackResourcesRequest
+                            {
+                                StackName = stack.StackName
+                            });
+                    if (responseDescribeResources.StackResources != null && responseDescribeResources.StackResources.Count > 0)
+                    {
+                        Console.WriteLine("  Resources:");
+                        foreach (StackResource resource in responseDescribeResources
+                                     .StackResources)
+                            Console.WriteLine(
+                                $"    {resource.LogicalResourceId}: {resource.ResourceStatus}");
+                    }
+                }
+            }
+
+            Console.WriteLine("\n------------------------------------------------");
+            return true;
+        }
+        catch (AmazonCloudFormationException ex)
+        {
+            Console.WriteLine("Unable to get stack information:\n" + ex.Message);
+            return false;
+        }
+        catch (AmazonServiceException ex)
+        {
+            if (ex.Message.Contains("Unable to get IAM security credentials"))
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("If you are usnig SSO, be sure to install" +
+                                  " the AWSSDK.SSO and AWSSDK.SSOOIDC packages.");
+            }
+            else
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return false;
+        }
+        catch (ArgumentNullException ex)
+        {
+            if (ex.Message.Contains("Options property cannot be empty: ClientName"))
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("If you are using SSO, have you logged in?");
+            }
+            else
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return false;
+        }
+    }
+}// snippet-end:[CloudFormation.dotnetv4.CloudFormationActions.HelloCloudFormation]
