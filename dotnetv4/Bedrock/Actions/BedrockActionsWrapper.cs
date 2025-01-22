cdbb9536b0ffe04b293cb319642dae5272f82d38
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-01-22 09:30:29 -0600

    Adding files.

---
+++ dotnetv4/Bedrock/Actions/BedrockActionsWrapper.cs
@@ -0,0 +1,8 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 
+// SPDX-License-Identifier: Apache-2.0
+
+namespace BedrockActions;
+
+public class BedrockActionsWrapper
+{
+}
