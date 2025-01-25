6e347955f7054b27dad65deab868b105d9130d05
scmacdon <scmacdon@amazon.com>
2025-01-25 13:08:54 -0500

    updated the Kotlin SDK build number in BOM

--- kotlin/services/polly/build.gradle.kts
+++ kotlin/services/polly/build.gradle.kts
@@ -27,9 +27,10 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:polly:1.3.112")
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
-    implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
+    implementation(platform("aws.sdk.kotlin:bom:1.3.112"))
+    implementation("aws.sdk.kotlin:polly")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp")
+    implementation("aws.smithy.kotlin:http-client-engine-crt")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("com.googlecode.soundlibs:jlayer:1.0.1.4")
