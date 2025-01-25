3d25f3fff295c73dd9193dc5a3427286495baf6e
scmacdon <scmacdon@amazon.com>
2025-01-25 13:11:28 -0500

    updated the Kotlin SDK build number in BOM

--- kotlin/services/rds/build.gradle.kts
+++ kotlin/services/rds/build.gradle.kts
@@ -27,10 +27,11 @@ repositories {
 }
 apply(plugin = "org.jlleitschuh.gradle.ktlint")
 dependencies {
-    implementation("aws.sdk.kotlin:rds:1.3.112")
-    implementation("aws.sdk.kotlin:secretsmanager:1.3.112")
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:0.30.0")
-    implementation("aws.smithy.kotlin:http-client-engine-crt:0.30.0")
+    implementation(platform("aws.sdk.kotlin:bom:1.3.112"))
+    implementation("aws.sdk.kotlin:rds")
+    implementation("aws.sdk.kotlin:secretsmanager")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp")
+    implementation("aws.smithy.kotlin:http-client-engine-crt")
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
