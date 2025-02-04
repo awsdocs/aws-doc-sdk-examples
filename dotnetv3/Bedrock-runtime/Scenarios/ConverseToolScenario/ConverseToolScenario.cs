c7af517bd122b83420762f3505eb68895e5c35ce
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-02-04 09:34:45 -0600

    Update to tool scenario.

--- dotnetv3/Bedrock-runtime/Scenarios/ConverseToolScenario/ConverseToolScenario.cs
+++ dotnetv3/Bedrock-runtime/Scenarios/ConverseToolScenario/ConverseToolScenario.cs
@@ -2,12 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using Amazon.BedrockRuntime;
-using Microsoft.Extensions.Configuration;
+using Amazon.BedrockRuntime.Model;
+using Amazon.Runtime.Documents;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
-using Microsoft.Extensions.Logging.Debug;
 
 namespace ConverseToolScenario;
 
@@ -16,66 +18,74 @@ public static class ConverseToolScenario
     /*
     Before running this .NET code example, set up your development environment, including your credentials.
 
-    This example demonstrates the use TODO
+    This demo illustrates a tool use scenario using Amazon Bedrock's Converse API and a weather tool.
+    The script interacts with a foundation model on Amazon Bedrock to provide weather information based on user
+    input. It uses the Open-Meteo API (https://open-meteo.com) to retrieve current weather data for a given location.
    */
 
     public static BedrockActionsWrapper _bedrockActionsWrapper = null!;
-    public static IConfiguration _configuration = null!;
-    public static string _resourcePrefix = null!;
-    public static string _sourceBucketName = null!;
-    public static string _destinationBucketName = null!;
-    public static string _sampleObjectKey = null!;
-    public static string _sampleObjectEtag = null!;
+    public static WeatherTool _weatherTool = null!;
     public static bool _interactive = true;
 
+    private static string model_id = "anthropic.claude-3-sonnet-20240229-v1:0";
+
+    private static string system_prompt = @"
+        You are a weather assistant that provides current weather data for user-specified locations using only
+        the Weather_Tool, which expects latitude and longitude. Infer the coordinates from the location yourself.
+        If the user provides coordinates, infer the approximate location and refer to it in your response.
+        To use the tool, you strictly apply the provided tool specification.
+
+        - Explain your step-by-step process, and give brief updates before each step.
+        - Only use the Weather_Tool for data. Never guess or make up information. 
+        - Repeat the tool use for subsequent requests if necessary.
+        - If the tool errors, apologize, explain weather is unavailable, and suggest other options.
+        - Report temperatures in °C (°F) and wind in km/h (mph). Keep weather reports concise. Sparingly use
+          emojis where appropriate.
+        - Only respond to weather queries. Remind off-topic users of your purpose. 
+        - Never claim to search online, access external data, or use tools besides Weather_Tool.
+        - Complete the entire process until you have all required data before sending the complete response.
+    "
+    ;
+
+    private static string default_prompt = "What is the weather like in Seattle?";
+
+    // The maximum number of recursive calls allowed in the tool use function.
+    // This helps prevent infinite loops and potential performance issues.
+    private static int max_recursions = 5;
 
     public static async Task Main(string[] args)
     {
         // Set up dependency injection for the Amazon service.
         using var host = Host.CreateDefaultBuilder(args)
             .ConfigureLogging(logging =>
-                logging.AddFilter("System", LogLevel.Debug)
-                    .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.Information)
+                logging.AddFilter("System", LogLevel.Error)
                     .AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Trace))
             .ConfigureServices((_, services) =>
-                services.AddAWSService<IAmazonBedrockRuntime>()
+                services.AddHttpClient()
+                    .AddAWSService<IAmazonBedrockRuntime>()
                     .AddTransient<BedrockActionsWrapper>()
+                    .AddTransient<WeatherTool>()
+                    .RemoveAll<IHttpMessageHandlerBuilderFilter>()
             )
-            .Build();
-
-        _configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("settings.json") // Load settings from .json file.
-            .AddJsonFile("settings.local.json",
-                true) // Optionally, load local settings.
             .Build();
 
         ServicesSetup(host);
 
         try
         {
-            Console.WriteLine(new string('-', 80));
-            Console.WriteLine("Welcome to the Amazon Bedrock Converse API with Tool Use Feature Scenario.");
-            Console.WriteLine(new string('-', 80));
-            ConfigurationSetup();
-            
+            await RunConversationAsync();
 
-            //await DisplayDemoChoices(_sourceBucketName, _destinationBucketName, _sampleObjectKey, _sampleObjectEtag, 0);
-
-            Console.WriteLine(new string('-', 80));
-            Console.WriteLine("Cleaning up resources.");
-            Console.WriteLine(new string('-', 80));
-            //await Cleanup(true);
-
-            Console.WriteLine(new string('-', 80));
-            Console.WriteLine("Amazon  Bedrock Converse API with Tool Use Feature Scenario is complete.");
-            Console.WriteLine(new string('-', 80));
         }
         catch (Exception ex)
         {
             Console.WriteLine(new string('-', 80));
-            Console.WriteLine($"There was a problem: {ex.Message}");
-            //await CleanupScenario(_sourceBucketName, _destinationBucketName);
+            Console.WriteLine($"There was a problem running the scenario: {ex.Message}");
+            Console.WriteLine(new string('-', 80));
+        }
+        finally
+        {
+            Console.WriteLine(
+                "Amazon  Bedrock Converse API with Tool Use Feature Scenario is complete.");
             Console.WriteLine(new string('-', 80));
         }
     }
@@ -87,17 +97,260 @@ public static class ConverseToolScenario
     private static void ServicesSetup(IHost host)
     {
         _bedrockActionsWrapper = host.Services.GetRequiredService<BedrockActionsWrapper>();
+        _weatherTool = host.Services.GetRequiredService<WeatherTool>();
     }
 
     /// <summary>
-    /// Any setup operations needed.
+    /// Starts the conversation with the user and handles the interaction with Bedrock.
     /// </summary>
-    public static void ConfigurationSetup()
+    /// <returns>The conversation array.</returns>
+    public static async Task<List<Message>> RunConversationAsync()
     {
-        _resourcePrefix = _configuration["resourcePrefix"] ?? "dotnet-example";
+        // Print the greeting and a short user guide
+        PrintHeader();
 
-        _sourceBucketName = _resourcePrefix + "-source";
-        _destinationBucketName = _resourcePrefix + "-dest";
-        _sampleObjectKey = _resourcePrefix + "-sample-object.txt";
+        // Start with an empty conversation
+        var conversation = new List<Message>();
+
+        // Get the first user input
+        var userInput = await GetUserInputAsync();
+
+        while (userInput != null)
+        {
+            // Create a new message with the user input and append it to the conversation
+            var message = new Message { Role = ConversationRole.User, Content = new List<ContentBlock> { new ContentBlock { Text = userInput } } };
+            conversation.Add(message);
+
+            // Send the conversation to Amazon Bedrock
+            var bedrockResponse = await SendConversationToBedrock(conversation);
+
+            // Recursively handle the model's response until the model has returned its final response or the recursion counter has reached 0
+            await ProcessModelResponseAsync(bedrockResponse, conversation, max_recursions);
+
+            // Repeat the loop until the user decides to exit the application
+            userInput = await GetUserInputAsync();
+        }
+
+        PrintFooter();
+        return conversation;
     }
+
+    /// <summary>
+    /// Sends the conversation, the system prompt, and the tool spec to Amazon Bedrock, and returns the response.
+    /// </summary>
+    /// <param name="conversation">The conversation history including the next message to send.</param>
+    /// <returns>The response from Amazon Bedrock.</returns>
+    private static async Task<ConverseResponse> SendConversationToBedrock(List<Message> conversation)
+    {
+        Console.WriteLine("\tCalling Bedrock...");
+
+        // Send the conversation, system prompt, and tool configuration, and return the response
+        return await _bedrockActionsWrapper.SendConverseRequestAsync(model_id, system_prompt, conversation, _weatherTool.GetToolSpec());
+    }
+
+    /// <summary>
+    /// Processes the response received via Amazon Bedrock and performs the necessary actions based on the stop reason.
+    /// </summary>
+    /// <param name="modelResponse">The model's response returned via Amazon Bedrock.</param>
+    /// <param name="conversation">The conversation history.</param>
+    /// <param name="maxRecursion">The maximum number of recursive calls allowed.</param>
+    private static async Task ProcessModelResponseAsync(ConverseResponse modelResponse, List<Message> conversation, int maxRecursion)
+    {
+        if (maxRecursion <= 0)
+        {
+            // Stop the process, the number of recursive calls could indicate an infinite loop
+            Console.WriteLine("\tWarning: Maximum number of recursions reached. Please try again.");
+        }
+
+        // Append the model's response to the ongoing conversation
+        conversation.Add(modelResponse.Output.Message);
+
+        if (modelResponse.StopReason == "tool_use")
+        {
+            // If the stop reason is "tool_use", forward everything to the tool use handler
+            await HandleToolUseAsync(modelResponse.Output, conversation, maxRecursion - 1);
+        }
+
+        if (modelResponse.StopReason == "end_turn")
+        {
+            // If the stop reason is "end_turn", print the model's response text, and finish the process
+            PrintModelResponse(modelResponse.Output.Message.Content[0].Text);
+            if (!_interactive)
+            {
+                default_prompt = "x";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles the tool use case by invoking the specified tool and sending the tool's response back to Bedrock.
+    /// The tool response is appended to the conversation, and the conversation is sent back to Amazon Bedrock for further processing.
+    /// </summary>
+    /// <param name="modelResponse">The model's response containing the tool use request.</param>
+    /// <param name="conversation">The conversation history.</param>
+    /// <param name="maxRecursion">The maximum number of recursive calls allowed.</param>
+    public static async Task HandleToolUseAsync(ConverseOutput modelResponse, List<Message> conversation, int maxRecursion)
+    {
+        // Initialize an empty list of tool results
+        var toolResults = new List<ContentBlock>();
+
+        // The model's response can consist of multiple content blocks
+        foreach (var contentBlock in modelResponse.Message.Content)
+        {
+            if (!String.IsNullOrEmpty(contentBlock.Text))
+            {
+                // If the content block contains text, print it to the console
+                PrintModelResponse(contentBlock.Text);
+            }
+
+            if (contentBlock.ToolUse != null)
+            {
+                // If the content block is a tool use request, forward it to the tool
+                var toolResponse = await InvokeTool(contentBlock.ToolUse);
+
+                // Add the tool use ID and the tool's response to the list of results
+                toolResults.Add(new ContentBlock
+                {
+                    ToolResult = new ToolResultBlock()
+                    {
+                        ToolUseId = toolResponse.ToolUseId,
+                        Content = new List<ToolResultContentBlock>()
+                            { new ToolResultContentBlock { Json = toolResponse.Content } }
+                    }
+                });
+            }
+        }
+
+        // Embed the tool results in a new user message
+        var message = new Message() { Role = ConversationRole.User, Content = toolResults };
+
+        // Append the new message to the ongoing conversation
+        conversation.Add(message);
+
+        // Send the conversation to Amazon Bedrock
+        var response = await SendConversationToBedrock(conversation);
+
+        // Recursively handle the model's response until the model has returned its final response or the recursion counter has reached 0
+        await ProcessModelResponseAsync(response, conversation, maxRecursion);
+    }
+
+    /// <summary>
+    /// Invokes the specified tool with the given payload and returns the tool's response.
+    /// If the requested tool does not exist, an error message is returned.
+    /// </summary>
+    /// <param name="payload">The payload containing the tool name and input data.</param>
+    /// <returns>The tool's response or an error message.</returns>
+    public static async Task<ToolResponse> InvokeTool(ToolUseBlock payload)
+    {
+        var toolName = payload.Name;
+
+        if (toolName == "Weather_Tool")
+        {
+            var inputData = payload.Input.AsDictionary();
+            PrintToolUse(toolName, inputData);
+
+            // Invoke the weather tool with the input data provided
+            var weatherResponse = await _weatherTool.FetchWeatherDataAsync(inputData["latitude"].ToString(), inputData["longitude"].ToString());
+            return new ToolResponse { ToolUseId = payload.ToolUseId, Content = weatherResponse };
+        }
+        else
+        {
+            var errorMessage = $"\tThe requested tool with name '{toolName}' does not exist.";
+            return new ToolResponse { ToolUseId = payload.ToolUseId, Content = new { error = true, message = errorMessage } };
+        }
+    }
+
+
+    /// <summary>
+    /// Prompts the user for input and returns the user's response.
+    /// Returns null if the user enters 'x' to exit.
+    /// </summary>
+    /// <param name="prompt">The prompt to display to the user.</param>
+    /// <returns>The user's input or null if the user chooses to exit.</returns>
+    private static async Task<string?> GetUserInputAsync(string prompt = "\tYour weather info request:")
+    {
+        var userInput = default_prompt;
+        if (_interactive)
+        {
+            Console.WriteLine(new string('*', 80));
+            Console.WriteLine($"{prompt} (x to exit): \n\t");
+            userInput = Console.ReadLine();
+        }
+
+        if (string.IsNullOrWhiteSpace(userInput))
+        {
+            prompt = "\tPlease enter your weather info request, e.g. the name of a city";
+            return await GetUserInputAsync(prompt);
+        }
+
+        if (userInput.ToLowerInvariant() == "x")
+        {
+            return null;
+        }
+
+        return userInput;
+    }
+
+    /// <summary>
+    /// Logs the welcome message and usage guide for the tool use demo.
+    /// </summary>
+    public static void PrintHeader()
+    {
+        Console.WriteLine(@"
+        =================================================
+        Welcome to the Amazon Bedrock Tool Use demo!
+        =================================================
+
+        This assistant provides current weather information for user-specified locations.
+        You can ask for weather details by providing the location name or coordinates.
+
+        Example queries:
+        - What's the weather like in New York?
+        - Current weather for latitude 40.70, longitude -74.01
+        - Is it warmer in Rome or Barcelona today?
+
+        To exit the program, simply type 'x' and press Enter.
+
+        P.S.: You're not limited to single locations, or even to using English!
+        Have fun and experiment with the app!
+        ");
+    }
+
+    /// <summary>
+    /// Logs the footer information for the tool use demo.
+    /// </summary>
+    public static void PrintFooter()
+    {
+        Console.WriteLine(@"
+        =================================================
+        Thank you for checking out the Amazon Bedrock Tool Use demo. We hope you
+        learned something new, or got some inspiration for your own apps today!
+
+        For more Bedrock examples in different programming languages, have a look at:
+        https://docs.aws.amazon.com/bedrock/latest/userguide/service_code_examples.html
+        =================================================
+        ");
+    }
+
+    /// <summary>
+    /// Logs information about the tool use.
+    /// </summary>
+    /// <param name="toolName">The name of the tool being used.</param>
+    /// <param name="inputData">The input data for the tool.</param>
+    public static void PrintToolUse(string toolName, Dictionary<string, Document> inputData)
+    {
+        Console.WriteLine($"\n\tInvoking tool: {toolName} with input: {inputData["latitude"].ToString()}, {inputData["longitude"].ToString()}...\n");
+    }
+
+    /// <summary>
+    /// Logs the model's response.
+    /// </summary>
+    /// <param name="message">The model's response message.</param>
+    public static void PrintModelResponse(string message)
+    {
+        Console.WriteLine("\tThe model's response:\n");
+        Console.WriteLine(message);
+        Console.WriteLine();
+    }
+
 }
