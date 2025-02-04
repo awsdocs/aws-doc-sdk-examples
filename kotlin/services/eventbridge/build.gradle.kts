c8f7ab95cc975e25e794aa4adb1e517642e9afd2
scmacdon <scmacdon@amazon.com>
2025-02-04 08:36:24 -0500

    added logging functionality

--- kotlin/services/eventbridge/build.gradle.kts
+++ kotlin/services/eventbridge/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation("aws.smithy.kotlin:http-client-engine-crt")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.slf4j:slf4j-api:2.0.15")
+    implementation("org.slf4j:slf4j-simple:2.0.15")
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
