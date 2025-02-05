c3eb1c09a6e9c715b8802cba0cdffaefe94461b6
scmacdon <scmacdon@amazon.com>
2025-02-05 11:43:04 -0500

    modified the tests

--- javav2/example_code/entityresolution/src/test/java/EntityResTests.java
+++ javav2/example_code/entityresolution/src/test/java/EntityResTests.java
@@ -3,27 +3,37 @@
 
 
 import com.example.entity.scenario.EntityResActions;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.entityresolution.model.CreateSchemaMappingResponse;
-
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EntityResTests {
-
+    private static final Logger logger = LoggerFactory.getLogger(EntityResTests.class);
     private static String workflowName = "";
     private static String schemaName = "";
 
     private static String roleARN = "";
-    private static String dataS3bucket = "glue-5ffb912c3d534e8493bac675c2a3196d";
+    private static String dataS3bucket = "";
     private static String outputBucket = "";
     private static String inputGlueTableArn = "";
 
@@ -35,12 +45,17 @@ public class EntityResTests {
     private static EntityResActions actions = new EntityResActions();
     @BeforeAll
     public static void setUp() {
-        workflowName = "MyMatchingWorkflow456";
-        schemaName = "schema456";
-        roleARN = "arn:aws:iam::814548047983:role/EntityResolutionCdkStack-EntityResolutionRoleB51A51-TSzkkBfrkbfm";
-        dataS3bucket = "glue-5ffb912c3d534e8493bac675c2a3196d";
-        outputBucket = "s3://entity-resolution-output-entityresolutioncdkstack";
-        inputGlueTableArn = "arn:aws:glue:us-east-1:814548047983:table/entity_resolution_db/entity_resolution";
+        Random random = new Random();
+        int randomValue = random.nextInt(10000) + 1;
+        workflowName = "MyMatchingWorkflow"+randomValue;
+        schemaName = "schema"+randomValue;
+        Gson gson = new Gson();
+        String jsonVal = getSecretValues();
+        SecretValues values = gson.fromJson(jsonVal, SecretValues.class);
+        roleARN = values.getRoleARN();
+        dataS3bucket = values.getDataS3bucket();
+        outputBucket = values.getOutputBucket();
+        inputGlueTableArn = values.getInputGlueTableArn();
 
         String json = """
             [
@@ -77,6 +92,7 @@ public class EntityResTests {
             mappingARN = response.schemaArn();
             assertNotNull(mappingARN);
         });
+        logger.info("Test 1 passed");
     }
 
     @Test
@@ -87,6 +103,7 @@ public class EntityResTests {
             workflowArn = actions.createMatchingWorkflowAsync(roleARN, workflowName, outputBucket, inputGlueTableArn, schemaName).join();
             assertNotNull(workflowArn);
         });
+        logger.info("Test 2 passed");
     }
 
     @Test
@@ -97,6 +114,7 @@ public class EntityResTests {
             jobId = actions.startMatchingJobAsync(workflowName).join();
             assertNotNull(workflowArn);
         });
+        logger.info("Test 3 passed");
     }
 
     @Test
@@ -106,6 +124,7 @@ public class EntityResTests {
         assertDoesNotThrow(() -> {
             actions.getMatchingJobAsync(jobId, workflowName).join();
         });
+        logger.info("Test 4 passed");
     }
 
     @Test
@@ -115,6 +134,7 @@ public class EntityResTests {
         assertDoesNotThrow(() -> {
             actions.getSchemaMappingAsync(schemaName).join();
         });
+        logger.info("Test 5 passed");
     }
 
     @Test
@@ -124,6 +144,7 @@ public class EntityResTests {
         assertDoesNotThrow(() -> {
             actions.ListSchemaMappings();
         });
+        logger.info("Test 6 passed");
     }
 
     @Test
@@ -133,6 +154,7 @@ public class EntityResTests {
         assertDoesNotThrow(() -> {
             actions.tagEntityResource(mappingARN).join();
         });
+        logger.info("Test 7 passed");
     }
 
     @Test
@@ -144,7 +166,47 @@ public class EntityResTests {
             Thread.sleep(1800000);
             actions.deleteMatchingWorkflowAsync(workflowName).join();
         });
+        logger.info("Test 8 passed");
     }
 
+    private static String getSecretValues() {
+        SecretsManagerClient secretClient = SecretsManagerClient.builder()
+            .region(Region.US_EAST_1)
+            .build();
+        String secretName = "test/entity";
 
+        GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
+            .secretId(secretName)
+            .build();
+
+        GetSecretValueResponse valueResponse = secretClient.getSecretValue(valueRequest);
+        return valueResponse.secretString();
+    }
+
+    @Nested
+    @DisplayName("A class used to get test values from test/cognito (an AWS Secrets Manager secret)")
+    class SecretValues {
+        private String roleARN;
+        private String dataS3bucket;
+
+        private String outputBucket;
+
+        private String inputGlueTableArn;
+
+        public String getRoleARN() {
+            return roleARN;
+        }
+
+        public String getDataS3bucket() {
+            return dataS3bucket;
+        }
+
+        public String getOutputBucket() {
+            return outputBucket;
+        }
+
+        public String getInputGlueTableArn() {
+            return inputGlueTableArn;
+        }
+    }
 }
