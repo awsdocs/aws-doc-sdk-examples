2c1efdb2f3ea3d3eb176a7b7443a058c78ec88bc
ford-at-aws <fprior@amazon.com>
2025-02-05 10:56:03 -0500

    prettier

--- .tools/test/stacks/nuke/typescript/account_nuker.ts
+++ .tools/test/stacks/nuke/typescript/account_nuker.ts
@@ -1,14 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import * as cdk from 'aws-cdk-lib';
-import * as events from 'aws-cdk-lib/aws-events';
-import * as targets from 'aws-cdk-lib/aws-events-targets';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as path from 'path';
+import * as cdk from "aws-cdk-lib";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as path from "path";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
 
 export interface NukeStackProps extends cdk.StackProps {
   awsNukeDryRunFlag?: string;
@@ -21,30 +21,33 @@ class NukeStack extends cdk.Stack {
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    
 
     // Lambda Function role
-    this.nukeLambdaRole = new iam.Role(this, 'NukeLambdaRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    this.nukeLambdaRole = new iam.Role(this, "NukeLambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
       managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess"),
       ],
     });
 
     // Create the Lambda function
-    const lambdaFunction = new DockerImageFunction(this, 'docker-lambda-function', {
-      functionName: "docker-lambda-fn",
-      code: DockerImageCode.fromImageAsset(path.join(__dirname)),
-      memorySize: 1024,
-      timeout: Duration.minutes(15),
-      architecture: lambda.Architecture.ARM_64,
-      description: "This is dockerized AWS Lambda function",
-      role: this.nukeLambdaRole
-    });
+    const lambdaFunction = new DockerImageFunction(
+      this,
+      "docker-lambda-function",
+      {
+        functionName: "docker-lambda-fn",
+        code: DockerImageCode.fromImageAsset(path.join(__dirname)),
+        memorySize: 1024,
+        timeout: Duration.minutes(15),
+        architecture: lambda.Architecture.ARM_64,
+        description: "This is dockerized AWS Lambda function",
+        role: this.nukeLambdaRole,
+      },
+    );
 
     // Create EventBridge rule to trigger the Lambda function weekly
-    const rule = new events.Rule(this, 'WeeklyTriggerRule', {
-      schedule: events.Schedule.expression('cron(0 0 ? * SUN *)')  // Runs at 00:00 every Sunday
+    const rule = new events.Rule(this, "WeeklyTriggerRule", {
+      schedule: events.Schedule.expression("cron(0 0 ? * SUN *)"), // Runs at 00:00 every Sunday
     });
 
     // Add the Lambda function as a target for the EventBridge rule
@@ -53,10 +56,10 @@ class NukeStack extends cdk.Stack {
 }
 
 const app = new cdk.App();
-new NukeStack(app, 'NukeStack', {
+new NukeStack(app, "NukeStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
-  terminationProtection: true
+  terminationProtection: true,
 });
