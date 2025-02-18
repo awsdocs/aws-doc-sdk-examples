1d50ab0c5394034aa8655089fff247f7118a2cce
Dennis Traub <dennis.traub@gmail.com>
2025-02-18 18:02:40 +0100

    Add Python examples for Amazon Nova and Nova Canvas

--- python/example_code/bedrock-runtime/test/test_converse.py
+++ python/example_code/bedrock-runtime/test/test_converse.py
@@ -1,12 +1,14 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
 import subprocess
 import sys
 
+import pytest
+
 files_under_test = [
     "models/ai21_labs_jurassic2/converse.py",
+    "models/amazon_nova/amazon_nova_text/converse.py",
     "models/amazon_titan_text/converse.py",
     "models/anthropic_claude/converse.py",
     "models/cohere_command/converse.py",
