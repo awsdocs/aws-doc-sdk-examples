494d86c5bfdb583774b8349e3928a6f38baca911
David Souther <davidsouther+github@gmail.com>
2025-02-19 08:12:11 -0500

    Replace argparse with typer for WRITEME flags.
    This has the immediate benefit of allowing multiple '--language' and '--service' options.

--- .tools/readmes/writeme.py
+++ .tools/readmes/writeme.py
@@ -2,25 +2,30 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import sys
-
-from update import update
-
 # For debugging, engineers can skip update with the --no-update flag. Yes, it's
 # a double negative, but it's quick and early in the startup because of the
 # reliance on the side-effect imports from `update` and needing them to happen
 # before importing runner, which means before importing the runner argparser.
 NO_UPDATE_FLAG = "--no-update"
 
-
 if __name__ == "__main__":
+    import sys
+
     if NO_UPDATE_FLAG not in sys.argv:
+        from update import update
+
         update()
     else:
         sys.argv.remove(NO_UPDATE_FLAG)
 
     # This import must remain in the main, after the update, as it depends on
     # importing the things that got changed during update.
-    from runner import main
+    from typer import run
 
-    sys.exit(main())
+    from runner import writeme
+
+    run(writeme)
+else:
+    from .runner import writeme
+
+    main = writeme
