d1ad628bb1449bfb06de75055973b05399f6eaac
scmacdon <scmacdon@amazon.com>
2025-03-05 12:19:20 -0500

    fixed linter error

--- kotlin/services/dynamodb/src/test/kotlin/DynamoDB.kt
+++ kotlin/services/dynamodb/src/test/kotlin/DynamoDB.kt
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-
 import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
 import aws.sdk.kotlin.services.secretsmanager.model.GetSecretValueRequest
 import aws.sdk.kotlin.services.secretsmanager.SecretsManagerClient
