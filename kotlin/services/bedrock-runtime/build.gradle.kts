43d48f789c6218ecb096333f69fe556cbd5a00fd
Dennis Traub <traubd@amazon.de>
2025-03-10 11:06:35 +0100

    Fix gradle config

--- kotlin/services/bedrock-runtime/build.gradle.kts
+++ kotlin/services/bedrock-runtime/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     application
 }
