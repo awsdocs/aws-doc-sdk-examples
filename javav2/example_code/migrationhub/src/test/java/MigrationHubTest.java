777e3575673fba76745bde71d61eef63dd3b0c06
Macdonald <scmacdon@amazon.com>
2025-03-28 14:23:34 -0400

    updated POM to use JDK 21

--- javav2/example_code/migrationhub/src/test/java/MigrationHubTest.java
+++ javav2/example_code/migrationhub/src/test/java/MigrationHubTest.java
@@ -28,7 +28,6 @@ public class MigrationHubTest {
     public static void setUp() {
         migrationClient = MigrationHubClient.builder()
                 .region(Region.US_WEST_2)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
 
         // Get the values to run these tests from AWS Secrets Manager.
@@ -38,29 +37,6 @@ public class MigrationHubTest {
         appId = values.getAppId();
         migrationtask = values.getMigrationtask();
         progress = values.getProgress();
-
-        // Uncomment this code block if you prefer using a config.properties file to
-        // retrieve AWS values required for these tests.
-        /*
-         * 
-         * try (InputStream input =
-         * MigrationHubTest.class.getClassLoader().getResourceAsStream(
-         * "config.properties")) {
-         * Properties prop = new Properties();
-         * if (input == null) {
-         * System.out.println("Sorry, unable to find config.properties");
-         * return;
-         * }
-         * 
-         * prop.load(input);
-         * appId = prop.getProperty("appId");
-         * migrationtask = prop.getProperty("migrationtask");
-         * progress = prop.getProperty("progress");
-         * 
-         * } catch (IOException ex) {
-         * ex.printStackTrace();
-         * }
-         */
     }
 
     @Test
@@ -114,7 +90,6 @@ public class MigrationHubTest {
     private static String getSecretValues() {
         SecretsManagerClient secretClient = SecretsManagerClient.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
         String secretName = "test/migrationhub";
 
