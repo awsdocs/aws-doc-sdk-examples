cac03d3103bceafe296f1da1f4a606a1aed24f66
Macdonald <scmacdon@amazon.com>
2025-04-03 14:42:53 -0400

    updated example for linter issue

--- kotlin/services/location/src/main/java/com/example/location/scenario/LocationScenario.kt
+++ kotlin/services/location/src/main/java/com/example/location/scenario/LocationScenario.kt
@@ -97,8 +97,9 @@ suspend fun main(args: Array<String>) {
       location names into geographic coordinates (latitude and longitude),
       and vice versa, enabling developers to integrate location-based search
       and routing functionality into their applications.
-    """.trimIndent()
+    """.trimIndent(),
     )
+
 
     waitForInputToContinue(scanner)
 
