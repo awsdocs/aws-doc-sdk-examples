d48ba7c3b7005110ae9fcdbde542ff10392b7d79
Macdonald <scmacdon@amazon.com>
2025-04-07 13:39:54 -0400

    added Bedrock Converse Java example

---
+++ javav2/example_code/bedrock-runtime/src/main/java/com/example/bedrockruntime/scenario/BedrockActions.java
@@ -0,0 +1,104 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.bedrockruntime.scenario;
+
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryMode;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeAsyncClient;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseRequest;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseResponse;
+import software.amazon.awssdk.services.bedrockruntime.model.Message;
+import software.amazon.awssdk.services.bedrockruntime.model.SystemContentBlock;
+import software.amazon.awssdk.services.bedrockruntime.model.Tool;
+import software.amazon.awssdk.services.bedrockruntime.model.ToolConfiguration;
+import software.amazon.awssdk.services.bedrockruntime.model.ToolSpecification;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BedrockActions {
+
+    private static volatile BedrockRuntimeAsyncClient bedrockRuntimeClient;
+
+    private BedrockRuntimeAsyncClient getClient() {
+        if (bedrockRuntimeClient == null) {
+         /*
+            The `NettyNioAsyncHttpClient` class is part of the AWS SDK for Java, version 2,
+            and it is designed to provide a high-performance, asynchronous HTTP client for interacting with AWS services.
+             It uses the Netty framework to handle the underlying network communication and the Java NIO API to
+             provide a non-blocking, event-driven approach to HTTP requests and responses.
+             */
+
+            SdkAsyncHttpClient httpClient = NettyNioAsyncHttpClient.builder()
+                .maxConcurrency(50)  // Adjust as needed.
+                .connectionTimeout(Duration.ofSeconds(60))  // Set the connection timeout.
+                .readTimeout(Duration.ofSeconds(60))  // Set the read timeout.
+                .writeTimeout(Duration.ofSeconds(60))  // Set the write timeout.
+                .build();
+
+            ClientOverrideConfiguration overrideConfig = ClientOverrideConfiguration.builder()
+                .apiCallTimeout(Duration.ofMinutes(2))  // Set the overall API call timeout.
+                .apiCallAttemptTimeout(Duration.ofSeconds(90))  // Set the individual call attempt timeout.
+                .retryStrategy(RetryMode.STANDARD)
+                .build();
+
+            bedrockRuntimeClient = BedrockRuntimeAsyncClient.builder()
+                .region(Region.US_EAST_1)
+                .httpClient(httpClient)
+                .overrideConfiguration(overrideConfig)
+                .build();
+        }
+        return bedrockRuntimeClient;
+    }
+
+    // snippet-start:[bedrockruntime.java2.converse.main]
+    /**
+     * Sends an asynchronous converse request to the AI model.
+     *
+     * @param modelId      the unique identifier of the AI model to be used for the converse request
+     * @param systemPrompt the system prompt to be included in the converse request
+     * @param conversation a list of messages representing the conversation history
+     * @param toolSpec     the specification of the tool to be used in the converse request
+     * @return the converse response received from the AI model
+     */
+    public ConverseResponse sendConverseRequestAsync(String modelId, String systemPrompt, List<Message> conversation, ToolSpecification toolSpec) {
+        List<Tool> toolList = new ArrayList<>();
+        Tool tool = Tool.builder()
+            .toolSpec(toolSpec)
+            .build();
+
+        toolList.add(tool);
+
+        ToolConfiguration configuration = ToolConfiguration.builder()
+            .tools(toolList)
+            .build();
+
+        SystemContentBlock block = SystemContentBlock.builder()
+            .text(systemPrompt)
+            .build();
+
+        ConverseRequest request = ConverseRequest.builder()
+            .modelId(modelId)
+            .system(block)
+            .messages(conversation)
+            .toolConfig(configuration)
+            .build();
+
+        try {
+            ConverseResponse response = getClient().converse(request).join();
+            return response;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+    // snippet-end:[bedrockruntime.java2.converse.main]
+}
+
+
