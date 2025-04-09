fbc3302b18b5647c5b7afaebd054652732a5f590
Eric Shepherd <ericsh@amazon.com>
2025-04-09 17:51:58 +0000

    Add example for Bedrock's ListFoundationModels

---
+++ swift/example_code/bedrock/ListFoundationModels/Sources/entry.swift
@@ -0,0 +1,145 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// An example demonstrating how to fetch a list of foundation models available
+// using Amazon Bedrock.
+
+import ArgumentParser
+import AWSClientRuntime
+import Foundation
+
+// snippet-start:[bedrock.swift.import]
+import AWSBedrock
+// snippet-end:[bedrock.swift.import]
+
+struct ExampleCommand: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "ListFoundationModels",
+        abstract: """
+        This example demonstrates how to retrieve a list of the available
+        foundation models from Amazon Bedrock.
+        """,
+        discussion: """
+        """
+    )
+
+    /// Construct a string listing the specified modalities.
+    /// 
+    /// - Parameter modalities: An array of the modalities to list.
+    ///
+    /// - Returns: A string with a human-readable list of modalities.
+    func buildModalityList(modalities: [BedrockClientTypes.ModelModality]?) -> String {
+        var first = true
+        var str = ""
+
+        if modalities == nil {
+            return "<none>"
+        }
+
+        for modality in modalities! {
+            if !first {
+                str += ", "
+            }
+            first = false
+            str += modality.rawValue
+        }
+
+        return str
+    }
+
+    /// Construct a string listing the specified customizations.
+    /// 
+    /// - Parameter customizations: An array of the customizations to list.
+    /// 
+    /// - Returns: A string listing the customizations.
+    func buildCustomizationList(customizations: [BedrockClientTypes.ModelCustomization]?) -> String {
+        var first = true
+        var str = ""
+
+        if customizations == nil {
+            return "<none>"
+        }
+
+        for customization in customizations! {
+            if !first {
+                str += ", "
+            }
+            first = false
+            str += customization.rawValue
+        }
+
+        return str
+    }
+
+    /// Construct a string listing the specified inferences.
+    /// 
+    /// - Parameter inferences: An array of inferences to list.
+    /// 
+    /// - Returns: A string listing the specified inferences.
+    func buildInferenceList(inferences: [BedrockClientTypes.InferenceType]?) -> String {
+        var first = true
+        var str = ""
+
+        if inferences == nil {
+            return "<none>"
+        }
+
+        for inference in inferences! {
+            if !first {
+                str += ", "
+            }
+            first = false
+            str += inference.rawValue
+        }
+
+        return str
+    }
+
+    /// Called by ``main()`` to run the bulk of the example.
+    func runAsync() async throws {
+        // snippet-start:[swift.bedrock.ListFoundationModels]
+        // Always use the Region "us-east-1" to have access to the most models.
+        let config = try await BedrockClient.BedrockClientConfiguration(region: "us-east-1")
+        let bedrockClient = BedrockClient(config: config)
+
+        let output = try await bedrockClient.listFoundationModels(
+            input: ListFoundationModelsInput()
+        )
+
+        guard let summaries = output.modelSummaries else {
+            print("No models returned.")
+            return
+        }
+        // snippet-end:[swift.bedrock.ListFoundationModels]
+        
+        for summary in summaries {
+            print("==========================================")
+            print(" Model ID: \(summary.modelId ?? "<unknown>")")
+            print("------------------------------------------")
+            print(" Name: \(summary.modelName ?? "<unknown>")")
+            print(" Provider: \(summary.providerName ?? "<unknown>")")
+            print(" Input modalities: \(buildModalityList(modalities: summary.inputModalities))")
+            print(" Output modalities: \(buildModalityList(modalities: summary.outputModalities))")
+            print(" Supported customizations: \(buildCustomizationList(customizations: summary.customizationsSupported ))")
+            print(" Supported inference types: \(buildInferenceList(inferences: summary.inferenceTypesSupported))")
+            print("------------------------------------------\n")
+        }
+        
+        print("\(summaries.count) models available.")
+    }
+}
+
+/// The program's asynchronous entry point.
+@main
+struct Main {
+    static func main() async {
+        let args = Array(CommandLine.arguments.dropFirst())
+
+        do {
+            let command = try ExampleCommand.parse(args)
+            try await command.runAsync()
+        } catch {
+            ExampleCommand.exit(withError: error)
+        }
+    }    
+}
