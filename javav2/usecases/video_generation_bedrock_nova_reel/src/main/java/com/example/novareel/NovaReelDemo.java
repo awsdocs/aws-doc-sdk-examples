fa7ce1a4f3371b1c13ebf691141d4796181379b5
danil <danil.temnikov@gmail.com>
2025-04-28 14:34:22 -0700

    fixing Missing SPDX check

--- javav2/usecases/video_generation_bedrock_nova_reel/src/main/java/com/example/novareel/NovaReelDemo.java
+++ javav2/usecases/video_generation_bedrock_nova_reel/src/main/java/com/example/novareel/NovaReelDemo.java
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.example.novareel;
 
 import org.springframework.boot.WebApplicationType;
