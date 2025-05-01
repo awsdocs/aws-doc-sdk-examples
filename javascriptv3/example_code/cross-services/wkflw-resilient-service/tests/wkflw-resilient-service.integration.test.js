ea709a5be452fd9faefa7ea611498cea3ca0bac4
Jason Q <81179619+beqqrry-aws@users.noreply.github.com>
2025-05-01 09:39:14 -0700

    Update wkflw-resilient-service.integration.test.js

--- javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw-resilient-service.integration.test.js
+++ javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw-resilient-service.integration.test.js
@@ -8,12 +8,12 @@ describe("workflow", () => {
   it("should run without error", async () => {
     // This test is failing regularly due to resource constraints. We're disabling it for now.
     // try {
-      // await scenarios.deploy.run({ confirmAll: true, verbose: true });
-      // await scenarios.demo.run({ confirmAll: true, verbose: true });
-      // await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    // await scenarios.deploy.run({ confirmAll: true, verbose: true });
+    // await scenarios.demo.run({ confirmAll: true, verbose: true });
+    // await scenarios.destroy.run({ confirmAll: true, verbose: true });
     // } catch (err) {
-      // await scenarios.destroy.run({ confirmAll: true, verbose: true });
-      // throw err;
+    // await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    // throw err;
     // }
   });
 });
