7e70c70f6db8fb43c50ed02da8608c50e433e653
Macdonald <scmacdon@amazon.com>
2025-05-02 13:58:24 -0400

    added the new header example

--- javav2/example_code/ses/src/main/java/com/example/sesv2/SendEmail.java
+++ javav2/example_code/ses/src/main/java/com/example/sesv2/SendEmail.java
@@ -5,6 +5,7 @@ package com.example.sesv2;
 
 // snippet-start:[ses.java2.sendmessage.sesv2.main]
 // snippet-start:[ses.java2.sendmessage.sesv2.import]
+
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sesv2.model.Body;
 import software.amazon.awssdk.services.sesv2.model.Content;
@@ -19,93 +20,93 @@ import software.amazon.awssdk.services.sesv2.SesV2Client;
 /**
  * Before running this AWS SDK for Java (v2) example, set up your development
  * environment, including your credentials.
- *
+ * <p>
  * For more information, see the following documentation topic:
- *
+ * <p>
  * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
  */
 
 public class SendEmail {
-        public static void main(String[] args) {
-                final String usage = """
+    public static void main(String[] args) {
+        final String usage = """
+                             
+                             Usage:
+                                 <sender> <recipient> <subject>\s
+                             
+                             Where:
+                                 sender - An email address that represents the sender.\s
+                                 recipient - An email address that represents the recipient.\s
+                                 subject - The subject line.\s
+                             """;
 
-                                Usage:
-                                    <sender> <recipient> <subject>\s
-
-                                Where:
-                                    sender - An email address that represents the sender.\s
-                                    recipient - An email address that represents the recipient.\s
-                                    subject - The subject line.\s
-                                """;
-
-                if (args.length != 3) {
-                        System.out.println(usage);
-                        System.exit(1);
-                }
-
-                String sender = args[0];
-                String recipient = args[1];
-                String subject = args[2];
-
-                Region region = Region.US_EAST_1;
-                SesV2Client sesv2Client = SesV2Client.builder()
-                                .region(region)
-                                .build();
-
-                // The HTML body of the email.
-                String bodyHTML = "<html>" + "<head></head>" + "<body>" + "<h1>Hello!</h1>"
-                                + "<p> See the list of customers.</p>" + "</body>" + "</html>";
-
-                send(sesv2Client, sender, recipient, subject, bodyHTML);
+        if (args.length != 3) {
+            System.out.println(usage);
+            System.exit(1);
         }
 
-        public static void send(SesV2Client client,
-                        String sender,
-                        String recipient,
-                        String subject,
-                        String bodyHTML) {
+        String sender = args[0];
+        String recipient = args[1];
+        String subject = args[2];
 
-                Destination destination = Destination.builder()
-                                .toAddresses(recipient)
-                                .build();
+        Region region = Region.US_EAST_1;
+        SesV2Client sesv2Client = SesV2Client.builder()
+                .region(region)
+                .build();
 
-                Content content = Content.builder()
-                                .data(bodyHTML)
-                                .build();
+        // The HTML body of the email.
+        String bodyHTML = "<html>" + "<head></head>" + "<body>" + "<h1>Hello!</h1>"
+                + "<p> See the list of customers.</p>" + "</body>" + "</html>";
 
-                Content sub = Content.builder()
-                                .data(subject)
-                                .build();
+        send(sesv2Client, sender, recipient, subject, bodyHTML);
+    }
 
-                Body body = Body.builder()
-                                .html(content)
-                                .build();
+    public static void send(SesV2Client client,
+                            String sender,
+                            String recipient,
+                            String subject,
+                            String bodyHTML) {
 
-                Message msg = Message.builder()
-                                .subject(sub)
-                                .body(body)
-                                .build();
+        Destination destination = Destination.builder()
+                .toAddresses(recipient)
+                .build();
 
-                EmailContent emailContent = EmailContent.builder()
-                                .simple(msg)
-                                .build();
+        Content content = Content.builder()
+                .data(bodyHTML)
+                .build();
 
-                SendEmailRequest emailRequest = SendEmailRequest.builder()
-                                .destination(destination)
-                                .content(emailContent)
-                                .fromEmailAddress(sender)
-                                .build();
+        Content sub = Content.builder()
+                .data(subject)
+                .build();
 
-                try {
-                        System.out.println("Attempting to send an email through Amazon SES "
-                                        + "using the AWS SDK for Java...");
-                        client.sendEmail(emailRequest);
-                        System.out.println("email was sent");
+        Body body = Body.builder()
+                .html(content)
+                .build();
 
-                } catch (SesV2Exception e) {
-                        System.err.println(e.awsErrorDetails().errorMessage());
-                        System.exit(1);
-                }
+        Message msg = Message.builder()
+                .subject(sub)
+                .body(body)
+                .build();
+
+        EmailContent emailContent = EmailContent.builder()
+                .simple(msg)
+                .build();
+
+        SendEmailRequest emailRequest = SendEmailRequest.builder()
+                .destination(destination)
+                .content(emailContent)
+                .fromEmailAddress(sender)
+                .build();
+
+        try {
+            System.out.println("Attempting to send an email through Amazon SES "
+                    + "using the AWS SDK for Java...");
+            client.sendEmail(emailRequest);
+            System.out.println("email was sent");
+
+        } catch (SesV2Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
         }
+    }
 }
 // snippet-end:[ses.java2.sendmessage.sesv2.main]
