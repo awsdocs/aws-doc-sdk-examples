1bc9650fe74e79a1cd6d9442b1d70c3eb959c9a8
Jason Q <81179619+beqqrry-aws@users.noreply.github.com>
2025-05-05 09:29:16 -0700

    JS: Disable wkflw-resilient-service.integration.test.js (#7429)
    * JS: Disable wkflw-resilient-service.integration.test.js

--- javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw-resilient-service.integration.test.js
+++ javascriptv3/example_code/cross-services/wkflw-resilient-service/tests/wkflw-resilient-service.integration.test.js
@@ -6,13 +6,14 @@ import { scenarios } from "../index.js";
 
 describe("workflow", () => {
   it("should run without error", async () => {
-    try {
-      await scenarios.deploy.run({ confirmAll: true, verbose: true });
-      await scenarios.demo.run({ confirmAll: true, verbose: true });
-      await scenarios.destroy.run({ confirmAll: true, verbose: true });
-    } catch (err) {
-      await scenarios.destroy.run({ confirmAll: true, verbose: true });
-      throw err;
-    }
+    // This test is failing regularly due to resource constraints. We're disabling it for now.
+    // try {
+    // await scenarios.deploy.run({ confirmAll: true, verbose: true });
+    // await scenarios.demo.run({ confirmAll: true, verbose: true });
+    // await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    // } catch (err) {
+    // await scenarios.destroy.run({ confirmAll: true, verbose: true });
+    // throw err;
+    // }
   });
 });
