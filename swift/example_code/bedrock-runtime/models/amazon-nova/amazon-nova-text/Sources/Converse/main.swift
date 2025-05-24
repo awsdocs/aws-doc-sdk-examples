23e27c698b072f306e470bd6ab808d257551138e
monadierickx <126071495+monadierickx@users.noreply.github.com>
2025-05-25 00:06:16 +0200

    nova reel example code

--- swift/example_code/bedrock-runtime/models/amazon-nova/amazon-nova-text/Sources/Converse/main.swift
+++ swift/example_code/bedrock-runtime/models/amazon-nova/amazon-nova-text/Sources/Converse/main.swift
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 //
-// snippet-start:[swift.example_code.bedrock-runtime.Converse_AmazonNovaText]
+// snippet-start:[swift.example_code.bedrock-runtime.Scenario_AmazonNova_TextToVideo]
 // An example demonstrating how to use the Conversation API to send 
 // a text message to Amazon Nova.
 
