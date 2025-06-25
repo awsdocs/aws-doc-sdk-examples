cb6c75f5d82cbd5efe5f2ef6041459aa90fc3a14
Tejas Ganesh Naik <tejasgn@amazon.com>
2025-06-25 23:27:20 +0000

    Lint fix by upgrading to 1.87.0 and adding SPDX header

--- rustv1/examples/bedrock-agent-runtime/src/bin/invoke-agent.rs
+++ rustv1/examples/bedrock-agent-runtime/src/bin/invoke-agent.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use aws_config::{BehaviorVersion, SdkConfig};
 use aws_sdk_bedrockagentruntime::{
     self as bedrockagentruntime,
