a44dbf23d0670a75175c72265ed980627bfa3b64
Todd Hill <tkhill@amazon.com>
2025-07-02 12:05:32 -0400

    Update examples for uploading from an inputstream of unknown size
    Addresses: https://issues.amazon.com/issues/V1827262060

--- javav2/example_code/s3/src/main/java/com/example/s3/transfermanager/UploadStream.java
+++ javav2/example_code/s3/src/main/java/com/example/s3/transfermanager/UploadStream.java
@@ -4,25 +4,28 @@ package com.example.s3.transfermanager;
 
 // snippet-start:[s3.tm.java2.upload_stream.complete]
 // snippet-start:[s3.tm.java2.upload_stream.import]
+
 import com.example.s3.util.AsyncExampleUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.core.async.BlockingInputStreamAsyncRequestBody;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.transfer.s3.model.CompletedUpload;
 import software.amazon.awssdk.transfer.s3.model.Upload;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 // snippet-end:[s3.tm.java2.upload_stream.import]
 
 public class UploadStream {
     private static final Logger logger = LoggerFactory.getLogger(UploadStream.class);
 
     public static void main(String[] args) {
-        String bucketName = "amzn-s3-demo-bucket" + UUID.randomUUID();
+        String bucketName = "qamzn-s3-demo-bucket" + UUID.randomUUID();
         String key = UUID.randomUUID().toString();
 
         AsyncExampleUtils.createBucket(bucketName);
@@ -41,30 +44,31 @@ public class UploadStream {
 
 // snippet-start:[s3.tm.java2.upload_stream.main]
     /**
-     * @param transferManager - To upload content from a stream of unknown size, use the S3TransferManager based on the AWS CRT-based S3 client.
-     *                       For more information, see https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/transfer-manager.html.
+     * @param transferManager - To upload content from a stream of unknown size, you can use the S3TransferManager based on the AWS CRT-based S3 client.
      * @param bucketName - The name of the bucket.
      * @param key - The name of the object.
      * @return - software.amazon.awssdk.transfer.s3.model.CompletedUpload - The result of the completed upload.
      */
     public CompletedUpload uploadStream(S3TransferManager transferManager, String bucketName, String key) {
 
-        BlockingInputStreamAsyncRequestBody body =
-                AsyncRequestBody.forBlockingInputStream(null); // 'null' indicates a stream will be provided later.
+        // AsyncExampleUtils.randomString() returns a random string up to 100 characters.
+        String randomString = AsyncExampleUtils.randomString();
+        logger.info("random string to upload: {}: length={}", randomString, randomString.length());
+        InputStream inputStream = new ByteArrayInputStream(randomString.getBytes());
+
+        // Executor required to handle reading from the InputStream on a separate thread so the main upload is not blocked.
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        // Specify `null` for the content length when you don't know the content length.
+        AsyncRequestBody body = AsyncRequestBody.fromInputStream(inputStream, null, executor);
 
         Upload upload = transferManager.upload(builder -> builder
                 .requestBody(body)
                 .putObjectRequest(req -> req.bucket(bucketName).key(key))
                 .build());
 
-        // AsyncExampleUtils.randomString() returns a random string up to 100 characters.
-        String randomString = AsyncExampleUtils.randomString();
-        logger.info("random string to upload: {}: length={}", randomString, randomString.length());
-
-        // Provide the stream of data to be uploaded.
-        body.writeInputStream(new ByteArrayInputStream(randomString.getBytes()));
-
-        return upload.completionFuture().join();
+        CompletedUpload completedUpload = upload.completionFuture().join();
+        executor.shutdown();
+        return completedUpload;
     }
 }
 // snippet-end:[s3.tm.java2.upload_stream.main]
