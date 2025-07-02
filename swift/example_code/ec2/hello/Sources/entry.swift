41da64071e7294d319a041b2c5ab79b25e6524de
Eric Shepherd <ericsh@amazon.com>
2025-07-02 20:16:33 +0000

    Add EC2 hello (basics) example for swift

---
+++ swift/example_code/ec2/hello/Sources/entry.swift
@@ -0,0 +1,101 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// snippet-start:[swift.glue.scenario]
+// An example that shows how to use the AWS SDK for Swift to perform a simple
+// operation using Amazon Elastic Compute Cloud (EC2).
+//
+
+
+import ArgumentParser
+import Foundation
+
+// snippet-start:[swift.glue.import]
+
+// import AWSClientRuntime
+import AWSEC2
+// snippet-end:[swift.glue.import]
+
+struct ExampleCommand: ParsableCommand {
+    @Option(help: "The AWS Region to run AWS API calls in.")
+    var awsRegion = "us-east-1"
+
+    @Option(
+        help: ArgumentHelp("The level of logging for the Swift SDK to perform."),
+        completion: .list([
+            "critical",
+            "debug",
+            "error",
+            "info",
+            "notice",
+            "trace",
+            "warning"
+        ])
+    )
+    var logLevel: String = "error"
+
+    static var configuration = CommandConfiguration(
+        commandName: "hello-ec2",
+        abstract: """
+        Demonstrates a simple operation using Amazon EC2.
+        """,
+        discussion: """
+        An example showing how to make a call to Amazon EC2 using the AWS SDK for Swift.
+        """
+    )
+
+    func getSecurityGroupNames(ec2Client: EC2Client) async -> [String] {
+        let pages = ec2Client.describeSecurityGroupsPaginated(
+            input: DescribeSecurityGroupsInput()
+        )
+
+        var groupNames: [String] = []
+
+        do {
+            for try await page in pages {
+                guard let groups = page.securityGroups else {
+                    print("*** Error: No groups returned.")
+                    continue
+                }
+
+                for group in groups {
+                    groupNames.append(group.groupName ?? "<unknown>")
+                }
+            }
+        } catch {
+            print("*** Error: \(error.localizedDescription)")
+        }
+
+        return groupNames
+    }
+
+    /// Called by ``main()`` to run the bulk of the example.
+    func runAsync() async throws {
+        let ec2Config = try await EC2Client.EC2ClientConfiguration(region: awsRegion)
+        let ec2Client = EC2Client(config: ec2Config)
+
+        let groupNames = await getSecurityGroupNames(ec2Client: ec2Client)
+
+        print("Found \(groupNames.count) security group(s):")
+
+        for group in groupNames {
+            print("    \(group)")
+        }
+    }
+}
+
+/// The program's asynchronous entry point.
+@main
+struct Main {
+    static func main() async {
+        let args = Array(CommandLine.arguments.dropFirst())
+
+        do {
+            let command = try ExampleCommand.parse(args)
+            try await command.runAsync()
+        } catch {
+            ExampleCommand.exit(withError: error)
+        }
+    }    
+}
+// snippet-end:[swift.glue.scenario]
