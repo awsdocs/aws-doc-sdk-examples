97879b0779b8de014649f7cf520a93822b82da58
Macdonald <scmacdon@amazon.com>
2025-07-03 12:46:13 -0400

    rolled in review comments

--- javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/CloudWatchLogQuery.java
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/CloudWatchLogQuery.java
@@ -28,9 +28,8 @@ import java.util.List;
 public class CloudWatchLogQuery {
     public static void main(final String[] args) {
         final String usage = """
-
                 Usage:
-                  <logGroupName> 
+                  <logGroupName>
 
                 Where:
                   logGroupName - The name of the log group (for example, /aws/lambda/ChatAIHandler).
@@ -41,28 +40,28 @@ public class CloudWatchLogQuery {
             System.exit(1);
         }
 
-        String logGroupName = args[0] ;
+        String logGroupName = "/aws/lambda/ChatAIHandler" ; //args[0];
         CloudWatchLogsClient logsClient = CloudWatchLogsClient.builder()
                 .region(Region.US_EAST_1)
                 .build();
 
-        getLogEvents(logsClient, logGroupName);
-
+        describeMostRecentLogStream(logsClient, logGroupName);
     }
 
     /**
-     * Retrieves and prints log events from the most recent log stream in the specified log group
+     * Describes and prints metadata about the most recent log stream in the specified log group.
      *
-     * @param logsClient the CloudWatchLogsClient used to interact with AWS CloudWatch Logs
-     * @param logGroupName the name of the log group from which to retrieve the log events
+     * @param logsClient   the CloudWatchLogsClient used to interact with AWS CloudWatch Logs
+     * @param logGroupName the name of the log group
      */
-    public static void getLogEvents(CloudWatchLogsClient logsClient, String logGroupName) {
+    public static void describeMostRecentLogStream(CloudWatchLogsClient logsClient, String logGroupName) {
         DescribeLogStreamsRequest streamsRequest = DescribeLogStreamsRequest.builder()
                 .logGroupName(logGroupName)
                 .orderBy(OrderBy.LAST_EVENT_TIME)
                 .descending(true)
                 .limit(1)
                 .build();
+
         try {
             DescribeLogStreamsResponse streamsResponse = logsClient.describeLogStreams(streamsRequest);
             List<LogStream> logStreams = streamsResponse.logStreams();
@@ -72,23 +71,18 @@ public class CloudWatchLogQuery {
                 return;
             }
 
-            String logStreamName = logStreams.get(0).logStreamName();
-
-            // Get Log Events.
-            GetLogEventsRequest eventsRequest = GetLogEventsRequest.builder()
-                    .logGroupName(logGroupName)
-                    .logStreamName(logStreamName)
-                    .startFromHead(true)
-                    .build();
-
-            GetLogEventsResponse eventsResponse = logsClient.getLogEvents(eventsRequest);
-            System.out.println("Log events from: " + logStreamName);
-            for (OutputLogEvent event : eventsResponse.events()) {
-                System.out.printf("[%s] %s%n", event.timestamp(), event.message());
-            }
+            LogStream stream = logStreams.get(0);
+            System.out.println("Most Recent Log Stream:");
+            System.out.println("  Name: " + stream.logStreamName());
+            System.out.println("  ARN: " + stream.arn());
+            System.out.println("  Creation Time: " + stream.creationTime());
+            System.out.println("  First Event Time: " + stream.firstEventTimestamp());
+            System.out.println("  Last Event Time: " + stream.lastEventTimestamp());
+            System.out.println("  Stored Bytes: " + stream.storedBytes());
+            System.out.println("  Upload Sequence Token: " + stream.uploadSequenceToken());
 
         } catch (CloudWatchLogsException e) {
-            System.err.println("Failed to fetch logs: " + e.awsErrorDetails().errorMessage());
+            System.err.println("Failed to describe log stream: " + e.awsErrorDetails().errorMessage());
         }
     }
 }
