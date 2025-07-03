3279e6a53d35afa97d668926268a330c7d79fd05
Macdonald <scmacdon@amazon.com>
2025-07-03 15:09:41 -0400

    fixed linter issue

--- kotlin/services/neptune/src/main/java/com/example/neptune/scenerio/NeptuneScenario.kt
+++ kotlin/services/neptune/src/main/java/com/example/neptune/scenerio/NeptuneScenario.kt
@@ -39,10 +39,10 @@ var scanner = Scanner(System.`in`)
  * https://docs.aws.amazon.com/sdk-for-kotlin/latest/developer-guide/setup.html
  */
 suspend fun main() {
-    val subnetGroupName = "neptuneSubnetGroup210"
-    val clusterName = "neptuneCluster210"
-    val dbInstanceId = "neptuneDB210"
-    val client = NeptuneClient.fromEnvironment {region = "us-east-1" }
+    val subnetGroupName = "neptuneSubnetGroup"
+    val clusterName = "neptuneCluster"
+    val dbInstanceId = "neptuneDB"
+    val client = NeptuneClient.fromEnvironment { region = "us-east-1" }
 
     println(
         """
