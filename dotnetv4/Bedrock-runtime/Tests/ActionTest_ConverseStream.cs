f3ff66f76a9acc4caf8bec59d0bd317038b5ee6a
Rachel Hagerman <110480692+rlhagerm@users.noreply.github.com>
2025-07-09 12:39:10 -0500

    Removing EOL model from Bedrock examples.

--- dotnetv4/Bedrock-runtime/Tests/ActionTest_ConverseStream.cs
+++ dotnetv4/Bedrock-runtime/Tests/ActionTest_ConverseStream.cs
@@ -6,13 +6,14 @@ namespace BedrockRuntimeTests;
 public class ActionTest_ConverseStream
 {
     [Theory, Trait("Category", "Integration")]
+    [InlineData(typeof(AmazonTitanText.ConverseStream))]
     [InlineData(typeof(Mistral.ConverseStream))]
     [InlineData(typeof(MetaLlama.ConverseStream))]
     [InlineData(typeof(CohereCommand.ConverseStream))]
     [InlineData(typeof(AnthropicClaude.ConverseStream))]
-    [InlineData(typeof(AmazonTitanText.ConverseStream))]
     public void ConverseStreamDoesNotThrow(Type type)
     {
+        Thread.Sleep(5000);
         var entryPoint = type.Assembly.EntryPoint!;
         var exception = Record.Exception(() => entryPoint.Invoke(null, [Array.Empty<string>()]));
         Assert.Null(exception);
