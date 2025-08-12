ae856d7d6bbac395c3aae87533bb3fe64e21320e
Lan Ding <dinglan@amazon.com>
2025-08-12 09:53:33 -0700

    Fix license header for the javascript files

--- python/example_code/cognito/scenarios/identity_pools_example_demo/web/frontend/js/animations.js
+++ python/example_code/cognito/scenarios/identity_pools_example_demo/web/frontend/js/animations.js
@@ -1,7 +1,5 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 // Global animation handler for the entire website
 (function() {
