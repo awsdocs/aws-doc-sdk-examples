79d9e2f6a5dc9b5db95d921151f321d9272f819a
Brian Murray <40031786+brmur@users.noreply.github.com>
2025-08-13 11:50:59 +0100

    Correcting minor formatting errors in licensing and README

--- python/example_code/cognito/scenarios/identity_pools_example_demo/web/frontend/js/flow-visualizer.js
+++ python/example_code/cognito/scenarios/identity_pools_example_demo/web/frontend/js/flow-visualizer.js
@@ -1,7 +1,5 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 class APIFlowManager {
     constructor() {
