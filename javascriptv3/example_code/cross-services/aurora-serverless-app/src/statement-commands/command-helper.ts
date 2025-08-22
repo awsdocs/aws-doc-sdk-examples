c28fdde73ac7321cbb94c4cf5c96b5ed84bbc95d
kellertk <kellertk@amazon.com>
2025-08-22 10:05:33 -0700

    chore: use paramaterized sql statements

--- javascriptv3/example_code/cross-services/aurora-serverless-app/src/statement-commands/command-helper.ts
+++ javascriptv3/example_code/cross-services/aurora-serverless-app/src/statement-commands/command-helper.ts
@@ -1,14 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { ExecuteStatementCommand } from "@aws-sdk/client-rds-data";
-import env from "../../env.json" assert { type: "json" };
+import env from "../../env.json" with { type: "json" };
 
-const buildStatementCommand = (sql: string) => {
+const buildStatementCommand = (sql: string, parameters?: { [key: string]: { [key: string]: unknown}}) => {
   return new ExecuteStatementCommand({
     resourceArn: env.CLUSTER_ARN,
     secretArn: env.SECRET_ARN,
     database: env.DB_NAME,
     sql,
+    [parameters ? "parameters" : ""]: [parameters]
   });
 };
 
