dab6c11752d0e7f26b31e9c8075842835f037d4b
Eric Shepherd <ericsh@amazon.com>
2025-08-22 17:56:35 +0000

    Add SFN Hello example

---
+++ swift/example_code/sfn/hello/Sources/entry.swift
@@ -0,0 +1,72 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// snippet-start:[swift.sfn.hello]
+// An example that shows how to use the AWS SDK for Swift to perform a simple
+// operation using Amazon Elastic Compute Cloud (EC2).
+//
+
+import ArgumentParser
+import Foundation
+
+// snippet-start:[swift.sfn.import]
+import AWSSFN
+// snippet-end:[swift.sfn.import]
+
+struct ExampleCommand: ParsableCommand {
+    @Option(help: "The AWS Region to run AWS API calls in.")
+    var awsRegion = "us-east-1"
+
+    static var configuration = CommandConfiguration(
+        commandName: "hello-sfn",
+        abstract: """
+        Demonstrates a simple operation using AWS Step Functions.
+        """,
+        discussion: """
+        An example showing how to make a call to AWS Step Functions using the
+        AWS SDK for Swift.
+        """
+    )
+
+    /// Called by ``main()`` to run the bulk of the example.
+    func runAsync() async throws {
+        let sfnConfig = try await SFNClient.SFNClientConfiguration(region: awsRegion)
+        let sfnClient = SFNClient(config: sfnConfig)
+
+        do {
+            let output = try await sfnClient.listStateMachines(
+                input: ListStateMachinesInput(
+                    maxResults: 10
+                )
+            )
+
+            guard let stateMachines = output.stateMachines else {
+                print("*** No state machines found.")
+                return
+            }
+
+            print("Found \(stateMachines.count) state machines (capped to 10)...")
+            for machine in stateMachines {
+                print("    \(machine.name ?? "<unnamed>"): \(machine.stateMachineArn ?? "<unknown>")")
+            }
+        } catch {
+            print("*** Error fetching state machine list: \(error.localizedDescription)")
+        }
+    }
+}
+
+/// The program's asynchronous entry point.
+@main
+struct Main {
+    static func main() async {
+        let args = Array(CommandLine.arguments.dropFirst())
+
+        do {
+            let command = try ExampleCommand.parse(args)
+            try await command.runAsync()
+        } catch {
+            ExampleCommand.exit(withError: error)
+        }
+    }    
+}
+// snippet-end:[swift.sfn.hello]
