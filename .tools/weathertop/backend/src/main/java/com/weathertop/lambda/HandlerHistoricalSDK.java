0fee2a7156d13d73c52db5c460eb0eb6b6b00028
Macdonald <scmacdon@amazon.com>
2025-09-29 14:23:52 -0400

    fixed a linter issue

--- .tools/weathertop/backend/src/main/java/com/weathertop/lambda/HandlerHistoricalSDK.java
+++ .tools/weathertop/backend/src/main/java/com/weathertop/lambda/HandlerHistoricalSDK.java
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package com.weathertop.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
