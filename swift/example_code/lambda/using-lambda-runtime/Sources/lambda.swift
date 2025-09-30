5c95ebe37a48313acb30bd9e86e4483a51c6ab03
Eric Shepherd <ericsh@amazon.com>
2025-09-30 13:10:09 -0400

    Update lambda.swift
    Remove stray newlines

--- swift/example_code/lambda/using-lambda-runtime/Sources/lambda.swift
+++ swift/example_code/lambda/using-lambda-runtime/Sources/lambda.swift
@@ -9,6 +9,7 @@ import AWSS3
 import protocol AWSClientRuntime.AWSServiceError
 import enum Smithy.ByteStream
 // snippet-end:[lambda.swift.function.imports]
+
 // snippet-start:[lambda.swift.function.types]
 // snippet-start:[lambda.swift.function.struct.request]
 /// Represents the contents of the requests being received from the client.
@@ -19,6 +20,7 @@ struct Request: Decodable, Sendable {
     let body: String
 }
 // snippet-end:[lambda.swift.function.struct.request]
+
 // snippet-start:[lambda.swift.function.struct.response]
 /// The contents of the response sent back to the client. This must be
 /// `Encodable`.
@@ -29,6 +31,7 @@ struct Response: Encodable, Sendable {
     let body: String
 }
 // snippet-end:[lambda.swift.function.struct.response]
+
 // snippet-start:[lambda.swift.function.errors]
 /// The errors that the Lambda function can return.
 enum S3ExampleLambdaErrors: Error {
