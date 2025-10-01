258d82b2c7fe72c493f8c0dff2816fdb1b424993
Macdonald <scmacdon@amazon.com>
2025-10-01 11:27:07 -0400

    added a new example that shows how search for log group using a pattern

---
+++ javav2/example_code/cloudwatch/src/main/java/com/example/cloudwatch/CloudWatchLogsSearch.java
@@ -0,0 +1,79 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.cloudwatch;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsResponse;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilterLogEventsRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilterLogEventsResponse;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilteredLogEvent;
+import software.amazon.awssdk.services.cloudwatchlogs.model.LogStream;
+import java.util.List;
+
+
+// snippet-start:[cloudwatch.javav2.read.log.streams.main]
+/**
+ * Before running this Java V2 code example, set up your development
+ * environment, including your credentials.
+ * <p>
+ * For more information, see the following documentation topic:
+ * <p>
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class CloudWatchLogsSearch {
+
+    private static final String LOG_GROUP_NAME = "WeathertopJavaContainerLogs";
+    private static final String LOG_STREAM_PREFIX = "weathertop-java-stream";
+    private static final String PATTERN = "INFO";
+    private static final Region REGION = Region.US_EAST_1;
+
+    public static void main(String[] args) {
+        CloudWatchLogsClient cwlClient = CloudWatchLogsClient.builder()
+                .region(REGION)
+                .build();
+
+        searchLogStreamsAndFilterEvents(cwlClient, LOG_GROUP_NAME, LOG_STREAM_PREFIX, PATTERN);
+    }
+
+    /**
+     * Searches for log streams with a specific prefix within a log group and filters log events based on a specified pattern.
+     *
+     * @param cwlClient       the CloudWatchLogsClient used to interact with AWS CloudWatch Logs
+     * @param logGroupName    the name of the log group to search within
+     * @param logStreamPrefix the prefix of the log streams to search for
+     * @param pattern         the pattern to filter log events by
+     */
+    private static void searchLogStreamsAndFilterEvents(CloudWatchLogsClient cwlClient, String logGroupName, String logStreamPrefix, String pattern) {
+        DescribeLogStreamsRequest describeLogStreamsRequest = DescribeLogStreamsRequest.builder()
+                .logGroupName(logGroupName)
+                .logStreamNamePrefix(logStreamPrefix)
+                .build();
+
+        DescribeLogStreamsResponse describeLogStreamsResponse = cwlClient.describeLogStreams(describeLogStreamsRequest);
+
+        List<LogStream> logStreams = describeLogStreamsResponse.logStreams();
+
+        for (LogStream logStream : logStreams) {
+            String logStreamName = logStream.logStreamName();
+            System.out.println("Searching in log stream: " + logStreamName);
+
+            FilterLogEventsRequest filterLogEventsRequest = FilterLogEventsRequest.builder()
+                    .logGroupName(logGroupName)
+                    .logStreamNames(logStreamName)
+                    .filterPattern(pattern)
+                    .build();
+
+            FilterLogEventsResponse filterLogEventsResponse = cwlClient.filterLogEvents(filterLogEventsRequest);
+
+            for (FilteredLogEvent event : filterLogEventsResponse.events()) {
+                System.out.println(event.message());
+            }
+
+            System.out.println("--------------------------------------------------"); // Separator for better readability
+        }
+    }
+}
+// snippet-end:[cloudwatch.javav2.read.log.streams.main]
