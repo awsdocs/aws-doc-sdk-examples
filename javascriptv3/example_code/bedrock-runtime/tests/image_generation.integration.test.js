60b3db4eb5e556a814c703da138aadf363728204
Dennis Traub <dennis.traub@gmail.com>
2025-10-01 17:32:26 +0200

    Fix dotnet issue

--- javascriptv3/example_code/bedrock-runtime/tests/image_generation.integration.test.js
+++ javascriptv3/example_code/bedrock-runtime/tests/image_generation.integration.test.js
@@ -1,16 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import {describe, it} from "vitest";
-import {invokeModel} from "../models/amazonNovaCanvas/invokeModel.js";
-import {expectToBeANonEmptyString} from "./test_tools.js";
+import { describe, it } from "vitest";
+import { invokeModel } from "../models/amazonNovaCanvas/invokeModel.js";
+import { expectToBeANonEmptyString } from "./test_tools.js";
 
 describe("Invoking Amazon Nova Canvas", () => {
-    it("should return a response",
-        async () => {
-            const response = await invokeModel();
-            expectToBeANonEmptyString(response);
-        },
-        {timeout: 600000},
-    );
+  it(
+    "should return a response",
+    async () => {
+      const response = await invokeModel();
+      expectToBeANonEmptyString(response);
+    },
+    { timeout: 600000 },
+  );
 });
