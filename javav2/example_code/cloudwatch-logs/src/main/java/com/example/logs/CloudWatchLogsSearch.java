54d79a2f48a8678958ada316f346f1234aab8754
Macdonald <scmacdon@amazon.com>
2025-10-08 17:20:40 -0400

    added cloudlog examples in own project and own readme

---
+++ javav2/example_code/cloudwatch-logs/src/main/java/com/example/logs/CloudWatchLogsSearch.java
@@ -0,0 +1,97 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.example.logs;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeLogStreamsResponse;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilterLogEventsRequest;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilterLogEventsResponse;
+import software.amazon.awssdk.services.cloudwatchlogs.model.FilteredLogEvent;
+import software.amazon.awssdk.services.cloudwatchlogs.model.LogStream;
+
+import java.util.List;
+
+
+// snippet-start:[cloudwatch.javav2.read.log.streams.main]
+
+/**
+ * Before running this Java V2 code example, set up your development
+ * environment, including your credentials.
+ * <p>
+ * For more information, see the following documentation topic:
+ * <p>
+ * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
+ */
+public class CloudWatchLogsSearch {
+
+    public static void main(String[] args) {
+        final String usage = """
+
+                Usage:
+                  <logGroupName> <logStreamName> 
+
+                Where:
+                  logGroupName - The name of the log group (for example, WeathertopJavaContainerLogs).
+                  logStreamName - The name of the log stream (for example, weathertop-java-stream).
+                  pattern - the pattern to use (for example, INFO) 
+                  
+                """;
+
+        if (args.length != 3) {
+            System.out.print(usage);
+            System.exit(1);
+        }
+
+        String logGroupName = args[0] ;
+        String logStreamName = args[1] ;
+        String pattern = args[2] ;
+
+        CloudWatchLogsClient cwlClient = CloudWatchLogsClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        searchLogStreamsAndFilterEvents(cwlClient, logGroupName, logStreamName, pattern);
+    }
+
+    /**
+     * Searches for log streams with a specific prefix within a log group and filters log events based on a specified pattern.
+     *
+     * @param cwlClient       the CloudWatchLogsClient used to interact with AWS CloudWatch Logs
+     * @param logGroupName    the name of the log group to search within
+     * @param logStreamPrefix the prefix of the log streams to search for
+     * @param pattern         the pattern to filter log events by
+     */
+    public static void searchLogStreamsAndFilterEvents(CloudWatchLogsClient cwlClient, String logGroupName, String logStreamPrefix, String pattern) {
+        DescribeLogStreamsRequest describeLogStreamsRequest = DescribeLogStreamsRequest.builder()
+                .logGroupName(logGroupName)
+                .logStreamNamePrefix(logStreamPrefix)
+                .build();
+
+        DescribeLogStreamsResponse describeLogStreamsResponse = cwlClient.describeLogStreams(describeLogStreamsRequest);
+
+        List<LogStream> logStreams = describeLogStreamsResponse.logStreams();
+
+        for (LogStream logStream : logStreams) {
+            String logStreamName = logStream.logStreamName();
+            System.out.println("Searching in log stream: " + logStreamName);
+
+            FilterLogEventsRequest filterLogEventsRequest = FilterLogEventsRequest.builder()
+                    .logGroupName(logGroupName)
+                    .logStreamNames(logStreamName)
+                    .filterPattern(pattern)
+                    .build();
+
+            FilterLogEventsResponse filterLogEventsResponse = cwlClient.filterLogEvents(filterLogEventsRequest);
+
+            for (FilteredLogEvent event : filterLogEventsResponse.events()) {
+                System.out.println(event.message());
+            }
+
+            System.out.println("--------------------------------------------------"); // Separator for better readability
+        }
+    }
+}
+// snippet-end:[cloudwatch.javav2.read.log.streams.main]
