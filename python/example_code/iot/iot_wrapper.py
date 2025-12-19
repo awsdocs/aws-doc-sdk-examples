f412f95cb5d2761ef6bb0006a686ecccb360f0ca
tejasgn <tejasgn@amazon.com>
2025-12-19 13:55:35 -0800

    iot basics code changes

---
+++ python/example_code/iot/iot_wrapper.py
@@ -0,0 +1,354 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Purpose
+
+Shows how to use the AWS SDK for Python (Boto3) with AWS IoT to manage things,
+certificates, and topic rules.
+"""
+
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+
+# snippet-start:[python.example_code.iot.IoTWrapper]
+class IoTWrapper:
+    """Encapsulates AWS IoT actions."""
+
+    def __init__(self, iot_client):
+        """
+        :param iot_client: A Boto3 AWS IoT client.
+        """
+        self.iot_client = iot_client
+
+    # snippet-end:[python.example_code.iot.IoTWrapper]
+
+    # snippet-start:[python.example_code.iot.CreateThing]
+    def create_thing(self, thing_name):
+        """
+        Creates an AWS IoT thing.
+
+        :param thing_name: The name of the thing to create.
+        :return: The name and ARN of the created thing.
+        """
+        try:
+            response = self.iot_client.create_thing(thingName=thing_name)
+            logger.info("Created thing %s.", thing_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't create thing %s. Here's why: %s: %s",
+                thing_name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response
+
+    # snippet-end:[python.example_code.iot.CreateThing]
+
+    # snippet-start:[python.example_code.iot.ListThings]
+    def list_things(self):
+        """
+        Lists AWS IoT things.
+
+        :return: The list of things.
+        """
+        try:
+            things = []
+            paginator = self.iot_client.get_paginator("list_things")
+            for page in paginator.paginate():
+                things.extend(page["things"])
+            logger.info("Retrieved %s things.", len(things))
+        except ClientError as err:
+            logger.error(
+                "Couldn't list things. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return things
+
+    # snippet-end:[python.example_code.iot.ListThings]
+
+    # snippet-start:[python.example_code.iot.CreateKeysAndCertificate]
+    def create_keys_and_certificate(self):
+        """
+        Creates keys and a certificate for an AWS IoT thing.
+
+        :return: The certificate ID, ARN, and PEM.
+        """
+        try:
+            response = self.iot_client.create_keys_and_certificate(setAsActive=True)
+            logger.info("Created certificate %s.", response["certificateId"])
+        except ClientError as err:
+            logger.error(
+                "Couldn't create keys and certificate. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response
+
+    # snippet-end:[python.example_code.iot.CreateKeysAndCertificate]
+
+    # snippet-start:[python.example_code.iot.AttachThingPrincipal]
+    def attach_thing_principal(self, thing_name, principal):
+        """
+        Attaches a certificate to an AWS IoT thing.
+
+        :param thing_name: The name of the thing.
+        :param principal: The ARN of the certificate.
+        """
+        try:
+            self.iot_client.attach_thing_principal(
+                thingName=thing_name, principal=principal
+            )
+            logger.info("Attached principal %s to thing %s.", principal, thing_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't attach principal to thing. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.AttachThingPrincipal]
+
+    # snippet-start:[python.example_code.iot.DescribeEndpoint]
+    def describe_endpoint(self, endpoint_type="iot:Data-ATS"):
+        """
+        Gets the AWS IoT endpoint.
+
+        :param endpoint_type: The endpoint type.
+        :return: The endpoint.
+        """
+        try:
+            response = self.iot_client.describe_endpoint(endpointType=endpoint_type)
+            logger.info("Retrieved endpoint %s.", response["endpointAddress"])
+        except ClientError as err:
+            logger.error(
+                "Couldn't describe endpoint. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response["endpointAddress"]
+
+    # snippet-end:[python.example_code.iot.DescribeEndpoint]
+
+    # snippet-start:[python.example_code.iot.ListCertificates]
+    def list_certificates(self):
+        """
+        Lists AWS IoT certificates.
+
+        :return: The list of certificates.
+        """
+        try:
+            certificates = []
+            paginator = self.iot_client.get_paginator("list_certificates")
+            for page in paginator.paginate():
+                certificates.extend(page["certificates"])
+            logger.info("Retrieved %s certificates.", len(certificates))
+        except ClientError as err:
+            logger.error(
+                "Couldn't list certificates. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return certificates
+
+    # snippet-end:[python.example_code.iot.ListCertificates]
+
+    # snippet-start:[python.example_code.iot.DetachThingPrincipal]
+    def detach_thing_principal(self, thing_name, principal):
+        """
+        Detaches a certificate from an AWS IoT thing.
+
+        :param thing_name: The name of the thing.
+        :param principal: The ARN of the certificate.
+        """
+        try:
+            self.iot_client.detach_thing_principal(
+                thingName=thing_name, principal=principal
+            )
+            logger.info("Detached principal %s from thing %s.", principal, thing_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't detach principal from thing. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.DetachThingPrincipal]
+
+    # snippet-start:[python.example_code.iot.DeleteCertificate]
+    def delete_certificate(self, certificate_id):
+        """
+        Deletes an AWS IoT certificate.
+
+        :param certificate_id: The ID of the certificate to delete.
+        """
+        try:
+            self.iot_client.update_certificate(
+                certificateId=certificate_id, newStatus="INACTIVE"
+            )
+            self.iot_client.delete_certificate(certificateId=certificate_id)
+            logger.info("Deleted certificate %s.", certificate_id)
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete certificate. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.DeleteCertificate]
+
+    # snippet-start:[python.example_code.iot.CreateTopicRule]
+    def create_topic_rule(self, rule_name, topic, sns_action_arn, role_arn):
+        """
+        Creates an AWS IoT topic rule.
+
+        :param rule_name: The name of the rule.
+        :param topic: The MQTT topic to subscribe to.
+        :param sns_action_arn: The ARN of the SNS topic to publish to.
+        :param role_arn: The ARN of the IAM role.
+        """
+        try:
+            self.iot_client.create_topic_rule(
+                ruleName=rule_name,
+                topicRulePayload={
+                    "sql": f"SELECT * FROM '{topic}'",
+                    "actions": [
+                        {"sns": {"targetArn": sns_action_arn, "roleArn": role_arn}}
+                    ],
+                },
+            )
+            logger.info("Created topic rule %s.", rule_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't create topic rule. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.CreateTopicRule]
+
+    # snippet-start:[python.example_code.iot.ListTopicRules]
+    def list_topic_rules(self):
+        """
+        Lists AWS IoT topic rules.
+
+        :return: The list of topic rules.
+        """
+        try:
+            rules = []
+            paginator = self.iot_client.get_paginator("list_topic_rules")
+            for page in paginator.paginate():
+                rules.extend(page["rules"])
+            logger.info("Retrieved %s topic rules.", len(rules))
+        except ClientError as err:
+            logger.error(
+                "Couldn't list topic rules. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return rules
+
+    # snippet-end:[python.example_code.iot.ListTopicRules]
+
+    # snippet-start:[python.example_code.iot.SearchIndex]
+    def search_index(self, query):
+        """
+        Searches the AWS IoT index.
+
+        :param query: The search query.
+        :return: The list of things found.
+        """
+        try:
+            response = self.iot_client.search_index(queryString=query)
+            logger.info("Found %s things.", len(response.get("things", [])))
+        except ClientError as err:
+            logger.error(
+                "Couldn't search index. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response.get("things", [])
+
+    # snippet-end:[python.example_code.iot.SearchIndex]
+
+    # snippet-start:[python.example_code.iot.UpdateIndexingConfiguration]
+    def update_indexing_configuration(self):
+        """
+        Updates the AWS IoT indexing configuration to enable thing indexing.
+        """
+        try:
+            self.iot_client.update_indexing_configuration(
+                thingIndexingConfiguration={"thingIndexingMode": "REGISTRY"}
+            )
+            logger.info("Updated indexing configuration.")
+        except ClientError as err:
+            logger.error(
+                "Couldn't update indexing configuration. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.UpdateIndexingConfiguration]
+
+    # snippet-start:[python.example_code.iot.DeleteThing]
+    def delete_thing(self, thing_name):
+        """
+        Deletes an AWS IoT thing.
+
+        :param thing_name: The name of the thing to delete.
+        """
+        try:
+            self.iot_client.delete_thing(thingName=thing_name)
+            logger.info("Deleted thing %s.", thing_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete thing. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.DeleteThing]
+
+    # snippet-start:[python.example_code.iot.DeleteTopicRule]
+    def delete_topic_rule(self, rule_name):
+        """
+        Deletes an AWS IoT topic rule.
+
+        :param rule_name: The name of the rule to delete.
+        """
+        try:
+            self.iot_client.delete_topic_rule(ruleName=rule_name)
+            logger.info("Deleted topic rule %s.", rule_name)
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete topic rule. Here's why: %s: %s",
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+
+    # snippet-end:[python.example_code.iot.DeleteTopicRule]
