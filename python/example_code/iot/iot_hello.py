bcfdae411f223075bf0f13caad3d8392e75a521b
Rachel Hagerman <rlhagerm@amazon.com>
2026-01-20 13:37:37 -0600

    Update iot_hello.py

--- python/example_code/iot/iot_hello.py
+++ python/example_code/iot/iot_hello.py
@@ -4,7 +4,7 @@
 """
 Purpose
 
-Shows how to get started with AWS IoT by describing the IoT endpoint.
+Shows how to get started with AWS IoT by listing up to 10 things.
 """
 
 import boto3
@@ -14,16 +14,22 @@ from botocore.exceptions import ClientError, NoCredentialsError
 # snippet-start:[python.example_code.iot.Hello]
 def hello_iot():
     """
-    Use the AWS SDK for Python (Boto3) to create an AWS IoT client and describe
-    the IoT endpoint for your account.
+    Use the AWS SDK for Python (Boto3) to create an AWS IoT client and list
+    up to 10 things in your AWS IoT account.
     This example uses the default settings specified in your shared credentials
     and config files.
     """
     try:
         iot_client = boto3.client("iot")
-        response = iot_client.describe_endpoint(endpointType="iot:Data-ATS")
-        endpoint = response["endpointAddress"]
-        print(f"Hello, AWS IoT! Your endpoint is: {endpoint}")
+        response = iot_client.list_things(maxResults=10)
+        things = response.get("things", [])
+        
+        print("Hello, AWS IoT! Here are your things:")
+        if things:
+            for i, thing in enumerate(things, 1):
+                print(f"{i}. {thing['thingName']}")
+        else:
+            print("No things found in your AWS IoT account.")
     except ClientError as e:
         if e.response["Error"]["Code"] == "UnauthorizedException":
             print("You don't have permission to access AWS IoT.")
