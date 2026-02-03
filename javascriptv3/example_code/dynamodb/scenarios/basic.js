6c6c43d7a970c79a6502eba43d20594b63954b34
Brian Murray <40031786+brmur@users.noreply.github.com>
2026-02-03 12:40:10 +0000

    Update basic.js

--- javascriptv3/example_code/dynamodb/scenarios/basic.js
+++ javascriptv3/example_code/dynamodb/scenarios/basic.js
@@ -1,4 +1,4 @@
-U789// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import { fileURLToPath } from "node:url";
