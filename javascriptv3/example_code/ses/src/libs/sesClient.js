7e9e4236bc2d0849ecfb0e6b4707a6e910c2d674
Sai Varshith Kothwali <81505138+saivarshithkothwali@users.noreply.github.com>
2026-03-18 17:05:54 +0530

    Add comment explaining AWS credential provider chain (#7771)
    * Add comment explaining AWS credential provider chain
    
    Adds a comment explaining how AWS SDK resolves credentials using the default credential provider chain (environment variables, shared credentials files, and IAM roles). This may help developers who are unfamiliar with how credentials are automatically discovered by the SDK.
    
    * Shorten credential provider chain comment based on review feedback

--- javascriptv3/example_code/ses/src/libs/sesClient.js
+++ javascriptv3/example_code/ses/src/libs/sesClient.js
@@ -14,6 +14,8 @@ sesClient.js is a helper function that creates an Amazon Simple Email Services (
 import { SESClient } from "@aws-sdk/client-ses";
 // Set the AWS Region.
 const REGION = "us-east-1";
+// Credentials are automatically resolved using the AWS SDK credential provider chain.
+// For more information, see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/setting-credentials-node.html
 // Create SES service object.
 const sesClient = new SESClient({ region: REGION });
 export { sesClient };
