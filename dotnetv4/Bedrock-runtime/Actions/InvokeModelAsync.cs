c2a7a001f519df4ea3116b8754a7efbf01ce0566
Brian Murray <brmur@amazon.com>
2026-07-15 18:02:01 +0000

    Fix dotnetv4: Library OutputType, remove unused using, fix final newline

--- dotnetv4/Bedrock-runtime/Actions/InvokeModelAsync.cs
+++ dotnetv4/Bedrock-runtime/Actions/InvokeModelAsync.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Text.Json;
 using System.Text.Json.Nodes;
 using Amazon;
 using Amazon.BedrockRuntime;
