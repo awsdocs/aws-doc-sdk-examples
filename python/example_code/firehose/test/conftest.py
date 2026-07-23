66800f1d63a6ac0eebed3dd4ee66f839050a66e8
Scott Macdonald <scmacdon@amazon.com>
2026-07-23 17:49:03 -0400

    fix: Remove unused CFN param, add conftest.py, fix test stub, remove duplicate firehose-put-actions, fix README run command, add stream deletion wait

---
+++ python/example_code/firehose/test/conftest.py
@@ -0,0 +1,7 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
