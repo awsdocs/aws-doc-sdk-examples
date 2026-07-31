2369284f4da3e3828faa2536f91bf8628421d608
brmur <brmur@amazon.com>
2026-07-31 11:52:45 +0000

    fix(tools): Add missing SPDX headers to shell scripts

--- .tools/docker-images/build-all.sh
+++ .tools/docker-images/build-all.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # Build and push all SDK example Docker images to ECR
 #
 # Usage:
--- .tools/docker-images/run-in-container.sh
+++ .tools/docker-images/run-in-container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 # Run SDK example tests using the pre-built Docker image for a language.
 #
 # Usage:
